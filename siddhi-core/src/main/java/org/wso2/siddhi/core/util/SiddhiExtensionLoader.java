2bb984021f6b5517a6dad5dba12ec57b5b4cbf21
sacjaya <sachinijayasekara.g@gmail.com>
2014-09-26 13:16:57 +0530

    adding extension loader

--- siddhi-core/src/main/java/org/wso2/siddhi/core/util/SiddhiExtensionLoader.java
+++ siddhi-core/src/main/java/org/wso2/siddhi/core/util/SiddhiExtensionLoader.java
@@ -23,89 +23,76 @@ import org.apache.log4j.Logger;
 
 import java.io.*;
 import java.util.*;
-import java.util.jar.JarFile;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
 public class SiddhiExtensionLoader {
 
     private static final String CLASS_PATH = "java.class.path";
+    private static final String SIDDHI_EXT = ".*\\.siddhiext";
     private static final Logger log = Logger.getLogger(SiddhiExtensionLoader.class);
 
     /**
-     * Helper method to load the siddhi config
+     * Helper method to load the siddhi extensions
      */
     public static Map<String, Class> loadSiddhiExtensions() {
         String classPath = System.getProperty(CLASS_PATH, ".");
         String[] classPathElements = classPath.split(":");
+        Pattern pattern = Pattern.compile(SIDDHI_EXT);
+        Collection<String> extensionsList = new ArrayList<String>();
 
-        ArrayList<String> jarFilesInClassPath = new ArrayList<String>();
-
-        Pattern pattern = Pattern.compile(".*.jar");
         for (String element : classPathElements) {
-            if (pattern.matcher(element).matches()) {
-                jarFilesInClassPath.add(element);
-            } else {
-                jarFilesInClassPath.addAll(getResourcesFromDirectory(new File(element), pattern));
-            }
+            extensionsList.addAll(getResources(element, pattern));
         }
 
         Map<String, Class> classMap = new HashMap<String, Class>();
 
-        for (String filePath : jarFilesInClassPath) {
-            File file = new File(filePath);
-            Collection<String> siddhiExtensionResource = getResourcesFromJarFile(file, Pattern.compile(".*siddhi.extension"));
-
-            if (!siddhiExtensionResource.isEmpty()) {
-                for (String resourceName : siddhiExtensionResource) {
-                    try {
-                        JarFile jar = new JarFile(file);
-                        ZipEntry entry = jar.getEntry(resourceName);
-                        InputStream inputStream = jar.getInputStream(entry);
-
-                        try {
-                            BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(inputStream)));
-                            String extensionDetails;
-                            while ((extensionDetails = br.readLine()) != null) {
-                                String[] info = extensionDetails.split("=");
-                                try {
-                                    classMap.put(info[0].trim(), Class.forName(info[1].trim()));
-                                } catch (ClassNotFoundException e) {
-                                    log.error("Cannot load Siddhi extension " + info[1].trim(), e);
-                                }
-                            }
-                        } finally {
-                            inputStream.close();
-                        }
-                    } catch (IOException e) {
-                        log.error("unable to get file " + file, e);
-                    }
-                }
+        for (String extension : extensionsList) {
+            String[] info = extension.split("=");
+            try {
+                classMap.put(info[0].trim(), Class.forName(info[1].trim()));
+            } catch (ClassNotFoundException e) {
+               log.error("Cannot load Siddhi extension " + info[1].trim(), e);
             }
-
         }
-
         return classMap;
     }
 
+    private static Collection<String> getResources(String element,Pattern pattern){
+        ArrayList<String> resources = new ArrayList<String>();
+        File file = new File(element);
+        if(file.isDirectory()){
+            resources.addAll(getContentFromDirectory(file, pattern));
+        } else{
+            resources.addAll(getContentFromJarFile(file, pattern));
+        }
+        return resources;
+    }
 
-    private static Collection<String> getResourcesFromDirectory(File directory, Pattern pattern) {
+    private static Collection<String> getContentFromDirectory(File directory, Pattern pattern) {
         ArrayList<String> resources = new ArrayList<String>();
         File[] fileList = directory.listFiles();
         if (fileList != null) {
             for (File file : fileList) {
                 if (file.isDirectory()) {
-                    resources.addAll(getResourcesFromDirectory(file, pattern));
+                    resources.addAll(getContentFromDirectory(file, pattern));
                 } else {
                     try {
                         String fileName = file.getCanonicalPath();
-                        if (pattern.matcher(fileName).matches()) {
-                            resources.add(fileName);
+                        Pattern jar  =Pattern.compile(".*.jar");
+                        if(jar.matcher(fileName).matches()){
+                            resources.addAll(getContentFromJarFile(new File(fileName), pattern));
+                        } else if (pattern.matcher(fileName).matches()) {
+                            try{
+                                InputStream inputStream = new FileInputStream(fileName);
+                                resources.addAll(readContent(fileName, inputStream));
+                            } catch (IOException ex) {
+                                log.error("unable to get input stream of "+ fileName, ex);
+                            }
                         }
                     } catch (IOException e) {
-                        log.error("Can not get canonical path of file:" + file, e);
+                        log.error("unable to get canonical path of file " + file, e);
                     }
                 }
             }
@@ -113,28 +100,55 @@ public class SiddhiExtensionLoader {
         return resources;
     }
 
-    private static Collection<String> getResourcesFromJarFile(File file, Pattern pattern) {
+    private static Collection<String> getContentFromJarFile(File file, Pattern pattern){
         ArrayList<String> resources = new ArrayList<String>();
-        ZipFile zf;
-        try {
+        ZipFile zf = null;
+        try{
             zf = new ZipFile(file);
-        } catch (ZipException e) {
-            throw new Error(e);
-        } catch (IOException e) {
-            throw new Error(e);
+        } catch( IOException e){
+            log.error("Error creating zip file ", e);
         }
-        Enumeration e = zf.entries();
-        while (e.hasMoreElements()) {
-            ZipEntry ze = (ZipEntry) e.nextElement();
-            String fileName = ze.getName();
-            if (pattern.matcher(fileName).matches()) {
-                resources.add(fileName);
+
+        if (zf != null) {
+            Enumeration e = zf.entries();
+            while(e.hasMoreElements()){
+                ZipEntry ze = (ZipEntry) e.nextElement();
+                String fileName = ze.getName();
+                if(pattern.matcher(fileName).matches()){
+                    try{
+                        InputStream inputStream = zf.getInputStream(ze);
+                        resources.addAll(readContent(fileName, inputStream));
+                    } catch (IOException ex) {
+                        log.error("unable to get input stream of " + fileName, ex);
+                    }
+                }
+            }
+            try{
+                zf.close();
+            } catch( IOException e1){
+                log.error("Error closing zip file ", e1);
             }
         }
-        try {
-            zf.close();
-        } catch (IOException e1) {
-            throw new Error(e1);
+
+        return resources;
+    }
+
+    public static Collection<String> readContent(String fileName,InputStream inputStream){
+        ArrayList<String> resources = new ArrayList<String>();
+        String[] file = fileName.split("/");
+        String namespace = file[file.length-1].split("\\.")[0];
+        try{
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(inputStream)));
+                String extensionDetails;
+                while ((extensionDetails = br.readLine()) != null) {
+                    resources.add(namespace+":"+extensionDetails);
+                }
+            } finally {
+                inputStream.close();
+            }
+        } catch (IOException ex) {
+            log.error("unable to read file " + fileName + ex);
         }
         return resources;
     }
