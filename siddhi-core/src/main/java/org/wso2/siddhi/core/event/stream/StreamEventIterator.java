0fae636d7fe28eda50aa8a1dbcd53ecaadf4e122
sacjaya <sachinijayasekara.g@gmail.com>
2014-10-06 16:58:15 +0530

    adding StreamEventIterator

--- siddhi-core/src/main/java/org/wso2/siddhi/core/event/stream/StreamEventIterator.java
+++ siddhi-core/src/main/java/org/wso2/siddhi/core/event/stream/StreamEventIterator.java
@@ -20,25 +20,85 @@
 package org.wso2.siddhi.core.event.stream;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class StreamEventIterator implements Iterator<StreamEvent> {
-    StreamEvent previous;
-    StreamEvent currentEvent;
+    StreamEvent previousToLastReturned;
+    StreamEvent lastReturned;
+    StreamEvent first;
 
-    public boolean hasNext() {
-        return currentEvent.getNext()!=null;
+    public void setEvent(StreamEvent streamEvent) {
+        first = streamEvent;
     }
 
+    /**
+     * Returns <tt>true</tt> if the iteration has more elements. (In other
+     * words, returns <tt>true</tt> if <tt>next</tt> would return an element
+     * rather than throwing an exception.)
+     *
+     * @return <tt>true</tt> if the iterator has more elements.
+     */
+    public boolean hasNext() {
+        if (lastReturned != null) {
+            return lastReturned.getNext() != null;
+        } else if (previousToLastReturned != null) {
+            return previousToLastReturned.getNext() != null;
+        } else {
+            return first != null;
+        }
+    }
+
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return the next element in the iteration.
+     * @throws java.util.NoSuchElementException
+     *          iteration has no more elements.
+     */
     public StreamEvent next() {
-        StreamEvent returnEvent =  currentEvent;
-        previous = currentEvent;
-        currentEvent = currentEvent.getNext();
-        returnEvent.setNext(null);
+        StreamEvent returnEvent;
+        if (lastReturned != null) {
+            returnEvent = lastReturned.getNext();
+            previousToLastReturned = lastReturned;
+        } else if (previousToLastReturned != null) {
+            returnEvent = previousToLastReturned.getNext();
+        } else {
+            returnEvent = first;
+        }
+        if (returnEvent == null) {
+            throw new NoSuchElementException();
+        }
+        lastReturned = returnEvent;
         return returnEvent;
     }
 
+    /**
+     * Removes from the underlying collection the last element returned by the
+     * iterator (optional operation).  This method can be called only once per
+     * call to <tt>next</tt>.  The behavior of an iterator is unspecified if
+     * the underlying collection is modified while the iteration is in
+     * progress in any way other than by calling this method.
+     *
+     * @throws UnsupportedOperationException if the <tt>remove</tt>
+     *                                       operation is not supported by this Iterator.
+     * @throws IllegalStateException         if the <tt>next</tt> method has not
+     *                                       yet been called, or the <tt>remove</tt> method has already
+     *                                       been called after the last call to the <tt>next</tt>
+     *                                       method.
+     */
     public void remove() {
-         currentEvent = currentEvent.getNext();
-         previous.setNext(currentEvent);
+        if (lastReturned == null) {
+            throw new IllegalStateException();
+        }
+        if (previousToLastReturned != null) {
+            previousToLastReturned.setNext(lastReturned.getNext());
+        } else {
+            first = lastReturned.getNext();
+        }
+        lastReturned = null;
+    }
+
+    public StreamEvent getFirstElement() {
+        return first;
     }
 }
