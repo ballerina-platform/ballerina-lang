284e272b2df51fa3810d0d43aa771a05f40eed19
tishan89 <dtishanpubudu@gmail.com>
2015-02-17 11:50:04 +0530

    fixing time window test case

--- siddhi-core/src/test/java/org/wso2/siddhi/core/query/window/TimeWindowTestCase.java
+++ siddhi-core/src/test/java/org/wso2/siddhi/core/query/window/TimeWindowTestCase.java
@@ -61,7 +61,7 @@ public class TimeWindowTestCase {
                     inEventCount = inEventCount + inEvents.length;
                 }
                 if (removeEvents != null) {
-                    Assert.assertEquals("InEvents arrived before RemoveEvents", inEventCount - 2, removeEventCount);
+                    Assert.assertTrue("InEvents arrived before RemoveEvents", inEventCount > removeEventCount);
                     removeEventCount = removeEventCount + removeEvents.length;
                 }
                 eventArrived = true;
@@ -73,7 +73,7 @@ public class TimeWindowTestCase {
         executionPlanRuntime.start();
         inputHandler.send(new Object[]{"IBM", 700f, 0});
         inputHandler.send(new Object[]{"WSO2", 60.5f, 1});
-        Thread.sleep(5000);
+        Thread.sleep(4000);
         Assert.assertEquals(2, inEventCount);
         Assert.assertEquals(2, removeEventCount);
         Assert.assertTrue(eventArrived);
@@ -86,49 +86,49 @@ public class TimeWindowTestCase {
      * Commenting out intermittent failing test case until fix this properly.
      */
 
-//    @Test
-//    public void timeWindowTest2() throws InterruptedException {
-//
-//        SiddhiManager siddhiManager = new SiddhiManager();
-//
-//        String cseEventStream = "define stream cseEventStream (symbol string, price float, volume int);";
-//        String query = "@info(name = 'query1') from cseEventStream#window.time(1 sec) select symbol,price," +
-//                "volume insert into outputStream ;";
-//
-//        ExecutionPlanRuntime executionPlanRuntime = siddhiManager.createExecutionPlanRuntime(cseEventStream + query);
-//
-//        executionPlanRuntime.addCallback("query1", new QueryCallback() {
-//            @Override
-//            public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
-//                EventPrinter.print(timeStamp, inEvents, removeEvents);
-//                if (inEvents != null) {
-//                    inEventCount = inEventCount + inEvents.length;
-//                }
-//                if (removeEvents != null) {
-//                    Assert.assertEquals("InEvents arrived before RemoveEvents", inEventCount - 2, removeEventCount);
-//                    removeEventCount = removeEventCount + removeEvents.length;
-//                }
-//                eventArrived = true;
-//            }
-//
-//        });
-//
-//        InputHandler inputHandler = executionPlanRuntime.getInputHandler("cseEventStream");
-//        executionPlanRuntime.start();
-//        inputHandler.send(new Object[]{"IBM", 700f, 1});
-//        inputHandler.send(new Object[]{"WSO2", 60.5f, 2});
-//        Thread.sleep(1100);
-//        inputHandler.send(new Object[]{"IBM", 700f, 3});
-//        inputHandler.send(new Object[]{"WSO2", 60.5f, 4});
-//        Thread.sleep(1100);
-//        inputHandler.send(new Object[]{"IBM", 700f, 5});
-//        inputHandler.send(new Object[]{"WSO2", 60.5f, 6});
-//        Thread.sleep(4000);
-//        Assert.assertEquals(6, inEventCount);
-//        Assert.assertEquals(6, removeEventCount);
-//        Assert.assertTrue(eventArrived);
-//        executionPlanRuntime.shutdown();
-//
-//    }
+    @Test
+    public void timeWindowTest2() throws InterruptedException {
+
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String cseEventStream = "define stream cseEventStream (symbol string, price float, volume int);";
+        String query = "@info(name = 'query1') from cseEventStream#window.time(1 sec) select symbol,price," +
+                "volume insert into outputStream ;";
+
+        ExecutionPlanRuntime executionPlanRuntime = siddhiManager.createExecutionPlanRuntime(cseEventStream + query);
+
+        executionPlanRuntime.addCallback("query1", new QueryCallback() {
+            @Override
+            public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+                EventPrinter.print(timeStamp, inEvents, removeEvents);
+                if (inEvents != null) {
+                    inEventCount = inEventCount + inEvents.length;
+                }
+                if (removeEvents != null) {
+                    Assert.assertTrue("InEvents arrived before RemoveEvents", inEventCount > removeEventCount);
+                    removeEventCount = removeEventCount + removeEvents.length;
+                }
+                eventArrived = true;
+            }
+
+        });
+
+        InputHandler inputHandler = executionPlanRuntime.getInputHandler("cseEventStream");
+        executionPlanRuntime.start();
+        inputHandler.send(new Object[]{"IBM", 700f, 1});
+        inputHandler.send(new Object[]{"WSO2", 60.5f, 2});
+        Thread.sleep(1100);
+        inputHandler.send(new Object[]{"IBM", 700f, 3});
+        inputHandler.send(new Object[]{"WSO2", 60.5f, 4});
+        Thread.sleep(1100);
+        inputHandler.send(new Object[]{"IBM", 700f, 5});
+        inputHandler.send(new Object[]{"WSO2", 60.5f, 6});
+        Thread.sleep(4000);
+        Assert.assertEquals(6, inEventCount);
+        Assert.assertEquals(6, removeEventCount);
+        Assert.assertTrue(eventArrived);
+        executionPlanRuntime.shutdown();
+
+    }
 
 }
