9d6b04843f92339d360dca2589d465acdd71b99a
Isuru Fernando <isuruf@gmail.com>
2015-03-03 17:43:08 +0530

    Port geocode extension to Siddhi-3.0.0

---
+++ siddhi-extensions/geo/src/main/java/org/wso2/siddhi/extension/geo/GeocodeStreamFunctionProcessor.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) 2005-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.siddhi.extension.geo;
+
+import com.google.code.geocoder.Geocoder;
+import com.google.code.geocoder.GeocoderRequestBuilder;
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.GeocoderRequest;
+import org.apache.log4j.Logger;
+import org.wso2.siddhi.core.config.ExecutionPlanContext;
+import org.wso2.siddhi.core.exception.ExecutionPlanCreationException;
+import org.wso2.siddhi.core.executor.ExpressionExecutor;
+import org.wso2.siddhi.core.query.processor.stream.function.StreamFunctionProcessor;
+import org.wso2.siddhi.query.api.definition.AbstractDefinition;
+import org.wso2.siddhi.query.api.definition.Attribute;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This extension transforms a location into its geo-coordinates and formatted
+ * address
+ */
+public class GeocodeStreamFunctionProcessor extends StreamFunctionProcessor {
+
+    private static final Logger LOGGER = Logger.getLogger(GeocodeStreamFunctionProcessor.class);
+    private final Geocoder geocoder = new Geocoder();
+    private boolean debugModeOn;
+
+    /**
+     * The process method of the StreamFunction, used when more then one function parameters are provided
+     *
+     * @param data the data values for the function parameters
+     * @return the data for additional output attributes introduced by the function
+     */
+    @Override
+    protected Object[] process(Object[] data) {
+        return process(data[0]);
+    }
+
+    /**
+     * The process method of the StreamFunction, used when zero or one function parameter is provided
+     *
+     * @param data null if the function parameter count is zero or runtime data value of the function parameter
+     * @return the data for additional output attribute introduced by the function
+     */
+    @Override
+    protected Object[] process(Object data) {
+        String location = data.toString();
+
+        // Make the geocode request to API library
+        GeocoderRequest geocoderRequest = new GeocoderRequestBuilder()
+                .setAddress(location)
+                .setLanguage("en")
+                .getGeocoderRequest();
+
+        double latitude, longitude;
+        String formattedAddress;
+        try {
+            GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
+
+            if (!geocoderResponse.getResults().isEmpty()) {
+                latitude = geocoderResponse.getResults().get(0).getGeometry().getLocation()
+                        .getLat().doubleValue();
+                longitude = geocoderResponse.getResults().get(0).getGeometry().getLocation()
+                        .getLng().doubleValue();
+                formattedAddress = geocoderResponse.getResults().get(0).getFormattedAddress();
+            } else {
+                latitude = -1.0;
+                longitude = -1.0;
+                formattedAddress = "N/A";
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error in connection to Google Maps API.");
+        }
+
+        if (debugModeOn) {
+            LOGGER.debug("Formatted address: " + formattedAddress + ", Location coordinates: (" +
+                    latitude + ", " + longitude + ")");
+        }
+        return new Object[]{formattedAddress, latitude, longitude};
+    }
+
+    /**
+     * The init method of the StreamProcessor, this method will be called before other methods
+     *
+     * @param inputDefinition              the incoming stream definition
+     * @param attributeExpressionExecutors the executors of each function parameters
+     * @param executionPlanContext         the context of the execution plan
+     * @return the additional output attributes introduced by the function
+     */
+    @Override
+    protected List<Attribute> init(AbstractDefinition inputDefinition, ExpressionExecutor[] attributeExpressionExecutors, ExecutionPlanContext executionPlanContext) {
+        debugModeOn = LOGGER.isDebugEnabled();
+        if (attributeExpressionExecutors[0].getReturnType() != Attribute.Type.STRING) {
+            throw new ExecutionPlanCreationException("First parameter should be of type string");
+        }
+        ArrayList<Attribute> attributes = new ArrayList<Attribute>(6);
+        attributes.add(new Attribute("formattedAddress", Attribute.Type.STRING));
+        attributes.add(new Attribute("latitude", Attribute.Type.DOUBLE));
+        attributes.add(new Attribute("longitude", Attribute.Type.DOUBLE));
+        return attributes;
+    }
+
+    /**
+     * This will be called only once and this can be used to acquire
+     * required resources for the processing element.
+     * This will be called after initializing the system and before
+     * starting to process the events.
+     */
+    @Override
+    public void start() {
+
+    }
+
+    /**
+     * This will be called only once and this can be used to release
+     * the acquired resources for processing.
+     * This will be called before shutting down the system.
+     */
+    @Override
+    public void stop() {
+
+    }
+
+    /**
+     * Used to collect the serializable state of the processing element, that need to be
+     * persisted for the reconstructing the element to the same state on a different point of time
+     *
+     * @return stateful objects of the processing element as an array
+     */
+    @Override
+    public Object[] currentState() {
+        return new Object[0];
+    }
+
+    /**
+     * Used to restore serialized state of the processing element, for reconstructing
+     * the element to the same state as if was on a previous point of time.
+     *
+     * @param state the stateful objects of the element as an array on
+     *              the same order provided by currentState().
+     */
+    @Override
+    public void restoreState(Object[] state) {
+
+    }
+}
---
+++ siddhi-extensions/geo/src/test/java/org/wso2/siddhi/extension/geo/GeocodeStreamFunctionProcessorTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2005-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.siddhi.extension.geo;
+
+import junit.framework.Assert;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.wso2.siddhi.core.ExecutionPlanRuntime;
+import org.wso2.siddhi.core.SiddhiManager;
+import org.wso2.siddhi.core.event.Event;
+import org.wso2.siddhi.core.query.output.callback.QueryCallback;
+import org.wso2.siddhi.core.stream.input.InputHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeocodeStreamFunctionProcessorTest {
+
+    private static final Logger LOGGER = Logger.getLogger(GeocodeStreamFunctionProcessorTest.class);
+    private static int eventCount = 0;
+
+    @Test
+    public void testProcess() throws Exception {
+        LOGGER.info("Init Siddhi setUp");
+
+        SiddhiManager siddhiManager = new SiddhiManager();
+        long start = System.currentTimeMillis();
+        ExecutionPlanRuntime executionPlanRuntime = siddhiManager.createExecutionPlanRuntime("@config(async = 'true') define stream geocodeStream (location string, level string, time string);"
+                + "@info(name = 'query1') from geocodeStream#geo:geocode(location) " +
+                " select latitude, longitude, formattedAddress " +
+                " insert into dataOut");
+        long end = System.currentTimeMillis();
+        LOGGER.info(String.format("Time to add query: [%f sec]", ((end - start) / 1000f)));
+
+        List<Object[]> data = new ArrayList<Object[]>();
+        data.add(new Object[]{"gunasekara mawatha", "Regular", "Sun Nov 02 13:36:05 +0000 2014"});
+        data.add(new Object[]{"hendala road", "Regular", "Sun Nov 12 13:36:05 +0000 2014"});
+        data.add(new Object[]{"mt lavinia", "Regular", "Sun Nov 10 13:36:05 +0000 2014"});
+        data.add(new Object[]{"duplication rd", "Regular", "Sun Nov 02 13:36:05 +0000 2014"});
+
+        final List<Object[]> expectedResult = new ArrayList<Object[]>();
+        expectedResult.add(new Object[]{5.9461591d, 80.4978628d, "Gunasekara Mawatha, Matara, Sri Lanka"});
+        expectedResult.add(new Object[]{6.9955449d, 79.8832741d, "Hendala Road, Wattala, Sri Lanka"});
+        expectedResult.add(new Object[]{6.8390463d, 79.8646835d, "Mount Lavinia, Sri Lanka"});
+        expectedResult.add(new Object[]{6.8995244d, 79.8556202d, "R A De Mel Mawatha, Colombo, Sri Lanka"});
+
+
+        executionPlanRuntime.addCallback("query1", new QueryCallback() {
+            @Override
+            public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+                for (Event event : inEvents) {
+                    Object[] expected = expectedResult.get(eventCount);
+                    Assert.assertEquals((Double) expected[0], (Double) event.getData(0), 1e-6);
+                    Assert.assertEquals((Double) expected[1], (Double) event.getData(1), 1e-6);
+                    Assert.assertEquals(expected[2], event.getData(2));
+                    eventCount++;
+                }
+            }
+        });
+
+        executionPlanRuntime.start();
+
+        InputHandler inputHandler = executionPlanRuntime.getInputHandler("geocodeStream");
+        for (Object[] dataLine : data) {
+            inputHandler.send(dataLine);
+        }
+        Thread.sleep(2000);
+    }
+}
