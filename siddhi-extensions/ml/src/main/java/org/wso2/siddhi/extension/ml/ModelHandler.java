18b68e9a64c1f18732b8f4218f3ddee2aa949c15
manoramahp <manorama@wso2.com>
2015-05-15 15:51:58 +0530

    ml:predict extension

---
+++ siddhi-extensions/ml/src/main/java/org/wso2/siddhi/extension/ml/ModelHandler.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.siddhi.extension.ml;
+
+import org.wso2.carbon.ml.commons.constants.MLConstants;
+import org.wso2.carbon.ml.commons.domain.Feature;
+import org.wso2.carbon.ml.commons.domain.MLModel;
+import org.wso2.carbon.ml.core.exceptions.MLInputAdapterException;
+import org.wso2.carbon.ml.core.exceptions.MLModelBuilderException;
+import org.wso2.carbon.ml.core.impl.MLIOFactory;
+import org.wso2.carbon.ml.core.impl.Predictor;
+import org.wso2.carbon.ml.core.interfaces.MLInputAdapter;
+import org.wso2.carbon.ml.core.utils.MLCoreServiceValueHolder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ModelHandler {
+
+    public static final String REGISTRY_STORAGE_PREFIX = "registry";
+    public static final String PATH_TO_GOVERNANCE_REGISTRY = "/_system/governance";
+
+    private MLModel mlModel;
+    private long modelId;
+
+    /**
+     *
+     * @param modelStorageLocation MLModel storage location
+     * @throws ClassNotFoundException
+     * @throws java.net.URISyntaxException
+     * @throws MLInputAdapterException
+     * @throws java.io.IOException
+     */
+    public ModelHandler(String modelStorageLocation)
+            throws ClassNotFoundException, URISyntaxException, MLInputAdapterException, IOException {
+        mlModel = retrieveModel(modelStorageLocation);
+    }
+
+    /**
+     * Retrieve the MLModel from the storage location
+     * @param modelStorageLocation model storage location (file path or registry path)
+     *                             file: -> file path
+     *                             registry: -> registry path
+     * @return
+     * @throws URISyntaxException
+     * @throws MLInputAdapterException
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private static MLModel retrieveModel(String modelStorageLocation)
+            throws URISyntaxException, MLInputAdapterException, IOException, ClassNotFoundException {
+
+        String[] modelStorage = modelStorageLocation.trim().split(":");
+        String storageType = modelStorage[0];
+        if(storageType.equals(REGISTRY_STORAGE_PREFIX)) {
+            if(modelStorage[1].startsWith(PATH_TO_GOVERNANCE_REGISTRY)) {
+                modelStorageLocation = modelStorage[1].substring(PATH_TO_GOVERNANCE_REGISTRY.length());
+            } else {
+                modelStorageLocation = modelStorage[1];
+            }
+        }
+
+        MLIOFactory ioFactory = new MLIOFactory(MLCoreServiceValueHolder.getInstance().getMlProperties());
+        MLInputAdapter inputAdapter = ioFactory.getInputAdapter(storageType + MLConstants.IN_SUFFIX);
+        InputStream in = inputAdapter.readDataset(new URI(modelStorageLocation));
+        ObjectInputStream ois = new ObjectInputStream(in);
+        MLModel mlModel = (MLModel) ois.readObject();
+        ois.close();
+        return mlModel;
+
+    }
+
+    /**
+     * Predict the value using the feature values
+     * @param data  feature values array
+     * @return      predicted value
+     * @throws      MLModelBuilderException
+     */
+    public double predict(double[] data) throws MLModelBuilderException {
+
+        Predictor predictor = new Predictor(modelId, mlModel, Arrays.asList(data));
+        List<?> predictions = predictor.predict();
+        return (Double) predictions.get(0);
+    }
+
+    /**
+     * Return the map containing <feature-name, index> pairs
+     * @return
+     */
+    public Map<String, Integer> getFeatures() {
+        List<Feature> features = mlModel.getFeatures();
+        Map<String, Integer> featureIndexMap = new HashMap<String, Integer>();
+        for(Feature feature : features) {
+            featureIndexMap.put(feature.getName(), feature.getIndex());
+        }
+        return featureIndexMap;
+    }
+}
---
+++ siddhi-extensions/ml/src/test/java/org/wso2/siddhi/extension/ml/PredictStreamProcessorTestCase.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.siddhi.extension.ml;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.wso2.carbon.ml.core.utils.MLCoreServiceValueHolder;
+import org.wso2.siddhi.core.ExecutionPlanRuntime;
+import org.wso2.siddhi.core.SiddhiManager;
+import org.wso2.siddhi.core.event.Event;
+import org.wso2.siddhi.core.query.output.callback.QueryCallback;
+import org.wso2.siddhi.core.stream.input.InputHandler;
+import org.wso2.siddhi.core.util.EventPrinter;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Properties;
+
+public class PredictStreamProcessorTestCase {
+
+    @Before
+    public void init() {
+        MLCoreServiceValueHolder valueHolder = MLCoreServiceValueHolder.getInstance();
+        valueHolder.setMlProperties(new Properties());
+    }
+
+    @Test
+    public void predictFunctionTest() throws InterruptedException, URISyntaxException {
+
+        URL resource = PredictStreamProcessorTestCase.class.getResource("/test-model");
+        String modelStorageLocation = new File(resource.toURI()).getAbsolutePath();
+
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inputStream = "define stream InputStream " +
+                "(NumPregnancies double, PG2 double, DBP double, TSFT double, SI2 double, BMI double, DPF double, Age double);";
+
+        String query = "@info(name = 'query1') " +
+                "from InputStream#ml:predict('"+ modelStorageLocation +"') " +
+                "select * " +
+                "insert into outputStream ;";
+
+        ExecutionPlanRuntime executionPlanRuntime = siddhiManager.createExecutionPlanRuntime(inputStream + query);
+
+        executionPlanRuntime.addCallback("query1", new QueryCallback() {
+            @Override
+            public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+                EventPrinter.print(timeStamp, inEvents, removeEvents);
+                if (inEvents != null) {
+                    Assert.assertEquals(0.9176214029655854, inEvents[0].getData(8));
+                }
+            }
+
+        });
+
+        InputHandler inputHandler = executionPlanRuntime.getInputHandler("InputStream");
+        executionPlanRuntime.start();
+        inputHandler.send(new Object[]{6, 148, 72, 35, 0, 33.6, 0.627, 50});
+        Thread.sleep(1000);
+        executionPlanRuntime.shutdown();
+    }
+
+    @Test
+    public void predictFunctionWithSelectedAttributesTest() throws InterruptedException, URISyntaxException {
+
+        URL resource = PredictStreamProcessorTestCase.class.getResource("/test-model");
+        String modelStorageLocation = new File(resource.toURI()).getAbsolutePath();
+
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inputStream = "define stream InputStream " +
+                "(NumPregnancies double, PG2 double, DBP double, TSFT double, SI2 double, BMI double, DPF double, Age double);";
+
+        String query = "@info(name = 'query1') " +
+                "from InputStream#ml:predict('" + modelStorageLocation + "', NumPregnancies, PG2, DBP, TSFT, SI2, BMI, DPF, Age) " +
+                "select prediction " +
+                "insert into outputStream ;";
+
+        ExecutionPlanRuntime executionPlanRuntime = siddhiManager.createExecutionPlanRuntime(inputStream + query);
+
+        executionPlanRuntime.addCallback("query1", new QueryCallback() {
+            @Override
+            public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+                EventPrinter.print(timeStamp, inEvents, removeEvents);
+                if (inEvents != null) {
+                    Assert.assertEquals(0.9176214029655854, inEvents[0].getData(0));
+                }
+            }
+
+        });
+
+        InputHandler inputHandler = executionPlanRuntime.getInputHandler("InputStream");
+        executionPlanRuntime.start();
+        inputHandler.send(new Object[]{6, 148, 72, 35, 0, 33.6, 0.627, 50});
+        Thread.sleep(1000);
+        executionPlanRuntime.shutdown();
+    }
+}
