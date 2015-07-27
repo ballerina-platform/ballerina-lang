0b3f27f6b9a07f82c562c28957284b1df918c855
suhothayan <suhothayan@gmail.com>
2015-07-27 15:36:11 +0530

    adding partition perf test

---
+++ siddhi-samples/performance-samples/src/main/java/org/wso2/siddhi/performance/PartitionPerformance.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.siddhi.performance;
+
+import org.wso2.siddhi.core.ExecutionPlanRuntime;
+import org.wso2.siddhi.core.SiddhiManager;
+import org.wso2.siddhi.core.event.Event;
+import org.wso2.siddhi.core.stream.input.InputHandler;
+import org.wso2.siddhi.core.stream.output.StreamCallback;
+
+public class PartitionPerformance {
+    private static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String executionPlan = "" +
+                "define stream cseEventStream (symbol string, price float, volume long);" +
+                "" +
+                "partition with (symbol of cseEventStream) " +
+                "begin " +
+                "   @info(name = 'query1') " +
+                "   from cseEventStream[700 > price] " +
+                "   select * " +
+                "   insert into outputStream ;" +
+                "end;";
+
+        ExecutionPlanRuntime executionPlanRuntime = siddhiManager.createExecutionPlanRuntime(executionPlan);
+        executionPlanRuntime.addCallback("outputStream", new StreamCallback() {
+
+            @Override
+            public void receive(Event[] events) {
+//                EventPrinter.print(events);
+            }
+        });
+
+
+        InputHandler inputHandler = executionPlanRuntime.getInputHandler("cseEventStream");
+        executionPlanRuntime.start();
+
+        while (true) {
+            count++;
+            inputHandler.send(new Object[]{"WSO2" + count, 55.6f, 100});
+            System.out.println("Partition created :" + count);
+        }
+
+    }
+}
