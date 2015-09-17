e7f9e2d22f26a184d32f4883fc1a92316b60200b
Miyuru Dayarathna <miyurud@yahoo.co.uk>
2015-09-17 15:04:14 +0530

    Modified the K-slack disorder handling code.

--- siddhi-extensions/ooh/src/test/java/org/wso2/siddhi/extension/ooh/KSlackExtensionTestCase.java
+++ siddhi-extensions/ooh/src/test/java/org/wso2/siddhi/extension/ooh/KSlackExtensionTestCase.java
@@ -6,9 +6,6 @@ import org.junit.Test;
 import org.wso2.siddhi.core.ExecutionPlanRuntime;
 import org.wso2.siddhi.core.SiddhiManager;
 import org.wso2.siddhi.core.stream.input.InputHandler;
-import org.wso2.siddhi.core.stream.input.order.simulator.OOSimulator;
-import org.wso2.siddhi.core.stream.input.order.simulator.org.wso2.siddhi.core.stream.input.order.simulator.threads.DEBS2015DataLoderThread;
-import org.wso2.siddhi.core.stream.input.order.simulator.org.wso2.siddhi.core.stream.input.order.simulator.threads.DataLoaderThread;
 import org.wso2.siddhi.core.stream.output.StreamCallback;
 
 /**
@@ -40,12 +37,9 @@ public class KSlackExtensionTestCase {
 
             @Override
             public void receive(org.wso2.siddhi.core.event.Event[] events) {
-                //EventPrinter.print(events);
-
                 for (org.wso2.siddhi.core.event.Event evt : events) {
                     Object[] dt = evt.getData();
-                    System.out.println(dt[0]);
-                    //log.info(dt[0]);
+                    log.info(dt[0]);
                 }
             }
         });
@@ -53,53 +47,21 @@ public class KSlackExtensionTestCase {
 
         InputHandler inputHandler = executionPlanRuntime.getInputHandler("inputStream");
         executionPlanRuntime.start();
-//        inputHandler.send(new Object[]{0l, 700f, 100l});
-//        inputHandler.send(new Object[]{1l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{2l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{3l, 700f, 100l});
-//        inputHandler.send(new Object[]{5l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{4l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{6l, 700f, 100l});
-//        inputHandler.send(new Object[]{7l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{8l, 60.5f, 200l});
 
-//        inputHandler.send(new Object[]{1l, 700f, 100l});
-//        inputHandler.send(new Object[]{4l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{3l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{5l, 700f, 100l});
-//        inputHandler.send(new Object[]{6l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{9l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{7l, 700f, 100l});
-//        inputHandler.send(new Object[]{8l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{10l, 60.5f, 200l});
-//        inputHandler.send(new Object[]{13l, 60.5f, 200l});
-
-        String inputFile = "/home/miyurud/DEBS2015/dataset/20M-dataset.csv";
-        int numberOfEventsToRead = 1000;
-        DataLoaderThread thrd = new DEBS2015DataLoderThread(inputFile, numberOfEventsToRead);
-        OOSimulator simulator = new OOSimulator(thrd, 5);
-        simulator.init();
-
-        simulator.setRunOO();
-
-        for(int i=0;i<numberOfEventsToRead;i++){
-            Object[] evt = simulator.getNextEvent();
-            //System.out.println(evt[0]);
-            inputHandler.send(evt);
-        }
-        //System.out.println(simulator.getNextEvent()[0]);
+        //The following implements the out-of-order disorder handling scenario described in the
+        //http://dl.acm.org/citation.cfm?doid=2675743.2771828
+        inputHandler.send(new Object[]{1l, 700f, 100l});
+        inputHandler.send(new Object[]{4l, 60.5f, 200l});
+        inputHandler.send(new Object[]{3l, 60.5f, 200l});
+        inputHandler.send(new Object[]{5l, 700f, 100l});
+        inputHandler.send(new Object[]{6l, 60.5f, 200l});
+        inputHandler.send(new Object[]{9l, 60.5f, 200l});
+        inputHandler.send(new Object[]{7l, 700f, 100l});
+        inputHandler.send(new Object[]{8l, 60.5f, 200l});
+        inputHandler.send(new Object[]{10l, 60.5f, 200l});
+        inputHandler.send(new Object[]{13l, 60.5f, 200l});
 
         Thread.sleep(1000);
         executionPlanRuntime.shutdown();
-    }
-
-    public static void main(String[] args){
-        KSlackExtensionTestCase testCase = new KSlackExtensionTestCase();
-
-        try {
-            testCase.OrderTest();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
