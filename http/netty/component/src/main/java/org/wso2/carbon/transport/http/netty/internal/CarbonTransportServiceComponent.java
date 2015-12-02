beeaa365143bfba299012070fa31241cc2cc389f
shafreen <shafreen@wso2.com>
2015-12-02 12:15:10 -0700

    added a new service component get the reference service initializer

---
+++ http/netty/component/src/main/java/org/wso2/carbon/transport/http/netty/internal/CarbonTransportServiceComponent.java
@@ -0,0 +1,36 @@
+package org.wso2.carbon.transport.http.netty.internal;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.messaging.CarbonTransportServerInitializer;
+
+import java.util.Map;
+
+/**
+ * OSGi declarative service for initializer.
+ * @since 5.0.0
+ */
+@Component(
+        name = "org.wso2.carbon.messaging.service.MessagingServiceComponent",
+        immediate = true
+)
+public class CarbonTransportServiceComponent {
+
+    @Reference(
+            name = "transport-initializer",
+            service = CarbonTransportServerInitializer.class,
+            cardinality = ReferenceCardinality.OPTIONAL,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "removeTransportInitializer"
+    )
+    protected void addTransportInitializer(CarbonTransportServerInitializer serverInitializer, Map<String, ?> ref) {
+        NettyTransportDataHolder.getInstance()
+                .addNettyChannelInitializer((String)ref.get("channel.id"), serverInitializer);
+    }
+
+    protected void removeTransportInitializer(CarbonTransportServerInitializer serverInitializer) {
+       //// TODO: 11/30/15
+    }
+}
