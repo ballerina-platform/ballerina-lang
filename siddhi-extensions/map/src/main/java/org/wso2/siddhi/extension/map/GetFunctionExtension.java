3559772a759baae5ae03900971895644cd939bca
minudika <minudika@wso2.com>
2015-12-10 15:26:11 +0530

    Siddhi Map Extension

---
+++ siddhi-extensions/map/src/main/java/org/wso2/siddhi/extension/map/GetFunctionExtension.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.siddhi.extension.map;
+
+import org.wso2.siddhi.core.config.ExecutionPlanContext;
+import org.wso2.siddhi.core.exception.ExecutionPlanRuntimeException;
+import org.wso2.siddhi.core.executor.ExpressionExecutor;
+import org.wso2.siddhi.core.executor.function.FunctionExecutor;
+import org.wso2.siddhi.query.api.definition.Attribute;
+import org.wso2.siddhi.query.api.exception.ExecutionPlanValidationException;
+
+import java.util.HashMap;
+
+/**
+ * get(HashMap , Key , Type)
+ * Returns required attribute value'.
+ * Accept Type(s): (HashMap , ValidAttributeType , ValidAttributeType)
+ * Return Type(s): Object
+ */
+public class GetFunctionExtension extends FunctionExecutor {
+    static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GetFunctionExtension.class);
+    Attribute.Type returnType = Attribute.Type.STRING;
+    private HashMap hashMap;
+
+    @Override
+    protected void init(ExpressionExecutor[] attributeExpressionExecutors, ExecutionPlanContext executionPlanContext) {
+        if (attributeExpressionExecutors.length != 3) {
+            throw new ExecutionPlanValidationException("Invalid no of arguments passed to map:get() function, " +
+                    "required 3, but found " + attributeExpressionExecutors.length);
+        }
+    }
+
+    @Override
+    protected Object execute(Object[] data) {
+        if (data == null) {
+            throw new ExecutionPlanRuntimeException("Data can not be null.");
+        }
+        hashMap = (HashMap) data[0];
+        String type = (String) data[2];
+        Object tmpVal = hashMap.get(data[1]);
+        Object value = null;
+
+        if (type.toLowerCase().equals("int")) {
+            try {
+                String tmpStr = tmpVal.toString();
+                value = Integer.parseInt(tmpStr);
+            } catch (ExecutionPlanRuntimeException e) {
+                throw new ExecutionPlanRuntimeException("Attribute value cannot be parsed to INTEGER");
+            }
+        } else if (type.toLowerCase().equals("double")) {
+            try {
+                String tmpStr = tmpVal.toString();
+                value = Double.parseDouble(tmpStr);
+            } catch (ExecutionPlanRuntimeException e) {
+                throw new ExecutionPlanRuntimeException("Attribute value cannot be parsed to DOUBLE");
+            }
+        } else if (type.toLowerCase().equals("long")) {
+            try {
+                String tmpStr = tmpVal.toString();
+                value = Long.parseLong(tmpStr);
+            } catch (ExecutionPlanRuntimeException e) {
+                throw new ExecutionPlanRuntimeException("Attribute value cannot be parsed to LONG");
+            }
+        } else if (type.toLowerCase().equals("bool")) {
+            try {
+                String tmpStr = tmpVal.toString();
+                value = Boolean.parseBoolean(tmpStr);
+            } catch (ExecutionPlanRuntimeException e) {
+                throw new ExecutionPlanRuntimeException("Attribute value cannot be parsed to BOOLEAN");
+            }
+        } else if (type.toLowerCase().equals("string")) {
+            try {
+                value = tmpVal.toString();
+            } catch (ExecutionPlanRuntimeException e) {
+                throw new ExecutionPlanRuntimeException("Attribute value cannot be converted to STRING");
+            }
+        } else {
+            value = tmpVal;
+        }
+        return value;
+    }
+
+    @Override
+    protected Object execute(Object data) {
+        log.info("checked");
+        return null;
+    }
+
+    @Override
+    public void start() {
+        //Nothing to start
+    }
+
+    @Override
+    public void stop() {
+        //Nothing to stop
+    }
+
+    @Override
+    public Attribute.Type getReturnType() {
+        return returnType;
+    }
+
+    @Override
+    public Object[] currentState() {
+        return null;    //No need to maintain a state.
+    }
+
+    @Override
+    public void restoreState(Object[] state) {
+        //Since there's no need to maintain a state, nothing needs to be done here.
+    }
+}
+
+
