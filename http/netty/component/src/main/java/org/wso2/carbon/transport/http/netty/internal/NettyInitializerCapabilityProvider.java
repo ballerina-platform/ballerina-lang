2964e5396ff55596afab5274cf51bb41b4fc9a44
Senduran <bsenduran@gmail.com>
2015-12-18 14:46:57 +0530

    removed initializer capabiltiy provider component

--- http/netty/component/src/main/java/org/wso2/carbon/transport/http/netty/internal/NettyInitializerCapabilityProvider.java
+++ http/netty/component/src/main/java/org/wso2/carbon/transport/http/netty/internal/NettyInitializerCapabilityProvider.java
@@ -2,19 +2,21 @@ package org.wso2.carbon.transport.http.netty.internal;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
 import org.wso2.carbon.kernel.startupresolver.CapabilityProvider;
 import org.wso2.carbon.transport.http.netty.internal.config.YAMLTransportConfigurationBuilder;
 
 /**
  * Component which registers the CarbonTransport capability information.
  */
-@Component(
-        name = "org.wso2.carbon.transport.http.netty.internal.NettyInitializerCapabilityProvider",
-        immediate = true,
-        service = CapabilityProvider.class,
-        property = "capability-name=org.wso2.carbon.transport.http.netty.listener.CarbonNettyServerInitializer"
-)
+//@Component(
+//        name = "org.wso2.carbon.transport.http.netty.internal.NettyInitializerCapabilityProvider",
+//        immediate = true,
+//        service = CapabilityProvider.class,
+//        property = "capability-name=org.wso2.carbon.transport.http.netty.listener.CarbonNettyServerInitializer"
+//)
+
+    //// TODO: 12/18/15 since we are have the default initializer, we are not registering it
+    // TODO: 12/18/15 have to check this
 public class NettyInitializerCapabilityProvider implements CapabilityProvider {
 
     @Activate
