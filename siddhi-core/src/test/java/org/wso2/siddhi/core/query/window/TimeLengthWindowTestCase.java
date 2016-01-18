ace590d6992072d6af69ff7f1e0514c763f99fc3
minudika <minudika@wso2.com>
2016-01-18 15:10:30 +0530

    TimeLengthWindow implementation    Code reformatted

--- siddhi-core/src/test/java/org/wso2/siddhi/core/query/window/TimeLengthWindowTestCase.java
+++ siddhi-core/src/test/java/org/wso2/siddhi/core/query/window/TimeLengthWindowTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -385,7 +385,6 @@ public class TimeLengthWindowTestCase {
     public void timeLengthWindowTest10() throws InterruptedException {
         log.info("Testing timeLength window with no of events greater than window length and time period less than window time");
 
-        //final int length = 4;
         SiddhiManager siddhiManager = new SiddhiManager();
 
         String cseEventStream = "define stream cseEventStream (symbol string, price float, volume int);";
