2e6765be4fb519d6f149103b17ddd8276ee0beb2
suho <suhothayan@gmail.com>
2016-07-28 18:44:53 +0530

    fixing test cases

--- siddhi-core/src/test/java/org/wso2/siddhi/core/query/window/TimeBatchWindowTestCase.java
+++ siddhi-core/src/test/java/org/wso2/siddhi/core/query/window/TimeBatchWindowTestCase.java
@@ -362,7 +362,7 @@ public class TimeBatchWindowTestCase {
         while (System.currentTimeMillis() % 2000 != 0) ;
         inputHandler.send(new Object[]{"IBM", 700f, 0});
         inputHandler.send(new Object[]{"WSO2", 60.5f, 1});
-        Thread.sleep(8000);
+        Thread.sleep(8500);
         inputHandler.send(new Object[]{"WSO2", 60.5f, 1});
         inputHandler.send(new Object[]{"II", 60.5f, 1});
         Thread.sleep(13000);
