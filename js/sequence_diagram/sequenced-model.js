2211927ce0c771a4b5024de622c327b98d2a970e
sumuditha-test <sumudithavi@gmail.com>
2016-10-27 00:50:26 +0530

    Introducing factory method for processor creation and defining a proper hierarchy for processors

--- js/sequence_diagram/sequenced-model.js
+++ js/sequence_diagram/sequenced-model.js
@@ -38,13 +38,6 @@ var SequenceD = (function (sequenced) {
 
                 this.widestChild = null;
 
-                var children = new Children([], {diagram: this});
-                this.children(children);
-
-                if(this.model.type == "ComplexProcessor"){
-                    var containableProcessorElements = new ContainableProcessorElements([], {diagram: this});
-                    this.containableProcessorElements(containableProcessorElements);
-                }
             },
 
             modelName: "Processor",
@@ -56,67 +49,6 @@ var SequenceD = (function (sequenced) {
             parameters: {},
 
             viewAttributes: {},
-
-            containableProcessorElements: function (containableProcessorElements) {
-                if (_.isUndefined(containableProcessorElements)) {
-                    return this.get('containableProcessorElements');
-                } else {
-                    this.set('containableProcessorElements', containableProcessorElements);
-                }
-            },
-
-            addContainableProcessorElement: function (paperID, center) {
-                var containableProcessorElem =  new SequenceD.Models.ContainableProcessorElement(lifeLineOptions);
-                this.containableProcessorElements().add(containableProcessorElem);
-                var containableProcessorElementView = new SequenceD.Views.ContainableProcessorElement({model: containableProcessorElem, options: lifeLineOptions});
-                containableProcessorElementView.render("#diagramWrapper", center);
-            },
-
-            children: function (children) {
-                if (_.isUndefined(children)) {
-                    return this.get('children');
-                } else {
-                    this.set('children', children);
-                }
-            },
-
-            addChild: function (element, opts) {
-                //this.children().add(element, opts);
-                element.parent(this);
-
-                var position = this.calculateIndex(element, element.get('centerPoint').get('y'));
-                var index = position.index;
-                this.children().add(element, {at: index});
-
-                //this.trigger("addChild", element, opts);
-                //this.trigger("addChildProcessor", element, opts);
-            },
-
-
-            calculateIndex: function (element, y) {
-                var previousChild;
-                var count = 1;
-                var position = {};
-                this.children().each(function (child) {
-                    if (!_.isEqual(element, child)) {
-                        if (child.get('centerPoint').get('y') > y) {
-                            previousChild = child;
-                            return false;
-                        }
-                        count = count + 1;
-                    }
-                });
-                if (_.isUndefined(previousChild)) {
-                    if (this.children().size() == 0) {
-                        position.index = 0;
-                    } else {
-                        position.index = this.children().indexOf(element);
-                    }
-                } else {
-                    position.index = this.children().indexOf(previousChild);
-                }
-                return position;
-            },
 
             setY: function (y) {
                 this.get('centerPoint').set('y', y);
@@ -144,52 +76,6 @@ var SequenceD = (function (sequenced) {
                 height : 30,
                 title: "Processor"
             }
-        });
-
-    var FixedSizedMediator = Diagrams.Models.Shape.extend(
-        /** @lends DiagramElement.prototype */
-        {
-
-            selectedNode: null,
-            /**
-             * @augments DiagramElement
-             * @constructs
-             * @class Element represents the model for elements in a diagram.
-             */
-            initialize: function (attrs, options) {
-                Diagrams.Models.Shape.prototype.initialize.call(this, attrs, options);
-            },
-
-            modelName: "FixedSizedMediator",
-
-            nameSpace: sequenced,
-
-            idAttribute: this.cid,
-
-            defaults: {
-                centerPoint: new GeoCore.Models.Point({x: 0, y: 0}),
-                title: "Mediator"
-            }
-        });
-
-
-    var FixedSizedMediators = Backbone.Collection.extend(
-        /** @lends DiagramElements.prototype */
-        {
-            /**
-             * @augments Backbone.Collection
-             * @constructs
-             * @class DiagramElements represents the collection for elements in a diagram.
-             */
-            initialize: function (models, options) {
-            },
-
-            modelName: "FixedSizedMediators",
-
-            nameSpace: sequenced,
-
-            model: FixedSizedMediator
-
         });
 
     var Child = Diagrams.Models.DiagramElement.extend(
@@ -249,11 +135,6 @@ var SequenceD = (function (sequenced) {
              */
             initialize: function (attrs, options) {
                 Diagrams.Models.Shape.prototype.initialize.call(this, attrs, options);
-
-                var elements = new FixedSizedMediators([], {diagram: this});
-
-                var fixedSizedMediators = new FixedSizedMediators([], {diagram: this});
-                this.fixedSizedMediators(fixedSizedMediators);
 
                 var children = new Children([], {diagram: this});
                 this.children(children);
@@ -326,22 +207,7 @@ var SequenceD = (function (sequenced) {
 
             createProcessor: function (title, center, type, model, viewAttributes, parameters, getMySubTree,
                                        saveMyProperties) {
-                return new SequenceD.Models.Processor({
-                    title: title,
-                    centerPoint: center,
-                    type: type,
-                    model: model,
-                    viewAttributes: viewAttributes,
-                    parameters: parameters,
-                    getMySubTree: getMySubTree,
-                    saveMyProperties: saveMyProperties
-                });
-            },
-
-            addFixedSizedMediator: function (element, opts) {
-                this.fixedSizedMediators().add(element, opts);
-                this.trigger("addFixedSizedMediator", element, opts);
-
+                return new SequenceD.Models.ProcessorFactory(title, center, model.type, model, viewAttributes, parameters, getMySubTree);
             },
 
             addChild: function (element, opts) {
@@ -390,15 +256,6 @@ var SequenceD = (function (sequenced) {
                 return position;
             },
 
-            fixedSizedMediators: function (fixedSizedMediators) {
-                if (_.isUndefined(fixedSizedMediators)) {
-                    return this.get('fixedSizedMediators');
-                    //console.log("fixedSizedMediators is undefined");
-                } else {
-                    this.set('fixedSizedMediators', fixedSizedMediators);
-                }
-            },
-
             children: function (children) {
                 if (_.isUndefined(children)) {
                     return this.get('children');
@@ -430,25 +287,6 @@ var SequenceD = (function (sequenced) {
             setHeight: function (height) {
                 this.set('height', height);
             }
-
-        });
-
-    var Activation = Diagrams.Models.ConnectionPoint.extend(
-        /** @lends Activation.prototype */
-        {
-            /**
-             * @augments ConnectionPoint
-             * @constructs
-             * @class Activation Represents the model for an activation in Sequence Diagrams.
-             */
-            initialize: function (attrs, options) {
-                Diagrams.Models.ConnectionPoint.prototype.initialize.call(this, attrs, options);
-                this.owner().addConnectionPoint(this);
-            },
-
-            modelName: "Activation",
-
-            nameSpace: sequenced
 
         });
 
@@ -613,148 +451,12 @@ var SequenceD = (function (sequenced) {
 
         });
 
-    var ContainableProcessorElement = Diagrams.Models.Shape.extend(
-        /** @lends ContainableProcessorElement.prototype */
-        {
-
-            selectedNode: null,
-            /**
-             * @augments DiagramElement
-             * @constructs
-             * @class ContainableProcessorElement represents the model for processor element which can contain
-             *     processors.
-             */
-            initialize: function (attrs, options) {
-                Diagrams.Models.Shape.prototype.initialize.call(this, attrs, options);
-                var children = new Children([], {diagram: this});
-                this.children(children);
-                this.widestChild = null;
-            },
-
-            modelName: "ContainableProcessorElement",
-
-            nameSpace: sequenced,
-
-            idAttribute: this.cid,
-
-            defaults: {
-                centerPoint: new GeoCore.Models.Point({x: 0, y: 0}),
-                title: "ContainableProcessorElement",
-                width : 130,
-                height : 30,
-                viewAttributes: {colour: "#ffffff"}
-            },
-
-            children: function (children) {
-                if (_.isUndefined(children)) {
-                    return this.get('children');
-                } else {
-                    this.set('children', children);
-                }
-            },
-
-            addChild: function (element, opts) {
-                element.parent(this);
-                var position = this.calculateIndex(element, element.get('centerPoint').get('y'));
-                var index = position.index;
-                this.children().add(element, {at:index});
-            },
-
-            calculateIndex: function (element, y) {
-                var previousChild;
-                var count = 1;
-                var position = {};
-                this.children().each(function (child) {
-                    if (!_.isEqual(element, child)) {
-                        if (child.get('centerPoint').get('y') > y) {
-                            previousChild = child;
-                            return false;
-                        }
-                        count = count + 1;
-                    }
-                });
-                if (_.isUndefined(previousChild)) {
-                    if(this.children().size() == 0){
-                        position.index = 0;
-                    }else {
-                        position.index = this.children().indexOf(element);
-                    }
-                } else {
-                    position.index = this.children().indexOf(previousChild);
-                }
-                return position;
-            },
-
-            createProcessor: function (title, center, type, model, viewAttributes, parameters, getMySubTree,
-                                       saveMyProperties) {
-                return new SequenceD.Models.Processor({
-                    title: title,
-                    centerPoint: center,
-                    type: type,
-                    model: model,
-                    viewAttributes: viewAttributes,
-                    parameters: parameters,
-                    getMySubTree: getMySubTree,
-                    saveMyProperties: saveMyProperties
-                });
-            },
-
-
-            setY: function (y) {
-                this.get('centerPoint').set('y', y);
-            },
-
-            setX: function (x) {
-                this.get('centerPoint').set('x', x);
-            },
-
-            getWidth: function () {
-                return this.get('width');
-            },
-
-            getHeight: function (){
-                return this.get('height');
-            },
-
-            setWidth: function (width) {
-                this.set('width', width);
-            },
-
-            setHeight: function (height) {
-                this.set('height', height);
-            },
-
-
-        });
-
-    var ContainableProcessorElements = Backbone.Collection.extend(
-        /** @lends ContainableProcessorElements.prototype */
-        {
-            /**
-             * @augments Backbone.Collection
-             * @constructs
-             * @class ContainableProcessorElements represents the collection for elements in a diagram.
-             */
-            initialize: function (models, options) {
-            },
-
-            modelName: "ContainableProcessorElements",
-
-            nameSpace: sequenced,
-
-            model: ContainableProcessorElement
-
-        });
-
-
     // set models
-    models.Activation = Activation;
     models.Message = Message;
     models.LifeLine = LifeLine;
     models.Processor = Processor;
     models.MessagePoint = MessagePoint;
     models.MessageLink = MessageLink;
-    models.ContainableProcessorElement = ContainableProcessorElement;
 
     sequenced.Models = models;
 
