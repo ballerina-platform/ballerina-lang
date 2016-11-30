3f8e4fb5c48e2bf880dd5bc5df8c80d9eab9da5d
isudana <isudana@gmail.com>
2016-11-30 09:55:32 +0530

    Add accessor methods to the Connector and Function

--- src/main/java/org/wso2/ballerina/model/Connector.java
+++ src/main/java/org/wso2/ballerina/model/Connector.java
@@ -21,7 +21,90 @@ package org.wso2.ballerina.model;
 import java.util.List;
 
 public class Connector {
+
     private List<Connection> connections;
     private List<Variable> variables;
     private List<Action> actions;
+
+    /**
+     * Get all Connections declared within the Connector scope
+     *
+     * @return list of all the Connections belongs to a Service
+     */
+    public List<Connection> getConnections() {
+        return connections;
+    }
+
+    /**
+     * Assign connections to the Connector
+     *
+     * @param connections list of connections to be assigned to a Connector
+     */
+    public void setConnections(List<Connection> connections) {
+        this.connections = connections;
+    }
+
+    /**
+     * Add a Connection to the Connector
+     *
+     * @param connection Connection to be added to the Connector
+     */
+    public void addConnection(Connection connection) {
+        connections.add(connection);
+    }
+
+    /**
+     * Get all the variables declared in the scope of Connector
+     *
+     * @return list of all Connector scoped variables
+     */
+    public List<Variable> getVariables() {
+        return variables;
+    }
+
+    /**
+     * Assign variables to the Connector
+     *
+     * @param variables list of Connector
+     */
+    public void setVariables(List<Variable> variables) {
+        this.variables = variables;
+    }
+
+    /**
+     * Add a variable to the Connector
+     *
+     * @param variable variable to be added to the Connector
+     */
+    public void addVariable(Variable variable) {
+        variables.add(variable);
+    }
+
+    /**
+     * Get all the Actions can be performed in the Connector
+     *
+     * @return list of all Actions
+     */
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    /**
+     * Set list of Actions to the Connector
+     *
+     * @param actions list of Actions
+     */
+    public void setActions(List<Action> actions) {
+        this.actions = actions;
+    }
+
+    /**
+     * Add an Action to the Connector
+     *
+     * @param action Action to be added to the Connector
+     */
+    public void addAction(Action action) {
+        actions.add(action);
+    }
+
 }
