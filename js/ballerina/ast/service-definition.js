0a07875741badacd0c47fdb7d7ff0abe847ddba6
Hemika Kodikara <hemika.kodikara@gmail.com>
2016-12-01 17:59:39 +0530

    Adding service definition view.

--- js/ballerina/ast/service-definition.js
+++ js/ballerina/ast/service-definition.js
@@ -15,27 +15,56 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-define(['lodash', './node'], function(_, ASTNode){
+define(['lodash', './node'], function (_, ASTNode) {
 
-    var ServiceDefinition = function() {
+    var ServiceDefinition = function () {
         this._basePath = "/";
-        this.resourceDefinitions = [];
-        this.variableDeclarations = [];
-        this.connectionDeclarations = [];
+        this._resourceDefinitions = [];
+        this._variableDeclarations = [];
+        this._connectionDeclarations = [];
     };
 
     ServiceDefinition.prototype = Object.create(ASTNode.prototype);
     ServiceDefinition.prototype.constructor = ServiceDefinition;
 
-
-    ServiceDefinition.prototype.setBasePath = function(basePath){
-        if(!_.isUndefined(basePath)){
+    ServiceDefinition.prototype.setBasePath = function (basePath) {
+        if (!_.isNil(basePath)) {
             this._basePath = basePath;
         }
     };
 
-    ServiceDefinition.prototype.getBasePath = function(){
+    ServiceDefinition.prototype.getBasePath = function () {
         return this._basePath;
+    };
+
+    ServiceDefinition.prototype.setResourceDefinitions = function (resourceDefinitions) {
+        if (!_.isNil(resourceDefinitions)) {
+            this._resourceDefinitions = resourceDefinitions;
+        }
+    };
+
+    ServiceDefinition.prototype.getResourceDefinitions = function () {
+        return this._resourceDefinitions;
+    };
+
+    ServiceDefinition.prototype.setVariableDeclarations = function (variableDeclarations) {
+        if (!_.isNil(variableDeclarations)) {
+            this._variableDeclarations = variableDeclarations;
+        }
+    };
+
+    ServiceDefinition.prototype.getVariableDeclarations = function () {
+        return this._variableDeclarations;
+    };
+
+    ServiceDefinition.prototype.setConnectionDeclarations = function (connectionDeclarations) {
+        if (!_.isNil(connectionDeclarations)) {
+            this._connectionDeclarations = connectionDeclarations;
+        }
+    };
+
+    ServiceDefinition.prototype.getConnectionDeclarations = function () {
+        return this._connectionDeclarations;
     };
 
 });
