84b20b67759c0fc7899d4f7f4f481bdadf842076
Sameera <sameera.madushan@gmail.com>
2016-12-06 13:56:23 +0530

    Add a class to represent any literal in Ballerina

---
+++ src/main/java/org/wso2/ballerina/model/expressions/BasicLiteral.java
@@ -0,0 +1,42 @@
+/*
+*  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.wso2.ballerina.model.expressions;
+
+import org.wso2.ballerina.interpreter.Context;
+import org.wso2.ballerina.model.values.BValueRef;
+
+/**
+ * {@code BasicLiteral} represents a literal in Ballerina
+ * <p>
+ * This class is used to hold IntegerLiterals, FloatingPointLiterals, QuotedStringLiterals, BooleanLiterals and 'nil'
+ *
+ * @since 1.0.0
+ */
+public class BasicLiteral extends AbstractExpression {
+
+    private BValueRef bValueRef;
+
+    public BasicLiteral(BValueRef bValueRef) {
+        this.bValueRef = bValueRef;
+    }
+
+    @Override
+    public BValueRef evaluate(Context ctx) {
+        return bValueRef;
+    }
+}
