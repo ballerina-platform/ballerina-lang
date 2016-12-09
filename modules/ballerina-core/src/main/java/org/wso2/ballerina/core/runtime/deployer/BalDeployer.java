1bf9c44649aa06746f9976bea2b74896b66b5f67
isudana <isudana@gmail.com>
2016-12-09 23:51:54 +0530

    Add 'Run this ballerina file...' mode

---
+++ modules/ballerina-core/src/main/java/org/wso2/ballerina/core/runtime/deployer/BalDeployer.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ * <p>
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.ballerina.core.runtime.deployer;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.deployment.engine.Artifact;
+import org.wso2.carbon.deployment.engine.ArtifactType;
+import org.wso2.carbon.deployment.engine.Deployer;
+import org.wso2.carbon.deployment.engine.exception.CarbonDeploymentException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * {@code BalDeployer} is responsible for all ballerina file deployment tasks
+ */
+@Component(
+        name = "org.wso2.ballerina.core.runtime.deployer.BalDeployer",
+        immediate = true,
+        service = Deployer.class)
+public class BalDeployer implements Deployer {
+
+    private static final Logger log = LoggerFactory.getLogger(BalDeployer.class);
+
+    private static final String EXTERNAL_DSL_CONFIGS_DIRECTORY = "ballerina-files";
+    private static final String FILE_EXTENSION = ".bal";
+    private static final Logger logger = LoggerFactory.getLogger(BalDeployer.class);
+    private ArtifactType artifactType = new ArtifactType<>("bal");
+    ;
+    private URL directoryLocation;
+
+    @Activate
+    protected void activate(BundleContext bundleContext) {
+    }
+
+    @Override
+    public void init() {
+        try {
+            directoryLocation = new URL("file:" + EXTERNAL_DSL_CONFIGS_DIRECTORY);
+        } catch (MalformedURLException e) {
+            logger.error("Error while initializing directoryLocation" + directoryLocation.getPath(), e);
+        }
+    }
+
+    @Override
+    public Object deploy(Artifact artifact) throws CarbonDeploymentException {
+        deployBalFile(artifact.getFile());
+        return artifact.getFile().getName();
+    }
+
+    @Override
+    public void undeploy(Object o) throws CarbonDeploymentException {
+    }
+
+    @Override
+    public Object update(Artifact artifact) throws CarbonDeploymentException {
+        return null;
+    }
+
+    @Override
+    public URL getLocation() {
+        return directoryLocation;
+    }
+
+    @Override
+    public ArtifactType getArtifactType() {
+        return artifactType;
+    }
+
+    public static void deployBalFile(File file) {
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+
+            if (file.getName().endsWith(FILE_EXTENSION)) {
+                //TODO: Create object model and register application from here
+                log.info("Deploying ballerina file : " + file.getName());
+            }
+        } catch (IOException e) {
+            logger.error("Error while creating Ballerina object model from file : " + file.getName(), e);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException ignore) {
+                }
+            }
+        }
+    }
+
+}
