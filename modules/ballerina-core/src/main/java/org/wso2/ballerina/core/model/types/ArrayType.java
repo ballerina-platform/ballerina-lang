2d261b0b3d5cfe0525515bd15e35956c6eabf240
Hasitha Aravinda <mail.hasitha27@gmail.com>
2016-12-12 20:36:10 +0530

    Move ArrayType and MapType to ArrayValue and MapValue

--- modules/ballerina-core/src/main/java/org/wso2/ballerina/core/model/types/ArrayType.java
+++ modules/ballerina-core/src/main/java/org/wso2/ballerina/core/model/types/ArrayType.java
@@ -1,5 +1,5 @@
 /*
-*  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*   Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 *
 *  WSO2 Inc. licenses this file to you under the Apache License,
 *  Version 2.0 (the "License"); you may not use this file except
@@ -8,80 +8,19 @@
 *
 *    http://www.apache.org/licenses/LICENSE-2.0
 *
-*  Unless required by applicable law or agreed to in writing,
-*  software distributed under the License is distributed on an
-*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-*  KIND, either express or implied.  See the License for the
-*  specific language governing permissions and limitations
-*  under the License.
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
 */
 package org.wso2.ballerina.core.model.types;
 
 /**
- * {@code ArrayType} represents an array
- * @param <T> type of the array
- * @since 1.0.0
+ *  {@code ArrayType} represents an array
+ *
+ *  @since 1.0.0
  */
-public class ArrayType<T> extends AbstractType {
-
-    private T[] thisArray;
-    private int size;
-
-    /**
-     * Constructor for array inline initialization
-     * @param args variable number of initial values
-     */
-    @SuppressWarnings("unchecked")
-    public ArrayType(T... args) {
-        thisArray = (T[]) new Object[args.length];
-        for (int i = 0; i < args.length; i++) {
-            thisArray[i] = args[i];
-
-        }
-    }
-
-    /**
-     * Constructor for creating an array with size
-     * @param size number of elements to be stored in this array
-     */
-    @SuppressWarnings("unchecked")
-    public ArrayType(int size) {
-        thisArray = (T[]) new Object[size];
-        this.size = size;
-    }
-
-    /**
-     * Constructor for creating an empty array
-     *
-     */
-    public ArrayType() {
-
-    }
-
-    /**
-     * Insert a value into a given position
-     * @param index position
-     * @param value value
-     */
-    public void insert(int index, T value) {
-        thisArray[index] = value;
-    }
-
-    /**
-     * Retrieve a value from a given index
-     * @param index position
-     * @return return value
-     */
-    public T get(int index) {
-        return thisArray[index];
-    }
-
-    /**
-     * Retrieve the size of the array
-     * @return returns the size
-     */
-    public int size() {
-        return size;
-    }
-
+public class ArrayType extends AbstractType {
 }
