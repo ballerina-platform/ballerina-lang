091f9dea170bdd8d6befb2ad37bf5b4d6b9900a8
Sameera <sameera.madushan@gmail.com>
2016-12-12 13:26:23 +0530

    Add a builder for funcs, actions and resources

---
+++ modules/ballerina-core/src/main/java/org/wso2/ballerina/core/model/builder/CallableUnitBuilder.java
@@ -0,0 +1,129 @@
+/*
+*  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.wso2.ballerina.core.model.builder;
+
+import org.wso2.ballerina.core.model.Action;
+import org.wso2.ballerina.core.model.Annotation;
+import org.wso2.ballerina.core.model.BallerinaFunction;
+import org.wso2.ballerina.core.model.Connection;
+import org.wso2.ballerina.core.model.Identifier;
+import org.wso2.ballerina.core.model.Parameter;
+import org.wso2.ballerina.core.model.Resource;
+import org.wso2.ballerina.core.model.VariableDcl;
+import org.wso2.ballerina.core.model.Worker;
+import org.wso2.ballerina.core.model.statements.BlockStmt;
+import org.wso2.ballerina.core.model.types.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@code CallableUnitBuilder} builds Functions, Actions and Resources
+ * <p>
+ * A CallableUnit represents a Function, an Action or a Resource
+ */
+class CallableUnitBuilder {
+
+    private Identifier name;
+    private List<Annotation> annotationList = new ArrayList<>();
+    private boolean publicFunc;
+    private List<Parameter> parameterList = new ArrayList<>();
+    private List<Type> rTypesList = new ArrayList<>();
+    private List<Connection> connectionList = new ArrayList<>();
+    private List<VariableDcl> variableDclList = new ArrayList<>();
+    private List<Worker> workerList = new ArrayList<>();
+    private BlockStmt body;
+
+    CallableUnitBuilder() {
+    }
+
+    void setName(Identifier name) {
+        this.name = name;
+    }
+
+    void addAnnotation(Annotation annotation) {
+        this.annotationList.add(annotation);
+    }
+
+    void setPublic(boolean isPublic) {
+        this.publicFunc = isPublic;
+    }
+
+    void addParameter(Parameter param) {
+        this.parameterList.add(param);
+    }
+
+    void addReturnType(Type type) {
+        this.rTypesList.add(type);
+    }
+
+    void addConnectorDcl(Connection connection) {
+        this.connectionList.add(connection);
+    }
+
+    void addVariableDcl(VariableDcl variableDcl) {
+        this.variableDclList.add(variableDcl);
+    }
+
+    void addWorker(Worker worker) {
+        this.workerList.add(worker);
+    }
+
+    void setBody(BlockStmt body) {
+        this.body = body;
+    }
+
+    BallerinaFunction buildFunction() {
+        return new BallerinaFunction(
+                name,
+                publicFunc,
+                annotationList.toArray(new Annotation[annotationList.size()]),
+                parameterList.toArray(new Parameter[parameterList.size()]),
+                rTypesList.toArray(new Type[rTypesList.size()]),
+                connectionList.toArray(new Connection[connectionList.size()]),
+                variableDclList.toArray(new VariableDcl[variableDclList.size()]),
+                workerList.toArray(new Worker[workerList.size()]),
+                body
+        );
+    }
+
+    Resource buildResource() {
+        return new Resource(
+                name,
+                annotationList.toArray(new Annotation[annotationList.size()]),
+                parameterList.toArray(new Parameter[parameterList.size()]),
+                connectionList.toArray(new Connection[connectionList.size()]),
+                variableDclList.toArray(new VariableDcl[variableDclList.size()]),
+                workerList.toArray(new Worker[workerList.size()]),
+                body
+        );
+    }
+
+    Action buildAction() {
+        return new Action(
+                name,
+                annotationList.toArray(new Annotation[annotationList.size()]),
+                parameterList.toArray(new Parameter[parameterList.size()]),
+                rTypesList.toArray(new Type[rTypesList.size()]),
+                connectionList.toArray(new Connection[connectionList.size()]),
+                variableDclList.toArray(new VariableDcl[variableDclList.size()]),
+                workerList.toArray(new Worker[workerList.size()]),
+                body
+        );
+    }
+}
