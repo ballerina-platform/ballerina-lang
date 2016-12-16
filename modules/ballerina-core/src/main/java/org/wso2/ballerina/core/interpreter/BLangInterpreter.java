1687cb066dc1e927fa8802ed46620d48e5d31082
SupunS <supuns@wso2.com>
2016-12-17 01:36:35 +0530

    Fix native function returns

--- modules/ballerina-core/src/main/java/org/wso2/ballerina/core/interpreter/BLangInterpreter.java
+++ modules/ballerina-core/src/main/java/org/wso2/ballerina/core/interpreter/BLangInterpreter.java
@@ -288,7 +288,7 @@ public class BLangInterpreter implements NodeVisitor {
             bFunction.accept(this);
         } else {
             AbstractNativeFunction nativeFunction = (AbstractNativeFunction) function;
-            nativeFunction.execute(bContext);
+            nativeFunction.executeNative(bContext);
         }
 
         controlStack.popFrame();
