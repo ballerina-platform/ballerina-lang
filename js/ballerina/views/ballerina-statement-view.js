fe4a3b9da1b094c8a4a7bf1ef5365c487ffd0597
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2016-12-20 13:07:31 +0530

    Fixing issues with expresion editors.

--- js/ballerina/views/ballerina-statement-view.js
+++ js/ballerina/views/ballerina-statement-view.js
@@ -204,8 +204,8 @@ define(['require', 'lodash', 'log', './../visitors/statement-visitor', 'd3', 'd3
                     .attr("height", "14");
 
                 // Bottom center point.
-                var centerPointX = statementBoundingBox._leftTop._x + (statementBoundingBox._w / 2);
-                var centerPointY = statementBoundingBox._leftTop._y + statementBoundingBox._h;
+                var centerPointX = statementBoundingBox.x()+ (statementBoundingBox.w() / 2);
+                var centerPointY = statementBoundingBox.y()+ statementBoundingBox.h();
 
                 var smallArrowPoints =
                     // Bottom point of the polygon.
