49663683721829e6ddbaf3d020b4fce5296d3e96
Sameera <sameera.madushan@gmail.com>
2016-12-26 00:41:37 +0530

    Add test case for array initialier expressions
    Relates to #333

---
+++ modules/ballerina-core/src/test/java/org/wso2/ballerina/lang/expressions/ArrayInitializerExprTest.java
@@ -0,0 +1,73 @@
+/*
+*  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.wso2.ballerina.lang.expressions;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.wso2.ballerina.core.model.BallerinaFile;
+import org.wso2.ballerina.core.model.values.BArray;
+import org.wso2.ballerina.core.model.values.BInteger;
+import org.wso2.ballerina.core.model.values.BString;
+import org.wso2.ballerina.core.model.values.BValue;
+import org.wso2.ballerina.core.utils.ParserUtils;
+import org.wso2.ballerina.lang.util.Functions;
+
+/**
+ * Test array initializer expression
+ *
+ * @since 1.0.0
+ */
+public class ArrayInitializerExprTest {
+
+    private BallerinaFile bFile;
+
+    @BeforeTest
+    public void setup() {
+        bFile = ParserUtils.getLinkedBLangModel("lang/expressions/array-initializer-expr.bal");
+    }
+
+    @Test(description = "Test array initializer expression")
+    public void testArrayInitExpr() {
+        BValue[] args = {};
+        BValue[] returns = Functions.invoke(bFile, "arrayInitTest", args);
+
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BInteger.class);
+
+        int actual = ((BInteger) returns[0]).intValue();
+        int expected = 110;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(description = "Test array return value")
+    public void testArrayReturnValueTest() {
+        BValue[] args = {};
+        BValue[] returns = Functions.invoke(bFile, "arrayReturnTest", args);
+
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BArray.class);
+
+        BArray<BString> arrayValue = (BArray<BString>) returns[0];
+        Assert.assertEquals(arrayValue.size(), 6);
+
+        Assert.assertEquals(arrayValue.get(0).stringValue(), "Lion");
+        Assert.assertEquals(arrayValue.get(1).stringValue(), "Cat");
+        Assert.assertEquals(arrayValue.get(5).stringValue(), "Croc");
+    }
+}
