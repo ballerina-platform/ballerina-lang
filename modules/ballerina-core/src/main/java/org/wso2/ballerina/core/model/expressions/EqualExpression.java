5919f3466d5a1488b4b917067ac9d5cec6eec4bb
Sameera <sameera.madushan@gmail.com>
2017-01-06 01:42:48 +0530

    Improve logical/compare b expr to support float

--- modules/ballerina-core/src/main/java/org/wso2/ballerina/core/model/expressions/EqualExpression.java
+++ modules/ballerina-core/src/main/java/org/wso2/ballerina/core/model/expressions/EqualExpression.java
@@ -44,6 +44,9 @@ public class EqualExpression extends BinaryEqualityExpression {
     public static final BiFunction<BValueType, BValueType, BValueType> EQUAL_DOUBLE_FUNC =
             (lVal, rVal) -> new BBoolean(lVal.doubleValue() == rVal.doubleValue());
 
+    public static final BiFunction<BValueType, BValueType, BValueType> EQUAL_BOOLEAN_FUNC =
+            (lVal, rVal) -> new BBoolean(lVal.booleanValue() == rVal.booleanValue());
+
     public static final BiFunction<BValueType, BValueType, BValueType> EQUAL_STRING_FUNC =
             (lVal, rVal) -> new BBoolean(lVal.stringValue().equals(rVal.stringValue()));
 
