6164be08e9fa90809605acb63de361c1d3a5dc19
Kevin Ratnasekera <djkevincr@yahoo.com>
2017-01-05 20:02:59 +0530

    resolving conflicts

--- modules/editor/services/workspace-service/src/main/java/org/wso2/ballerina/tooling/service/workspace/rest/datamodel/BLangJSONModelBuilder.java
+++ modules/editor/services/workspace-service/src/main/java/org/wso2/ballerina/tooling/service/workspace/rest/datamodel/BLangJSONModelBuilder.java
@@ -212,8 +212,8 @@ public class BLangJSONModelBuilder implements NodeVisitor {
         }
         jsonConnectObj.add(BLangJSONModelConstants.ANNOTATION_DEFINITIONS, this.tempJsonArrayRef.peek());
         this.tempJsonArrayRef.pop();
-        if (connector.getArguments() != null) {
-            for (Parameter parameter : connector.getArguments()) {
+        if (connector.getParameters() != null) {
+            for (Parameter parameter : connector.getParameters()) {
                 parameter.accept(this);
             }
         }
@@ -222,8 +222,8 @@ public class BLangJSONModelBuilder implements NodeVisitor {
                 connectDcl.accept(this);
             }
         }
-        if (connector.getVariables() != null) {
-            for (VariableDcl variableDcl : connector.getVariables()) {
+        if (connector.getVariableDcls() != null) {
+            for (VariableDcl variableDcl : connector.getVariableDcls()) {
                 variableDcl.accept(this);
             }
         }
