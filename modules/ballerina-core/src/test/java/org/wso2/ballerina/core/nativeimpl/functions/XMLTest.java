394b83e7da2dc16131e1cf490f4f816686374e6c
Chanaka Fernando <chanaka.leadership@gmail.com>
2017-01-10 11:19:42 +0530

    Add failure path XML unit tests (#689)
    * Add failure path XML unit tests
    
    * Add the test packages which were removed during resolving the merge conflict

--- modules/ballerina-core/src/test/java/org/wso2/ballerina/core/nativeimpl/functions/XMLTest.java
+++ modules/ballerina-core/src/test/java/org/wso2/ballerina/core/nativeimpl/functions/XMLTest.java
@@ -67,6 +67,20 @@ public class XMLTest {
         Assert.assertEquals(returns[0].stringValue(), expected);
     }
 
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testGetNonExistingString() {
+        BValue[] args = {new BXML(s1), new BString("/xxx/text()")};
+        BValue[] returns = Functions.invoke(bFile, "getString", args);
+
+        Assert.assertEquals(returns[0], null);
+    }
+
+    @Test(expectedExceptions = {BallerinaException.class})
+    public void testGetStringFromMalformedXpath() {
+        BValue[] args = {new BXML(s1), new BString("$worng#path")};
+        Functions.invoke(bFile, "getString", args);
+    }
+
     @Test
     public void testGetXML() {
         BValue[] args = {new BXML(s1), new BString("/persons/person")};
@@ -77,48 +91,6 @@ public class XMLTest {
         OMElement returnElement = ((BXML) returns[0]).value();
         Assert.assertEquals(returnElement.toString().replaceAll("\\r|\\n|\\t| ", ""), "<person><name>Jack</name>" +
                 "<address>wso2</address></person>");
-    }
-
-    @Test
-    public void testSetString() {
-        BValue[] args = {new BXML(s1), new BString("/persons/person/name/text()"), new BString("Peter")};
-        BValue[] returns = Functions.invoke(bFile, "setString", args);
-
-        Assert.assertEquals(returns[0].stringValue(), "<persons><person><name>Peter" +
-                "</name><address>wso2</address></person></persons>");
-    }
-
-    @Test
-    public void testSetXML() {
-        BValue[] args = {new BXML(s2), new BString("/person/name"),
-                new BXML("<name><fname>Jack</fname><lname>Peter</lname></name>")};
-        BValue[] returns = Functions.invoke(bFile, "setXML", args);
-
-        Assert.assertTrue(returns[0] instanceof BXML);
-
-        OMElement returnElement = ((BXML) returns[0]).value();
-        Assert.assertEquals(returnElement.toString().replaceAll("\\r|\\n|\\t| ", ""), "<person><name><fname>Jack" +
-                "</fname><lname>Peter</lname></name></person>");
-    }
-
-    @Test
-    public void testAddElement() {
-        BValue[] args = {new BXML(s2), new BString("/person"), new BXML("<address>wso2</address>")};
-        BValue[] returns = Functions.invoke(bFile, "addElement", args);
-
-        Assert.assertEquals(returns[0].stringValue(), "<person><name>Jack</name>" +
-                "<address>wso2</address></person>");
-    }
-
-    @Test
-    public void testAddAttribute() {
-        BValue[] args = {new BXML(s2), new BString("/person/name"), new BString("id"), new BString("person123")};
-        BValue[] returns = Functions.invoke(bFile, "addAttribute", args);
-
-        Assert.assertTrue(returns[0] instanceof BXML);
-
-        OMElement returnElement = ((BXML) returns[0]).value();
-        Assert.assertEquals(returnElement.toString(), "<person><name id=\"person123\">Jack</name></person>");
     }
 
     @Test
@@ -138,27 +110,6 @@ public class XMLTest {
                 "</person>");
     }
 
-    @Test
-    public void testRemove() {
-        BValue[] args = {new BXML(s1), new BString("/persons/person/address")};
-        BValue[] returns = Functions.invoke(bFile, "remove", args);
-
-        Assert.assertTrue(returns[0] instanceof BXML);
-
-        OMElement returnElement = ((BXML) returns[0]).value();
-        Assert.assertEquals(returnElement.toString().replaceAll("\\r|\\n|\\t| ", ""), "<persons><person><name>Jack" +
-                "</name></person></persons>");
-    }
-
-
-    @Test(expectedExceptions = BallerinaException.class)
-    public void testGetNonExistingString() {
-        BValue[] args = {new BXML(s1), new BString("/xxx/text()")};
-        BValue[] returns = Functions.invoke(bFile, "getString", args);
-
-        Assert.assertEquals(returns[0], null);
-    }
-
     @Test(expectedExceptions = BallerinaException.class)
     public void testGetNonExistingXML() {
         BValue[] args = {new BXML(s1), new BString("/xxx")};
@@ -167,12 +118,78 @@ public class XMLTest {
         Assert.assertEquals(returns[0], null);
     }
 
+    @Test(expectedExceptions = {BallerinaException.class})
+    public void testGetXMLFromMalformedXpath() {
+        BValue[] args = {new BXML(s1), new BString("$worng#path")};
+        Functions.invoke(bFile, "getXML", args);
+    }
+
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testGetXMLFromText() {
+        BValue[] args = {new BXML(s1), new BString("/persons/person/name/text()")};
+        Functions.invoke(bFile, "getXML", args);
+    }
+
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testGetXMLFromDocumentElement() {
+        String d1 = XMLUtils.readFileToString("datafiles/xmlDocumentSample.xml");
+        BValue[] args = {new BXML(d1), new BString("/")};
+        Functions.invoke(bFile, "getXML", args);
+    }
+
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testGetXMLFromAttribute() {
+        String a1 = XMLUtils.readFileToString("datafiles/messageComplex.xml");
+        BValue[] args = {new BXML(a1), new BString("/employees/employee/@id")};
+        Functions.invoke(bFile, "getXML", args);
+    }
+
+    @Test
+    public void testSetString() {
+        BValue[] args = {new BXML(s1), new BString("/persons/person/name/text()"), new BString("Peter")};
+        BValue[] returns = Functions.invoke(bFile, "setString", args);
+
+        Assert.assertEquals(returns[0].stringValue(), "<persons><person><name>Peter" +
+                "</name><address>wso2</address></person></persons>");
+    }
+
     @Test
     public void testSetStringToNonExistingElement() {
         BValue[] args = {new BXML(s1), new BString("/xxx/text()"), new BString("Peter")};
         BValue[] returns = Functions.invoke(bFile, "setString", args);
 
         Assert.assertEquals(returns[0].stringValue(), s1);
+    }
+
+    @Test(expectedExceptions = {BallerinaException.class})
+    public void testSetStringToMalformedXpath() {
+        BValue[] args = {new BXML(s1), new BString("$worng#path"), new BString("Peter")};
+        Functions.invoke(bFile, "setString", args);
+    }
+
+    @Test
+    public void testSetStringToAttribute() {
+        String a1 = XMLUtils.readFileToString("datafiles/messageSimple.xml");
+        BValue[] args = {new BXML(a1), new BString("/employee/@id"), new BString("0")};
+        BValue[] returns = Functions.invoke(bFile, "setString", args);
+
+        Assert.assertEquals(returns[0].stringValue(), "<employee id=\"0\">\n" +
+                "    <name>Parakum</name>\n" +
+                "    <age>32</age>\n" +
+                "</employee>");
+    }
+
+    @Test
+    public void testSetXML() {
+        BValue[] args = {new BXML(s2), new BString("/person/name"),
+                new BXML("<name><fname>Jack</fname><lname>Peter</lname></name>")};
+        BValue[] returns = Functions.invoke(bFile, "setXML", args);
+
+        Assert.assertTrue(returns[0] instanceof BXML);
+
+        OMElement returnElement = ((BXML) returns[0]).value();
+        Assert.assertEquals(returnElement.toString().replaceAll("\\r|\\n|\\t| ", ""), "<person><name><fname>Jack" +
+                "</fname><lname>Peter</lname></name></person>");
     }
 
     @Test
@@ -187,12 +204,73 @@ public class XMLTest {
         Assert.assertEquals(returnElement.toString().replaceAll("\\r|\\n|\\t| ", ""), s2);
     }
 
+
+    @Test(expectedExceptions = {BallerinaException.class})
+    public void testSetXMLToMalformedXpath() {
+        BValue[] args = {new BXML(s2), new BString("$worng#path"),
+                new BXML("<name><fname>Jack</fname><lname>Peter</lname></name>")};
+        Functions.invoke(bFile, "setXML", args);
+    }
+
+    @Test(expectedExceptions = {BallerinaException.class})
+    public void testSetXMLNullValue() {
+        BValue[] args = {new BXML(s2), new BString("/person/name"),
+                null};
+        Functions.invoke(bFile, "setXML", args);
+    }
+
+    @Test
+    public void testAddElement() {
+        BValue[] args = {new BXML(s2), new BString("/person"), new BXML("<address>wso2</address>")};
+        BValue[] returns = Functions.invoke(bFile, "addElement", args);
+
+        Assert.assertEquals(returns[0].stringValue(), "<person><name>Jack</name>" +
+                "<address>wso2</address></person>");
+    }
+
     @Test(expectedExceptions = BallerinaException.class)
     public void testAddElementToNonExistingElement() {
         BValue[] args = {new BXML(s2), new BString("/xxx"), new BXML("<address>wso2</address>")};
         BValue[] returns = Functions.invoke(bFile, "addElement", args);
 
         Assert.assertEquals(returns[0].stringValue(), s2);
+    }
+
+    @Test(expectedExceptions = {BallerinaException.class})
+    public void testAddElementToMalformedXpath() {
+        BValue[] args = {new BXML(s2), new BString("$worng#path"), new BXML("<address>wso2</address>")};
+        Functions.invoke(bFile, "addElement", args);
+    }
+
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testAddElementToText() {
+        BValue[] args = {new BXML(s1), new BString("/persons/person/name/text()"), new BXML("<address>wso2</address>")};
+        Functions.invoke(bFile, "addElement", args);
+    }
+
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testAddElementToDocumentElement() {
+        String d1 = XMLUtils.readFileToString("datafiles/xmlDocumentSample.xml");
+        BValue[] args = {new BXML(d1), new BString("/"), new BXML("<address>wso2</address>")};
+        Functions.invoke(bFile, "addElement", args);
+    }
+
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testAddElementToAttribute() {
+        String a1 = XMLUtils.readFileToString("datafiles/messageComplex.xml");
+        BValue[] args = {new BXML(a1), new BString("/employees/employee/@id"), new BXML("<address>wso2</address>")};
+        Functions.invoke(bFile, "addElement", args);
+    }
+
+    @Test
+    public void testAddAttribute() {
+        BValue[] args = {new BXML(s2), new BString("/person/name"), new BString("id"), new BString("person123")};
+        BValue[] returns = Functions.invoke(bFile, "addAttribute", args);
+
+        Assert.assertTrue(returns[0] instanceof BXML);
+
+        OMElement returnElement = ((BXML) returns[0]).value();
+        Assert.assertEquals(returnElement.toString(), "<person><name id=\"person123\">Jack</name></person>");
     }
 
     @Test(expectedExceptions = BallerinaException.class)
@@ -206,48 +284,50 @@ public class XMLTest {
         Assert.assertEquals(returnElement.toString(), s2);
     }
 
-    @Test
-    public void testRemoveNonExistingElement() {
-        BValue[] args = {new BXML(s1), new BString("/xxx")};
-        Functions.invoke(bFile, "remove", args);
-    }
-
-
-    @Test(expectedExceptions = {BallerinaException.class})
-    public void testGetStringFromMalformedXpath() {
-        BValue[] args = {new BXML(s1), new BString("$worng#path")};
-        Functions.invoke(bFile, "getString", args);
-    }
-
-    @Test(expectedExceptions = {BallerinaException.class})
-    public void testGetXMLFromMalformedXpath() {
-        BValue[] args = {new BXML(s1), new BString("$worng#path")};
-        Functions.invoke(bFile, "getXML", args);
-    }
-
-    @Test(expectedExceptions = {BallerinaException.class})
-    public void testSetStringToMalformedXpath() {
-        BValue[] args = {new BXML(s1), new BString("$worng#path"), new BString("Peter")};
-        Functions.invoke(bFile, "setString", args);
-    }
-
-    @Test(expectedExceptions = {BallerinaException.class})
-    public void testSetXMLToMalformedXpath() {
-        BValue[] args = {new BXML(s2), new BString("$worng#path"),
-                new BXML("<name><fname>Jack</fname><lname>Peter</lname></name>")};
-        Functions.invoke(bFile, "setXML", args);
-    }
-
-    @Test(expectedExceptions = {BallerinaException.class})
-    public void testAddElementToMalformedXpath() {
-        BValue[] args = {new BXML(s2), new BString("$worng#path"), new BXML("<address>wso2</address>")};
-        Functions.invoke(bFile, "addElement", args);
-    }
-
     @Test(expectedExceptions = {BallerinaException.class})
     public void testAddAttributeToMalformedXpath() {
         BValue[] args = {new BXML(s2), new BString("$worng#path"), new BString("id"), new BString("person123")};
         Functions.invoke(bFile, "addAttribute", args);
+    }
+
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testAddAttributeToText() {
+        BValue[] args = {new BXML(s2), new BString("/persons/person/name/text()"), new BString("id"),
+                new BString("person123")};
+        Functions.invoke(bFile, "addAttribute", args);
+    }
+
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testAddAttributeToDocumentElement() {
+        String d1 = XMLUtils.readFileToString("datafiles/xmlDocumentSample.xml");
+        BValue[] args = {new BXML(d1), new BString("/"), new BString("id"), new BString("person123")};
+        Functions.invoke(bFile, "addAttribute", args);
+    }
+
+    @Test(expectedExceptions = BallerinaException.class)
+    public void testAddAttributeToAttribute() {
+        String a1 = XMLUtils.readFileToString("datafiles/messageComplex.xml");
+        BValue[] args = {new BXML(a1), new BString("/employees/employee/@id"), new BString("id"),
+                new BString("person123")};
+        Functions.invoke(bFile, "addAttribute", args);
+    }
+
+    @Test
+    public void testRemove() {
+        BValue[] args = {new BXML(s1), new BString("/persons/person/address")};
+        BValue[] returns = Functions.invoke(bFile, "remove", args);
+
+        Assert.assertTrue(returns[0] instanceof BXML);
+
+        OMElement returnElement = ((BXML) returns[0]).value();
+        Assert.assertEquals(returnElement.toString().replaceAll("\\r|\\n|\\t| ", ""), "<persons><person><name>Jack" +
+                "</name></person></persons>");
+    }
+
+    @Test
+    public void testRemoveNonExistingElement() {
+        BValue[] args = {new BXML(s1), new BString("/xxx")};
+        Functions.invoke(bFile, "remove", args);
     }
 
     @Test(expectedExceptions = {BallerinaException.class})
