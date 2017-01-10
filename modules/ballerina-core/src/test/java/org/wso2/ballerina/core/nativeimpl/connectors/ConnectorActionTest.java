fa961f5d497a74fbe4827386cfb46896a7b126ec
Senduran <bsenduran@gmail.com>
2017-01-10 16:43:29 +0530

    Add connector tests

--- modules/ballerina-core/src/test/java/org/wso2/ballerina/core/nativeimpl/connectors/ConnectorActionTest.java
+++ modules/ballerina-core/src/test/java/org/wso2/ballerina/core/nativeimpl/connectors/ConnectorActionTest.java
@@ -22,6 +22,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.wso2.ballerina.core.model.BallerinaFile;
 import org.wso2.ballerina.core.model.values.BBoolean;
+import org.wso2.ballerina.core.model.values.BInteger;
 import org.wso2.ballerina.core.model.values.BString;
 import org.wso2.ballerina.core.model.values.BValue;
 import org.wso2.ballerina.core.utils.ParserUtils;
@@ -35,18 +36,84 @@ public class ConnectorActionTest {
         bFile = ParserUtils.parseBalFile("lang/connectors/connector-actions.bal");
     }
 
-    @Test(description = "Test connector actions")
-    public void testConnectorAction() {
-        BValue[] returns = Functions.invoke(bFile, "callConnector");
+    @Test(description = "Test TestConnector action1")
+    public void testConnectorAction1() {
+        BValue[] returns = Functions.invoke(bFile, "testAction1");
 
-        Assert.assertEquals(returns.length, 2);
+        Assert.assertEquals(returns.length, 1);
 
-        BBoolean actionInvoked = (BBoolean) returns[0];
-        Assert.assertSame(actionInvoked.getClass(), BBoolean.class, "Invalid class type returned.");
-        Assert.assertEquals(actionInvoked.booleanValue(), true, "action named action1 failed");
+        BBoolean actionReturned = (BBoolean) returns[0];
+        Assert.assertSame(actionReturned.getClass(), BBoolean.class, "Invalid class type returned.");
+        Assert.assertEquals(actionReturned.booleanValue(), false, "action named action1 failed");
+    }
 
-        BString parameter = (BString) returns[1];
-        Assert.assertSame(parameter.getClass(), BString.class, "Invalid class type returned.");
-        Assert.assertEquals(parameter.stringValue(), "MyParameter", "action named action3 failed");
+    @Test(description = "Test TestConnector action2")
+    public void testConnectorAction2() {
+        BValue[] returns = Functions.invoke(bFile, "testAction2");
+
+        Assert.assertEquals(returns.length, 0);
+    }
+
+    @Test(description = "Test TestConnector action3")
+    public void testConnectorAction3() {
+        BValue[] returns = Functions.invoke(bFile, "testAction3");
+
+        Assert.assertEquals(returns.length, 1);
+
+        BBoolean actionReturned = (BBoolean) returns[0];
+        Assert.assertSame(actionReturned.getClass(), BBoolean.class, "Invalid class type returned.");
+        Assert.assertEquals(actionReturned.booleanValue(), false, "action named action3 failed");
+    }
+
+    @Test(description = "Test TestConnector action2 and action3")
+    public void testConnectorAction2andAction3() {
+        BValue[] returns = Functions.invoke(bFile, "testAction2andAction3");
+
+        Assert.assertEquals(returns.length, 1);
+
+        BBoolean actionReturned = (BBoolean) returns[0];
+        Assert.assertSame(actionReturned.getClass(), BBoolean.class, "Invalid class type returned.");
+        Assert.assertEquals(actionReturned.booleanValue(), true, "action 2 and 3 failed");
+    }
+
+    @Test(description = "Test TestConnector action4")
+    public void testConnectorAction4() {
+        String inputParam = "inputParam";
+        BValue[] functionArgs = new BValue[] { new BString(inputParam) };
+        BValue[] returns = Functions.invoke(bFile, "testAction4", functionArgs);
+
+        Assert.assertEquals(returns.length, 1);
+
+        BString actionReturned = (BString) returns[0];
+        Assert.assertSame(actionReturned.getClass(), BString.class, "Invalid class type returned.");
+        Assert.assertEquals(actionReturned.stringValue(), inputParam, "action 4 failed");
+    }
+
+    @Test(description = "Test TestConnector action5")
+    public void testConnectorAction5() {
+        String functionArg1 = "inputParam1";
+        String functionArg2 = "inputParam2";
+        int functionArg3 = 3;
+        String functionArg4 = "inputParam4";
+        BValue[] functionArgs = new BValue[] {
+                new BString(functionArg1), new BString(functionArg2), new BInteger(functionArg3),
+                new BString(functionArg4)
+        };
+        BValue[] returns = Functions.invoke(bFile, "testAction5", functionArgs);
+
+        Assert.assertEquals(returns.length, 3);
+
+        BString returnVal1 = (BString) returns[0];
+        Assert.assertSame(returnVal1.getClass(), BString.class, "Invalid class type returned.");
+        Assert.assertEquals(returnVal1.stringValue(), functionArg4, "action 5 failed on first return value");
+
+        //TODO: uncomment the following assertion once the multi value return support is added. Issue #702
+//        BString returnVal2 = (BString) returns[1];
+//        Assert.assertSame(returnVal2.getClass(), BString.class, "Invalid class type returned.");
+//        Assert.assertEquals(returnVal2.stringValue(), functionArg2, "action 5 failed on 2nd return value");
+//
+//        BInteger returnVal3 = (BInteger) returns[2];
+//        Assert.assertSame(returnVal3.getClass(), BString.class, "Invalid class type returned.");
+//        Assert.assertEquals(returnVal3.intValue(), functionArg3, "action 5 failed on 3rd return value");
     }
 }
