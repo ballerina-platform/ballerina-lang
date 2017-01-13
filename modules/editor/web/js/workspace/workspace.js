fdbb6044eccd02f5799e93b4172c31b50e7a8fe8
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-01-13 17:39:15 +0530

    Add shortcut label & make commands a part of menu

--- modules/editor/web/js/workspace/workspace.js
+++ modules/editor/web/js/workspace/workspace.js
@@ -15,7 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-define(['jquery', 'lodash', 'backbone', 'log', 'dialogs', 'welcome-page', 'tab/tab', 'workspace'],
+define(['jquery', 'lodash', 'backbone', 'log', 'dialogs', 'welcome-page', 'tab/tab', 'workspace', 'bootstrap'],
     function ($, _, Backbone, log, Dialogs, WelcomePages, GenericTab, Workspace) {
 
     // workspace manager constructor
@@ -164,26 +164,27 @@ define(['jquery', 'lodash', 'backbone', 'log', 'dialogs', 'welcome-page', 'tab/t
             self.updateUndoRedoMenus();
         };
 
-        app.commandManager.registerCommand("create-new-tab", {key: ["ctrl+alt+n", "command+option+n"]});
+        this.showAboutDialog = function(){
+            var aboutModal = $(_.get(app, 'config.about_dialog.selector'));
+            aboutModal.modal('show')
+        };
+
         app.commandManager.registerHandler('create-new-tab', this.createNewTab);
 
-        app.commandManager.registerCommand("undo", {key: ["ctrl+z", "command+z"]});
         app.commandManager.registerHandler('undo', this.handleUndo);
 
-        app.commandManager.registerCommand("redo", {key: ["ctrl+shift+z", "command+shift+z"]});
         app.commandManager.registerHandler('redo', this.handleRedo);
 
         // Open file save dialog
-        app.commandManager.registerCommand("open-file-save-dialog", {key:  ["ctrl+s", "command+s"]});
         app.commandManager.registerHandler('open-file-save-dialog', this.openFileSaveDialog);
 
         // Open file open dialog
-        app.commandManager.registerCommand("open-file-open-dialog", {key:  ["ctrl+o", "command+o"]});
         app.commandManager.registerHandler('open-file-open-dialog', this.openFileOpenDialog);
 
         // Go to Welcome Page.
-        app.commandManager.registerCommand("go-to-welcome-page", {key: ["ctrl+alt+w", "command+option+w"]});
         app.commandManager.registerHandler('go-to-welcome-page', this.goToWelcomePage);
+
+        app.commandManager.registerHandler('show-about-dialog', this.showAboutDialog);
 
     }
 
