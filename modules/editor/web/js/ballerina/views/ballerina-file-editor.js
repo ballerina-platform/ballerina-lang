a1e6ac86e1fe29469d15f0903f9235124f686dda
Viraj <sumudithavi@gmail.com>
2017-01-30 00:11:16 +0530

    Update tool palette with added connectors

--- modules/editor/web/js/ballerina/views/ballerina-file-editor.js
+++ modules/editor/web/js/ballerina/views/ballerina-file-editor.js
@@ -19,11 +19,11 @@ define(['lodash', 'jquery', 'log', './ballerina-view', './service-definition-vie
         './../ast/ballerina-ast-factory', './../ast/package-definition', './source-view',
         './../visitors/source-gen/ballerina-ast-root-visitor','./../visitors/symbol-table/ballerina-ast-root-visitor', './../tool-palette/tool-palette',
         './../undo-manager/undo-manager','./backend', './../ast/ballerina-ast-deserializer', './connector-definition-view', './struct-definition-view',
-        './../env/package', './../env/package-scoped-environment', './../env/environment', './constant-definitions-pane-view'],
+        './../env/package', './../env/package-scoped-environment', './../env/environment', './constant-definitions-pane-view', './../item-provider/tool-palette-item-provider'],
     function (_, $, log, BallerinaView, ServiceDefinitionView, FunctionDefinitionView, BallerinaASTRoot, BallerinaASTFactory,
               PackageDefinition, SourceView, SourceGenVisitor, SymbolTableGenVisitor, ToolPalette, UndoManager, Backend, BallerinaASTDeserializer,
               ConnectorDefinitionView, StructDefinitionView, Package, PackageScopedEnvironment, BallerinaEnvironment,
-              ConstantsDefinitionsPaneView) {
+              ConstantsDefinitionsPaneView, ToolPaletteItemProvider) {
 
         /**
          * The view to represent a ballerina file editor which is an AST visitor.
@@ -234,8 +234,10 @@ define(['lodash', 'jquery', 'log', './ballerina-view', './service-definition-vie
                 throw errMsg;
             }
 
+            var toolPaletteItemProvider = new ToolPaletteItemProvider();
             var toolPaletteContainer = $(this._container).find(_.get(this._viewOptions, 'design_view.tool_palette.container')).get(0);
             var toolPaletteOpts = _.clone(_.get(this._viewOptions, 'design_view.tool_palette'));
+            toolPaletteOpts.itemProvider = toolPaletteItemProvider;
             toolPaletteOpts.container = toolPaletteContainer;
             toolPaletteOpts.ballerinaFileEditor = this;
             this.toolPalette = new ToolPalette(toolPaletteOpts);
@@ -259,14 +261,14 @@ define(['lodash', 'jquery', 'log', './ballerina-view', './service-definition-vie
             //TODO remove this for adding filecontext to the map
             this.diagramRenderingContext.ballerinaFileEditor = this;
 
-            var symbolTableGenVisitor = new SymbolTableGenVisitor(this._package);
+            var symbolTableGenVisitor = new SymbolTableGenVisitor(this._package, this._model);
             this._model.accept(symbolTableGenVisitor);
             var package = symbolTableGenVisitor.getPackage();
-            this.toolPalette._imports.push(package);
+            this.toolPalette.getItemProvider().addImport(package);
 
             //adding default packages TODO : this needs to be rendered by referring to imports in the model
             var httpPackage = BallerinaEnvironment.searchPackage("ballerina.net.http");
-            this.toolPalette._imports.push(httpPackage[0]);
+            this.toolPalette.getItemProvider().addImport(httpPackage[0]);
 
             // render tool palette
             this.toolPalette.render();
