71659ac0a19f5d55f9d044fc98851ed6007cc5a6
shan1024 <gambit1024@gmail.com>
2017-01-29 12:23:56 +0530

    Change source location to src

--- src/org/ballerinalang/plugins/idea/sdk/BallerinaSdkUtil.java
+++ src/org/ballerinalang/plugins/idea/sdk/BallerinaSdkUtil.java
@@ -142,7 +142,7 @@ public class BallerinaSdkUtil {
 
     @NotNull
     private static String getSrcLocation(@NotNull String version) {
-        return "src/ballerina";
+        return "src";
     }
 
     public static String getSdkHome(Project project) {
