7c9ae4b366e26eca9f49dd5861368c44912574c6
malintha <malintha@wso2.com>
2017-01-31 05:29:27 -0800

    Adding test module for ballerina editor UI tests

---
+++ modules/editor/web/test/UITests/src/test/java/TestUtils.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ * <p>
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.xml.transform.TransformerException;
+
+public class TestUtils {
+
+    public static void saveScreenshot(WebDriver driver, String screenshotFileName) throws IOException {
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File(screenshotFileName));
+    }
+
+    public static void fileWriter(String domElement, String fileName) throws ParserConfigurationException, IOException,
+            SAXException, TransformerException {
+
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        try {
+
+            fw = new FileWriter("/home/malintha/ScreenCap/src/test/java/resources/DOMFiles/"+ fileName);
+            bw = new BufferedWriter(fw);
+            bw.write(domElement);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+
+                if (bw != null)
+                    bw.close();
+
+                if (fw != null)
+                    fw.close();
+
+            } catch (IOException ex) {
+
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static String fileReader(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+    public static String preprocessDOMContent(String currentDOM){
+        return currentDOM.replaceAll("#*[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}","");
+    }
+}
