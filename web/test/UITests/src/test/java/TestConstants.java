0919ee99263a1d2e915c8c691b3500a1b76a2792
malintha <malintha@wso2.com>
2017-01-31 05:29:27 -0800

    Adding test module for ballerina editor UI tests

---
+++ web/test/UITests/src/test/java/TestConstants.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ * <p>
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+public class TestConstants {
+
+    public static String WELCOME_PAGE_OPEN_BUTTON_XPATH = "/html/body/div[1]/div[2]/div/div[3]/div[2]/div/div/div/div[2]/div[1]/div/div/div/div[1]/div[3]/button";
+    public static String FILE_OPEN_POPUP_LOCATION_INPUT_XPATH = "/html/body/div[11]/div/div/div[2]/div/form/div[1]/div/input";
+    public static String FILE_OPEN_POPUP_LOCATION_OPEN_XPATH = "/html/body/div[11]/div/div/div[2]/div/form/div[3]/div/button[1]";
+    public static String SVG_XPATH = "/html/body/div[1]/div[2]/div/div[3]/div[2]/div/div/div/div[2]/div[1]/div[2]/div[1]/div[4]";
+
+    public static String SERVER_URL = "http://localhost:9091";
+}
