90e0c2790db313738110ff44c5cdc4e185d1c3ce
malintha <malintha@wso2.com>
2017-02-01 01:31:57 -0800

    Adding test module for ballerina editor UI tests

--- modules/editor/web/test/UITests/src/test/java/TestConstants.java
+++ modules/editor/web/test/UITests/src/test/java/TestConstants.java
@@ -18,6 +18,7 @@
 
 public class TestConstants {
 
+    //XPATHS
     public static final String WELCOME_PAGE_OPEN_BUTTON_XPATH = "/html/body/div[1]/div[2]/div/div[3]/div[2]/div/div/div" +
             "/div[2]/div[1]/div/div/div/div[1]/div[3]/button";
     public static final String FILE_OPEN_POPUP_LOCATION_INPUT_XPATH = "/html/body/div[11]/div/div/div[2]/div/form/div[1]" +
@@ -28,4 +29,7 @@ public class TestConstants {
             "/div[1]/div[4]";
 
     public static final String SERVER_URL = "http://localhost:9091";
+    public static final String BALLERINA_RESOURCE_FOLDER = "BallerinaSourceFiles";
+    public static final String DOM_RESOURCE_FOLDER = "DOMFiles";
+
 }
