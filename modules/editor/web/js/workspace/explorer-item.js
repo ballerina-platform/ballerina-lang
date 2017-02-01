ac3856b0c7e4aef78c5c3a2a26373cef5c1d8031
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-02-01 13:53:03 +0530

    Remove root folder from explorer upon delete

--- modules/editor/web/js/workspace/explorer-item.js
+++ modules/editor/web/js/workspace/explorer-item.js
@@ -136,7 +136,7 @@ define(['lodash', 'log', 'file_browser', 'event_channel', 'context_menu', 'theme
                         self._fileBrowser.refresh(node);
                     }
                 };
-                    items.deleteFolder = {
+                items.deleteFolder = {
                     name: "delete",
                     icon: "",
                     callback: function () {
@@ -145,7 +145,11 @@ define(['lodash', 'log', 'file_browser', 'event_channel', 'context_menu', 'theme
                                 type: "folder",
                                 path: path,
                                 onSuccess: function(){
-                                    self._fileBrowser.refresh(node.parent);
+                                    if(isRoot){
+                                        self.application.commandManager.dispatch("remove-explorer-item", self);
+                                    } else {
+                                        self._fileBrowser.refresh(node.parent);
+                                    }
                                 }
                             });
                     }
