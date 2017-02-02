26749fe75c0f82725c5c4b6d9810f8b8d418e631
Rasika Perera <info.rasika@gmail.com>
2017-02-02 19:40:09 +0530

    Changed packages service api base path

--- modules/editor/services/workspace-service/src/gen/java/org/wso2/ballerina/tooling/service/workspace/api/PackagesApi.java
+++ modules/editor/services/workspace-service/src/gen/java/org/wso2/ballerina/tooling/service/workspace/api/PackagesApi.java
@@ -33,7 +33,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-@Path("/packages")
+@Path("/service/packages")
 @Consumes({"application/json"})
 @Produces({"application/json"})
 @io.swagger.annotations.Api(description = "the packages API")
