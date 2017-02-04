5f6efb3391e6f446d78f0a0a661a4773d0afae03
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2017-02-04 22:39:52 +0530

    add toggle option to package button

--- web/js/ballerina/views/package-definition-pane-view.js
+++ web/js/ballerina/views/package-definition-pane-view.js
@@ -50,20 +50,21 @@ define(['require', 'lodash', 'jquery', 'log', './../ast/package-definition',
                                     }).insertBefore($(currentContainer).find('.constant-definition-main-wrapper'));
             
             // Creating package button.
-            this._packageDefinitionsButton = $("<div class='package-name-btn'></div>")
+            var packageDefinitionsButton = $("<div class='package-name-btn'></div>")
                                                 .appendTo(packageWrapper);
 
-            $("<span class='fw-stack fw-lg' data-toggle='tooltip' title='Package Name' data-placement='bottom'> " +
+            var packageButtonIcon = $("<span class='fw-stack fw-lg' data-toggle='tooltip' title='Package Name' " +
+                "data-placement='bottom'> " +
                 "<i class='fw fw-circle fw-stack-2x'></i>" +
                 "<i class='fw fw-package fw-stack-1x fw-inverse'></i> </span>")
-                .appendTo(this._packageDefinitionsButton).tooltip();
+                .appendTo(packageDefinitionsButton).tooltip();
 
-            this._packageDefinitionsMainWrapper = $("<span class='package-pane'/>")
-                                                    .appendTo(this._packageDefinitionsButton);
+            var packageDefinitionsMainWrapper = $("<span class='package-pane'/>")
+                                                    .appendTo(packageDefinitionsButton);
 
             $("<span class='package-name-wrapper'>" +
                 "<input type='text' autocomplete='off' id='package-name-input'></span>")
-                .appendTo(this._packageDefinitionsMainWrapper);
+                .appendTo(packageDefinitionsMainWrapper);
 
             var packageInput = packageWrapper.find('#package-name-input');
 
@@ -75,6 +76,12 @@ define(['require', 'lodash', 'jquery', 'log', './../ast/package-definition',
             packageInput.on("change", function () {
                 currentASTRoot.setPackageName($(this).val());
             });
+
+            //handle click event on package-btn
+            $(packageButtonIcon).click(function (e) {
+                $(packageDefinitionsMainWrapper).toggle();
+            });
+            
 
         }
 
