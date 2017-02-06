70e7289883deda395736b4d853cb1528b8e4f1e9
Isuru Udana Loku Narangoda <isudana@gmail.com>
2017-02-06 22:02:41 +0530

    Introduce server connector framework to ballerina (#1488)
    * add BallerinaConnectorManager and use new API's introduced with ServerConnectors
    
    * add missing method call to registerServerConnectorProvider
    
    * Update transport version to depend on latest development branch which has server connector refactoring
    
    * add support for key/val pair support for annotaion
    
    * add support for key/val pair support for annotaion (#1290)
    
    * Remove HTTP Listener initialization logic from BServiceRunner since Listener is deprecated
    
    * Remove the usage of deprecated HTTP Transport APIs
    
    * adding Dispatcher via SPI, fix ordering of registerServerConnectorProvider
    
    * incorperate changes form carbon-message error handler interface
    
    * fix test failures and use a TestServerConnetorErrorHandler for http related unit testcases
    
    * adding a TestServerConnetorErrorHandler for http related unit test cases
    
    * update java doc and comment
    
    * changes according to server connector API changes
    
    * use ServerConnectorManager from trasnport f/w
    
    * Update packaage name of server connector framwork
    
    * handle API change from ServerConnectorErrorHandler
    
    * remove exception handling logic as it is moved to runtime exception
    
    * Fix test failures caused due to cleanup issues
    
    * update carbon-messaging and carbon-transport versions
    
    * update carbon-messaging, carbon-transport to released versions

--- modules/ballerina-core/src/main/java/org/wso2/ballerina/core/nativeimpl/connectors/http/server/HTTPListenerInitializer.java
+++ modules/ballerina-core/src/main/java/org/wso2/ballerina/core/nativeimpl/connectors/http/server/HTTPListenerInitializer.java
@@ -18,9 +18,9 @@
 
 package org.wso2.ballerina.core.nativeimpl.connectors.http.server;
 
+import org.wso2.ballerina.core.nativeimpl.connectors.BallerinaConnectorManager;
 import org.wso2.ballerina.core.nativeimpl.connectors.http.TransportConfigProvider;
 import org.wso2.ballerina.core.runtime.MessageProcessor;
-import org.wso2.ballerina.core.runtime.internal.ServiceContextHolder;
 import org.wso2.carbon.messaging.handler.HandlerExecutor;
 import org.wso2.carbon.transport.http.netty.config.ListenerConfiguration;
 import org.wso2.carbon.transport.http.netty.config.TransportProperty;
@@ -34,6 +34,7 @@ import java.util.Set;
 /**
  * {@code HTTPListenerInitializer} is responsible for initializing http transport listener side configuration
  */
+@Deprecated
 public class HTTPListenerInitializer {
 
     private static PrintStream outStream = System.out;
@@ -44,7 +45,7 @@ public class HTTPListenerInitializer {
         httpTransportContextHolder.setHandlerExecutor(new HandlerExecutor());
         httpTransportContextHolder.setMessageProcessor(new MessageProcessor());
 
-        ServiceContextHolder.getInstance().registerErrorHandler(new HTTPErrorHandler());
+        BallerinaConnectorManager.getInstance().registerServerConnectorErrorHandler(new HTTPErrorHandler());
 
         TransportsConfiguration trpConfig = TransportConfigProvider.getConfiguration();
         Set<ListenerConfiguration> listenerConfigurations = trpConfig.getListenerConfigurations();
