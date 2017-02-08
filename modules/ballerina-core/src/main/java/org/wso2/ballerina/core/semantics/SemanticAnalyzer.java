2bb112558ce8675f91e4d9e38ef75c9f790ced5a
chanakaudaya <chanaka.leadership@gmail.com>
2017-02-09 00:11:52 +0530

    Improve error printed when type convertor is not found

--- modules/ballerina-core/src/main/java/org/wso2/ballerina/core/semantics/SemanticAnalyzer.java
+++ modules/ballerina-core/src/main/java/org/wso2/ballerina/core/semantics/SemanticAnalyzer.java
@@ -1835,9 +1835,8 @@ public class SemanticAnalyzer implements NodeVisitor {
                         String funcName = (typeCastExpression.getPackageName() != null) ?
                                 typeCastExpression.getPackageName() + ":" +
                                         typeCastExpression.getName() : typeCastExpression.getName();
-                        throw new SemanticException(typeCastExpression.getNodeLocation().getFileName() + ":" +
-                                typeCastExpression.getNodeLocation().getLineNumber() +
-                                ": undefined function '" + funcName + "'");
+                        throw new SemanticException(getNodeLocationStr(typeCastExpression.getNodeLocation()) +
+                                "'" + sourceType + "' cannot be cast to '" + targetType + "'");
                     }
 
                     if (typeConvertorSymbol instanceof NativeUnitProxy) {
@@ -1865,10 +1864,8 @@ public class SemanticAnalyzer implements NodeVisitor {
                         // Link the function with the function invocation expression
                         typeCastExpression.setCallableUnit(typeConvertor);
                     } else {
-                        throw new LinkerException(typeCastExpression.getNodeLocation().getFileName() + ":" +
-                                typeCastExpression.getNodeLocation().getLineNumber() +
-                                ": type converter cannot be found for '" + sourceType
-                                + " to " + targetType + "'");
+                        throw new SemanticException(getNodeLocationStr(typeCastExpression.getNodeLocation()) +
+                                "'" + sourceType + "' cannot be cast to '" + targetType + "'");
                     }
                 }
             }
