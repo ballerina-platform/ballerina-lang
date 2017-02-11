bdeb5312336ff10649adca9d3e88752ff82430d2
Grainier Perera <grainier@wso2.com>
2017-02-11 09:02:10 +0530

    Fix build failure (#735)
    # Conflicts:
    #	modules/siddhi-core/src/main/java/org/wso2/siddhi/core/util/parser/OutputParser.java
    #	modules/siddhi-extensions/output-mappers/map-output-mapper/src/main/java/org/wso2/siddhi/extension/output/mapper/map/MapOutputMapper.java
    #	modules/siddhi-extensions/output-mappers/map-output-mapper/src/test/java/org/wso2/siddhi/extension/output/mapper/map/MapOutputMapperTestCase.java
    #	modules/siddhi-extensions/output-mappers/map-output-mapper/src/test/java/org/wso2/siddhi/extension/output/mapper/map/MapOutputMapperWithSiddhiQLTestCase.java

--- siddhi-query-api/src/main/java/org/wso2/siddhi/query/api/execution/io/map/AttributeMapping.java
+++ siddhi-query-api/src/main/java/org/wso2/siddhi/query/api/execution/io/map/AttributeMapping.java
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-package org.wso2.siddhi.core.util.transport;
+package org.wso2.siddhi.query.api.execution.io.map;
 
 public class AttributeMapping {
     private String mapping;
