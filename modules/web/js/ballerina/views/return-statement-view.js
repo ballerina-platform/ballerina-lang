226f5067f8ea6646cbcf7d27c7fbea0637ae924d
nipunamarcus <nipunamarcus2@gmail.com>
2017-02-12 20:18:45 +0530

    added return string when statement box is empty

--- modules/web/js/ballerina/views/return-statement-view.js
+++ modules/web/js/ballerina/views/return-statement-view.js
@@ -85,6 +85,9 @@ define(['lodash', 'log', './../ast/return-statement', './simple-statement-view',
         };
 
         ReturnStatementView.prototype.updateReturnExpression = function (newReturnExpression, propertyKey) {
+            if(!newReturnExpression.includes("return")){
+                newReturnExpression = "return "+newReturnExpression;
+            }
             this._model.setReturnExpression(newReturnExpression);
             var displayText = this._model.getReturnExpression();
             this.renderDisplayText(displayText);
