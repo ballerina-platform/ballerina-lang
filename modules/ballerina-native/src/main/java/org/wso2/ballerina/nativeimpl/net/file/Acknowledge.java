a1fd8367b6667a5db6715fac43a1619ba97da1e3
dilini <dilini@wso2.com>
2017-02-13 18:58:03 +0530

    Adding Acknowledge funtion for file server connector.

--- modules/ballerina-native/src/main/java/org/wso2/ballerina/nativeimpl/net/file/Acknowledge.java
+++ modules/ballerina-native/src/main/java/org/wso2/ballerina/nativeimpl/net/file/Acknowledge.java
@@ -23,14 +23,11 @@ import org.slf4j.LoggerFactory;
 import org.wso2.ballerina.core.exception.BallerinaException;
 import org.wso2.ballerina.core.interpreter.Context;
 import org.wso2.ballerina.core.model.types.TypeEnum;
-import org.wso2.ballerina.core.model.util.MessageUtils;
 import org.wso2.ballerina.core.model.values.BMessage;
-import org.wso2.ballerina.core.model.values.BString;
 import org.wso2.ballerina.core.model.values.BValue;
 import org.wso2.ballerina.core.nativeimpl.AbstractNativeFunction;
 import org.wso2.ballerina.core.nativeimpl.annotations.Argument;
 import org.wso2.ballerina.core.nativeimpl.annotations.BallerinaFunction;
-import org.wso2.ballerina.core.nativeimpl.annotations.ReturnType;
 import org.wso2.carbon.messaging.CarbonMessage;
 import org.wso2.carbon.messaging.StreamingCarbonMessage;
 
@@ -42,46 +39,39 @@ import org.wso2.carbon.messaging.StreamingCarbonMessage;
  */
 @BallerinaFunction(
         packageName = "ballerina.net.file",
-        functionName = "getStringPayload",
+        functionName = "acknowledge",
         args = {@Argument(name = "message", type = TypeEnum.MESSAGE)},
-        returnType = {@ReturnType(type = TypeEnum.STRING)},
         isPublic = true
 )
-public class GetStringPayload extends AbstractNativeFunction {
+public class Acknowledge extends AbstractNativeFunction {
 
-    private static final Logger log = LoggerFactory.getLogger(GetStringPayload.class);
+    private static final Logger log = LoggerFactory.getLogger(Acknowledge.class);
+    private static final String FILE_NAME = "FILE_NAME";
 
     @Override
     public BValue[] execute(Context context) {
-        BString result;
+        BMessage msg;
+        CarbonMessage cMsg = null;
         try {
-            BMessage msg = (BMessage) getArgument(context, 0);
-            if (msg.isAlreadyRead()) {
-                result = new BString(msg.getMessageDataSource().getMessageAsString());
+            msg = (BMessage) getArgument(context, 0);
+            cMsg = msg.value();
+
+            if (cMsg instanceof StreamingCarbonMessage) {
+                context.getBalCallback().done(cMsg);
             } else {
-                CarbonMessage cMsg = msg.value();
-                String payload = MessageUtils.getStringFromInputStream(cMsg.getInputStream());
-                handleCallBack(cMsg, context);
-                result = new BString(payload);
-                msg.setMessageDataSource(payload);
-                msg.setAlreadyRead(true);
+                throw new BallerinaException("Invalid message type received. " +
+                        "Required: " + StreamingCarbonMessage.class.getCanonicalName() +
+                        ". Found: " + cMsg.getClass().getCanonicalName());
             }
+
             if (log.isDebugEnabled()) {
-                log.debug("Payload in String:" + result.stringValue());
+                log.debug("Acknowledged file: " + cMsg.getHeader(FILE_NAME));
             }
         } catch (Throwable e) {
-            throw new BallerinaException("Error while retrieving string payload from message: " + e.getMessage());
+            throw new BallerinaException("Error while acknowledging file" +
+                    (cMsg == null ? ". " : ": " + cMsg.getHeader(FILE_NAME))
+                    + " . Reason : " + e.getMessage());
         }
-        return getBValues(result);
-    }
-
-    private void handleCallBack(CarbonMessage message, Context context) {
-        if (message instanceof StreamingCarbonMessage) {
-            context.getBalCallback().done(message);
-        } else {
-            throw new BallerinaException("Invalid message type received. " +
-                    "Required: " + StreamingCarbonMessage.class.getCanonicalName() +
-                    ". Found: " + message.getClass().getCanonicalName());
-        }
+        return VOID_RETURN;
     }
 }
