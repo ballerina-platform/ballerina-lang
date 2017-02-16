85f05c6ec3c0ef028b5e4ad75e4b617c08ceaa48
irunika <irunikail@gmail.com>
2017-02-16 14:32:00 +0530

    Fixed bug of null session return return.

--- http/org.wso2.carbon.transport.http.netty/src/main/java/org/wso2/carbon/transport/http/netty/listener/SourceHandler.java
+++ http/org.wso2.carbon.transport.http.netty/src/main/java/org/wso2/carbon/transport/http/netty/listener/SourceHandler.java
@@ -190,6 +190,7 @@ public class SourceHandler extends ChannelInboundHandlerAdapter {
             due to a protocol error.
              */
             ctx.channel().writeAndFlush(new CloseWebSocketFrame(1002, ""));
+
             ctx.close();
             throw new ProtocolException("Error occurred in HTTP to WebSocket Upgrade : " + e.getMessage());
         }
