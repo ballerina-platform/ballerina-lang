2245df34451d3ada7030adbaa12c6173fdc3ef8c
tishan89 <dtishanpubudu@gmail.com>
2017-02-17 15:27:18 +0530

    adding licence

--- modules/swagger-ballerina-generator/src/main/java/org/wso2/ballerina/swagger/code/generator/cmd/Langs.java
+++ modules/swagger-ballerina-generator/src/main/java/org/wso2/ballerina/swagger/code/generator/cmd/Langs.java
@@ -1,3 +1,20 @@
+/*
+*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
 package org.wso2.ballerina.swagger.code.generator.cmd;
 
 import ch.lambdaj.collection.LambdaIterable;
@@ -8,11 +25,6 @@ import static ch.lambdaj.Lambda.on;
 import static ch.lambdaj.collection.LambdaCollections.with;
 import static java.util.ServiceLoader.load;
 
-/**
- * User: lanwen
- * Date: 24.03.15
- * Time: 20:25
- */
 @Command(name = "langs", description = "Shows available langs")
 public class Langs implements Runnable {
     @Override
--- modules/swagger-ballerina-generator/src/main/java/org/wso2/ballerina/swagger/code/generator/cmd/Meta.java
+++ modules/swagger-ballerina-generator/src/main/java/org/wso2/ballerina/swagger/code/generator/cmd/Meta.java
@@ -1,3 +1,20 @@
+/*
+*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
 package org.wso2.ballerina.swagger.code.generator.cmd;
 
 import ch.lambdaj.function.convert.Converter;
@@ -21,12 +38,6 @@ import java.util.Map;
 
 import static ch.lambdaj.collection.LambdaCollections.with;
 import static com.google.common.base.Joiner.on;
-
-/**
- * User: lanwen
- * Date: 24.03.15
- * Time: 20:22
- */
 
 @Command(name = "meta", description = "MetaGenerator. Generator for creating a new template set " +
         "and configuration for Codegen.  The output will be based on the language you " +
