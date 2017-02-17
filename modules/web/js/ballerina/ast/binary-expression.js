9835550d52ad7fd716d89d39e03ef9d5a265c718
Malintha <malintha@wso2.com>
2017-02-17 13:19:17 +0530

    Fixing extra quotes issue for string type variables

--- modules/web/js/ballerina/ast/binary-expression.js
+++ modules/web/js/ballerina/ast/binary-expression.js
@@ -48,12 +48,13 @@ define(['lodash', './expression'], function (_, Expression) {
     BinaryExpression.prototype.generateExpressionString = function (jsonNode) {
         var self = this;
         var expString = "";
-
-        var leftExpression = self.getFactory().createFromJson(jsonNode.children[0]);
-        var rightExpression = self.getFactory().createFromJson(jsonNode.children[1]);
-        leftExpression.initFromJson(jsonNode.children[0]);
-        rightExpression.initFromJson(jsonNode.children[1]);
-        expString = leftExpression.getExpression() + this.getOperator() + rightExpression.getExpression();
+        if(!_.isNil(jsonNode.children[0]) && !_.isNil(jsonNode.children[1])){
+            var leftExpression = self.getFactory().createFromJson(jsonNode.children[0]);
+            var rightExpression = self.getFactory().createFromJson(jsonNode.children[1]);
+            leftExpression.initFromJson(jsonNode.children[0]);
+            rightExpression.initFromJson(jsonNode.children[1]);
+            expString = leftExpression.getExpression() + " " + this.getOperator() + " " + rightExpression.getExpression();
+        }
         return expString;
     };
 
