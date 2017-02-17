26a1fe74b68eae7c755b0def89809679792a26cb
shan1024 <gambit1024@gmail.com>
2017-02-17 17:37:31 +0530

    Add multi parameter support to run config
    Resolves #117

--- src/org/ballerinalang/plugins/idea/run/configuration/BallerinaApplicationRunningState.java
+++ src/org/ballerinalang/plugins/idea/run/configuration/BallerinaApplicationRunningState.java
@@ -28,6 +28,13 @@ import com.intellij.util.ObjectUtils;
 import org.ballerinalang.plugins.idea.sdk.BallerinaSdkUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.ballerinalang.plugins.idea.run.configuration.BallerinaRunUtil.PARAMETER_REGEX;
+
 public class BallerinaApplicationRunningState extends BallerinaRunningState {
 
     public BallerinaApplicationRunningState(Project project, String params, ExecutionEnvironment environment) {
@@ -49,10 +56,20 @@ public class BallerinaApplicationRunningState extends BallerinaRunningState {
         commandLine.addParameter(BallerinaRunUtil.getOpenFilePath(getProject()));
         commandLine.withCharset(CharsetToolkit.UTF8_CHARSET);
         // Add program arguments
-        commandLine.addParameter(getParams());
+        getArguments(getParams()).forEach(commandLine::addParameter);
 
         KillableColoredProcessHandler handler = new KillableColoredProcessHandler(commandLine, true);
         ProcessTerminatedListener.attach(handler);
         return handler;
+    }
+
+    private List<String> getArguments(String params) {
+        List<String> results = new ArrayList<>();
+        Pattern pattern = Pattern.compile(PARAMETER_REGEX);
+        Matcher matcher = pattern.matcher(params);
+        while (matcher.find()) {
+            results.add(matcher.group(0));
+        }
+        return results;
     }
 }
