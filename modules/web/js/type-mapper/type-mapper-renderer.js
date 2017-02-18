917e16bb4de787dbd1b26e3b45803191b2a4b951
sinthuja <sinthuja@wso2.com>
2017-02-18 15:50:22 +0530

    Putting back tharik's chnages which I removed temporarily.

--- modules/web/js/type-mapper/type-mapper-renderer.js
+++ modules/web/js/type-mapper/type-mapper-renderer.js
@@ -619,6 +619,7 @@ define(['require', 'lodash', 'jquery', 'jsPlumb', 'dagre', 'alerts'], function (
                     self.midpoint = self.midpoint + self.midpointVariance;
                     self.jsPlumbInstance.importDefaults({Connector: self.getConnectorConfig(self.midpoint)});
                     self.onConnection(connection);
+                    return false;
                     // self.disableParentsJsTree(params.sourceId, self);
                     // self.disableParentsJsTree(params.targetId, self);
                 } else {
@@ -630,6 +631,7 @@ define(['require', 'lodash', 'jquery', 'jsPlumb', 'dagre', 'alerts'], function (
                         connection.complexMapperName = compatibleTypeConverters[0];
                         self.jsPlumbInstance.importDefaults({Connector: self.getConnectorConfig(self.midpoint)});
                         self.onConnection(connection);
+                        return false;
                         // self.disableParentsJsTree(params.sourceId, self);
                         // self.disableParentsJsTree(params.targetId, self);
                     } else {
