974d2eee9b0c0a713a76e37f1508f36229a3a69f
Pahan Sarathchandra <pahan@wso2.com>
2017-02-18 23:47:30 +0530

    filter breakpoints by file name when triggering breakpoints reset

--- modules/web/js/tab/file-tab.js
+++ modules/web/js/tab/file-tab.js
@@ -112,10 +112,13 @@ define(['require', 'log', 'jquery', 'lodash', './tab', 'ballerina', 'workspace/f
                 this.removeAllBreakpoints();
             });
 
-            var breakPointChangeCallback = function() {
+            var breakPointChangeCallback = function(debugPointChangedFileName) {
                 var fileName = self._file.getName();
-                var newBreakpoints = DebugManager.getDebugPoints(fileName);
-                fileEditor.trigger('reset-breakpoints', newBreakpoints);
+                
+                if(debugPointChangedFileName === fileName) {
+                    var newBreakpoints = DebugManager.getDebugPoints(fileName);
+                    fileEditor.trigger('reset-breakpoints', newBreakpoints);
+                }
             };
             DebugManager.on('breakpoint-added', breakPointChangeCallback);
             DebugManager.on('breakpoint-removed', breakPointChangeCallback);
