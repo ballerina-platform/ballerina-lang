dab338adc4e06fa123c0e9511afb66c293e440e7
Chanaka Fernando <chanaka.leadership@gmail.com>
2017-02-23 15:04:47 +0530

    Fix for not allowing built-in type name as identifiers (#2091)
    * Fix built-in type name as identifiers issue #1986
    
    * Remove additional new line
    
    * Add test case for built-in-types not allowed as identifiers fix
    
    * Fix line length formatting

--- modules/ballerina-core/src/main/java/org/ballerinalang/util/exceptions/SemanticErrors.java
+++ modules/ballerina-core/src/main/java/org/ballerinalang/util/exceptions/SemanticErrors.java
@@ -78,7 +78,8 @@ public enum SemanticErrors {
     BREAK_STMT_NOT_ALLOWED_HERE("break.stmt.not.allowed.here", "SEMANTIC_0053"),
     UNDEFINED_TYPE_MAPPER("undefined.type.mapper", "SEMANTIC_0054"),
     UNDEFINED_ACTION_IN_CONNECTOR("undefined.action.in.connector", "SEMANTIC_0055"),
-    INCOMPATIBLE_TYPES_UNKNOWN_FOUND("incompatible.types.unknown.found", "SEMANTIC_0056");
+    INCOMPATIBLE_TYPES_UNKNOWN_FOUND("incompatible.types.unknown.found", "SEMANTIC_0056"),
+    BUILT_IN_TYPE_NAMES_NOT_ALLOWED_AS_IDENTIFIER("built.in.type.names.not.allowed.as.identifier", "SEMANTIC_0057");
 
     private String errorMsgKey;
     private String errorCode;
