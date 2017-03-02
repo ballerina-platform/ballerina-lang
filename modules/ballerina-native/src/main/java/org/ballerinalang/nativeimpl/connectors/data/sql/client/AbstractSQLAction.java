e56ae3f91fee846dd55bc78fff415078f8b33d3d
anupama-pathirage <anupama@wso2.com>
2017-03-02 23:17:26 +0530

    Fix issue with stored procedure call with resultset returns

--- modules/ballerina-native/src/main/java/org/ballerinalang/nativeimpl/connectors/data/sql/client/AbstractSQLAction.java
+++ modules/ballerina-native/src/main/java/org/ballerinalang/nativeimpl/connectors/data/sql/client/AbstractSQLAction.java
@@ -140,10 +140,9 @@ public abstract class AbstractSQLAction extends AbstractNativeAction {
             conn = connector.getSQLConnection();
             stmt = getPreparedCall(conn, connector, query);
             createProcessedStatement(stmt, parameters);
-            boolean hasResult = stmt.execute();
+            rs = executeStoredProc(stmt);
             setOutParameters(stmt, parameters);
-            if (hasResult) {
-                rs = stmt.getResultSet();
+            if (rs != null) {
                 BDataTable datatable = new BDataTable(new SQLDataIterator(conn, stmt, rs), new HashMap<>(),
                         getColumnDefinitions(rs));
                 context.getControlStack().setReturnValue(0, datatable);
@@ -403,5 +402,27 @@ public abstract class AbstractSQLAction extends AbstractNativeAction {
         } catch (SQLException e) {
             throw new BallerinaException("error in getting out parameter value: " + e.getMessage(), e);
         }
+    }
+
+    private ResultSet executeStoredProc(CallableStatement stmt) throws SQLException {
+        boolean resultAndNoUpdateCount = stmt.execute();
+        ResultSet result = null;
+        while (true) {
+            if (!resultAndNoUpdateCount) {
+                int updateCount = stmt.getUpdateCount();
+                if (updateCount == -1) {
+                    break;
+                }
+            } else {
+                result = stmt.getResultSet();
+                break;
+            }
+            try {
+                resultAndNoUpdateCount = stmt.getMoreResults(Statement.KEEP_CURRENT_RESULT);
+            } catch (SQLException e) {
+                break;
+            }
+        }
+        return result;
     }
 }
