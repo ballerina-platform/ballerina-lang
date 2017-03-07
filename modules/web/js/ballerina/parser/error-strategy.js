769713ae06917f68dd45c87862e87304401c8e40
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-03-07 14:58:10 +0530

    Refactor error-strategy module to use ES6

--- modules/web/js/ballerina/parser/error-strategy.js
+++ modules/web/js/ballerina/parser/error-strategy.js
@@ -16,122 +16,61 @@
  * under the License.
  */
 
-var DefaultErrorStrategy = require('antlr4/error/ErrorStrategy').DefaultErrorStrategy;
+import {DefaultErrorStrategy} from 'antlr4/error/ErrorStrategy;
 
-var BLangParserErrorStrategy = function() {
+class BLangParserErrorStrategy extends DefaultErrorStrategy {
 
-};
-
-BLangParserErrorStrategy.prototype = Object.create(DefaultErrorStrategy.prototype);
-BLangParserErrorStrategy.prototype.constructor = BLangParserErrorStrategy;
-
-// This is called by {@link //reportError} when the exception is a
-// {@link NoViableAltException}.
-//
-// @see //reportError
-//
-// @param recognizer the parser instance
-// @param e the recognition exception
-//
-BLangParserErrorStrategy.prototype.reportNoViableAlternative = function(recognizer, e) {
-    var tokens = recognizer.getTokenStream();
-    var input;
-    if(tokens !== null) {
-        if (e.startToken.type===Token.EOF) {
-            input = "<EOF>";
+    reportNoViableAlternative(recognizer, e) {
+        var tokens = recognizer.getTokenStream();
+        var input;
+        if (tokens !== null) {
+            if (e.startToken.type === Token.EOF) {
+                input = "<EOF>";
+            } else {
+                input = tokens.getText(new Interval(e.startToken, e.offendingToken));
+            }
         } else {
-            input = tokens.getText(new Interval(e.startToken, e.offendingToken));
+            input = "<unknown input>";
         }
-    } else {
-        input = "<unknown input>";
+        var msg = "no viable alternative at input " + this.escapeWSAndQuote(input);
+        recognizer.notifyErrorListeners(msg, e.offendingToken, e);
     }
-    var msg = "no viable alternative at input " + this.escapeWSAndQuote(input);
-    recognizer.notifyErrorListeners(msg, e.offendingToken, e);
-};
 
-//
-// This is called by {@link //reportError} when the exception is an
-// {@link InputMismatchException}.
-//
-// @see //reportError
-//
-// @param recognizer the parser instance
-// @param e the recognition exception
-//
-BLangParserErrorStrategy.prototype.reportInputMismatch = function(recognizer, e) {
-    var msg = "mismatched input " + this.getTokenErrorDisplay(e.offendingToken) +
-        " expecting " + e.getExpectedTokens().toString(recognizer.literalNames, recognizer.symbolicNames);
-    recognizer.notifyErrorListeners(msg, e.offendingToken, e);
-};
-
-//
-// This is called by {@link //reportError} when the exception is a
-// {@link FailedPredicateException}.
-//
-// @see //reportError
-//
-// @param recognizer the parser instance
-// @param e the recognition exception
-//
-BLangParserErrorStrategy.prototype.reportFailedPredicate = function(recognizer, e) {
-    var ruleName = recognizer.ruleNames[recognizer._ctx.ruleIndex];
-    var msg = "rule " + ruleName + " " + e.message;
-    recognizer.notifyErrorListeners(msg, e.offendingToken, e);
-};
-
-// This method is called to report a syntax error which requires the removal
-// of a token from the input stream. At the time this method is called, the
-// erroneous symbol is current {@code LT(1)} symbol and has not yet been
-// removed from the input stream. When this method returns,
-// {@code recognizer} is in error recovery mode.
-//
-// <p>This method is called when {@link //singleTokenDeletion} identifies
-// single-token deletion as a viable recovery strategy for a mismatched
-// input error.</p>
-//
-// <p>The default implementation simply returns if the handler is already in
-// error recovery mode. Otherwise, it calls {@link //beginErrorCondition} to
-// enter error recovery mode, followed by calling
-// {@link Parser//notifyErrorListeners}.</p>
-//
-// @param recognizer the parser instance
-//
-BLangParserErrorStrategy.prototype.reportUnwantedToken = function(recognizer) {
-    if (DefaultErrorStrategy.prototype.inErrorRecoveryMode.call(this, recognizer)) {
-        return;
+    reportInputMismatch(recognizer, e) {
+        var msg = "mismatched input " + this.getTokenErrorDisplay(e.offendingToken) +
+            " expecting " + e.getExpectedTokens().toString(recognizer.literalNames, recognizer.symbolicNames);
+        recognizer.notifyErrorListeners(msg, e.offendingToken, e);
     }
-    DefaultErrorStrategy.prototype.beginErrorCondition.call(this, recognizer);
-    var t = recognizer.getCurrentToken();
-    var tokenName = this.getTokenErrorDisplay(t);
-    var expecting = this.getExpectedTokens(recognizer);
-    var msg = "extraneous input " + tokenName + " expecting " +
-        expecting.toString(recognizer.literalNames, recognizer.symbolicNames);
-    recognizer.notifyErrorListeners(msg, t, null);
-};
-// This method is called to report a syntax error which requires the
-// insertion of a missing token into the input stream. At the time this
-// method is called, the missing token has not yet been inserted. When this
-// method returns, {@code recognizer} is in error recovery mode.
-//
-// <p>This method is called when {@link //singleTokenInsertion} identifies
-// single-token insertion as a viable recovery strategy for a mismatched
-// input error.</p>
-//
-// <p>The default implementation simply returns if the handler is already in
-// error recovery mode. Otherwise, it calls {@link //beginErrorCondition} to
-// enter error recovery mode, followed by calling
-// {@link Parser//notifyErrorListeners}.</p>
-//
-// @param recognizer the parser instance
-//
-BLangParserErrorStrategy.prototype.reportMissingToken = function(recognizer) {
-    if ( DefaultErrorStrategy.prototype.inErrorRecoveryMode.call(this, recognizer)) {
-        return;
+
+    reportFailedPredicate(recognizer, e) {
+        var ruleName = recognizer.ruleNames[recognizer._ctx.ruleIndex];
+        var msg = "rule " + ruleName + " " + e.message;
+        recognizer.notifyErrorListeners(msg, e.offendingToken, e);
     }
-    DefaultErrorStrategy.prototype.beginErrorCondition.call(this, recognizer);
-    var t = recognizer.getCurrentToken();
-    var expecting = DefaultErrorStrategy.prototype.getExpectedTokens.call(this, recognizer);
-    var msg = "missing " + expecting.toString(recognizer.literalNames, recognizer.symbolicNames) +
-        " at " + DefaultErrorStrategy.prototype.getTokenErrorDisplay.call(this, t);
-    recognizer.notifyErrorListeners(msg, t, null);
-};
+
+    reportUnwantedToken(recognizer) {
+        if (DefaultErrorStrategy.prototype.inErrorRecoveryMode.call(this, recognizer)) {
+            return;
+        }
+        DefaultErrorStrategy.prototype.beginErrorCondition.call(this, recognizer);
+        var t = recognizer.getCurrentToken();
+        var tokenName = this.getTokenErrorDisplay(t);
+        var expecting = this.getExpectedTokens(recognizer);
+        var msg = "extraneous input " + tokenName + " expecting " +
+            expecting.toString(recognizer.literalNames, recognizer.symbolicNames);
+        recognizer.notifyErrorListeners(msg, t, null);
+    }
+    
+    reportMissingToken(recognizer) {
+        if (DefaultErrorStrategy.prototype.inErrorRecoveryMode.call(this, recognizer)) {
+            return;
+        }
+        DefaultErrorStrategy.prototype.beginErrorCondition.call(this, recognizer);
+        var t = recognizer.getCurrentToken();
+        var expecting = DefaultErrorStrategy.prototype.getExpectedTokens.call(this, recognizer);
+        var msg = "missing " + expecting.toString(recognizer.literalNames, recognizer.symbolicNames) +
+            " at " + DefaultErrorStrategy.prototype.getTokenErrorDisplay.call(this, t);
+        recognizer.notifyErrorListeners(msg, t, null);
+    }
+
+}
