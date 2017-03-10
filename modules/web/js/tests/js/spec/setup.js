4e6a61a47387936d5bb0675955a7faa2a55b0683
Aruna Herath <arunah@wso2.com>
2017-03-10 11:00:18 +0530

    Fix some of the lint problems

--- modules/web/js/tests/js/spec/setup.js
+++ modules/web/js/tests/js/spec/setup.js
@@ -22,7 +22,7 @@ var jsdom = require('jsdom').jsdom;
 
 global.document = jsdom('');
 global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
+Object.keys(document.defaultView).forEach(function(property) {
   if (typeof global[property] === 'undefined') {
     global[property] = document.defaultView[property];
   }
