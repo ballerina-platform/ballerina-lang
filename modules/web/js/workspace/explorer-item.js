c06d0dc76a2a20902a016fcf922a9f78e3bd0e26
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-03-23 10:42:38 +0530

    Refactor explorer item to use ES6 & fix lint issues

--- modules/web/js/workspace/explorer-item.js
+++ modules/web/js/workspace/explorer-item.js
@@ -15,46 +15,45 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
+import $ from 'jquery';
 import _ from 'lodash';
-import log from 'log';
 import FileBrowser from 'file_browser';
 import EventChannel from 'event_channel';
 import ContextMenu from 'context_menu';
 import 'bootstrap';
 
-    var ExplorerItem = function(args){
+class ExplorerItem extends EventChannel {
+
+    constructor(args) {
+        super();
         _.assign(this, args);
-    };
+    }
 
-    ExplorerItem.prototype = Object.create(EventChannel.prototype);
-    ExplorerItem.prototype.constructor = ExplorerItem;
+    getFolderName (folderPath) {
+        var splitArr = _.split(folderPath, this.application.getPathSeperator());
+        return _.gt(_.last(splitArr).length, 0) ? _.last(splitArr) :
+                _.nth(splitArr, splitArr.length - 2);
+    }
 
-    ExplorerItem.prototype.getFolderName = function (folderPath) {
-            var splitArr = _.split(folderPath, this.application.getPathSeperator());
-            return _.gt(_.last(splitArr).length, 0) ? _.last(splitArr) :
-                    _.nth(splitArr, splitArr.length - 2);
-    };
-
-    ExplorerItem.prototype.render = function(){
+    render(){
         var item = $('<div class="folder-tree"><div>'),
-            folderName = $("<span>" + this.getFolderName(this.path) +  "</span>"),
-            id = "folder-tree_" + this.index,
+            folderName = $('<span>' + this.getFolderName(this.path) +  '</span>'),
+            id = 'folder-tree_' + this.index,
             header = $('<div class="folder-tree-header" role="button" href="#' + id +
                 '"+ data-toggle="collapse" aria-expanded="true" aria-controls="' +
                 id + '"></div>'),
             body = $('<div class="collapse folder-tree-body" id="' + id +
                 '"></div>'),
-            folderIcon = $("<i class='fw fw-folder item-icon'></i>"),
-            arrowHeadIcon = $("<i class='fw fw-right expand-icon'></i>");
+            folderIcon = $('<i class="fw fw-folder item-icon"></i>'),
+            arrowHeadIcon = $('<i class="fw fw-right expand-icon"></i>');
 
-        header.attr("id", this.path);
+        header.attr('id', this.path);
         header.append(arrowHeadIcon);
         header.append(folderIcon);
         header.append(folderName);
         item.append(header);
         item.append(body);
-        this.container.find('.mCSB_container').append(item); //add to mscroller container 
+        this.container.find('.mCSB_container').append(item); //add to mscroller container
         this._itemElement = item;
 
         header.attr('title', this.path);
@@ -65,11 +64,11 @@ import 'bootstrap';
         });
 
         body.on('show.bs.collapse', function(){
-            arrowHeadIcon.addClass("fw-rotate-90");
+            arrowHeadIcon.addClass('fw-rotate-90');
         });
 
         body.on('hide.bs.collapse', function(){
-            arrowHeadIcon.removeClass("fw-rotate-90");
+            arrowHeadIcon.removeClass('fw-rotate-90');
         });
 
         var fileBrowser = new FileBrowser({
@@ -78,39 +77,39 @@ import 'bootstrap';
             fetchFiles: true
         });
         fileBrowser.render();
-        fileBrowser.on("double-click-node", function(node){
+        fileBrowser.on('double-click-node', function(node){
             if(_.isEqual('file', node.type)){
-                this.application.commandManager.dispatch("open-file", node.id);
+                this.application.commandManager.dispatch('open-file', node.id);
             }
         }, this);
 
         this._contextMenu = new ContextMenu({
             container: item,
-            selector: ".folder-tree-header, li",
+            selector: '.folder-tree-header, li',
             provider: this.createContextMenuProvider()
         });
         this._fileBrowser = fileBrowser;
-    };
+    }
 
-    ExplorerItem.prototype.remove = function(){
+    remove(){
         this._itemElement.remove();
-    };
+    }
 
-    ExplorerItem.prototype.createContextMenuProvider = function(){
+    createContextMenuProvider(){
         var self = this;
-        return function($trigger, e) {
+        return function($trigger) {
             var items = {},
                 menu = {items: items},
-                isRoot = $trigger.hasClass("folder-tree-header"),
+                isRoot = $trigger.hasClass('folder-tree-header'),
                 path = $trigger.attr('id'),
                 node = isRoot ? self._fileBrowser.getNode('#') : self._fileBrowser.getNode(path);
 
             if(isRoot || _.isEqual('folder', node.type)){
                 items.createNewFile = {
-                    name: "new file",
-                    icon: "",
+                    name: 'new file',
+                    icon: '',
                     callback: function () {
-                        self.application.commandManager.dispatch("create-new-item-at-path",
+                        self.application.commandManager.dispatch('create-new-item-at-path',
                             {
                                 path: path,
                                 type: 'ballerina-file',
@@ -121,10 +120,10 @@ import 'bootstrap';
                     }
                 };
                 items.createNewFolder = {
-                    name: "new folder",
-                    icon: "",
+                    name: 'new folder',
+                    icon: '',
                     callback: function () {
-                        self.application.commandManager.dispatch("create-new-item-at-path",
+                        self.application.commandManager.dispatch('create-new-item-at-path',
                             {
                                 path: path,
                                 type: 'folder',
@@ -135,60 +134,61 @@ import 'bootstrap';
                     }
                 };
                 items.refreshBtn = {
-                    name: "refresh",
-                    icon: "",
+                    name: 'refresh',
+                    icon: '',
                     callback: function () {
                         self._fileBrowser.refresh(node);
                     }
                 };
                 items.deleteFolder = {
-                    name: "delete",
-                    icon: "",
+                    name: 'delete',
+                    icon: '',
                     callback: function () {
-                        self.application.commandManager.dispatch("remove-from-disk",
+                        self.application.commandManager.dispatch('remove-from-disk',
                             {
-                                type: "folder",
+                                type: 'folder',
                                 path: path,
                                 onSuccess: function(){
                                     if(isRoot){
-                                        self.application.commandManager.dispatch("remove-explorer-item", self);
+                                        self.application.commandManager.dispatch('remove-explorer-item', self);
                                     } else {
                                         self._fileBrowser.refresh(node.parent);
                                     }
                                 }
                             });
                     }
-                }
+                };
             }
             else if(_.isEqual('file', node.type)){
                 items.deleteFile = {
-                    name: "delete",
-                    icon: "",
+                    name: 'delete',
+                    icon: '',
                     callback: function () {
-                        self.application.commandManager.dispatch("remove-from-disk",
+                        self.application.commandManager.dispatch('remove-from-disk',
                             {
-                                type: "file",
+                                type: 'file',
                                 path: node.id,
                                 onSuccess: function(){
                                     self._fileBrowser.refresh(node.parent);
                                 }
                             });
                     }
-                }
+                };
             }
 
             if(isRoot){
                 items.removeFolderFromExplorer = {
-                    name: "remove folder",
-                    icon: "",
+                    name: 'remove folder',
+                    icon: '',
                     callback: function () {
-                        self.application.commandManager.dispatch("remove-explorer-item", self);
+                        self.application.commandManager.dispatch('remove-explorer-item', self);
                     }
                 };
             }
             return menu;
         };
-    };
+    }
 
-    export default ExplorerItem;
-        
+}
+
+export default ExplorerItem;
