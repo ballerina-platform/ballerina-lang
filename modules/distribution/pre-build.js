7cd613f5f245e22f9f9460148e221563b1c80f9e
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-03-27 17:47:03 +0530

    Improve electron build

---
+++ modules/distribution/pre-build.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const unzip = require('unzip2');
+const path = require('path');
+
+function functionName() {
+
+    fs.createReadStream(path.join(__dirname, '../web/target/ballerina-composer-web-0.85-SNAPSHOT.zip')).pipe(unzip.Extract({ path: './resources' }));
+}
+
+functionName();
