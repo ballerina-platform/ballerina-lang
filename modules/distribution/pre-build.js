72bc987fa201dfc0212485f063a818961f8f9d40
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-03-28 15:27:32 +0530

    Refactor electron module

--- modules/distribution/pre-build.js
+++ modules/distribution/pre-build.js
@@ -3,7 +3,6 @@ const unzip = require('unzip2');
 const path = require('path');
 
 function functionName() {
-
     fs.createReadStream(path.join(__dirname, '../web/target/ballerina-composer-web-0.85-SNAPSHOT.zip')).pipe(unzip.Extract({ path: './resources' }));
 }
 
