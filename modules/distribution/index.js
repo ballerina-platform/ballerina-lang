9ef445fc1004552f2f74c72553e2b4e8c49efb81
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-03-29 15:58:29 +0530

    Improve electron build

--- modules/distribution/index.js
+++ modules/distribution/index.js
@@ -9,10 +9,12 @@ let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1024, height: 768});
+  win = new BrowserWindow({width: 1024, height: 768, frame: true});
 
+  // maximize the window
   win.maximize();
 
+  //disable native menu
   Menu.setApplicationMenu(Menu.buildFromTemplate([]));
 
   // and load the index.html of the app.
