e9b4cf3234b9f1ecc37ec998bf47ff4035caf8eb
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-04-07 10:18:01 +0530

    Add annotation keyword to ace grammar

--- modules/web/js/ballerina/utils/ace-mode.js
+++ modules/web/js/ballerina/utils/ace-mode.js
@@ -34,7 +34,7 @@ ace.define('ace/mode/ballerina',
                 "ballerina-keyword-other": "import|version|public",
                 "ballerina-keyword-primitive-type": "boolean|int|long|float|double|string",
                 "ballerina-keyword-non-primitive-type": "message|map|exception|json|xml|xmlDocument",
-                "ballerina-keyword-definition": "package|type|typemapper|connector|function|resource|service|action|worker|struct",
+                "ballerina-keyword-definition": "annotation|package|type|typemapper|connector|function|resource|service|action|worker|struct",
                 "ballerina-keyword-language": "const|true|false|reply|create"
             }, "ballerina-identifier");
 
