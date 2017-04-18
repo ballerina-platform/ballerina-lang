54dd877b2af4d80fff203b2aa061b26dd3875bbd
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-04-18 11:57:12 +0530

    Apply restrictions & beautify native open wizards

--- modules/distribution/src/workspace.js
+++ modules/distribution/src/workspace.js
@@ -3,7 +3,11 @@ const {ipcMain, dialog} = require('electron');
 function setupNativeWizards() {
       ipcMain.on('show-file-open-dialog', function (event) {
           dialog.showOpenDialog({
-              properties: ['openFile']
+              title: 'Open Ballerina File',
+              filters: [
+                  {name: 'Ballerina Files (*.bal) ', extensions: ['bal']},
+              ],
+              properties: ['openFile', 'promptToCreate']
             }, function (file) {
               if (file) event.sender.send('file-opened', file)
             }
@@ -12,7 +16,8 @@ function setupNativeWizards() {
 
       ipcMain.on('show-folder-open-dialog', function (event) {
           dialog.showOpenDialog({
-              properties: ['openFolder']
+                title: 'Open Ballerina Folder',
+                properties: ['openDirectory', 'createDirectory']
             }, function (folder) {
               if (folder) event.sender.send('folder-opened', folder)
             }
