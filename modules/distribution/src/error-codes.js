ad412d86fc10ce31dd9332c32b6a5e48b88a5cb0
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2017-04-20 17:43:38 +0530

    Add eslint to dist module & fix lint issues

--- modules/distribution/src/error-codes.js
+++ modules/distribution/src/error-codes.js
@@ -1,5 +1,6 @@
-module.exports = { ErrorCodes : {
-    'JAVA_NOT_FOUND': 101,
-    'SERVICE_FAILED': 102
-  }
-}
+module.exports = {
+    ErrorCodes : {
+        'JAVA_NOT_FOUND': 101,
+        'SERVICE_FAILED': 102
+    }
+};
--- modules/distribution/src/error-window.js
+++ modules/distribution/src/error-window.js
@@ -1,21 +1,20 @@
-const url = require("url");
-const path = require("path");
-const {app, BrowserWindow, Menu} = require("electron");
+const url = require('url');
+const path = require('path');
+const {BrowserWindow} = require('electron');
 
 function createErrorWindow(args) {
-        // Create the browser window.
-        let win = new BrowserWindow({width: 600, height: 500,
-            resizable: false, frame: true});
-        let windowUrl = url.format({
-            pathname: path.join(__dirname, "..", "pages", "error.html"),
-            protocol: "file:",
-            slashes: true
-        });
-        console.log(new Buffer(args.errorMessage).toString('base64'));
-        win.loadURL(windowUrl + '?errorCode=' + args.errorCode
-                + '&errorMessage='
-                + new Buffer(args.errorMessage).toString('base64'));
-        return win;
+    // Create the browser window.
+    let win = new BrowserWindow({width: 600, height: 500,
+        resizable: false, frame: true});
+    let windowUrl = url.format({
+        pathname: path.join(__dirname, '..', 'pages', 'error.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+    win.loadURL(windowUrl + '?errorCode=' + args.errorCode
+            + '&errorMessage='
+            + new Buffer(args.errorMessage).toString('base64'));
+    return win;
 }
 
 module.exports = {createErrorWindow};
