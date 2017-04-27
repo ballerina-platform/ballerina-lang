1f98cc07b3cc29a4ce90bbe36d6a1b2da3e5e9cc
irunika <irunikail@gmail.com>
2017-04-27 11:44:34 +0530

    Add integration test cases for WebSocket pushText

--- modules/tests/test-integration/src/test/java/org/ballerinalang/test/listener/TestExecutionListener.java
+++ modules/tests/test-integration/src/test/java/org/ballerinalang/test/listener/TestExecutionListener.java
@@ -137,10 +137,13 @@ public class TestExecutionListener implements IExecutionListener {
         String[] sampleFiles = {
                 sampleDir + File.separator + "echoService" + File.separator + "echoService.bal",
                 sampleDir + File.separator + "helloWorldService" + File.separator + "helloWorldService.bal",
+                sampleDir + File.separator + "helloWorldService" + File.separator + "helloWorldService.bal",
                 sampleDir + File.separator + "passthroughService" + File.separator + "passthroughService.bsz",
                 sampleDir + File.separator + "restfulService" + File.separator + "ecommerceService.bsz",
                 sampleDir + File.separator + "routingServices" + File.separator + "routingServices.bsz",
-                sampleDir + File.separator + "serviceChaining" + File.separator + "ATMLocatorService.bsz"
+                sampleDir + File.separator + "serviceChaining" + File.separator + "ATMLocatorService.bsz",
+                sampleDir + File.separator + "websocket" + File.separator + "echoServer" + File.separator + "server" +
+                        File.separator + "websocketEchoServer.bal"
         };
         return sampleFiles;
     }
