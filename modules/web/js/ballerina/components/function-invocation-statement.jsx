76ca0987c91de4e5587ebaad3543adb8d10561a6
Viraj <sumudithavi@gmail.com>
2017-04-27 17:36:49 +0530

    Add Reply statement react component

--- modules/web/js/ballerina/components/function-invocation-statement.jsx
+++ modules/web/js/ballerina/components/function-invocation-statement.jsx
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 import React from "react";
 import StatementDecorator from "./statement-decorator";
 import PropTypes from 'prop-types';
@@ -6,13 +23,10 @@ class FunctionInvocationStatement extends React.Component {
 
 	render() {
 		let model = this.props.model,
-				bBox = model.viewState.bBox;
-		const text_x = bBox.x + (bBox.w / 2);
-		const text_y = bBox.y + (bBox.h / 2);
-		return (<StatementDecorator bBox={bBox}>
-							<text x={text_x} y={text_y} className="statement-text">{model.expression}</text>
-						</StatementDecorator>) ;
-    }
+			expression = model.expression,
+			bBox = model.viewState.bBox;
+		return (<StatementDecorator bBox={bBox} expression={expression} />);
+	}
 }
 
 FunctionInvocationStatement.propTypes = {
@@ -22,8 +36,8 @@ FunctionInvocationStatement.propTypes = {
 		w: PropTypes.number.isRequired,
 		h: PropTypes.number.isRequired,
 	}),
-	model: PropTypes.shape({
-		expression:  PropTypes.string
+	expression: PropTypes.shape({
+		expression: PropTypes.string
 	})
 }
 
