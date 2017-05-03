d64cd37f9507b1253d5344c05f740bb8dafc92e7
Joseph Fonseka <hevayo@gmail.com>
2017-05-03 12:28:00 +0530

    fix canvas size issues

--- modules/web/js/ballerina/configs/designer-defaults.js
+++ modules/web/js/ballerina/configs/designer-defaults.js
@@ -120,13 +120,11 @@ export const blockStatement = {
 };
 
 export const canvas = {
-    body: {
-        padding: {
-            top: 50,
-            right: 50,
-            bottom: 50,
-            left: 50
-        },
+    padding: {
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50
     }
 };
 
