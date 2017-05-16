c5e9ca494ae44d93ce31e0ab458d52db5a3380b4
Jerad Rutnam <jeradrutnam@outlook.com>
2017-05-16 18:24:30 +0530

    import Declaration Styling

--- modules/web/js/ballerina/components/import-declaration-expanded.jsx
+++ modules/web/js/ballerina/components/import-declaration-expanded.jsx
@@ -29,7 +29,7 @@ export default class importDeclarationExpanded extends React.Component {
         super();
         this.state = {
             showSuggestions: false,
-        }
+        };
         this.packageSuggestions = BallerinaEnvironment.getPackages().map(p => {
             return {
                 name: p.getName()
@@ -61,7 +61,7 @@ export default class importDeclarationExpanded extends React.Component {
         const topBarBbox = {
             x: bBox.x,
             y: bBox.y + topGutter
-        }
+        };
 
         let lastImportElementY = topBarBbox.y + topBarHeight;
 
@@ -89,22 +89,24 @@ export default class importDeclarationExpanded extends React.Component {
             bBox: textBoxBBox,
             onChange: () => {},
             initialValue: '',
-        }
+        };
 
         return (
             <g className="package-definitions-collection">
-                <rect x={ topBarBbox.x } y={ topBarBbox.y } height={topBarHeight} width={importDeclarationWidth} style={ { fill: "#ddd"} } />
+                <rect x={ topBarBbox.x } y={ topBarBbox.y } height={ topBarHeight } width={importDeclarationWidth} style={ { fill: "#ddd"} } />
+                <rect x={ topBarBbox.x } y={ topBarBbox.y } height={ topBarHeight } className="import-definition-decorator" />
                 <text x={ topBarBbox.x + leftPadding } y={ topBarBbox.y + topBarHeight/2} className="import-declaration-topbar-label">Imports</text>
                 <image width={ iconSize } height={ iconSize } className="property-pane-action-button-delete"
                     onClick={this.props.onCollapse} xlinkHref={ ImageUtil.getSVGIconString('hide') }
                     x={bBox.x + importDeclarationWidth - iconSize - 6 } y={topBarBbox.y + (topBarHeight-iconSize)/2}/>
                 {importElements}
-                <rect x={ bBox.x } y={ lastImportElementY } height={importInputHeight} width={importDeclarationWidth}/>
+                <rect x={ bBox.x } y={ lastImportElementY } height={importInputHeight} width={importDeclarationWidth} className="add-import-button-background" />
                 <g onClick={this.handleAddImportClick}>
                     <rect x={ bBox.x + 7 } y={ lastImportElementY + 7 } height={importInputHeight - 14} width={importDeclarationWidth - 14}
                         className="add-import-button" />
                     <text x={ bBox.x + 14 } y={ lastImportElementY + importInputHeight/2 } className="add-import-button-text" >{'+ Add Import'}</text>
                 </g>
+                <rect x={ bBox.x } y={ lastImportElementY } height={importInputHeight} className="import-definition-decorator" />
                 <SuggestionsText x={ bBox.x + 5 } y={lastImportElementY + 5} height={importInputHeight - 10}
                     width={importDeclarationWidth - 10} suggestionsPool={this.packageSuggestions} show={this.state.showSuggestions}
                     onBlur={this.handleAddImportBlur} onEnter={this.props.onAddImport}/>
