646c616e2dd30177f515dcc2a711576500847069
Sajith Ravindra <sajithshn@gmail.com>
2017-05-17 14:13:42 +0530

    Adding documentation to a public method (#372)
    * Adding documentation to a public method
    
    * Correcting docs of method

--- siddhi-core/src/main/java/org/wso2/siddhi/core/stream/output/sink/distributed/DistributedTransport.java
+++ siddhi-core/src/main/java/org/wso2/siddhi/core/stream/output/sink/distributed/DistributedTransport.java
@@ -61,28 +61,33 @@ public abstract class DistributedTransport extends Sink {
     }
 
     /**
-     * This is method contains the additional parameters which require to initialize distributed transport
      *
-     * @param streamDefinition
-     * @param transportOptionHolder
-     * @param sinkConfigReader
-     * @param mapperConfigReader
-     * @param executionPlanContext
-     * @param destinationOptionHolders
-     * @param sinkAnnotation
-     * @param strategy
+     * @param streamDefinition Definition of the stream this sink instance is publishing to
+     * @param type Type of the transport that (e.g., TCP, JMS)
+     * @param transportOptionHolder Option holder for carrying options for the transport
+     * @param sinkConfigReader  This hold the {@link Sink} extensions configuration reader for the sink
+     * @param sinkMapper Hold the mapper that's used in this sink
+     * @param mapType Type of the mapper
+     * @param mapOptionHolder Options of the mapper
+     * @param payload The template of the payload message
+     * @param mapperConfigReader This hold the {@link Sink} extensions configuration reader for the mapper
+     * @param executionPlanContext The execution plan context
+     * @param destinationOptionHolders List of option holders containing the options mentioned in @destination
+     * @param sinkAnnotation The annotation of the Sink
+     * @param strategy Publishing strategy to be used by the distributed transport
+     * @param supportedDynamicOptions List of supported dynamic options
      */
     public void init(StreamDefinition streamDefinition, String type, OptionHolder transportOptionHolder,
                      ConfigReader sinkConfigReader,
-                     SinkMapper sinkMapper, String mapType, OptionHolder mapOptionHolder, String payload,
+                     SinkMapper sinkMapper, String mapType, OptionHolder mapOptionHolder, String payloadTemplate
+            ,
                      ConfigReader mapperConfigReader, ExecutionPlanContext executionPlanContext, List<OptionHolder>
                              destinationOptionHolders, Annotation sinkAnnotation, DistributionStrategy strategy,
-                     String[]
-                             supportedDynamicOptions) {
+                     String[] supportedDynamicOptions) {
         this.strategy = strategy;
         this.supportedDynamicOptions = supportedDynamicOptions;
         init(streamDefinition, type, transportOptionHolder, sinkConfigReader, sinkMapper, mapType, mapOptionHolder,
-             payload, mapperConfigReader, executionPlanContext);
+                payloadTemplate, mapperConfigReader, executionPlanContext);
         initTransport(sinkOptionHolder, destinationOptionHolders, sinkAnnotation, sinkConfigReader,
                       executionPlanContext);
     }
