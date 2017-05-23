5beefa60742859654af5e0151f3dfb746f2ec301
Kevin Ratnasekera <djkevincr@yahoo.com>
2017-05-23 13:59:10 +0530

    Clran up commented code

--- modules/ballerina-core/src/test/java/org/ballerinalang/model/expressions/ArrayLengthAccessExprTest.java
+++ modules/ballerina-core/src/test/java/org/ballerinalang/model/expressions/ArrayLengthAccessExprTest.java
@@ -39,7 +39,6 @@ public class ArrayLengthAccessExprTest {
     @BeforeClass
     public void setup() {
         bLangProgram = BTestUtils.parseBalFile("lang/expressions/array-length-access-expr.bal");
-        //System.setProperty(Constants.SYS_PROP_ENABLE_NONBLOCKING, "true");
     }
 
     @Test(description = "Test array length access expression")
