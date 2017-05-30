51c2963194cb59895dc3042d9573759a6db8548a
Rajith <lanka.vitharana@gmail.com>
2017-05-30 11:40:03 +0530

    Remove unused import

--- modules/ballerina-core/src/test/java/org/ballerinalang/model/statements/IfElseStmtTest.java
+++ modules/ballerina-core/src/test/java/org/ballerinalang/model/statements/IfElseStmtTest.java
@@ -18,7 +18,6 @@
 package org.ballerinalang.model.statements;
 
 import org.ballerinalang.core.utils.BTestUtils;
-import org.ballerinalang.model.BLangProgram;
 import org.ballerinalang.model.values.BInteger;
 import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BValue;
