9b345623738f406a6e243bdd711ed78976de10c4
shan1024 <gambit1024@gmail.com>
2017-06-06 14:40:28 +0530

    Fix map key value formatting issue
    Resolves #364

--- src/main/java/org/ballerinalang/plugins/idea/formatter/BallerinaBlock.java
+++ src/main/java/org/ballerinalang/plugins/idea/formatter/BallerinaBlock.java
@@ -121,6 +121,10 @@ public class BallerinaBlock extends AbstractBlock {
                     if (parentElementType == WORKER_DECLARATION) {
                         indent = Indent.getSpaceIndent(4);
                     }
+                } else if (childElementType == MAP_STRUCT_KEY_VALUE) {
+                    if (parentElementType == MAP_STRUCT_LITERAL) {
+                        indent = Indent.getSpaceIndent(4);
+                    }
                 }
 
                 // If the child node text is empty, the IDEA core will throw an exception.
