56b81ee997ad3bd59725bfe5b253a819c2a421dc
shan1024 <gambit1024@gmail.com>
2017-06-07 14:31:26 +0530

    Fix suggestions issue in transform statement
    Resolves #370

--- src/main/java/org/ballerinalang/plugins/idea/BallerinaParserDefinition.java
+++ src/main/java/org/ballerinalang/plugins/idea/BallerinaParserDefinition.java
@@ -78,6 +78,7 @@ import org.ballerinalang.plugins.idea.psi.ResourceDefinitionNode;
 import org.ballerinalang.plugins.idea.psi.ReturnTypeListNode;
 import org.ballerinalang.plugins.idea.psi.ServiceBodyNode;
 import org.ballerinalang.plugins.idea.psi.StructBodyNode;
+import org.ballerinalang.plugins.idea.psi.TransformStatementBodyNode;
 import org.ballerinalang.plugins.idea.psi.TypeNameNode;
 import org.ballerinalang.plugins.idea.psi.ServiceDefinitionNode;
 import org.ballerinalang.plugins.idea.psi.StatementNode;
@@ -299,6 +300,8 @@ public class BallerinaParserDefinition implements ParserDefinition {
                 return new FunctionInvocationNode(node);
             case BallerinaParser.RULE_annotationAttribute:
                 return new AnnotationAttributeNode(node);
+            case BallerinaParser.RULE_transformStatementBody:
+                return new TransformStatementBodyNode(node);
             default:
                 return new ANTLRPsiNode(node);
         }
