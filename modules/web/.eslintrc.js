5eb3b5ea63f24b100a162ed571c1c46dd400b302
Hemika Kodikara <hemika.kodikara@gmail.com>
2017-06-13 16:18:23 +0530

    Adding linter rules.

--- modules/web/.eslintrc.js
+++ modules/web/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": "airbnb",
     "rules": {
         "linebreak-style": [
             "error",
