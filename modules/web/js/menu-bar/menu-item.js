bb564104c670cbe2cdd70037dd2b980d6c6409bd
Maheeka Jayasuriya <maheeka@wso2.com>
2017-06-16 14:31:00 +0530

    move menu-item to es6

--- modules/web/js/menu-bar/menu-item.js
+++ modules/web/js/menu-bar/menu-item.js
@@ -20,81 +20,82 @@ import $ from 'jquery';
 import _ from 'lodash';
 import EventChannel from 'event_channel';
 
-const MenuItem = function (args) {
-    _.assign(this, args);
-    this._application = _.get(this, 'options.application');
-};
+class MenuItem extends EventChannel {
 
-MenuItem.prototype = Object.create(EventChannel.prototype);
-MenuItem.prototype.constructor = MenuItem;
-
-MenuItem.prototype.render = function () {
-    const parent = _.get(this, 'options.parent');
-
-    const item = $('<li></li>');
-    const title = $('<span class="pull-left"></span>');
-    const link = $('<a></a>');
-    parent.append(item);
-    item.append(link);
-    link.append(title);
-
-    title.text(_.get(this, 'definition.label'));
-    this._linkElement = link;
-    this._title = title;
-    this._listItemElement = item;
-
-    const shortcuts = _.get(this, 'definition.command.shortcuts');
-    const commandId = _.get(this, 'definition.command.id');
-    if (!_.isNil(shortcuts)) {
-        this._application.commandManager.registerCommand(commandId, { shortcuts });
-        this.renderShortcutLabel();
-    } else {
-        this._application.commandManager.registerCommand(commandId, {});
+    constructor(args) {
+        super();
+        _.assign(this, args);
+        this._application = _.get(this, 'options.application');
     }
 
-    if (_.get(this, 'definition.disabled')) {
-        this.disable();
-    } else {
-        this.enable();
+    render() {
+        const parent = _.get(this, 'options.parent');
+
+        const item = $('<li></li>');
+        const title = $('<span class="pull-left"></span>');
+        const link = $('<a></a>');
+        parent.append(item);
+        item.append(link);
+        link.append(title);
+
+        title.text(_.get(this, 'definition.label'));
+        this._linkElement = link;
+        this._title = title;
+        this._listItemElement = item;
+
+        const shortcuts = _.get(this, 'definition.command.shortcuts');
+        const commandId = _.get(this, 'definition.command.id');
+        if (!_.isNil(shortcuts)) {
+            this._application.commandManager.registerCommand(commandId, { shortcuts });
+            this.renderShortcutLabel();
+        } else {
+            this._application.commandManager.registerCommand(commandId, {});
+        }
+
+        if (_.get(this, 'definition.disabled')) {
+            this.disable();
+        } else {
+            this.enable();
+        }
     }
-};
 
-MenuItem.prototype.getID = function () {
-    return _.get(this, 'definition.id');
-};
-
-MenuItem.prototype.renderShortcutLabel = function () {
-    const shortcuts = _.get(this, 'definition.command.shortcuts');
-    const shortcutLabel = $('<span></span>');
-    const shortcut = this._application.isRunningOnMacOS() ? shortcuts.mac.label : shortcuts.other.label;
-    shortcutLabel.addClass(_.get(this, 'options.cssClass.shortcut'));
-    shortcutLabel.text(shortcut);
-    this._linkElement.append(shortcutLabel);
-};
-
-MenuItem.prototype.disable = function () {
-    this._listItemElement.addClass(_.get(this, 'options.cssClass.inactive'));
-    this._listItemElement.removeClass(_.get(this, 'options.cssClass.active'));
-    this._linkElement.off('click');
-};
-
-MenuItem.prototype.enable = function () {
-    this._listItemElement.addClass(_.get(this, 'options.cssClass.active'));
-    this._listItemElement.removeClass(_.get(this, 'options.cssClass.inactive'));
-    this._linkElement.off('click');
-    this._linkElement.click(() => {
-        this._application.commandManager.dispatch(this.definition.command.id);
-    });
-};
-
-MenuItem.prototype.addLabelSuffix = function (labelSuffix) {
-    if (!_.isNil(labelSuffix)) {
-        this._title.text(`${_.get(this, 'definition.label')} ${labelSuffix}`);
+    getID() {
+        return _.get(this, 'definition.id');
     }
-};
 
-MenuItem.prototype.clearLabelSuffix = function () {
-    this._title.text(_.get(this, 'definition.label'));
-};
+    renderShortcutLabel() {
+        const shortcuts = _.get(this, 'definition.command.shortcuts');
+        const shortcutLabel = $('<span></span>');
+        const shortcut = this._application.isRunningOnMacOS() ? shortcuts.mac.label : shortcuts.other.label;
+        shortcutLabel.addClass(_.get(this, 'options.cssClass.shortcut'));
+        shortcutLabel.text(shortcut);
+        this._linkElement.append(shortcutLabel);
+    }
+
+    disable() {
+        this._listItemElement.addClass(_.get(this, 'options.cssClass.inactive'));
+        this._listItemElement.removeClass(_.get(this, 'options.cssClass.active'));
+        this._linkElement.off('click');
+    }
+
+    enable() {
+        this._listItemElement.addClass(_.get(this, 'options.cssClass.active'));
+        this._listItemElement.removeClass(_.get(this, 'options.cssClass.inactive'));
+        this._linkElement.off('click');
+        this._linkElement.click(() => {
+            this._application.commandManager.dispatch(this.definition.command.id);
+        });
+    }
+
+    addLabelSuffix(labelSuffix) {
+        if (!_.isNil(labelSuffix)) {
+            this._title.text(`${_.get(this, 'definition.label')} ${labelSuffix}`);
+        }
+    }
+
+    clearLabelSuffix() {
+        this._title.text(_.get(this, 'definition.label'));
+    }
+}
 
 export default MenuItem;
