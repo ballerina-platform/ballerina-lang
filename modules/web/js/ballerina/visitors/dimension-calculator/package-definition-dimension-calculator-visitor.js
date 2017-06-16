0a78d37fdb7c1c5426050a5b05e7738962dcd0ca
Aruna Herath <arunah@wso2.com>
2017-06-16 16:01:27 +0530

    Define magic numbers as constants
    There were some numbers representing lengths of sub components of package definitions view that were hardcoded.
    Defined them as constants. Also took the dimension constants to the dimension calc visitor so the parent view
    for package definition and imports know before hand the lenghts of sub components.

--- modules/web/js/ballerina/visitors/dimension-calculator/package-definition-dimension-calculator-visitor.js
+++ modules/web/js/ballerina/visitors/dimension-calculator/package-definition-dimension-calculator-visitor.js
@@ -16,6 +16,7 @@
  * under the License.
  */
 import { packageDefinition } from '../../configs/designer-defaults';
+import { util as SizingUtils } from '../sizing-utils';
 import ASTFactory from '../../ast/ballerina-ast-factory';
 
 /**
@@ -52,6 +53,50 @@ class PackageDefinitionDimensionCalculatorVisitor {
     visit() {
     }
 
+    _getImportDeclarationExpandedViewState() {
+        return {
+            importDeclarationHeight: 30,
+            importInputHeight: 40,
+            topBarHeight: 25,
+        };
+    }
+
+    _getImportDeclarationBadgeViewState(node) {
+        const headerHeight = 35;
+        const leftPadding = 10;
+        const iconSize = 20;
+        const importNoFontSize = 13;
+        const noOfImportsLeftPadding = 12;
+        const iconLeftPadding = 12;
+        const noOfImportsBGHeight = 18;
+        const importLabelWidth = 48.37;
+        const noOfImportsTextPadding = 10;
+
+        const imports = node.children.filter(c => c.constructor.name === 'ImportDeclaration');
+        const noOfImports = imports.length;
+
+        const noOfImportsTextWidth = SizingUtils.getOnlyTextWidth(noOfImports, { fontSize: importNoFontSize });
+        const noOfImportsBGWidth = Math.max(noOfImportsTextWidth + noOfImportsTextPadding, noOfImportsBGHeight);
+
+        const badgeWidth = leftPadding + importLabelWidth + noOfImportsLeftPadding + noOfImportsTextWidth +
+                           iconLeftPadding + iconSize + leftPadding;
+
+        return {
+            headerHeight,
+            leftPadding,
+            iconSize,
+            importNoFontSize,
+            noOfImportsLeftPadding,
+            iconLeftPadding,
+            noOfImportsBGHeight,
+            importLabelWidth,
+            noOfImportsTextPadding,
+            noOfImportsTextWidth,
+            noOfImportsBGWidth,
+            badgeWidth,
+        };
+    }
+
     /**
      * visit the visitor at the end.
      *
@@ -86,6 +131,10 @@ class PackageDefinitionDimensionCalculatorVisitor {
 
         viewState.bBox.h = height;
         viewState.bBox.w = 0;
+
+        viewState.components = viewState.components || {};
+        viewState.components.importDeclaration = this._getImportDeclarationBadgeViewState(node);
+        viewState.components.importsExpanded = this._getImportDeclarationExpandedViewState(node);
     }
 }
 
