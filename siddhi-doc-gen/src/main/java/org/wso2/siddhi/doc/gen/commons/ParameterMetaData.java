2233f7a37fff74e390d6740341e57837ab906ed9
nadundesilva <nadunrds@gmail.com>
2017-06-19 13:31:56 +0530

    Add Siddhi Doc Generation Module

---
+++ siddhi-doc-gen/src/main/java/org/wso2/siddhi/doc/gen/commons/ParameterMetaData.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.siddhi.doc.gen.commons;
+
+import org.wso2.siddhi.annotation.util.DataType;
+
+/**
+ * POJO for holding extension parameter meta data
+ */
+public class ParameterMetaData {
+    private String name;
+    private DataType[] type;
+    private String description;
+    private boolean optional;
+    private boolean dynamic;
+    private String defaultValue;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DataType[] getType() {
+        return type;
+    }
+
+    public void setType(DataType[] type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isOptional() {
+        return optional;
+    }
+
+    public void setOptional(boolean optional) {
+        this.optional = optional;
+    }
+
+    public boolean isDynamic() {
+        return dynamic;
+    }
+
+    public void setDynamic(boolean dynamic) {
+        this.dynamic = dynamic;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+}
---
+++ siddhi-doc-gen/src/main/java/org/wso2/siddhi/doc/gen/core/DocumentationGenerationMojo.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.siddhi.doc.gen.core;
+
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.wso2.siddhi.doc.gen.commons.NamespaceMetaData;
+import org.wso2.siddhi.doc.gen.core.utils.Constants;
+import org.wso2.siddhi.doc.gen.core.utils.DocumentationUtils;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Mojo for generating Siddhi Documentation
+ */
+@Mojo(
+        name = "generate",
+        defaultPhase = LifecyclePhase.INSTALL,
+        requiresDependencyResolution = ResolutionScope.TEST
+)
+public class DocumentationGenerationMojo extends AbstractMojo {
+    /**
+     * The maven project object for the current module
+     */
+    @Parameter(defaultValue = "${project}", readonly = true)
+    private MavenProject mavenProject;
+
+    /**
+     * The target module for which the files will be generated
+     * Optional
+     */
+    @Parameter(property = "module.target.directory")
+    private File moduleTargetDirectory;
+
+    /**
+     * The directory in which the docs will be generated
+     * Optional
+     */
+    @Parameter(property = "doc.gen.directory")
+    private File docGenDirectory;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        String moduleTargetPath;
+        if (moduleTargetDirectory != null) {
+            moduleTargetPath = moduleTargetDirectory.getAbsolutePath();
+        } else {
+            moduleTargetPath = mavenProject.getBuild().getDirectory();
+        }
+
+        String docGenPath;
+        if (docGenDirectory != null) {
+            docGenPath = docGenDirectory.getAbsolutePath();
+        } else {
+            docGenPath = mavenProject.getParent().getBasedir() + File.separator + Constants.DOCS_DIRECTORY;
+        }
+
+        List<NamespaceMetaData> namespaceMetaDataList;
+        try {
+            namespaceMetaDataList = DocumentationUtils.getExtensionMetaData(
+                    moduleTargetPath,
+                    mavenProject.getRuntimeClasspathElements(),
+                    getLog()
+            );
+        } catch (DependencyResolutionRequiredException e) {
+            throw new MojoFailureException("Unable to resolve dependencies of the project", e);
+        }
+
+        if (namespaceMetaDataList.size() > 0) {
+            DocumentationUtils.generateDocumentation(namespaceMetaDataList, docGenPath);
+        }
+    }
+}
