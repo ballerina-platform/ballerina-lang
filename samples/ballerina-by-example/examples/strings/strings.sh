b4193cb61bc6a3bd4e17e87034c61b4d00093523
dilinisg_89 <dilinisg@gmail.com>
2017-06-23 20:42:24 +0530

    Add examples for Strings package

---
+++ samples/ballerina-by-example/examples/strings/strings.sh
@@ -0,0 +1,26 @@
+# To run the program, create a file named `strings.bal`
+# and run the command below.
+
+$ $BALLERINA_HOME/bin/ballerina strings.bal
+ToUpper: LION IN TOWN. CATCH THE LION
+ToLower: lion in town. catch the lion
+EqualsIgnoreCase: true
+SubString: Lion
+Contains: true
+IndexOf: 2
+LastIndexOf: 26
+ValueOf: 5.8
+ReplaceFirst: Tiger in Town. Catch the Lion
+Replace: Tiger in Town. Catch the Tiger
+ReplaceAll: Li0n in T0wn. Catch the Li0n
+Length: 29
+Trim: Lion in Town. Catch the Lion
+HasSuffix: true
+HasPreffix: true
+Unescape: Lion in Town. Catch the Lion
+Split: Lion
+Split: in
+Split: Town.
+Blob: Lion in Town. Catch the Lion
+
+# Next example:
