2d3b4e8c2efa795ec4794e19633fc7a04850c475
Rajith <lanka.vitharana@gmail.com>
2017-06-23 20:49:02 +0530

    Add global var by sample

---
+++ samples/ballerina-by-example/examples/global-variables/global-variables.sh
@@ -0,0 +1,6 @@
+# To run the program, put the code in `global-variables.bal` and
+# use `ballerina run main global-variables.bal`.
+$ ballerina run main global-variables.bal
+global int - 98
+global string - string value
+global int changed - 164
---
+++ samples/ballerina-by-example/examples/identifier-literals/identifier-literals.sh
@@ -0,0 +1,6 @@
+# To run the program, put the code in `identifier-literals.bal` and
+# use `ballerina run main globalVar.bal`.
+$ ballerina run main globalVar.bal
+global int - 98
+global string - string value
+global int changed - 164
