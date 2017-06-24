6439faccd5fc4de4c525078cf37d204e9f1ba3e1
irunika <irunikail@gmail.com>
2017-06-24 20:38:41 +0530

    Add ballerina-by-samples for WebSocket

---
+++ samples/ballerina-by-example/examples/websocket-basic-endpoint/websocket-basic-endpoint.sh
@@ -0,0 +1,8 @@
+# To run the program, put the code in `hello-world-server.bal`
+# and use `$BALLERINA_HOME/bin/ballerina`.
+
+$ $BALLERINA_HOME/bin/ballerina websocket-basic-endpoint.bal
+
+# Now that we can run basic Ballerina programs, let's
+# learn more about the language.
+
