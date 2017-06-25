52276afbbe52eb22285ff8d9db0a5dac5725df04
anupama-pathirage <anupama@wso2.com>
2017-06-25 18:02:53 +0530

    Add transaction sample files

---
+++ samples/ballerina-by-example/examples/transactions/transactions.sh
@@ -0,0 +1,6 @@
+# To run the program, put the code in `transactions.bal`
+# and use `$BALLERINA_HOME/bin/ballerina run main transactions.bal`.
+
+$ $BALLERINA_HOME/bin/ballerina run main transactions.bal
+Inserted count:1
+Transaction committed
