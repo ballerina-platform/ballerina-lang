7bba0d4adc68785b5a5f00b0b5d2213200731970
Hasitha Aravinda <mail.hasitha27@gmail.com>
2017-06-26 15:46:16 +0530

    Implement continue support to BVM.

--- modules/ballerina-core/src/main/java/org/ballerinalang/util/semantics/SemanticAnalyzer.java
+++ modules/ballerina-core/src/main/java/org/ballerinalang/util/semantics/SemanticAnalyzer.java
@@ -107,6 +107,7 @@ import org.ballerinalang.model.statements.AssignStmt;
 import org.ballerinalang.model.statements.BlockStmt;
 import org.ballerinalang.model.statements.BreakStmt;
 import org.ballerinalang.model.statements.CommentStmt;
+import org.ballerinalang.model.statements.ContinueStmt;
 import org.ballerinalang.model.statements.ForkJoinStmt;
 import org.ballerinalang.model.statements.FunctionInvocationStmt;
 import org.ballerinalang.model.statements.IfElseStmt;
@@ -1239,6 +1240,10 @@ public class SemanticAnalyzer implements NodeVisitor {
                 BLangExceptionHelper.throwSemanticError(stmt,
                         SemanticErrors.BREAK_STMT_NOT_ALLOWED_HERE);
             }
+            if (stmt instanceof ContinueStmt && whileStmtCount < 1) {
+                BLangExceptionHelper.throwSemanticError(stmt,
+                        SemanticErrors.CONTINUE_STMT_NOT_ALLOWED_HERE);
+            }
 
             if (stmt instanceof AbortStmt && transactionStmtCount < 1) {
                 BLangExceptionHelper.throwSemanticError(stmt,
@@ -1256,7 +1261,8 @@ public class SemanticAnalyzer implements NodeVisitor {
 //                }
             }
 
-            if (stmt instanceof BreakStmt || stmt instanceof ReplyStmt || stmt instanceof AbortStmt) {
+            if (stmt instanceof BreakStmt || stmt instanceof ContinueStmt || stmt instanceof ReplyStmt
+                    || stmt instanceof AbortStmt) {
                 checkUnreachableStmt(blockStmt.getStatements(), stmtIndex + 1);
             }
 
@@ -1341,6 +1347,11 @@ public class SemanticAnalyzer implements NodeVisitor {
 
     @Override
     public void visit(BreakStmt breakStmt) {
+
+    }
+
+    @Override
+    public void visit(ContinueStmt continueStmt) {
 
     }
 
