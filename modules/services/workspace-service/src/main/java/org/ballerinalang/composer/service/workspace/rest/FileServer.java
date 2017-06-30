445f04daf46f3152fe2415c24c717a1e79e9ead2
nipunamarcus <nipunamarcus2@gmail.com>
2017-06-30 09:44:46 +0530

    Remove unnecessary html tags from comments

--- modules/services/workspace-service/src/main/java/org/ballerinalang/composer/service/workspace/rest/FileServer.java
+++ modules/services/workspace-service/src/main/java/org/ballerinalang/composer/service/workspace/rest/FileServer.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- * <p>
+ *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -15,7 +15,6 @@
  * specific language governing permissions and limitations
  * under the License.
  **/
-
 
 package org.ballerinalang.composer.service.workspace.rest;
 
