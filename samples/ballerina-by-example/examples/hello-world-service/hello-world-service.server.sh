8a405b31cb2ba5ba76b2d54eed7a54dfc98dede2
samgnaniah <samuel@wso2.com>
2017-07-05 22:12:22 +0530

    Update hello-world-service.server.sh

--- samples/ballerina-by-example/examples/hello-world-service/hello-world-service.server.sh
+++ samples/ballerina-by-example/examples/hello-world-service/hello-world-service.server.sh
@@ -1,10 +1,10 @@
-# To start the service, put the code in "hello-world-service.bal"
-# and use "ballerina run service" command.
+# To start the service, place the code in a "hello-world-service.bal" file
+# and use the "ballerina run service" command.
 $ ballerina run service hello-world-service.bal
 ballerina: deploying service(s) in 'hello-world-service.bal'
 ballerina: started server connector http-9090
 
-# To build a service archive, we can use the
+# To build a service archive, you can use the
 # "ballerina build service" command followed by
 # one or more packages to be included into the archive.
 $ ballerina build service hello-world-service.bal
