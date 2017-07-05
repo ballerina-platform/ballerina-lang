1c7294289b174603a952bf6f440ed1ec0ad2f4d8
shan1024 <gambit1024@gmail.com>
2017-07-05 15:42:19 +0530

    Add connector reference resolve logic

--- src/main/java/org/ballerinalang/plugins/idea/psi/references/ConnectorReference.java
+++ src/main/java/org/ballerinalang/plugins/idea/psi/references/ConnectorReference.java
@@ -20,11 +20,15 @@ import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.ballerinalang.plugins.idea.completion.BallerinaCompletionUtils;
 import org.ballerinalang.plugins.idea.completion.ParenthesisInsertHandler;
 import org.ballerinalang.plugins.idea.psi.IdentifierPSINode;
+import org.ballerinalang.plugins.idea.psi.PackageNameNode;
 import org.ballerinalang.plugins.idea.psi.impl.BallerinaPsiImplUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -40,16 +44,76 @@ public class ConnectorReference extends BallerinaElementReference {
         return false;
     }
 
+    @Nullable
+    @Override
+    public PsiElement resolve() {
+        IdentifierPSINode identifier = getElement();
+        PsiElement parent = identifier.getParent();
+
+        PackageNameNode packageNameNode = PsiTreeUtil.getChildOfType(parent, PackageNameNode.class);
+        if (packageNameNode == null) {
+            return resolveLocalConnector(identifier);
+        } else {
+            return resolveConnectorInPackage(packageNameNode, identifier);
+        }
+    }
+
+    @Nullable
+    private PsiElement resolveLocalConnector(@NotNull IdentifierPSINode identifier) {
+        PsiFile containingFile = identifier.getContainingFile();
+        if (containingFile == null) {
+            return null;
+        }
+        PsiDirectory psiDirectory = containingFile.getParent();
+        if (psiDirectory == null) {
+            return null;
+        }
+        List<PsiElement> connectors = BallerinaPsiImplUtil.getAllConnectorsFromPackage(psiDirectory);
+        for (PsiElement connector : connectors) {
+            if (connector.getText().equals(identifier.getText())) {
+                return connector;
+            }
+        }
+        return null;
+    }
+
+    @Nullable
+    private PsiElement resolveConnectorInPackage(@NotNull PackageNameNode packageNameNode,
+                                                 @NotNull IdentifierPSINode identifier) {
+        PsiReference reference = packageNameNode.findReferenceAt(0);
+        if (reference == null) {
+            return null;
+        }
+
+        PsiElement resolvedElement = reference.resolve();
+        if (!(resolvedElement instanceof PsiDirectory)) {
+            return null;
+        }
+
+        PsiDirectory psiDirectory = (PsiDirectory) resolvedElement;
+        List<PsiElement> connectors = BallerinaPsiImplUtil.getAllConnectorsFromPackage(psiDirectory);
+        for (PsiElement connector : connectors) {
+            String connectorName = connector.getText();
+            if (connectorName == null || connectorName.isEmpty()) {
+                continue;
+            }
+            if (connectorName.equals(identifier.getText())) {
+                return connector;
+            }
+        }
+        return null;
+    }
+
     @NotNull
     @Override
     public Object[] getVariants() {
+        List<LookupElement> results = new LinkedList<>();
+
         IdentifierPSINode identifier = getElement();
         PsiFile containingFile = identifier.getContainingFile();
         PsiFile originalFile = containingFile.getOriginalFile();
-        List<LookupElement> results = new LinkedList<>();
 
         PsiDirectory containingPackage = originalFile.getParent();
-
         if (containingPackage != null) {
             List<PsiElement> connectors = BallerinaPsiImplUtil.getAllConnectorsFromPackage(containingPackage);
             for (PsiElement connector : connectors) {
