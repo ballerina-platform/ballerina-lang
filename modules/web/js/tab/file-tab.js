3c8c0a1f5351454233ea92983edc04be49f00db5
Kavith Lokuhewage <kaviththiranga@gmail.com>
2017-07-10 18:47:42 +0530

    Cleanup file-tab

--- modules/web/js/tab/file-tab.js
+++ modules/web/js/tab/file-tab.js
@@ -62,28 +62,7 @@ class FileTab extends Tab {
         } else {
             this.file = _.get(options, 'file');
         }
-        // if (_.has(options, 'astRoot')) {
-        //     this._astRoot = _.get(options, 'astRoot');
-        // }
-        // if (_.has(options, 'parseResponse')) {
-        //     this._parseResponse = _.get(options, 'parseResponse');
-        // }
-        // if (_.has(options, 'programPackages')) {
-        //     this._programPackages = _.get(options, 'programPackages');
-        // } else {
-        //     this._programPackages = [];
-        // }
-        // TODO convert Backend to a singleton
         this.app = options.application;
-        // this.parseBackend = new Backend({ url: this.app.config.services.parser.endpoint });
-        // this.validateBackend = new Backend({ url: this.app.config.services.validator.endpoint });
-        // this.programPackagesBackend = new Backend({ url: this.app.config.services.programPackages.endpoint });
-        // this.deserializer = BallerinaASTDeserializer;
-        // this._file.setLangserverCallbacks({
-        //     documentDidSaveNotification: (langServerOptions) => {
-        //         this.app.langseverClientController.documentDidSaveNotification(langServerOptions);
-        //     },
-        // });
         this._undoManager = new UndoManager();
     }
 
@@ -108,24 +87,6 @@ class FileTab extends Tab {
     }
 
     /**
-     * Callback function for handling response from the program package resolving backend
-     * @param {File} fileTab - same filetab object(this)
-     * @param {Object} data - response recieved from backend
-     */
-    onResponseRecieved(fileTab, data) {
-        const packages = data.packages;
-        const programPackages = fileTab.getProgramPackages();
-        _.forEach(packages, (packageNode) => {
-            const pckg = BallerinaEnvFactory.createPackage();
-            pckg.initFromJson(packageNode);
-            programPackages.push(pckg);
-        });
-
-        fileTab.getBallerinaFileEditor().getEnvironment().addPackages(programPackages);
-        fileTab.getBallerinaFileEditor().reRender();
-    }
-
-    /**
      * Renders the view of the file tab.
      *
      * @returns {void}
@@ -142,103 +103,11 @@ class FileTab extends Tab {
         // create Rect component for diagram
         const editorReactRoot = React.createElement(BallerinaFileEditor, editorProps, null);
         ReactDOM.render(editorReactRoot, this.getContentContainer());
-        // if file already has content
-        // if (!_.isNil(this._file.getContent()) && !_.isEmpty(this._file.getContent().trim())) {
-        //     if (!_.isEmpty(this._file.getContent().trim())) {
-        //         const validateResponse = this.validateBackend.parse({
-        //             name: this._file.getName(),
-        //             path: this._file.getPath(),
-        //             package: this._astRoot,
-        //             content: this._file.getContent(),
-        //         });
-        //         if (validateResponse.errors !== undefined && !_.isEmpty(validateResponse.errors)) {
-        //             this.renderBallerinaEditor(this._parseResponse, true);
-        //             return;
-        //         }
-        //     }
-        //     const parseResponse = this.parseBackend.parse({
-        //         name: this._file.getName(),
-        //         path: this._file.getPath(),
-        //         package: this._astRoot,
-        //         content: this._file.getContent(),
-        //     });
-        //     // if no errors display the design.
-        //     this.renderBallerinaEditor(parseResponse);
-
-        //     const model = this._fileEditor.getModel();
-        //     let packageName = '.';
-        //     if (!_.isNil(model.getChildrenOfType(model.getFactory().isPackageDefinition)[0])) {
-        //         packageName = model.getChildrenOfType(model.getFactory().isPackageDefinition)[0].getPackageName();
-        //     }
-        //     const content = {
-        //         fileName: this._file.getName(),
-        //         filePath: this._file.getPath(),
-        //         packageName,
-        //         content: this._file.getContent(),
-        //     };
-        //     this.programPackagesBackend.call({
-        //         method: 'POST',
-        //         content,
-        //         async: true,
-        //         callback: this.onResponseRecieved,
-        //         callbackObj: this,
-        //     });
-        // } else if (!_.isNil(this._parseResponse)) {
-        //     this.renderBallerinaEditor(this._parseResponse, false);
-        //     const updatedContent = this.getBallerinaFileEditor().generateSource();
-        //     this._file.setContent(updatedContent);
-        //     this._file.setDirty(true);
-        //     this._file.save();
-        // } else {
-        //     this.renderBallerinaEditor(undefined, false, this.createEmptyBallerinaRoot());
-        //     const updatedContent = this.getBallerinaFileEditor().generateSource();
-        //     this._file.setContent(updatedContent);
-        //     this._file.save();
-        // }
-        // // Send document open notification to the language server
-        // const docUri = this._file.isPersisted() ? this._file.getPath() : (`/temp/${this._file.id}`);
-        // const documentOptions = {
-        //     textDocument: {
-        //         documentUri: docUri,
-        //         languageId: 'ballerina',
-        //         version: 1,
-        //         text: this._file.getContent(),
-        //     },
-        // };
-        // this.app.langseverClientController.documentDidOpenNotification(documentOptions);
-        // $(this.app.config.tab_controller.tabs.tab.ballerina_editor.design_view.container).scrollTop(0);
     }
-
-    /**
-     * Rendering Ballerina editor. Either you an provide ASTRoot element or a parseResponse recieved from the backend
-     * service.
-     * @param {Object} parseResponse - response recieved from the deserilizer backend service for a particular .bal
-     * content
-     * @param {boolean} parseFailed - whether the parsing failed
-     * @param {BallerinaASTRoot} root - ASTRoot to render
-     */
-    renderBallerinaEditor(parseResponse, parseFailed, root) {
-        const ballerinaEditorOptions = _.get(this.options, 'ballerina_editor');
-        const backendEndpointsOptions = _.get(this.options, 'application.config.services');
-        const renderingContextOpts = { application: this.options.application };
-        const diagramRenderingContext = new DiagramRenderContext(renderingContextOpts);
-
-        let astRoot;
-        if (!_.isUndefined(parseResponse)) {
-            astRoot = this.deserializer.getASTModel(parseResponse);
-        } else {
-            astRoot = root;
-        }
+   
+    initHandlers() {
 
         const fileEditorEventChannel = new EventChannel();
-        const editorProps = {
-            file: this._file,
-            application: this.app,
-        };
-
-        // create Rect component for diagram
-        const editorReactRoot = React.createElement(BallerinaFileEditor, editorProps, null);
-        ReactDOM.render(editorReactRoot, this.getContentContainer());
 
         // change tab header class to match look and feel of source view
         fileEditorEventChannel.on('source-view-activated swagger-view-activated', function () {
@@ -309,15 +178,6 @@ class FileTab extends Tab {
             this.app.workspaceManager.updateSaveMenuItem();
             this.updateHeader();
         }, this);
-
-        fileEditorEventChannel.on('dispatch-command', function (id) {
-            this.app.commandManager.dispatch(id);
-        }, this);
-
-        // bind app commands to source editor commands
-        this.app.commandManager.getCommands().forEach((command) => {
-            //fileEditor.getSourceView().bindCommand(command);
-        });
     }
 
     /**
@@ -340,23 +200,6 @@ class FileTab extends Tab {
         if (this._fileEditor) {
             this._fileEditor.reRender();
         }
-    }
-
-    /**
-     * Creates an empty ballerina AST root.
-     *
-     * @returns {BallerinaASTRoot} New ast root.
-     * @memberof FileTab
-     */
-    createEmptyBallerinaRoot() {
-        const ballerinaAstRoot = BallerinaASTFactory.createBallerinaAstRoot();
-
-        // package definition
-        const packageDefinition = BallerinaASTFactory.createPackageDefinition();
-        packageDefinition.setPackageName('');
-        ballerinaAstRoot.addChild(packageDefinition);
-
-        return ballerinaAstRoot;
     }
 
     /**
@@ -386,16 +229,6 @@ class FileTab extends Tab {
      */
     getBreakPoints() {
         return DebugManager.getDebugPoints(this.file.getName());
-    }
-
-    /**
-     * Gets the program package of current file-tab.
-     *
-     * @returns {Object[]} Program packages.
-     * @memberof FileTab
-     */
-    getProgramPackages() {
-        return this._programPackages;
     }
 
     /**
