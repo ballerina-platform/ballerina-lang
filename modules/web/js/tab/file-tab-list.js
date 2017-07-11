c5abffb56d3c418491e78ba73838e9f30db85528
Kavith Lokuhewage <kaviththiranga@gmail.com>
2017-07-11 12:33:40 +0530

    Fix active tab persistence issue

--- modules/web/js/tab/file-tab-list.js
+++ modules/web/js/tab/file-tab-list.js
@@ -101,6 +101,7 @@ const FileTabList = TabList.extend(
             TabList.prototype.addTab.call(this, tab);
             // avoid re-addition of init time files
             if (tab instanceof FileTab && !_.includes(this._workingFileSet, tab.getFile().id)) {
+                tab.getFile()._saveToLocalStorage();
                 this._workingFileSet.push(tab.getFile().id);
                 this.getBrowserStorage().put('workingFileSet', this._workingFileSet);
             }
