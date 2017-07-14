af55979f1dcb19fbe7de35d7d19ada178c000220
shan1024 <gambit1024@gmail.com>
2017-07-14 15:51:30 +0530

    Add language injection support

--- src/main/java/org/ballerinalang/plugins/idea/psi/impl/BallerinaPsiImplUtil.java
+++ src/main/java/org/ballerinalang/plugins/idea/psi/impl/BallerinaPsiImplUtil.java
@@ -16,6 +16,7 @@
 
 package org.ballerinalang.plugins.idea.psi.impl;
 
+import com.intellij.lang.ASTNode;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.progress.ProgressManager;
@@ -34,6 +35,7 @@ import com.intellij.psi.PsiNamedElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.ResolveResult;
+import com.intellij.psi.impl.source.tree.LeafElement;
 import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.GlobalSearchScope;
@@ -67,6 +69,7 @@ import org.ballerinalang.plugins.idea.psi.MapStructKeyValueNode;
 import org.ballerinalang.plugins.idea.psi.NameReferenceNode;
 import org.ballerinalang.plugins.idea.psi.PackageDeclarationNode;
 import org.ballerinalang.plugins.idea.psi.PackageNameNode;
+import org.ballerinalang.plugins.idea.psi.QuotedLiteralString;
 import org.ballerinalang.plugins.idea.psi.StatementNode;
 import org.ballerinalang.plugins.idea.psi.StructDefinitionNode;
 import org.ballerinalang.plugins.idea.psi.TypeNameNode;
@@ -76,6 +79,7 @@ import org.ballerinalang.plugins.idea.psi.VariableReferenceListNode;
 import org.ballerinalang.plugins.idea.psi.VariableReferenceNode;
 import org.ballerinalang.plugins.idea.psi.WorkerDeclarationNode;
 import org.ballerinalang.plugins.idea.psi.references.NameReference;
+import org.ballerinalang.plugins.idea.util.BallerinaStringLiteralEscaper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -1505,5 +1509,25 @@ public class BallerinaPsiImplUtil {
                 WorkerDeclarationNode.class);
         results.addAll(workerDeclarationNodes);
         return results;
+    }
+
+    @NotNull
+    public static QuotedLiteralString updateText(@NotNull QuotedLiteralString quotedLiteralString,
+                                                 @NotNull String text) {
+        if (text.length() > 2) {
+            if (quotedLiteralString.getText() != null) {
+                StringBuilder outChars = new StringBuilder();
+                BallerinaStringLiteralEscaper.escapeString(text.substring(1, text.length() - 1), outChars);
+                outChars.insert(0, '"');
+                outChars.append('"');
+                text = outChars.toString();
+            }
+        }
+
+        ASTNode valueNode = quotedLiteralString.getNode();
+        assert valueNode instanceof LeafElement;
+
+        ((LeafElement) valueNode).replaceWithText(text);
+        return quotedLiteralString;
     }
 }
