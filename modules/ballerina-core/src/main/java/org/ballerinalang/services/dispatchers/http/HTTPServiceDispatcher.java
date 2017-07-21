8831f518bedc1912c8fef7b1d7ad7fad528aea8d
chamil321 <chamilmadawa8@gmail.com>
2017-07-21 15:25:16 +0530

    resolve #2928, Add "/" for empty path values

--- modules/ballerina-core/src/main/java/org/ballerinalang/services/dispatchers/http/HTTPServiceDispatcher.java
+++ modules/ballerina-core/src/main/java/org/ballerinalang/services/dispatchers/http/HTTPServiceDispatcher.java
@@ -101,17 +101,12 @@ public class HTTPServiceDispatcher implements ServiceDispatcher {
                     .getAnnotationAttachmentInfo(Constants.HTTP_PACKAGE_PATH, Constants.ANNOTATION_NAME_PATH);
             String subPathAnnotationVal;
             if (pathAnnotationInfo != null
-                    && pathAnnotationInfo.getAttributeValue(Constants.VALUE_ATTRIBUTE) != null
-                    && !pathAnnotationInfo.getAttributeValue(Constants.VALUE_ATTRIBUTE)
-                    .getStringValue().trim().isEmpty()) {
-                subPathAnnotationVal = pathAnnotationInfo.getAttributeValue(Constants.VALUE_ATTRIBUTE)
-                        .getStringValue();
-                    && pathAnnotationInfo.getAnnotationAttributeValue(Constants.VALUE_ATTRIBUTE) != null) {
-                if (pathAnnotationInfo.getAnnotationAttributeValue(Constants.VALUE_ATTRIBUTE).getStringValue()
+                    && pathAnnotationInfo.getAttributeValue(Constants.VALUE_ATTRIBUTE) != null) {
+                if (pathAnnotationInfo.getAttributeValue(Constants.VALUE_ATTRIBUTE).getStringValue()
                         .trim().isEmpty()) {
                     subPathAnnotationVal = Constants.DEFAULT_BASE_PATH;
                 } else {
-                    subPathAnnotationVal = pathAnnotationInfo.getAnnotationAttributeValue(Constants.VALUE_ATTRIBUTE)
+                    subPathAnnotationVal = pathAnnotationInfo.getAttributeValue(Constants.VALUE_ATTRIBUTE)
                             .getStringValue();
                 }
             } else {
