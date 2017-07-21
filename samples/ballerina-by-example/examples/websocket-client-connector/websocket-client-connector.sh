ba2c4bc20fe8b2736bcca2cc07980afef8e34d37
shafreenAnfar <www.call911@gmail.com>
2017-07-22 01:35:57 +0530

    Update websocket-client-connector.sh

--- samples/ballerina-by-example/examples/websocket-client-connector/websocket-client-connector.sh
+++ samples/ballerina-by-example/examples/websocket-client-connector/websocket-client-connector.sh
@@ -1,9 +1,8 @@
 # To run the program, put the code in `websocket-client-connector`
 # and use `ballerina run service websocket-client-connector` command.
 $ ballerina run service websocket-client-connector.bal
-# Now that we can run this program using any WebSocket client with "ws://localhost:9090/client-connector/ws"
 
-# To check the sample, you can use Chrome or Firefox javascript console and run the below commands <br>
+# To check the sample, use Chrome or Firefox javascript console and run the below commands <br>
 # To check the broadcast run first 3 scripts in more than 2 consoles and check.
 $ var ws = new WebSocket("ws://localhost:9090/client-connector/ws");
 $ ws.onmessage = function(frame) {console.log(frame.data)};
