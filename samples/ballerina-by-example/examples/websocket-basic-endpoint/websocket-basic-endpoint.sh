a709f1d72a4b4c4158f655bb1d28acb614313dd2
shafreenAnfar <www.call911@gmail.com>
2017-07-22 01:20:36 +0530

    Update websocket-basic-endpoint.sh

--- samples/ballerina-by-example/examples/websocket-basic-endpoint/websocket-basic-endpoint.sh
+++ samples/ballerina-by-example/examples/websocket-basic-endpoint/websocket-basic-endpoint.sh
@@ -1,9 +1,8 @@
 # To run the program, put the code in `websocket-basic-endpoint.bal`
 # and use `ballerina run service websocket-basic-endpoint.bal` command.
 $ ballerina run service websocket-basic-endpoint.bal
-# Now that we can run this program using any WebSocket client with "ws://host:port/endpoint/ws"
 
-# To check the sample, you can use Chrome or Firefox javascript console and run the below commands <br>
+# To invoke the service, use Chrome or Firefox javascript console and run the below commands <br>
 $ var ws = new WebSocket("ws://localhost:9090/endpoint/ws");
 $ ws.onmessage = function(frame) {console.log(frame.data)};
 $ ws.onclose = function(frame) {console.log(frame)};
