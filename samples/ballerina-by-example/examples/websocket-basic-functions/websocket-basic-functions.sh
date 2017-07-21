5d5197a65b195f5233e68d1915f9f3d1a6adec57
shafreenAnfar <www.call911@gmail.com>
2017-07-22 01:21:51 +0530

    Update websocket-basic-functions.sh

--- samples/ballerina-by-example/examples/websocket-basic-functions/websocket-basic-functions.sh
+++ samples/ballerina-by-example/examples/websocket-basic-functions/websocket-basic-functions.sh
@@ -1,9 +1,8 @@
 # To run the program, put the code in `websocket-basic-functions.bal`
 # and use `ballerina run service websocket-basic-functions.bal` command.
 $ ballerina run service websocket-basic-functions.bal
-# Now that we can run this program using any WebSocket client with "ws://host:port/functions/ws"
 
-# To check the sample, you can use Chrome or Firefox javascript console and run the below commands <br>
+# To check the sample,use Chrome or Firefox javascript console and run the below commands <br>
 $ var ws = new WebSocket("ws://localhost:9090/functions/ws");
 $ ws.onmessage = function(frame) {console.log(frame.data)};
 $ ws.onclose = function(frame) {console.log(frame)};
