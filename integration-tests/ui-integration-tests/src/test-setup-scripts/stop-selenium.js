ac301df2355466db2c5d91b11d495e930fc51de7
nipunamarcus <nipunamarcus2@gmail.com>
2017-07-31 12:07:52 +0530

    Pom changes for integration tests

--- integration-tests/ui-integration-tests/src/test-setup-scripts/stop-selenium.js
+++ integration-tests/ui-integration-tests/src/test-setup-scripts/stop-selenium.js
@@ -13,14 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var argv = require('yargs').argv;
 var shell = require('shelljs');
-if (argv.skipTests === "true") {
-    console.log('Skipping Tests');
-    return;
-} else {
-    console.log('Running Integration Tests');
-    shell.exec("NODE_ENV=test mocha-webpack --require ./target/js/tests/js/spec/setup.js --webpack-config webpack.config.js ./target/js/tests/js/spec/BallerinaTest.js", function(code) {
-        shell.exit(code);
-    });
-}
+
+var platform = process.platform;
+shell.exec("ps xu | grep \".*composer.*selenium.*\" | grep -v grep | awk '{ print $2 }' | xargs kill -9", function (code) {
+    shell.exit(code);
+});
--- integration-tests/ui-integration-tests/webpack.config.js
+++ integration-tests/ui-integration-tests/webpack.config.js
@@ -1,113 +1,207 @@
-var path = require('path');
-var webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UnusedFilesWebpackPlugin = require('unused-files-webpack-plugin').UnusedFilesWebpackPlugin;
 
-var config = {
+const extractThemes = new ExtractTextPlugin('./[name].css');
+const extractCSSBundle = new ExtractTextPlugin('./bundle.css');
+var exportConfig = {};
+const config = [{
     entry: {
-      bundle: './target/index.js',
-      'worker-ballerina': './target/js/ballerina/utils/ace-worker.js'
+        bundle: './target/index.js',
+        'worker-ballerina': './target/js/ballerina/utils/ace-worker.js',
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [{
-          test: /\.js$/,
-          exclude: /(node_modules|modules\/web\/lib)/,
-          use: [
+            test: /\.js$/,
+            exclude: /(node_modules|modules\/web\/lib\/scss)/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015', 'react'],
+                    },
+                },
+            ],
+        },
             {
-              loader: 'babel-loader',
-              query: {
-                  presets: ['es2015']
-              }
-            }
-          ]
-        },
-        {
-            test: /\.html$/,
-            use: [ {
-                loader: 'html-loader'
-            }]
-        },
-        {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-        },
-        {
-            test: /\.(png|jpg|svg|cur|gif)$/,
-            use: [ 'url-loader' ]
-        }
-      ]
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader',
+                }],
+            },
+            {
+                test: /\.css$/,
+                use: extractCSSBundle.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    }],
+                }),
+            },
+            {
+                test: /\.(png|jpg|svg|cur|gif)$/,
+                use: ['url-loader'],
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules|modules\/web\/lib\/scss)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            presets: ['es2015', 'react'],
+                        },
+                    },
+                ],
+            },
+        ],
     },
-    plugins: [],
+    plugins: [
+        extractCSSBundle,
+        new UnusedFilesWebpackPlugin({
+            pattern: 'js/**/*.*',
+            globOptions: {
+                ignore: 'js/tests/**/*.*',
+            },
+        }),
+    ],
     devServer: {
-      publicPath: '/dist/'
+        publicPath: '/dist/',
     },
-    node: { module: "empty", net: "empty", fs: "empty" },
+    node: {module: 'empty', net: 'empty', fs: 'empty'},
     devtool: 'source-map',
     resolve: {
-        modules: [path.resolve('./target/lib'), path.resolve('./target/js'), path.resolve('./node_modules')],
+        extensions: ['.js', '.json', '.jsx'],
+        modules: [path.resolve('./target/lib'), path.resolve('./target/js'), path.resolve('./node_modules'), path.resolve(__dirname)],
         alias: {
-            /////////////////////////
+            // ///////////////////////
             // third party modules //
-            ////////////////////////
-            svg_pan_zoom: "svg-panNZoom/jquery.svg.pan.zoom",
-            theme_wso2: "theme-wso2-2.0.0/js/theme-wso2",
-            mcustom_scroller: "malihu-custom-scrollbar-plugin",
-            respond: "respond_1.4.2/respond.min",
-            select2: "select2-4.0.3/dist/js/select2.full.min",
-            underscore: "lodash",
-            ace: "ace-builds/src-noconflict",
+            // //////////////////////
+            svg_pan_zoom: 'svg-panNZoom/jquery.svg.pan.zoom',
+            theme_wso2: 'theme-wso2-2.0.0/js/theme-wso2',
+            mcustom_scroller: 'malihu-custom-scrollbar-plugin',
+            respond: 'respond_1.4.2/respond.min',
+            select2: 'select2-4.0.3/dist/js/select2.full.min',
+            underscore: 'lodash',
+            ace: 'ace-builds/src-noconflict',
             // dagre uses an older version of lodash which conflicts
             // with the lodash version used by the composer.
             // hence dagre is added to libs
             // https://github.com/cpettitt/graphlib/issues/58
-            dagre : "dagre-0.7.4/dagre.min.js",
-            ///////////////////////
+            dagre: 'dagre-0.7.4/dagre.min.js',
+
+            // /////////////////////
             // custom modules ////
-            //////////////////////
-            log: "log/log",
-            d3utils: "utils/d3-utils",
-            diagram_core: "diagram-core/module",
-            command: "command/command",
-            event_channel: "event/channel",
-            drag_drop_manager: "drag-drop/manager",
-            main_elements: "main-elements/module",
-            processors: "processors/module",
-            file_browser: "file-browser/file-browser",
-            menu_bar: "menu-bar/menu-bar",
-            context_menu: "context-menu/context-menu",
-            tool_bar: "tool-bar/tool-bar",
-            alerts: "utils/alerts",
-            breadcrumbs: "breadcrumbs/breadcrumbs",
-            property_pane_utils: "property-pane/property-pane-utils",
-            expression_editor_utils: "expression-editor/expression-editor-utils",
+            // ////////////////////
+            log: 'log/log',
+            d3utils: 'utils/d3-utils',
+            diagram_core: 'diagram-core/module',
+            command: 'command/command',
+            event_channel: 'event/channel',
+            drag_drop_manager: 'drag-drop/manager',
+            main_elements: 'main-elements/module',
+            processors: 'processors/module',
+            file_browser: 'file-browser/file-browser',
+            menu_bar: 'menu-bar/menu-bar',
+            context_menu: 'context-menu/context-menu',
+            tool_bar: 'tool-bar/tool-bar',
+            alerts: 'utils/alerts',
+            breadcrumbs: 'breadcrumbs/breadcrumbs',
+            property_pane_utils: 'property-pane/property-pane-utils',
+            expression_editor_utils: 'expression-editor/expression-editor-utils',
             constants: 'constants/constants',
             typeMapper: 'type-mapper/type-mapper-renderer',
-            environment_content: "ballerina/env/environment-content",
-            bal_utils: "ballerina/utils",
-            bal_configs: "ballerina/configs",
-            console: "launcher/console",
-            workspace$: "workspace/module",
-            ballerina$: "ballerina/module",
-            "welcome-page$": "welcome-page/module",
-            jstree : "js-tree-v3.3.2/jstree.js"
-        }
-    }
+            environment_content: 'ballerina/env/environment-content',
+            bal_utils: 'ballerina/utils',
+            bal_configs: 'ballerina/configs',
+            console: 'launcher/console',
+            workspace$: 'workspace/module',
+            ballerina$: 'ballerina/module',
+            'welcome-page$': 'welcome-page/module',
+        },
+    },
 
-};
-
+}, {
+    entry: {
+        default: './target/scss/themes/default.scss',
+        light: './target/scss/themes/light.scss',
+        dark: './target/scss/themes/dark.scss',
+    },
+    output: {
+        filename: '[name].css',
+        path: path.resolve(__dirname, 'dist/themes/'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: extractThemes.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    }],
+                }),
+            },
+        ],
+    },
+    plugins: [
+        extractThemes,
+    ],
+    devtool: 'source-map',
+}];
+exportConfig = config;
 if (process.env.NODE_ENV === 'production') {
-  // Add UglifyJsPlugin only when we build for production.
-  // uglyfying slows down webpack build so we avoid in when in development
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    sourceMap: true
-  }));
+    config[0].plugins.push(new webpack.DefinePlugin({
+        PRODUCTION: JSON.stringify(true),
+
+        // React does some optimizations to it if NODE_ENV is set to 'production'
+        'process.env': {
+            NODE_ENV: JSON.stringify('production'),
+        },
+    }));
+
+    // Add UglifyJsPlugin only when we build for production.
+    // uglyfying slows down webpack build so we avoid in when in development
+    config[0].plugins.push(new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        mangle: {keep_fnames: true},
+    }));
+} else {
+    config[0].plugins.push(new webpack.DefinePlugin({
+        PRODUCTION: JSON.stringify(false),
+    }));
 }
 
 if (process.env.NODE_ENV === 'test') {
-  // we run tests on nodejs. So compile for nodejs
-  config.target = 'node';
+    // we run tests on nodejs. So compile for nodejs
+    config[0].target = 'node';
+    exportConfig = config[0];
 }
 
-module.exports = config;
+if (process.env.NODE_ENV === 'electron-dev' || process.env.NODE_ENV === 'electron') {
+    // we run tests on nodejs. So compile for nodejs
+    config[0].target = 'electron-renderer';
+
+    // reassign entry so it uses the entry point for the electron app
+    config[0].entry = {
+        bundle: './electron-index.js',
+    };
+}
+
+module.exports = exportConfig;
