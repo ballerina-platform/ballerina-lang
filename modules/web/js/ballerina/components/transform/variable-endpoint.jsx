b5a411a1ca8deeb6e10acb066e20c2d2927845c9
Tharik Kanaka <tharik.kanaka@gmail.com>
2017-08-01 08:38:57 +0530

    improve transform complex type view

--- modules/web/js/ballerina/components/transform/variable-endpoint.jsx
+++ modules/web/js/ballerina/components/transform/variable-endpoint.jsx
@@ -5,17 +5,20 @@ export default class VariableEndpoint extends React.Component {
     render() {
         const {variable, makeConnectPoint, type, level, id} = this.props;
         const connectPointClass = `${type}-connect-point`;
-
+        let iconType = "variable";
+        if (_.isEqual(variable.type, "struct")) {
+          iconType = "struct";
+        }
         return (
-            <div style={{paddingLeft: level*5}} className='transform-endpoint variable'>
-                <i className='transform-endpoint-icon fw fw-variable'/>
+            <div style={{paddingLeft: level*15}} className='transform-endpoint variable'>
+                <i className={`transform-endpoint-icon fw fw-${iconType}`}/>
                 <a className='variable-content'>
                     <span className='property-name'>
                         {variable.name}
                     </span>
                     <span className='property-name'>:</span>
                     <span className='property-type'>
-                        {variable.type}
+                        {type}
                     </span>
                 </a>
                 <span id={variable.id + '-button'} className={`fw-stack fw-lg btn ${connectPointClass}`}>
