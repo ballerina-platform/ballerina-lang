11fd6f0ef9180f4df63a48087a0abd286349ac6b
Kavith Lokuhewage <kaviththiranga@gmail.com>
2017-08-03 12:26:31 +0530

    Improve POC layout

--- modules/web/src/core/plugins/layout-manager/plugin.js
+++ modules/web/src/core/plugins/layout-manager/plugin.js
@@ -26,24 +26,101 @@ import { getHandlerDefinitions } from './handlers';
 /* This is the default layout of the composer third party apps can
  overise this by passing in a special config. */
 const defaultLayout = {
+    settings: {
+        constrainDragToContainer: true,
+        reorderEnabled: true,
+        selectionEnabled: false,
+        popoutWholeStack: false,
+        blockedPopoutsThrowError: true,
+        closePopoutsOnUnload: true,
+        showPopoutIcon: false,
+        showMaximiseIcon: false,
+        showCloseIcon: false,
+    },
+    dimensions: {
+        borderWidth: 5,
+        minItemHeight: 10,
+        minItemWidth: 10,
+        headerHeight: 20,
+        dragProxyWidth: 300,
+        dragProxyHeight: 200,
+    },
+    labels: {
+        close: 'close',
+        maximise: 'maximise',
+        minimise: 'minimise',
+        popout: 'open in new window',
+    },
     content: [{
-        type: 'row',
+        id: 'root',
+        type: 'column',
         content: [{
-            type: 'component',
-            componentName: 'testComponent',
-            componentState: { label: 'A' },
-        }, {
+            id: 'header-area',
             type: 'column',
+            height: 5,
             content: [{
+                header: {
+                    show: false,
+                },
+                splitters: false,
                 type: 'component',
                 componentName: 'testComponent',
-                componentState: { label: 'B' },
-            }, {
-                type: 'component',
-                componentName: 'testComponent',
-                componentState: { label: 'C' },
+                componentState: { label: 'North' },
             }],
-        }],
+        }, {
+            id: 'body',
+            type: 'row',
+            content: [{
+                header: {
+                    show: false,
+                },
+                width: 20,
+                id: 'left-panel',
+                type: 'component',
+                componentName: 'testComponent',
+                componentState: { label: 'Left' },
+            },
+            {
+                id: 'right-panel',
+                type: 'column',
+                content: [{
+                    id: 'editor-area',
+                    type: 'stack',
+                    height: 70,
+                    hasHeaders: true,
+                    content: [{
+                        title: 'file1.bal',
+                        type: 'component',
+                        componentName: 'testComponent',
+                        componentState: { label: 'File1' },
+                    },
+                    {
+                        title: 'file2.bal',
+                        type: 'component',
+                        componentName: 'testComponent',
+                        componentState: { label: 'File2' },
+                    }],
+                }, {
+                    id: 'footer-area',
+                    type: 'stack',
+                    hasHeaders: true,
+                    content: [{
+                        title: 'debug output',
+                        type: 'component',
+                        componentName: 'testComponent',
+                        componentState: { label: 'Debug output' },
+                    },
+                    {
+                        title: 'console',
+                        type: 'component',
+                        componentName: 'testComponent',
+                        componentState: { label: 'Console' },
+                    }],
+                }],
+            },
+            ],
+        },
+        ],
     }],
 };
 
