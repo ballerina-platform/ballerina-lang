abb4000e567b530d7d3703c9dda4f51bd220870f
Hasitha Aravinda <mail.hasitha27@gmail.com>
2017-08-11 18:23:55 +0530

    Fix typo

--- samples/ballerina-by-example/examples/constrained-json/constrained-json.server.sh
+++ samples/ballerina-by-example/examples/constrained-json/constrained-json.server.sh
@@ -3,10 +3,3 @@
 $ ballerina run service constrained-json.bal
 ballerina: deploying service(s) in 'constrained-json.bal'
 ballerina: started server connector http-9090
-
-# To build a service archive, we can use the
-# "ballerina build service" command followed by
-# one or more packages to be included into the archive.
-$ ballerina build service constrained-json.bal
-$ ls
-constrained-json.bsz	constrained-json.bal
