a9f279800f1e5be422580e2b776efafaaf7a207c
chanakaudaya <chanaka.leadership@gmail.com>
2017-08-18 11:58:23 +0530

    Fix issue related to import package in filter connectors

--- modules/ballerina-core/src/main/java/org/ballerinalang/model/builder/BLangModelBuilder.java
+++ modules/ballerina-core/src/main/java/org/ballerinalang/model/builder/BLangModelBuilder.java
@@ -971,8 +971,10 @@ public class BLangModelBuilder {
         //int j = 0;
         for (int j = filterNameReferenceList.size() - 1; j >= 0; j--) {
             BLangModelBuilder.NameReference filterNameReference = filterNameReferenceList.get(j);
+            this.resolvePackageFromNameReference(filterNameReference);
+            this.createSimpleVarRefExpr(location, filterNameReference.getWhiteSpaceDescriptor(), filterNameReference);
             SimpleTypeName filterTypeName = new SimpleTypeName(filterNameReference.getName(),
-                    filterNameReference.getPackageName(), null);
+                    filterNameReference.getPackageName(), filterNameReference.getPackagePath());
             List<Expression> argExpr = filterArgExprList.get(j);
             filterTypeName.setWhiteSpaceDescriptor(filterNameReference.getWhiteSpaceDescriptor());
             WhiteSpaceDescriptor wsDescriptor = (filterInitWSDescriptors.size() == 0 ?
