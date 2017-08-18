d47dac2460cefa0cf2c13aeb54336c4008ae313e
L.A. Fernando <lafernando@gmail.com>
2017-08-18 10:30:37 +0530

    Initial Source Repo Impl.

---
+++ modules/ballerina-lang/src/main/java/org/ballerinalang/repository/PackageSource.java
@@ -0,0 +1,62 @@
+/*
+*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.ballerinalang.repository;
+
+import org.ballerinalang.model.elements.PackageID;
+
+import java.util.List;
+
+/**
+ * This represents a Ballerina package source.
+ * 
+ * @since 0.94
+ */
+public interface PackageSource {
+
+    /**
+     * Returns the package identifier package source belongs to.
+     * @return the package ID
+     */
+    PackageID getPackageID();
+    
+    /**
+     * Returns the package source entry names this package holds.
+     * @return the package source entry names
+     */
+    List<String> getEntryNames();
+    
+    /**
+     * Returns a specific {@link PackageSourceEntry}.
+     * @param name the package source entry name
+     * @return the package source entry
+     */
+    PackageSourceEntry getPackageSourceEntry(String name);
+    
+    /**
+     * Returns all the package source entries this package holds.
+     * @return the package source entries iterator
+     */
+    List<PackageSourceEntry> getPackageSourceEntries();
+    
+    /**
+     * Returns the package repository this package source was loaded from.
+     * @return the package repository
+     */
+    PackageSourceRepository getPackageRepository();
+    
+}
