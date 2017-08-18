648bb87bae255371ba22a4f58df27d9e338ea173
L.A. Fernando <lafernando@gmail.com>
2017-08-18 14:02:26 +0530

    Initial TreeBuilder impl.

---
+++ modules/ballerina-lang/src/main/java/org/ballerinalang/model/TreeBuilder.java
@@ -0,0 +1,79 @@
+/*
+*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.ballerinalang.model;
+
+import org.ballerinalang.model.tree.AnnotationNode;
+import org.ballerinalang.model.tree.ConnectorNode;
+import org.ballerinalang.model.tree.FunctionNode;
+import org.ballerinalang.model.tree.IdentifierNode;
+import org.ballerinalang.model.tree.ImportPackageNode;
+import org.ballerinalang.model.tree.PackageNode;
+import org.ballerinalang.model.tree.ServiceNode;
+import org.ballerinalang.model.tree.StructNode;
+import org.ballerinalang.model.tree.VariableNode;
+import org.ballerinalang.model.tree.XMLNSDeclarationNode;
+import org.wso2.ballerinalang.compiler.tree.BLangAnnotation;
+import org.wso2.ballerinalang.compiler.tree.BLangConnector;
+import org.wso2.ballerinalang.compiler.tree.BLangFunction;
+import org.wso2.ballerinalang.compiler.tree.BLangIdentifier;
+import org.wso2.ballerinalang.compiler.tree.BLangImportPackage;
+import org.wso2.ballerinalang.compiler.tree.BLangPackage;
+import org.wso2.ballerinalang.compiler.tree.BLangService;
+import org.wso2.ballerinalang.compiler.tree.BLangStruct;
+import org.wso2.ballerinalang.compiler.tree.BLangVariable;
+import org.wso2.ballerinalang.compiler.tree.BLangXMLNS;
+
+import java.util.List;
+
+/**
+ * This contains the functionality of building the nodes in the AST.
+ * 
+ * @since 0.94
+ */
+public class TreeBuilder {
+
+    @SuppressWarnings("unchecked")
+    PackageNode createPackageNode(List<? extends IdentifierNode> nameComps, IdentifierNode version, 
+            List<? extends ImportPackageNode> imports, List<? extends XMLNSDeclarationNode> xmlnsList,
+            List<? extends VariableNode> globalVars, List<? extends ServiceNode> services,
+            List<? extends ConnectorNode> connectors, List<? extends FunctionNode> functions,
+            List<? extends StructNode> structs, List<? extends AnnotationNode> annotations) {
+        return new BLangPackage((List<BLangIdentifier>) nameComps, (BLangIdentifier) version, 
+                (List<BLangImportPackage>) imports, (List<BLangXMLNS>) xmlnsList, (List<BLangVariable>) globalVars, 
+                (List<BLangService>) services, (List<BLangConnector>) connectors, (List<BLangFunction>) functions, 
+                (List<BLangStruct>) structs, (List<BLangAnnotation>) annotations);
+    }
+    
+    IdentifierNode createIdentifierNode(String value, boolean isLiteral) {
+        return new BLangIdentifier(value, isLiteral);
+    }
+    
+    @SuppressWarnings("unchecked")
+    ImportPackageNode createImportPackageNode(List<? extends IdentifierNode> nameComps,
+            IdentifierNode version, IdentifierNode alias) {
+        return new BLangImportPackage((List<BLangIdentifier>) nameComps, 
+                (BLangIdentifier) version, (BLangIdentifier) alias);
+    }
+    
+    XMLNSDeclarationNode createXMLNSNode(IdentifierNode namespaceURI, IdentifierNode prefix) {
+        return new BLangXMLNS((BLangIdentifier) namespaceURI, (BLangIdentifier) prefix);
+    }
+    
+    
+    
+}
