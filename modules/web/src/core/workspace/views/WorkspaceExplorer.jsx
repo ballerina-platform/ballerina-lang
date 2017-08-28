7495aa467769ecde8be4dd043b1ffb37e1b3ae1d
Kavith Lokuhewage <kaviththiranga@gmail.com>
2017-08-28 15:59:51 +0530

    Tryout rc-tree for explorer

--- modules/web/src/core/workspace/views/WorkspaceExplorer.jsx
+++ modules/web/src/core/workspace/views/WorkspaceExplorer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Tree, { TreeNode } from 'rc-tree';
+import 'rc-tree/assets/index.css';
 import View from './../../view/view';
 import { VIEWS } from './../constants';
 
@@ -21,7 +23,21 @@ class WorkspaceExplorer extends View {
     render() {
         return (
             <div>
-                Explorer
+                <Tree
+                    defaultExpandAll={false}
+                    defaultExpandedKeys={['p1']}
+                >
+                    <TreeNode title="parent 1" key="p1">
+                        <TreeNode key="p10" title="leaf" />
+                        <TreeNode title="parent 1-1" key="p11">
+                            <TreeNode title="parent 2-1" key="p21">
+                                <TreeNode title="leaf" />
+                                <TreeNode title="leaf" />
+                            </TreeNode>
+                            <TreeNode key="p22" title="leaf" />
+                        </TreeNode>
+                    </TreeNode>
+                </Tree>
             </div>
         );
     }
