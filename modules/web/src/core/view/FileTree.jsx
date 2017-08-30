e1507dd6a872f9abdb9213787d882b1b8f25bf38
Kavith Lokuhewage <kaviththiranga@gmail.com>
2017-08-30 08:58:05 +0530

    Fix styles of file tree

--- modules/web/src/core/view/FileTree.jsx
+++ modules/web/src/core/view/FileTree.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
-import { Treebeard, theme as DefaultTheme } from 'react-treebeard';
+import { Treebeard, decorators as Decorators } from 'react-treebeard';
 import { getFSRoots, listFiles } from 'api-client/api-client';
 
 const decorators = {
+    Container: (props) => {
+        return (
+            <div className="file-tree-item">
+                <div className="whole-row" />
+                <Decorators.Container {...props} />
+            </div>
+        );
+    },
     Loading: (props) => {
         return (
             <div style={props.style}>
@@ -16,9 +24,8 @@ const decorators = {
         const icon = node.type !== 'file' ? 'folder' : 'document';
         const marginLeft = node.children ? '0' : '20px';
         return (
-            <div className="unseletable-content" style={style.base}>
+            <div className="unseletable-content file-tree-header" style={style.base}>
                 <div style={style.title}>
-                    <div className="whole-row" />
                     <i className={`fw fw-${icon}`} style={{ marginRight: '5px', marginLeft }} />
                     {node.text}
                 </div>
@@ -27,14 +34,85 @@ const decorators = {
     },
 };
 
-const theme = DefaultTheme;
-const color = '#dedede';
+const color = '#c7c7c7';
 const highlightColor = '#232323';
-_.set(theme, 'tree.base.backgroundColor', 'transparent');
-_.set(theme, 'tree.node.toggle.arrow.fill', color);
-_.set(theme, 'tree.node.activeLink.background', highlightColor);
-_.set(theme, 'tree.base.color', color);
-_.set(theme, 'tree.base.color', color);
+const theme = {
+    tree: {
+        base: {
+            listStyle: 'none',
+            backgroundColor: 'transparent',
+            margin: 0,
+            padding: 0,
+            color,
+            fontFamily: 'lucida grande ,tahoma,verdana,arial,sans-serif',
+            fontSize: '14px',
+        },
+        node: {
+            base: {
+                position: 'relative',
+            },
+            link: {
+                cursor: 'pointer',
+                position: 'relative',
+                padding: '0px 5px',
+                display: 'block',
+            },
+            activeLink: {
+                background: highlightColor,
+            },
+            toggle: {
+                base: {
+                    position: 'relative',
+                    display: 'inline-block',
+                    verticalAlign: 'top',
+                    marginLeft: '-5px',
+                    height: '24px',
+                    width: '24px',
+                },
+                wrapper: {
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    margin: '-7px 0 0 -7px',
+                    height: '14px',
+                },
+                height: 14,
+                width: 14,
+                arrow: {
+                    fill: color,
+                    strokeWidth: 0,
+                },
+            },
+            header: {
+                base: {
+                    display: 'inline-block',
+                    verticalAlign: 'top',
+                    color,
+                },
+                connector: {
+                    width: '2px',
+                    height: '12px',
+                    borderLeft: 'solid 2px black',
+                    borderBottom: 'solid 2px black',
+                    position: 'absolute',
+                    top: '0px',
+                    left: '-21px',
+                },
+                title: {
+                    lineHeight: '24px',
+                    verticalAlign: 'middle',
+                },
+            },
+            subtree: {
+                listStyle: 'none',
+                paddingLeft: '19px',
+            },
+            loading: {
+                color,
+            },
+        },
+    },
+};
 
 /**
  * File Tree
@@ -113,6 +191,7 @@ class FileTree extends React.Component {
         return (
             <div className="file-tree">
                 <Treebeard
+                    style={theme}
                     data={this.state.data}
                     onToggle={this.onToggle}
                 />
