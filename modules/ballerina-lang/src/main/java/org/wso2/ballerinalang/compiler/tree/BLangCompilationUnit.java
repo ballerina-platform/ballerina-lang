f2f5aedd079ef6ca750d43c1e94b4af1200a7c1b
L.A. Fernando <lafernando@gmail.com>
2017-09-02 10:05:41 +0530

    Adding missing files

---
+++ modules/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/tree/BLangCompilationUnit.java
@@ -0,0 +1,59 @@
+/*
+*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.wso2.ballerinalang.compiler.tree;
+
+import org.ballerinalang.model.tree.CompilationUnitNode;
+import org.ballerinalang.model.tree.TopLevelNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @since 0.94
+ */
+public class BLangCompilationUnit implements CompilationUnitNode {
+
+    public String name;
+    
+    public List<TopLevelNode> topLevelNodes;
+    
+    public BLangCompilationUnit() {
+        this.topLevelNodes = new ArrayList<>();
+    }
+    
+    @Override
+    public void addTopLevelNode(TopLevelNode node) {
+        this.topLevelNodes.add(node);
+    }
+
+    @Override
+    public List<TopLevelNode> getTopLevelNodes() {
+        return topLevelNodes;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
