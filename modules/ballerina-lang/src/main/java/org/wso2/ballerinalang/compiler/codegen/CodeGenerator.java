8111d33a99106e5ccd4967329ed7ea748b52ec09
Sameera <sameera.madushan@gmail.com>
2017-09-06 22:20:48 +0530

    Add code gen class skeleton

---
+++ modules/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/codegen/CodeGenerator.java
@@ -0,0 +1,45 @@
+/*
+*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.wso2.ballerinalang.compiler.codegen;
+
+import org.wso2.ballerinalang.compiler.tree.BLangNodeVisitor;
+import org.wso2.ballerinalang.compiler.util.CompilerContext;
+
+/**
+ * @since 0.94
+ */
+public class CodeGenerator extends BLangNodeVisitor {
+
+    private static final CompilerContext.Key<CodeGenerator> CODE_GENERATOR_KEY =
+            new CompilerContext.Key<>();
+
+    public static CodeGenerator getInstance(CompilerContext context) {
+        CodeGenerator codeGenerator = context.get(CODE_GENERATOR_KEY);
+        if (codeGenerator == null) {
+            codeGenerator = new CodeGenerator(context);
+        }
+
+        return codeGenerator;
+    }
+
+    public CodeGenerator(CompilerContext context) {
+        context.put(CODE_GENERATOR_KEY, this);
+    }
+
+    
+}
