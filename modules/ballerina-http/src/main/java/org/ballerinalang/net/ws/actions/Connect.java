e590e77d676fe219ff164bdaf2cfa995b25e8da5
irunika <irunikail@gmail.com>
2017-09-18 15:02:25 +0530

    Add ConnectorFuture for connect methods

--- modules/ballerina-http/src/main/java/org/ballerinalang/net/ws/actions/Connect.java
+++ modules/ballerina-http/src/main/java/org/ballerinalang/net/ws/actions/Connect.java
@@ -19,19 +19,20 @@
 package org.ballerinalang.net.ws.actions;
 
 import org.ballerinalang.bre.Context;
+import org.ballerinalang.connector.api.BallerinaConnectorException;
+import org.ballerinalang.connector.api.ConnectorFuture;
 import org.ballerinalang.model.types.TypeEnum;
 import org.ballerinalang.model.values.BConnector;
 import org.ballerinalang.model.values.BMap;
 import org.ballerinalang.model.values.BRefType;
 import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BStruct;
-import org.ballerinalang.model.values.BValue;
+import org.ballerinalang.nativeimpl.actions.ClientConnectorFuture;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaAction;
 import org.ballerinalang.natives.annotations.ReturnType;
 import org.ballerinalang.net.ws.BallerinaWsServerConnectorListener;
 import org.ballerinalang.net.ws.Constants;
-import org.ballerinalang.util.exceptions.BallerinaException;
 import org.wso2.carbon.transport.http.netty.contract.HttpWsConnectorFactory;
 import org.wso2.carbon.transport.http.netty.contract.websocket.HandshakeFuture;
 import org.wso2.carbon.transport.http.netty.contract.websocket.HandshakeListener;
@@ -39,8 +40,6 @@ import org.wso2.carbon.transport.http.netty.contract.websocket.WebSocketClientCo
 import org.wso2.carbon.transport.http.netty.contract.websocket.WsClientConnectorConfig;
 import org.wso2.carbon.transport.http.netty.contractimpl.HttpWsConnectorFactoryImpl;
 
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
 import javax.websocket.Session;
 
 /**
@@ -63,7 +62,7 @@ import javax.websocket.Session;
 public class Connect extends AbstractNativeWsAction {
 
     @Override
-    public BValue execute(Context context) {
+    public ConnectorFuture execute(Context context) {
         BConnector bconnector = (BConnector) getRefArgument(context, 0);
         BStruct clientConfig = (BStruct) getRefArgument(context, 1);
 
@@ -85,34 +84,27 @@ public class Connect extends AbstractNativeWsAction {
             clientConnectorConfig.setIdleTimeoutInMillis(idleTimeoutInSeconds * 1000);
         }
 
-        try {
-            BlockingDeque<BStruct> structBlockingDeque = new LinkedBlockingDeque<>();
-            HttpWsConnectorFactory connectorFactory = new HttpWsConnectorFactoryImpl();
-            WebSocketClientConnector clientConnector =
-                    connectorFactory.createWsClientConnector(clientConnectorConfig);
-            HandshakeFuture handshakeFuture = clientConnector.connect(new BallerinaWsServerConnectorListener());
-            handshakeFuture.setHandshakeListener(new HandshakeListener() {
-                @Override
-                public void onSuccess(Session session) {
-                    BStruct wsConnection = createWSConnectionStruct(context, session, wsParentConnectionID);
-                    context.getControlStackNew().currentFrame.returnValues[0] = wsConnection;
-                    storeWsConnection(session.getId(), wsConnection);
-                    try {
-                        structBlockingDeque.put(wsConnection);
-                    } catch (InterruptedException e) {
-                        throw new BallerinaException("Error while connecting to the server: " + e.getMessage());
-                    }
-                }
+        ClientConnectorFuture connectorFuture = new ClientConnectorFuture();
+        HttpWsConnectorFactory connectorFactory = new HttpWsConnectorFactoryImpl();
+        WebSocketClientConnector clientConnector =
+                connectorFactory.createWsClientConnector(clientConnectorConfig);
+        HandshakeFuture handshakeFuture = clientConnector.connect(new BallerinaWsServerConnectorListener());
+        handshakeFuture.setHandshakeListener(new HandshakeListener() {
+            @Override
+            public void onSuccess(Session session) {
+                BStruct wsConnection = createWSConnectionStruct(context, session, wsParentConnectionID);
+                context.getControlStackNew().currentFrame.returnValues[0] = wsConnection;
+                storeWsConnection(session.getId(), wsConnection);
+                connectorFuture.notifyReply(wsConnection);
+            }
 
-                @Override
-                public void onError(Throwable throwable) {
-                    throw new BallerinaException("Error while connecting to the server: " + throwable.getMessage());
-                }
-            });
-            return structBlockingDeque.take();
-        } catch (InterruptedException e) {
-            // TODO: should return error instead of null.
-            throw new BallerinaException("Error while connecting to the server: " + e.getMessage());
-        }
+            @Override
+            public void onError(Throwable t) {
+                // TODO: This is working since this runs in a single thread. Has to change after changes are done.
+                throw new BallerinaConnectorException(t.getMessage());
+//                connectorFuture.notifyFailure(ex);
+            }
+        });
+        return connectorFuture;
     }
 }
--- modules/ballerina-http/src/main/java/org/ballerinalang/net/ws/actions/ConnectWithDefault.java
+++ modules/ballerina-http/src/main/java/org/ballerinalang/net/ws/actions/ConnectWithDefault.java
@@ -19,16 +19,17 @@
 package org.ballerinalang.net.ws.actions;
 
 import org.ballerinalang.bre.Context;
+import org.ballerinalang.connector.api.BallerinaConnectorException;
+import org.ballerinalang.connector.api.ConnectorFuture;
 import org.ballerinalang.model.types.TypeEnum;
 import org.ballerinalang.model.values.BConnector;
 import org.ballerinalang.model.values.BStruct;
-import org.ballerinalang.model.values.BValue;
+import org.ballerinalang.nativeimpl.actions.ClientConnectorFuture;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaAction;
 import org.ballerinalang.natives.annotations.ReturnType;
 import org.ballerinalang.net.ws.BallerinaWsServerConnectorListener;
 import org.ballerinalang.net.ws.Constants;
-import org.ballerinalang.util.exceptions.BallerinaException;
 import org.wso2.carbon.transport.http.netty.contract.HttpWsConnectorFactory;
 import org.wso2.carbon.transport.http.netty.contract.websocket.HandshakeFuture;
 import org.wso2.carbon.transport.http.netty.contract.websocket.HandshakeListener;
@@ -36,8 +37,6 @@ import org.wso2.carbon.transport.http.netty.contract.websocket.WebSocketClientCo
 import org.wso2.carbon.transport.http.netty.contract.websocket.WsClientConnectorConfig;
 import org.wso2.carbon.transport.http.netty.contractimpl.HttpWsConnectorFactoryImpl;
 
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
 import javax.websocket.Session;
 
 /**
@@ -57,40 +56,33 @@ import javax.websocket.Session;
 )
 public class ConnectWithDefault extends AbstractNativeWsAction {
     @Override
-    public BValue execute(Context context) {
+    public ConnectorFuture execute(Context context) {
         BConnector bconnector = (BConnector) getRefArgument(context, 0);
         WsClientConnectorConfig clientConnectorConfig =
                 new WsClientConnectorConfig(getUrlFromConnector(bconnector));
         clientConnectorConfig.setTarget(getClientServiceFromConnector(bconnector));
-
         HttpWsConnectorFactory connectorFactory = new HttpWsConnectorFactoryImpl();
-        try {
-            BlockingDeque<BStruct> bStructBlockingDeque = new LinkedBlockingDeque<>();
-            WebSocketClientConnector clientConnector =
-                    connectorFactory.createWsClientConnector(clientConnectorConfig);
-            HandshakeFuture handshakeFuture = clientConnector.connect(new BallerinaWsServerConnectorListener());
-            handshakeFuture.setHandshakeListener(new HandshakeListener() {
-                @Override
-                public void onSuccess(Session session) {
-                    BStruct wsConnection = createWSConnectionStruct(context, session, null);
-                    context.getControlStackNew().currentFrame.returnValues[0] = wsConnection;
-                    storeWsConnection(session.getId(), wsConnection);
-                    try {
-                        bStructBlockingDeque.put(wsConnection);
-                    } catch (InterruptedException e) {
-                        throw new BallerinaException("Error while connecting to the server: " + e.getMessage());
-                    }
-                }
 
-                @Override
-                public void onError(Throwable throwable) {
-                    throw new BallerinaException("Error while connecting to the server: " + throwable.getMessage());
-                }
-            });
-            return bStructBlockingDeque.take();
-        } catch (InterruptedException e) {
-            // TODO: should return error instead of null.
-            throw new BallerinaException("Error while connecting to the server: " + e.getMessage());
-        }
+        ClientConnectorFuture connectorFuture = new ClientConnectorFuture();
+        WebSocketClientConnector clientConnector =
+                connectorFactory.createWsClientConnector(clientConnectorConfig);
+        HandshakeFuture handshakeFuture = clientConnector.connect(new BallerinaWsServerConnectorListener());
+        handshakeFuture.setHandshakeListener(new HandshakeListener() {
+            @Override
+            public void onSuccess(Session session) {
+                BStruct wsConnection = createWSConnectionStruct(context, session, null);
+                context.getControlStackNew().currentFrame.returnValues[0] = wsConnection;
+                storeWsConnection(session.getId(), wsConnection);
+                connectorFuture.notifyReply(wsConnection);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // TODO: This is working since this runs in a single thread. Has to change after changes are done.
+                throw new BallerinaConnectorException(t.getMessage());
+//                connectorFuture.notifyFailure(ex);
+            }
+        });
+        return connectorFuture;
     }
 }
