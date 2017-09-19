e242c3a42e1f9b50564b0c83a7a20c10c9d3bd5a
erandacr <erandacr@gmail.com>
2017-09-19 13:51:59 +0530

    Migrate JMS connector to the new connector API design

--- modules/ballerina-jms/src/main/java/org/ballerinalang/net/jms/actions/utils/Constants.java
+++ modules/ballerina-jms/src/main/java/org/ballerinalang/net/jms/actions/utils/Constants.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.ballerinalang.nativeimpl.actions.jms.utils;
+package org.ballerinalang.net.jms.actions.utils;
 
 /**
  * JMS Constants required for ballerina JMS actions.
---
+++ modules/ballerina-jms/src/main/java/org/ballerinalang/net/jms/nativeimpl/message/GetContent.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ballerinalang.net.jms.nativeimpl.message;
+
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.model.types.TypeEnum;
+import org.ballerinalang.model.values.BString;
+import org.ballerinalang.model.values.BStruct;
+import org.ballerinalang.model.values.BValue;
+import org.ballerinalang.natives.AbstractNativeFunction;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.Attribute;
+import org.ballerinalang.natives.annotations.BallerinaAnnotation;
+import org.ballerinalang.natives.annotations.BallerinaFunction;
+import org.ballerinalang.natives.annotations.ReturnType;
+import org.ballerinalang.net.jms.Constants;
+import org.ballerinalang.net.jms.JMSUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.messaging.CarbonMessage;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+/**
+ * Get content of the JMS Message.
+ */
+@BallerinaFunction(
+        packageName = "ballerina.net.jms.jmsmessage",
+        functionName = "getContent",
+        args = {@Argument(name = "msg", type = TypeEnum.STRUCT, structType = "JMSMessage",
+                          structPackage = "ballerina.net.jms")},
+        returnType = {@ReturnType(type = TypeEnum.STRING)},
+        isPublic = true
+)
+@BallerinaAnnotation(annotationName = "Description", attributes = {@Attribute(name = "value",
+        value = "Get content of the message") })
+@BallerinaAnnotation(annotationName = "Param", attributes = {@Attribute(name = "message",
+        value = "The JMS message") })
+@BallerinaAnnotation(annotationName = "Return", attributes = {@Attribute(name = "string",
+        value = "The message content") })
+public class GetContent extends AbstractNativeFunction {
+
+    private static final Logger log = LoggerFactory.getLogger(GetContent.class);
+
+    public BValue[] execute(Context context) {
+
+        BStruct messageStruct  = ((BStruct) this.getRefArgument(context, 0));
+
+//        CarbonMessage carbonMessage = (CarbonMessage) messageStruct.getNativeData(Constants.TRANSPORT_MESSAGE);
+//        Message jmsMessage = (Message) carbonMessage.getProperty(Constants.JMS_API_MESSAGE);
+        Message jmsMessage = JMSUtils.getJMSMessage(messageStruct);
+
+        String messageContent = null;
+
+        try {
+            if (jmsMessage instanceof TextMessage) {
+                messageContent = ((TextMessage) jmsMessage).getText();
+            } else {
+                log.error("Unsupported JMS Message type. ");
+            }
+        } catch (JMSException e) {
+            log.error("Error when retrieving JMS message content :" + e.getLocalizedMessage());
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("Get content from the JMS message");
+        }
+
+        return this.getBValues(new BString(messageContent));
+    }
+}
