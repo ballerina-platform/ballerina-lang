343fcf7255bd77280a44fe35a4632231fa1216c3
Kavith Lokuhewage <kaviththiranga@gmail.com>
2017-09-20 11:44:21 +0530

    Refactor WS service & fix hasChildren to use ext filter

--- modules/services/workspace-service/src/main/java/org/ballerinalang/composer/service/workspace/local/LocalFSWorkspace.java
+++ modules/services/workspace-service/src/main/java/org/ballerinalang/composer/service/workspace/local/LocalFSWorkspace.java
@@ -18,7 +18,6 @@ package org.ballerinalang.composer.service.workspace.local;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOCase;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.lang3.SystemUtils;
 import org.ballerinalang.composer.service.workspace.Workspace;
@@ -35,10 +34,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.DosFileAttributes;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+
+import static org.apache.commons.io.IOCase.INSENSITIVE;
 
 /**
  * Workspace implementation for local file system.
@@ -47,7 +47,6 @@ public class LocalFSWorkspace implements Workspace {
 
     private static final Logger logger = LoggerFactory.getLogger(LocalFSWorkspace.class);
     private static final String FOLDER_TYPE = "folder";
-    private static final String ROOT_TYPE = "root";
     private static final String FILE_TYPE = "file";
     private static final String NODE_ID = "id";
     private static final String NODE_TYPE = "type";
@@ -59,60 +58,29 @@ public class LocalFSWorkspace implements Workspace {
     private static final String FILE_PATH = "filePath";
     private static final String FILE_FULL_PATH = "fileFullPath";
     private static final String EXTENSION = "extension";
-    private static final String BAL_EXT = ".bal";
+    private static final String EXISTS = "exists";
 
     @Override
-    public JsonArray listRoots() throws IOException {
+    public JsonArray listRoots(List<String> extensionList) throws IOException {
         final Iterable<Path> rootDirs = FileSystems.getDefault().getRootDirectories();
         List<Path> rootDirsList = new ArrayList<>();
         rootDirs.forEach(rootDirsList::add);
-        return getJsonArrayForDirs(rootDirsList);
-    }
-
-    private JsonArray getJsonArrayForDirs(List<Path> rootDirs) {
-        JsonArray rootArray = new JsonArray();
-        for (Path root : rootDirs) {
-            JsonObject rootObj = getJsonObjForFile(root, false);
-            try {
-                if (Files.isDirectory(root) && Files.list(root).count() > 0) {
-                    JsonArray children = listFilesInPath(root.toFile().getAbsolutePath(), Arrays.asList(BAL_EXT));
-                    rootObj.add(NODE_CHILDREN, children);
-                }
-            } catch (IOException e) {
-                logger.debug("Error while traversing children of " + e.toString(), e);
-                rootObj.addProperty(NODE_ERROR, e.toString());
-            }
-            if (Files.isDirectory(root)) {
-                rootArray.add(rootObj);
-            }
-        }
-        return rootArray;
+        return getJsonArrayForDirs(rootDirsList, extensionList);
     }
 
     @Override
-    public JsonArray getRoots(List<Path> rootPaths) throws IOException {
-        return getJsonArrayForDirs(rootPaths);
+    public JsonArray getJsonForRoots(List<Path> rootPaths, List<String> extensionList) throws IOException {
+        return getJsonArrayForDirs(rootPaths, extensionList);
     }
 
     @Override
-    public JsonArray listDirectoriesInPath(String path) throws IOException {
+    public JsonObject exists(String path) throws IOException {
         Path ioPath = Paths.get(path);
-        JsonArray dirs = new JsonArray();
-        Iterator<Path> iterator = Files.list(ioPath).iterator();
-        while (iterator.hasNext()) {
-            Path next = iterator.next();
-            if (Files.isDirectory(next) && !Files.isHidden(next)) {
-                JsonObject jsnObj = getJsonObjForFile(next, true);
-                dirs.add(jsnObj);
-            }
-        }
-        return dirs;
-    }
-
-    @Override
-    public void write(String path, String content) throws IOException {
-        Path ioPath = Paths.get(path);
-        Files.write(ioPath, content.getBytes(Charset.defaultCharset()));
+        JsonObject result = new JsonObject();
+        boolean exists = Files.exists(ioPath);
+        result.addProperty(FILE_TYPE, path);
+        result.addProperty(EXISTS, exists);
+        return result;
     }
 
     @Override
@@ -128,6 +96,22 @@ public class LocalFSWorkspace implements Workspace {
     }
 
     @Override
+    public void create(String path, String type) throws IOException {
+        Path ioPath = Paths.get(path);
+        if (FOLDER_TYPE.equals(type)) {
+            Files.createDirectories(ioPath);
+        } else {
+            Files.createFile(ioPath);
+        }
+    }
+
+    @Override
+    public void write(String path, String content) throws IOException {
+        Path ioPath = Paths.get(path);
+        Files.write(ioPath, content.getBytes(Charset.defaultCharset()));
+    }
+
+    @Override
     public void delete(String path, String type) throws IOException {
         Path ioPath = Paths.get(path);
         if (FOLDER_TYPE.equals(type)) {
@@ -138,14 +122,36 @@ public class LocalFSWorkspace implements Workspace {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void create(String path, String type) throws IOException {
+    public JsonArray listFilesInPath(String path, List<String> extensions) throws IOException {
         Path ioPath = Paths.get(path);
-        if (FOLDER_TYPE.equals(type)) {
-            Files.createDirectories(ioPath);
-        } else {
-            Files.createFile(ioPath);
+        JsonArray dirs = new JsonArray();
+        Iterator<Path> iterator = Files.list(ioPath).iterator();
+        while (iterator.hasNext()) {
+            Path next = iterator.next();
+            if ((Files.isDirectory(next) || Files.isRegularFile(next)) && !Files.isHidden(next) &&
+                !isWindowsSystemFile(next)) {
+                JsonObject jsnObj = getJsonObjForFile(next, extensions);
+                if (Files.isRegularFile(next)) {
+                    Path fileName = next.getFileName();
+                    SuffixFileFilter fileFilter = new SuffixFileFilter(extensions, INSENSITIVE);
+                    if (null != fileName && fileFilter.accept(next.toFile())) {
+                        jsnObj.addProperty(FILE_FULL_PATH, next.toString());
+                        jsnObj.addProperty(FILE_NAME, FilenameUtils.getBaseName(next.toString()));
+                        jsnObj.addProperty(FILE_PATH, FilenameUtils.getFullPath(next.toString()));
+                        jsnObj.addProperty(EXTENSION, FilenameUtils.getExtension(next.toString()));
+                        dirs.add(jsnObj);
+                    }
+                } else {
+                    dirs.add(jsnObj);
+                }
+
+            }
         }
+        return dirs;
     }
 
     @Override
@@ -170,10 +176,20 @@ public class LocalFSWorkspace implements Workspace {
             default:
                 frontEndLog.debug(logMessage);
         }
-
     }
 
-    private JsonObject getJsonObjForFile(Path root, boolean checkChildren) {
+    private JsonArray getJsonArrayForDirs(List<Path> rootDirs, List<String> extensionList) {
+        JsonArray rootArray = new JsonArray();
+        for (Path root : rootDirs) {
+            JsonObject rootObj = getJsonObjForFile(root, extensionList);
+            if (Files.isDirectory(root)) {
+                rootArray.add(rootObj);
+            }
+        }
+        return rootArray;
+    }
+
+    private JsonObject getJsonObjForFile(Path root, List<String> extensions) {
         JsonObject rootObj = new JsonObject();
         Path fileName = root.getFileName();
         if (fileName != null) {
@@ -182,65 +198,37 @@ public class LocalFSWorkspace implements Workspace {
             rootObj.addProperty(NODE_LABEL, root.toString());
         }
         rootObj.addProperty(NODE_ID, root.toAbsolutePath().toString());
-        if (Files.isDirectory(root) && checkChildren) {
+        if (Files.isDirectory(root)) {
             rootObj.addProperty(NODE_TYPE, FOLDER_TYPE);
             try {
-                if (Files.list(root).count() > 0) {
-                    rootObj.addProperty(NODE_CHILDREN, Boolean.TRUE);
-                } else {
-                    rootObj.addProperty(NODE_CHILDREN, Boolean.FALSE);
+                File rootFile = root.toFile();
+                boolean hasChildren = Boolean.FALSE;
+                if (rootFile != null) {
+                    // check if this folder contains sub-directories
+                    File[] subDirs = rootFile.listFiles(File::isDirectory);
+                    if (subDirs != null && subDirs.length > 0) {
+                        hasChildren = Boolean.TRUE;
+                    } else {
+                        // if there are no sub-directories, check whether this folder
+                        // contains files with given extension list
+                        String[] list = extensions != null && !extensions.isEmpty()
+                                ? rootFile.list(new SuffixFileFilter(extensions, INSENSITIVE))
+                                : rootFile.list();
+                        if (list != null && list.length > 0) {
+                            hasChildren = true;
+                        }
+                    }
                 }
-            } catch (IOException e) {
+                rootObj.addProperty(NODE_CHILDREN, hasChildren);
+            } catch (Exception e) {
                 logger.debug("Error while fetching children of " + root.toString(), e);
                 rootObj.addProperty(NODE_ERROR, e.toString());
             }
-        } else if (Files.isRegularFile(root) && checkChildren) {
+        } else if (Files.isRegularFile(root)) {
             rootObj.addProperty(NODE_TYPE, FILE_TYPE);
             rootObj.addProperty(NODE_CHILDREN, Boolean.FALSE);
         }
         return rootObj;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public JsonArray listFilesInPath(String path, List<String> extensions) throws IOException {
-        Path ioPath = Paths.get(path);
-        JsonArray dirs = new JsonArray();
-        Iterator<Path> iterator = Files.list(ioPath).iterator();
-        while (iterator.hasNext()) {
-            Path next = iterator.next();
-            if ((Files.isDirectory(next) || Files.isRegularFile(next)) && !Files.isHidden(next) &&
-                !isWindowsSystemFile(next)) {
-                JsonObject jsnObj = getJsonObjForFile(next, true);
-                if (Files.isRegularFile(next)) {
-                    Path fileName = next.getFileName();
-                    SuffixFileFilter fileFilter = new SuffixFileFilter(extensions, IOCase.INSENSITIVE);
-                    if (null != fileName && fileFilter.accept(next.toFile())) {
-                        jsnObj.addProperty(FILE_FULL_PATH, next.toString());
-                        jsnObj.addProperty(FILE_NAME, FilenameUtils.getBaseName(next.toString()));
-                        jsnObj.addProperty(FILE_PATH, FilenameUtils.getFullPath(next.toString()));
-                        jsnObj.addProperty(EXTENSION, FilenameUtils.getExtension(next.toString()));
-                        dirs.add(jsnObj);
-                    }
-                } else {
-                    dirs.add(jsnObj);
-                }
-
-            }
-        }
-        return dirs;
-    }
-
-    @Override
-    public JsonObject exists(String path) throws IOException {
-        Path ioPath = Paths.get(path);
-        JsonObject result = new JsonObject();
-        boolean exists = Files.exists(ioPath);
-        result.addProperty("file", path);
-        result.addProperty("exists", exists);
-        return result;
     }
 
     private boolean isWindowsSystemFile(Path filePath) throws IOException {
