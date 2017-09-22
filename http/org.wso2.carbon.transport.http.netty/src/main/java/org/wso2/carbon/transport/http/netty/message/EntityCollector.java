7d2a81e0de2d3e92d0fa3838052b86fea2b34e7c
shafreen <shafreen@wso2.com>
2017-09-22 11:16:35 +0530

    Add doc comments for the api

--- http/org.wso2.carbon.transport.http.netty/src/main/java/org/wso2/carbon/transport/http/netty/message/EntityCollector.java
+++ http/org.wso2.carbon.transport.http.netty/src/main/java/org/wso2/carbon/transport/http/netty/message/EntityCollector.java
@@ -28,17 +28,79 @@ import java.util.List;
  */
 public interface EntityCollector {
 
+    /**
+     * Add httpContent to the queue.
+     * @param httpContent httpContent
+     */
     void addHttpContent(HttpContent httpContent);
+
+    /**
+     * Get the first httpContent from the queue.
+     * @return HttpContent
+     */
     HttpContent getHttpContent();
+
+    /**
+     * Get the first ByteBuffer version of the HttpContent from the queue.
+     * @return ByteBuffer
+     */
     ByteBuffer getMessageBody();
-    List<ByteBuffer> getFullMessageBody();
-    boolean isEmpty();
-    int getFullMessageLength();
-    boolean isEndOfMsgAdded();
+
+    /**
+     * Add the ByteBuffer version the httpContent to the queue.
+     * @param msgBody ByteBuffer version of the httpContent.
+     */
     void addMessageBody(ByteBuffer msgBody);
+
+    /**
+     * Get the full message body
+     * @return complete message body
+     */
+    List<ByteBuffer> getFullMessageBody();
+
+    /**
+     * Check if the queue is empty.
+     * @return true or false
+     */
+    boolean isEmpty();
+
+    /**
+     * Get the full message length
+     * @return message length
+     */
+    int getFullMessageLength();
+
+    /**
+     * Check if the last message is added to the queue.
+     * @return true or false
+     */
+    boolean isEndOfMsgAdded();
+
+    /**
+     * Mark end of message.
+     */
     void markMessageEnd();
+
+    /**
+     * Set end of message
+     * @param endOfMsgAdded indicated using true or false
+     */
     void setEndOfMsgAdded(boolean endOfMsgAdded);
+
+    /**
+     * Release the allocated netty buffers
+     */
     void release();
+
+    /**
+     * Allows check if the stream is consumed or not.
+     * @return true or false
+     */
     boolean isAlreadyRead();
+
+    /**
+     * Update the state if already read.
+     * @param alreadyRead indicated using true or false
+     */
     void setAlreadyRead(boolean alreadyRead);
 }
