e591c8600556bb438083a1e519eda7164c9ae1ee
Kavith Lokuhewage <kaviththiranga@gmail.com>
2017-09-27 14:14:57 +0530

    Remove unused old modules & cleanup package.json and web aliases

--- modules/web/js/ballerina/utils/ace-mode.js
+++ modules/web/js/ballerina/utils/ace-mode.js
@@ -63,7 +63,7 @@ ace.define('ace/mode/ballerina',
             this.HighlightRules = BallerinaHighlightRules;
 
             this.createWorker = function (session) {
-                const worker = new WorkerClient(['ace/aceb', 'bal_utils', 'bal_configs'], 'ace/worker/ballerina', 'WorkerModule');
+                const worker = new WorkerClient(['ace/aceb'], 'ace/worker/ballerina', 'WorkerModule');
                 worker.attachToDocument(session.getDocument());
 
                 worker.on('lint', (results) => {
