39a1b2bf4c1eeebdde6b17902f31ee55ef6e7338
anupama-pathirage <anupama@wso2.com>
2017-09-28 14:26:53 +0530

    Fixes for binary and unary expressions

--- modules/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/codegen/CodeGenerator.java
+++ modules/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/codegen/CodeGenerator.java
@@ -21,6 +21,7 @@ import org.ballerinalang.model.Name;
 import org.ballerinalang.model.TreeBuilder;
 import org.ballerinalang.model.elements.PackageID;
 import org.ballerinalang.model.tree.NodeKind;
+import org.ballerinalang.model.tree.OperatorKind;
 import org.ballerinalang.model.tree.TopLevelNode;
 import org.ballerinalang.model.tree.expressions.AnnotationAttachmentAttributeNode;
 import org.wso2.ballerinalang.compiler.semantics.analyzer.SymbolEnter;
@@ -750,14 +751,86 @@ public class CodeGenerator extends BLangNodeVisitor {
     }
 
     public void visit(BLangBinaryExpr binaryExpr) {
+        if (OperatorKind.AND.equals(binaryExpr.opKind)) {
+            visitAndExpression(binaryExpr);
+        } else if (OperatorKind.OR.equals(binaryExpr.opKind)) {
+            visitOrExpression(binaryExpr);
+        } else {
+            genNode(binaryExpr.lhsExpr, this.env);
+            genNode(binaryExpr.rhsExpr, this.env);
+
+            int opcode = binaryExpr.opSymbol.opcode;
+            int exprIndex = getNextIndex(binaryExpr.type.tag, regIndexes);
+
+            binaryExpr.regIndex = exprIndex;
+            emit(opcode, binaryExpr.lhsExpr.regIndex, binaryExpr.rhsExpr.regIndex, exprIndex);
+        }
+    }
+
+    private void visitAndExpression(BLangBinaryExpr binaryExpr) {
+        // Generate code for the left hand side
         genNode(binaryExpr.lhsExpr, this.env);
+
+        // Last operand will be filled later.
+        Instruction lEvalInstruction = InstructionFactory.get(InstructionCodes.BR_FALSE,
+                binaryExpr.lhsExpr.regIndex, -1);
+        emit(lEvalInstruction);
+
+        // Generate code for the right hand side
         genNode(binaryExpr.rhsExpr, this.env);
 
-        int opcode = binaryExpr.opSymbol.opcode;
-        int exprIndex = getNextIndex(binaryExpr.type.tag, regIndexes);
+        // Last operand will be filled later.
+        Instruction rEvalInstruction = InstructionFactory.get(InstructionCodes.BR_FALSE,
+                binaryExpr.rhsExpr.regIndex, -1);
+        emit(rEvalInstruction);
 
-        binaryExpr.regIndex = exprIndex;
-        emit(opcode, binaryExpr.lhsExpr.regIndex, binaryExpr.rhsExpr.regIndex, exprIndex);
+        // If both l and r conditions are true, then load 'true'
+        int exprRegIndex = ++regIndexes.tBoolean;
+        binaryExpr.regIndex = exprRegIndex;
+        emit(InstructionCodes.BCONST_1, exprRegIndex);
+
+        Instruction goToIns = InstructionFactory.get(InstructionCodes.GOTO, -1);
+        emit(goToIns);
+
+        int loadFalseIP = nextIP();
+        lEvalInstruction.setOperand(1, loadFalseIP);
+        rEvalInstruction.setOperand(1, loadFalseIP);
+
+        // Load 'false' if the both conditions are false;
+        emit(InstructionCodes.BCONST_0, exprRegIndex);
+        goToIns.setOperand(0, nextIP());
+    }
+
+    private void visitOrExpression (BLangBinaryExpr binaryExpr) {
+        // Generate code for the left hand side
+        genNode(binaryExpr.lhsExpr, this.env);
+
+        // Last operand will be filled later.
+        Instruction lEvalInstruction = InstructionFactory.get(InstructionCodes.BR_TRUE,
+                binaryExpr.lhsExpr.regIndex, -1);
+        emit(lEvalInstruction);
+
+        // Generate code for the right hand side
+        genNode(binaryExpr.rhsExpr, this.env);
+
+        // Last operand will be filled later.
+        Instruction rEvalInstruction = InstructionFactory.get(InstructionCodes.BR_FALSE,
+                binaryExpr.rhsExpr.regIndex, -1);
+        emit(rEvalInstruction);
+
+        // If either l and r conditions are true, then load 'true'
+        lEvalInstruction.setOperand(1, nextIP());
+        int exprRegIndex = ++regIndexes.tBoolean;
+        binaryExpr.regIndex = exprRegIndex;
+        emit(InstructionCodes.BCONST_1, exprRegIndex);
+
+        Instruction goToIns = InstructionFactory.get(InstructionCodes.GOTO, -1);
+        emit(goToIns);
+        rEvalInstruction.setOperand(1, nextIP());
+
+        // Load 'false' if the both conditions are false;
+        emit(InstructionCodes.BCONST_0, exprRegIndex);
+        goToIns.setOperand(0, nextIP());
     }
 
     public void visit(BLangInvocation iExpr) {
@@ -885,7 +958,35 @@ public class CodeGenerator extends BLangNodeVisitor {
     }
 
     public void visit(BLangUnaryExpr unaryExpr) {
-        /* ignore */
+        genNode(unaryExpr.expr, this.env);
+
+        int opcode;
+        int exprIndex;
+
+        if (OperatorKind.TYPEOF.equals(unaryExpr.operator)) {
+            if (unaryExpr.expr.type.tag == TypeTags.ANY) {
+                exprIndex = ++regIndexes.tRef;
+                opcode = unaryExpr.opSymbol.opcode;
+                emit(opcode, unaryExpr.expr.regIndex, exprIndex);
+            } else {
+                int typeSigCPIndex = addUTF8CPEntry(currentPkgInfo, unaryExpr.expr.type.getDesc());
+                TypeRefCPEntry typeRefCPEntry = new TypeRefCPEntry(typeSigCPIndex);
+                int typeCPIndex = currentPkgInfo.addCPEntry(typeRefCPEntry);
+
+                exprIndex = ++regIndexes.tRef;
+                opcode = unaryExpr.opSymbol.opcode;
+                emit(opcode, typeCPIndex, exprIndex);
+            }
+            unaryExpr.regIndex = exprIndex;
+        } else if (OperatorKind.ADD.equals(unaryExpr.operator)) {
+            unaryExpr.regIndex = unaryExpr.expr.regIndex;
+        } else {
+            opcode = unaryExpr.opSymbol.opcode;
+            exprIndex = getNextIndex(unaryExpr.type.tag, regIndexes);
+
+            unaryExpr.regIndex = exprIndex;
+            emit(opcode, unaryExpr.expr.regIndex, exprIndex);
+        }
     }
 
     public void visit(BLangLambdaFunction bLangLambdaFunction) {
