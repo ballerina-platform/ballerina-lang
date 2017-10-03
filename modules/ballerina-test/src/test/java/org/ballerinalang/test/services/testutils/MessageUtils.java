bf9a4bab26425c0e383295449ae197a9af51e4c4
shafreen <shafreen@wso2.com>
2017-10-03 10:13:24 +0530

    Fix compilation failures

--- modules/ballerina-test/src/test/java/org/ballerinalang/test/services/testutils/MessageUtils.java
+++ modules/ballerina-test/src/test/java/org/ballerinalang/test/services/testutils/MessageUtils.java
@@ -18,6 +18,7 @@
 
 package org.ballerinalang.test.services.testutils;
 
+import io.netty.handler.codec.http.HttpHeaders;
 import org.ballerinalang.net.http.Constants;
 import org.ballerinalang.runtime.message.BallerinaMessageDataSource;
 import org.ballerinalang.runtime.message.StringDataSource;
@@ -74,8 +75,11 @@ public class MessageUtils {
         carbonMessage.setProperty(Constants.HTTP_METHOD, method.trim().toUpperCase(Locale.getDefault()));
 
         // Set Headers
+        HttpHeaders httpHeaders = carbonMessage.getHeaders();
         if (headers != null) {
-            carbonMessage.setHeaders(headers);
+            for (Header header : headers) {
+                httpHeaders.set(header.getName(), header.getValue());
+            }
         }
 
         // Set message body
