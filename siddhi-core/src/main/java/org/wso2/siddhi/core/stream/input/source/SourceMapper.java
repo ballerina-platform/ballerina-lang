52968618be4c83ce7b2e458e7a1e7d4287ce1ec4
tishan89 <dtishanpubudu@gmail.com>
2017-10-25 18:40:41 +0530

    adding null assertion

--- siddhi-core/src/main/java/org/wso2/siddhi/core/stream/input/source/SourceMapper.java
+++ siddhi-core/src/main/java/org/wso2/siddhi/core/stream/input/source/SourceMapper.java
@@ -93,7 +93,7 @@ public abstract class SourceMapper implements SourceEventListener {
     public final void onEvent(Object eventObject, String[] transportProperties) {
         try {
             if (eventObject != null) {
-                if (!allowNullInTransportProperties()) {
+                if (!allowNullInTransportProperties() && transportProperties != null) {
                     for (String property : transportProperties) {
                         if (property == null) {
                             log.error("Dropping event " + eventObject.toString() + " belonging to stream " +
