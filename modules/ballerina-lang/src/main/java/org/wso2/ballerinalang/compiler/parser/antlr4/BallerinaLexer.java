dfe913b5984b7def494e81b5d11fffcda157a4d6
Kevin Ratnasekera <djkevincr@yahoo.com>
2017-10-31 01:27:45 +0530

    Remove generated file headers

--- modules/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/antlr4/BallerinaLexer.java
+++ modules/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/antlr4/BallerinaLexer.java
@@ -1,4 +1,4 @@
-// Generated from /home/djkevincr/log_trace_works/ballerina_rewrite/ballerina/docs/grammar/BallerinaLexer.g4 by ANTLR 4.5.3
+// Generated from BallerinaLexer.g4 by ANTLR 4.5.3
 package org.wso2.ballerinalang.compiler.parser.antlr4;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
