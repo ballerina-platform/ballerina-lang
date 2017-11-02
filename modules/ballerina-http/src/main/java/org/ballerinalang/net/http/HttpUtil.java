32fe2fe003a3e99b037495c25b03e57800204c85
chamil321 <chamilmadawa8@gmail.com>
2017-11-02 15:04:03 +0530

    Add review requested changes

--- modules/ballerina-http/src/main/java/org/ballerinalang/net/http/HttpUtil.java
+++ modules/ballerina-http/src/main/java/org/ballerinalang/net/http/HttpUtil.java
@@ -505,7 +505,7 @@ public class HttpUtil {
         struct.addNativeData(TRANSPORT_MESSAGE, httpCarbonMessage);
     }
 
-    public static void flagRequestResponse(BStruct request, BStruct response) {
+    public static void addRequestResponseFlag(BStruct request, BStruct response) {
         request.addNativeData(Constants.INBOUND_REQUEST, true);
         response.addNativeData(Constants.OUTBOUND_RESPONSE, true);
     }
