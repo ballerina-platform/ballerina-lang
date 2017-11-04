9bb00100e74667e2097dd6895271e2837bc59cdd
Pubudu Fernando <pubuduf@wso2.com>
2017-11-04 14:47:22 +0530

    Change trace log enable level to TRACE

--- modules/ballerina-http/src/main/java/org/ballerinalang/net/http/HttpConnectionManager.java
+++ modules/ballerina-http/src/main/java/org/ballerinalang/net/http/HttpConnectionManager.java
@@ -274,7 +274,7 @@ public class HttpConnectionManager {
     private boolean isHTTPTraceLoggerEnabled() {
         // TODO: Take a closer look at this since looking up from the Config Registry here caused test failures
         return ((BLogManager) LogManager.getLogManager()).getPackageLogLevel(
-                org.ballerinalang.logging.util.Constants.HTTP_TRACE_LOG) == BLogLevel.DEBUG;
+                org.ballerinalang.logging.util.Constants.HTTP_TRACE_LOG) == BLogLevel.TRACE;
     }
 
     private void populateSenderConfigurationOptions(SenderConfiguration senderConfiguration, BStruct options) {
