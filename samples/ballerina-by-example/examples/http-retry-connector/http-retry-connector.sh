63aeb65732de6546de640fc869a3ad7aa0b29aa5
Rajith <lanka.vitharana@gmail.com>
2017-11-04 19:07:23 +0530

    Remove retry connector sample

--- samples/ballerina-by-example/examples/http-retry-connector/http-retry-connector.sh
+++
@@ -1,4 +0,0 @@
-$ ballerina run http-retry-connector.bal
-GET request:
-{"args":{"test":"123"},"headers":{"host":"postman-echo.com","user-agent":"ballerina/0.92.1-SNAPSHOT","x-forwarded-port":"443","x-forwarded-proto":"https"},"url":"https://postman-echo.com/get?test=123"}
-
