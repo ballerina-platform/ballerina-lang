3c79f63a6584f3bdc2220b8a0234962a196d4d5e
Nadeeshaan <nadeeshaangunasinghe@gmail.com>
2017-11-14 14:20:57 +0530

    Add missing license text

--- modules/services/workspace-service/src/test/java/org/ballerinalang/composer/service/workspace/langserver/compilationunit/CompilationUnitCompletionTest.java
+++ modules/services/workspace-service/src/test/java/org/ballerinalang/composer/service/workspace/langserver/compilationunit/CompilationUnitCompletionTest.java
@@ -15,7 +15,6 @@
 *  specific language governing permissions and limitations
 *  under the License.
 */
-
 package org.ballerinalang.composer.service.workspace.langserver.compilationunit;
 
 import com.google.gson.Gson;
--- modules/services/workspace-service/src/test/java/org/ballerinalang/composer/service/workspace/langserver/definitions/ResourceDefinitionCompletionTest.java
+++ modules/services/workspace-service/src/test/java/org/ballerinalang/composer/service/workspace/langserver/definitions/ResourceDefinitionCompletionTest.java
@@ -15,7 +15,6 @@
 *  specific language governing permissions and limitations
 *  under the License.
 */
-
 package org.ballerinalang.composer.service.workspace.langserver.definitions;
 
 import org.ballerinalang.composer.service.workspace.langserver.FileUtils;
