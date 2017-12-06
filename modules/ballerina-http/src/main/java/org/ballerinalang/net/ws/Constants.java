9b7cb2df20948887393a8a895ff3e79d5b5b3837
irunika <irunikail@gmail.com>
2017-12-06 17:42:45 +0530

    Rename constant WEBSOCKET_PACKAGE_NAME to PROTOCOL_PACKAGE_WS

--- modules/ballerina-http/src/main/java/org/ballerinalang/net/ws/Constants.java
+++ modules/ballerina-http/src/main/java/org/ballerinalang/net/ws/Constants.java
@@ -27,8 +27,6 @@ public class Constants extends org.ballerinalang.net.http.Constants {
     public static final String CONNECTOR_NAME = "WsClient";
     public static final String TO = "TO";
 
-    // TODO: use PROTOCOL_PACKAGE_WS everywhere instead of WEBSOCKET_PACKAGE_NAME
-    public static final String WEBSOCKET_PACKAGE_NAME = "ballerina.net.ws";
     public static final String PROTOCOL_PACKAGE_WS = "ballerina.net.ws";
     public static final String PROTOCOL_WEBSOCKET = "ws";
 
