7397cc01e6cb0e281891903b50c9b0034c194c17
maheeka <maheeka@wso2.com>
2017-12-08 07:21:03 +0530

    New flow chart while statement

--- modules/web/src/plugins/ballerina/configs/designer-defaults.js
+++ modules/web/src/plugins/ballerina/configs/designer-defaults.js
@@ -142,6 +142,25 @@ export const blockStatement = {
     },
 };
 
+export const flowChartBlockStatement = {
+    heading: {
+        width: 80,
+        height: 80,
+        paramPaddingX: 5,
+        paramSeparatorOffsetX: 20, // blockStatement.heading.width - 40
+        paramEndOffsetX: 6,
+    },
+    body: {
+        padding: {
+            top: 25,
+            right: 25,
+            bottom: 25,
+            left: 25,
+        },
+        height: 100,
+    },
+};
+
 export const fork = {
     padding: {
         top: statement.gutter.v,
