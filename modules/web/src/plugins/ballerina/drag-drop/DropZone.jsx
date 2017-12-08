12dad401a256223ddd8d7c3264ce35af212b4cdf
Tharik Kanaka <tharik.kanaka@gmail.com>
2017-12-08 13:35:59 +0530

    include add button to the current drop zone

--- modules/web/src/plugins/ballerina/drag-drop/DropZone.jsx
+++ modules/web/src/plugins/ballerina/drag-drop/DropZone.jsx
@@ -28,14 +28,24 @@ const BASE_TYPES = {
 };
 
 class DropZone extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          mouseHovering:false,
+          overButton: false,
+      };
+      this.onMouseOverArea = this.onMouseOverArea.bind(this);
+      this.onMouseOutArea = this.onMouseOutArea.bind(this);
+  }
+
     render() {
         const { baseComponent, className, connectDropTarget, isOver, isOverCurrent, enableCenterOverlayLine,
             isDragging, dropTarget, dropBefore, canDrop, enableDragBg, renderUponDragStart,
             ...restProps } = this.props;
         // render nothing when drag-drop isn't happening
         if (!isDragging && renderUponDragStart) {
-            return (<g />);
-        }
+            return this.drawAddButton(restProps.x, restProps.y, restProps.height, restProps.width);
+}
         const Component = baseComponent;
         let result;
         if (isDragging && enableDragBg && BASE_TYPES.RECT === baseComponent) {
@@ -71,19 +81,65 @@ class DropZone extends React.Component {
             );
         } else {
             result = (
-                <Component
-                    {...restProps}
-                    className={
-                        cn(baseComponent, className, 'drop-zone',
-                            { active: isOverCurrent },
-                            { blocked: !canDrop },
-                            { possible: isDragging && !isOverCurrent && canDrop },
-                        )
-                    }
-                />
+              <g>
+                {this.drawAddButton(restProps.x, restProps.y + (restProps.height - 25), restProps.height, restProps.width)}
+                                <Component
+                                    {...restProps}
+                                    className={
+                                        cn(baseComponent, className, 'drop-zone',
+                                            { active: isOverCurrent },
+                                            { blocked: !canDrop },
+                                            { possible: isDragging && !isOverCurrent && canDrop },
+                                        )
+                                    }
+                                />
+              </g>
+
             );
         }
         return connectDropTarget(result);
+    }
+
+    drawAddButton(x, y, height, width) {
+      return (<g>
+                  <title>Add Block</title>
+                  <rect x={x}
+                    y={y}
+                    className='add-statement-area'
+                    onMouseOver={this.onMouseOverArea}
+                    onMouseOut={this.onMouseOutArea}
+                    width={120}
+                    height={50}/>
+                  <rect
+                      x={x + (width / 2) - 10}
+                      y={y + 2.5}
+                      width='20'
+                      height='20'
+                      onMouseOver={this.onMouseOverArea}
+                      onMouseOut={this.onMouseOutArea}
+                      rx='10'
+                      ry='10'
+                      className= {this.state.overArea ? 'add-statement-button'
+                        : 'add-statement-button-hidden'}
+                  />
+                  <text
+                      x={x - 4 + (width / 2)}
+                      y={y + 12.5}
+                      width='20'
+                      height='20'
+                      onMouseOver={this.onMouseOverArea}
+                      onMouseOut={this.onMouseOutArea}
+                      className={this.state.overArea ? 'add-statement-button-label'
+                        : 'add-statement-button-label-hidden'}
+                  >+</text>
+              </g>);
+    }
+
+    onMouseOverArea() {
+        this.setState({overArea:true});
+    }
+    onMouseOutArea() {
+        this.setState({ overArea:false });
     }
 }
 
