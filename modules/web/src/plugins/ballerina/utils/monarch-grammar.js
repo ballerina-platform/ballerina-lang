1e40ab6ed7bbd12a07b71d3b7159cbcfe767424e
Kavith Lokuhewage <kaviththiranga@gmail.com>
2017-12-16 16:14:28 +0530

    Correct monaco grammar for ballerina

--- modules/web/src/plugins/ballerina/utils/monarch-grammar.js
+++ modules/web/src/plugins/ballerina/utils/monarch-grammar.js
@@ -1,98 +1,108 @@
-// Difficulty: "Easy"
-// Language definition for Java
 export default {
-    // Set defaultToken to invalid to see what you do not tokenize yet
-    // defaultToken: 'invalid',
+    defaultToken: 'invalid',
+    controlKeywords: [
+        'if', 'else', 'iterator', 'try', 'catch', 'finally', 'fork', 'join', 'all', 'some',
+        'while', 'throw', 'return', 'returns', 'break', 'timeout', 'transaction', 'aborted',
+        'abort', 'committed', 'failed', 'retries', 'next', 'bind', 'with', 'lengthof', 'typeof', 'enum',
+    ],
 
-    keywords: [
-        'hahaha', 'continue', 'for', 'new', 'switch', 'assert', 'default',
-        'goto', 'package', 'synchronized', 'boolean', 'do', 'if', 'private',
-        'this', 'break', 'double', 'implements', 'protected', 'throw', 'byte',
-        'else', 'import', 'public', 'throws', 'case', 'enum', 'instanceof', 'return',
-        'transient', 'catch', 'extends', 'int', 'short', 'try', 'char', 'final',
-        'interface', 'static', 'void', 'class', 'finally', 'long', 'strictfp',
-        'volatile', 'const', 'float', 'native', 'super', 'while', 'true', 'false',
+    otherKeywords: [
+        'import', 'version', 'public', 'attach', 'as', 'native',
+        'annotation', 'package', 'type', 'connector', 'function', 'resource', 'service', 'action',
+        'worker', 'struct', 'transformer', 'endpoint',
+        'const', 'true', 'false', 'reply', 'create', 'parameter',
     ],
 
     typeKeywords: [
-      'boolean', 'double', 'byte', 'int', 'short', 'char', 'void', 'long', 'float'
+        'boolean', 'int', 'float', 'string', 'var', 'any', 'datatable', 'blob',
+        'map', 'exception', 'json', 'xml', 'xmlns', 'error',
     ],
-  
+
     operators: [
-      '=', '>', '<', '!', '~', '?', ':',
-      '==', '<=', '>=', '!=', '&&', '||', '++', '--',
-      '+', '-', '*', '/', '&', '|', '^', '%', '<<',
-      '>>', '>>>', '+=', '-=', '*=', '/=', '&=', '|=',
-      '^=', '%=', '<<=', '>>=', '>>>='
+        '!', '%', '+', '-', '~=', '==', '=', '!=', '<', '>', '&&', '\\',
     ],
-  
+
+    brackets: [
+      ['{', '}', 'delimiter.curly'],
+      ['[', ']', 'delimiter.square'],
+      ['(', ')', 'delimiter.parenthesis'],
+      ['<', '>', 'delimiter.angle'],
+    ],
+
     // we include these common regular expressions
-    symbols:  /[=><!~?:&|+\-*\/\^%]+/,
-    escapes:  /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
-  
+    symbols: /[=><!~?:&|+\-*\/\^%]+/,
+
+    // C# style strings
+    escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
     // The main tokenizer for our languages
     tokenizer: {
-      root: [
-        // identifiers and keywords
-        [/[a-z_$][\w$]*/, { cases: { '@typeKeywords': 'keyword',
-                                     '@keywords': 'keyword',
-                                     '@default': 'identifier' } }],
-        [/[A-Z][\w\$]*/, 'type.identifier' ],  // to show class names nicely
-  
-        // whitespace
-        { include: '@whitespace' },
-  
-        // delimiters and operators
-        [/[{}()\[\]]/, '@brackets'],
-        [/[<>](?!@symbols)/, '@brackets'],
-        [/@symbols/, { cases: { '@operators': 'operator',
-                                '@default'  : '' } } ],
-  
-        // @ annotations.
-        // As an example, we emit a debugging log message on these tokens.
-        // Note: message are supressed during the first load -- change some lines to see them.
-        [/@\s*[a-zA-Z_\$][\w\$]*/, { token: 'annotation', log: 'annotation token: $0' }],
-  
-        // numbers
-        [/\d*\.\d+([eE][\-+]?\d+)?[fFdD]?/, 'number.float'],
-        [/0[xX][0-9a-fA-F_]*[0-9a-fA-F][Ll]?/, 'number.hex'],
-        [/0[0-7_]*[0-7][Ll]?/, 'number.octal'],
-        [/0[bB][0-1_]*[0-1][Ll]?/, 'number.binary'],
-        [/\d+[lL]?/, 'number'],
-  
-        // delimiter: after number because of .\d floats
-        [/[;,.]/, 'delimiter'],
-  
-        // strings
-        [/"([^"\\]|\\.)*$/, 'string.invalid' ],  // non-teminated string
-        [/"/,  'string', '@string' ],
-  
-        // characters
-        [/'[^\\']'/, 'string'],
-        [/(')(@escapes)(')/, ['string','string.escape','string']],
-        [/'/, 'string.invalid']
-      ],
-  
-      whitespace: [
-        [/[ \t\r\n]+/, 'white'],
-        [/\/\*/,       'comment', '@comment' ],
-        [/\/\/.*$/,    'comment'],
-      ],
-  
-      comment: [
-        [/[^\/*]+/, 'comment' ],
-        // [/\/\*/, 'comment', '@push' ],    // nested comment not allowed :-(
-        [/\/\*/,    'comment.invalid' ],
-        ["\\*/",    'comment', '@pop'  ],
-        [/[\/*]/,   'comment' ]
-      ],
-  
-      string: [
-        [/[^\\"]+/,  'string'],
-        [/@escapes/, 'string.escape'],
-        [/\\./,      'string.escape.invalid'],
-        [/"/,        'string', '@pop' ]
-      ],
+        root: [
+            // identifiers and keywords
+            [/[a-z_$][\w$]*/, {
+                cases: {
+                    '@controlKeywords': 'keyword.control.ballerina',
+                    '@otherKeywords': 'keyword.other.ballerina',
+                    '@typeKeywords': 'type.ballerina',
+                    '@default': 'identifier',
+                },
+            }],
+            [/[A-Z][\w\$]*/, 'type.identifier'],  // to show class names nicely
+
+            // whitespace
+            { include: '@whitespace' },
+
+            // delimiters and operators
+            [/[{}()\[\]]/, '@brackets'],
+            [/[<>](?!@symbols)/, '@brackets'],
+            [/@symbols/, {
+                cases: {
+                    '@operators': 'operator',
+                    '@default': '',
+                },
+            }],
+
+            // @ annotations.
+            // As an example, we emit a debugging log message on these tokens.
+            // Note: message are supressed during the first load -- change some lines to see them.
+            [/@\s*[a-zA-Z_\$][\w\$]*/, { token: 'annotation', log: 'annotation token: $0' }],
+
+            // numbers
+            [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+            [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+            [/\d+/, 'number'],
+
+            // delimiter: after number because of .\d floats
+            [/[;,.]/, 'delimiter'],
+
+            // strings
+            [/"([^"\\]|\\.)*$/, 'string.invalid'],  // non-teminated string
+            [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+
+            // characters
+            [/'[^\\']'/, 'string'],
+            [/(')(@escapes)(')/, ['string', 'string.escape', 'string']],
+            [/'/, 'string.invalid'],
+        ],
+
+        comment: [
+          [/[^\/*]+/, 'comment'],
+          [/\/\*/, 'comment', '@push'], // nested comment
+          ["\\*/", 'comment', '@pop'],
+          [/[\/*]/, 'comment'],
+        ],
+
+        string: [
+          [/[^\\"]+/, 'string'],
+          [/@escapes/, 'string.escape'],
+          [/\\./, 'string.escape.invalid'],
+          [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }],
+        ],
+
+        whitespace: [
+          [/[ \t\r\n]+/, 'white'],
+          [/\/\*/, 'comment', '@comment'],
+          [/\/\/.*$/, 'comment'],
+        ],
     },
-  };
-  
+};
