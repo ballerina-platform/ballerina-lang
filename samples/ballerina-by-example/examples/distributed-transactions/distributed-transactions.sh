e9495e6dbb768c27e7a473bb8d5866cec14af131
anupama-pathirage <anupama@wso2.com>
2017-12-21 12:08:00 +0530

    Fix Ballerina by Example issues

---
+++ samples/ballerina-by-example/examples/distributed-transactions/distributed-transactions.sh
@@ -0,0 +1,9 @@
+$ ballerina run distributed-transactions.bal
+CUSTOMER table create status in first DB:0
+SALARY table create status in second DB:0
+Inserted count to CUSTOMER table:1
+Generated key for the inserted row:1
+Inserted count to SALARY table:1
+Transaction committed
+CUSTOMER table drop status:0
+SALARY table drop status:0
