bdea60481abb4e6e6873bfe3707a627d6a07d9ae
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-01-02 11:38:24 +0530

    Prevent opening of program dir upon sample open
    Prevent it for samples which have a package declared.
    Composer will automatically popup a dialog asking
    user's confirmation to open the program dir in such
    cases. Hence we need to stop duplicated task of opening the
    folder here. However, there are some samples which doesn't
    have a package declared but contains multiple files and
    we need to open the relavant folder
    while opening the sample. Hence, I did not remove all the logic
    for opening folder while opening samples. Instead removed that
    feature from the samples which we know have packages and let
    the automatic wizard handles that as it does for other files.

--- modules/web/src/config.js
+++ modules/web/src/config.js
@@ -76,7 +76,7 @@ export default {
                 },
                 {
                     name: 'Passthrough Service',
-                    isFile: false,
+                    isFile: true,
                     folder: '/samples/passthroughService/passthroughservice',
                     path: '/samples/passthroughService/passthroughservice/samples/nyseStockQuoteService.bal',
                     image: 'preview_passthroughService',
@@ -90,21 +90,21 @@ export default {
                 },
                 {
                     name: 'Service Chaining',
-                    isFile: false,
+                    isFile: true,
                     folder: '/samples/serviceChaining/',
                     path: '/samples/serviceChaining/servicechaining/samples/ATMLocatorService.bal',
                     image: 'preview_servicechaining',
                 },
                 {
                     name: 'RESTful Service',
-                    isFile: false,
+                    isFile: true,
                     folder: '/samples/restfulService/restfulservice',
                     path: '/samples/restfulService/restfulservice/samples/productsService.bal',
                     image: 'preview_restfulService',
                 },
                 {
                     name: 'Routing Services',
-                    isFile: false,
+                    isFile: true,
                     folder: '/samples/routingServices',
                     path: '/samples/routingServices/routingServices/samples/contentBasedRoutingService.bal',
                     image: 'preview_routingServices',
