016df9ad12543c67d16f3f2921347485e5ec3481
Tharik Kanaka <tharik.kanaka@gmail.com>
2018-01-05 11:31:37 +0530

    refactor calculations to variables

--- modules/web/src/plugins/ballerina/interactions/button.jsx
+++ modules/web/src/plugins/ballerina/interactions/button.jsx
@@ -35,6 +35,8 @@ class Button extends React.Component {
         const btnColor = this.props.buttonColor;
         const btnIconColor = this.props.buttonIconColor;
         const IconOpacity = this.props.hideIconBackground ? 0 : 1;
+        const leftPadding = btnRadius + 2 + (btnRadius / 4);
+        const topPadding = (-3 * btnRadius) + (btnRadius / 2);
         return (
             <div
                 className='button-area'
@@ -42,7 +44,7 @@ class Button extends React.Component {
             >
                 <div
                     className='interaction-menu-area'
-                    style={{ left: btnX, top: btnY, '--button-size': (btnRadius + 2 + (btnRadius / 4)) + 'px' }}
+                    style={{ left: btnX, top: btnY, '--button-size': leftPadding + 'px' }}
                 >
                     <div style={{ fontSize: btnRadius }} className='button-panel'>
                         <span
@@ -58,8 +60,8 @@ class Button extends React.Component {
                         </span>
                     </div>
                     <div style={{
-                        left: btnRadius + 2 + (btnRadius / 4),
-                        top: (-3 * btnRadius) + (btnRadius / 2),
+                        left: leftPadding,
+                        top: topPadding,
                         display: 'block',
                         position: 'relative',
                         float: 'left' }}
