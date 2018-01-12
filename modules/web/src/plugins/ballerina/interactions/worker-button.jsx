c0997c06e52aafefa53ac3d7dd053c697b9e1e02
Tharik Kanaka <tharik.kanaka@gmail.com>
2018-01-12 16:26:44 +0530

    include adding action on search endpoint actions

--- modules/web/src/plugins/ballerina/interactions/worker-button.jsx
+++ modules/web/src/plugins/ballerina/interactions/worker-button.jsx
@@ -24,6 +24,7 @@ import Menu from './menu';
 import Item from './item';
 import DefaultNodeFactory from '../model/default-node-factory';
 import TreeUtil from '../model/tree-util';
+import ConnectorAction from '../env/connector-action';
 
 // Use your imagination to render suggestions.
 const renderSuggestion = suggestion => (
@@ -44,6 +45,7 @@ class LifelineButton extends React.Component {
             listConnectors: false,
             listActions: false,
             selectedConnecter: '',
+            selectedEndpoint: '',
             value: '',
             suggestions: [],
         };
@@ -61,6 +63,7 @@ class LifelineButton extends React.Component {
         this.onChange = this.onChange.bind(this);
         this.onSuggestionSelected = this.onSuggestionSelected.bind(this);
         this.getSuggestionValue = this.getSuggestionValue.bind(this);
+        this.createAction = this.createAction.bind(this);
     }
 
     getSuggestionValue(suggestion) {
@@ -129,8 +132,13 @@ class LifelineButton extends React.Component {
         }
     }
 
-    showConnectors(connectorName) {
-        this.setState({ listConnectors: true, listActions: false, selectedConnecter: connectorName });
+    showConnectors(connectorName, endpointName) {
+        this.setState({
+            listConnectors: true,
+            listActions: false,
+            selectedConnecter: connectorName,
+            selectedEndpoint: endpointName,
+        });
     }
 
     hideConnectors() {
@@ -146,9 +154,41 @@ class LifelineButton extends React.Component {
     }
 
     onSuggestionSelected(event, item) {
-        this.setState({ listConnectors: false, listActions: false, selectedConnecter: '' });
+        item.suggestion.endpoint = this.state.selectedEndpoint;
+        this.setState({ listConnectors: false, listActions: false, selectedConnecter: '', selectedEndpoint: '' });
         const node = DefaultNodeFactory.createConnectorActionInvocationAssignmentStatement(item.suggestion);
         this.props.model.acceptDrop(node);
+    }
+
+
+    createAction() {
+        const actionNode = DefaultNodeFactory.createConnectorAction();
+        actionNode.name.setValue(this.state.value);
+        this.props.model.getRoot().topLevelNodes.forEach((topLevelNode) => {
+            if (TreeUtil.isConnector(topLevelNode) &&
+                topLevelNode.getName().getValue() === this.state.selectedConnecter) {
+                topLevelNode.addActions(actionNode);
+            }
+        });
+        const currentPackage = this.context.editor.environment.getCurrentPackage();
+        const currentConnector = currentPackage.getConnectorByName(this.state.selectedConnecter);
+        const node = DefaultNodeFactory.createConnectorActionInvocationAssignmentStatement({
+            pkg: currentPackage,
+            action: new ConnectorAction(
+                {
+                    name: this.state.value,
+                    id: '',
+                    action: this.state.value,
+                    parameters: [],
+                    returnParams: [],
+                }),
+            connector: currentConnector,
+            endpoint: this.state.selectedEndpoint,
+            packageName: currentPackage.getName(),
+            fullPackageName: currentPackage.getName(),
+        });
+        this.props.model.acceptDrop(node);
+        this.setState({ listConnectors: false, listActions: false, selectedConnecter: '', selectedEndpoint: '' });
     }
 
     /**
@@ -201,7 +241,9 @@ class LifelineButton extends React.Component {
                                 return (<Item
                                     label={statement.getVariable().getName().getValue()}
                                     icon='fw fw-endpoint'
-                                    callback={() => this.showConnectors(statement.getVariable().getTypeNode().getConstraint().getTypeName().getValue())}
+                                    callback={() => this.showConnectors(
+                                        statement.getVariable().getTypeNode().getConstraint().getTypeName().getValue()
+                                        , statement.getVariable().getName().getValue())}
                                 />);
                             })
                             }
@@ -229,6 +271,15 @@ class LifelineButton extends React.Component {
                                     inputProps={inputProps}
                                     ref={this.storeInputReference}
                                 />
+                                {this.state.value !== '' &&
+                                <div className='add-new-connector-area'>
+                                    <a className='add-new-connector-button' onClick={this.createAction}>
+                                        <i className='fw fw-action' />
+                                        {' Create new action "'}
+                                        <b>{this.state.value + '"'}</b>
+                                    </a>
+                                </div>
+                                }
                             </div>
                         }
                     </Menu>
