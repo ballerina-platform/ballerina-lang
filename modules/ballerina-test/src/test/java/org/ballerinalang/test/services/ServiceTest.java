e61f19e8067c2a6602352640653f7f0075083440
Hasitha Aravinda <mail.hasitha27@gmail.com>
2018-01-24 11:07:20 +0530

    Fix code analyzer doesn't run in services.
    Resolves #4486

--- modules/ballerina-test/src/test/java/org/ballerinalang/test/services/ServiceTest.java
+++ modules/ballerina-test/src/test/java/org/ballerinalang/test/services/ServiceTest.java
@@ -18,6 +18,8 @@
 
 package org.ballerinalang.test.services;
 
+import org.ballerinalang.launcher.util.BAssertUtil;
+import org.ballerinalang.launcher.util.BCompileUtil;
 import org.ballerinalang.launcher.util.BServiceUtil;
 import org.ballerinalang.launcher.util.CompileResult;
 import org.ballerinalang.model.util.StringUtils;
@@ -40,11 +42,12 @@ import java.nio.ByteBuffer;
  */
 public class ServiceTest {
 
-    CompileResult compileResult;
+    CompileResult compileResult, negativeResult;
 
     @BeforeClass
     public void setup() {
         compileResult = BServiceUtil.setupProgramFile(this, "test-src/services/echo-service.bal");
+        negativeResult = BCompileUtil.compile("test-src/services/service-negative.bal");
     }
 
     @Test
@@ -243,4 +246,13 @@ public class ServiceTest {
 
     //TODO: add more test cases
 
+
+    /* Negative cases */
+    @Test(description = "verify code analyzer errors in services.")
+    public void testCheckCodeAnalyzerErrors() {
+        BAssertUtil.validateError(negativeResult, 0, "break cannot be used outside of a loop", 7, 9);
+        BAssertUtil.validateError(negativeResult, 1, "continue cannot be used outside of a loop", 13, 9);
+        BAssertUtil.validateError(negativeResult, 2, "abort cannot be used outside of a transaction block", 19, 9);
+        BAssertUtil.validateError(negativeResult, 3, "unreachable code", 26, 9);
+    }
 }
---
+++ modules/ballerina-test/src/test/resources/test-src/services/service-negative.bal
@@ -0,0 +1,28 @@
+import ballerina.net.http;
+
+service<http> FooService {
+
+    resource test1 (string dummyParam) {
+        println("test1-before");
+        break;
+        println("test1-after");
+    }
+
+    resource test2 (string dummyParam) {
+        println("test2-before");
+        next;
+        println("test2-after");
+    }
+
+    resource test3 (string dummyParam) {
+        println("test3-before");
+        abort;
+        println("test3-after");
+    }
+
+    resource test4 (string dummyParam) {
+        println("test4-before");
+        return;
+        println("test4-after");
+    }
+}
