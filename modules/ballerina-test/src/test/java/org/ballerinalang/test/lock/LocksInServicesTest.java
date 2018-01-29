cc3ffabf20c7b229f0e1c8ddd52b255e4bba45e8
Rajith <lanka.vitharana@gmail.com>
2018-01-29 20:07:44 +0530

    Add test cases for lock implementation

---
+++ modules/ballerina-test/src/test/java/org/ballerinalang/test/lock/LocksInServicesTest.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.test.lock;
+
+import org.ballerinalang.launcher.util.BServiceUtil;
+import org.ballerinalang.launcher.util.CompileResult;
+import org.ballerinalang.model.util.StringUtils;
+import org.ballerinalang.test.services.testutils.HTTPTestRequest;
+import org.ballerinalang.test.services.testutils.MessageUtils;
+import org.ballerinalang.test.services.testutils.Services;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.transport.http.netty.message.HTTPCarbonMessage;
+import org.wso2.transport.http.netty.message.HttpMessageDataStreamer;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Test class Ballerina locks in services.
+ *
+ * @since 0.96.1
+ */
+public class LocksInServicesTest {
+
+    CompileResult compileResult;
+
+    @BeforeClass()
+    public void setup() {
+        compileResult = BServiceUtil
+                .setupProgramFile(this, "test-src/lock/locks-in-services.bal");
+    }
+
+    @Test(description = "Test locking service level variable basic")
+    public void testServiceLvlVarLockBasic() {
+        Semaphore semaphore = new Semaphore(-499);
+
+        ExecutorService executor = TestThreadPool.getInstance().getExecutor();
+
+        for (int i = 0; i < 500; i++) {
+            executor.submit(new TestRequestSender(compileResult, semaphore, "/sample/echo"));
+        }
+
+        try {
+            if (!semaphore.tryAcquire(500, TimeUnit.MILLISECONDS)) {
+                Assert.fail("request execution not finished within 100ms");
+            }
+            String path = "/sample/getCount";
+            HTTPTestRequest cMsg = MessageUtils.generateHTTPMessage(path, "GET");
+            HTTPCarbonMessage response = Services.invokeNew(compileResult, cMsg);
+
+            Assert.assertNotNull(response, "Response message not found");
+            String responseMsgPayload = StringUtils.getStringFromInputStream(new HttpMessageDataStreamer(response)
+                    .getInputStream());
+            Assert.assertEquals(responseMsgPayload, "count - 500", "incorrect request count");
+        } catch (InterruptedException e) {
+            Assert.fail("thread interrupted before request execution finished - " + e.getMessage(), e);
+        }
+    }
+
+
+    @Test(description = "Test locking service level variable complex")
+    public void testServiceLvlVarLockComplex() {
+        Semaphore semaphore = new Semaphore(-29);
+
+        ExecutorService executor = TestThreadPool.getInstance().getExecutor();
+
+        for (int i = 0; i < 10; i++) {
+            executor.submit(new TestRequestSender(compileResult, semaphore, "/sample1/echo"));
+            executor.submit(new TestRequestSender(compileResult, semaphore, "/sample1/echo1"));
+            executor.submit(new TestRequestSender(compileResult, semaphore, "/sample1/echo2"));
+        }
+
+        try {
+            if (!semaphore.tryAcquire(500, TimeUnit.MILLISECONDS)) {
+                Assert.fail("request execution not finished within 100ms");
+            }
+            String path = "/sample1/getResult";
+            HTTPTestRequest cMsg = MessageUtils.generateHTTPMessage(path, "GET");
+            HTTPCarbonMessage response = Services.invokeNew(compileResult, cMsg);
+
+            Assert.assertNotNull(response, "Response message not found");
+            String responseMsgPayload = StringUtils.getStringFromInputStream(new HttpMessageDataStreamer(response)
+                    .getInputStream());
+            Assert.assertEquals(responseMsgPayload, "3333333333333333333333333333333060.03255555555555555" +
+                    "555555555555555531.062.0777777777777777777777777777777", "incorrect request count");
+        } catch (InterruptedException e) {
+            Assert.fail("thread interrupted before request execution finished - " + e.getMessage(), e);
+        }
+    }
+
+    @Test(description = "Test locking service level and package level variable complex")
+    public void testServiceLvlPkgLvlVarLockComplex() {
+        Semaphore semaphore = new Semaphore(-29);
+
+        ExecutorService executor = TestThreadPool.getInstance().getExecutor();
+
+        for (int i = 0; i < 10; i++) {
+            executor.submit(new TestRequestSender(compileResult, semaphore, "/sample2/echo"));
+            executor.submit(new TestRequestSender(compileResult, semaphore, "/sample2/echo1"));
+            executor.submit(new TestRequestSender(compileResult, semaphore, "/sample2/echo2"));
+        }
+
+        try {
+            if (!semaphore.tryAcquire(500, TimeUnit.MILLISECONDS)) {
+                Assert.fail("request execution not finished within 100ms");
+            }
+            String path = "/sample2/getResult";
+            HTTPTestRequest cMsg = MessageUtils.generateHTTPMessage(path, "GET");
+            HTTPCarbonMessage response = Services.invokeNew(compileResult, cMsg);
+
+            Assert.assertNotNull(response, "Response message not found");
+            String responseMsgPayload = StringUtils.getStringFromInputStream(new HttpMessageDataStreamer(response)
+                    .getInputStream());
+            Assert.assertEquals(responseMsgPayload, "3333333333333333333333333333333060.03255555555555555" +
+                    "555555555555555531.062.0777777777777777777777777777777", "incorrect request count");
+        } catch (InterruptedException e) {
+            Assert.fail("thread interrupted before request execution finished - " + e.getMessage(), e);
+        }
+    }
+
+    @Test(description = "Test throwing error inside lock statement")
+    public void testThrowErrorInsideLock() {
+        Semaphore semaphore = new Semaphore(0);
+
+        ExecutorService executor = TestThreadPool.getInstance().getExecutor();
+
+        executor.submit(new TestRequestSender(compileResult, semaphore, "/sample3/echo"));
+
+        try {
+            if (!semaphore.tryAcquire(500, TimeUnit.MILLISECONDS)) {
+                Assert.fail("request execution not finished within 100ms");
+            }
+            String path = "/sample3/getMsg";
+            HTTPTestRequest cMsg = MessageUtils.generateHTTPMessage(path, "GET");
+            HTTPCarbonMessage response = Services.invokeNew(compileResult, cMsg);
+
+            Assert.assertNotNull(response, "Response message not found");
+            String responseMsgPayload = StringUtils.getStringFromInputStream(new HttpMessageDataStreamer(response)
+                    .getInputStream());
+            Assert.assertEquals(responseMsgPayload, "modified by second resource", "incorrect response msg");
+        } catch (InterruptedException e) {
+            Assert.fail("thread interrupted before request execution finished - " + e.getMessage(), e);
+        }
+    }
+
+    private class TestRequestSender implements Runnable {
+
+        private CompileResult compileResult;
+
+        private Semaphore semaphore;
+
+        private String path;
+
+        TestRequestSender(CompileResult compileResult, Semaphore semaphore, String path) {
+            this.compileResult = compileResult;
+            this.semaphore = semaphore;
+            this.path = path;
+        }
+
+        @Override
+        public void run() {
+            HTTPTestRequest cMsg = MessageUtils.generateHTTPMessage(path, "GET");
+            Services.invokeNew(compileResult, cMsg);
+            semaphore.release();
+        }
+    }
+}
---
+++ modules/ballerina-test/src/test/resources/test-src/lock/locks-in-functions.bal
@@ -0,0 +1,248 @@
+
+int lockWithinLockInt1 = 0;
+
+string lockWithinLockString1 = "";
+
+function lockWithinLock() (int, string) {
+    lock {
+        lockWithinLockInt1 = 50;
+        lockWithinLockString1 = "sample value";
+        lock {
+            lockWithinLockString1 = "second sample value";
+            lockWithinLockInt1 = 99;
+            lock {
+                lockWithinLockInt1 = 90;
+            }
+        }
+        lockWithinLockInt1 = 77;
+    }
+    return lockWithinLockInt1, lockWithinLockString1;
+}
+
+function lockWithinLockInWorkers() (int, string) {
+    worker w1 {
+        lock {
+            lockWithinLockInt1 = 90;
+            lockWithinLockString1 = "sample output";
+            lock {
+                lockWithinLockInt1 = 66;
+            }
+            sleep(100);
+            lockWithinLockInt1 = 45;
+        }
+    }
+    worker w2 {
+        sleep(20);
+        lock {
+            lockWithinLockString1 = "hello";
+            lock {
+                lockWithinLockInt1 = 88;
+            }
+            lockWithinLockInt1 = 56;
+        }
+    }
+    worker w3 {
+        sleep(30);
+        return lockWithinLockInt1, lockWithinLockString1;
+    }
+}
+
+function lockInsideWhileLoop() (int) {
+    worker w1 {
+        int i = 0;
+        while (i < 6) {
+            lock {
+                lockWithinLockInt1 = lockWithinLockInt1 + 1;
+            }
+            i = i +1;
+            sleep(10);
+        }
+        return lockWithinLockInt1;
+    }
+    worker w2 {
+        sleep(10);
+        lock {
+            lockWithinLockInt1 = lockWithinLockInt1 + 50;
+        }
+    }
+}
+
+function throwErrorInsideLock() (int, string) {
+    worker w1 {
+        lock {
+            sleep(50);
+            lockWithinLockInt1 = lockWithinLockInt1 + 1;
+            lockWithinLockString1 = "hello";
+            var ddd, err = <int>lockWithinLockString1;
+            if (err != null) {
+                throw err;
+            }
+        }
+    }
+    worker w2 {
+        sleep(10);
+        lock {
+            lockWithinLockString1 = "second worker string";
+            lockWithinLockInt1 = lockWithinLockInt1 + 50;
+        }
+        return lockWithinLockInt1, lockWithinLockString1;
+    }
+}
+
+function throwErrorInsideLockInsideTryFinally() (int, string) {
+    worker w1 {
+        try {
+            lock {
+                sleep(50);
+                lockWithinLockInt1 = lockWithinLockInt1 + 1;
+                lockWithinLockString1 = "hello";
+                var ddd, err = <int>lockWithinLockString1;
+                if (err != null) {
+                    throw err;
+                }
+            }
+        } catch (error e) {
+            sleep (10);
+            lock {
+                lockWithinLockInt1 = lockWithinLockInt1 + 1;
+            }
+        } finally {
+            lock {
+                lockWithinLockInt1 = lockWithinLockInt1 + 1;
+            }
+        }
+        return lockWithinLockInt1, lockWithinLockString1;
+    }
+    worker w2 {
+        sleep(10);
+        lock {
+            lockWithinLockString1 = "worker 2 sets the string value after try catch finally";
+            lockWithinLockInt1 = lockWithinLockInt1 + 50;
+        }
+    }
+}
+
+function throwErrorInsideTryCatchFinallyInsideLock() (int, string) {
+    worker w1 {
+        lock {
+            try {
+                sleep(50);
+                lockWithinLockInt1 = lockWithinLockInt1 + 1;
+                lockWithinLockString1 = "hello";
+                var ddd, err = <int>lockWithinLockString1;
+                if (err != null) {
+                    throw err;
+                }
+
+            } catch (error e) {
+                sleep (10);
+                lock {
+                    lockWithinLockInt1 = lockWithinLockInt1 + 1;
+                }
+            } finally {
+                lock {
+                    lockWithinLockInt1 = lockWithinLockInt1 + 1;
+                }
+            }
+        }
+        sleep(10);
+        return lockWithinLockInt1, lockWithinLockString1;
+    }
+    worker w2 {
+        sleep(10);
+        lock {
+            lockWithinLockString1 = "worker 2 sets the string after try catch finally inside lock";
+            lockWithinLockInt1 = lockWithinLockInt1 + 50;
+        }
+    }
+}
+
+function throwErrorInsideTryFinallyInsideLock() (int, string) {
+    worker w1 {
+        lock {
+            try {
+                sleep(50);
+                lockWithinLockInt1 = lockWithinLockInt1 + 1;
+                lockWithinLockString1 = "hello";
+                var ddd, err = <int>lockWithinLockString1;
+                if (err != null) {
+                    throw err;
+                }
+            } finally {
+                lock {
+                    lockWithinLockInt1 = lockWithinLockInt1 + 1;
+                }
+            }
+        }
+    }
+    worker w2 {
+        sleep(10);
+        lock {
+            lockWithinLockString1 = "worker 2 sets the string after try finally";
+            lockWithinLockInt1 = lockWithinLockInt1 + 50;
+        }
+        return lockWithinLockInt1, lockWithinLockString1;
+    }
+}
+
+function throwErrorInsideLockInsideTryCatch() (int, string) {
+    worker w1 {
+        try {
+            lock {
+                sleep(50);
+                lockWithinLockInt1 = lockWithinLockInt1 + 1;
+                lockWithinLockString1 = "hello";
+                var ddd, err = <int>lockWithinLockString1;
+                if (err != null) {
+                    throw err;
+                }
+            }
+        } catch (error e) {
+            sleep (10);
+            lock {
+                lockWithinLockInt1 = lockWithinLockInt1 + 1;
+            }
+        }
+        return lockWithinLockInt1, lockWithinLockString1;
+    }
+    worker w2 {
+        sleep(10);
+        lock {
+            lockWithinLockString1 = "worker 2 sets the string value after try catch";
+            lockWithinLockInt1 = lockWithinLockInt1 + 50;
+        }
+    }
+}
+
+function throwErrorInsideTryCatchInsideLock() (int, string) {
+    worker w1 {
+        lock {
+            try {
+                sleep(50);
+                lockWithinLockInt1 = lockWithinLockInt1 + 1;
+                lockWithinLockString1 = "hello";
+                var ddd, err = <int>lockWithinLockString1;
+                if (err != null) {
+                    throw err;
+                }
+
+            } catch (error e) {
+                sleep (10);
+                lock {
+                    lockWithinLockInt1 = lockWithinLockInt1 + 1;
+                }
+            }
+        }
+        sleep(10);
+        return lockWithinLockInt1, lockWithinLockString1;
+    }
+    worker w2 {
+        sleep(10);
+        lock {
+            lockWithinLockString1 = "worker 2 sets the string after try catch inside lock";
+            lockWithinLockInt1 = lockWithinLockInt1 + 50;
+        }
+    }
+}
+
+
