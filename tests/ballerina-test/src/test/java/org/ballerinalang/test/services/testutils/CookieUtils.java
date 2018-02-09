2d3942c7fd994d661750629dbe2dbc27aa91b69a
Ayoma Wijethunga <ayomawdb@gmail.com>
2018-02-09 10:53:37 +0530

    Remove unnecessary empty line and add license header

--- tests/ballerina-test/src/test/java/org/ballerinalang/test/services/testutils/CookieUtils.java
+++ tests/ballerina-test/src/test/java/org/ballerinalang/test/services/testutils/CookieUtils.java
@@ -1,17 +1,36 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.ballerinalang.test.services.testutils;
 
 import org.ballerinalang.net.http.Constants;
 
+/**
+ * Utility for properly split and parsing cookie header
+ */
 public class CookieUtils {
 
     public static Cookie getCookie(String headerValue) {
         String[] headerSplits = headerValue.split(";");
         Cookie cookie = new Cookie();
-
         if (headerSplits.length > 0) {
             cookie.name = headerSplits[0].split("=")[0];
             cookie.value = headerSplits[0].split("=")[1];
-
             for (String headerSplit : headerSplits) {
                 if (headerSplit.trim().equals(Constants.HTTP_ONLY)) {
                     cookie.httpOnly = true;
@@ -22,7 +41,6 @@ public class CookieUtils {
                 }
             }
         }
-
         return cookie;
     }
 
