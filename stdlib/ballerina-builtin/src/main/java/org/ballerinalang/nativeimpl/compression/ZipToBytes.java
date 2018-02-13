38c7f6ca04a9feea1d9a625498a190ea0e5568ca
NatashaWso2 <nwijesekare@gmail.com>
2018-02-13 17:07:45 +0530

    Implement ballerina:compression library to compress and decompress files/folders

---
+++ stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/compression/ZipToBytes.java
@@ -0,0 +1,111 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.ballerinalang.nativeimpl.compression;
+
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BBlob;
+import org.ballerinalang.model.values.BValue;
+import org.ballerinalang.natives.AbstractNativeFunction;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaFunction;
+import org.ballerinalang.natives.annotations.ReturnType;
+import org.ballerinalang.util.CompressionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Native function ballerina.compression:ZipToBytes.
+ *
+ * @since 0.962.0
+ */
+@BallerinaFunction(
+        packageName = "ballerina.compression",
+        functionName = "zipToBytes",
+        args = {@Argument(name = "dirPath", type = TypeKind.STRING)},
+        returnType = {@ReturnType(type = TypeKind.BLOB)},
+        isPublic = true
+)
+public class ZipToBytes extends AbstractNativeFunction {
+
+    private static final Logger log = LoggerFactory.getLogger(ZipToBytes.class);
+
+    /**
+     * File path defined in ballerina.compression
+     */
+    private static final int SRC_PATH_FIELD_INDEX = 0;
+
+    /**
+     * @param dirPath file content as a byte array.
+     */
+    private static byte[] zipToByte(String dirPath) throws IOException {
+        File dir = new File(dirPath);
+        List<String> filesListInDir = CompressionUtils.populateFilesList(dir);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ZipOutputStream zos = new ZipOutputStream(bos);
+        byte[] buffer = new byte[4096];
+        try {
+            for (String filePath : filesListInDir) {
+                ZipEntry ze = new ZipEntry(filePath.substring(dir.getAbsolutePath().length() + 1, filePath.length()));
+                zos.putNextEntry(ze);
+                FileInputStream fis = null;
+                try {
+                    fis = new FileInputStream(filePath);
+                    int len;
+                    while ((len = fis.read(buffer)) > 0) {
+                        zos.write(buffer, 0, len);
+                    }
+                    zos.closeEntry();
+                } finally {
+                    if (fis != null) {
+                        fis.close();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            log.debug("I/O Exception when processing files ", e);
+            log.error("I/O Exception when processing files " + e.getMessage());
+        } finally {
+            zos.close();
+        }
+        return bos.toByteArray();
+
+    }
+
+    @Override
+    public BValue[] execute(Context context) {
+        BBlob readByteBlob;
+        String dirPath = getStringArgument(context, SRC_PATH_FIELD_INDEX);
+        byte[] compressedBytes = new byte[0];
+        try {
+            compressedBytes = zipToByte(dirPath);
+        } catch (IOException e) {
+            log.debug("I/O Exception when processing files ", e);
+            log.error("I/O Exception when processing files " + e.getMessage());
+        }
+        readByteBlob = new BBlob(compressedBytes);
+        return getBValues(readByteBlob);
+    }
+}
