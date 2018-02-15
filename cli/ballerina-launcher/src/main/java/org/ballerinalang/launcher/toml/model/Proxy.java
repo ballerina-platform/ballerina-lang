fb399599113378f1b5fdd1b94fffb72df8eff494
NatashaWso2 <nwijesekare@gmail.com>
2018-02-15 09:06:55 +0530

    Add support for toml parser to read proxy configurations from file

---
+++ cli/ballerina-launcher/src/main/java/org/ballerinalang/launcher/toml/model/Proxy.java
@@ -0,0 +1,40 @@
+package org.ballerinalang.launcher.toml.model;
+
+public class Proxy {
+    private String host;
+    private String port;
+    private String userName;
+    private String password;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
