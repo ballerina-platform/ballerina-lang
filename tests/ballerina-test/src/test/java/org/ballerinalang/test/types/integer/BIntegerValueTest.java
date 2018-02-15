0d3726e0f31bd83bde4ea81af584a0c19d5026f3
jayamine <jayamine.alupotha@gmail.com>
2018-02-15 15:08:57 +0530

    Add Hex, Binary, Octal test cases

--- tests/ballerina-test/src/test/java/org/ballerinalang/test/types/integer/BIntegerValueTest.java
+++ tests/ballerina-test/src/test/java/org/ballerinalang/test/types/integer/BIntegerValueTest.java
@@ -1,26 +1,27 @@
 /*
-*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*  http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing,
-*  software distributed under the License is distributed on an
-*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-*  KIND, either express or implied.  See the License for the
-*  specific language governing permissions and limitations
-*  under the License.
-*/
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.ballerinalang.test.types.integer;
 
 import org.ballerinalang.launcher.util.BCompileUtil;
 import org.ballerinalang.launcher.util.BRunUtil;
 import org.ballerinalang.launcher.util.CompileResult;
 import org.ballerinalang.model.values.BInteger;
+import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BValue;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -28,6 +29,9 @@ import org.testng.annotations.Test;
 
 /**
  * This test class will test the behaviour of int values with expressions.
+ * Hex assignment
+ * Octal assignment
+ * Binary assignment
  * Addition
  * Multiplication
  * Division
@@ -36,6 +40,9 @@ import org.testng.annotations.Test;
  * Defining a Integer value
  * long b;
  * b = 10.1L;
+ * toHexString
+ * toOctalString
+ * toBinaryString
  */
 public class BIntegerValueTest {
     private CompileResult result;
@@ -63,9 +70,63 @@ public class BIntegerValueTest {
         Assert.assertEquals(intValue.intValue(), (-10), "Invalid int value returned.");
     }
 
+    @Test(description = "Test long(hex) value assignment")
+    public void testHexValue() {
+        BValue[] returns = BRunUtil.invoke(result, "testHexValue", new BValue[]{});
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BInteger.class);
+        BInteger intValue = (BInteger) returns[0];
+        Assert.assertEquals(intValue.intValue(), 10, "Invalid int value returned.");
+    }
+
+    @Test(description = "Test negative long(hex) value assignment")
+    public void testNegativeHexValue() {
+        BValue[] returns = BRunUtil.invoke(result, "testNegativeHaxValue", new BValue[]{});
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BInteger.class);
+        BInteger intValue = (BInteger) returns[0];
+        Assert.assertEquals(intValue.intValue(), (-10), "Invalid int value returned.");
+    }
+
+    @Test(description = "Test long(octal) value assignment")
+    public void testOctalValue() {
+        BValue[] returns = BRunUtil.invoke(result, "testOctalValue", new BValue[]{});
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BInteger.class);
+        BInteger intValue = (BInteger) returns[0];
+        Assert.assertEquals(intValue.intValue(), 10, "Invalid int value returned.");
+    }
+
+    @Test(description = "Test negative long(octal) value assignment")
+    public void testNegativeOctalValue() {
+        BValue[] returns = BRunUtil.invoke(result, "testNegativeOctalValue", new BValue[]{});
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BInteger.class);
+        BInteger intValue = (BInteger) returns[0];
+        Assert.assertEquals(intValue.intValue(), (-10), "Invalid int value returned.");
+    }
+
+    @Test(description = "Test long(binary) value assignment")
+    public void testBinaryValue() {
+        BValue[] returns = BRunUtil.invoke(result, "testBinaryValue", new BValue[]{});
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BInteger.class);
+        BInteger intValue = (BInteger) returns[0];
+        Assert.assertEquals(intValue.intValue(), 10, "Invalid int value returned.");
+    }
+
+    @Test(description = "Test negative long(binary) value assignment")
+    public void testNegativeBinaryValue() {
+        BValue[] returns = BRunUtil.invoke(result, "testNegativeBinaryValue", new BValue[]{});
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BInteger.class);
+        BInteger intValue = (BInteger) returns[0];
+        Assert.assertEquals(intValue.intValue(), (-10), "Invalid int value returned.");
+    }
+
     @Test(description = "Test long value assignment from a value returned by function")
     public void testIntegerValueAssignmentByReturnValue() {
-        BValue[] returns =  BRunUtil.invoke(result, "testIntegerValueAssignmentByReturnValue", new BValue[]{});
+        BValue[] returns = BRunUtil.invoke(result, "testIntegerValueAssignmentByReturnValue", new BValue[]{});
         Assert.assertEquals(returns.length, 1);
         Assert.assertSame(returns[0].getClass(), BInteger.class);
         BInteger intValue = (BInteger) returns[0];
@@ -93,7 +154,7 @@ public class BIntegerValueTest {
 
     @Test(description = "Test long value Subtraction")
     public void testIntegerValueSubtraction() {
-        BValue[] returns =  BRunUtil.invoke(result, "testIntegerSubtraction", new BValue[]{});
+        BValue[] returns = BRunUtil.invoke(result, "testIntegerSubtraction", new BValue[]{});
         Assert.assertEquals(returns.length, 1);
         Assert.assertSame(returns[0].getClass(), BInteger.class);
         BInteger intValue = (BInteger) returns[0];
@@ -111,10 +172,37 @@ public class BIntegerValueTest {
 
     @Test(description = "Test long value Division")
     public void testIntegerValueDivision() {
-        BValue[] returns =  BRunUtil.invoke(result, "testIntegerDivision", new BValue[]{});
+        BValue[] returns = BRunUtil.invoke(result, "testIntegerDivision", new BValue[]{});
         Assert.assertEquals(returns.length, 1);
         Assert.assertSame(returns[0].getClass(), BInteger.class);
         BInteger intValue = (BInteger) returns[0];
         Assert.assertEquals(intValue.intValue(), 5, "Invalid int value returned.");
+    }
+
+    @Test(description = "Test toHexString function")
+    public void testToHexString() {
+        BValue[] returns = BRunUtil.invoke(result, "testToHexString", new BValue[]{});
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BString.class);
+        BString stringValue = (BString) returns[0];
+        Assert.assertEquals(stringValue.stringValue(), "0xa", "Invalid hex value returned.");
+    }
+
+    @Test(description = "Test toOctalString function")
+    public void testToOctalString() {
+        BValue[] returns = BRunUtil.invoke(result, "testToOctalString", new BValue[]{});
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BString.class);
+        BString stringValue = (BString) returns[0];
+        Assert.assertEquals(stringValue.stringValue(), "0_12", "Invalid octal value returned.");
+    }
+
+    @Test(description = "Test toBinary function")
+    public void testToBinaryString() {
+        BValue[] returns = BRunUtil.invoke(result, "testToBinaryString", new BValue[]{});
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BString.class);
+        BString stringValue = (BString) returns[0];
+        Assert.assertEquals(stringValue.stringValue(), "0b1010", "Invalid binary value returned.");
     }
 }
--- tests/ballerina-test/src/test/resources/test-src/types/integer/integer-value.bal
+++ tests/ballerina-test/src/test/resources/test-src/types/integer/integer-value.bal
@@ -1,22 +1,76 @@
-function testIntegerValue() (int) {
+//
+// Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+//
+// WSO2 Inc. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+function testIntegerValue () (int) {
     int b;
     b = 10;
     return b;
 }
 
-function testNegativeIntegerValue() (int) {
+function testNegativeIntegerValue () (int) {
     int y;
     y = -10;
     return y;
 }
 
-function testIntegerValueAssignmentByReturnValue() (int) {
+function testHexValue () (int) {
+    int b;
+    b = 0xa;
+    return b;
+}
+
+function testNegativeHaxValue () (int) {
+    int b;
+    b = -0xa;
+    return b;
+}
+
+function testOctalValue () (int) {
+    int b;
+    b = 0_12;
+    return b;
+}
+
+function testNegativeOctalValue () (int) {
+    int b;
+    b = -0_12;
+    return b;
+}
+
+function testBinaryValue () (int) {
+    int b;
+    b = 0b1010;
+    return b;
+}
+
+function testNegativeBinaryValue () (int) {
+    int b;
+    b = -0b1010;
+    return b;
+}
+
+function testIntegerValueAssignmentByReturnValue () (int) {
     int x;
     x = testIntegerValue();
     return x;
 }
 
-function testIntegerAddition() (int) {
+function testIntegerAddition () (int) {
     int b;
     int a;
     a = 9;
@@ -24,7 +78,7 @@ function testIntegerAddition() (int) {
     return a + b;
 }
 
-function testIntegerMultiplication() (int) {
+function testIntegerMultiplication () (int) {
     int b;
     int a;
     a = 2;
@@ -32,7 +86,7 @@ function testIntegerMultiplication() (int) {
     return a * b;
 }
 
-function testIntegerSubtraction() (int) {
+function testIntegerSubtraction () (int) {
     int b;
     int a;
     a = 25;
@@ -40,7 +94,7 @@ function testIntegerSubtraction() (int) {
     return a - b;
 }
 
-function testIntegerDivision() (int) {
+function testIntegerDivision () (int) {
     int b;
     int a;
     a = 25;
@@ -48,9 +102,23 @@ function testIntegerDivision() (int) {
     return a / b;
 }
 
-function testIntegerParameter(int a) (int) {
+function testIntegerParameter (int a) (int) {
     int b;
     b = a;
     return b;
 }
 
+function testToHexString() (string) {
+    int a = 0xa;
+    return a.toHexString();
+}
+
+function testToOctalString() (string) {
+    int a = 0_12;
+    return a.toOctalString();
+}
+
+function testToBinaryString() (string) {
+    int a = 0b1010;
+    return a.toBinaryString();
+}
