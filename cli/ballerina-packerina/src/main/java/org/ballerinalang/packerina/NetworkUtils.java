26f9b40eb4448e2b43a7e1907b67fabd2662a6ad
NatashaWso2 <nwijesekare@gmail.com>
2018-02-15 22:01:39 +0530

    Add support to read proxy configurations to direct the request to the central

--- cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/NetworkUtils.java
+++ cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/NetworkUtils.java
@@ -20,14 +20,22 @@ package org.ballerinalang.packerina;
 import org.ballerinalang.bre.Context;
 import org.ballerinalang.compiler.CompilerPhase;
 import org.ballerinalang.launcher.LauncherUtils;
+import org.ballerinalang.launcher.toml.model.Proxy;
+import org.ballerinalang.launcher.toml.parser.ProxyProcessor;
 import org.ballerinalang.launcher.util.BCompileUtil;
 import org.ballerinalang.launcher.util.CompileResult;
 import org.ballerinalang.util.codegen.PackageInfo;
 import org.ballerinalang.util.codegen.ProgramFile;
 import org.ballerinalang.util.debugger.Debugger;
 import org.ballerinalang.util.program.BLangFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.ballerinalang.util.BLangConstants.USER_REPO_ARTIFACTS_DIRNAME;
 import static org.ballerinalang.util.BLangConstants.USER_REPO_SRC_DIRNAME;
@@ -36,35 +44,9 @@ import static org.ballerinalang.util.BLangConstants.USER_REPO_SRC_DIRNAME;
  * Util class for network calls.
  */
 public class NetworkUtils {
+    private static final Logger log = LoggerFactory.getLogger(NetworkUtils.class);
+    private static final String BALLERINA_CENTRAL_REPO_URL = "http://0.0.0.0:9090/p/";
     private static CompileResult compileResult;
-
-    /**
-     * Pull/Downloads packages from the package repository.
-     *
-     * @param resourceName package name to be pulled
-     */
-    public static void pullPackages(String resourceName) {
-        compileResult = compilePullCmdBalFile("ballerina.pull");
-        Path targetDirectoryPath = UserRepositoryUtils.initializeUserRepository()
-                .resolve(USER_REPO_ARTIFACTS_DIRNAME).resolve(USER_REPO_SRC_DIRNAME);
-        String ballerinaCentralRepoURL = "http://52.54.136.13:9090/p/";
-        String dstPath = targetDirectoryPath.toString();
-        String resourcePath = ballerinaCentralRepoURL + resourceName;
-
-        String[] arguments = new String[]{resourcePath, dstPath};
-        LauncherUtils.runMain(compileResult.getProgFile(), arguments);
-    }
-
-    /**
-     * Push/Uploads packages to the central repository.
-     * @param resourceName path of the package folder to be pushed
-     */
-    public static void pushPackages(String resourceName) {
-        compileResult = compilePullCmdBalFile("ballerina.push");
-        String ballerinaCentralRepoURL = "http://52.54.136.13:9090/p/" + resourceName + "/1.0.0";
-        String[] arguments = new String[]{ballerinaCentralRepoURL, resourceName};
-        LauncherUtils.runMain(compileResult.getProgFile(), arguments);
-    }
 
     /**
      * Compile the bal file.
@@ -81,5 +63,86 @@ public class NetworkUtils {
         compileResult.setContext(context);
         BLangFunctions.invokePackageInitFunction(programFile, packageInfo.getInitFunctionInfo(), context);
         return compileResult;
+    }
+
+    /**
+     * Pull/Downloads packages from the package repository.
+     *
+     * @param resourceName package name to be pulled
+     */
+    public static void pullPackages(String resourceName) {
+        compileResult = compilePullCmdBalFile("ballerina.pull");
+        Path targetDirectoryPath = UserRepositoryUtils.initializeUserRepository()
+                .resolve(USER_REPO_ARTIFACTS_DIRNAME).resolve(USER_REPO_SRC_DIRNAME);
+        String dstPath = targetDirectoryPath + File.separator;
+        String resourcePath = BALLERINA_CENTRAL_REPO_URL + File.separator + resourceName + "/1.0.0";
+        String[] proxyConfigs = readProxyConfigurations();
+        String[] arguments = new String[]{resourcePath, dstPath};
+        arguments = Stream.concat(Arrays.stream(arguments), Arrays.stream(proxyConfigs))
+                .toArray(String[]::new);
+        LauncherUtils.runMain(compileResult.getProgFile(), arguments);
+    }
+
+    /**
+     * Push/Uploads packages to the central repository.
+     *
+     * @param resourceName path of the package folder to be pushed
+     */
+    public static void pushPackages(String resourceName) {
+        compileResult = compilePullCmdBalFile("ballerina.push");
+        String resourcePath = BALLERINA_CENTRAL_REPO_URL + resourceName + "/1.0.0";
+        String[] proxyConfigs = readProxyConfigurations();
+        String[] arguments = new String[]{resourcePath, resourceName};
+        arguments = Stream.concat(Arrays.stream(arguments), Arrays.stream(proxyConfigs))
+                .toArray(String[]::new);
+        LauncherUtils.runMain(compileResult.getProgFile(), arguments);
+    }
+
+    /**
+     * Read proxy configurations from the BallerinaCLI.toml file
+     *
+     * @return array with proxy configurations
+     */
+    private static String[] readProxyConfigurations() {
+        File cliTomlFile = new File(UserRepositoryUtils.initializeUserRepository().toString()
+                + File.separator + "BallerinaCLI.toml");
+        String host = "", port = "", username = "", password = "";
+        String proxyConfigArr[] = new String[]{host, port, username, password};
+        if (cliTomlFile.exists()) {
+            try {
+                Proxy proxy = ProxyProcessor.parseTomlContentFromFile(cliTomlFile.toString());
+
+                if (proxy.getHost() != null) {
+                    host = removeQuotationsFromValue(proxy.getHost());
+                    proxyConfigArr[0] = host;
+                }
+                if (proxy.getPort() != null) {
+                    port = removeQuotationsFromValue(proxy.getPort());
+                    proxyConfigArr[1] = port;
+                }
+                if (proxy.getUserName() != null) {
+                    username = removeQuotationsFromValue(proxy.getUserName());
+                    proxyConfigArr[2] = username;
+                }
+                if (proxy.getPassword() != null) {
+                    password = removeQuotationsFromValue(proxy.getPassword());
+                    proxyConfigArr[3] = password;
+                }
+            } catch (IOException e) {
+                log.debug("I/O Exception when processing the toml file ", e);
+                log.error("I/O Exception when processing the toml file " + e.getMessage());
+            }
+        }
+        return proxyConfigArr;
+    }
+
+    /**
+     * Remove enclosing quotation from the string value
+     *
+     * @param value string value with enclosing quotations
+     * @return string value after removing the enclosing quotations
+     */
+    private static String removeQuotationsFromValue(String value) {
+        return value.replace("\"", "");
     }
 }
--- cli/ballerina-packerina/src/main/resources/ballerina/ballerina/push/main.bal
+++ cli/ballerina-packerina/src/main/resources/ballerina/ballerina/push/main.bal
@@ -7,7 +7,7 @@ import ballerina.net.http;
 function main (string[] args) {
 
     endpoint<http:HttpClient> httpEndpoint {
-        create http:HttpClient(args[0], {});
+        create http:HttpClient(args[0], getConnectorConfigs(args[2], args[3], args[4], args[5]));
     }
     mime:Entity topLevelEntity = {};
     mime:MediaType mediaType = mime:getMediaType(mime:MULTIPART_FORM_DATA);
@@ -26,4 +26,26 @@ function main (string[] args) {
     http:InResponse resp1 = {};
     resp1, _ = httpEndpoint.post("", request);
     println("Ballerina package pushed successfully");
+}
+
+function getConnectorConfigs(string host, string port, string username, string password) (http:Options) {
+    http:Options option;
+    int portInt = 0;
+    if (host != ""){
+        if (port != ""){
+            var portI, _ = <int> port;
+            portInt = portI;
+        }
+        option = {
+                     proxy:{
+                               host:host,
+                               port:portInt,
+                               userName:username,
+                               password:password
+                           }
+                 };
+    } else {
+        option = {};
+    }
+    return option;
 }
