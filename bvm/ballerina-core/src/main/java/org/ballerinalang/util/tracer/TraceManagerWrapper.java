3d6a8b0ac9aaee25a37f863f404d416bae9fa223
Grainier Perera <grainier@wso2.com>
2018-03-04 13:59:14 +0530

    Fix the issue of span context not getting injected.

--- bvm/ballerina-core/src/main/java/org/ballerinalang/util/tracer/TraceManagerWrapper.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/util/tracer/TraceManagerWrapper.java
@@ -94,7 +94,7 @@ public class TraceManagerWrapper {
             Map<String, Object> spanList = manager.buildSpan(invocationId, resource, spanContext,
                     activeBTracer.getTags(), true, service);
 
-            Map<String, String> traceContextMap = manager.inject(activeBTracer.getSpans(), null, service);
+            Map<String, String> traceContextMap = manager.inject(spanList, null, service);
             activeBTracer.getProperties().putAll(addTracePrefix(traceContextMap));
 
             activeBTracer.setSpans(spanList);
