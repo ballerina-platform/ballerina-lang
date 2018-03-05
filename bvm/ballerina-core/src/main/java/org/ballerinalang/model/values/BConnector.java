e8870dbf3c0f89f9f32c1db3a405ed86a6e5d26d
Kevin Ratnasekera <djkevincr@yahoo.com>
2018-03-05 23:54:03 +0530

    Fix checkstyle issues

--- bvm/ballerina-core/src/main/java/org/ballerinalang/model/values/BConnector.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/model/values/BConnector.java
@@ -18,7 +18,6 @@ package org.ballerinalang.model.values;
 
 import org.ballerinalang.model.types.BConnectorType;
 import org.ballerinalang.model.types.BType;
-import org.ballerinalang.model.types.BTypes;
 
 import java.util.Arrays;
 import java.util.HashMap;
