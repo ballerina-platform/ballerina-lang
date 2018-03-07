28f9b3c95fe09d3159cf97b8f8a61f088a372912
isudana <isudana@gmail.com>
2018-03-07 09:45:17 +0530

    Add reject promise native implementation

---
+++ stdlib/ballerina-http/src/main/java/org/ballerinalang/net/http/actions/RejectPromise.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ballerinalang.net.http.actions;
+
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.connector.api.ConnectorFuture;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BBoolean;
+import org.ballerinalang.model.values.BConnector;
+import org.ballerinalang.model.values.BStruct;
+import org.ballerinalang.nativeimpl.actions.ClientConnectorFuture;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaAction;
+import org.ballerinalang.natives.annotations.ReturnType;
+import org.ballerinalang.net.http.HttpConstants;
+import org.ballerinalang.net.http.HttpUtil;
+import org.ballerinalang.util.exceptions.BallerinaException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.transport.http.netty.contract.HttpClientConnector;
+import org.wso2.transport.http.netty.message.Http2PushPromise;
+import org.wso2.transport.http.netty.message.ResponseHandle;
+
+/**
+ * {@code RejectPromise} action can be used to reject a push promise.
+ */
+@BallerinaAction(
+        packageName = "ballerina.net.http",
+        actionName = "rejectPromise",
+        connectorName = HttpConstants.CONNECTOR_NAME,
+        args = {
+                @Argument(name = "c", type = TypeKind.CONNECTOR),
+                @Argument(name = "handle", type = TypeKind.STRUCT, structType = "HttpHandle",
+                        structPackage = "ballerina.net.http"),
+                @Argument(name = "promise", type = TypeKind.STRUCT, structType = "PushPromise",
+                        structPackage = "ballerina.net.http")
+        },
+        returnType = {
+                @ReturnType(type = TypeKind.BOOLEAN)
+        },
+        connectorArgs = {
+                @Argument(name = "serviceUri", type = TypeKind.STRING),
+                @Argument(name = "options", type = TypeKind.STRUCT, structType = "Options",
+                        structPackage = "ballerina.net.http")
+        }
+)
+public class RejectPromise extends AbstractHTTPAction {
+
+    private static final Logger logger = LoggerFactory.getLogger(RejectPromise.class);
+
+    @Override
+    public ConnectorFuture execute(Context context) {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Executing Native Action : {}", this.getName());
+        }
+        BStruct handleStruct = ((BStruct) getRefArgument(context, 1));
+
+        ResponseHandle responseHandle = (ResponseHandle) handleStruct.getNativeData(HttpConstants.TRANSPORT_HANDLE);
+        if (responseHandle == null) {
+            throw new BallerinaException("invalid handle");
+        }
+        BStruct pushPromiseStruct = (BStruct) getRefArgument(context, 2);
+        Http2PushPromise http2PushPromise = HttpUtil.getPushPromise(pushPromiseStruct,
+                                                                    HttpUtil.createHttpPushPromise());
+        BConnector bConnector = (BConnector) getRefArgument(context, 0);
+        HttpClientConnector clientConnector =
+                (HttpClientConnector) bConnector.getnativeData(HttpConstants.CONNECTOR_NAME);
+        clientConnector.rejectPromise(responseHandle, http2PushPromise);
+
+        ClientConnectorFuture ballerinaFuture = new ClientConnectorFuture();
+        ballerinaFuture.notifyReply(new BBoolean(true));  // TODO: Implement a listener to see the progress
+        return ballerinaFuture;
+    }
+}
