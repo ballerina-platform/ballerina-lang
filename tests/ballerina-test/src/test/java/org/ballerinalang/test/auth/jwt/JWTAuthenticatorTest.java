a1cd87c782f5bb04058cf9b85c5bcf360b1c05fc
Ishara Karunarathna <isharak@wso2.com>
2018-03-07 12:47:01 +0530

    Add JWT authenticator

--- tests/ballerina-test/src/test/java/org/ballerinalang/test/auth/jwt/JWTAuthenticatorTest.java
+++ tests/ballerina-test/src/test/java/org/ballerinalang/test/auth/jwt/JWTAuthenticatorTest.java
@@ -16,13 +16,15 @@
  * under the License.
  */
 
-package org.ballerinalang.test.jwt;
+package org.ballerinalang.test.auth.jwt;
 
 import org.ballerinalang.config.ConfigRegistry;
 import org.ballerinalang.launcher.util.BCompileUtil;
 import org.ballerinalang.launcher.util.BRunUtil;
 import org.ballerinalang.launcher.util.CompileResult;
 import org.ballerinalang.model.values.BBoolean;
+import org.ballerinalang.model.values.BString;
+import org.ballerinalang.model.values.BStruct;
 import org.ballerinalang.model.values.BValue;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -38,9 +40,38 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
- * Test JWT token signing and signature verification functions.
+ * Test JWT authenticator.
  */
-public class JwtSignatureTest {
+public class JWTAuthenticatorTest {
+
+    /**
+     * #JWT Authenticator configurations
+     * [authenticator_jwt]
+     * issuer=<jwt token issuer>
+     * audience=<audience>
+     * certificateAlias=<public certificate of the issuer>
+     * <p>
+     * #JWT Authenticator cache configuration
+     * [jwt_auth_cache]
+     * enabled=<true of false>
+     * expiryTime=<expiry time in milliseconds>
+     * capacity=<capacity eg: 100>
+     * evictionFactor=<evictionFactor eg: 0.25>
+     * <p>
+     * #Keystore configuration
+     * [keyStore]
+     * location=<keyStore location>
+     * type=<keystore type eg: PKCS12>
+     * keyStorePassword=<keystore password>
+     * keyAlias=<default private key alias>
+     * keyPassword=<default private key password>
+     * <p>
+     * #Truststore configuration
+     * [trustStore]
+     * location=<trustStore location>
+     * type=<trustStore type eg: PKCS12>
+     * trustStorePassword=<trustStore password>
+     */
 
     private static final String KEY_STORE_CONFIG = "keyStore";
     private static final String KEY_STORE_LOCATION = "location";
@@ -51,15 +82,26 @@ public class JwtSignatureTest {
     private static final String TRUST_STORE_TYPE = "type";
     private static final String TRUST_STORE_PASSWORD = "trustStorePassword";
 
+    private static final String JWT_AUTHENTICATOR_CONFIG = "authenticator_jwt";
+    private static final String ISSUER = "issuer";
+    private static final String AUDIENCE = "audience";
+    private static final String CERTIFICATE_ALIAS = "certificateAlias";
+    private static final String JWT_AUTH_CACHE_CONFIG = "jwt_auth_cache";
+    private static final String ENABLED = "enabled";
+    private static final String EXPIRY_TIME = "expiryTime";
+    private static final String CAPACITY = "capacity";
+    private static final String EVICTION_FACTOR = "evictionFactor";
+
     private ConfigRegistry initialConfigRegistry;
-    private String resourceRoot;
     private CompileResult compileResult;
+    private String jwtToken;
 
     @BeforeClass
     public void setup() throws Exception {
         initialConfigRegistry = ConfigRegistry.getInstance();
 
         ConfigRegistry configRegistry = mock(ConfigRegistry.class);
+        //KeyStore configurations
         when(configRegistry.getInstanceConfigValue(KEY_STORE_CONFIG, KEY_STORE_LOCATION))
                 .thenReturn(getClass().getClassLoader().getResource(
                         "datafiles/security/keyStore/ballerinaKeystore.p12").getPath());
@@ -74,6 +116,22 @@ public class JwtSignatureTest {
                 .thenReturn("ballerina");
         when(configRegistry.getInstanceConfigValue(TRUST_STORE_CONFIG, TRUST_STORE_TYPE))
                 .thenReturn("pkcs12");
+        //Authenticator configurations
+        when(configRegistry.getInstanceConfigValue(JWT_AUTHENTICATOR_CONFIG, ISSUER))
+                .thenReturn("wso2");
+        when(configRegistry.getInstanceConfigValue(JWT_AUTHENTICATOR_CONFIG, AUDIENCE))
+                .thenReturn("ballerina");
+        when(configRegistry.getInstanceConfigValue(JWT_AUTHENTICATOR_CONFIG, CERTIFICATE_ALIAS))
+                .thenReturn("ballerina");
+        //Authenticator cache configurations
+        when(configRegistry.getInstanceConfigValue(JWT_AUTH_CACHE_CONFIG, ENABLED))
+                .thenReturn("true");
+        when(configRegistry.getInstanceConfigValue(JWT_AUTH_CACHE_CONFIG, EXPIRY_TIME))
+                .thenReturn("10000");
+        when(configRegistry.getInstanceConfigValue(JWT_AUTH_CACHE_CONFIG, CAPACITY))
+                .thenReturn("100");
+        when(configRegistry.getInstanceConfigValue(JWT_AUTH_CACHE_CONFIG, EVICTION_FACTOR))
+                .thenReturn("0.25");
 
         Field field = ConfigRegistry.class.getDeclaredField("configRegistry");
         field.setAccessible(true);
@@ -85,21 +143,29 @@ public class JwtSignatureTest {
         modifiersField.setAccessible(false);
         field.setAccessible(false);
 
-        resourceRoot = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-        Path sourceRoot = Paths.get(resourceRoot, "test-src", "jwt");
-        compileResult = BCompileUtil.compile(sourceRoot.resolve("signature.bal").toString());
+        String resourceRoot = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+        Path sourceRoot = Paths.get(resourceRoot, "test-src", "auth");
+        compileResult = BCompileUtil.compile(sourceRoot.resolve("jwt-authenticator-test.bal").toString());
+
+        //Generate a JWT token
+        Path jwtIssuerSourceRoot = Paths.get(resourceRoot, "test-src", "jwt");
+        CompileResult jwtIssuerCompileResult = BCompileUtil.compile(
+                jwtIssuerSourceRoot.resolve("jwt-test.bal").toString());
+        BValue[] returns = BRunUtil.invoke(jwtIssuerCompileResult, "testIssueJwt");
+        jwtToken = returns[0].stringValue();
     }
 
-    @Test(description = "Test case for JWT signature verification with valid signature")
-    public void testVerifyJWTSignature() {
-        BValue[] returns = BRunUtil.invoke(compileResult, "testVerifyJWTSignature");
-        Assert.assertTrue(returns[0] instanceof BBoolean);
-        Assert.assertTrue(((BBoolean) returns[0]).booleanValue());
+    @Test(description = "Test case for creating JWT authenticator with a cache")
+    public void testCreateJwtAuthenticatorWithCache() {
+        BValue[] returns = BRunUtil.invoke(compileResult, "testJwtAuthenticatorCreationWithCache");
+        Assert.assertNotNull(returns);
+        Assert.assertTrue(returns[0] instanceof BStruct);
     }
 
-    @Test(description = "Test case for JWT signing with valid data")
-    public void testSignJWTData() {
-        BValue[] returns = BRunUtil.invoke(compileResult, "testSignJWTData");
+    @Test(description = "Test JWT authentication success")
+    public void testAuthenticationSuccess() {
+        BValue[] inputBValues = {new BString(jwtToken)};
+        BValue[] returns = BRunUtil.invoke(compileResult, "testAuthenticationSuccess", inputBValues);
         Assert.assertTrue(returns[0] instanceof BBoolean);
         Assert.assertTrue(((BBoolean) returns[0]).booleanValue());
     }
@@ -116,4 +182,5 @@ public class JwtSignatureTest {
         modifiersField.setAccessible(false);
         field.setAccessible(false);
     }
+
 }
