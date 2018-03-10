7c3bbfc957592656a4318a8384447b278e85f0db
wggihan <wggihan@gmail.com>
2018-03-10 13:42:55 +0530

    Add error returns for channel creation
    Returns IOError as a second param to handle error situation.

--- stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/socket/OpenSecureSocket.java
+++ stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/socket/OpenSecureSocket.java
@@ -25,13 +25,15 @@ import org.ballerinalang.model.values.BValue;
 import org.ballerinalang.nativeimpl.io.IOConstants;
 import org.ballerinalang.nativeimpl.io.channels.SocketIOChannel;
 import org.ballerinalang.nativeimpl.io.channels.base.Channel;
+import org.ballerinalang.nativeimpl.io.utils.IOUtils;
 import org.ballerinalang.natives.AbstractNativeFunction;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaFunction;
 import org.ballerinalang.natives.annotations.ReturnType;
 import org.ballerinalang.util.codegen.PackageInfo;
 import org.ballerinalang.util.codegen.StructInfo;
-import org.ballerinalang.util.exceptions.BallerinaException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -65,9 +67,13 @@ import javax.net.ssl.TrustManagerFactory;
                  @Argument(name = "port", type = TypeKind.INT),
                  @Argument(name = "option", type = TypeKind.STRUCT, structType = "SocketProperties",
                            structPackage = "ballerina.io") },
-        returnType = { @ReturnType(type = TypeKind.STRUCT, structType = "Socket", structPackage = "ballerina.io") },
+        returnType = {
+                @ReturnType(type = TypeKind.STRUCT, structType = "Socket", structPackage = "ballerina.io"),
+                @ReturnType(type = TypeKind.STRUCT, structType = "IOError", structPackage = "ballerina.io") },
         isPublic = true)
 public class OpenSecureSocket extends AbstractNativeFunction {
+
+    private static final Logger log = LoggerFactory.getLogger(OpenSecureSocket.class);
 
     private static final String SEPARATOR = ",";
     private static final String SOCKET_PACKAGE = "ballerina.io";
@@ -78,42 +84,55 @@ public class OpenSecureSocket extends AbstractNativeFunction {
     public BValue[] execute(Context context) {
         final String host = getStringArgument(context, 0);
         final int port = (int) getIntArgument(context, 0);
+        if (log.isDebugEnabled()) {
+            log.debug("Remote host: " + host);
+            log.debug("Remote port: " + port);
+        }
         final BStruct options = (BStruct) getRefArgument(context, 0);
         Socket socket;
+        BStruct socketStruct;
         try {
             SSLContext sslContext = getSslContext(options);
             SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
             socket = sslSocketFactory.createSocket(host, port);
-        } catch (Throwable e) {
-            String msg = "Failed to open a connection to [" + host + ":" + port + "] : " + e.getMessage();
-            throw new BallerinaException(msg, e, context);
-        }
-        SSLSocket sslSocket;
-        ByteChannel channel;
-        BStruct socketStruct;
-        try {
+            if (log.isDebugEnabled()) {
+                log.debug("Socket connected: " + socket.isConnected());
+                log.debug("Local port: " + socket.getLocalPort());
+            }
+            SSLSocket sslSocket;
+            ByteChannel channel;
             if (socket instanceof SSLSocket) {
                 sslSocket = (SSLSocket) socket;
                 sslSocket.setUseClientMode(true);
             } else {
+                log.error("Socket is not a SSLSocket instance.");
                 throw new RuntimeException("Failed to get a SSLSocket instance.");
             }
             String ciphers = options.getStringField(SocketConstants.CIPHERS_OPTION_FIELD_INDEX);
             if (ciphers != null && !ciphers.isEmpty()) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Setting ciphers: " + ciphers);
+                }
                 sslSocket.setEnabledCipherSuites(ciphers.replaceAll("\\s+", "").split(SEPARATOR));
             }
             String enabledProtocols = options.getStringField(SocketConstants.SSL_ENABLED_PROTOCOLS_OPTION_FIELD_INDEX);
             if (enabledProtocols != null && !enabledProtocols.isEmpty()) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Enabled protocols: " + enabledProtocols);
+                }
                 sslSocket.setEnabledProtocols(enabledProtocols.replaceAll("\\s+", "").split(SEPARATOR));
             }
+            log.debug("Start handshake!!!");
             sslSocket.startHandshake();
+            log.debug("Secure handshake successful.");
             channel = new SocketByteChannel(sslSocket.getInputStream(), sslSocket.getOutputStream());
             socketStruct = createReturnStruct(context, sslSocket, channel);
         } catch (Throwable e) {
-            String msg = "Failed to start handshake with remote server [" + host + ":" + port + "] : " + e.getMessage();
-            throw new BallerinaException(msg, e, context);
+            String msg = "Failed to open a connection to [" + host + ":" + port + "] : " + e.getMessage();
+            log.error(msg, e);
+            return getBValues(null, IOUtils.createError(context, msg));
         }
-        return getBValues(socketStruct);
+        return getBValues(socketStruct, null);
     }
 
     private BStruct createReturnStruct(Context context, SSLSocket sslSocket, ByteChannel channel) throws IOException {
@@ -143,6 +162,9 @@ public class OpenSecureSocket extends AbstractNativeFunction {
         if (algorithm == null) {
             algorithm = "SunX509";
         }
+        if (log.isDebugEnabled()) {
+            log.debug("SSL algorithm : " + algorithm);
+        }
         String keyStorePath = options.getStringField(SocketConstants.KEY_STORE_OPTION_FIELD_INDEX);
         String keyStorePass = options.getStringField(SocketConstants.KEY_STORE_PASS_OPTION_FIELD_INDEX);
         String trustStorePath = options.getStringField(SocketConstants.TRUST_STORE_OPTION_FIELD_INDEX);
@@ -152,6 +174,11 @@ public class OpenSecureSocket extends AbstractNativeFunction {
         String sslProtocol = (protocol == null || protocol.isEmpty()) ?
                 SocketConstants.DEFAULT_SSL_PROTOCOL :
                 options.getStringField(SocketConstants.SSL_PROTOCOL_OPTION_FIELD_INDEX);
+        if (log.isDebugEnabled()) {
+            log.debug("KeyStore path: " + keyStorePath);
+            log.debug("TrustStore path: " + trustStorePath);
+            log.debug("Protocol: " + sslProtocol);
+        }
         KeyManager[] keyManagers = getKeyManagers(certPassword, keyStorePath, keyStorePass, algorithm);
         TrustManager[] trustManagers = getTrustManagers(trustStorePath, trustStorePass, algorithm);
         SSLContext sslContext = SSLContext.getInstance(sslProtocol);
