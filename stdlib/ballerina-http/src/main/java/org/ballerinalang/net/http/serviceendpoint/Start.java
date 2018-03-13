9f62260b245ac3c7065137032dafcc69879921a7
Pubudu Fernando <pubuduf@wso2.com>
2018-03-14 00:09:12 +0530

    Fix typo in BLangConnectorSPIUtil

--- stdlib/ballerina-http/src/main/java/org/ballerinalang/net/http/serviceendpoint/Start.java
+++ stdlib/ballerina-http/src/main/java/org/ballerinalang/net/http/serviceendpoint/Start.java
@@ -73,7 +73,7 @@ public class Start extends AbstractHttpNativeFunction {
         context.setReturnValues();
     }
 
-    public boolean isHTTPTraceLoggerEnabled() {
+    private boolean isHTTPTraceLoggerEnabled() {
         // TODO: Take a closer look at this since looking up from the Config Registry here caused test failures
         return ((BLogManager) LogManager.getLogManager()).getPackageLogLevel(
                 org.ballerinalang.logging.util.Constants.HTTP_TRACE_LOG) == BLogLevel.TRACE;
