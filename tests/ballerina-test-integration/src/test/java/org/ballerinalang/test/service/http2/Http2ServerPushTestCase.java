704bf7ed0cfe0f9770b680d3fcb2ff437f9b928a
isudana <isudana@gmail.com>
2018-03-13 12:15:22 +0530

    Fix minor formatting issues

--- tests/ballerina-test-integration/src/test/java/org/ballerinalang/test/service/http2/Http2ServerPushTestCase.java
+++ tests/ballerina-test-integration/src/test/java/org/ballerinalang/test/service/http2/Http2ServerPushTestCase.java
@@ -1,5 +1,5 @@
 /*
-*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 *
 *  WSO2 Inc. licenses this file to you under the Apache License,
 *  Version 2.0 (the "License"); you may not use this file except
@@ -31,7 +31,6 @@ import java.io.IOException;
 
 /**
  * Test case for HTTP/2.0 server push scenario.
- *
  */
 public class Http2ServerPushTestCase extends IntegrationTestCase {
     private ServerInstance ballerinaServer;
