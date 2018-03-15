f619d3021159f81ccf794c725afe2f884fc33190
anupama-pathirage <anupama@wso2.com>
2018-03-15 13:10:21 +0530

    Add test case

--- tests/ballerina-test/src/test/java/org/ballerinalang/test/types/table/TableLiteralTest.java
+++ tests/ballerina-test/src/test/java/org/ballerinalang/test/types/table/TableLiteralTest.java
@@ -298,6 +298,16 @@ public class TableLiteralTest {
                 + "\"name\":\"john\",\"married\":false}]");
     }
 
+    @Test(priority = 1)
+    public void testTableAddAndAccess() {
+        BValue[] returns = BRunUtil.invoke(result, "testTableAddAndAccess");
+        Assert.assertEquals((returns[0]).stringValue(), "[{\"id\":1,\"age\":35,\"salary\":300.5,\"name\":\"jane\","
+                + "\"married\":true},{\"id\":2,\"age\":40,\"salary\":200.5,\"name\":\"martin\",\"married\":true}]");
+        Assert.assertEquals((returns[1]).stringValue(), "[{\"id\":1,\"age\":35,\"salary\":300.5,\"name\":\"jane\","
+                + "\"married\":true},{\"id\":2,\"age\":40,\"salary\":200.5,\"name\":\"martin\",\"married\":true},"
+                + "{\"id\":3,\"age\":42,\"salary\":100.5,\"name\":\"john\",\"married\":false}]");
+    }
+
     @Test(priority = 1,
           description = "Test struct with any typed field",
           expectedExceptions = { BLangRuntimeException.class },
