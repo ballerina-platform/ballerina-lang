10773b1f91a201fa4bc1bd3844268570ee361360
NatashaWso2 <nwijesekare@gmail.com>
2018-03-18 20:36:43 +0530

    Remove unused methods

--- bvm/ballerina-core/src/main/java/org/ballerinalang/packaging/ExecutorUtils.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/packaging/ExecutorUtils.java
@@ -18,24 +18,8 @@
 package org.wso2.ballerinalang.util;
 
 import org.ballerinalang.BLangProgramRunner;
-import org.ballerinalang.bre.Context;
-import org.ballerinalang.bre.bvm.BLangVM;
-import org.ballerinalang.bre.bvm.BLangVMErrors;
-import org.ballerinalang.bre.bvm.ControlStack;
-import org.ballerinalang.bre.bvm.StackFrame;
-import org.ballerinalang.model.values.BRefType;
-import org.ballerinalang.model.values.BStringArray;
-import org.ballerinalang.runtime.threadpool.ThreadPoolFactory;
-import org.ballerinalang.util.codegen.FunctionInfo;
-import org.ballerinalang.util.codegen.PackageInfo;
 import org.ballerinalang.util.codegen.ProgramFile;
 import org.ballerinalang.util.codegen.ProgramFileReader;
-import org.ballerinalang.util.codegen.WorkerInfo;
-import org.ballerinalang.util.debugger.DebugContext;
-import org.ballerinalang.util.debugger.Debugger;
-import org.ballerinalang.util.exceptions.BLangRuntimeException;
-import org.ballerinalang.util.exceptions.BallerinaException;
-import org.ballerinalang.util.program.BLangFunctions;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -46,7 +30,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Util class for packaging both pull and push.
@@ -58,121 +41,10 @@ public class ExecutorUtils {
     private static boolean packageInit = false;
 
     /**
-     * Invoke function.
+     * Run balx that lives within jars.
      *
-     * @param programFile bal program file
-     * @param methodName  method to be invoked
-     * @param args        arguments passed to the method
-     */
-    private static void invokeFunction(ProgramFile programFile, String methodName, String[] args) {
-        runFunction(programFile, methodName, args);
-        try {
-            ThreadPoolFactory.getInstance().getWorkerExecutor().shutdown();
-            ThreadPoolFactory.getInstance().getWorkerExecutor().awaitTermination(10000, TimeUnit.MILLISECONDS);
-        } catch (InterruptedException ignore) {
-            // Ignore the error
-        }
-        Runtime.getRuntime().exit(0);
-    }
-
-    /**
-     * Invoke package init.
-     *
-     * @param programFile program file
-     * @param packageInfo package info object
-     * @param bContext    context object
-     */
-    private static void initPackage(ProgramFile programFile, PackageInfo packageInfo, Context bContext) {
-        if (!packageInit) {
-            BLangFunctions.invokePackageInitFunction(programFile, packageInfo.getInitFunctionInfo(), bContext);
-            packageInit = true;
-        }
-    }
-
-    /**
-     * Run function from the bal file.
-     *
-     * @param programFile bal program file
-     * @param methodName  method to be invoked
-     * @param args        arguments passed to the method
-     */
-    private static void runFunction(ProgramFile programFile, String methodName, String[] args) {
-        PackageInfo pkgInfo = programFile.getEntryPackage();
-        Context bContext = new Context(programFile);
-
-        Debugger debugger = new Debugger(programFile);
-        initDebugger(bContext, debugger);
-        FunctionInfo mainFuncInfo = getFunctionInfo(pkgInfo, methodName);
-        initPackage(programFile, pkgInfo, bContext);
-
-        // Prepare function arguments
-        BStringArray arrayArgs = new BStringArray();
-        for (int i = 0; i < args.length; i++) {
-            arrayArgs.add(i, args[i]);
-        }
-
-        WorkerInfo defaultWorkerInfo = mainFuncInfo.getDefaultWorkerInfo();
-
-        // Execute workers
-        StackFrame callerSF = new StackFrame(pkgInfo, -1, new int[0]);
-        callerSF.setRefRegs(new BRefType[1]);
-        callerSF.getRefRegs()[0] = arrayArgs;
-
-        StackFrame stackFrame = new StackFrame(mainFuncInfo, defaultWorkerInfo, -1, new int[0]);
-        stackFrame.getRefRegs()[0] = arrayArgs;
-        ControlStack controlStack = bContext.getControlStack();
-        controlStack.pushFrame(stackFrame);
-        bContext.startTrackWorker();
-        bContext.setStartIP(defaultWorkerInfo.getCodeAttributeInfo().getCodeAddrs());
-
-        BLangVM bLangVM = new BLangVM(programFile);
-        bLangVM.run(bContext);
-        bContext.await();
-        if (debugger.isDebugEnabled()) {
-            debugger.notifyExit();
-        }
-        if (bContext.getError() != null) {
-            String stackTraceStr = BLangVMErrors.getPrintableStackTrace(bContext.getError());
-            throw new BLangRuntimeException("error: " + stackTraceStr);
-        }
-    }
-
-    /**
-     * Init debugger context.
-     *
-     * @param bContext context
-     * @param debugger debugger
-     */
-    private static void initDebugger(Context bContext, Debugger debugger) {
-        bContext.getProgramFile().setDebugger(debugger);
-        if (debugger.isDebugEnabled()) {
-            DebugContext debugContext = new DebugContext();
-            bContext.setDebugContext(debugContext);
-            debugger.init();
-            debugger.addDebugContextAndWait(debugContext);
-        }
-    }
-
-    /**
-     * Get function info.
-     *
-     * @param pkgInfo    package in which the function exists
-     * @param methodName name of the method to be invoked
-     * @return functionInfo object
-     */
-    private static FunctionInfo getFunctionInfo(PackageInfo pkgInfo, String methodName) {
-        String errorMsg = "function not found in  '" +
-                pkgInfo.getProgramFile().getProgramFilePath() + "'";
-
-        FunctionInfo mainFuncInfo = pkgInfo.getFunctionInfo(methodName);
-        if (mainFuncInfo == null) {
-            throw new BallerinaException(errorMsg);
-        }
-        return mainFuncInfo;
-    }
-
-    /**
-     * Run balx that lives within jars
+     * @param balxResource URI of the balx resource
+     * @param args         arguments passed to the function
      */
     public static void execute(URI balxResource, String... args) {
         initFileSystem(balxResource);
