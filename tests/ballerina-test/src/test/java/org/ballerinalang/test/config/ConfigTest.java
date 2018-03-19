4d2682a1f3129f98765a4382c328479c9ef889cc
Pubudu Fernando <pubuduf@wso2.com>
2018-03-19 11:55:52 +0530

    Disable configuring services test in ConfigTest

--- tests/ballerina-test/src/test/java/org/ballerinalang/test/config/ConfigTest.java
+++ tests/ballerina-test/src/test/java/org/ballerinalang/test/config/ConfigTest.java
@@ -223,7 +223,7 @@ public class ConfigTest {
         Assert.assertTrue(((BBoolean) returnVals[0]).booleanValue());
     }
 
-    @Test(description = "Test for configuring a service")
+    @Test(description = "Test for configuring a service", enabled = false)
     public void testConfiguringAService() throws IOException {
         registry.initRegistry(null, Paths.get(resourceRoot, "datafiles", "config", "service-config.conf").toString(),
                               null);
