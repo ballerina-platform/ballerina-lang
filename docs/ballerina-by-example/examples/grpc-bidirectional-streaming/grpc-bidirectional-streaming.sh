e2b3e36bdb5dbb2b3a1a3e4e1fdf0dcd08469b60
Yasara Dissanayake <yasaradiss101@gmail.com>
2018-03-19 16:05:05 +0530

    Remove bidirection sample until fix

--- docs/ballerina-by-example/examples/grpc-bidirectional-streaming/grpc-bidirectional-streaming.sh
+++
@@ -1,6 +0,0 @@
-#Run the service
-$ ballerina run grpc-bidirectional-streaming.bal
-ballerina: deploying service(s) in 'grpc-bidirectional-streaming.bal'
-
-#Run client connector
-ballerina run client
