c83ee32f4f91c0ea5c303a9584db8fa175eceba8
Yasara Dissanayake <yasaradiss101@gmail.com>
2018-03-26 20:03:50 +0530

    Add unit test for standard data type.

--- tests/ballerina-test/src/test/java/org/ballerinalang/test/net/grpc/BalGenToolTest.java
+++ tests/ballerina-test/src/test/java/org/ballerinalang/test/net/grpc/BalGenToolTest.java
@@ -37,6 +37,7 @@ import java.util.Scanner;
  * Protobuff to bal generation function testcase.
  */
 public class BalGenToolTest {
+    private static final String PACKAGE_NAME = ".";
     private static Path resourceDir = Paths.get(
             BalGenToolTest.class.getProtectionDomain().getCodeSource().getLocation().getPath());
     
@@ -56,25 +57,25 @@ public class BalGenToolTest {
         Path destFileRoot = resourceDir.resolve(Paths.get("protoFiles/helloWorld.gen.pb.bal"));
         removePackage(sourceFileRoot.toString(), destFileRoot.toString());
         CompileResult compileResult = BCompileUtil.compile(destFileRoot.toString());
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getStructInfo("helloWorldClient"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getStructInfo("helloWorldBlockingClient"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getStructInfo("helloWorldBlockingStub"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getFunctionInfo("helloWorldBlockingStub.hello"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getFunctionInfo("helloWorldStub.hello"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getFunctionInfo("helloWorldBlockingStub.bye"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getFunctionInfo("helloWorldStub.bye"),
                 "Connector not found.");
         String protoExeName = "protoc-" + OSDetector.getDetectedClassifier() + ".exe";
@@ -97,13 +98,13 @@ public class BalGenToolTest {
         Path destFileRoot = resourceDir.resolve(Paths.get("protoFiles/helloWorldClientStreaming.gen.pb.bal"));
         removePackage(sourceFileRoot.toString(), destFileRoot.toString());
         CompileResult compileResult = BCompileUtil.compile(destFileRoot.toString());
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getStructInfo("helloWorldClientStreamingClient"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getStructInfo("helloWorldClientStreamingStub"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getFunctionInfo("helloWorldClientStreamingStub.LotsOfGreetings"),
                 "Connector not found.");
         String protoExeName = "protoc-" + OSDetector.getDetectedClassifier() + ".exe";
@@ -126,14 +127,55 @@ public class BalGenToolTest {
         Path destFileRoot = resourceDir.resolve(Paths.get("protoFiles/helloWorldServerStreaming.gen.pb.bal"));
         removePackage(sourceFileRoot.toString(), destFileRoot.toString());
         CompileResult compileResult = BCompileUtil.compile(destFileRoot.toString());
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getStructInfo("helloWorldServerStreamingClient"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getStructInfo("helloWorldServerStreamingStub"),
                 "Connector not found.");
-        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(".")
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
                         .getFunctionInfo("helloWorldServerStreamingStub.lotsOfReplies"),
+                "Connector not found.");
+        String protoExeName = "protoc-" + OSDetector.getDetectedClassifier() + ".exe";
+        BalFileGenerationUtils.delete(new File(protoExeName));
+    }
+    
+    @Test
+    public void testStandardDataTypes() throws IllegalAccessException,
+            ClassNotFoundException, InstantiationException, IOException {
+        Class<?> grpcCmd = Class.forName("org.ballerinalang.protobuf.cmd.GrpcCmd");
+        GrpcCmd grpcCmd1 = (GrpcCmd) grpcCmd.newInstance();
+        Path sourcePath = Paths.get("protoFiles");
+        Path sourceRoot = resourceDir.resolve(sourcePath);
+        Path protoPath = Paths.get("protoFiles/helloWorldString.proto");
+        Path protoRoot = resourceDir.resolve(protoPath);
+        grpcCmd1.setBalOutPath(sourceRoot.toString());
+        grpcCmd1.setProtoPath(protoRoot.toString());
+        grpcCmd1.execute();
+        Path sourceFileRoot = resourceDir.resolve(Paths.get("protoFiles/helloWorld.pb.bal"));
+        Path destFileRoot = resourceDir.resolve(Paths.get("protoFiles/helloWorld.gen.pb.bal"));
+        removePackage(sourceFileRoot.toString(), destFileRoot.toString());
+        CompileResult compileResult = BCompileUtil.compile(destFileRoot.toString());
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
+                        .getStructInfo("helloWorldClient"),
+                "Connector not found.");
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
+                        .getStructInfo("helloWorldBlockingClient"),
+                "Connector not found.");
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
+                        .getStructInfo("helloWorldBlockingStub"),
+                "Connector not found.");
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
+                        .getFunctionInfo("helloWorldBlockingStub.hello"),
+                "Connector not found.");
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
+                        .getFunctionInfo("helloWorldStub.hello"),
+                "Connector not found.");
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
+                        .getFunctionInfo("helloWorldBlockingStub.bye"),
+                "Connector not found.");
+        Assert.assertNotNull(compileResult.getProgFile().getPackageInfo(PACKAGE_NAME)
+                        .getFunctionInfo("helloWorldStub.bye"),
                 "Connector not found.");
         String protoExeName = "protoc-" + OSDetector.getDetectedClassifier() + ".exe";
         BalFileGenerationUtils.delete(new File(protoExeName));
