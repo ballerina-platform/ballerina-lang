a653bb8e9a2e5176d59d724242a8125dee46473c
chamil321 <chamilmadawa8@gmail.com>
2018-03-27 10:58:44 +0530

    Change deploying service to initiating service

--- docs/ballerina-by-example/examples/http-load-balancer/http-load-balancer.server.sh
+++ docs/ballerina-by-example/examples/http-load-balancer/http-load-balancer.server.sh
@@ -2,5 +2,5 @@
 # and execute the following command from `$BALLERINA_HOME/bin`.
 $ ballerina run http-load-balancer.bal
 # Service deployment:
-ballerina: deploying service(s) in 'http-load-balancer.bal'
+ballerina: initiating service(s) in 'http-load-balancer.bal'
 ballerina: started HTTP/WS server connector 0.0.0.0:9090
