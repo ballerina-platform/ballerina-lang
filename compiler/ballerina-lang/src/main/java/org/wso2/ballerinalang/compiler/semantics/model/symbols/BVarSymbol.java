c9f2926fbb0cff7d5ef33642a98ac74757f66cae
Rajith <lanka.vitharana@gmail.com>
2018-04-01 10:05:05 +0530

    Fix findbugs issue

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/model/symbols/BVarSymbol.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/model/symbols/BVarSymbol.java
@@ -32,7 +32,7 @@ import static org.wso2.ballerinalang.compiler.semantics.model.symbols.SymTag.VAR
 public class BVarSymbol extends BSymbol implements VariableSymbol {
 
     public boolean field;
-    public Name originalName;
+    public Name originalName = null;
     /**
      * Represent the index of the variable in a memory block of the VM.
      */
