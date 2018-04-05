04ee52d2454be6ee36a74f2a003e8c6deaf8c75c
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2018-04-05 14:50:28 +0530

    Add missing copyright headers

--- stdlib/ballerina-jms/src/main/java/org/ballerinalang/net/jms/Constants.java
+++ stdlib/ballerina-jms/src/main/java/org/ballerinalang/net/jms/Constants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -11,9 +11,10 @@
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
  */
 
 package org.ballerinalang.net.jms;
