a58ed6c2d4e06350c07d8656362d1f00773103e9
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2018-04-06 10:11:53 +0530

    Rename JMS queue consumer/producer samples

--- docs/ballerina-by-example/examples/jms-queue-message-consumer/jms-queue-message-consumer.sh
+++ docs/ballerina-by-example/examples/jms-queue-message-consumer/jms-queue-message-consumer.sh
@@ -1,6 +1,6 @@
-# To run the program, put the code in `jms-message-consumer.bal`
+# To run the program, put the code in `jms-queue-message-consumer.bal`
 # and use `$BALLERINA_HOME/bin/ballerina`.
-$ ballerina run jms-message-consumer.bal
+$ ballerina run jms-queue-message-consumer.bal
 
 #JMS subscriber run as a Ballerina service listening on the subscribed queue
 # or topic
---
+++ docs/ballerina-by-example/examples/jms-queue-message-producer/jms-queue-message-producer.sh
@@ -0,0 +1,3 @@
+# To run the program, put the code in `jms-queue-message-producer.bal`
+# and use `$BALLERINA_HOME/bin/ballerina`.
+$ ballerina run jms-queue-message-producer.bal
