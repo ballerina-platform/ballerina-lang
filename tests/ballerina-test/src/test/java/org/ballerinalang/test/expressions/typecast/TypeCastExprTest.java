e8b29c79ef2526dd9e486fb66c536ecbd58d8f84
Pubudu Fernando <pubuduf@wso2.com>
2018-04-07 15:21:24 +0530

    Remove outdated test

--- tests/ballerina-test/src/test/java/org/ballerinalang/test/expressions/typecast/TypeCastExprTest.java
+++ tests/ballerina-test/src/test/java/org/ballerinalang/test/expressions/typecast/TypeCastExprTest.java
@@ -604,13 +604,6 @@ public class TypeCastExprTest {
         Assert.assertEquals(errorMsg, "'B' cannot be cast to 'A'");
     }
 
-    @Test(description = "Test casting with too many returns")
-    public void testCastingWithTooManyReturns() {
-        CompileResult res = BCompileUtil.compile("test-src/expressions/typecast/cast-too-many-returns-negative.bal");
-        Assert.assertEquals(res.getErrorCount(), 1);
-        BAssertUtil.validateError(res, 0, "invalid token ','", 15, 6);
-    }
-
     @Test (description = "Test any to int casting happens without errors, error struct should be null")
     public void testAnyToIntWithoutErrors() {
         BValue[] returns = BRunUtil.invoke(result, "testAnyToIntWithoutErrors");
