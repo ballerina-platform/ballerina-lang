5876cf25b1c4248e5ee6605ddda956c10b95ad29
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2018-04-08 20:49:21 +0530

    Implement simple queue consumer endpoint

---
+++ examples/jms-simple-queue-message-consumer/jms-simple-queue-message-consumer.sh
@@ -0,0 +1,6 @@
+# To run the program, put the code in `jms-simple-queue-message-consumer.bal`
+# and use `$BALLERINA_HOME/bin/ballerina`.
+$ ballerina run jms-simple-queue-message-consumer.bal
+
+#JMS subscriber run as a Ballerina service listening on the subscribed queue
+# or topic
