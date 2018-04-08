4325ce349f0933781a6b636e641689c05783e363
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2018-04-08 21:58:54 +0530

    Rename SimpleQueueListener

--- examples/jms-simple-queue-message-listener/jms-simple-queue-message-listener.sh
+++ examples/jms-simple-queue-message-listener/jms-simple-queue-message-listener.sh
@@ -1,6 +1,6 @@
-# To run the program, put the code in `jms-simple-queue-message-consumer.bal`
+# To run the program, put the code in `jms-simple-queue-message-listener.bal`
 # and use `$BALLERINA_HOME/bin/ballerina`.
-$ ballerina run jms-simple-queue-message-consumer.bal
+$ ballerina run jms-simple-queue-message-listener.bal
 
 #JMS subscriber run as a Ballerina service listening on the subscribed queue
 # or topic
