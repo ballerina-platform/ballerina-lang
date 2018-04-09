259752e4ac0381f1021d97845b02c7c523032a5f
wggihan <wggihan@gmail.com>
2018-04-09 13:19:47 +0530

    Update ballerina-by-example for directory listener

--- examples/file-system-server-connector/file-system-server-connector.sh
+++
@@ -1,6 +0,0 @@
-# When the user create a new file called test1.txt in observed-dir and delete it.
-$ ballerina run file-system-server-connector.bal
-ballerina: initiating service(s) in 'file-system-server-connector.bal'
-Create: /home/ballerina/fs-server-connector/observed-dir/test1.txt
-Modify: /home/ballerina/fs-server-connector/observed-dir/test2.txt
-Delete: /home/ballerina/fs-server-connector/observed-dir/test3.txt
