4a6d9bd5b3e9a9fbee870dd2bd276121ead5cc90
chamil321 <chamilmadawa8@gmail.com>
2018-04-09 17:31:15 +0530

    Update packageId with manifest package version

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/SourceDirectoryManager.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/SourceDirectoryManager.java
@@ -19,8 +19,11 @@ package org.wso2.ballerinalang.compiler;
 
 import org.ballerinalang.compiler.CompilerOptionName;
 import org.ballerinalang.model.elements.PackageID;
+import org.ballerinalang.toml.model.Manifest;
+import org.ballerinalang.toml.parser.ManifestProcessor;
 import org.wso2.ballerinalang.compiler.util.CompilerContext;
 import org.wso2.ballerinalang.compiler.util.CompilerOptions;
+import org.wso2.ballerinalang.compiler.util.Name;
 import org.wso2.ballerinalang.compiler.util.Names;
 
 import java.nio.file.Path;
@@ -61,10 +64,24 @@ public class SourceDirectoryManager {
     public Stream<PackageID> listSourceFilesAndPackages() {
         List<String> sourceFileNames = this.sourceDirectory.getSourceFileNames();
         List<String> packageNames = this.sourceDirectory.getSourcePackageNames();
+        Manifest manifest = getManifest();
         return Stream.concat(sourceFileNames.stream().map(PackageID::new),
                              packageNames.stream().map(name -> new PackageID(Names.ANON_ORG,
                                                                              names.fromString(name),
-                                                                             Names.DEFAULT_VERSION)));
+                                                                             new Name(manifest.getVersion()))));
+    }
+
+    private Manifest getManifest() {
+        Manifest manifest;
+        if (sourceDirectory.getManifestContent() == null) {
+            manifest = new Manifest();
+        } else {
+            manifest = ManifestProcessor.parseTomlContentAsStream(sourceDirectory.getManifestContent());
+        }
+        if (manifest.getVersion() == null) {
+            manifest.setVersion(Names.DEFAULT_VERSION.getValue());
+        }
+        return manifest;
     }
 
     public Stream<PackageID> listPackages() {
@@ -81,7 +98,8 @@ public class SourceDirectoryManager {
 
         List<String> packageNames = this.sourceDirectory.getSourcePackageNames();
         if (packageNames.contains(sourcePackage)) {
-            return new PackageID(Names.ANON_ORG, names.fromString(sourcePackage), Names.DEFAULT_VERSION);
+            Manifest manifest = getManifest();
+            return new PackageID(Names.ANON_ORG, names.fromString(sourcePackage), new Name(manifest.getVersion()));
         }
 
         return null;
