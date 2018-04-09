9a72be242636f5ca9b753a280dc6d938f6a68a27
bhashinee <bhashineen@gmail.com>
2018-04-09 14:15:22 +0530

    Change server conector to endpoint
    Change the console output saying "ballerina: started HTTPS/WSS server connector" to "ballerina: started HTTPS/WSS endpoint".

--- docs/ballerina-by-example/examples/secured-hello-world-service-with-basic-auth/secured-hello-world-service-with-basic-auth.server.sh
+++ docs/ballerina-by-example/examples/secured-hello-world-service-with-basic-auth/secured-hello-world-service-with-basic-auth.server.sh
@@ -95,7 +95,7 @@ $ bash permissionstore-generator.sh -s {scope name} -g {comma separated groups}
 # same directory as the secured-hello-world-service-with-basic-auth.bal file.
 $ ballerina run secured-hello-world-service-with-basic-auth.bal
 ballerina: initiating service(s) in 'secured-hello-world-service-with-basic-auth.bal'
-ballerina: started HTTPS/WSS server connector 0.0.0.0:9090
+ballerina: started HTTPS/WSS endpoint 0.0.0.0:9090
 
 # To build a compiled program file, we can use the
 # "ballerina build" command followed by
