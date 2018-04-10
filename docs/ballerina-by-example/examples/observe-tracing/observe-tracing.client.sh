48192975e69985411b841d89a85f930584502009
Anoukh <anoukh.ashley@gmail.com>
2018-04-10 09:35:26 +0530

    Refactor observability tracing BBE

--- docs/ballerina-by-example/examples/observe-tracing/observe-tracing.client.sh
+++ docs/ballerina-by-example/examples/observe-tracing/observe-tracing.client.sh
@@ -1,1 +1,6 @@
 $ curl -X GET http://localhost:9090/store/add
+# Server response:
+Item Added Successfully
+# The trace can be viewed in Jaeger UI. The default url is: http://localhost:16686
+# To observe an error trace, change the `clientEndpoint` in line 10 of `observe-tracing.bal`
+# to "http://localhost:9091" and resend curl command
