f8c9d758b415e669a56fd43aa63f79f655c09fc0
Anoukh <anoukh.ashley@gmail.com>
2018-04-11 10:20:41 +0530

    Refactor observe tracing BBE

--- docs/ballerina-by-example/examples/observe-tracing/observe-tracing.server.sh
+++ docs/ballerina-by-example/examples/observe-tracing/observe-tracing.server.sh
@@ -1,4 +1,6 @@
 $ ballerina run observe-tracing.bal --observe
 # Service deployment:
+ballerina: started Prometheus HTTP endpoint 0.0.0.0/0.0.0.0:9797
+ballerina: started publishing tracers to Jaeger on 0.0.0.0:5775
 ballerina: initiating service(s) in 'observe-tracing.bal'
-ballerina: started HTTP/WS server connector 0.0.0.0:9090
+ballerina: started HTTP/WS endpoint 0.0.0.0:9090
