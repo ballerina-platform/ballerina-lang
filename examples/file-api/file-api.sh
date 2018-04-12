8d5534b96f06139e9d0b4b8656c4e719291882af
pamod <pamod@live.com>
2018-04-12 11:05:47 +0530

    Add path sample

--- examples/file-api/file-api.sh
+++ examples/file-api/file-api.sh
@@ -1,14 +1,8 @@
 $ ballerina run file-api.bal
-file exists: true
-file copied: /tmp/result.txt to /tmp/copy.txt
-file deleted: /tmp/copy.txt
-file moved: /tmp/result.txt to /tmp/move.txt
-file deleted: /tmp/move.txt
-file is a directory: true
-{path:"/tmp/dir/abc/file3.txt"}
-{path:"/tmp/dir/abc/file2.txt"}
-{path:"/tmp/dir/abc/file1.txt"}
-{path:"/tmp/dir/abc/def"}
-file1.txt modified at: 1509125400000
-file1.txt is readable: true
-file1.txt is writable: true
+File exists in {path}/tmp/src/test.txt
+Iterating through directory content
+./tmp/dst
+./tmp/src
+File Details, test.txt yyyy-mm-dd
+Folder deleted successfully
+
