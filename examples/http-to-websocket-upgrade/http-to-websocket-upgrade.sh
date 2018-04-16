3ce0216cdd59d92a7c36e42e3fe06b8a379b455a
Nirdesha <nirdesha@wso2.com>
2018-04-16 16:53:54 +0530

    Update http-to-websocket-upgrade.sh
    Changed code comments.

--- examples/http-to-websocket-upgrade/http-to-websocket-upgrade.sh
+++ examples/http-to-websocket-upgrade/http-to-websocket-upgrade.sh
@@ -1,15 +1,15 @@
 # To run the program, put the code in `http-to-websocket-upgrade.bal`
-# and use `ballerina run http-to-websocket-upgrade.bal` command.
+# and use the `ballerina run http-to-websocket-upgrade.bal` command.
 $ ballerina run http-to-websocket-upgrade.bal
 
-# To check the sample,use Chrome or Firefox javascript console and run the below commands <br>
-# change "xml" to another sub protocol to observe the behavior of WebSocket server.
-# There are two endpoints for this WebSocket sample since it is configured that way.
+# To check the sample, use the Chrome or Firefox javascript console and run the below commands. <br>
+# Change "xml" to another sub protocol to observe the behavior of the WebSocket server.
+# There are two endpoints for this WebSocket sample as it is configured that way.
 $ var ws = new WebSocket("ws://localhost:9090/hello/ws", "xml", "my-protocol");
 $ var ws = new WebSocket("ws://localhost:9090/world/ws", "xml", "my-protocol");
 
 $ ws.onmessage = function(frame) {console.log(frame.data)};
 $ ws.onclose = function(frame) {console.log(frame)};
 
-# To send messages
+# Send messages.
 $ ws.send("hello world");
