1268310fc95219b1c694ea557675d1654b1bb9f9
sherene <sherene@wso2.com>
2018-04-16 11:42:50 +0530

    Update xml-namespaces.sh

--- examples/xml-namespaces/xml-namespaces.sh
+++ examples/xml-namespaces/xml-namespaces.sh
@@ -1,3 +1,5 @@
+#At the command line, navigate to the directory that contains the 
+#`.bal` file and run the `ballerina run` command. 
 $ ballerina run xml-namespaces.bal
 {http://ballerina.com/aa}foo
 {http://ballerina.com/updated}foo
