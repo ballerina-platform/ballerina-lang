4f1eb62712a1ca953594fff520e769d40727c4ff
sherene <sherene@wso2.com>
2018-04-16 11:30:13 +0530

    Update xml-literal.sh

--- examples/xml-literal/xml-literal.sh
+++ examples/xml-literal/xml-literal.sh
@@ -1,3 +1,5 @@
+#At the command line, navigate to the directory that contains the 
+#`.bal` file and run the `ballerina run` command. 
 $ ballerina run xml-literal.bal
 <book>
                     <name>Sherlock Holmes</name>
