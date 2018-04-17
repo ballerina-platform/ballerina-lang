41cef85f0cfd1757921eb97a6bde212ca40f1783
gimantha <gmnth.bndr@gmail.com>
2018-04-17 12:47:35 +0530

    Change license header and the class level comment

--- tests/ballerina-test-integration/src/test/java/org/ballerinalang/test/streaming/StreamsWithinServicesTestCase.java
+++ tests/ballerina-test-integration/src/test/java/org/ballerinalang/test/streaming/StreamsWithinServicesTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -32,7 +32,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Service/Resource dispatchers test class.
+ * This class tests the streaming functionality in a service.
  */
 public class StreamsWithinServicesTestCase extends IntegrationTestCase {
 
