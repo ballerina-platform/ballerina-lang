16f35bb6d30015137556c8ac5137d29ceaa4b52a
NatashaWso2 <nwijesekare@gmail.com>
2018-04-19 17:54:40 +0530

    Refactor resolving packages with the cache repo

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/packaging/repo/CacheRepo.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/packaging/repo/CacheRepo.java
@@ -3,21 +3,29 @@ package org.wso2.ballerinalang.compiler.packaging.repo;
 import org.ballerinalang.model.elements.PackageID;
 import org.wso2.ballerinalang.compiler.packaging.Patten;
 import org.wso2.ballerinalang.compiler.packaging.converters.Converter;
-import org.wso2.ballerinalang.compiler.packaging.converters.PathConverter;
+import org.wso2.ballerinalang.compiler.packaging.converters.ZipConverter;
+import org.wso2.ballerinalang.compiler.util.ProjectDirConstants;
 
 import java.nio.file.Path;
+
+import static org.wso2.ballerinalang.compiler.packaging.Patten.LATEST_VERSION_DIR;
+import static org.wso2.ballerinalang.compiler.packaging.Patten.WILDCARD_SOURCE;
+import static org.wso2.ballerinalang.compiler.packaging.Patten.path;
 
 /**
  * Calculate path pattens for project and home cache.
  */
 public class CacheRepo extends NonSysRepo<Path> {
 
-    public CacheRepo(Converter<Path> converter) {
+    private final String cacheName;
+
+    public CacheRepo(Converter<Path> converter, String cacheName) {
         super(converter);
+        this.cacheName = cacheName;
     }
 
-    public CacheRepo(Path path) {
-        this(new PathConverter(path));
+    public CacheRepo(Path path, String cacheName) {
+        this(new ZipConverter(path), cacheName);
     }
 
     @Override
@@ -25,10 +33,12 @@ public class CacheRepo extends NonSysRepo<Path> {
         String orgName = pkg.getOrgName().value;
         String pkgName = pkg.getName().value;
         String pkgVersion = pkg.version.value;
-
-        return new Patten(Patten.path("caches"),
-                          Patten.LATEST_VERSION_DIR,
-                          Patten.path(orgName, pkgName, pkgVersion, "src"),
-                          Patten.WILDCARD_SOURCE);
+        if (pkgVersion.isEmpty()) {
+            return new Patten(path(ProjectDirConstants.CACHES_DIR_NAME, cacheName, orgName, pkgName),
+                              LATEST_VERSION_DIR, path(pkgName + ".zip"), path("src"), WILDCARD_SOURCE);
+        } else {
+            return new Patten(path(ProjectDirConstants.CACHES_DIR_NAME, cacheName, orgName, pkgName, pkgVersion),
+                              path(pkgName + ".zip"), path("src"), WILDCARD_SOURCE);
+        }
     }
 }
