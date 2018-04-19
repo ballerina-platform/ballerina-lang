03fe72f21715ba49faa6c2dd57331d9eadb7d3ef
Pubudu Fernando <pubuduf@wso2.com>
2018-04-19 10:54:01 +0530

    Merge formatTo -> format and parseTo -> parse

--- stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/time/Format.java
+++ stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/time/Format.java
@@ -1,20 +1,3 @@
-/*
-*   Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
 package org.ballerinalang.nativeimpl.time;
 
 import org.ballerinalang.bre.Context;
@@ -23,28 +6,42 @@ import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BStruct;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaFunction;
+import org.ballerinalang.natives.annotations.Receiver;
 import org.ballerinalang.natives.annotations.ReturnType;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.ballerinalang.nativeimpl.Utils.STRUCT_TYPE_TIME;
 
 /**
  * Convert a Time to string in the given format.
  *
- * @since 0.89
+ * @since 0.970.0-alpha1
  */
 @BallerinaFunction(
         orgName = "ballerina", packageName = "time",
-        functionName = "Time.format",
-        args = {@Argument(name = "time", type = TypeKind.STRUCT, structType = "Time",
-                          structPackage = "ballerina.time"),
-                @Argument(name = "pattern", type = TypeKind.STRING)},
+        functionName = "format",
+        receiver = @Receiver(type = TypeKind.STRUCT, structType = STRUCT_TYPE_TIME, structPackage = "ballerina.time"),
+        args = {@Argument(name = "pattern", type = TypeKind.UNION)},
         returnType = {@ReturnType(type = TypeKind.STRING)},
         isPublic = true
 )
-public class Format extends AbstractTimeFunction {
+public class FormatTo extends AbstractTimeFunction {
 
     @Override
     public void execute(Context context) {
         BStruct timeStruct = ((BStruct) context.getRefArgument(0));
-        String pattern = context.getStringArgument(0);
-        context.setReturnValues(new BString(getFormattedtString(timeStruct, pattern)));
+        BString pattern = (BString) context.getNullableRefArgument(1);
+
+        switch (pattern.stringValue()) {
+            case "RFC_1123":
+                ZonedDateTime zonedDateTime = getZonedDateTime(timeStruct);
+                String formattedDateTime = zonedDateTime.format(DateTimeFormatter.RFC_1123_DATE_TIME);
+                context.setReturnValues(new BString(formattedDateTime));
+                break;
+            default:
+                context.setReturnValues(new BString(getFormattedtString(timeStruct, pattern.stringValue())));
+        }
     }
 }
--- stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/time/Parse.java
+++ stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/time/Parse.java
@@ -1,48 +1,68 @@
-/*
-*   Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
 package org.ballerinalang.nativeimpl.time;
 
 import org.ballerinalang.bre.Context;
 import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BString;
+import org.ballerinalang.model.values.BStruct;
+import org.ballerinalang.nativeimpl.Utils;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaFunction;
 import org.ballerinalang.natives.annotations.ReturnType;
+import org.ballerinalang.util.codegen.StructInfo;
+import org.ballerinalang.util.exceptions.BallerinaException;
+
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 
 /**
  * Get the Time for the given string.
  *
- * @since 0.89
+ * @since 0.970.0-alpha1
  */
 @BallerinaFunction(
         orgName = "ballerina", packageName = "time",
         functionName = "parse",
-        args = {@Argument(name = "dateString", type = TypeKind.STRING),
-                @Argument(name = "pattern", type = TypeKind.STRING)},
-        returnType = {@ReturnType(type = TypeKind.STRUCT, structType = "Time",
-                                  structPackage = "ballerina.time")},
+        args = {@Argument(name = "timestamp", type = TypeKind.STRING),
+                @Argument(name = "format", type = TypeKind.UNION)},
+        returnType = {@ReturnType(type = TypeKind.STRUCT, structType = "Time", structPackage = "ballerina.time")},
         isPublic = true
 )
-public class Parse extends  AbstractTimeFunction {
+public class ParseTo extends AbstractTimeFunction {
 
     @Override
     public void execute(Context context) {
         String dateString = context.getStringArgument(0);
-        String pattern = context.getStringArgument(1);
-        context.setReturnValues(parseTime(context, dateString, pattern));
+        BString pattern = (BString) context.getNullableRefArgument(0);
+
+        TemporalAccessor parsedDateTime;
+        switch (pattern.stringValue()) {
+            case "RFC_1123":
+                parsedDateTime = DateTimeFormatter.RFC_1123_DATE_TIME.parse(dateString);
+                context.setReturnValues(getTimeStruct(parsedDateTime, context, dateString, pattern.stringValue()));
+                break;
+            default:
+                context.setReturnValues(parseTime(context, dateString, pattern.stringValue()));
+        }
+    }
+
+    private BStruct getTimeStruct(TemporalAccessor dateTime, Context context, String dateString, String pattern) {
+        StructInfo timeZoneStructInfo = Utils.getTimeZoneStructInfo(context);
+        StructInfo timeStructInfo = Utils.getTimeStructInfo(context);
+        long epochTime = -1;
+        String zoneId;
+        try {
+            epochTime = Instant.from(dateTime).toEpochMilli();
+            zoneId = String.valueOf(ZoneId.from(dateTime));
+        } catch (DateTimeException e) {
+            if (epochTime < 0) {
+                throw new BallerinaException(
+                        "failed to parse \"" + dateString + "\" to the " + pattern + " format");
+            }
+            zoneId = ZoneId.systemDefault().toString();
+        }
+        return Utils.createTimeStruct(timeZoneStructInfo, timeStructInfo, epochTime, zoneId);
     }
 }
