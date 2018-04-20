6e25ca5bb0d9d8ee87400c2935b4df16a26558f3
anupama-pathirage <anupama@wso2.com>
2018-04-20 14:00:57 +0530

    Refactor database modules

---
+++ stdlib/ballerina-database/src/main/java/org/ballerinalang/database/sql/actions/Call.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ballerinalang.database.sql.actions;
+
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.database.sql.Constants;
+import org.ballerinalang.database.sql.SQLDatasource;
+import org.ballerinalang.database.sql.SQLDatasourceUtils;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BRefValueArray;
+import org.ballerinalang.model.values.BStruct;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaFunction;
+import org.ballerinalang.natives.annotations.Receiver;
+import org.ballerinalang.natives.annotations.ReturnType;
+import org.ballerinalang.util.observability.ObservabilityUtils;
+import org.ballerinalang.util.observability.ObserverContext;
+
+import static org.ballerinalang.util.observability.ObservabilityConstants.TAG_DB_TYPE_SQL;
+import static org.ballerinalang.util.observability.ObservabilityConstants.TAG_KEY_DB_STATEMENT;
+import static org.ballerinalang.util.observability.ObservabilityConstants.TAG_KEY_DB_TYPE;
+
+/**
+ * {@code Call} is the Call action implementation of the SQL Connector.
+ *
+ * @since 0.8.0
+ */
+@BallerinaFunction(
+        orgName = "ballerina", packageName = "sql",
+        functionName = "call",
+        receiver = @Receiver(type = TypeKind.STRUCT, structType = Constants.CALLER_ACTIONS),
+        args = {
+                @Argument(name = "sqlQuery", type = TypeKind.STRING),
+                @Argument(name = "parameters", type = TypeKind.ARRAY, elementType = TypeKind.STRUCT,
+                          structType = "Parameter")
+        },
+        returnType = {
+                @ReturnType(type = TypeKind.TABLE),
+                @ReturnType(type = TypeKind.STRUCT, structType = "error", structPackage = "ballerina.builtin")
+        }
+)
+public class Call extends AbstractSQLAction {
+
+    @Override
+    public void execute(Context context) {
+        try {
+            BStruct bConnector = (BStruct) context.getRefArgument(0);
+            String query = context.getStringArgument(0);
+            BRefValueArray structTypes = (BRefValueArray) context.getNullableRefArgument(1);
+            BRefValueArray parameters = (BRefValueArray) context.getNullableRefArgument(2);
+
+            SQLDatasource datasource = (SQLDatasource) bConnector.getNativeData(Constants.CALLER_ACTIONS);
+
+            ObserverContext observerContext = ObservabilityUtils.getParentContext(context);
+            observerContext.addTag(TAG_KEY_DB_STATEMENT, query);
+            observerContext.addTag(TAG_KEY_DB_TYPE, TAG_DB_TYPE_SQL);
+
+            executeProcedure(context, datasource, query, parameters, structTypes);
+        } catch (Throwable e) {
+            context.setReturnValues(SQLDatasourceUtils.getSQLConnectorError(context, e));
+            SQLDatasourceUtils.handleErrorOnTransaction(context);
+        }
+    }
+}
---
+++ stdlib/ballerina-database/src/main/java/org/ballerinalang/database/sql/actions/UpdateWithGeneratedKeys.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ballerinalang.database.sql.actions;
+
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.database.sql.Constants;
+import org.ballerinalang.database.sql.SQLDatasource;
+import org.ballerinalang.database.sql.SQLDatasourceUtils;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BRefValueArray;
+import org.ballerinalang.model.values.BStringArray;
+import org.ballerinalang.model.values.BStruct;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaFunction;
+import org.ballerinalang.natives.annotations.Receiver;
+import org.ballerinalang.natives.annotations.ReturnType;
+import org.ballerinalang.util.observability.ObservabilityUtils;
+import org.ballerinalang.util.observability.ObserverContext;
+
+import static org.ballerinalang.util.observability.ObservabilityConstants.TAG_DB_TYPE_SQL;
+import static org.ballerinalang.util.observability.ObservabilityConstants.TAG_KEY_DB_STATEMENT;
+import static org.ballerinalang.util.observability.ObservabilityConstants.TAG_KEY_DB_TYPE;
+
+/**
+ * {@code UpdateWithGeneratedKeys} is the updateWithGeneratedKeys action implementation of the SQL Connector.
+ *
+ * @since 0.8.0
+ */
+@BallerinaFunction(
+        orgName = "ballerina", packageName = "sql",
+        functionName = "updateWithGeneratedKeys",
+        receiver = @Receiver(type = TypeKind.STRUCT, structType = Constants.CALLER_ACTIONS),
+        args = {
+                @Argument(name = "sqlQuery", type = TypeKind.STRING),
+                @Argument(name = "parameters", type = TypeKind.ARRAY, elementType = TypeKind.STRUCT,
+                          structType = "Parameter"),
+                @Argument(name = "keyColumns", type = TypeKind.ARRAY, elementType = TypeKind.STRING)
+        },
+        returnType = {
+                @ReturnType(type = TypeKind.INT),
+                @ReturnType(type = TypeKind.ARRAY, elementType = TypeKind.STRING),
+                @ReturnType(type = TypeKind.STRUCT, structType = "error", structPackage = "ballerina.builtin")
+        }
+)
+public class UpdateWithGeneratedKeys extends AbstractSQLAction {
+
+    @Override
+    public void execute(Context context) {
+        try {
+            BStruct bConnector = (BStruct) context.getRefArgument(0);
+            String query = context.getStringArgument(0);
+            BStringArray keyColumns = (BStringArray) context.getNullableRefArgument(1);
+            BRefValueArray parameters = (BRefValueArray) context.getNullableRefArgument(2);
+
+            SQLDatasource datasource = (SQLDatasource) bConnector.getNativeData(Constants.CALLER_ACTIONS);
+
+            ObserverContext observerContext = ObservabilityUtils.getParentContext(context);
+            observerContext.addTag(TAG_KEY_DB_STATEMENT, query);
+            observerContext.addTag(TAG_KEY_DB_TYPE, TAG_DB_TYPE_SQL);
+
+            executeUpdateWithKeys(context, datasource, query, keyColumns, parameters);
+        } catch (Throwable e) {
+            context.setReturnValues(SQLDatasourceUtils.getSQLConnectorError(context, e));
+            SQLDatasourceUtils.handleErrorOnTransaction(context);
+        }
+    }
+}
