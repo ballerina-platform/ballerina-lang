276c215b1b5865e35c6547277dad4eabc1b86bd5
irunika <irunikail@gmail.com>
2018-04-20 14:49:05 +0530

    Update java doc

--- components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/contract/websocket/WebSocketConnection.java
+++ components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/contract/websocket/WebSocketConnection.java
@@ -17,6 +17,11 @@ public interface WebSocketConnection {
     /**
      * Get the {@link javax.websocket.Session} of the WebSocket connection.
      *
+     * <br><i>This is going to be removed in the future.</i>
+     * <br><i>For more details </i>
+     * @see <a href="https://github.com/wso2/transport-http/issues/130">
+     *     [WebSocket] Need to Remove javax.websocket.session interface from Http-Transport</a>
+     *
      * @return the session of the WebSocket connection.
      */
     Session getSession();
@@ -25,7 +30,7 @@ public interface WebSocketConnection {
      * Reading WebSocket frames after successful handshake is blocked by default in transport level.
      * In order to read the next frame from wire this method should be called.
      *
-     * * <b>Note: This will allow reading frame by frame from the wire. If all frames should be read automatically
+     * <br><b>Note: This will allow reading frame by frame from the wire. If all frames should be read automatically
      * then startReadingFrames() method should be called.</b>
      */
     void readNextFrame();
@@ -34,7 +39,7 @@ public interface WebSocketConnection {
      * Reading WebSocket frames after successful handshake is blocked by default in transport level.
      * In order to start reading WebSocket frames from the wire this method should be called.
      *
-     * <b>Note: This will allow reading frames automatically from the wire. If frame by frame should be read
+     * <br><b>Note: This will allow reading frames automatically from the wire. If frame by frame should be read
      * from the wire readNextFrame() method should be called.</b>
      */
     void startReadingFrames();
