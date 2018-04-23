71e83e12a3ac1522831e1372d25d5a2e882c3d79
Shankar <shankar@Shankars-MacBook-Pro.local>
2018-04-23 17:31:24 +0530

    Improve example outputs, tests

--- examples/websocket-chat-application/websocket-chat-application.sh
+++ examples/websocket-chat-application/websocket-chat-application.sh
@@ -5,9 +5,9 @@ $ ballerina run websocket-chat-application.bal
 # To check the sample, use Chrome or Firefox JavaScript console and run the following commands <br>
 # Run the first 3 lines of the below code in two or more consoles and see how the messages are received by sending messages
 # To check the capability of change the names the URI such that /chat/fistName+LastName/age with multiple clients
-$ var ws = new WebSocket("ws://localhost:9090/chat/bruce?age=30");
-$ ws.onmessage = function(frame) {console.log(frame.data)};
-$ ws.onclose = function(frame) {console.log(frame)};
+var ws = new WebSocket("ws://localhost:9090/chat/bruce?age=30");
+ws.onmessage = function(frame) {console.log(frame.data)};
+ws.onclose = function(frame) {console.log(frame)};
 
 # To send messages
-$ ws.send("hello world");
+ws.send("hello world");
