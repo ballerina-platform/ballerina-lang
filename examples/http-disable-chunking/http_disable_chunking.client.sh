3615580e63e75454f6197a5bbb4a1513f95a2efa
Vijitha Ekanayake <vijitha@Vijithas-MacBook-Pro-2.local>
2018-04-23 17:28:38 +0530

    Update disable-chunking example

--- examples/http-disable-chunking/http_disable_chunking.client.sh
+++ examples/http-disable-chunking/http_disable_chunking.client.sh
@@ -1,4 +1,4 @@
-$ curl http://localhost:9092/chunkingSample/
+$ curl http://localhost:9092/chunkingSample
 {"Outbound request content":"Lenght-18"}
 
 To enable chunking, try changing the chunking option of the clientEndpoint to http:Chunking.ALWAYS.
