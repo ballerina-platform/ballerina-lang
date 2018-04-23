82436fee46d95966d1765403aa34fd3fb4f27a9c
bhashinee <bhashineen@gmail.com>
2018-04-23 16:53:46 +0530

    Update https-server-connector sample

--- examples/https-listener/https-listener.server.sh
+++ examples/https-listener/https-listener.server.sh
@@ -1,5 +1,5 @@
 #At the command line, navigate to the directory that contains the 
 #`.bal` file and run the `ballerina run` command to start the server.
-$ ballerina run https-server-connector.bal
-ballerina: initiating service(s) in 'https-server-connector.bal'
+$ ballerina run https-listener.bal
+ballerina: initiating service(s) in 'https-listener.bal'
 ballerina: started HTTPS/WSS endpoint 0.0.0.0:9095
