93b7d6c1cdf7b702aba0b7e01bfb92ae5cb5448f
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2018-04-23 16:49:21 +0530

    Add instruction to start the broker

--- examples/mb-simple-queue-message-producer/mb-simple-queue-message-producer.sh
+++ examples/mb-simple-queue-message-producer/mb-simple-queue-message-producer.sh
@@ -1,3 +1,6 @@
+# Start Ballerina message broker using `broker.sh` or `broker.bat` command
+$ broker.sh
+
 # To run the program, put the code in `mb-simple-queue-message-producer.bal`
 # and use `$BALLERINA_HOME/bin/ballerina`.
 $ ballerina run mb-simple-queue-message-producer.bal
