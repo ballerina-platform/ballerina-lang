61fc290a53d26570b30d28b92dc0f06a5a44bdcb
TharinduNM <tharindunm93@gmail.com>
2018-04-23 17:54:00 +0530

    remove unnecessary if statement

--- stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/internal/execBallerina/ExecBallerina.java
+++ stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/internal/execBallerina/ExecBallerina.java
@@ -38,56 +38,52 @@ public class ExecBallerina extends BlockingNativeCallableUnit {
         String command = context.getRefArgument(0).stringValue();
         String packageName = context.getStringArgument(0);
 
-        if (!packageName.contains(";") && !packageName.contains("|") && !packageName.contains("\"")) {
-            String balCommand = path + "/bin/ballerina " + command + " " + packageName;
+        String balCommand = path + "/bin/ballerina " + command + " " + packageName;
 
-            Process process;
-            BufferedReader reader = null;
-            BufferedReader readerEr = null;
-            try {
-                StringBuilder output = new StringBuilder();
-                process = Runtime.getRuntime().exec(balCommand);
-                process.waitFor();
-                reader =
-                        new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
-                readerEr =
-                        new BufferedReader(new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8));
+        Process process;
+        BufferedReader reader = null;
+        BufferedReader readerEr = null;
+        try {
+            StringBuilder output = new StringBuilder();
+            process = Runtime.getRuntime().exec(balCommand);
+            process.waitFor();
+            reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
+            readerEr =
+                    new BufferedReader(new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8));
 
-                String lineEr = "";
-                while ((lineEr = readerEr.readLine()) != null) {
-                    output.append(lineEr).append("\n");
-                }
-
-                String line = "";
-                while ((line = reader.readLine()) != null) {
-                    output.append(line).append("\n");
-                }
-
-                String adjusted = output.toString().replaceAll("(?m)^[ \t]*\r?\n", "");
-                context.setReturnValues(new BString(adjusted));
-            } catch (InterruptedException e) {
-                throw new BallerinaException(e.getMessage());
-            } catch (IOException e) {
-                throw new BallerinaException(e.getMessage());
-            } finally {
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    if (readerEr != null) {
-                        readerEr.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            String lineEr = "";
+            while ((lineEr = readerEr.readLine()) != null) {
+                output.append(lineEr).append("\n");
             }
-        } else {
-            String message = "The arguments to the ballerina command: " + command + " contains invalid characters";
-            context.setReturnValues(new BString(message));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            String adjusted = output.toString().replaceAll("(?m)^[ \t]*\r?\n", "");
+            context.setReturnValues(new BString(adjusted));
+        } catch (InterruptedException e) {
+            throw new BallerinaException(e.getMessage());
+        } catch (IOException e) {
+            throw new BallerinaException(e.getMessage());
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (readerEr != null) {
+                    readerEr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
     }
 }
