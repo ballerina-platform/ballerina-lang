5e5b9d8ab1a5b0a6d37dc0e9eeb768dd8097f2d5
Shankar <shankar@Shankars-MacBook-Pro.local>
2018-04-23 15:11:41 +0530

    Improve outputs

--- examples/character-i-o/character-i-o.sh
+++ examples/character-i-o/character-i-o.sh
@@ -1,3 +1,5 @@
+#At the command line, navigate to the directory that contains the
+#`.bal` file and run the `ballerina run` command.
 $ ballerina run character-i-o.bal
 Started to process the file.
  No of characters written : 5
