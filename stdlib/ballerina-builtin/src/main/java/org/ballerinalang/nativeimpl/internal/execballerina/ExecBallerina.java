a30cb93c19d7d29bff3c57263af3706414deab66
Hemika Kodikara <hemika.kodikara@gmail.com>
2018-04-24 19:54:38 +0530

    Improvements for internal:execBallerina function

--- stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/internal/execballerina/ExecBallerina.java
+++ stdlib/ballerina-builtin/src/main/java/org/ballerinalang/nativeimpl/internal/execballerina/ExecBallerina.java
@@ -1,6 +1,25 @@
-package org.ballerinalang.nativeimpl.internal.execBallerina;
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.nativeimpl.internal.execballerina;
 
 import org.ballerinalang.bre.Context;
+import org.ballerinalang.bre.bvm.BLangVMErrors;
 import org.ballerinalang.bre.bvm.BlockingNativeCallableUnit;
 import org.ballerinalang.model.types.TypeKind;
 import org.ballerinalang.model.values.BString;
@@ -13,10 +32,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 /**
- * Native function ballerina.runtime:exec.
+ * Native function ballerina.internal:execBallerina.
  *
  * @since 0.970.0-alpha1
  */
@@ -25,7 +45,8 @@ import java.util.Properties;
         functionName = "execBallerina",
         args = {@Argument(name = "command", type = TypeKind.STRING),
                 @Argument(name = "packagePath", type = TypeKind.STRING)},
-        returnType = {@ReturnType(type = TypeKind.STRING)},
+        returnType = {@ReturnType(type = TypeKind.STRING),
+                      @ReturnType(type = TypeKind.STRUCT)},
         isPublic = true
 )
 public class ExecBallerina extends BlockingNativeCallableUnit {
@@ -38,7 +59,7 @@ public class ExecBallerina extends BlockingNativeCallableUnit {
         String command = context.getRefArgument(0).stringValue();
         String packagePath = context.getStringArgument(0);
 
-        String balCommand = path + "/bin/ballerina " + command + " " + packagePath;
+        String balCommand = Paths.get(path, "bin", "ballerina ") + command + " " + packagePath;
 
         Process process;
         BufferedReader reader = null;
@@ -52,12 +73,12 @@ public class ExecBallerina extends BlockingNativeCallableUnit {
             readerEr =
                     new BufferedReader(new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8));
 
-            String lineEr = "";
+            String lineEr;
             while ((lineEr = readerEr.readLine()) != null) {
                 output.append(lineEr).append("\n");
             }
 
-            String line = "";
+            String line;
             while ((line = reader.readLine()) != null) {
                 output.append(line).append("\n");
             }
@@ -65,23 +86,28 @@ public class ExecBallerina extends BlockingNativeCallableUnit {
             String adjusted = output.toString().replaceAll("(?m)^[ \t]*\r?\n", "");
             context.setReturnValues(new BString(adjusted));
         } catch (InterruptedException e) {
+            context.setReturnValues(BLangVMErrors.createError(context,
+                    "Error occurred while waiting for ballerina command to finish: " + path));
             throw new BallerinaException(e.getMessage());
         } catch (IOException e) {
-            throw new BallerinaException(e.getMessage());
+            context.setReturnValues(BLangVMErrors.createError(context,
+                    "Error occurred executing command or reading out of the command: " + path));
         } finally {
             try {
                 if (reader != null) {
                     reader.close();
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                context.setReturnValues(BLangVMErrors.createError(context,
+                        "Error occurred closing standard output: " + path));
             }
             try {
                 if (readerEr != null) {
                     readerEr.close();
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                context.setReturnValues(BLangVMErrors.createError(context,
+                        "Error occurred closing standard error output: " + path));
             }
         }
 
