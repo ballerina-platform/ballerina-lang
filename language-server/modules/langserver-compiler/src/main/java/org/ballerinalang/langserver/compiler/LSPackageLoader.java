8ad46d10db51d844decbd323bcdc1f041398ed90
Nadeeshaan <nadeeshaangunasinghe@gmail.com>
2018-04-24 15:39:36 +0530

    Fix import issue after org name

--- language-server/modules/langserver-compiler/src/main/java/org/ballerinalang/langserver/compiler/LSPackageLoader.java
+++ language-server/modules/langserver-compiler/src/main/java/org/ballerinalang/langserver/compiler/LSPackageLoader.java
@@ -32,8 +32,8 @@ import java.util.List;
 public class LSPackageLoader {
 
     private static final String[] STATIC_PKG_NAMES = {"http", "swagger", "mime", "auth", "cache", "config", "sql",
-            "file", "internal", "io", "jwt", "log", "math", "system", "reflect", "runtime", "crypto", "task",
-            "time", "transactions", "user", "util", "builtin"};
+            "file", "internal", "io", "log", "math", "system", "reflect", "runtime", "crypto", "task",
+            "time", "transactions", "builtin"};
 
     /**
      * Get the Builtin Package.
