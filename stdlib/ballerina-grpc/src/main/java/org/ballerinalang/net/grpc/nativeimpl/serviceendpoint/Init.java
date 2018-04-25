a1a6f5b8e5b2f621ceacf2d5f37b39577450e53b
Danesh Kuruppu <dknkuruppu@gmail.com>
2018-04-25 19:04:36 +0530

    Fix error handling issue server and client endpoint native functions

--- stdlib/ballerina-grpc/src/main/java/org/ballerinalang/net/grpc/nativeimpl/serviceendpoint/Init.java
+++ stdlib/ballerina-grpc/src/main/java/org/ballerinalang/net/grpc/nativeimpl/serviceendpoint/Init.java
@@ -31,7 +31,6 @@ import org.ballerinalang.natives.annotations.BallerinaFunction;
 import org.ballerinalang.natives.annotations.Receiver;
 import org.ballerinalang.net.grpc.GrpcConstants;
 import org.ballerinalang.net.grpc.GrpcServicesBuilder;
-import org.ballerinalang.net.grpc.MessageUtils;
 import org.ballerinalang.net.grpc.ssl.SSLHandlerFactory;
 import org.ballerinalang.util.exceptions.BallerinaException;
 import org.wso2.transport.http.netty.config.ListenerConfiguration;
@@ -69,29 +68,21 @@ public class Init extends BlockingNativeCallableUnit {
     
     @Override
     public void execute(Context context) {
-        try {
-            Struct serviceEndpoint = BLangConnectorSPIUtil.getConnectorEndpointStruct(context);
-            BStruct endpointConfigStruct = (BStruct) context.getRefArgument(1);
-            Struct serviceEndpointConfig = BLangConnectorSPIUtil.toStruct(endpointConfigStruct);
-            ListenerConfiguration configuration = getListenerConfig(serviceEndpointConfig);
-            io.grpc.ServerBuilder serverBuilder;
-            if (configuration.getSSLConfig() != null) {
-                SslContext sslCtx = new SSLHandlerFactory(configuration.getSSLConfig())
-                        .createHttp2TLSContextForServer();
-                serverBuilder = GrpcServicesBuilder.initService(configuration, sslCtx);
-            } else {
-                serverBuilder = GrpcServicesBuilder.initService(configuration, null);
-            }
-            serviceEndpoint.addNativeData(SERVICE_BUILDER, serverBuilder);
-            context.setReturnValues();
-        } catch (Throwable throwable) {
-            BStruct err = getConnectorError(context, throwable);
-            context.setError(err);
+
+        Struct serviceEndpoint = BLangConnectorSPIUtil.getConnectorEndpointStruct(context);
+        BStruct endpointConfigStruct = (BStruct) context.getRefArgument(1);
+        Struct serviceEndpointConfig = BLangConnectorSPIUtil.toStruct(endpointConfigStruct);
+        ListenerConfiguration configuration = getListenerConfig(serviceEndpointConfig);
+        io.grpc.ServerBuilder serverBuilder;
+        if (configuration.getSSLConfig() != null) {
+            SslContext sslCtx = new SSLHandlerFactory(configuration.getSSLConfig())
+                    .createHttp2TLSContextForServer();
+            serverBuilder = GrpcServicesBuilder.initService(configuration, sslCtx);
+        } else {
+            serverBuilder = GrpcServicesBuilder.initService(configuration, null);
         }
-    }
-    
-    private static BStruct getConnectorError(Context context, Throwable throwable) {
-        return MessageUtils.getConnectorError(context, throwable);
+        serviceEndpoint.addNativeData(SERVICE_BUILDER, serverBuilder);
+        context.setReturnValues();
     }
     
     private ListenerConfiguration getListenerConfig(Struct endpointConfig) {
