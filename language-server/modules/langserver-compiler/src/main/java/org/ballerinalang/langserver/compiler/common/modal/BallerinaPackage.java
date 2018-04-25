0a96c41e902c93bf75d4f86b03f5c1bd480f79f6
Nipuna Marcus <nipunac@wso2.com>
2018-04-25 16:15:55 +0530

    Add functionality to load packages dynamically

---
+++ language-server/modules/langserver-compiler/src/main/java/org/ballerinalang/langserver/compiler/common/modal/BallerinaPackage.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ballerinalang.langserver.compiler.common.modal;
+
+/**
+ * Class which contains Ballerina package details.
+ */
+public class BallerinaPackage {
+    private String packageName;
+    private String orgName;
+    private String version;
+
+    public BallerinaPackage() {
+        this.packageName = "";
+        this.orgName = "";
+        this.version = "0.0.0.0";
+    }
+
+    public BallerinaPackage(String orgName, String packageName, String version) {
+        this.packageName = packageName;
+        this.orgName = orgName;
+        this.version = version;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
