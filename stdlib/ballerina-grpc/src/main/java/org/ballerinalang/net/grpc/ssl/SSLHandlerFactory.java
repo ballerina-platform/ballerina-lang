b83bfde0a841ea11c5fe83d8eeca9a2d5fd6764c
Yasara Dissanayake <yasaradiss101@gmail.com>
2018-05-09 09:55:18 +0530

    Add licence and fixlicence years

--- stdlib/ballerina-grpc/src/main/java/org/ballerinalang/net/grpc/ssl/SSLHandlerFactory.java
+++ stdlib/ballerina-grpc/src/main/java/org/ballerinalang/net/grpc/ssl/SSLHandlerFactory.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015 WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *  Copyright (c) 2018 WSO2 Inc. (http://wso2.com) All Rights Reserved.
  *
  *  WSO2 Inc. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
