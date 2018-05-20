6cd5b169124c098aba47155d0ee65424f97734f8
MaryamZi <maryamziyadm@gmail.com>
2018-05-20 14:02:44 +0530

    Introduce a common BallerinaBrokerByteBuf

---
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/broker/BallerinaBrokerByteBuf.java
@@ -0,0 +1,25 @@
+package org.ballerinalang.broker;
+
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.buffer.UnpooledHeapByteBuf;
+import org.ballerinalang.model.values.BValue;
+
+/**
+ * Implementation of {@link io.netty.buffer.ByteBuf}, to hold a {@link BValue}, to use with the internal broker core
+ * used in Ballerina.
+ *
+ * @since 0.972.0
+ */
+public class BallerinaBrokerByteBuf extends UnpooledHeapByteBuf {
+
+    private final BValue value;
+
+    public BallerinaBrokerByteBuf(BValue value) {
+        super(UnpooledByteBufAllocator.DEFAULT, 0, 0);
+        this.value = value;
+    }
+
+    public BValue getValue() {
+        return value;
+    }
+}
