e50955db32df4170d62aba257372d5cfffd67448
NatashaWso2 <nwijesekare@gmail.com>
2018-05-21 17:10:56 +0530

    Remove redundant code line

--- cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/SearchUtils.java
+++ cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/SearchUtils.java
@@ -19,7 +19,6 @@ package org.ballerinalang.packerina;
 
 import org.ballerinalang.spi.EmbeddedExecutor;
 import org.ballerinalang.toml.model.Proxy;
-import org.ballerinalang.toml.model.Settings;
 import org.ballerinalang.util.EmbeddedExecutorProvider;
 import org.wso2.ballerinalang.util.RepoUtils;
 
@@ -38,8 +37,7 @@ public class SearchUtils {
     public static void searchInCentral(String argument) {
         String query = "?q=" + argument;
         EmbeddedExecutor executor = EmbeddedExecutorProvider.getInstance().getExecutor();
-        Settings settings = RepoUtils.readSettings();
-        Proxy proxy = settings.getProxy();
+        Proxy proxy = RepoUtils.readSettings().getProxy();
         executor.execute("packaging_search/packaging_search.balx", true, RepoUtils.getRemoteRepoURL(), query,
                          proxy.getHost(), proxy.getPort(), proxy.getUserName(), proxy.getPassword());
     }
