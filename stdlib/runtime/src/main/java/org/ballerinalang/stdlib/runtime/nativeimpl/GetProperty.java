902f6fc8822c3ee29e93b9929d6c5ac3378bee7d
Pubudu Fernando <pubuduf@wso2.com>
2018-05-25 12:37:15 +0530

    Make ballerina/runtime a separate module

--- stdlib/runtime/src/main/java/org/ballerinalang/stdlib/runtime/nativeimpl/GetProperty.java
+++ stdlib/runtime/src/main/java/org/ballerinalang/stdlib/runtime/nativeimpl/GetProperty.java
@@ -1,19 +1,21 @@
 /*
- *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-package org.ballerinalang.nativeimpl.runtime;
+package org.ballerinalang.stdlib.runtime.nativeimpl;
 
 import org.ballerinalang.bre.Context;
 import org.ballerinalang.bre.bvm.BlockingNativeCallableUnit;
@@ -21,7 +23,7 @@ import org.ballerinalang.model.types.TypeKind;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaFunction;
 import org.ballerinalang.natives.annotations.ReturnType;
-import org.ballerinalang.util.BuiltInUtils;
+import org.ballerinalang.stdlib.runtime.utils.RuntimeUtils;
 
 /**
  * Native function ballerina.runtime:getProperty.
@@ -40,6 +42,6 @@ public class GetProperty extends BlockingNativeCallableUnit {
     @Override
     public void execute(Context context) {
         String name = context.getStringArgument(0);
-        context.setReturnValues(BuiltInUtils.getSystemProperty(name));
+        context.setReturnValues(RuntimeUtils.getSystemProperty(name));
     }
 }
