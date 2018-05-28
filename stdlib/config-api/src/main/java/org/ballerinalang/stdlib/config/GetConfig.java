dee07ac2090f98cca1f9df18b5c1a3b57e4ba500
Pubudu Fernando <pubuduf@wso2.com>
2018-05-28 10:34:48 +0530

    Make ballerina/config a separate module

--- stdlib/config-api/src/main/java/org/ballerinalang/stdlib/config/GetConfig.java
+++ stdlib/config-api/src/main/java/org/ballerinalang/stdlib/config/GetConfig.java
@@ -6,17 +6,17 @@
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
-package org.ballerinalang.nativeimpl.config;
+package org.ballerinalang.stdlib.config;
 
 import org.ballerinalang.bre.Context;
 import org.ballerinalang.bre.bvm.BlockingNativeCallableUnit;
