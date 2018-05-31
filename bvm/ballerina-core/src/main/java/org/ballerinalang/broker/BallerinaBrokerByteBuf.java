47271e50e68562813be5f9352d358f922f9aeeb2
MaryamZi <maryamziyadm@gmail.com>
2018-05-31 10:37:17 +0530

    Fix callback for localhost

--- bvm/ballerina-core/src/main/java/org/ballerinalang/broker/BallerinaBrokerByteBuf.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/broker/BallerinaBrokerByteBuf.java
@@ -8,7 +8,7 @@ import org.ballerinalang.model.values.BValue;
  * Implementation of {@link io.netty.buffer.ByteBuf}, to hold a {@link BValue}, to use with the internal broker core
  * used in Ballerina.
  *
- * @since 0.972.0
+ * @since 0.973.0
  */
 public class BallerinaBrokerByteBuf extends UnpooledHeapByteBuf {
 
