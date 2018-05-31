77694d932370039fb7a18bd79017a9bdfb746248
chamil321 <chamilmadawa8@gmail.com>
2018-05-31 10:01:41 +0530

    Address review suggestions

--- components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/listener/SourceHandlerErrorHandler.java
+++ components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/listener/SourceHandlerErrorHandler.java
@@ -1,20 +1,19 @@
 /*
- *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.transport.http.netty.listener;
@@ -35,6 +34,7 @@ import static org.wso2.transport.http.netty.common.Constants.IDLE_TIMEOUT_TRIGGE
 import static org.wso2.transport.http.netty.common.Constants.IDLE_TIMEOUT_TRIGGERED_BEFORE_WRITING_OUTBOUND_RESPONSE;
 import static org.wso2.transport.http.netty.common.Constants.IDLE_TIMEOUT_TRIGGERED_WHILE_READING_INBOUND_REQUEST;
 import static org.wso2.transport.http.netty.common.Constants.REMOTE_CLIENT_ABRUPTLY_CLOSE_CONNECTION;
+import static org.wso2.transport.http.netty.common.Constants.REMOTE_CLIENT_ABRUPTLY_CLOSE_RESPONSE_CONNECTION;
 import static org.wso2.transport.http.netty.listener.InboundState.CONNECTED;
 import static org.wso2.transport.http.netty.listener.InboundState.ENTITY_BODY_RECEIVED;
 import static org.wso2.transport.http.netty.listener.InboundState.RECEIVING_ENTITY_BODY;
@@ -57,16 +57,26 @@ public class SourceHandlerErrorHandler {
 
     void handleErrorCloseScenario(HTTPCarbonMessage inboundRequestMsg, HttpResponseFuture httpOutboundRespFuture) {
         this.inboundRequestMsg = inboundRequestMsg;
-        String errorMessage = REMOTE_CLIENT_ABRUPTLY_CLOSE_CONNECTION;
-        if (state == RECEIVING_ENTITY_BODY) {
-            handleIncompleteInboundRequest(errorMessage);
-        } else if (state == ENTITY_BODY_RECEIVED) {
-            try {
-                serverConnectorFuture.notifyErrorListener(new ServerConnectorException(errorMessage));
-                httpOutboundRespFuture.notifyHttpListener(new ServerConnectorException(errorMessage));
-            } catch (ServerConnectorException e) {
-                log.error("Couldn't notify error state to application layer");
+        try {
+            switch (state) {
+                case CONNECTED:
+                    serverConnectorFuture.notifyErrorListener(
+                            new ServerConnectorException(REMOTE_CLIENT_ABRUPTLY_CLOSE_CONNECTION));
+                    break;
+                case RECEIVING_ENTITY_BODY:
+                    handleIncompleteInboundRequest(REMOTE_CLIENT_ABRUPTLY_CLOSE_CONNECTION);
+                    break;
+                case ENTITY_BODY_RECEIVED:
+                    serverConnectorFuture.notifyErrorListener(
+                            new ServerConnectorException(REMOTE_CLIENT_ABRUPTLY_CLOSE_RESPONSE_CONNECTION));
+                    httpOutboundRespFuture.notifyHttpListener(
+                            new ServerConnectorException(REMOTE_CLIENT_ABRUPTLY_CLOSE_RESPONSE_CONNECTION));
+                    break;
+                default:
+                    log.error("Unexpected state detected ", state);
             }
+        } catch (ServerConnectorException e) {
+            log.error("Error while notifying error state to server-connector listener");
         }
     }
 
@@ -84,13 +94,13 @@ public class SourceHandlerErrorHandler {
             case ENTITY_BODY_RECEIVED:
                 // This means we have received the complete inbound request. But nothing happened
                 // after that.
-                serverConnectorFuture.notifyErrorListener(new ServerConnectorException(
-                        IDLE_TIMEOUT_TRIGGERED_BEFORE_WRITING_OUTBOUND_RESPONSE));
+                serverConnectorFuture.notifyErrorListener(
+                        new ServerConnectorException(IDLE_TIMEOUT_TRIGGERED_BEFORE_WRITING_OUTBOUND_RESPONSE));
 
                 // This is to stop application layer writing response after
                 // idle timeout is triggered.
-                httpOutboundRespFuture.notifyHttpListener(new ServerConnectorException(
-                        IDLE_TIMEOUT_TRIGGERED_BEFORE_WRITING_OUTBOUND_RESPONSE));
+                httpOutboundRespFuture.notifyHttpListener(
+                        new ServerConnectorException(IDLE_TIMEOUT_TRIGGERED_BEFORE_WRITING_OUTBOUND_RESPONSE));
                 break;
             default:
                 log.error("Unexpected state detected ", state);
