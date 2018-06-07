4571fa66ad97b181164f07da00c81a5403249387
NatashaWso2 <nwijesekare@gmail.com>
2018-06-07 12:00:34 +0530

    Use TreeMap to preserve the order of packages in the LockFile

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/LockFileWriter.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/LockFileWriter.java
@@ -29,9 +29,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 /**
@@ -42,7 +42,11 @@ import java.util.stream.Collectors;
 public class LockFileWriter {
     private static final CompilerContext.Key<LockFileWriter> LOCK_FILE_WRITER_KEY = new CompilerContext.Key<>();
     private final SourceDirectory sourceDirectory;
-    private Map<PackageID, LockFilePackage> ballerinaLockPackages = new LinkedHashMap<>();
+    private TreeMap<PackageID, LockFilePackage> ballerinaLockPackages = new TreeMap<>
+            (Comparator.comparing((PackageID p) -> p.orgName.value)
+                       .thenComparing(p -> p.name.value)
+                       .thenComparing(p -> p.version.value)
+            );
     private List<String> entryPackages = new ArrayList<>();
 
     /**
