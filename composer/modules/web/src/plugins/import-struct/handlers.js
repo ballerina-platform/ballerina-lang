3027b6eefa16f5f52853b06314382422efb351cc
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2018-06-13 13:51:39 +0530

    Update record import logic for new modal

--- composer/modules/web/src/plugins/import-struct/handlers.js
+++ composer/modules/web/src/plugins/import-struct/handlers.js
@@ -52,7 +52,7 @@ function isFloat(val) {
 function processJSONStruct(parent, literalExpr, topLevelNodes, removeDefaults) {
     let currentValue;
     let success = true;
-    parent.setFields([], true);
+    parent.typeNode.fields = [];
     literalExpr.keyValuePairs.every((keyValPair) => {
         let currentName;
         let key = keyValPair.key;
@@ -96,7 +96,7 @@ function processJSONStruct(parent, literalExpr, topLevelNodes, removeDefaults) {
             if (!refExpr.error) {
                 // Add ; white space for the field in to record.
                 parent.ws.splice(parent.ws.length - 2, 0, {ws: "", text: ";"})
-                parent.addFields(refExpr);
+                parent.typeNode.fields.push(refExpr);
             } else {
                 success = false;
             }
@@ -127,8 +127,7 @@ export function getHandlerDefinitions(plugin) {
                     let success = true;
 
                     if (structName && structName !== '') {
-                        structNode.getName().setValue(structName, true);
-                        structNode.setName(structNode.getName(), false);
+                        structNode.name.setValue(structName);
                     }
                     if (json === '') {
                         topLevelNodes.addTopLevelNodes(structNode);
