85333cddc2e53c5427a06048c42b22dbdc7faeeb
Rajith <lanka.vitharana@gmail.com>
2018-06-13 17:21:01 +0530

    Remove ordering logic and use packageinfo map as it is already ordered

--- bvm/ballerina-core/src/main/java/org/ballerinalang/util/program/BLangFunctions.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/util/program/BLangFunctions.java
@@ -39,13 +39,11 @@ import org.ballerinalang.model.types.BTypes;
 import org.ballerinalang.model.values.BCallableFuture;
 import org.ballerinalang.model.values.BStruct;
 import org.ballerinalang.model.values.BValue;
-import org.ballerinalang.util.BLangConstants;
 import org.ballerinalang.util.FunctionFlags;
 import org.ballerinalang.util.codegen.CallableUnitInfo;
 import org.ballerinalang.util.codegen.CallableUnitInfo.WorkerSet;
 import org.ballerinalang.util.codegen.ForkjoinInfo;
 import org.ballerinalang.util.codegen.FunctionInfo;
-import org.ballerinalang.util.codegen.ImportPackageInfo;
 import org.ballerinalang.util.codegen.PackageInfo;
 import org.ballerinalang.util.codegen.ProgramFile;
 import org.ballerinalang.util.codegen.WorkerInfo;
@@ -121,9 +119,7 @@ public class BLangFunctions {
      * @param programFile to be invoked.
      */
     public static void invokePackageInitFunctions(ProgramFile programFile) {
-        orderInitFunctions(programFile);
-
-        for (PackageInfo info : programFile.getImportPackageInfoEntries()) {
+        for (PackageInfo info : programFile.getPackageInfoEntries()) {
             invokePackageInitFunction(info.getInitFunctionInfo());
         }
     }
@@ -135,9 +131,7 @@ public class BLangFunctions {
      * @param context to be used.
      */
     public static void invokePackageInitFunctions(ProgramFile programFile, WorkerExecutionContext context) {
-        orderInitFunctions(programFile);
-
-        for (PackageInfo info : programFile.getImportPackageInfoEntries()) {
+        for (PackageInfo info : programFile.getPackageInfoEntries()) {
             invokePackageInitFunction(info.getInitFunctionInfo(), context);
         }
     }
@@ -149,7 +143,7 @@ public class BLangFunctions {
      * @param programFile to be invoked.
      */
     public static void invokePackageStartFunctions(ProgramFile programFile) {
-        for (PackageInfo info : programFile.getImportPackageInfoEntries()) {
+        for (PackageInfo info : programFile.getPackageInfoEntries()) {
             BLangFunctions.invokeVMUtilFunction(info.getStartFunctionInfo());
         }
     }
@@ -162,7 +156,7 @@ public class BLangFunctions {
      * @param context to be used.
      */
     public static void invokePackageStartFunctions(ProgramFile programFile, WorkerExecutionContext context) {
-        for (PackageInfo info : programFile.getImportPackageInfoEntries()) {
+        for (PackageInfo info : programFile.getPackageInfoEntries()) {
             BLangFunctions.invokeVMUtilFunction(info.getStartFunctionInfo());
         }
     }
@@ -173,28 +167,8 @@ public class BLangFunctions {
      * @param programFile to be invoked.
      */
     public static void invokePackageStopFunctions(ProgramFile programFile) {
-        for (PackageInfo info : programFile.getImportPackageInfoEntries()) {
+        for (PackageInfo info : programFile.getPackageInfoEntries()) {
             BLangFunctions.invokeVMUtilFunction(info.getStopFunctionInfo());
-        }
-    }
-
-    private static void orderInitFunctions(ProgramFile programFile) {
-        PackageInfo entry = programFile.getEntryPackage();
-        programFile.addImportPackageInfo(programFile.getPackageInfo(BLangConstants.BALLERINA_BUILTIN_PKG));
-        programFile.addImportPackageInfo(programFile.getPackageInfo(BLangConstants.BALLERINA_RUNTIME_PKG));
-        orderPackages(programFile, entry);
-        programFile.addImportPackageInfo(entry);
-    }
-
-    private static void orderPackages(ProgramFile programFile, PackageInfo pkgInfo) {
-        for (ImportPackageInfo importPackageInfo : pkgInfo.importPkgInfoList) {
-            if (importPackageInfo.packageInfo == null) {
-                importPackageInfo.packageInfo = programFile.getPackageInfo(importPackageInfo.pkgPath);
-            }
-            orderPackages(programFile, importPackageInfo.packageInfo);
-            if (!programFile.importPackageAlreadyExist(importPackageInfo.packageInfo)) {
-                programFile.addImportPackageInfo(importPackageInfo.packageInfo);
-            }
         }
     }
     
