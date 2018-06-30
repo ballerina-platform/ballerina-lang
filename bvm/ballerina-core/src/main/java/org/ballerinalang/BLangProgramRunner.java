3965315c8f1e9d890b71aad712f8cd2ae9a239e2
MaryamZi <maryamziyadm@gmail.com>
2018-06-30 12:25:12 +0530

    Add initial impl. for data binding main This commit adds the initial implementation for the data binding main, with support for: a) required params of simple basic types, xml and json b) a single array or rest param defined at the end c) no params

--- bvm/ballerina-core/src/main/java/org/ballerinalang/BLangProgramRunner.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/BLangProgramRunner.java
@@ -19,7 +19,20 @@ package org.ballerinalang;
 
 import org.ballerinalang.model.types.BArrayType;
 import org.ballerinalang.model.types.BType;
-import org.ballerinalang.model.types.BTypes;
+import org.ballerinalang.model.types.TypeTags;
+import org.ballerinalang.model.util.JsonParser;
+import org.ballerinalang.model.util.XMLUtils;
+import org.ballerinalang.model.values.BBlob;
+import org.ballerinalang.model.values.BBlobArray;
+import org.ballerinalang.model.values.BBoolean;
+import org.ballerinalang.model.values.BBooleanArray;
+import org.ballerinalang.model.values.BFloat;
+import org.ballerinalang.model.values.BFloatArray;
+import org.ballerinalang.model.values.BIntArray;
+import org.ballerinalang.model.values.BInteger;
+import org.ballerinalang.model.values.BJSON;
+import org.ballerinalang.model.values.BRefValueArray;
+import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BStringArray;
 import org.ballerinalang.model.values.BValue;
 import org.ballerinalang.util.codegen.FunctionInfo;
@@ -67,7 +80,7 @@ public class BLangProgramRunner {
 
         FunctionInfo mainFuncInfo = getMainFunction(mainPkgInfo);
         try {
-            BLangFunctions.invokeEntrypointCallable(programFile, mainFuncInfo, extractMainArgs(args));
+            BLangFunctions.invokeEntrypointCallable(programFile, mainFuncInfo, extractMainArgs(mainFuncInfo, args));
         } finally {
             if (programFile.isServiceEPAvailable()) {
                 return;
@@ -79,12 +92,117 @@ public class BLangProgramRunner {
         }
     }
 
-    private static BValue[] extractMainArgs(String[] args) {
-        BStringArray arrayArgs = new BStringArray();
-        for (int i = 0; i < args.length; i++) {
-            arrayArgs.add(i, args[i]);
+    private static BValue[] extractMainArgs(FunctionInfo mainFuncInfo, String[] args) {
+        BType[] paramTypes = mainFuncInfo.getParamTypes();
+        BValue[] bValueArgs = new BValue[paramTypes.length];
+        for (int index = 0; index < paramTypes.length; index++) {
+            switch (paramTypes[index].getTag()) {
+                case TypeTags.INT_TAG:
+                    bValueArgs[index] = getBIntegerValue(args[index]);
+                    break;
+                case TypeTags.FLOAT_TAG:
+                    bValueArgs[index] = getBFloatValue(args[index]);
+                    break;
+                case TypeTags.STRING_TAG:
+                    bValueArgs[index] = new BString(args[index]);
+                    break;
+                case TypeTags.BOOLEAN_TAG:
+                    bValueArgs[index] = getBBooleanValue(args[index]);
+                    break;
+                case TypeTags.BLOB_TAG:
+                    bValueArgs[index] = new BBlob(args[index].getBytes());
+                    break;
+                case TypeTags.XML_TAG:
+                    bValueArgs[index] = XMLUtils.parse(args[index]);
+                    break;
+                case TypeTags.JSON_TAG:
+                    bValueArgs[index] = new BJSON(JsonParser.parse(args[index]));
+                    break;
+                case TypeTags.ARRAY_TAG:
+                    if (index == paramTypes.length - 1) {
+                        switch (((BArrayType) paramTypes[index]).getElementType().getTag()) {
+                            case TypeTags.INT_TAG:
+                                BIntArray intArrayArgs = new BIntArray();
+                                for (int i = index; i < args.length; i++) {
+                                    intArrayArgs.add(i - index, Integer.parseInt(args[i]));
+                                }
+                                bValueArgs[index] = intArrayArgs;
+                                break;
+                            case TypeTags.FLOAT_TAG:
+                                BFloatArray floatArrayArgs = new BFloatArray();
+                                for (int i = index; i < args.length; i++) {
+                                    floatArrayArgs.add(i - index, Double.parseDouble(args[i]));
+                                }
+                                bValueArgs[index] = floatArrayArgs;
+                                break;
+                            case TypeTags.STRING_TAG:
+                                BStringArray stringArrayArgs = new BStringArray();
+                                for (int i = index; i < args.length; i++) {
+                                    stringArrayArgs.add(i - index, args[i]);
+                                }
+                                bValueArgs[index] = stringArrayArgs;
+                                break;
+                            case TypeTags.BOOLEAN_TAG:
+                                BBooleanArray booleanArrayArgs = new BBooleanArray();
+                                for (int i = index; i < args.length; i++) {
+                                    booleanArrayArgs.add(i - index, Boolean.parseBoolean(args[i]) ? 1 : 0);
+                                }
+                                bValueArgs[index] = booleanArrayArgs;
+                                break;
+                            case TypeTags.BLOB_TAG:
+                                BBlobArray blobArrayArgs = new BBlobArray();
+                                for (int i = index; i < args.length; i++) {
+                                    blobArrayArgs.add(i - index, args[i].getBytes());
+                                }
+                                bValueArgs[index] = blobArrayArgs;
+                                break;
+                            case TypeTags.XML_TAG:
+                                BRefValueArray xmlArrayArgs = new BRefValueArray();
+                                for (int i = index; i < args.length; i++) {
+                                    xmlArrayArgs.add(i - index, XMLUtils.parse(args[i]));
+                                }
+                                bValueArgs[index] = xmlArrayArgs;
+                                break;
+                            case TypeTags.JSON_TAG:
+                                BRefValueArray jsonArrayArgs = new BRefValueArray();
+                                for (int i = index; i < args.length; i++) {
+                                    jsonArrayArgs.add(i - index, new BJSON(JsonParser.parse(args[i])));
+                                }
+                                bValueArgs[index] = jsonArrayArgs;
+                                break;
+                            default:
+                                //Ideally shouldn't reach here
+                                throw new BallerinaException("array of unsupported element type: " + paramTypes[index]);
+                        }
+                        break;
+                    }
+                    throw new BallerinaException("array or rest parameter only supported as the final parameter");
+            }
         }
-        return new BValue[] {arrayArgs};
+        return bValueArgs;
+    }
+
+    private static BInteger getBIntegerValue(String argument) {
+        try {
+            return new BInteger(Integer.parseInt(argument));
+        } catch (NumberFormatException e) {
+            throw new BallerinaException("invalid argument: " + argument + ", integer expected");
+        }
+    }
+
+    private static BFloat getBFloatValue(String argument) {
+        try {
+            return new BFloat(Double.parseDouble(argument));
+        } catch (NumberFormatException e) {
+            throw new BallerinaException("invalid argument: " + argument + ", float expected");
+        }
+    }
+
+    private static BBoolean getBBooleanValue(String argument) {
+        if (!"TRUE".equalsIgnoreCase(argument) && !"FALSE".equalsIgnoreCase(argument)) {
+            throw new BallerinaException("invalid argument: " + argument + ", boolean expected");
+        }
+        return new BBoolean(Boolean.parseBoolean(argument));
     }
 
     private static void initDebugger(ProgramFile programFile, Debugger debugger) {
@@ -104,10 +222,7 @@ public class BLangProgramRunner {
             throw new BallerinaException(errorMsg);
         }
 
-        BType[] paramTypes = mainFuncInfo.getParamTypes();
-        BType[] retParamTypes = mainFuncInfo.getRetParamTypes();
-        BArrayType argsType = new BArrayType(BTypes.typeString);
-        if (paramTypes.length != 1 || !paramTypes[0].equals(argsType) || retParamTypes.length != 0) {
+        if (mainFuncInfo.getRetParamTypes().length != 0) {
             throw new BallerinaException(errorMsg);
         }
 
