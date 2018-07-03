f5164cbc1e41a67bd96d47f34b370724da0900e2
mohan <mohan.vive@gmail.com>
2018-07-03 11:28:21 +0530

    Add improvements to have function and global vars in select & filter

---
+++ tests/ballerina-test/src/test/java/org/ballerinalang/test/streaming/BallerinaStreamsV2SelectorWithVariableTest.java
@@ -0,0 +1,65 @@
+/*
+*  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+
+package org.ballerinalang.test.streaming;
+
+import org.ballerinalang.launcher.util.BCompileUtil;
+import org.ballerinalang.launcher.util.BRunUtil;
+import org.ballerinalang.launcher.util.CompileResult;
+import org.ballerinalang.model.values.BInteger;
+import org.ballerinalang.model.values.BMap;
+import org.ballerinalang.model.values.BValue;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * This contains methods to test external variable in select clause in Ballerina Streaming V2.
+ *
+ * @since 0.980.0
+ */
+public class BallerinaStreamsV2SelectorWithVariableTest {
+
+    private CompileResult result;
+
+    @BeforeClass
+    public void setup() {
+        System.setProperty("enable.siddhiRuntime", "false");
+        result = BCompileUtil.compile("test-src/streaming/streamingv2-select-with-variable-test.bal");
+    }
+
+    @Test(description = "Test filter streaming query")
+    public void testSelectQuery() {
+        BValue[] outputEmployeeEvents = BRunUtil.invoke(result, "startSelectQuery");
+        System.setProperty("enable.siddhiRuntime", "true");
+        Assert.assertNotNull(outputEmployeeEvents);
+
+        Assert.assertEquals(outputEmployeeEvents.length, 3, "Expected events are not received");
+
+        BMap<String, BValue> employee0 = (BMap<String, BValue>) outputEmployeeEvents[0];
+        BMap<String, BValue> employee1 = (BMap<String, BValue>) outputEmployeeEvents[1];
+        BMap<String, BValue> employee2 = (BMap<String, BValue>) outputEmployeeEvents[2];
+
+        Assert.assertEquals(employee0.get("teacherName").stringValue(), "Raja");
+        Assert.assertEquals(((BInteger) employee0.get("age")).intValue(), 40);
+        Assert.assertEquals(employee1.get("teacherName").stringValue(), "Mohan");
+        Assert.assertEquals(((BInteger) employee1.get("age")).intValue(), 40);
+        Assert.assertEquals(employee2.get("teacherName").stringValue(), "Shareek");
+        Assert.assertEquals(((BInteger) employee2.get("age")).intValue(), 40);
+    }
+}
