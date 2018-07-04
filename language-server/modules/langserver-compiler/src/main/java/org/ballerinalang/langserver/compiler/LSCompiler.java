a3543ff8ef148a9e8bfe53c3489582746cd4c841
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2018-07-04 12:05:02 +0530

    Revert "Fix issue #9321"
    This reverts commit cdb6bc3db004e8be86219ff7f75e83e42d4697b0.

--- language-server/modules/langserver-compiler/src/main/java/org/ballerinalang/langserver/compiler/LSCompiler.java
+++ language-server/modules/langserver-compiler/src/main/java/org/ballerinalang/langserver/compiler/LSCompiler.java
@@ -356,13 +356,8 @@ public class LSCompiler {
     @CheckForNull
     public static String findProjectRoot(String parentDir, Path homePath) {
         Path path = Paths.get(parentDir, ProjectDirConstants.DOT_BALLERINA_DIR_NAME);
-        try {
-            if (!java.nio.file.Files.isSameFile(path, homePath) &&
-                    java.nio.file.Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
-                return parentDir;
-            }
-        } catch (IOException e) {
-            //ignore error
+        if (!path.equals(homePath) && java.nio.file.Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
+            return parentDir;
         }
         Path parent = Paths.get(parentDir);
         Path parentsParent = parent.getParent();
