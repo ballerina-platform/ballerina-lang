043f7d7925138eb2e56c5635da5d1d62af959006
Rajith <lanka.vitharana@gmail.com>
2018-07-10 16:59:15 +0530

    Add dummy integer value as we can't change binary format right now

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/CompiledPackageSymbolEnter.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/CompiledPackageSymbolEnter.java
@@ -875,6 +875,8 @@ public class CompiledPackageSymbolEnter {
         int noOfParams = documentDataStream.readShort();
         for (int i = 0; i < noOfParams; i++) {
             String name = getUTF8CPEntryValue(documentDataStream);
+            //TODO remove below line ASAP, adding dummy value as we can't change binary file right now
+            documentDataStream.readInt();
             String paramKind = getUTF8CPEntryValue(documentDataStream);
             String paramDesc = getUTF8CPEntryValue(documentDataStream);
 
