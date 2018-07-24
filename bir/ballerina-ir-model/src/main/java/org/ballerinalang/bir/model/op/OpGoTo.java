bef175f318b6c829ba804c7ddee5f221631297e3
manuranga <manu@wso2.com>
2018-07-24 21:58:24 +0530

    Add BIR writer

--- bir/ballerina-ir-model/src/main/java/org/ballerinalang/bir/model/op/OpGoTo.java
+++ bir/ballerina-ir-model/src/main/java/org/ballerinalang/bir/model/op/OpGoTo.java
@@ -1,6 +1,7 @@
 package org.ballerinalang.bir.model.op;
 
 import org.ballerinalang.bir.model.IrBasicBlock;
+import org.ballerinalang.bir.model.OpVisitor;
 
 /**
  * GoTo opcode.
@@ -13,5 +14,10 @@ public class OpGoTo extends Op {
 
     public OpGoTo(IrBasicBlock destination) {
         this.destination = destination;
+    }
+
+    @Override
+    public void accept(OpVisitor visitor) {
+        visitor.accept(this);
     }
 }
