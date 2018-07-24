7b4d696dfc7ace68caf3c5b8fe5d9c1651e12e43
manuranga <manu@wso2.com>
2018-07-24 21:58:24 +0530

    Add BIR writer

--- bir/ballerina-ir-model/src/main/java/org/ballerinalang/bir/model/op/Op.java
+++ bir/ballerina-ir-model/src/main/java/org/ballerinalang/bir/model/op/Op.java
@@ -1,9 +1,11 @@
 package org.ballerinalang.bir.model.op;
 
 import org.ballerinalang.bir.model.IrElement;
+import org.ballerinalang.bir.model.OpVisitor;
 
 /**
  * Abstract super class of all the op code elements.
  */
 public abstract class Op extends IrElement {
+    public abstract void accept(OpVisitor visitor);
 }
--- bir/ballerina-ir-model/src/main/java/org/ballerinalang/bir/model/op/OpReturn.java
+++ bir/ballerina-ir-model/src/main/java/org/ballerinalang/bir/model/op/OpReturn.java
@@ -1,5 +1,7 @@
 package org.ballerinalang.bir.model.op;
 
+import org.ballerinalang.bir.model.OpVisitor;
+
 /**
  * GoTo opcode.
  * <p>
@@ -7,4 +9,9 @@ package org.ballerinalang.bir.model.op;
  * return;
  */
 public class OpReturn extends Op {
+
+    @Override
+    public void accept(OpVisitor visitor) {
+        visitor.accept(this);
+    }
 }
