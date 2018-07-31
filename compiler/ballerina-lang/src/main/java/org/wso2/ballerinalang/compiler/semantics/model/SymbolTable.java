726eea8bd8a99033dfd10587eee3f408b31b52b7
Anoukh <anoukh.ashley@gmail.com>
2018-07-31 20:52:05 +0530

    Fix record literal assignment to union types

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/model/SymbolTable.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/model/SymbolTable.java
@@ -158,7 +158,7 @@ public class SymbolTable {
         BTypeSymbol errorStructSymbol = new BRecordTypeSymbol(SymTag.RECORD, Flags.PUBLIC, Names.ERROR,
                 rootPkgSymbol.pkgID, null, rootPkgSymbol);
         this.errStructType = new BRecordType(errorStructSymbol);
-        this.errStructType.restFieldType = new BAnyType(TypeTags.ANY, null);;
+        this.errStructType.restFieldType = new BAnyType(TypeTags.ANY, null);
         errorStructSymbol.type = this.errStructType;
         errorStructSymbol.scope = new Scope(errorStructSymbol);
         defineType(this.errStructType, errorStructSymbol);
