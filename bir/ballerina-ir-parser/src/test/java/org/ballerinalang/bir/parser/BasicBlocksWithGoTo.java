21022a0a78853e98d7bbe5e75d51d9f2aa472a82
manuranga <manu@wso2.com>
2018-08-02 01:08:18 +0530

    Add llvm bindings

--- bir/ballerina-ir-parser/src/test/java/org/ballerinalang/bir/parser/BasicBlocksWithGoTo.java
+++ bir/ballerina-ir-parser/src/test/java/org/ballerinalang/bir/parser/BasicBlocksWithGoTo.java
@@ -3,9 +3,14 @@ package org.ballerinalang.bir.parser;
 import org.ballerinalang.bir.model.IrBasicBlock;
 import org.ballerinalang.bir.model.IrPackage;
 import org.ballerinalang.bir.model.op.OpGoTo;
+import org.ballerinalang.bir.parser.writer.binary.BinaryWriter;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.List;
 
 public class BasicBlocksWithGoTo extends ParserTest {
@@ -17,6 +22,13 @@ public class BasicBlocksWithGoTo extends ParserTest {
         Assert.assertEquals(bbs.size(), 2);
         Assert.assertEquals(bbs.get(0).ops.size(), 1);
         Assert.assertEquals(bbs.get(0).ops.get(0).getClass(), OpGoTo.class);
+
+
+        ByteBuffer out = ByteBuffer.allocate(1024);
+        new BinaryWriter(out).write(irPackage);
+        out.flip();
+        FileChannel channel = new FileOutputStream(new File("/home/manu/sc/2017-07-24/myfunc.bir"), false).getChannel();
+        channel.write(out);
     }
 
 }
