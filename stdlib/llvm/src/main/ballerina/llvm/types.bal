e1556b04151e20119c24d78f4bd05ed396a6fa80
manuranga <manu@wso2.com>
2018-08-05 23:20:38 +0530

    Add auto generated bindings

--- stdlib/llvm/src/main/ballerina/llvm/types.bal
+++ stdlib/llvm/src/main/ballerina/llvm/types.bal
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+// Copyright (c) 2018 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 //
 // WSO2 Inc. licenses this file to you under the Apache License,
 // Version 2.0 (the "License"); you may not use this file except
@@ -14,5 +14,45 @@
 // specific language governing permissions and limitations
 // under the License.
 
-public type LLVMModuleRef record {
-};
+public type ByteBuffer record {};
+public type BytePointer record {};
+public type IntPointer record {};
+public type LLVMAttributeRef record {};
+public type LLVMBasicBlockRef record {};
+public type LLVMBuilderRef record {};
+public type LLVMContextRef record {};
+public type LLVMDiagnosticHandler record {};
+public type LLVMDiagnosticInfoRef record {};
+public type LLVMDisasmContextRef record {};
+public type LLVMExecutionEngineRef record {};
+public type LLVMGenericValueRef record {};
+public type LLVMMCJITCompilerOptions record {};
+public type LLVMMCJITMemoryManagerRef record {};
+public type LLVMMemoryBufferRef record {};
+public type LLVMMemoryManagerAllocateCodeSectionCallback record {};
+public type LLVMMemoryManagerAllocateDataSectionCallback record {};
+public type LLVMMemoryManagerDestroyCallback record {};
+public type LLVMMemoryManagerFinalizeMemoryCallback record {};
+public type LLVMMetadataRef record {};
+public type LLVMModuleProviderRef record {};
+public type LLVMModuleRef record {};
+public type LLVMObjectFileRef record {};
+public type LLVMOpInfoCallback record {};
+public type LLVMPassManagerBuilderRef record {};
+public type LLVMPassManagerRef record {};
+public type LLVMPassRegistryRef record {};
+public type LLVMRelocationIteratorRef record {};
+public type LLVMSectionIteratorRef record {};
+public type LLVMSymbolIteratorRef record {};
+public type LLVMSymbolLookupCallback record {};
+public type LLVMTargetDataRef record {};
+public type LLVMTargetLibraryInfoRef record {};
+public type LLVMTargetMachineRef record {};
+public type LLVMTargetRef record {};
+public type LLVMTypeRef record {};
+public type LLVMUseRef record {};
+public type LLVMValueRef record {};
+public type LLVMYieldCallback record {};
+public type Pointer record {};
+public type PointerPointer record {};
+public type SizeTPointer record {};
---
+++ stdlib/llvm/src/main/java/org/ballerinalang/nativeimpl/llvm/gen/LLVMCreateTargetMachine.java
@@ -0,0 +1,70 @@
+// Copyright (c) 2018 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+//
+// WSO2 Inc. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.ballerinalang.nativeimpl.llvm.gen;
+
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.bre.bvm.BlockingNativeCallableUnit;
+import org.ballerinalang.model.values.BMap;
+import org.ballerinalang.model.values.BValue;
+import org.ballerinalang.nativeimpl.llvm.FFIUtil;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaFunction;
+import org.ballerinalang.natives.annotations.ReturnType;
+import org.bytedeco.javacpp.LLVM;
+import org.bytedeco.javacpp.LLVM.LLVMTargetMachineRef;
+
+import static org.ballerinalang.model.types.TypeKind.INT;
+import static org.ballerinalang.model.types.TypeKind.RECORD;
+import static org.ballerinalang.model.types.TypeKind.STRING;
+import static org.bytedeco.javacpp.LLVM.LLVMCreateTargetMachine;
+
+/**
+ * Auto generated class.
+ */
+@BallerinaFunction(
+        orgName = "ballerina", packageName = "llvm",
+        functionName = "LLVMCreateTargetMachine",
+        args = {
+                @Argument(name = "t", type = RECORD, structType = "LLVMTargetRef"),
+                @Argument(name = "triple", type = STRING),
+                @Argument(name = "cpu", type = STRING),
+                @Argument(name = "features", type = STRING),
+                @Argument(name = "level", type = INT),
+                @Argument(name = "reloc", type = INT),
+                @Argument(name = "codeModel", type = INT),
+        },
+        returnType = {
+                @ReturnType(type = RECORD, structType = "LLVMTargetMachineRef", structPackage = "ballerina/llvm"),
+        }
+)
+public class LLVMCreateTargetMachine extends BlockingNativeCallableUnit {
+
+    @Override
+    public void execute(Context context) {
+        LLVM.LLVMTargetRef t = FFIUtil.getRecodeArgumentNative(context, 0);
+        String triple = context.getStringArgument(0);
+        String cpu = context.getStringArgument(1);
+        String features = context.getStringArgument(2);
+        int level = (int) context.getIntArgument(0);
+        int reloc = (int) context.getIntArgument(1);
+        int codeModel = (int) context.getIntArgument(2);
+        LLVMTargetMachineRef returnValue = LLVMCreateTargetMachine(t, triple, cpu, features, level, reloc, codeModel);
+        BMap<String, BValue> rerunWrapperRecode = FFIUtil.newRecord(context, "LLVMTargetMachineRef");
+        FFIUtil.addNativeToRecode(returnValue, rerunWrapperRecode);
+        context.setReturnValues(rerunWrapperRecode);
+    }
+}
