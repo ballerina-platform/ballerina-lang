c1689292dd9ef39abc9ab411869e30e82d6bcd4f
Dhananjaya Wickramasingha <dhananjayaw@wso2.com>
2018-08-12 09:13:06 +0530

    Add SerializationBValueProviders
    For BBoolean and BInteger

---
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/persistence/serializable/serializer/providers/bvalue/BIntegerBValueProvider.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * you may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ballerinalang.persistence.serializable.serializer.providers.bvalue;
+
+import org.ballerinalang.model.values.BInteger;
+import org.ballerinalang.model.values.BMap;
+import org.ballerinalang.model.values.BValue;
+import org.ballerinalang.persistence.serializable.serializer.BValueDeserializer;
+import org.ballerinalang.persistence.serializable.serializer.BValueSerializer;
+import org.ballerinalang.persistence.serializable.serializer.SerializationBValueProvider;
+
+/**
+ * Provide mapping between {@link Class} and {@link BValue} representation of it.
+ */
+public class BIntegerBValueProvider implements SerializationBValueProvider {
+    @Override
+    public String typeName() {
+        return getType().getSimpleName();
+    }
+
+    @Override
+    public Class<?> getType() {
+        return BInteger.class;
+    }
+
+    @Override
+    public BValue toBValue(Object object, BValueSerializer serializer) {
+        if (object instanceof BInteger) {
+            BInteger bInteger = (BInteger) object;
+            return BValueProviderHelper.wrap(typeName(), bInteger);
+        }
+        throw BValueProviderHelper.incorrectObjectType(object, typeName());
+    }
+
+    @Override
+    public Object toObject(BValue bValue, BValueDeserializer bValueDeserializer) {
+        if (bValue instanceof BMap) {
+            @SuppressWarnings("unchecked")
+            BMap<String, BValue> wrapper = (BMap<String, BValue>) bValue;
+            return (BInteger) BValueProviderHelper.getPayload(wrapper);
+        }
+        throw BValueProviderHelper.deserializationIncorrectType(bValue, typeName());
+    }
+}
