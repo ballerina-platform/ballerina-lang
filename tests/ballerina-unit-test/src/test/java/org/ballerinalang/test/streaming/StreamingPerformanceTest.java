a1195147cee77f62ee350e66968d3a5db7a4a283
mohan <mohan.vive@gmail.com>
2018-08-14 16:40:20 +0530

    Add Testcases for performance tests

---
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/streaming/StreamingPerformanceTest.java
@@ -0,0 +1,46 @@
+/*
+*  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.ballerinalang.test.streaming;
+
+import org.ballerinalang.launcher.util.BCompileUtil;
+import org.ballerinalang.launcher.util.BRunUtil;
+import org.ballerinalang.launcher.util.CompileResult;
+import org.ballerinalang.model.values.BValue;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * This contains methods to verify the performance of Siddhi based streams vs Ballerina based streams.
+ *
+ * @since 0.981.0
+ */
+public class StreamingPerformanceTest {
+
+    private CompileResult result;
+
+    @BeforeClass
+    public void setup() {
+        result = BCompileUtil.compile("test-src/streaming/streaming-performance-test.bal");
+    }
+
+    @Test(description = "Test performance of streaming query")
+    public void testFilterQuery() {
+        BValue[] outputEmployeeEvents = BRunUtil.invoke(result, "startFilterQuery");
+
+    }
+}
