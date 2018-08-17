5d323f658abd046ca9327a1e9410d5162c220f0b
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2018-08-17 11:59:22 +0530

    Add variable resolving test cases

---
+++ tool-plugins/intellij/src/test/java/io/ballerina/psi/vars/BallerinaResolveConstantTest.java
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.ballerina.psi.vars;
+
+import io.ballerina.psi.BallerinaResolveTestBase;
+
+import java.io.IOException;
+
+/**
+ * Test constant resolving.
+ */
+public class BallerinaResolveConstantTest extends BallerinaResolveTestBase {
+
+    private String constant = "@final int /*def*/a;";
+
+    @Override
+    protected String getTestDataPath() {
+        return getTestDataPath("psi/resolve/vars/constant");
+    }
+
+    public void testConstantInSameFileInFunction() {
+        doFileTest();
+    }
+
+    public void testConstantInSameFileInService() {
+        doFileTest();
+    }
+
+    public void testConstantInDifferentFileInFunction() throws IOException {
+        doFileTest(constant);
+    }
+
+    public void testConstantInDifferentFileInService() throws IOException {
+        doFileTest(constant);
+    }
+
+    //TODO: Uncomment after fixing package resolving issue
+    //    public void testConstantInDifferentPackageInFunction() throws IOException {
+    //        doFileTest(constant, "org/test/test.bal");
+    //    }
+    //
+    //    public void testConstantInDifferentPackageInService() throws IOException {
+    //        doFileTest(constant, "org/test/test.bal");
+    //    }
+}
---
+++ tool-plugins/intellij/src/test/java/io/ballerina/psi/vars/BallerinaResolveGlobalVariableTest.java
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.ballerina.psi.vars;
+
+import io.ballerina.psi.BallerinaResolveTestBase;
+
+import java.io.IOException;
+
+/**
+ * Test global variable resolving.
+ */
+public class BallerinaResolveGlobalVariableTest extends BallerinaResolveTestBase {
+
+    private String globalVariable = "public int /*def*/a;";
+
+    @Override
+    protected String getTestDataPath() {
+        return getTestDataPath("psi/resolve/vars/global");
+    }
+
+    public void testGlobalVariableInSameFileInFunction() {
+        doFileTest();
+    }
+
+    public void testGlobalVariableInSameFileInService() {
+        doFileTest();
+    }
+
+    public void testGlobalVariableInDifferentFileInFunction() throws IOException {
+        doFileTest(globalVariable);
+    }
+
+    public void testGlobalVariableInDifferentFileInService() throws IOException {
+        doFileTest(globalVariable);
+    }
+
+    //TODO: Uncomment after fixing package resolving issue
+    //    public void testGlobalVariableInDifferentPackageInFunction() throws IOException {
+    //        doFileTest(globalVariable, "org/test/test.bal");
+    //    }
+    //
+    //    public void testGlobalVariableInDifferentPackageInService() throws IOException {
+    //        doFileTest(globalVariable, "org/test/test.bal");
+    //    }
+}
