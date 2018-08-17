68ca72ac3eaea35d228fa99eb3269945e729eef5
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2018-08-17 15:18:05 +0530

    Update licence headers

--- tool-plugins/intellij/src/test/java/io/ballerina/psi/field/BallerinaResolveRecordFieldTest.java
+++ tool-plugins/intellij/src/test/java/io/ballerina/psi/field/BallerinaResolveRecordFieldTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
