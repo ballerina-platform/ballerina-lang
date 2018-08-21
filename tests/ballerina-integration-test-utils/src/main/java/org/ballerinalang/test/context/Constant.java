1275fa0f320f358430fcb0f439992ce0ea2d1e45
Gihan Anuruddha <wggihan@users.noreply.github.com>
2018-08-21 14:42:51 +0530

    Revert "Add integration test coverage to the report"

--- tests/ballerina-integration-test-utils/src/main/java/org/ballerinalang/test/context/Constant.java
+++ tests/ballerina-integration-test-utils/src/main/java/org/ballerinalang/test/context/Constant.java
@@ -44,5 +44,4 @@ public class Constant {
     public static final String VFS_LOCATION = "FTPLocation";
 
     public static final String PROJECT_BUILD_DIR = "project.build.directory";
-    public static final String EXIT_AGENT_LOCATION = "ballerina.test.exit.agent.location";
 }
