3e83343c373e2cbe2c64f5c29ca5138e923feb6e
Rukshani <arukshani@gmail.com>
2018-08-21 21:55:31 +0530

    Introduce pipelining logic

--- stdlib/http/src/main/java/org/ballerinalang/net/http/nativeimpl/connection/Respond.java
+++ stdlib/http/src/main/java/org/ballerinalang/net/http/nativeimpl/connection/Respond.java
@@ -18,6 +18,7 @@
 
 package org.ballerinalang.net.http.nativeimpl.connection;
 
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.EncoderException;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpResponseStatus;
@@ -37,9 +38,11 @@ import org.ballerinalang.util.observability.ObservabilityUtils;
 import org.ballerinalang.util.observability.ObserverContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wso2.transport.http.netty.common.Constants;
 import org.wso2.transport.http.netty.message.HttpCarbonMessage;
 
 import java.util.Optional;
+import java.util.Queue;
 
 import static org.ballerinalang.net.http.HttpConstants.HTTP_STATUS_CODE;
 import static org.ballerinalang.net.http.HttpConstants.RESPONSE_CACHE_CONTROL_FIELD;
@@ -92,7 +95,63 @@ public class Respond extends ConnectionAction {
                 String.valueOf(outboundResponseStruct.get(RESPONSE_STATUS_CODE_FIELD))));
 
         try {
-            sendOutboundResponseRobust(dataContext, inboundRequestMsg, outboundResponseStruct, outboundResponseMsg);
+            boolean pipeliningNeeded = true;
+            if(pipeliningNeeded) { //keep-alive true, http2 false
+                PipelinedResponse pipelinedResponse = new PipelinedResponse(inboundRequestMsg.getSequenceId(),
+                        inboundRequestMsg, outboundResponseMsg, dataContext, outboundResponseStruct);
+                ChannelHandlerContext sourceContext = inboundRequestMsg.getSourceContext();
+                Queue<PipelinedResponse> responseQueue = sourceContext.channel().attr(Constants.RESPONSE_QUEUE).get();
+                Integer maxQueuedResponses = sourceContext.channel().attr(Constants.MAX_RESPONSES_ALLOWED_TO_BE_QUEUED).get();
+                if (responseQueue.size() > maxQueuedResponses) {
+                    //Cannot queue up indefinitely which might cause out of memory issues, so closing the connection
+                    sourceContext.close();
+                    return;
+                }
+                responseQueue.add(pipelinedResponse);
+                while (!responseQueue.isEmpty()) {
+                    Integer nextSequenceNumber = sourceContext.channel().attr(Constants.NEXT_SEQUENCE_NUMBER).get();
+                    final PipelinedResponse queuedPipelinedResponse = responseQueue.peek();
+                    int currentSequenceNumber = queuedPipelinedResponse.getSequenceId();
+                    if (currentSequenceNumber != nextSequenceNumber) {
+                        break;
+                    }
+                    responseQueue.remove();
+                    //TODO:
+                    //ctx.write(queuedPipelinedResponse.getResponse());
+                    //call send response robust
+                    PipeliningHandler.sendOutboundResponseRobust(queuedPipelinedResponse.getDataContext(),
+                            queuedPipelinedResponse.getInboundRequestMsg(),
+                            queuedPipelinedResponse.getOutboundResponseStruct(),
+                            queuedPipelinedResponse.getOutboundResponseMsg());
+                }
+
+                /*while (!responseQueue.isEmpty()) {
+                    Integer nextSequenceNumber = sourceContext.channel().attr(Constants.NEXT_SEQUENCE_NUMBER).get();
+                    final HttpCarbonMessage queuedPipelinedResponse = responseQueue.peek();
+                    int currentSequenceNumber = queuedPipelinedResponse.getSequenceId();
+                    if (currentSequenceNumber != RESPONSE_QUEUING_NOT_NEEDED) {
+                        if (currentSequenceNumber != nextSequenceNumber) {
+                            break;
+                        }
+                *//*Remove the piped response from the queue even if not all the content has been received. When there are
+                 delayed contents, pipeline listener will trigger this method again with delayed content as the next
+                 runnable task queued up in same IO thread. We do not have to worry about other responses
+                 getting executed before the delayed content because the nextSequence number will get updated
+                 only when the last http content of the delayed message has been received.*//*
+                        responseQueue.remove();
+                        while (!queuedPipelinedResponse.isEmpty()) {
+                            sendQueuedResponse(sourceContext, nextSequenceNumber, queuedPipelinedResponse, respListener);
+                        }
+                    } else { //No queuing needed since this has not come from source handler
+                        responseQueue.remove();
+                        respListener.sendResponse(queuedPipelinedResponse, KEEP_ALIVE_TRUE);
+                    }
+                }*/
+
+            } else {
+                sendOutboundResponseRobust(dataContext, inboundRequestMsg, outboundResponseStruct, outboundResponseMsg);
+            }
+
         } catch (EncoderException e) {
             //Exception is already notified by http transport.
             log.debug("Couldn't complete outbound response", e);
