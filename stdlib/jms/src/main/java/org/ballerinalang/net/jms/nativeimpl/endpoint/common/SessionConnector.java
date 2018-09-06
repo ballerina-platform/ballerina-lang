08d7aea9acc1c6c3653578c2836767c3e8961347
anupama-pathirage <anupama@wso2.com>
2018-09-06 17:07:05 +0530

    Fix unused method in Transaction Context

--- stdlib/jms/src/main/java/org/ballerinalang/net/jms/nativeimpl/endpoint/common/SessionConnector.java
+++ stdlib/jms/src/main/java/org/ballerinalang/net/jms/nativeimpl/endpoint/common/SessionConnector.java
@@ -86,11 +86,6 @@ public class SessionConnector implements BallerinaTransactionContext {
     }
 
     @Override
-    public void done() {
-        // do nothing
-    }
-
-    @Override
     public XAResource getXAResource() {
         if (session instanceof XASession) {
             return ((XASession) session).getXAResource();
