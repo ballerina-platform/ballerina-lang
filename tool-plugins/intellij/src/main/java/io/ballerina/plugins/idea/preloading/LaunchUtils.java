d4c60119d21280c0e079073237cdd60ce8eaddf9
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2018-09-18 18:32:00 +0530

    Add language server launcher utils

---
+++ tool-plugins/intellij/src/main/java/io/ballerina/plugins/idea/preloading/LaunchUtils.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.ballerina.plugins.idea.preloading;
+
+import java.util.Locale;
+
+/**
+ * Utility functions used by launcher.
+ */
+public class LaunchUtils {
+
+    private static final String OS = System.getProperty("os.name").toLowerCase(Locale.getDefault());
+
+    public static boolean isWindows() {
+        return (OS.contains("win"));
+    }
+
+    public static boolean isMac() {
+        return (OS.contains("mac"));
+    }
+
+    public static boolean isUnix() {
+        return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
+    }
+
+    public static boolean isSolaris() {
+        return (OS.contains("sunos"));
+    }
+
+    /**
+     * Returns name of the operating system running. null if not a unsupported operating system.
+     * @return operating system
+     */
+    static String getOperatingSystem() {
+
+        if (LaunchUtils.isWindows()) {
+            return "windows";
+        } else if (LaunchUtils.isUnix() || LaunchUtils.isSolaris()) {
+            return "unix";
+        } else if (LaunchUtils.isMac()) {
+            return "mac";
+        }
+        return null;
+    }
+}
