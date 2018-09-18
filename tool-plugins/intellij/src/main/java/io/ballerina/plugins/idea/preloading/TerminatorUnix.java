56b728a9182323f94c7791c63b0919e6d0a31929
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2018-09-18 15:29:23 +0530

    Add language server terminators

---
+++ tool-plugins/intellij/src/main/java/io/ballerina/plugins/idea/preloading/TerminatorUnix.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.ballerina.plugins.idea.preloading;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+/**
+ * Launcher Terminator Implementation for Unix.
+ */
+public class TerminatorUnix implements Terminator {
+    private final String PROCESS_IDENTIFIER = "org.ballerinalang.langserver.launchers.stdio.Main";
+    private static final Logger LOGGER = LoggerFactory.getLogger(TerminatorUnix.class);
+
+    /**
+     * Get find process command.
+     *
+     * @param script absolute path of ballerina file running
+     * @return find process command
+     */
+    private String[] getFindProcessCommand(String script) {
+
+        String[] cmd = {
+                "/bin/sh", "-c", "ps ax | grep " + script + " |  grep -v 'grep' | awk '{print $1}'"
+        };
+        return cmd;
+    }
+
+    /**
+     * Terminate running ballerina program.
+     */
+    public void terminate() {
+        int processID;
+        String[] findProcessCommand = getFindProcessCommand(PROCESS_IDENTIFIER);
+        BufferedReader reader = null;
+        try {
+            Process findProcess = Runtime.getRuntime().exec(findProcessCommand);
+            findProcess.waitFor();
+            reader = new BufferedReader(new InputStreamReader(findProcess.getInputStream(), Charset.defaultCharset()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    processID = Integer.parseInt(line);
+                    killChildProcesses(processID);
+                    kill(processID);
+                } catch (Throwable e) {
+                    LOGGER.error("Launcher was unable to kill process " + line + ".");
+                }
+            }
+        } catch (Throwable e) {
+            LOGGER.error("Launcher was unable to find the process ID for " + PROCESS_IDENTIFIER + ".");
+        } finally {
+            if (reader != null) {
+                IOUtils.closeQuietly(reader);
+            }
+        }
+    }
+
+    /**
+     * Terminate running ballerina program.
+     *
+     * @param pid - process id
+     */
+    public void kill(int pid) {
+        //todo need to put aditional validation
+        if (pid < 0) {
+            return;
+        }
+        String killCommand = String.format("kill -9 %d", pid);
+        try {
+            Process kill = Runtime.getRuntime().exec(killCommand);
+            kill.waitFor();
+        } catch (Throwable e) {
+            LOGGER.error("Launcher was unable to terminate process:" + pid + ".");
+        }
+    }
+
+    /**
+     * Terminate running all child processes for a given pid.
+     *
+     * @param pid - process id
+     */
+    void killChildProcesses(int pid) {
+        BufferedReader reader = null;
+        try {
+            Process findChildProcess = Runtime.getRuntime().exec(String.format("pgrep -P %d", pid));
+            findChildProcess.waitFor();
+            reader = new BufferedReader(
+                    new InputStreamReader(findChildProcess.getInputStream(), Charset.defaultCharset()));
+            String line;
+            int childProcessID;
+            while ((line = reader.readLine()) != null) {
+                childProcessID = Integer.parseInt(line);
+                kill(childProcessID);
+            }
+        } catch (Throwable e) {
+            LOGGER.error("Launcher was unable to find parent for process:" + pid + ".");
+        } finally {
+            if (reader != null) {
+                IOUtils.closeQuietly(reader);
+            }
+        }
+    }
+
+}
---
+++ tool-plugins/intellij/src/main/java/io/ballerina/plugins/idea/preloading/TerminatorWindows.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.ballerina.plugins.idea.preloading;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Launcher Terminator Implementation for Windows. ( Xp professional SP2++).
+ */
+public class TerminatorWindows implements Terminator {
+    private final String PROCESS_IDENTIFIER = "org.ballerinalang.langserver.launchers.stdio.Main";
+    private static final Logger LOGGER = LoggerFactory.getLogger(TerminatorWindows.class);
+
+    /**
+     * @return file process command.
+     */
+    private String getFindProcessCommand() {
+        // escape forward slashes
+        return "cmd /c wmic.exe Process where \"Commandline like '%" + PROCESS_IDENTIFIER + "%'\" CALL TERMINATE";
+    }
+
+    /**
+     * Terminate running ballerina program.
+     */
+    public void terminate() {
+        String findProcessCommand = getFindProcessCommand();
+        try {
+            Process findProcess = Runtime.getRuntime().exec(findProcessCommand);
+            findProcess.waitFor();
+        } catch (Throwable e) {
+            LOGGER.error("Launcher was unable to find the process ID for " + PROCESS_IDENTIFIER + ".");
+        }
+    }
+}
