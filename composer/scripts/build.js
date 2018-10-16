abba7276b5d5b3a8efdd0da93f93c4a4383449da
Joseph Fonseka <hevayo@gmail.com>
2018-10-16 17:36:29 +0530

    Add a seperator to build script

--- composer/scripts/build.js
+++ composer/scripts/build.js
@@ -3,6 +3,10 @@ const { forEachSubModule, runNpmCommandSync, runGitCommand } = require('./common
 const args = process.argv.slice(2);
 
 forEachSubModule((modPath) => {
+    process.stdout.write("==============================================================================\n");
+    process.stdout.write(" Building - " + modPath +"\n");
+    process.stdout.write("==============================================================================\n");
+
     runNpmCommandSync(["i"], modPath);
     runNpmCommandSync(["run", "build"], modPath);
     if (args[0] !== "--skipTest=true") {
