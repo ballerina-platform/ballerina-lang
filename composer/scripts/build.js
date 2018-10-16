e91cf1b7d040ee3ef60036d09028b7f6ec69d05d
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-10-16 17:17:34 +0530

    Add script for running submodule tests of composer

--- composer/scripts/build.js
+++ composer/scripts/build.js
@@ -1,8 +1,13 @@
 const { forEachSubModule, runNpmCommandSync, runGitCommand } = require('./common');
 
+const args = process.argv.slice(2);
+
 forEachSubModule((modPath) => {
     runNpmCommandSync(["i"], modPath);
     runNpmCommandSync(["run", "build"], modPath);
+    if (args[0] !== "--skipTest=true") {
+        runNpmCommandSync(["run", "test"], modPath);
+    }
     // Undo changes to package-lock files. These changes mostly happen
     // because of differences between npm versions
     runGitCommand(["checkout", "--", "package-lock.json"], modPath);
