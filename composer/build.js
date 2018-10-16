bc9c4a496c5415ef310fa4ed1183f2e07365776e
Aruna Herath <arunah@wso2.com>
2018-10-16 11:43:01 +0530

    Reset changes to package-lock files after a build

--- composer/build.js
+++ composer/build.js
@@ -30,6 +30,17 @@ function runNpmCommand(args, cwd) {
   }
 }
 
+function runGitCommand(args, cwd) {
+  // git cmd based on OS
+  const gitCmd = os.platform().startsWith('win') ? 'git.cmd' : 'git';
+
+  // execute command sync
+  const spawnResult = cp.spawnSync(gitCmd, args, { env: process.env, cwd, stdio: 'inherit' });
+  if (spawnResult.status !== 0) {
+      process.kill(process.pid);
+  }
+}
+
 function forEachSubModule(callback) {
     modules.forEach(function (mod) {
         const modPath = join(modulesRoot, mod);
@@ -44,4 +55,7 @@ function forEachSubModule(callback) {
 forEachSubModule((modPath) => {
     runNpmCommand(["i"], modPath);
     runNpmCommand(["run", "build"], modPath);
+    // Undo changes to package-lock files. These changes mostly happen
+    // because of differences between npm versions
+    runGitCommand(["checkout", "--", "package-lock.json"], modPath);
 });
