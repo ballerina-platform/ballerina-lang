riyafa/transport-http

Add outbound throttling
Client can go OOM if the server is slow during a send operation because of data collecting in the netty buffer. Similarly server can go OOM if the client is slow during a respond operation. Outbound throttling fixes this issue.

Resolve ballerina-platform/ballerina-lang#8767

components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/message/DefaultOutboundThrottlingListener.java
@@ -1,0 +1,69 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.wso2.transport.http.netty.message;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Default implementation of the {@link OutboundThrottlingListener}.
+ */
+public class DefaultOutboundThrottlingListener implements OutboundThrottlingListener {
+    private static final Logger LOG = LoggerFactory.getLogger(HttpMessageDataStreamer.class);
+
+    private Semaphore semaphore;
+    private Channel channel;
+
+    /**
+     * Creates the semaphore and sets the source or target channel.
+     *
+     * @param context The {@link org.wso2.transport.http.netty.contractimpl.listener.SourceHandler} context for
+     *                outbound response and the
+     *                {@link org.wso2.transport.http.netty.contractimpl.sender.TargetHandler} context for the
+     *                outbound request.
+     */
+    public DefaultOutboundThrottlingListener(ChannelHandlerContext context) {
+        this.channel = context.channel();
+        this.semaphore = new Semaphore(0);
+    }
+
+    @Override
+    public void onAcquire() {
+        if (!channel.isWritable() && channel.isActive()) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Semaphore acquired for channel {}.", channel.id());
+            }
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    @Override
+    public void onRelease() {
+        LOG.debug("Semaphore released for channel {}.", channel.id());
+        semaphore.release();
+    }
+}

components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/message/MessageFuture.java
@@ -47,6 +47,11 @@ public class MessageFuture {
                     return;
                 }
             }
+
+            //Removes Inbound throttling listener during passthrough so that only outbound throttling would be present.
+            if (httpCarbonMessage.isPassthrough()) {
+                httpCarbonMessage.removeContentListener();
+            }
         }
     }
 

ballerina-platform/ballerina-lang

components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/message/DefaultOutboundThrottlingListener.java
@@ -1,0 +1,71 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.wso2.transport.http.netty.message;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Default implementation of the {@link OutboundThrottlingListener}.
+ */
+public class DefaultOutboundThrottlingListener implements OutboundThrottlingListener {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultOutboundThrottlingListener.class);
+
+    private Semaphore semaphore;
+    private Channel channel;
+
+    /**
+     * Creates the semaphore and sets the source or target channel.
+     *
+     * @param context The {@link org.wso2.transport.http.netty.contractimpl.listener.SourceHandler} context for
+     *                outbound response and the
+     *                {@link org.wso2.transport.http.netty.contractimpl.sender.TargetHandler} context for the
+     *                outbound request.
+     */
+    public DefaultOutboundThrottlingListener(ChannelHandlerContext context) {
+        this.channel = context.channel();
+        this.semaphore = new Semaphore(0);
+    }
+
+    @Override
+    public void onAcquire() {
+        if (!channel.isWritable() && channel.isActive()) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Semaphore acquired for channel {}.", channel.id());
+            }
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    @Override
+    public void onRelease() {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Semaphore released for channel {}.", channel.id());
+        }
+        semaphore.release();
+    }
+}

components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/message/MessageFuture.java
@@ -47,6 +47,11 @@ public class MessageFuture {
                     return;
                 }
             }
+
+            //Removes Inbound throttling listener during passthrough so that only outbound throttling would be present.
+            if (httpCarbonMessage.isPassthrough()) {
+                httpCarbonMessage.removeInboundContentListener();
+            }
         }
     }
 