6063e471724e9fd9bea108418a3bb268ea855924
Aruna Herath <arunah@wso2.com>
2018-10-25 00:05:45 +0530

    Support zero config debugging
    Users now can start debugging without configuring anything. The debugger will assume reasonable defaults for the possible configurations.

--- tool-plugins/vscode/src/debugger/config-provider.ts
+++ tool-plugins/vscode/src/debugger/config-provider.ts
@@ -1,5 +1,6 @@
 
-import { DebugConfigurationProvider, WorkspaceFolder, DebugConfiguration, ProviderResult, debug, ExtensionContext } from 'vscode';
+import { DebugConfigurationProvider, WorkspaceFolder, DebugConfiguration, 
+	ProviderResult, debug, ExtensionContext, window } from 'vscode';
 import { ballerinaExtInstance, BallerinaExtension } from '../core/index';
 
 const debugConfigProvider: DebugConfigurationProvider = {
@@ -8,9 +9,35 @@ const debugConfigProvider: DebugConfigurationProvider = {
 		const ballerinaHome = ballerinaExtInstance.getBallerinaHome();
 		if (!ballerinaHome) {
 			ballerinaExtInstance.showMessageInstallBallerina();
+			return;
 		} else {
 			config['ballerina.home'] = ballerinaHome;
 		}
+
+		if (!config.type) {
+			config.type = 'ballerina';
+		}
+
+		if (!config.request) {
+			config.request = 'launch';
+		}
+
+		if (!config.script) {
+			if (!window.activeTextEditor) {
+				ballerinaExtInstance.showMessageInvalidFile();
+				return
+			}
+
+			const activeDoc = window.activeTextEditor.document;
+
+			if (!activeDoc.fileName.endsWith('.bal')) {
+				ballerinaExtInstance.showMessageInvalidFile();
+				return;
+			}
+
+			config.script = window.activeTextEditor.document.uri.path;
+		}
+
 		return config;
 	}
 };
