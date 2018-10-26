3972b258b2efa555666cd050794204401ad6e944
jeradrutnam <jeradrutnam@outlook.com>
2018-10-26 19:47:55 +0530

    Update Documentation component base styles

--- composer/documentation/src/components/Documentation.jsx
+++ composer/documentation/src/components/Documentation.jsx
@@ -1,51 +1,46 @@
 import React from 'react';
 import Description from './Description';
+import { Table } from 'semantic-ui-react';
 import './Documentation.css';
 
 const Documentation = ({ docDetails }) => {
     const { title, description, parameters, returnParameter } = docDetails;
     return (
         <div className='documentation'>
-            <div className='title'><b>{title}</b></div>
+            <div className='title'><i className='fw fw-function fw-fw icon'></i>{title}()</div>
             <Description source={description} className='description' />
+            {returnParameter && returnParameter.type !== 'nil' && (
+                <div className='return-details'>
+                    <div><strong>return:</strong> <span className='type'>{returnParameter.type}</span></div> 
+                    <div className='return-description'>{<Description source={returnParameter.description || ''} />}</div>
+                </div>
+            )}
             {parameters.length > 0 && (
-                <table className='parameters'>
-                    <tbody>
-                        <tr>
-                            <th>Parameter Name</th>
-                            <th>Data Type</th>
-                            <th>Default Value</th>
-                            <th>Description</th>
-                        </tr>
+                <Table celled compact className='parameters'>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell singleLine>Parameter Name</Table.HeaderCell>
+                            <Table.HeaderCell>Data Type</Table.HeaderCell>
+                            <Table.HeaderCell>Default Value</Table.HeaderCell>
+                            <Table.HeaderCell>Description</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
                         {
                             parameters.map((param) => {
                                 const { name, type, defaultValue, description } = param;
                                 return (
-                                    <tr>
-                                        <td>{name}</td>
-                                        <td>{type}</td>
-                                        <td>{defaultValue}</td>
-                                        <td>{<Description source={description} />}</td>
-                                    </tr>
+                                    <Table.Row>
+                                        <Table.Cell>{name}</Table.Cell>
+                                        <Table.Cell className='type'>{type}</Table.Cell>
+                                        <Table.Cell>{defaultValue}</Table.Cell>
+                                        <Table.Cell>{<Description source={description} />}</Table.Cell>
+                                    </Table.Row>
                                 );
                             })
                         }
-                    </tbody>
-                </table>
-            )}
-            {returnParameter && returnParameter.type !== 'nil' && (
-                <table className='return-parameters'>
-                    <tbody>
-                        <tr>
-                            <th>Return Type</th>
-                            <th>Description</th>
-                        </tr>
-                        <tr>
-                            <td>{returnParameter.type}</td>
-                            <td>{<Description source={returnParameter.description || ""} />}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                    </Table.Body>
+                </Table>
             )}
         </div>
     );
