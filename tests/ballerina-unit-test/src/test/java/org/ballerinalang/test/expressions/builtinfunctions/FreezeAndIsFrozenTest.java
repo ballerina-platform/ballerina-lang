5955462ebba0e23f1283a3a6ff395212eed43a14
MaryamZi <maryamziyadm@gmail.com>
2018-10-28 19:36:30 +0530

    Add tests for anydata ref types freeze

--- tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/builtinfunctions/FreezeAndIsFrozenTest.java
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/builtinfunctions/FreezeAndIsFrozenTest.java
@@ -25,6 +25,7 @@ import org.ballerinalang.model.values.BFloat;
 import org.ballerinalang.model.values.BInteger;
 import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BValue;
+import org.ballerinalang.util.exceptions.BLangRuntimeException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -92,6 +93,205 @@ public class FreezeAndIsFrozenTest {
         Assert.assertTrue(((BBoolean) returns[0]).booleanValue(), "Expected json values to be the same");
     }
 
+    @Test
+    public void testIsFrozenOnStructuralTypes() {
+        BValue[] returns = BRunUtil.invoke(result, "testIsFrozenOnStructuralTypes", new BValue[0]);
+        Assert.assertEquals(returns.length, 2);
+        Assert.assertSame(returns[0].getClass(), BBoolean.class);
+        Assert.assertFalse(((BBoolean) returns[0]).booleanValue(), "Expected values to be identified as not frozen");
+        Assert.assertSame(returns[1].getClass(), BBoolean.class);
+        Assert.assertTrue(((BBoolean) returns[1]).booleanValue(), "Expected values to be identified as frozen");
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: modification not allowed on frozen value.*",
+            dataProvider = "frozenBasicTypeArrayModificationFunctions")
+    public void testFrozenBasicTypeArrayModification(String frozenBasicTypeArrayModificationFunction) {
+        BRunUtil.invoke(result, frozenBasicTypeArrayModificationFunction, new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to set element to json: modification " +
+                    "not allowed on frozen value.*")
+    public void testFrozenJsonArrayModification() {
+        BRunUtil.invoke(result, "testFrozenJsonArrayModification", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to set element to json: modification " +
+                    "not allowed on frozen value.*")
+    public void testFrozenJsonModification() {
+        BRunUtil.invoke(result, "testFrozenJsonModification", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to set element to json: modification " +
+                    "not allowed on frozen value.*")
+    public void testAdditionToFrozenJson() {
+        BRunUtil.invoke(result, "testAdditionToFrozenJson", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to remove element from json: " +
+                    "modification not allowed on frozen value.*")
+    public void testRemovalFromFrozenJson() {
+        BRunUtil.invoke(result, "testRemovalFromFrozenJson", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to set element to json: modification " +
+                    "not allowed on frozen value.*")
+    public void testFrozenInnerJsonModification() {
+        BRunUtil.invoke(result, "testFrozenInnerJsonModification", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to set element to json: modification " +
+                    "not allowed on frozen value.*")
+    public void testAdditionToFrozenInnerJson() {
+        BRunUtil.invoke(result, "testAdditionToFrozenInnerJson", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to remove element from json: " +
+                    "modification not allowed on frozen value.*")
+    public void testRemovalFromFrozenInnerJson() {
+        BRunUtil.invoke(result, "testRemovalFromFrozenInnerJson", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to add children to xml element: " +
+                    "modification not allowed on frozen value.*")
+    public void testFrozenXmlAppendChildren() {
+        BRunUtil.invoke(result, "testFrozenXmlAppendChildren", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to remove children form xml element: " +
+                    "modification not allowed on frozen value.*")
+    public void testFrozenXmlRemoveChildren() {
+        BRunUtil.invoke(result, "testFrozenXmlRemoveChildren", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to remove attribute: modification not " +
+                    "allowed on frozen value.*")
+    public void testFrozenXmlRemoveAttribute() {
+        BRunUtil.invoke(result, "testFrozenXmlRemoveAttribute", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to set attributes: modification not " +
+                    "allowed on frozen value.*")
+    public void testFrozenXmlSetAttributes() {
+        BRunUtil.invoke(result, "testFrozenXmlSetAttributes", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to set children to xml element: " +
+                    "modification not allowed on frozen value.*")
+    public void testFrozenXmlSetChildren() {
+        BRunUtil.invoke(result, "testFrozenXmlSetChildren", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: invalid map insertion: modification not " +
+                    "allowed on frozen value.*")
+    public void testFrozenMapUpdate() {
+        BRunUtil.invoke(result, "testFrozenMapUpdate", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to remove element from map: " +
+                    "modification not allowed on frozen value.*")
+    public void testFrozenMapRemoval() {
+        BRunUtil.invoke(result, "testFrozenMapRemoval", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to clear map: modification not allowed" +
+                    " on frozen value.*")
+    public void testFrozenMapClear() {
+        BRunUtil.invoke(result, "testFrozenMapClear", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: invalid map insertion: modification not " +
+                    "allowed on frozen value.*")
+    public void testFrozenInnerMapUpdate() {
+        BRunUtil.invoke(result, "testFrozenInnerMapUpdate", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to remove element from map: " +
+                    "modification not allowed on frozen value.*")
+    public void testFrozenInnerMapRemoval() {
+        BRunUtil.invoke(result, "testFrozenInnerMapRemoval", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to clear map: modification not allowed" +
+                    " on frozen value.*")
+    public void testFrozenInnerMapClear() {
+        BRunUtil.invoke(result, "testFrozenInnerMapClear", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to add element: modification not " +
+                    "allowed on frozen value.*")
+    public void testFrozenAnyArrayAddition() {
+        BRunUtil.invoke(result, "testFrozenAnyArrayAddition", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to add element: modification not " +
+                    "allowed on frozen value.*")
+    public void testFrozenAnyArrayUpdate() {
+        BRunUtil.invoke(result, "testFrozenAnyArrayUpdate", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: invalid update of record field: modification " +
+                    "not allowed on frozen value.*")
+    public void testFrozenAnyArrayElementUpdate() {
+        BRunUtil.invoke(result, "testFrozenAnyArrayElementUpdate", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to add element: modification not " +
+                    "allowed on frozen value.*")
+    public void testFrozenTupleUpdate() {
+        BRunUtil.invoke(result, "testFrozenTupleUpdate", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: invalid update of record field: modification " +
+                    "not allowed on frozen value.*")
+    public void testFrozenRecordUpdate() {
+        BRunUtil.invoke(result, "testFrozenRecordUpdate", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: invalid update of record field: modification " +
+                    "not allowed on frozen value.*")
+    public void testFrozenInnerRecordUpdate() {
+        BRunUtil.invoke(result, "testFrozenInnerRecordUpdate", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to add data to the table: modification" +
+                    " not allowed on frozen value.*")
+    public void testFrozenTableAddition() {
+        BRunUtil.invoke(result, "testFrozenTableAddition", new BValue[0]);
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+            expectedExceptionsMessageRegExp = ".*error: error, message: failed to remove data from the table: " +
+                    "modification not allowed on frozen value.*")
+    public void testFrozenTableRemoval() {
+        BRunUtil.invoke(result, "testFrozenTableRemoval", new BValue[0]);
+    }
+
     @DataProvider(name = "booleanValues")
     public Object[][] booleanValues() {
         return new Object[][]{
@@ -134,6 +334,17 @@ public class FreezeAndIsFrozenTest {
         return new Object[][]{
                 {"a"},
                 {"Hello, from Ballerina!"}
+        };
+    }
+
+    @DataProvider(name = "frozenBasicTypeArrayModificationFunctions")
+    public Object[][] frozenBasicTypeArrayModificationFunctions() {
+        return new Object[][]{
+                {"testFrozenIntArrayModification"},
+                {"testFrozenByteArrayModification"},
+                {"testFrozenBooleanArrayModification"},
+                {"testFrozenFloatArrayModification"},
+                {"testFrozenStringArrayModification"}
         };
     }
 }
--- tests/ballerina-unit-test/src/test/resources/test-src/expressions/builtinfunctions/freeze-and-isfrozen.bal
+++ tests/ballerina-unit-test/src/test/resources/test-src/expressions/builtinfunctions/freeze-and-isfrozen.bal
@@ -57,3 +57,290 @@ function testBasicTypesAsJson() returns boolean {
     b = a.freeze();
     return equals && a == b;
 }
+
+function testIsFrozenOnStructuralTypes() returns (boolean, boolean) {
+    Employee e = { name: "Em" };
+    map m = { one: "1", two: 2, three: (), rec: e };
+
+    any[] a = [1, "hi", 2.0, false, m, (), e];
+    (int, string, Employee) t = (1, "Em", e);
+    
+    json j = { name: "Em", dataType: "json" };
+    xml x = xml `<bookItem>The Lost World</bookItem>`;
+
+    table<Employee> empTable = table {
+        { primarykey id, name },
+        [
+            { 1, "Mary" },
+            { 2, "John" },
+            { 3, "Jim" }
+        ]
+    };
+
+    byte byteVal = 255;
+    map m1 = { intVal: 1, byteVal: byteVal, floatVal: 200.1, stringVal: "Ballerina says freeze", booleanVal: false,
+                arrayVal: a, mapVal: m, tupleVal: t, jsonVal: j, xmlVal: x, tableVal: empTable };
+    boolean isFrozenBeforeFreeze = m.isFrozen() || a.isFrozen() || m1.isFrozen() || e.isFrozen() || t.isFrozen() ||
+                                    j.isFrozen() || x.isFrozen() || empTable.isFrozen();
+
+    map m2 = m1.freeze();
+    boolean isFrozenAfterFreeze = m.isFrozen() && a.isFrozen() && m1.isFrozen() && e.isFrozen() && t.isFrozen() &&
+                                    m2.isFrozen() && j.isFrozen() && x.isFrozen() && empTable.isFrozen();
+    return (isFrozenBeforeFreeze, isFrozenAfterFreeze);
+}
+
+function testFrozenIntArrayModification() {
+    int[] a = [1, 2, 3];
+    _ = a.freeze();
+    a[4] = 200;
+}
+
+function testFrozenBooleanArrayModification() {
+    boolean[] a = [false, true];
+    _ = a.freeze();
+    a[1] = true;
+}
+
+function testFrozenByteArrayModification() {
+    byte[] a = [1, 2, 3];
+    _ = a.freeze();
+    a[5] = 200;
+}
+
+function testFrozenFloatArrayModification() {
+    float[] a = [1.0, 0.2, 300.23];
+    _ = a.freeze();
+    a[1] = 200.23;
+}
+
+function testFrozenStringArrayModification() {
+    string[] a = ["ballerina", "2", "hello world"];
+    _ = a.freeze();
+    a[1] = "addition to frozen array";
+}
+
+function testFrozenJsonArrayModification() {
+    json j = { hello: "world "};
+    json[] a = [j, "ballerina", 2, 10.3];
+    _ = a.freeze();
+    a[8] = j;
+}
+
+json j1 = { name: "Em", dataType: "json", pos: "inner" };
+json j2 = { name: "Zee", dataType: "json", pos: "outer", innerJson: j1 };
+
+function testFrozenJsonModification() {
+    _ = j2.freeze();
+    j2.name = "Em";
+}
+
+function testAdditionToFrozenJson() {
+    _ = j2.freeze();
+    j2.newField = "Em";
+}
+
+function testRemovalFromFrozenJson() {
+    json j3 = j2.freeze();
+    _ = j3.remove("name");
+}
+
+function testFrozenInnerJsonModification() {
+    _ = j2.freeze();
+    j2.innerJson.name = "Em";
+}
+
+function testAdditionToFrozenInnerJson() {
+    _ = j2.freeze();
+    j2.innerJson.newField = "Em";
+}
+
+function testRemovalFromFrozenInnerJson() {
+    json j3 = j2.freeze();
+    _ = j3.innerJson.remove("name");
+}
+
+function testFrozenXmlAppendChildren() {
+    xml x1 = xml `<book>The Lost World</book>`;
+    xml x2 = xml `<author>Doyle</author>`;
+
+    xml x3 = x1.freeze();
+    x3.appendChildren(x2);
+}
+
+function testFrozenXmlRemoveChildren() {
+    xml x1 = xml `<book>The Lost World<author>Doyle</author></book>`;
+    xml x2 = x1.freeze();
+    x2.removeChildren("author");
+}
+
+function testFrozenXmlRemoveAttribute() {
+    xml x1 = xml `<book attr="one">The Lost World</book>`;
+    _ = x1.freeze();
+    x1.removeAttribute("attr");
+}
+
+function testFrozenXmlSetAttributes() {
+    map m = { attr1: "one", attr2: "two"};
+    xml x1 = xml `<book>The Lost World</book>`;
+    _ = x1.freeze();
+    x1.setAttributes(m);
+}
+
+function testFrozenXmlSetChildren() {
+    xml x1 = xml `<book>The Lost World</book>`;
+    xml x2 = xml `<author>Doyle</author>`;
+
+    xml x3 = x1.freeze();
+    x3.setChildren(x2);
+}
+
+function testFrozenMapUpdate() {
+    map m1 = { one: "1", two: 2 };
+    map m2 = { one: "21", two: 22, mapVal: m1 };
+
+    _ = m2.freeze();
+    m2.one = 22;
+}
+
+function testFrozenMapRemoval() {
+    map m1 = { one: "1", two: 2 };
+    map m2 = { one: "21", two: 22, mapVal: m1 };
+
+    _ = m2.freeze();
+    _ = m2.remove("one");
+}
+
+function testFrozenMapClear() {
+    map m1 = { one: "1", two: 2 };
+
+    _ = m1.freeze();
+    m1.clear();
+}
+
+function testFrozenInnerMapUpdate() {
+    map m1 = { one: "1", two: 2 };
+    map m2 = { one: "21", two: 22, mapVal: m1 };
+
+    _ = m2.freeze();
+    m1["one"] = 12;
+}
+
+function testFrozenInnerMapRemoval() {
+    map m1 = { one: "1", two: 2 };
+    map m2 = { one: "21", two: 22, mapVal: m1 };
+
+    _ = m2.freeze();
+    map m3 = check <map> m2.mapVal;
+    _ = m3.remove("one");
+}
+
+function testFrozenInnerMapClear() {
+    map m1 = { one: "1", two: 2 };
+    map m2 = { one: "21", two: 22, mapVal: m1 };
+
+    _ = m2.freeze();
+    m1.clear();
+}
+
+function testFrozen2DBasicTypedArrayUpdate() {
+    int[][] i1 = [[1, 2], [3]];
+    _ = i1.freeze();
+    i1[0][0] = 9;
+}
+
+function testFrozenAnyArrayAddition() {
+    Employee e1 = { name: "Em", id: 1000 };
+    int[] i = [1, 2];
+    any[] i1 = [i, e1];
+    _ = i1.freeze();
+    i1[3] = "freeze addition";
+}
+
+function testFrozenAnyArrayUpdate() {
+    Employee e1 = { name: "Em", id: 1000 };
+    int[] i = [1, 2];
+    any[] i1 = [i, e1];
+    _ = i1.freeze();
+    Employee e2 = check <Employee> i1[1];
+    i1[1] = 100;
+}
+
+function testFrozenAnyArrayElementUpdate() {
+    Employee e1 = { name: "Em", id: 1000 };
+    int[] i = [1, 2];
+    any[] i1 = [i, e1];
+    _ = i1.freeze();
+    Employee e2 = check <Employee> i1[1];
+    e2["name"] = "Zee";
+}
+
+function testFrozenTupleUpdate() {
+    Employee e1 = { name: "Em", id: 1000 };
+    (int, Employee) t1 = (1, e1);
+    _ = t1.freeze();
+    Employee e2 = { name: "Zee", id: 1200 };
+    t1[1] = e2;
+}
+
+function testFrozenRecordUpdate() {
+    Dept d1 = { code: "fe11", name: "finance" };
+    DeptEmployee e1 = { name: "Em", id: 1000, dept: d1 };
+    DeptEmployee e3 = e1.freeze();
+    e3.name = "Zee";
+}
+
+function testFrozenInnerRecordUpdate() {
+    Dept d1 = { code: "fe11", name: "finance" };
+    DeptEmployee e1 = { name: "Em", id: 1000, dept: d1 };
+    _ = e1.freeze();
+    Dept d2 = e1.dept;
+    d2.code = "fe12";
+}
+
+function testFrozenTableAddition() {
+    table<Employee> empTable = table {
+        { primarykey id, name },
+        [
+            { 1, "Mary" },
+            { 2, "John" },
+            { 3, "Jim" }
+        ]
+    };
+    Employee e = { id: 5, name: "Anne" };
+    _ = empTable.freeze();
+    _ = empTable.add(e);
+}
+
+function testFrozenTableRemoval() {
+    table<Employee> empTable = table {
+        { primarykey id, name },
+        [
+            { 1, "Mary" },
+            { 2, "John" },
+            { 3, "Jim" }
+        ]
+    };
+    _ = empTable.freeze();
+    _ = empTable.remove(isIdTwo);
+}
+
+function isIdTwo(Employee e) returns boolean {
+    return e.id == 2;
+}
+
+type Employee record {
+    int id;
+    string name;
+};
+
+type DeptEmployee record {
+    int id;
+    string name;
+    Dept dept;
+};
+
+type Dept record {
+    string code;
+    string name;
+
+};
