d426e4360de82b999ca138704746155be46c2ba5
shan1024 <shanm@wso2.com>
2018-10-29 10:54:47 +0530

    Add initial tests

---
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/types/constant/ConstantTest.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.ballerinalang.test.types.constant;
+
+import org.ballerinalang.launcher.util.BCompileUtil;
+import org.ballerinalang.launcher.util.BRunUtil;
+import org.ballerinalang.launcher.util.CompileResult;
+import org.ballerinalang.model.values.BValue;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Constant test cases.
+ */
+public class ConstantTest {
+
+    private static CompileResult compileResult;
+
+    @BeforeClass
+    public void setup() {
+        compileResult = BCompileUtil.compile("test-src/types/constant/constant.bal");
+    }
+
+    @Test
+    public void getName() {
+        BValue[] returns = BRunUtil.invoke(compileResult, "getName");
+        Assert.assertNotNull(returns[0]);
+        Assert.assertEquals(returns[0].stringValue(), "Ballerina");
+    }
+}
---
+++ tests/ballerina-unit-test/src/test/resources/test-src/types/constant/constant.bal
@@ -0,0 +1,20 @@
+//string name = "Ballerina";
+//const string name = "Ballerina";
+
+//function getName() returns string {
+//    return name;
+//}
+
+const Name firstName = "Shan";
+
+type Name "Shan"|"Mahanama";
+
+Name lastName = "Mahanama";
+
+const s = "Bal";
+
+
+function getName() returns string {
+    string abc = s;
+    return abc + "lerina";
+}
