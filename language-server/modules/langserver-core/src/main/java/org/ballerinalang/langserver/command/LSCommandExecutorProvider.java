f87b9d8636f02d1a4f43ae1896f6b1caa6abaa18
Nadeeshaan <nadeeshaangunasinghe@gmail.com>
2018-10-30 16:46:11 +0530

    Fix checkstyle issue

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/command/LSCommandExecutorProvider.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/command/LSCommandExecutorProvider.java
@@ -24,7 +24,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.ServiceLoader;
-import java.util.stream.Collectors;
 
 /**
  * Loads and provides the Command Executor Providers.
