b84f18cd3e2ed1988138360c58f367114848b713
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-10-30 14:32:12 +0530

    Use composer webpack distribution for rendering tests

--- tests/composer-integration-tests/jest.config.js
+++ tests/composer-integration-tests/jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    "testEnvironment": "node",
+    "testEnvironment": "jsdom",
     'roots': [
         '<rootDir>/tests'
     ],
     'transform': {
         '.*\.tsx?$': 'ts-jest',
-        "^.+\\.ts$": "ts-jest"
+        "^.+\\.ts$": "ts-jest",
+        ".+\\.(css|styl|less|sass|scss)$": "<rootDir>/node_modules/jest-css-modules-transform"
     },
     'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     'moduleFileExtensions': [
@@ -20,5 +21,8 @@ module.exports = {
         "!**/build/**",
         "**/src/**",
         "!**/node_modules/**",
-    ]
+    ],
+    "moduleNameMapper": {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js"
+    }
 }
