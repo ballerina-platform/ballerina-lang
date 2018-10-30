d2f2ab3432a01e26b81c73ab132e0967b9dfd61e
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-10-30 11:34:10 +0530

    Add initial rendering tests for diagram

---
+++ tests/composer-integration-tests/tests/types.d.ts
@@ -0,0 +1,20 @@
+
+declare module '@ballerina/diagram' {
+    
+    export interface BallerinaDiagramWrapperProps {
+        getAST: (uri: string) => Thenable<any>;
+        onChange: (t: any) => any;
+        docUri: string;
+        width: number;
+        height: number;
+        getEndpoints: (t: any) => any;
+        parseFragment: (t: any) => any;
+        goToSource: (t: any) => any;
+    }
+    export function renderStaticDiagram(args: any, callback: any) : void;
+    export function renderEditableDiagram(target: HTMLElement | null,
+        docUri: string, width: number, height: number, getAST: Function) : void;
+    export class BallerinaDiagramWrapper extends React.Component<BallerinaDiagramWrapperProps> {
+    }
+}
+
