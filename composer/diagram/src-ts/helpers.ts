da5dde7f91c96cd368ba54d6766bd5ad04688492
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-11-07 09:53:09 +0530

    Configure typescript, jest & storybooks for diagram

---
+++ composer/diagram/src-ts/helpers.ts
@@ -0,0 +1,10 @@
+export interface StaticDiagramArgs {
+    target: HTMLElement;
+    documentUri: string;
+    width: number;
+    height: number;
+}
+
+export const renderStaticDiagram = (args: StaticDiagramArgs) => {
+
+}
