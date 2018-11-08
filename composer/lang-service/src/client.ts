cdfc13ab0c71d9f87de6b8323a12755fbec46c6e
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-11-08 11:14:30 +0530

    Add missing apis to monaco lang-client

--- composer/lang-service/src/client.ts
+++ composer/lang-service/src/client.ts
@@ -1,4 +1,4 @@
-import { BallerinaAST } from '@ballerina/ast-model';
+import { BallerinaAST, BallerinaASTNode, BallerinaEndpoint, BallerinaSourceFragment } from '@ballerina/ast-model';
 import { ChildProcess } from "child_process";
 // tslint:disable-next-line:no-submodule-imports
 import { IConnection } from "monaco-languageclient/lib/connection";
@@ -16,6 +16,48 @@ export interface GetASTResponse {
     parseSuccess: boolean;
 }
 
+export interface ASTDidChangeResponse {
+    content?: string;
+}
+
+export interface ASTDidChangeParams {
+    textDocumentIdentifier: {
+        uri: string;
+    };
+    ast: BallerinaAST;
+}
+
+export interface BallerinaExample {
+    title: string;
+    url: string;
+}
+
+export interface BallerinaExampleCategory {
+    title: string;
+    column: number;
+    samples: BallerinaExample[];
+}   
+
+export interface BallerinaExampleListParams {
+    filter?: string;
+}
+
+export interface BallerinaExampleListResponse {
+    samples: BallerinaExampleCategory[];
+}
+
+export interface BallerinaProject {
+    path?: string;
+    version?: string;
+    author?: string;
+}
+
+export interface GetBallerinaProjectParams {
+    documentIdentifier: {
+        uri: string;
+    };
+}
+
 export class BallerinaLangClient {
 
     constructor(
@@ -26,6 +68,27 @@ export class BallerinaLangClient {
 
     public getAST(params: GetASTParams): Thenable<GetASTResponse>{
         return this.lsConnection.sendRequest<GetASTResponse>("ballerinaDocument/ast", params);
+    }
+
+    public astDidChange(params: ASTDidChangeParams): Thenable<ASTDidChangeResponse> {
+        return this.lsConnection.sendRequest("ballerinaDocument/astDidChange", params);
+    }
+
+    public fetchExamples(params: BallerinaExampleListParams = {}): Thenable<BallerinaExampleListResponse> {
+        return this.lsConnection.sendRequest("ballerinaExample/list", params);
+    }
+
+    public parseFragment(params: BallerinaSourceFragment): Thenable<BallerinaASTNode> {
+        return this.lsConnection.sendRequest("ballerinaFragment/ast", params).then((resp: any)=> resp.ast);
+    }
+
+    public getEndpoints(): Thenable<BallerinaEndpoint[]> {
+        return this.lsConnection.sendRequest("ballerinaSymbol/endpoints", {})
+                    .then((resp: any) => resp.endpoints);
+    }
+
+    public getBallerinaProject(params: GetBallerinaProjectParams): Thenable<BallerinaProject> {
+        return this.lsConnection.sendRequest("ballerinaDocument/project", params);
     }
 
     public kill(): void {
