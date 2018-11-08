10fcd872d55e77e1ca9b14ccf3973aa216563765
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-11-08 20:02:37 +0530

    Update stories & snapshots to use generated BBE asts

--- composer/diagram/tests/EditableDiagram.test.tsx
+++ composer/diagram/tests/EditableDiagram.test.tsx
@@ -1,9 +1,23 @@
+import { BallerinaAST } from "@ballerina/ast-model";
 import * as React from "react";
 import { create } from "react-test-renderer";
-import { DiagramEditor } from "../src-ts/DiagramEditor";
+import bbeASTs from "../resources/bbe-asts.json";
+import { Diagram } from "../src-ts";
+import { MockLangClient } from "./utils";
 
-// test("DiagramEditor renders properly", () => {
-//     const component = create(<DiagramEditor />);
-//     const tree = component.toJSON();
-//     expect(tree).toMatchSnapshot();
-// });
+const bbeASTsArray = bbeASTs as Array<{ bbe: string, ast: BallerinaAST, title: string }>;
+
+bbeASTsArray.forEach((bbeAST) => {
+    test(`Diagram for ${bbeAST.title} renders properly`, () => {
+        const component = create(
+            <Diagram
+              docUri={bbeAST.bbe}
+              height={1000}
+              width={1000}
+              langClient={new MockLangClient(bbeAST.ast)}
+            />,
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
