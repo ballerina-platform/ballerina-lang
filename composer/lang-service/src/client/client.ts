76b807ed3cf5ff9c4b3c41b7ac4f67bdeb1b727e
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-11-11 06:46:32 +0530

    Revamp ls module to support both stdio & websockets

---
+++ composer/lang-service/src/client/client.ts
@@ -0,0 +1,60 @@
+import { BallerinaASTNode, BallerinaEndpoint, BallerinaSourceFragment } from "@ballerina/ast-model";
+// tslint:disable-next-line:no-submodule-imports
+import { IConnection } from "monaco-languageclient/lib/connection";
+import { InitializeParams, InitializeResult } from "vscode-languageserver-protocol";
+import { ASTDidChangeParams, ASTDidChangeResponse, BallerinaExampleListParams,
+    BallerinaExampleListResponse, BallerinaProject, GetASTParams, GetASTResponse,
+    GetBallerinaProjectParams, IBallerinaLangClient } from "./model";
+
+export class BallerinaLangClient implements IBallerinaLangClient {
+
+    constructor(
+        public lsConnection: IConnection) {
+    }
+
+    public init(params: InitializeParams = initParams): Thenable<InitializeResult> {
+        this.lsConnection.listen();
+        return this.lsConnection.initialize(params);
+    }
+
+    public getAST(params: GetASTParams): Thenable<GetASTResponse> {
+        return this.lsConnection.sendRequest<GetASTResponse>("ballerinaDocument/ast", params);
+    }
+
+    public astDidChange(params: ASTDidChangeParams): Thenable<ASTDidChangeResponse> {
+        return this.lsConnection.sendRequest("ballerinaDocument/astDidChange", params);
+    }
+
+    public fetchExamples(params: BallerinaExampleListParams = {}): Thenable<BallerinaExampleListResponse> {
+        return this.lsConnection.sendRequest("ballerinaExample/list", params);
+    }
+
+    public parseFragment(params: BallerinaSourceFragment): Thenable<BallerinaASTNode> {
+        return this.lsConnection.sendRequest("ballerinaFragment/ast", params).then((resp: any) => resp.ast);
+    }
+
+    public getEndpoints(): Thenable<BallerinaEndpoint[]> {
+        return this.lsConnection.sendRequest("ballerinaSymbol/endpoints", {})
+                    .then((resp: any) => resp.endpoints);
+    }
+
+    public getBallerinaProject(params: GetBallerinaProjectParams): Thenable<BallerinaProject> {
+        return this.lsConnection.sendRequest("ballerinaDocument/project", params);
+    }
+
+    public goToSource(line: number, column: number): void {
+        // TODO
+    }
+
+    public close(): void {
+        this.lsConnection.shutdown();
+    }
+}
+
+export const initParams: InitializeParams = {
+    capabilities: {
+    },
+    processId: process.pid,
+    rootUri: null,
+    workspaceFolders: null,
+};
---
+++ composer/lang-service/tests/stdio-server.test.ts
@@ -0,0 +1,34 @@
+import { createStdioLangClient, IBallerinaLangClient, StdioBallerinaLangServer } from "../src";
+
+let client: IBallerinaLangClient;
+let server: StdioBallerinaLangServer;
+
+beforeAll((done) => {
+    server = new StdioBallerinaLangServer();
+    server.start();
+    // tslint:disable-next-line:no-empty
+    createStdioLangClient(server.lsProcess, () => {}, () => {})
+        .then((stdioClient) => {
+            client = stdioClient;
+            done();
+        });
+});
+
+test("stdio server init success", (done) => {
+    expect(client).toBeTruthy();
+    if (client) {
+        client.init()
+            .then((result) => {
+                expect(result.capabilities).toBeTruthy();
+                expect(result.capabilities.experimental.astProvider).toBeTruthy();
+                expect(result.capabilities.experimental.examplesProvider).toBeTruthy();
+                done();
+            });
+    }
+});
+
+afterAll((done) => {
+    client.close();
+    server.shutdown();
+    done();
+});
