f943a91b00aa49caeec63ead4fcd89d2b6278e52
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-11-12 17:05:44 +0530

    Add initial storybook connected to LS over websockets

--- composer/diagram/.storybook/webpack.config.js
+++ composer/diagram/.storybook/webpack.config.js
@@ -7,5 +7,7 @@ module.exports = (baseConfig, env, config) => {
   });
   config.plugins.push(new TSDocgenPlugin()); // optional
   config.resolve.extensions.push(".ts", ".tsx");
+  config.node = {};
+  config.node.net = "mock";
   return config;
 };
