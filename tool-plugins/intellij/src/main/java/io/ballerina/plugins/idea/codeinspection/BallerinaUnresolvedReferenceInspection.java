7266f8b0c43cbe450d9c600a1cfd3a0d4cc31276
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2018-11-15 17:47:04 +0530

    Add checkstyle fixes

--- tool-plugins/intellij/src/main/java/io/ballerina/plugins/idea/codeinspection/BallerinaUnresolvedReferenceInspection.java
+++ tool-plugins/intellij/src/main/java/io/ballerina/plugins/idea/codeinspection/BallerinaUnresolvedReferenceInspection.java
@@ -40,7 +40,6 @@ import io.ballerina.plugins.idea.psi.BallerinaPackageName;
 import io.ballerina.plugins.idea.psi.BallerinaPackageReference;
 import io.ballerina.plugins.idea.psi.BallerinaRecordKey;
 import io.ballerina.plugins.idea.psi.BallerinaResourceDefinition;
-import io.ballerina.plugins.idea.psi.BallerinaSimpleVariableReference;
 import io.ballerina.plugins.idea.psi.BallerinaTableQueryExpression;
 import io.ballerina.plugins.idea.psi.BallerinaTypes;
 import io.ballerina.plugins.idea.psi.BallerinaVariableDefinitionStatement;
