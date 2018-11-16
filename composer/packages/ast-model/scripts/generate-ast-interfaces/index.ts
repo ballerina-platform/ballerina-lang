93f62f8cd0a28b7e8c692b5485805e5f1000e7af
Aruna Herath <arunabherath@gmail.com>
2018-11-16 17:22:29 +0530

    Add util function generation to check if a AST node is of a given kind

--- composer/packages/ast-model/scripts/generate-ast-interfaces/index.ts
+++ composer/packages/ast-model/scripts/generate-ast-interfaces/index.ts
@@ -1,18 +1,23 @@
 import * as fs from "fs";
 import { fix } from "prettier-tslint";
-import { findModelInfo, genBaseVisitorFileCode, genInterfacesFileCode } from "./generate";
+import { findModelInfo, genBaseVisitorFileCode, genCheckKindUtilCode,
+    genInterfacesFileCode } from "./generate";
 import { forEachAST } from "./get-asts";
 
 const modelInfo: any = {};
 
 const AST_INTERFACES_PATH = "./src/ast-interfaces.ts";
 const BASE_VISITOR_PATH = "./src/base-visitor.ts";
+const CHECK_KIND_UTIL_PATH = "./src/check-kind-util.ts";
 
 forEachAST((ast) => {
     findModelInfo(ast, modelInfo);
 }).then(() => {
     fs.writeFileSync(AST_INTERFACES_PATH, genInterfacesFileCode(modelInfo));
     fix(AST_INTERFACES_PATH);
-    fs.writeFileSync(BASE_VISITOR_PATH, genBaseVisitorFileCode(Object.keys(modelInfo)));
+    const modelNames = Object.keys(modelInfo).sort();
+    fs.writeFileSync(BASE_VISITOR_PATH, genBaseVisitorFileCode(modelNames));
     fix(BASE_VISITOR_PATH);
+    fs.writeFileSync(CHECK_KIND_UTIL_PATH, genCheckKindUtilCode(modelNames));
+    fix(CHECK_KIND_UTIL_PATH);
 });
