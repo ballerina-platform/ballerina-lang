d983ba131688aa393b9a1bc3ada6654ea8bd632e
Supun Setunga <supun.setunga@gmail.com>
2018-11-19 18:38:43 +0530

    Make 'is' type check behave as 'isLike' for immutable values

--- tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/binaryoperations/TypeTestExprTest.java
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/binaryoperations/TypeTestExprTest.java
@@ -481,4 +481,20 @@ public class TypeTestExprTest {
         Assert.assertEquals(returns[0].stringValue(), "on");
         Assert.assertEquals(returns[1].stringValue(), "on");
     }
+
+    @Test
+    public void textFiniteType_1() {
+        BValue[] returns = BRunUtil.invoke(result, "textFiniteType_1");
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BString.class);
+        Assert.assertEquals(returns[0].stringValue(), "a is a fruit");
+    }
+
+    @Test
+    public void textFiniteType_2() {
+        BValue[] returns = BRunUtil.invoke(result, "textFiniteType_2");
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertSame(returns[0].getClass(), BString.class);
+        Assert.assertEquals(returns[0].stringValue(), "a is an Apple");
+    }
 }
