aa846edf6682973776d7557993fa6b0603a32f1d
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-11-19 10:44:09 +0530

    Refactor composer pkgs to use composer cli tool for building

--- composer/packages/diagram/.storybook/webpack.config.js
+++ composer/packages/diagram/.storybook/webpack.config.js
@@ -3,7 +3,7 @@ const TSDocgenPlugin = require("react-docgen-typescript-webpack-plugin");
 module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve("awesome-typescript-loader")
+    loader: require.resolve("ts-loader")
   });
   config.plugins.push(new TSDocgenPlugin()); // optional
   config.resolve.extensions.push(".ts", ".tsx");
--- composer/packages/diagram/src/config/default.ts
+++ composer/packages/diagram/src/config/default.ts
@@ -1,7 +1,2 @@
-
-
-
-
 export const DiagramConfig = {
-
-}
+};
