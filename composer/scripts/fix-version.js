25ff0b90b463a63d5659261458c2255af881bc52
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-11-21 10:19:04 +0530

    Format pkg json deps & avoid git commit in fix-versions cmd

--- composer/scripts/fix-version.js
+++ composer/scripts/fix-version.js
@@ -4,5 +4,5 @@ const { join } = require('path');
 
 const projectVersion = getProjectVersion();
 
-execSync("npx lerna version " + projectVersion + " --yes", { cwd: join(__dirname, "..")});
+execSync("npx lerna version " + projectVersion + " --yes --no-git-tag-version", { cwd: join(__dirname, "..")});
 console.log("updated project version" + projectVersion);
