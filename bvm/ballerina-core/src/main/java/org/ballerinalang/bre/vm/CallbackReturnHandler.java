60af52fef69a50201a9fd302d969b5addaae38f1
NatashaWso2 <nwijesekare@gmail.com>
2018-11-22 18:29:52 +0530

    Add runtime support for wait-for-all action and test cases

--- bvm/ballerina-core/src/main/java/org/ballerinalang/bre/vm/CallbackReturnHandler.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/bre/vm/CallbackReturnHandler.java
@@ -23,7 +23,10 @@ import org.ballerinalang.model.values.BBoolean;
 import org.ballerinalang.model.values.BByte;
 import org.ballerinalang.model.values.BFloat;
 import org.ballerinalang.model.values.BInteger;
+import org.ballerinalang.model.values.BMap;
 import org.ballerinalang.model.values.BString;
+import org.ballerinalang.util.codegen.cpentries.UTF8CPEntry;
+import java.util.Map;
 
 /**
  * This represents handler class which handles callback returns.
@@ -32,20 +35,23 @@ import org.ballerinalang.model.values.BString;
  */
 public class CallbackReturnHandler {
 
-    // TODO: fix bug second strand may also return a value
     static Strand handleReturn(Strand strand, BType expType, int retReg, SafeStrandCallback... callbacks) {
         try {
             strand.acquireExecutionLock();
             for (SafeStrandCallback callback : callbacks) {
                 callback.acquireDataLock();
-
+                // TODO: 11/22/18 this fails if there are mutltiple waits in a single strand
+//                if (strand.completed) {
+//                    return null;
+//                }
                 if (callback.returnDataAvailable()) {
                     handleReturn(strand.currentFrame, callback, expType, retReg);
+                    strand.completed = true;
                     callback.releaseDataLock();
                     return strand;
                 }
 
-                callback.setRetData(strand, expType, retReg);
+                callback.setRetData(strand, expType, retReg, false, 0);
                 callback.releaseDataLock();
             }
 
@@ -53,6 +59,59 @@ public class CallbackReturnHandler {
             strand.releaseExecutionLock();
         }
         return null;
+    }
+
+    static Strand handleReturn(Strand strand, int retReg, Map<Integer, SafeStrandCallback> callbacks) {
+        try {
+            strand.acquireExecutionLock();
+            for (Map.Entry<Integer, SafeStrandCallback> entry : callbacks.entrySet()) {
+                Integer keyReg = entry.getKey();
+                SafeStrandCallback strandCallback = entry.getValue();
+                strandCallback.acquireDataLock();
+
+                if (strandCallback.returnDataAvailable()) {
+                    handleWaitAllReturn(strand.currentFrame, strandCallback, retReg, keyReg);
+                    strand.callbacksToWaitFor.remove(keyReg);
+                    strandCallback.releaseDataLock();
+
+                    // Check if there are no strands to wait for
+                    if (strand.callbacksToWaitFor.isEmpty()) {
+                        return strand;
+                    }
+                } else {
+                    strandCallback.setRetData(strand, null, retReg, true, keyReg);
+                    strandCallback.releaseDataLock();
+                }
+            }
+
+        } finally {
+            strand.releaseExecutionLock();
+        }
+        return null;
+    }
+
+    private static void handleWaitAllReturn(StackFrame sf, SafeStrandCallback strandCallback, int retReg, int keyReg) {
+        String keyValue = ((UTF8CPEntry) sf.constPool[keyReg]).getValue();
+        switch (strandCallback.retType.getTag()) {
+            case TypeTags.INT_TAG:
+                ((BMap) sf.refRegs[retReg]).put(keyValue, new BInteger (strandCallback.getIntRetVal()));
+                break;
+            case TypeTags.BYTE_TAG:
+                ((BMap) sf.refRegs[retReg]).put(keyValue, new BByte((byte) strandCallback.getByteRetVal()));
+                break;
+            case TypeTags.FLOAT_TAG:
+                ((BMap) sf.refRegs[retReg]).put(keyValue, new BFloat(strandCallback.getFloatRetVal()));
+                break;
+            case TypeTags.STRING_TAG:
+                ((BMap) sf.refRegs[retReg]).put(keyValue, new BString(strandCallback.getStringRetVal()));
+                break;
+            case TypeTags.BOOLEAN_TAG:
+                ((BMap) sf.refRegs[retReg]).put(keyValue, new BBoolean(strandCallback.getBooleanRetVal() == 1));
+                break;
+            default:
+                ((BMap) sf.refRegs[retReg]).put(keyValue, strandCallback.getRefRetVal());
+                break;
+        }
     }
 
     private static void handleReturn(StackFrame sf, SafeStrandCallback strandCallback, BType expType, int retReg) {
--- tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/worker/WaitForAllActionsTest.java
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/worker/WaitForAllActionsTest.java
@@ -19,18 +19,21 @@ package org.ballerinalang.test.worker;
 import org.ballerinalang.launcher.util.BCompileUtil;
 import org.ballerinalang.launcher.util.BRunUtil;
 import org.ballerinalang.launcher.util.CompileResult;
+import org.ballerinalang.model.values.BMap;
 import org.ballerinalang.model.values.BValue;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Wait for all action related tests.
+ *
+ * @since 0.985.0
  */
 public class WaitForAllActionsTest {
-    // TODO: 11/21/18 Need to add the test cases one by one after the support is added
     private CompileResult result;
 
     @BeforeClass
@@ -41,5 +44,188 @@ public class WaitForAllActionsTest {
 
     @Test
     public void waitTest1() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest1");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("f1", "7");
+        expectedMap.put("f2", "22");
+        expectedMap.put("f3", "hello foo");
+        expectedMap.put("f4", "true");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest2() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest2");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("f1", "7");
+        expectedMap.put("str1", "hello foo");
+        expectedMap.put("f3", "150");
+        expectedMap.put("str2", "hello xyz");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest3() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest3");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("f1", "7");
+        expectedMap.put("f3", "150");
+        expectedMap.put("f5", "hello bar");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest4() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest4");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("f1", "7");
+        expectedMap.put("f4", "hello foo");
+        expectedMap.put("f2", "22");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest5() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest5");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("id", "66");
+        expectedMap.put("name", "hello foo");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest6() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest6");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("idField", "150");
+        expectedMap.put("stringField", "hello foo");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest7() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest7");
+        Assert.assertEquals(returns.length, 1);
+        Assert.assertEquals(returns[0].stringValue(), "7");
+    }
+
+    @Test
+    public void waitTest8() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest8");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("id", "8");
+        expectedMap.put("name", "hello moo");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest9() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest9");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("f1", "20");
+        expectedMap.put("f5", "hello foo");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest10() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest10");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("f1", "30");
+        expectedMap.put("f5", "hello xyz");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest11() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest11");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("f1", "30");
+        expectedMap.put("field", "hello bar");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+    @Test
+    public void waitTest12() {
+        BValue[] returns = BRunUtil.invoke(result, "waitTest12");
+
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put("id", "86");
+
+        Assert.assertEquals(returns.length, 1);
+        Map<String, String> resultMap = new HashMap<>();
+        ((BMap) returns[0]).getMap().forEach((o, o2) -> resultMap.put(o.toString(), o2.toString()));
+        Assert.assertTrue(mapsAreEqual(expectedMap, resultMap));
+    }
+
+
+    /**
+     * Util method to compare 2 maps
+     *
+     * @param expected expected map
+     * @param result   resulting map
+     * @return if both the maps are equal or not
+     */
+    private boolean mapsAreEqual(Map<String, String> expected, Map<String, String> result) {
+        for (String y : expected.keySet()) {
+            if (!result.containsKey(y)) {
+                return false;
+            } else {
+                if (!expected.get(y).equals(result.get(y))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
