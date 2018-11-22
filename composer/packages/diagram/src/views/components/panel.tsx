a6e9b210fbfe84b3076488f9a25b036906b03cf9
Joseph Fonseka <hevayo@gmail.com>
2018-11-22 13:55:25 +0530

    Position panel header and body

--- composer/packages/diagram/src/views/components/panel.tsx
+++ composer/packages/diagram/src/views/components/panel.tsx
@@ -1,23 +1,31 @@
 
 import * as React from "react";
-import { FunctionViewState , SimpleBBox} from "../../view-model/index";
+import { FunctionViewState, SimpleBBox } from "../../view-model/index";
 
-export const Panel: React.StatelessComponent<{ model: FunctionViewState }> = ({ model , children}) => {
+export const Panel: React.StatelessComponent<{ model: FunctionViewState }> = ({ model, children }) => {
 
     const body: SimpleBBox = model.body;
+    const header: SimpleBBox = model.header;
 
     return (
         <g className="panel">
             <g className="panel-header">
-                <g className="panel-body">
-                    <rect
-                        x={body.x}
-                        y={body.y}
-                        width={body.w}
-                        height={body.h}
-                    />
-                    {children}
-                </g>
+                <rect
+                    x={header.x}
+                    y={header.y}
+                    width={header.w}
+                    height={header.h}
+                />
             </g>
+            <g className="panel-body">
+                <rect
+                    x={body.x}
+                    y={body.y}
+                    width={body.w}
+                    height={body.h}
+                />
+                {children}
+            </g>
+
         </g>);
 };
