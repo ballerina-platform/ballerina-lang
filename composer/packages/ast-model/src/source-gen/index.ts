d4137d698c89931b041598dd39fc3c5397ad56ce
Aruna Herath <arunabherath@gmail.com>
2018-11-24 20:14:16 +0530

    Add attachNode function based on the previous implementation
    Also add some basic tests. Need update both attachNode and tests to cover all the possible scenarios.

--- composer/packages/ast-model/src/source-gen/index.ts
+++ composer/packages/ast-model/src/source-gen/index.ts
@@ -13,10 +13,13 @@ class SourceGenVisitor implements Visitor {
     }
 
     public getSource(): string {
-        return Object.keys(this.ws)
-            .map((i) => (parseInt(i, 10)))
-            .sort((a, b) => (a - b))
+        return this.getWSKeys()
             .reduce((source, i) => (source + this.ws[i].ws + this.ws[i].text), "");
+    }
+
+    public getWS(): any[] {
+        return this.getWSKeys()
+            .map((i) => (this.ws[i]));
     }
 
     public beginVisitASTNode(node: ASTNode) {
@@ -30,6 +33,12 @@ class SourceGenVisitor implements Visitor {
             }
         });
     }
+
+    private getWSKeys() {
+        return Object.keys(this.ws)
+            .map((i) => (parseInt(i, 10)))
+            .sort((a, b) => (a - b));
+    }
 }
 
 const sourceGenVisitor = new SourceGenVisitor();
@@ -38,4 +47,40 @@ export function genSource(node: ASTNode): string {
     sourceGenVisitor.reset();
     traversNode(node, sourceGenVisitor);
     return sourceGenVisitor.getSource();
+}
+
+export function getWS(node: ASTNode): any[] {
+    sourceGenVisitor.reset();
+    traversNode(node, sourceGenVisitor);
+    return sourceGenVisitor.getWS();
+}
+
+export function attachNode(newNode: ASTNode, tree: ASTNode, attachPoint: string, insertAt: number = 0) {
+    const newNodeWS = getWS(newNode);
+    const treeWS = getWS(tree);
+    const attachPointNodes: ASTNode[] = (tree as any)[attachPoint];
+
+    let startIndex = 1; // should be index of the first whitespace of the new node
+
+    if (attachPointNodes[insertAt - 1]) {
+        const attachPointWS = getWS(attachPointNodes[insertAt - 1]);
+        startIndex = attachPointWS[attachPointWS.length - 1].i + 1;
+    }
+
+    // get the diff between the current and should be index of the first ws of new node
+    const newNodeDiff = startIndex - newNodeWS[0].i;
+    // update new node's  ws
+    newNodeWS.forEach((ws) => {
+        ws.i = ws.i + newNodeDiff;
+    });
+
+    // get the range of new nodes ws. tree should be updated to accomadate these new ws.
+    const treeDiff = newNodeWS[newNodeWS.length - 1].i - startIndex + 1;
+    // update rest of the tree
+    treeWS.forEach((ws) => {
+        if (ws.i >= startIndex) {
+            ws.i = ws.i + treeDiff;
+        }
+    });
+    attachPointNodes[insertAt] = newNode;
 }
