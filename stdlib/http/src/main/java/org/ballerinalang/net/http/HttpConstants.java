18e83b6f9e2f6b5b2e75672dbb248ad31aac4e58
Rukshani <arukshani@gmail.com>
2018-11-24 13:53:47 +0530

    Fix HTTP mock server

--- stdlib/http/src/main/java/org/ballerinalang/net/http/HttpConstants.java
+++ stdlib/http/src/main/java/org/ballerinalang/net/http/HttpConstants.java
@@ -49,6 +49,7 @@ public class HttpConstants {
     public static final String PROTOCOL_HTTP = "http";
     public static final String PROTOCOL_PACKAGE_HTTP = "ballerina" + ORG_NAME_SEPARATOR + "http";
     public static final String HTTP_SERVICE_ENDPOINT_NAME = "Tballerina/http:Server;";
+    public static final String HTTP_MOCK_SERVER_ENDPOINT_NAME = "Tballerina/http:MockServer;";
     public static final String PROTOCOL_HTTPS = "https";
     public static final String HTTP_METHOD = "HTTP_METHOD";
     public static final String HTTP_STATUS_CODE = "HTTP_STATUS_CODE";
