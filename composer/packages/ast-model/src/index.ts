7fc45a6ea2406f51685f860ddb8c79489740e4fc
Aruna Herath <arunabherath@gmail.com>
2018-11-25 15:27:19 +0530

    Export the new genSource function through ASTUtil

--- composer/packages/ast-model/src/index.ts
+++ composer/packages/ast-model/src/index.ts
@@ -1,5 +1,11 @@
-export * from "./ast-util";
+import * as modelUtils from "./model-utils";
+import * as sourceUtils from "./source-gen";
+
+export const ASTUtil = {
+    ...sourceUtils,
+    ...modelUtils,
+};
+
 export * from "./ast-interfaces";
-export * from "./model-utils";
 export * from "./base-visitor";
 export * from "./check-kind-util";
