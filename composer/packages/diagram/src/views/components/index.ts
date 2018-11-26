2fe67031c86db5fae8a139c698869d3a7f421279
Joseph Fonseka <hevayo@gmail.com>
2018-11-26 17:56:09 +0530

    Implement while node rendering

--- composer/packages/diagram/src/views/components/index.ts
+++ composer/packages/diagram/src/views/components/index.ts
@@ -3,4 +3,6 @@ import { Statement } from "./statement";
 export * from "./function";
 export * from "./svg-canvas";
 export * from "./block";
+export * from "./while";
 export { Statement as ExpressionStatement};
+export { Statement as VariableDef};
