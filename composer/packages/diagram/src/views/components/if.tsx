ad1753f43c550c92f939d8eb0dcb9c8e6c8d7512
Joseph Fonseka <hevayo@gmail.com>
2018-11-26 21:14:08 +0530

    Fix If rendering issue

--- composer/packages/diagram/src/views/components/if.tsx
+++ composer/packages/diagram/src/views/components/if.tsx
@@ -1,5 +1,5 @@
 
-import { ASTUtil, If as IfNode} from "@ballerina/ast-model";
+import { ASTUtil, If as IfNode } from "@ballerina/ast-model";
 import * as React from "react";
 import { DiagramConfig } from "../../config/default";
 import { DiagramUtils } from "../../diagram/diagram-utils";
@@ -10,13 +10,12 @@ import { Condition } from "./condition";
 const config: DiagramConfig = DiagramUtils.getConfig();
 
 export const If: React.StatelessComponent<{
-        model: IfNode
-    }> = ({
-        model
-    }) => {
+    model: IfNode
+}> = ({
+    model
+}) => {
         const viewState: ViewState = model.viewState;
-        const body = [];
-        const elseBlock = [];
+        const children = [];
 
         const conditionProps = {
             expression: ASTUtil.genSource(model.condition),
@@ -26,10 +25,10 @@ export const If: React.StatelessComponent<{
         };
 
         // Continue Line
-        const p1 = { x: 0, y: 0};
-        const p2 = { x: 0, y: 0};
-        const p3 = { x: 0, y: 0};
-        const p4 = { x: 0, y: 0};
+        const p1 = { x: 0, y: 0 };
+        const p2 = { x: 0, y: 0 };
+        const p3 = { x: 0, y: 0 };
+        const p4 = { x: 0, y: 0 };
 
         p1.x = model.body.viewState.bBox.x;
         p1.y = model.body.viewState.bBox.y + model.body.viewState.bBox.h;
@@ -43,35 +42,39 @@ export const If: React.StatelessComponent<{
         p4.x = p1.x - (config.flowCtrl.header.height / 2);
         p4.y = conditionProps.y;
 
-        body.push(DiagramUtils.getComponents(model.body));
-        elseBlock.push(DiagramUtils.getComponents(model.elseStatement));
+        children.push(DiagramUtils.getComponents(model.body));
+        if (model.elseStatement) {
+            children.push(DiagramUtils.getComponents(model.elseStatement));
+        }
 
         // Return Line
-        const r1 = { x: 0, y: 0};
-        const r2 = { x: 0, y: 0};
-        const r3 = { x: 0, y: 0};
-        const r4 = { x: 0, y: 0};
+        const r1 = { x: 0, y: 0 };
+        const r2 = { x: 0, y: 0 };
+        const r3 = { x: 0, y: 0 };
+        const r4 = { x: 0, y: 0 };
 
         r1.x = conditionProps.x + (config.flowCtrl.header.height / 2);
         r1.y = conditionProps.y;
 
-        r2.x = conditionProps.x +  model.body.viewState.bBox.w;
+        r2.x = conditionProps.x + model.body.viewState.bBox.w;
         r2.y = r1.y;
 
         r3.x = r2.x;
         r3.y = p1.y + config.flowCtrl.whileGap;
+        if (model.elseStatement) {
+            r3.y += model.elseStatement.viewState.bBox.h;
+        }
 
-        r4.x =  conditionProps.x;
+        r4.x = conditionProps.x;
         r4.y = r3.y;
 
         return (
             <g className="panel">
-                <Condition {...conditionProps}/>
-                {body}
-                {elseBlock}
+                <Condition {...conditionProps} />
                 <polyline className="condition-line"
                     points={`${r1.x},${r1.y} ${r2.x},${r2.y} ${r3.x},${r3.y} ${r4.x},${r4.y}`}
                 />
                 <ArrowHead direction={"left"} {...r4} />
+                {children}
             </g>);
     };
