2fbf4fccda73886c7d93efb39bf8eb07ca2528e9
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-11-28 10:51:28 +0530

    Handle errors during get ast in LS

--- composer/packages/diagram/tools/update-bbe-asts.ts
+++ composer/packages/diagram/tools/update-bbe-asts.ts
@@ -5,10 +5,11 @@ import { existsSync, mkdirSync, writeFileSync } from "fs";
 import * as path from "path";
 import { sync as rimrafSync } from "rimraf";
 
+// tslint:disable:no-console
+
 const bbeASTsPath = path.join(__dirname, "..", "..", "resources", "bbe-asts");
 const bbeASTsIndexPath = path.join(__dirname, "..", "..", "resources", "bbe-asts.json");
 
-// tslint:disable-next-line:no-console
 console.log("Removing existing bbe ast jsons if any.");
 if (existsSync(bbeASTsPath)) {
   rimrafSync(bbeASTsPath);
@@ -28,7 +29,6 @@ createStdioLangClient(server.lsProcess as ChildProcess, () => {}, () => {})
       const bbes = getBBEs(process.env.BALLERINA_HOME);
       const promises: Array<Thenable<string>> = [];
       bbes.forEach((bbe, index) => {
-        // tslint:disable-next-line:no-console
         console.log("getting ast for ", bbe);
         promises.push(
           client.getAST({
@@ -39,18 +39,23 @@ createStdioLangClient(server.lsProcess as ChildProcess, () => {}, () => {})
             const bbeData = { bbe, ast: resp.ast, title: path.basename(bbe) };
             const jsonFileName = `${path.parse(bbe).name}.json`;
             const astPath = path.join(bbeASTsPath, jsonFileName);
-            // tslint:disable-next-line:no-console
             console.log("Writing data to ", astPath);
             writeFileSync(astPath, JSON.stringify(bbeData));
             return jsonFileName;
           })
         );
       });
+      const closeAll = () => {
+        client.close();
+        server.shutdown();
+      };
       Promise.all(promises)
         .then((responses) => {
           writeFileSync(bbeASTsIndexPath, JSON.stringify(responses));
-          client.close();
-          server.shutdown();
+          closeAll();
+        }, (reason) => {
+          console.log("Error while fetching AST", reason);
+          closeAll();
         });
       });
     }
