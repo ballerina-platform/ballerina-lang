5393d61b03c843c7e06dc86aa907cf37fd52b697
shan1024 <shanm@wso2.com>
2018-11-28 11:06:07 +0530

    Update json array stamping logic

--- bvm/ballerina-core/src/main/java/org/ballerinalang/model/values/BRefValueArray.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/model/values/BRefValueArray.java
@@ -122,9 +122,8 @@ public class BRefValueArray extends BNewArray implements Serializable {
                     arrayValues[i].stamp(type);
                 }
             }
-            if (this.arrayType != null) {
-                type = new BArrayType(type);
-            }
+            // Set the type as json[]. Otherwise it will be treated as a json and will fail.
+            type = new BArrayType(type);
         } else if (type.getTag() == TypeTags.UNION_TAG) {
             for (BType memberType : ((BUnionType) type).getMemberTypes()) {
                 if (CPU.checkIsLikeType(this, memberType)) {
