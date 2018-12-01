7c84c9dd5b4ba755686241d81606e3f06a285095
MaryamZi <maryamziyadm@gmail.com>
2018-12-01 21:51:53 +0530

    Consider constraint in table type assertion

--- bvm/ballerina-core/src/main/java/org/ballerinalang/model/types/BTableType.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/model/types/BTableType.java
@@ -70,4 +70,18 @@ public class BTableType extends BType {
             return super.toString() + "<" + constraint.getName() + ">";
         }
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!super.equals(obj) || !(obj instanceof BTableType)) {
+            return false;
+        }
+
+        BTableType other = (BTableType) obj;
+        if (constraint == other.constraint) {
+            return true;
+        }
+
+        return constraint.equals(other.constraint);
+    }
 }
