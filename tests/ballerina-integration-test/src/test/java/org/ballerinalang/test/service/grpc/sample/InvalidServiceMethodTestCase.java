51fe667cdf1a8c04b7e00d47cbcdfec74d85bd5f
warunaj@wso2.com <warudanu@gmail.com>
2018-12-05 21:44:32 +0530

    Fix integration test with conversion message

--- tests/ballerina-integration-test/src/test/java/org/ballerinalang/test/service/grpc/sample/InvalidServiceMethodTestCase.java
+++ tests/ballerina-integration-test/src/test/java/org/ballerinalang/test/service/grpc/sample/InvalidServiceMethodTestCase.java
@@ -77,7 +77,7 @@ public class InvalidServiceMethodTestCase extends GrpcBaseTest {
             "error is expected with Invalid protobuf byte sequence")
     public void testInvalidOutputResponse() {
         BFloat request = new BFloat(1000.5);
-        final String expectedMsg = "Error from Connector: {ballerina/grpc}INTERNAL - 'string' cannot be cast to " +
+        final String expectedMsg = "Error from Connector: {ballerina/grpc}INTERNAL - 'string' cannot be converted to " +
                 "'float'";
 
         BValue[] responses = BRunUtil.invoke(result, "testInvalidOutputResponse", new BValue[]{request});
