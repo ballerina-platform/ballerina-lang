fc09280c4ffab81844e23ec812862e43f5649db5
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-12-06 15:18:49 +0530

    Use Block component to render while node body

--- composer/packages/diagram/src/views/components/while.tsx
+++ composer/packages/diagram/src/views/components/while.tsx
@@ -5,6 +5,7 @@ import { DiagramConfig } from "../../config/default";
 import { DiagramUtils } from "../../diagram/diagram-utils";
 import { ViewState } from "../../view-model";
 import { ArrowHead } from "./arrow-head";
+import { Block } from "./block";
 import { Condition } from "./condition";
 
 const config: DiagramConfig = DiagramUtils.getConfig();
@@ -15,7 +16,6 @@ export const While: React.StatelessComponent<{
         model
     }) => {
         const viewState: ViewState = model.viewState;
-        const body = [];
 
         const conditionProps = {
             expression: ASTUtil.genSource(model.condition),
@@ -42,8 +42,6 @@ export const While: React.StatelessComponent<{
         p4.x = p1.x - (config.flowCtrl.condition.height / 2);
         p4.y = conditionProps.y;
 
-        body.push(DiagramUtils.getComponents(model.body));
-
         // Return Line
         const r1 = { x: 0, y: 0};
         const r2 = { x: 0, y: 0};
@@ -65,7 +63,6 @@ export const While: React.StatelessComponent<{
         return (
             <g className="worker-block">
                 <g className="condition-block">
-                    {body}
                     <polyline className="condition-line"
                         points={`${p1.x},${p1.y} ${p2.x},${p2.y} ${p3.x},${p3.y} ${p4.x},${p4.y}`}
                     />
@@ -75,6 +72,7 @@ export const While: React.StatelessComponent<{
                     <line className="hide-line" x1={p1.x} y1={p1.y + 1} x2={r4.x} y2={r4.y - 1} strokeLinecap="round" />
                     <ArrowHead direction={"right"} {...p4} />
                     <Condition {...conditionProps} astModel={model} />
+                    {model.body && <Block model={model.body} />}
                 </g>
             </g>);
     };
