5afb5aef4c157d6dcce3609455a29297a4aeba70
Manuri <amaya.uom@gmail.com>
2018-12-14 11:48:07 +0530

    Add constraint to cursor tables

--- bvm/ballerina-core/src/main/java/org/ballerinalang/model/values/BTable.java
+++ bvm/ballerina-core/src/main/java/org/ballerinalang/model/values/BTable.java
@@ -67,6 +67,16 @@ public class BTable implements BRefType<Object>, BCollection {
         this.type = BTypes.typeTable;
     }
 
+    public BTable(BStructureType constraintType) {
+        this.iterator = null;
+        this.tableProvider = null;
+        this.nextPrefetched = false;
+        this.hasNextVal = false;
+        this.tableName = null;
+        this.constraintType = constraintType;
+        this.type = new BTableType(this.constraintType);
+    }
+
     public BTable(String tableName, BStructureType constraintType) {
         this.nextPrefetched = false;
         this.hasNextVal = false;
