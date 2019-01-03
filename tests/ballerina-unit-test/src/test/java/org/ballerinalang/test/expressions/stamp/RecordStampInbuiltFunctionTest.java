40dafa48f3877e6fe1063d447694dedd2e2d31b4
mohan <mohan.vive@gmail.com>
2019-01-03 11:11:39 +0530

    Revert mistakenly removed test cases

--- tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/stamp/RecordStampInbuiltFunctionTest.java
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/stamp/RecordStampInbuiltFunctionTest.java
@@ -546,6 +546,40 @@ public class RecordStampInbuiltFunctionTest {
                 BAnydataType.class);
     }
 
+    @Test
+    public void testStampRecordToConstraintJSON() {
+
+        BValue[] results = BRunUtil.invoke(compileResult, "stampRecordToConstraintJSON");
+        BMap<String, BValue> mapValue0 = (BMap<String, BValue>) results[0];
+
+        Assert.assertEquals(results.length, 1);
+        Assert.assertEquals(mapValue0.getType().getClass(), BJSONType.class);
+        Assert.assertEquals(((BJSONType) mapValue0.getType()).getConstrainedType().getClass(), BRecordType.class);
+        Assert.assertEquals(((BJSONType) mapValue0.getType()).getConstrainedType().getName(), "Student");
+
+        Assert.assertEquals((mapValue0.getMap()).size(), 4);
+        Assert.assertEquals(((LinkedHashMap) mapValue0.getMap()).get("school").toString(), "Hindu College");
+        Assert.assertEquals(((BValue) ((LinkedHashMap) mapValue0.getMap()).get("school")).getType().getClass(),
+                BStringType.class);
+    }
+
+    @Test
+    public void testStampRecordToConstraintJSONV2() {
+
+        BValue[] results = BRunUtil.invoke(compileResult, "stampRecordToConstraintJSONV2");
+        BMap<String, BValue> mapValue0 = (BMap<String, BValue>) results[0];
+
+        Assert.assertEquals(results.length, 1);
+        Assert.assertEquals(mapValue0.getType().getClass(), BJSONType.class);
+        Assert.assertEquals(((BJSONType) mapValue0.getType()).getConstrainedType().getClass(), BRecordType.class);
+        Assert.assertEquals(((BJSONType) mapValue0.getType()).getConstrainedType().getName(), "Person");
+
+        Assert.assertEquals((mapValue0.getMap()).size(), 4);
+        Assert.assertEquals(((LinkedHashMap) mapValue0.getMap()).get("school").toString(), "Hindu College");
+        Assert.assertEquals(((BValue) ((LinkedHashMap) mapValue0.getMap()).get("school")).getType().getClass(),
+                BStringType.class);
+    }
+
     //---------------------------------- Negative Test cases ----------------------------------------------
 
     @Test
