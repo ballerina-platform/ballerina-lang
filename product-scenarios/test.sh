8e59edfc45b592db14471050dfe5e69842cb09f4
Manuri <amaya.uom@gmail.com>
2019-01-04 09:33:17 +0530

    Add template test.sh

---
+++ product-scenarios/test.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Copyright (c) 2018, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o xtrace
+
+HOME=`pwd`
+TEST_SCRIPT=test.sh
+
+function usage()
+{
+    echo "
+    Usage bash test.sh --input-dir /workspace/data-bucket/in --output-dir /workspace/data-bucket/out
+    Following are the expected input parameters. all of these are optional
+    --input-dir       | -i    : input directory for test.sh
+    --output-dir      | -o    : output directory for test.sh
+    "
+}
+
+# Process inputs
+# ex. bash test.sh --input-dir <path-to-input-dir> --output-dir <path-to-output-dir>
+optspec=":hiom-:"
+while getopts "$optspec" optchar; do
+    case "${optchar}" in
+        -)
+            case "${OPTARG}" in
+                input-dir)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    INPUT_DIR=$val
+                    ;;
+                output-dir)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    OUTPUT_DIR=$val
+                    ;;
+                mvn-opts)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    MAVEN_OPTS=$val
+                    ;;
+                *)
+                    usage
+                    if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+                        echo "Unknown option --${OPTARG}" >&2
+                    fi
+                    ;;
+            esac;;
+        h)
+            usage
+            exit 2
+            ;;
+        o)
+            OUTPUT_DIR=$val
+            ;;
+        m)
+            MVN_OPTS=$val
+            ;;
+        i)
+            INPUT_DIR=$val
+            ;;
+        *)
+            usage
+            if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
+                echo "Non-option argument: '-${OPTARG}'" >&2
+            fi
+            ;;
+    esac
+done
+
+echo "working Directory : ${HOME}"
+echo "input directory : ${INPUT_DIR}"
+echo "output directory : ${OUTPUT_DIR}"
+
+export DATA_BUCKET_LOCATION=${INPUT_DIR}
+
+#=============== Execute Scenarios ===============================================
+# YOUR TEST EXECUTION LOGIC GOES HERE
+# A sample execution for maven-based testng/junit tests is shown below.
+# For maven, we add -fae (fail-at-end), and a system property to reduce jar download log verbosity.
+
+mvn clean install -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+-fae -B -f ./pom.xml
+
+
+#=============== Copy Surefire Reports ===========================================
+# SUREFIRE REPORTS MUST NEED TO BE COPIED TO OUTPUT_DIR.
+# You need to preserve the folder structure in order to identify executed scenarios.
+echo "Copying surefire-reports to ${OUTPUT_DIR}"
+
+mkdir -p ${OUTPUT_DIR}
+find ./* -name "surefire-reports" -exec cp --parents -r {} ${OUTPUT_DIR} \;
