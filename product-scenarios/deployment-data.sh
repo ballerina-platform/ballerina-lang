66a6c0c98a20aec0ea2da81e80b0bf6a4c9e4f26
Manuri <amaya.uom@gmail.com>
2019-01-05 18:18:30 +0530

    Update test scripts

--- product-scenarios/deployment-data.sh
+++ product-scenarios/deployment-data.sh
@@ -1,1 +1,100 @@
 #!/bin/bash
+
+# Copyright (c) 2018, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o xtrace
+
+HOME=`pwd`
+
+function usage()
+{
+    echo "
+    Usage bash deployment-data.sh --input-dir /workspace/data-bucket/in --output-dir /workspace/data-bucket/out
+    Following are the expected input parameters. all of these are optional
+    --input-dir       | -i    : input directory for test.sh
+    --output-dir      | -o    : output directory for test.sh
+    "
+}
+
+# Process inputs
+# ex. bash test.sh --input-dir <path-to-input-dir> --output-dir <path-to-output-dir>
+optspec=":hio-:"
+while getopts "$optspec" optchar; do
+    case "${optchar}" in
+        -)
+            case "${OPTARG}" in
+                input-dir)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    INPUT_DIR=$val
+                    ;;
+                output-dir)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    OUTPUT_DIR=$val
+                    ;;
+            esac;;
+        h)
+            usage
+            exit 2
+            ;;
+        o)
+            OUTPUT_DIR=$val
+            ;;
+        i)
+            INPUT_DIR=$val
+            ;;
+        *)
+            usage
+            if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
+                echo "Non-option argument: '-${OPTARG}'" >&2
+            fi
+            ;;
+    esac
+done
+
+echo "working Directory : ${HOME}"
+echo "input directory : ${INPUT_DIR}"
+echo "output directory : ${OUTPUT_DIR}"
+
+export DATA_BUCKET_LOCATION=${INPUT_DIR}
+
+#=============== Run deployment configuration ===============================================
+# YOUR TEST EXECUTION LOGIC GOES HERE
+# A sample execution for maven-based testng/junit tests is shown below.
+# For maven, we add -fae (fail-at-end), and a system property to reduce jar download log verbosity.
+IFS='=' read -r -a array <<< "$(head -n 1 infrastructure.properties)"
+
+export DATABASE_HOST=${array[1]};
+export DATABASE_PORT=${array[3]};
+export DATABASE_NAME=${array[5]};
+export DATABASE_USERNAME=${array[7]};
+export DATABASE_PASSWORD=${array[9]};
+ClusterName${array[11]};
+
+wget https://product-dist.ballerina.io/downloads/0.990.2/ballerina-linux-installer-x64-0.990.2.deb
+sudo dpkg -i ballerina-linux-installer-x64-0.990.2.deb
+
+ballerina version
+
+ballerina build scenarios/1/data-service.bal
+
+kubectl apply -f scenarios/1/kubernetes/
+
+#=============== Copy Surefire Reports ===========================================
+# SUREFIRE REPORTS MUST NEED TO BE COPIED TO OUTPUT_DIR.
+# You need to preserve the folder structure in order to identify executed scenarios.
+echo "Copying surefire-reports to ${OUTPUT_DIR}"
+
+mkdir -p ${OUTPUT_DIR}
+find ./* -name "surefire-reports" -exec cp --parents -r {} ${OUTPUT_DIR} \;
---
+++ product-scenarios/scenarios/1/data-service.bal
@@ -0,0 +1,92 @@
+import ballerina/http;
+import ballerina/log;
+import ballerina/mysql;
+import ballerina/io;
+import ballerina/config;
+import ballerinax/kubernetes;
+
+mysql:Client testDB = new({
+        host: config:getAsString("DATABASE_HOST", default = "localhost"),
+        port: config:getAsInt("DATABASE_PORT", default = 3306),
+        name: config:getAsString("DATABASE_NAME", default = "EMPLOYEE_RECORDS"),
+        username: config:getAsString("DATABASE_USERNAME", default = "root"),
+        password: config:getAsString("DATABASE_PASSWORD", default = ""),
+        poolOptions: { maximumPoolSize: 5 },
+        dbOptions: { useSSL: false }
+    });
+
+type Employee record {
+    int id;
+    string name;
+};
+
+@kubernetes:Ingress {
+    hostname: "ballerina.scenariotests",
+    name: "ballerina-data-service",
+    path: "/"
+}
+
+@kubernetes:Service {
+    serviceType: "NodePort",
+    name: "ballerina-data-service"
+}
+
+@kubernetes:Deployment {
+    image: "ballerina.scenariotests/employee_database_service:v1.0",
+    baseImage: "ballerina/ballerina:0.990.2",
+    name: "ballerina-employee-database-service",
+    copyFiles: [{ target: "/ballerina/runtime/bre/lib",
+                source: "/home/manurip/Documents/Work/Tasks/Support/Tools/mysql-connector-java-5.1.46.jar" }]
+}
+listener http:Listener httpListener = new(9090);
+
+// By default, Ballerina assumes that the service is to be exposed via HTTP/1.1.
+service hello on httpListener {
+    // curl -v http://localhost:9090/hello/select
+    resource function select(http:Caller caller, http:Request req) {
+        http:Response res = new;
+
+        var dt = testDB->select("select * from selecttest where id = 1", Employee);
+
+        if (dt is table<Employee>) {
+            var employees = json.convert(dt);
+            if (employees is json) {
+                res.setPayload(untaint employees);
+                var ret = caller->respond(res);
+                if (ret is error) {
+                    log:printError("Error sending response", err = ret);
+                }
+            } else {
+                log:printError("Error converting table to json", err = employees);
+                panic employees;
+            }
+        } else if (dt is error) {
+            panic dt;
+        }
+    }
+
+    // curl -v http://localhost:9090/hello/insert -d'{"id":4, "name":"C"}' -H'Content-Type:application/json'
+    @http:ResourceConfig {
+        methods: ["POST"]
+    }
+    resource function insert(http:Caller caller, http:Request req) {
+        http:Response res = new;
+        var employee = req.getJsonPayload();
+
+        if (employee is json) {
+            var x = testDB->update("insert into inserttest values(?, ?)", employee.id.toString(), employee.name.
+                toString());
+
+            if (x is int) {
+                var ret = caller->respond(res);
+                if (ret is error) {
+                    log:printError("Error sending response", err = ret);
+                }
+            } else {
+                panic x;
+            }
+        } else {
+            panic employee;
+        }
+    }
+}
