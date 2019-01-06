b0bc3fb897e2a9aa9d97d1b2322758153a4aa841
VijithaEkanayake <ekvijitha@gmail.com>
2019-01-06 19:07:46 +0530

    Add cb scenarios and test scripts

--- product-scenarios/deployment-http.sh
+++ product-scenarios/deployment-http.sh
@@ -17,12 +17,11 @@
 set -o xtrace
 
 HOME=`pwd`
-TEST_SCRIPT=test.sh
 
 function usage()
 {
     echo "
-    Usage bash test.sh --input-dir /workspace/data-bucket/in --output-dir /workspace/data-bucket/out
+    Usage bash deployment-http.sh --input-dir /workspace/data-bucket/in --output-dir /workspace/data-bucket/out
     Following are the expected input parameters. all of these are optional
     --input-dir       | -i    : input directory for test.sh
     --output-dir      | -o    : output directory for test.sh
@@ -31,7 +30,7 @@ function usage()
 
 # Process inputs
 # ex. bash test.sh --input-dir <path-to-input-dir> --output-dir <path-to-output-dir>
-optspec=":hiom-:"
+optspec=":hio-:"
 while getopts "$optspec" optchar; do
     case "${optchar}" in
         -)
@@ -44,16 +43,6 @@ while getopts "$optspec" optchar; do
                     val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
                     OUTPUT_DIR=$val
                     ;;
-                mvn-opts)
-                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-                    MAVEN_OPTS=$val
-                    ;;
-                *)
-                    usage
-                    if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
-                        echo "Unknown option --${OPTARG}" >&2
-                    fi
-                    ;;
             esac;;
         h)
             usage
@@ -61,9 +50,6 @@ while getopts "$optspec" optchar; do
             ;;
         o)
             OUTPUT_DIR=$val
-            ;;
-        m)
-            MVN_OPTS=$val
             ;;
         i)
             INPUT_DIR=$val
@@ -83,23 +69,38 @@ echo "output directory : ${OUTPUT_DIR}"
 
 export DATA_BUCKET_LOCATION=${INPUT_DIR}
 
-#=============== Execute Scenarios ===============================================
+#=============== Run deployment configuration ===============================================
 # YOUR TEST EXECUTION LOGIC GOES HERE
 # A sample execution for maven-based testng/junit tests is shown below.
 # For maven, we add -fae (fail-at-end), and a system property to reduce jar download log verbosity.
+#IFS='=' read -r -a array <<< "$(head -n 1 infrastructure.properties)"
 
-IFS='=' read -r -a array <<< "$(head -n 1 $INPUT_DIR/deployment.properties)"
-EXTERNAL_IP=${array[1]};
+cat $INPUT_DIR/infrastructure.properties
+
+pwd
+ls
+
+# Read configuration into an associative array
+declare -A CONFIG
+# IFS is the 'internal field separator'. In this case, your file uses '='
+IFS="="
+while read -r key value
+do
+     CONFIG[$key]=$value
+done < $INPUT_DIR/infrastructure.properties
 unset IFS
 
-cat $INPUT_DIR/deployment.properties
+ClusterName${CONFIG[ClusterName]};
 
-curl http://$EXTERNAL-IP:80/cb -v
+wget https://product-dist.ballerina.io/downloads/0.990.2/ballerina-linux-installer-x64-0.990.2.deb
+sudo dpkg -i ballerina-linux-installer-x64-0.990.2.deb
 
-#=============== Copy Surefire Reports ===========================================
-# SUREFIRE REPORTS MUST NEED TO BE COPIED TO OUTPUT_DIR.
-# You need to preserve the folder structure in order to identify executed scenarios.
-echo "Copying surefire-reports to ${OUTPUT_DIR}"
+ballerina version
 
-mkdir -p ${OUTPUT_DIR}
-find ./* -name "surefire-reports" -exec cp --parents -r {} ${OUTPUT_DIR} \;
+ballerina build scenarios/2/
+
+kubectl apply -f scenarios/2/kubernetes/
+
+external_ip=kubectl get svc ballerina-circuit-breaker-service -o jsonpath='{.spec.externalIP}'
+
+echo "ExternalIP=$external_ip" >> $output_dir/deployment.properties
---
+++ product-scenarios/scenarios/2/http_circuit_breaker_frontend.bal
@@ -0,0 +1,71 @@
+import ballerina/http;
+import ballerina/log;
+import ballerina/runtime;
+import ballerinax/kubernetes;
+
+@kubernetes:Ingress {
+    hostname: "scenariotests.ballerina.io",
+    name: "ballerina-circuit-breaker-service",
+    path: "/"
+}
+
+@kubernetes:Service {
+    serviceType:"LoadBalancer",
+    port:80,
+    name: "ballerina-circuit-breaker-service"
+}
+
+@kubernetes:Deployment {
+    image: "scenariotests.ballerina.io/circuit_breaker_frontend_service:v1.0",
+    baseImage: "ballerina/ballerina:0.990.2",
+    name: "circuit_breaker_frontend_service"
+}
+listener http:Listener httpListener = new(9090);
+
+http:Client backendClientEP = new("http://ballerina-circuit-breaker-backend-service:8080", config = {
+        circuitBreaker: {
+            rollingWindow: {
+                timeWindowMillis: 10000,
+                bucketSizeMillis: 2000,
+                requestVolumeThreshold: 0
+
+            },
+            failureThreshold: 0.2,
+            resetTimeMillis: 10000,
+            statusCodes: [500, 501, 502, 503]
+        },
+        timeoutMillis: 2000
+    });
+
+// Create an HTTP service bound to the endpoint (circuitBreakerEP).
+@http:ServiceConfig {
+    basePath: "/cb"
+}
+service circuitbreaker on httpListener {
+    // Create a REST resource within the API.
+    @http:ResourceConfig {
+        methods: ["GET"],
+        path: "/"
+    }
+    // The parameters include a reference to the caller
+    // and an object of the request data.
+    resource function invokeEndpoint(http:Caller caller, http:Request request) {
+
+        var backendResponse = backendClientEP->forward("/hello", request);
+
+        if (backendResponse is http:Response) {
+            var responseToCaller = caller->respond(backendResponse);
+            if (responseToCaller is error) {
+                log:printError("Error sending response", err = responseToCaller);
+            }
+        } else {
+            http:Response response = new;
+            response.statusCode = http:INTERNAL_SERVER_ERROR_500;
+            response.setPayload(<string> backendResponse.detail().message);
+            var responseToCaller = caller->respond(response);
+            if (responseToCaller is error) {
+                log:printError("Error sending response", err = responseToCaller);
+            }
+        }
+    }
+}
