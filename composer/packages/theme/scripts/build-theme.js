97be3c3f1305a45bf51eb236bd52517d6ab72409
jeradrutnam <jeradrutnam@outlook.com>
2019-01-16 09:35:56 +0530

    Fix semantic UI to support less v3.9.0

--- composer/packages/theme/scripts/build-theme.js
+++ composer/packages/theme/scripts/build-theme.js
@@ -21,17 +21,50 @@ const less = require('less');
 const path = require('path');
 const fs = require('fs-extra');
 const rimraf = require('rimraf');
+const lessToJs = require('less-vars-to-js');
 
 const CleanCSS = require('clean-css');
 const RewriteImportPlugin = require("less-plugin-rewrite-import");
 const NpmImportPlugin = require('less-plugin-npm-import');
+const InlineUrlsPlugin = require('less-plugin-inline-urls');
 
 const libDir = path.join(__dirname, '..', 'lib');
 const buildDir = path.join(__dirname, '..', 'build');
 const themesDir = path.join(__dirname, '..', 'src', 'themes');
+const semanticImports = path.join(__dirname, '..', 'src', 'semantic.imports');
+const semanticIndexLess = path.join(buildDir, "semantic.less");
 
 const lessNpmModuleDir = path.dirname(require.resolve('less'));
 const semanticUILessModuleDir = path.join(lessNpmModuleDir, '..', 'semantic-ui-less');
+
+const createSemanticIndexLess = () => {
+    const src = fs.readFileSync(semanticImports, 'utf8');
+    const extractedVariables = lessToJs(src, {
+        resolveVariables: true,
+        stripPrefix: true
+    });
+    let lessString = '';
+
+    Object.keys(extractedVariables).map(key => {
+        const components = extractedVariables[key].replace(/\s/g, '').split(',');
+
+        components.map(component => {
+            lessString += '& {\n' +
+                '    @import "~semantic-ui-less/themes/default/' + key + '/' + component + '.variables";\n' +
+                '    @import "~semantic-ui-less/definitions/' + key + '/' + component + '.less";\n' +
+                '}\n';
+        });
+    });
+
+    fs.writeFileSync(semanticIndexLess, lessString, (error) => {
+        console.error("semantic.less generation failed.");
+        console.error(error);
+    });
+
+    copyAssets();
+
+    console.info("semantic.less generated.");
+};
 
 const generateThemes = () => {
     const themes = fs.readdirSync(themesDir);
@@ -39,8 +72,10 @@ const generateThemes = () => {
     let fileWritePromises = themes.map(theme => {
         const filePath = path.join(themesDir, theme, 'theme.less');
         const options = {
+            ieCompat: false,
             compress: false,
             sourceMap: true,
+            javascriptEnabled: true,
             filename: path.resolve(filePath),
             plugins: [
                 new NpmImportPlugin({ prefix: '~' }),
@@ -48,7 +83,8 @@ const generateThemes = () => {
                     paths: {
                         "../../theme.config": path.join(themesDir, theme, 'theme.config'),
                     }
-                })
+                }),
+                InlineUrlsPlugin
             ]
         };
 
@@ -99,17 +135,6 @@ const copyCSS = () => {
     fs.copy(buildDir, libDir)
         .then(() => {
             console.error('generated css files copied.');
-            copyAssets();
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-};
-
-const copyAssets = () => {
-    fs.copy(path.join(semanticUILessModuleDir, 'themes', 'default', 'assets'), path.join(libDir, 'assets'))
-        .then(() => {
-            console.error('semantic-ui-less assets copied.');
             fs.removeSync(buildDir);
             console.error('Done.');
         })
@@ -118,11 +143,23 @@ const copyAssets = () => {
         });
 };
 
+const copyAssets = () => {
+    fs.copy(path.join(semanticUILessModuleDir, 'themes', 'default', 'assets'), path.join(buildDir, 'assets'))
+        .then(() => {
+            console.error('semantic-ui-less assets copied.');
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
 if (!fs.existsSync(buildDir)) {
     fs.mkdirSync(buildDir);
+    createSemanticIndexLess();
     generateThemes();
 } else {
     rimraf(buildDir + '/*', () => {
+        createSemanticIndexLess();
         generateThemes();
     });
 }
