4022e7290bf2e8017bc41e2b3f318e72936cc3db
shan1024 <shanm@wso2.com>
2019-01-16 17:00:21 +0530

    Fix issue in test

--- tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/typedefs/TypeDefinitionsNegativeTest.java
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/typedefs/TypeDefinitionsNegativeTest.java
@@ -31,7 +31,7 @@ public class TypeDefinitionsNegativeTest {
     @Test
     public void testNegative() {
         CompileResult compileResult = BCompileUtil.compile("test-src/typedefs/type-definitions-negative.bal");
-        Assert.assertEquals(compileResult.getErrorCount(), 22);
+        Assert.assertEquals(compileResult.getErrorCount(), 24);
 
         int index = 0;
         BAssertUtil.validateError(compileResult, index++, "unknown type 'A'", 1, 9);
@@ -50,6 +50,7 @@ public class TypeDefinitionsNegativeTest {
         BAssertUtil.validateError(compileResult, index++, "unknown type 'E'", 13, 45);
         BAssertUtil.validateError(compileResult, index++, "unknown type 'F'", 13, 60);
         BAssertUtil.validateError(compileResult, index++, "unknown type 'G'", 13, 76);
+        BAssertUtil.validateError(compileResult, index++, "uninitialized field 'g'", 13, 76);
 
         BAssertUtil.validateError(compileResult, index++, "unknown type 'A'", 15, 17);
         BAssertUtil.validateError(compileResult, index++, "unknown type 'B'", 15, 23);
@@ -57,6 +58,7 @@ public class TypeDefinitionsNegativeTest {
         BAssertUtil.validateError(compileResult, index++, "unknown type 'D'", 15, 47);
         BAssertUtil.validateError(compileResult, index++, "unknown type 'E'", 15, 51);
         BAssertUtil.validateError(compileResult, index++, "unknown type 'F'", 15, 68);
-        BAssertUtil.validateError(compileResult, index, "unknown type 'G'", 15, 85);
+        BAssertUtil.validateError(compileResult, index++, "unknown type 'G'", 15, 85);
+        BAssertUtil.validateError(compileResult, index, "uninitialized field 'g'", 15, 85);
     }
 }
