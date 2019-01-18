e489dbd3a3e56c55a53f78fdaba6ec56b1d759fe
Manuri <amaya.uom@gmail.com>
2019-01-18 17:16:57 +0530

    Pass params from deployment script to bal as env vars
    This replaces the temp workaround applied to the issue caused by ballerinax/kubernetes#257

--- product-scenarios/deployment-data.sh
+++ product-scenarios/deployment-data.sh
@@ -90,16 +90,12 @@ do
 done < $INPUT_DIR/infrastructure.properties
 unset IFS
 
-#export DATABASE_HOST=${CONFIG[DatabaseHost]}
-#export DATABASE_PORT=${CONFIG[DatabasePort]}
-#export DATABASE_NAME=${CONFIG[DatabaseName]}
-#export DATABASE_USERNAME=${CONFIG[DBUsername]}
-#export DATABASE_PASSWORD=${CONFIG[DBPassword]}
-DATABASE_HOST=${CONFIG[DatabaseHost]}
-DATABASE_PORT=${CONFIG[DatabasePort]}
-DATABASE_NAME=test
-DATABASE_USERNAME=${CONFIG[DBUsername]}
-DATABASE_PASSWORD=${CONFIG[DBPassword]}
+export DATABASE_HOST=${CONFIG[DatabaseHost]}
+export DATABASE_PORT=${CONFIG[DatabasePort]}
+export DATABASE_NAME=test
+export DATABASE_USERNAME=${CONFIG[DBUsername]}
+export DATABASE_PASSWORD=${CONFIG[DBPassword]}
+
 ClusterName=${CONFIG[ClusterName]};
 ClusterRegion=${CONFIG[ClusterRegion]};
 
@@ -113,12 +109,6 @@ ballerina version
 wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.47.tar.gz --quiet
 
 tar -xvzf mysql-connector-java-5.1.47.tar.gz --directory ./
-
-sed -i "s/__DATABASE_HOST__/${DATABASE_HOST}/" product-scenarios/scenarios/1/data-service.bal
-sed -i "s/__DATABASE_PORT__/${DATABASE_PORT}/" product-scenarios/scenarios/1/data-service.bal
-sed -i "s/__DATABASE_NAME__/${DATABASE_NAME}/" product-scenarios/scenarios/1/data-service.bal
-sed -i "s/__DATABASE_USERNAME__/${DATABASE_USERNAME}/" product-scenarios/scenarios/1/data-service.bal
-sed -i "s/__DATABASE_PASSWORD__/${DATABASE_PASSWORD}/" product-scenarios/scenarios/1/data-service.bal
 
 # Write config to a custom location
 eksctl utils write-kubeconfig --name ${ClusterName} --region ${ClusterRegion} --kubeconfig "ballerina-config"
