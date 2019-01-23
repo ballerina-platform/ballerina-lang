df62269687ff9e01d04f60317ed6b9367ff1192c
Rasika <info.rasika@gmail.com>
2019-01-23 11:11:36 +0530

    Fx test cases
    Signed-off-by: Rasika <info.rasika@gmail.com>

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codelenses/CodeLensTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codelenses/CodeLensTest.java
@@ -17,10 +17,14 @@
  */
 package org.ballerinalang.langserver.codelenses;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import org.ballerinalang.langserver.compiler.LSContextManager;
 import org.ballerinalang.langserver.util.FileUtils;
 import org.ballerinalang.langserver.util.TestUtil;
+import org.eclipse.lsp4j.CodeLens;
+import org.eclipse.lsp4j.Command;
 import org.eclipse.lsp4j.jsonrpc.Endpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,6 +38,8 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Test code lens feature in language server.
@@ -42,8 +48,9 @@ public class CodeLensTest {
     private Path servicesBalPath = FileUtils.RES_DIR.resolve("codelens").resolve("services.bal");
     private Path functionsBalPath = FileUtils.RES_DIR.resolve("codelens").resolve("functions.bal");
     private Endpoint serviceEndpoint;
-    private JsonParser parser = new JsonParser();
+    private static final JsonParser JSON_PARSER = new JsonParser();
     private static final Logger log = LoggerFactory.getLogger(CodeLensTest.class);
+    private static final Gson GSON = new Gson();
 
     @BeforeClass
     public void loadLangServer() throws IOException {
@@ -58,21 +65,28 @@ public class CodeLensTest {
     }
 
     @Test(description = "Test Code Lenses for functions", dataProvider = "codeLensFunctionPositions")
-    public void codeLensForBuiltInFunctionTest(String expectedFile) throws IOException {
-        String response = TestUtil.getCodeLensesResponse(servicesBalPath.toString(), serviceEndpoint);
-        String expected = getExpectedValue(expectedFile);
-
-        Assert.assertEquals(parser.parse(response).getAsJsonObject(), parser.parse(expected).getAsJsonObject(),
-                            "Did not match the codelens content for " + expectedFile);
+    public void codeLensForBuiltInFunctionTest(String expectedConfigName) throws IOException {
+        String response = TestUtil.getCodeLensesResponse(functionsBalPath.toString(), serviceEndpoint);
+        testCodeLenses(expectedConfigName, response);
     }
 
     @Test(description = "Test Code Lenses for services", dataProvider = "codeLensServicesPositions")
-    public void codeLensForCurrentPackageFunctionTest(String expectedFile) throws IOException {
+    public void codeLensForCurrentPackageFunctionTest(String expectedConfigName) throws IOException {
         String response = TestUtil.getCodeLensesResponse(servicesBalPath.toString(), serviceEndpoint);
-        String expected = getExpectedValue(expectedFile);
+        testCodeLenses(expectedConfigName, response);
+    }
+    
+    private void testCodeLenses(String expectedConfigName, String response) throws IOException {
+        String expected = getExpectedValue(expectedConfigName);
 
-        Assert.assertEquals(parser.parse(response).getAsJsonObject(), parser.parse(expected).getAsJsonObject(),
-                            "Did not match the codelens content for " + expectedFile);
+        List<CodeLens> expectedItemList = getFlattenItemList(
+                JSON_PARSER.parse(expected).getAsJsonObject().getAsJsonArray("result"));
+        List<CodeLens> responseItemList = getFlattenItemList(
+                JSON_PARSER.parse(response).getAsJsonObject().getAsJsonArray("result"));
+
+        boolean isSubList = getStringListForEvaluation(responseItemList).containsAll(
+                getStringListForEvaluation(expectedItemList));
+        Assert.assertTrue(isSubList, "Did not match the codelens content for " + expectedConfigName);
     }
 
     @AfterClass
@@ -108,5 +122,35 @@ public class CodeLensTest {
         Path expectedFilePath = FileUtils.RES_DIR.resolve("codelens").resolve("expected").resolve(expectedFile);
         byte[] expectedByte = Files.readAllBytes(expectedFilePath);
         return new String(expectedByte);
+    }
+
+    private static List<CodeLens> getFlattenItemList(JsonArray expectedItems) {
+        List<CodeLens> flattenList = new ArrayList<>();
+        expectedItems.forEach(jsonElement -> {
+            CodeLens codeLens = GSON.fromJson(jsonElement, CodeLens.class);
+            flattenList.add(codeLens);
+        });
+
+        return flattenList;
+    }
+
+    private static String getCompletionItemPropertyString(CodeLens completionItem) {
+        String additionalTextEdits = "";
+        if (completionItem.getRange() != null) {
+            additionalTextEdits = "," + GSON.toJson(completionItem.getRange());
+        }
+
+        Command command = completionItem.getCommand();
+        if (command != null) {
+            additionalTextEdits = "," + command.getTitle() + ", " + command.getCommand();
+        }
+
+        return ("{" + additionalTextEdits + "}");
+    }
+
+    private static List<String> getStringListForEvaluation(List<CodeLens> completionItems) {
+        List<String> evalList = new ArrayList<>();
+        completionItems.forEach(completionItem -> evalList.add(getCompletionItemPropertyString(completionItem)));
+        return evalList;
     }
 }
