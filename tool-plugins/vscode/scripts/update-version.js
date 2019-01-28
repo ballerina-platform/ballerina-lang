c55bc10888b0aa4b165b2fce39fcc1884517b88d
Aruna Herath <arunabherath@gmail.com>
2019-01-28 16:45:58 +0530

    Make vscode plugin infer the version by from mvn project version
    This works even for non semver project versions like 0.990-r3

---
+++ tool-plugins/vscode/scripts/update-version.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+const packageJson = require("../package.json");
+packageJson.version = process.argv[2].match(/\d+/g).join(".");
+
+fs.writeFileSync(path.join(__dirname, "../package.json"), JSON.stringify(packageJson, null, 4)+'\n');
