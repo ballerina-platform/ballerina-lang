91cf673a4cd3e22fe936a996cf77e7fe5b2ead55
NatashaWso2 <nwijesekare@gmail.com>
2019-03-04 09:15:42 +0530

    Update parser listener

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangParserListener.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangParserListener.java
@@ -1645,8 +1645,10 @@ public class BLangParserListener extends BallerinaParserBaseListener {
             return;
         }
 
-        this.pkgBuilder.addWorkerReceiveExpr(getCurrentPos(ctx), getWS(ctx), ctx.Identifier().getText(), ctx
-                .expression() != null);
+        String workerName = ctx.workerIdentifier().DEFAULT() != null ?
+                ctx.workerIdentifier().DEFAULT().getText() : ctx.workerIdentifier().Identifier().getText();
+
+        this.pkgBuilder.addWorkerReceiveExpr(getCurrentPos(ctx), getWS(ctx), workerName, ctx.expression() != null);
     }
 
     @Override
@@ -1664,8 +1666,10 @@ public class BLangParserListener extends BallerinaParserBaseListener {
             return;
         }
 
-        this.pkgBuilder.addWorkerSendStmt(getCurrentPos(ctx), getWS(ctx), ctx.Identifier().getText(),
-                                          ctx.expression().size() > 1);
+        String workerName = ctx.workerIdentifier().DEFAULT() != null ?
+                ctx.workerIdentifier().DEFAULT().getText() : ctx.workerIdentifier().Identifier().getText();
+
+        this.pkgBuilder.addWorkerSendStmt(getCurrentPos(ctx), getWS(ctx), workerName, ctx.expression().size() > 1);
     }
 
     @Override
@@ -1674,7 +1678,10 @@ public class BLangParserListener extends BallerinaParserBaseListener {
             return;
         }
 
-        this.pkgBuilder.addWorkerSendSyncExpr(getCurrentPos(ctx), getWS(ctx), ctx.Identifier().getText());
+        String workerName = ctx.workerIdentifier().DEFAULT() != null ?
+                ctx.workerIdentifier().DEFAULT().getText() : ctx.workerIdentifier().Identifier().getText();
+
+        this.pkgBuilder.addWorkerSendSyncExpr(getCurrentPos(ctx), getWS(ctx), workerName);
     }
 
     @Override
