64098ca12e46e5abc9559945602e50c654b4aa01
Anoukh <anoukh.ashley@gmail.com>
2019-03-05 16:32:25 +0530

    Fix unit test module for gradle build Update relevant version dependencies Set the resource directory Copy dependant balos Set system properties Exclude conflicting dependant modules

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/util/ProjectDirConstants.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/util/ProjectDirConstants.java
@@ -49,6 +49,7 @@ public class ProjectDirConstants {
 
     public static final String BALLERINA_HOME = "ballerina.home";
     public static final String BALLERINA_HOME_LIB = "lib";
+    public static final String USER_DIR = "user.dir";
 
     public static final String BALLERINA_VERSION = "ballerina.version";
     public static final String PROPERTIES_FILE = "/META-INF/launcher.properties";
