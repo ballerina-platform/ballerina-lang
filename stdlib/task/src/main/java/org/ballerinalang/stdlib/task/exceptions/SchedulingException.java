29b5fbbd76463e167b42063bc3d7aa00ca4fd264
Thisaru Guruge <thisaru@wso2.com>
2019-03-06 20:54:15 +0530

    Remove unused constructors

--- stdlib/task/src/main/java/org/ballerinalang/stdlib/task/exceptions/SchedulingException.java
+++ stdlib/task/src/main/java/org/ballerinalang/stdlib/task/exceptions/SchedulingException.java
@@ -28,17 +28,4 @@ public class SchedulingException extends Exception {
     public SchedulingException(String message) {
         super(message);
     }
-
-    public SchedulingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public SchedulingException(Throwable cause) {
-        super(cause);
-    }
-
-    protected SchedulingException(String message, Throwable cause,
-                                  boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
