d5b092b0c7bdc70167393c49df62b95f3d9e636a
Manuri <amaya.uom@gmail.com>
2019-03-08 14:37:04 +0530

    Add test script

--- product-scenarios/test-grid-scripts/bbg/data-backed-service/deployment-data-backed-service.sh
+++ product-scenarios/test-grid-scripts/bbg/data-backed-service/deployment-data-backed-service.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# Copyright (c) 2019, WSO2 Inc. (http://wso2.org) All Rights Reserved.
+#
+# WSO2 Inc. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 grand_parent_path=$(dirname ${parent_path})
@@ -61,6 +76,12 @@ kubectl get ingress
 
 kubectl get nodes -o=json
 
-EXTERNAL_IP=$(kubectl get svc ballerina-guides-employee-database-service -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'
+external_ip=$(kubectl get nodes -o go-template --template='{{range.items}}{{range.status.addresses}}{{if eq .type "ExternalIP"}}{{.address}}{{end}}{{end}} {{end}}')
 
-echo "ExternalIP=$EXTERNAL_IP" >> ${OUTPUT_DIR}/deployment.properties
+node_port=$(kubectl get svc ballerina-guides-employee-database-service -o go-template --template='{{range .spec.ports}}{{.nodePort}}{{end}}')
+
+echo "ExternalIP: ${external_ip}"
+echo "NodePort: ${node_port}"
+
+echo "ExternalIP=${external_ip}" >> ${OUTPUT_DIR}/deployment.properties
+echo "NodePort=${node_port}" >> ${OUTPUT_DIR}/deployment.properties
