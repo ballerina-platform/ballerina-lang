fd521829f716a8a9bdb8d858f172a0c7e5641787
Kishanthan <kshanth2101@gmail.com>
2019-03-10 00:45:58 +0530

    Add testng initializer that initalizes jballerina test suite

---
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/utils/JBallerinaTestInitializer.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.test.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+
+import static org.ballerinalang.test.util.TestConstant.ENABLE_JBALLERINA_TESTS;
+
+/**
+ * A test suit listener for jballerina test cases initialization.
+ *
+ * @since 0.955.0
+ */
+public class JBallerinaTestInitializer implements ISuiteListener {
+
+    private static Logger log = LoggerFactory.getLogger(JBallerinaTestInitializer.class);
+
+    @Override
+    public void onStart(ISuite iSuite) {
+        log.info("JBallerina tests initialized...");
+        System.setProperty(ENABLE_JBALLERINA_TESTS, "true");
+    }
+
+    @Override
+    public void onFinish(ISuite iSuite) {
+        System.clearProperty(ENABLE_JBALLERINA_TESTS);
+    }
+}
