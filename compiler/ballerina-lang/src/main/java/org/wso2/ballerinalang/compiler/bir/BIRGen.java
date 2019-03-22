15d220e5b0581459dcc600a79195eacdb6275709
Vinod Kavinda <vinod@wso2.com>
2019-03-22 11:51:35 +0530

    Add BIR gen for future

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/BIRGen.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/BIRGen.java
@@ -299,10 +299,10 @@ public class BIRGen extends BLangNodeVisitor {
         }
 
         if (!invocationExpr.async) {
-            this.env.enclBB.terminator = new BIRTerminator.AsyncCall(invocationExpr.pos, invocationExpr.symbol.pkgID,
+            this.env.enclBB.terminator = new BIRTerminator.Call(invocationExpr.pos, invocationExpr.symbol.pkgID,
                     names.fromString(invocationExpr.name.value), args, lhsOp, thenBB);
         } else {
-            this.env.enclBB.terminator = new BIRTerminator.Call(invocationExpr.pos, invocationExpr.symbol.pkgID,
+            this.env.enclBB.terminator = new BIRTerminator.AsyncCall(invocationExpr.pos, invocationExpr.symbol.pkgID,
                     names.fromString(invocationExpr.name.value), args, lhsOp, thenBB);
         }
 
