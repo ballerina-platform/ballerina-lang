a610b0f1b887d6bd64c38efe5095c862795a8e3a
Thisaru Guruge <thisaru@wso2.com>
2019-03-27 17:38:02 +0530

    Use Ballerina builtin constants for org name

--- stdlib/task/src/main/java/org/ballerinalang/stdlib/task/utils/TaskConstants.java
+++ stdlib/task/src/main/java/org/ballerinalang/stdlib/task/utils/TaskConstants.java
@@ -19,6 +19,7 @@
 package org.ballerinalang.stdlib.task.utils;
 
 import static org.ballerinalang.util.BLangConstants.BALLERINA_BUILTIN_PKG_PREFIX;
+import static org.ballerinalang.util.BLangConstants.BALLERINA_PACKAGE_PREFIX;
 
 /**
  * Task related constants.
@@ -26,11 +27,10 @@ import static org.ballerinalang.util.BLangConstants.BALLERINA_BUILTIN_PKG_PREFIX
 public class TaskConstants {
 
     // Package related constants
-    public static final String PATH_SEPARATOR = "/";
     public static final String ORGANIZATION_NAME = BALLERINA_BUILTIN_PKG_PREFIX;
     public static final String PACKAGE_NAME = "task";
     public static final String OBJECT_NAME_LISTENER = "Listener";
-    public static final String PACKAGE_STRUCK_NAME = ORGANIZATION_NAME + PATH_SEPARATOR + PACKAGE_NAME;
+    public static final String PACKAGE_STRUCK_NAME = BALLERINA_PACKAGE_PREFIX + PACKAGE_NAME;
 
     // Record types used
     public static final String RECORD_TIMER_CONFIGURATION = "TimerConfiguration";
