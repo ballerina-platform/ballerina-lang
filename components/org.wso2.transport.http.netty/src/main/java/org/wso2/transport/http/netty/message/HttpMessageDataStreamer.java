aa99d5f8114942acd3af184c5ed92b818b079280
chamil321 <chamilmadawa8@gmail.com>
2019-04-01 11:05:06 +0530

    Disable content release in dataStreamer close() funct

--- components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/message/HttpMessageDataStreamer.java
+++ components/org.wso2.transport.http.netty/src/main/java/org/wso2/transport/http/netty/message/HttpMessageDataStreamer.java
@@ -35,7 +35,6 @@ import org.wso2.transport.http.netty.contract.Constants;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.nio.ByteBuffer;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.InflaterInputStream;
@@ -74,7 +73,6 @@ public class HttpMessageDataStreamer {
         private int limit;
         private ByteBuffer byteBuffer;
         private HttpContent httpContent;
-        private PrintStream printStream = System.out;
 
         @Override
         public int read() {
@@ -113,14 +111,12 @@ public class HttpMessageDataStreamer {
         @Override
         public void close() throws IOException {
             byteBuffer = null;
-            printStream.println("---------------DataStream close is called-----------------");
-            releaseHttpContent();
+//            releaseHttpContent();    //fix memory leak issue in error path
             super.close();
         }
 
         private synchronized void releaseHttpContent() {
             if (httpContent != null && httpContent.refCnt() > 0) {
-                printStream.println("---------------DataStream release HttpContent is called---------------");
                 httpContent.release();
             }
         }
