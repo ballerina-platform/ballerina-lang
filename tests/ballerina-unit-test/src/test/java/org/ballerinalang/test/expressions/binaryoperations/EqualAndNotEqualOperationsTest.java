acc5b7abf02d741384ebc2e6bc6a5ee5e6d594d9
MaryamZi <maryamziyadm@gmail.com>
2019-04-01 10:34:47 +0530

    Fix assert equals/not equals and disallow table equality

--- tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/binaryoperations/EqualAndNotEqualOperationsTest.java
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/binaryoperations/EqualAndNotEqualOperationsTest.java
@@ -845,7 +845,7 @@ public class EqualAndNotEqualOperationsTest {
 
     @Test(description = "Test equal and not equal with errors")
     public void testEqualAndNotEqualNegativeCases() {
-        Assert.assertEquals(resultNegative.getErrorCount(), 32);
+        Assert.assertEquals(resultNegative.getErrorCount(), 34);
         validateError(resultNegative, 0, "operator '==' not defined for 'int' and 'string'", 20, 12);
         validateError(resultNegative, 1, "operator '!=' not defined for 'int' and 'string'", 20, 24);
         validateError(resultNegative, 2, "operator '==' not defined for 'int[2]' and 'string[2]'", 26, 21);
@@ -888,6 +888,8 @@ public class EqualAndNotEqualOperationsTest {
         validateError(resultNegative, 29, "operator '!=' not defined for 'int' and 'any'", 115, 26);
         validateError(resultNegative, 30, "operator '==' not defined for 'map<int|string>' and 'map'", 119, 14);
         validateError(resultNegative, 31, "operator '!=' not defined for 'map' and 'map<int|string>'", 119, 26);
+        validateError(resultNegative, 32, "equality not yet supported for type 'table'", 131, 17);
+        validateError(resultNegative, 33, "equality not yet supported for type 'table'", 132, 9);
     }
 
     @DataProvider(name = "equalIntValues")
