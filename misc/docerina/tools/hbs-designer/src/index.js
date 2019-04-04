ab3a07f5a234a1ca36749100c1d9fcd1ba318583
srsharon <srsharondavid@gmail.com>
2019-04-04 21:04:52 +0530

    Update API documntation hbs stucture pages

--- misc/docerina/tools/hbs-designer/src/index.js
+++ misc/docerina/tools/hbs-designer/src/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const outPath = path.join(__dirname, "..", "dist");
 const indexHBS = fs.readFileSync(path.join(__dirname, "index.hbs")).toString();
-const data = fs.readFileSync(path.join(__dirname, "..", "data", "model.json")).toString();
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "data", "model.json")).toString());
 
 function buildModules(modules) {
     const modHBS = fs.readFileSync(path.join(__dirname, "module.hbs")).toString();
@@ -13,15 +13,92 @@ function buildModules(modules) {
         const modOutDir = path.join(outPath, mod.id);
         fs.ensureDirSync(modOutDir);
         fs.writeFileSync(path.join(modOutDir, "index.html"), modTemplate(mod));
+        if (mod.constructs){
+            buildContructs(mod);
+        }
     });
+}
+
+function buildRecords(modId, records) {
+    const recordHBS = fs.readFileSync(path.join(__dirname, "records.hbs")).toString();
+    const recordTemplate = Handlebars.compile(recordHBS);
+    records.forEach((rec) => {
+        const recOutDir = path.join(outPath, modId, 'records');
+        fs.ensureDirSync(recOutDir);
+        fs.writeFileSync(path.join(recOutDir, rec.name + ".html"), recordTemplate({ rec, all: data }));
+    });
+}
+function buildObjects(modId, objects) {
+    const objectsHBS = fs.readFileSync(path.join(__dirname, "objects.hbs")).toString();
+    const objectsTemplate = Handlebars.compile(objectsHBS);
+    objects.forEach((obj) => {
+        const objOutDir = path.join(outPath, modId, 'objects');
+        fs.ensureDirSync(objOutDir);
+        fs.writeFileSync(path.join(objOutDir, obj.name + ".html"), objectsTemplate({ obj, all: data }));
+    });
+}
+function buildClients(modId, clients) {
+    const clientsHBS = fs.readFileSync(path.join(__dirname, "clients.hbs")).toString();
+    const clientsTemplate = Handlebars.compile(clientsHBS);
+    clients.forEach((clnt) => {
+        const clntOutDir = path.join(outPath, modId, 'clients');
+        fs.ensureDirSync(clntOutDir);
+        fs.writeFileSync(path.join(clntOutDir, clnt.name + ".html"), clientsTemplate({ clnt, all: data }));
+    });
+}
+
+function buildFunctions(modId, functions) {
+    const functionsHBS = fs.readFileSync(path.join(__dirname, "functions.hbs")).toString();
+    const functionsTemplate = Handlebars.compile(functionsHBS);
+    functions.forEach((func) => {
+        const funcOutDir = path.join(outPath, modId, 'functions');
+        fs.ensureDirSync(funcOutDir);
+        fs.writeFileSync(path.join(funcOutDir, clnt.name + ".html"), functionsTemplate({ func, all: data }));
+    });
+}
+
+
+function buildContructs(mod) {
+    const records = mod.constructs.records;
+    const objects = mod.constructs.objects;
+    const clients = mod.constructs.clients;
+    const functions = mod.constructs.functions;
+    const listeners = mod.constructs.listeners;
+    const constants = mod.constructs.constants;
+    const annotations = mod.constructs.annotations;
+    const errors = mod.constructs.errors;
+
+    if (records.length != 0) {
+        buildRecords(mod.id, records);
+    }
+    if (objects.length != 0) {
+        buildObjects(mod.id, objects);
+    }
+    if (clients.length != 0) {
+        buildClients(mod.id, clients);
+    }
+    if (functions.length != 0) {
+        buildFunctions(mod.id, functions);
+    }
+    if (listeners.length != 0) {
+        buildListeners(mod.id, listeners);
+    }
+    if (constants.length != 0) {
+        buildConstants(mod.id, constants);
+    }
+    if (annotations.length != 0) {
+        buildAnnotations(mod.id, annotations);
+    }
+    if (errors.length != 0) {
+        buildErrors(mod.id, errors);
+    }
 }
 
 try {
     var indexTemplate = Handlebars.compile(indexHBS);
-    var model = JSON.parse(data);
     fs.ensureDirSync(outPath);
-    fs.writeFileSync(path.join(outPath, "index.html"), indexTemplate(model));
-    buildModules(model.modules);
+    fs.writeFileSync(path.join(outPath, "index.html"), indexTemplate(data));
+    buildModules(data.modules);
     fs.copy(path.join(__dirname, "styles"), path.join(outPath, "styles"));
     fs.copy(path.join(__dirname, "images"), path.join(outPath, "images"));
     
