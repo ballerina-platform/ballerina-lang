f3c3718d7082d05a5373626092eb2c3edd394fee
shan1024 <shanm@wso2.com>
2019-04-17 08:36:52 +0530

    Remove redundant diagnostic

--- compiler/ballerina-lang/src/main/java/org/ballerinalang/util/diagnostic/DiagnosticCode.java
+++ compiler/ballerina-lang/src/main/java/org/ballerinalang/util/diagnostic/DiagnosticCode.java
@@ -27,7 +27,6 @@ public enum DiagnosticCode {
     UNDEFINED_MODULE("undefined.module"),
     CYCLIC_MODULE_IMPORTS_DETECTED("cyclic.module.imports.detected"),
     UNUSED_IMPORT_MODULE("unused.import.module"),
-    SELF_IMPORT_NOT_ALLOWED("self.import.not.allowed"),
     MODULE_NOT_FOUND("module.not.found"),
     REDECLARED_IMPORT_MODULE("redeclared.import.module"),
     INVALID_MODULE_DECLARATION("invalid.module.declaration"),
