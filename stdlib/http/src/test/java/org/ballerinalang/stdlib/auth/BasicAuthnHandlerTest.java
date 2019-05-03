00d78aaa627982aa6aa9a00d4764ca62a6fc52a1
Chanaka Lakmal <ldclakmal@gmail.com>
2019-05-03 13:24:50 +0530

    Fix checkstyle bug

--- stdlib/http/src/test/java/org/ballerinalang/stdlib/auth/BasicAuthnHandlerTest.java
+++ stdlib/http/src/test/java/org/ballerinalang/stdlib/auth/BasicAuthnHandlerTest.java
@@ -23,7 +23,6 @@ import org.ballerinalang.launcher.util.BCompileUtil;
 import org.ballerinalang.launcher.util.BRunUtil;
 import org.ballerinalang.launcher.util.CompileResult;
 import org.ballerinalang.model.values.BBoolean;
-import org.ballerinalang.model.values.BError;
 import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BValue;
 import org.testng.Assert;
