3028df933cb63fb2ac09ff700def9fd37c966a6e
Vinod Kavinda <vinod@wso2.com>
2019-05-10 00:18:22 +0530

    Fix formatting

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/jvm/WorkerTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/jvm/WorkerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -30,6 +30,8 @@ import java.util.Arrays;
 
 /**
  * Basic worker related tests.
+ *
+ * @since 0.995.0
  */
 public class WorkerTest {
 
