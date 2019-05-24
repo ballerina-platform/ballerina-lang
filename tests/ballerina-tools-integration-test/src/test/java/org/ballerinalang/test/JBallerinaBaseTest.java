fa9512d51e01131781293174991a1d1671c4b6a9
warunaj@wso2.com <warudanu@gmail.com>
2019-05-24 12:10:24 +0530

    Refactor JBallerinaTestInitializer class

--- tests/ballerina-tools-integration-test/src/test/java/org/ballerinalang/test/JBallerinaBaseTest.java
+++ tests/ballerina-tools-integration-test/src/test/java/org/ballerinalang/test/JBallerinaBaseTest.java
@@ -22,17 +22,26 @@ import org.ballerinalang.test.context.BMainInstance;
 import org.ballerinalang.test.context.BalServer;
 import org.ballerinalang.test.context.BallerinaTestException;
 import org.ballerinalang.test.context.Constant;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 /**
  * Parent test class for all integration test cases which required to be tested with jballerina distribution.
  * TODO : We can get rid of this class once we move all test cases to run on jballerina distribution.
  */
-public class JBallerinaBaseTest extends BaseTest {
+public class JBallerinaBaseTest {
 
+    protected static BalServer balServer;
+    protected static BMainInstance balClient;
+    
     @BeforeSuite(alwaysRun = true)
     public void initialize() throws BallerinaTestException {
         balServer = new BalServer(System.getProperty(Constant.SYSTEM_PROP_JBALLERINA_SERVER_ZIP));
         balClient = new BMainInstance(balServer);
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void destroy() throws BallerinaTestException {
+        balServer.cleanup();
     }
 }
