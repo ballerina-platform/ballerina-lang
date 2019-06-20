e05355db0ac4883596c3e78632a47e53501aaf3b
Pubudu Fernando <pubuduf@wso2.com>
2019-06-20 12:24:29 +0530

    Add new float lib functions

---
+++ langlib/lang.float/src/main/java/org/ballerinalang/langlib/floatingpoint/Sum.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.langlib.floatingpoint;
+
+import org.ballerinalang.jvm.Strand;
+import org.ballerinalang.jvm.values.ArrayValue;
+
+/**
+ * Native implementation of lang.float:sum(float...).
+ */
+public class Sum {
+
+    public static double sum(Strand strand, ArrayValue ns) {
+        double sum = 0;
+        int size = ns.size();
+        for (int i = 0; i < size; i++) {
+            sum += ns.getFloat(i);
+        }
+        return sum;
+    }
+}
