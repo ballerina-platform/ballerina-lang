0efa250421284aad6a4e30a1816d4274517a910e
Manuri Perera <amaya.uom@gmail.com>
2019-06-22 10:34:14 +0530

    Add missing changes to jvm classes

--- bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/types/BTypes.java
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/types/BTypes.java
@@ -41,9 +41,13 @@ public class BTypes {
     public static BType typeTypedesc = new BTypedescType(TypeConstants.TYPEDESC_TNAME, new BPackage(null,
             null, null));
     public static BType typeMap = new BMapType(TypeConstants.MAP_TNAME, typeAny, new BPackage(null, null, null));
-    public static BType typeTable = new BTableType(TypeConstants.TABLE_TNAME, typeAnydata, new BPackage(null,
-            null, null));
-    public static BType typeFuture = new BFutureType(TypeConstants.FUTURE_TNAME, new BPackage(null, null, null));
+    //TODO: JBalMigration check whether this is correct
+    public static BType typeAnonymousOpenRecord = new BRecordType(TypeConstants.RECORD_TNAME,
+            new BPackage(null, null, null), 0, false);
+    public static BType typeTable = new BTableType(TypeConstants.TABLE_TNAME, typeAnonymousOpenRecord,
+            new BPackage(null, null, null));
+    public static BType typeFuture = new BFutureType(TypeConstants.FUTURE_TNAME,
+            new BPackage(null, null, null));
     public static BType typeNull = new BNullType(TypeConstants.NULL_TNAME, new BPackage(null, null, null));
     public static BType typeXMLAttributes = new BXMLAttributesType(TypeConstants.XML_ATTRIBUTES_TNAME,
                                                                    new BPackage(null, null, null));
@@ -57,6 +61,7 @@ public class BTypes {
 
     static {
         typeError.detailType = new BMapType(typePureType);
+        ((BRecordType) typeAnonymousOpenRecord).restFieldType = typeAnydata;
     }
     
     private BTypes() {
