dc27bd1e934d1327f128a59d6051c4fb36f1fc9c
Rajith <lanka.vitharana@gmail.com>
2019-06-27 16:59:38 +0530

    Fix checkstyle issue

--- stdlib/internal/src/main/java/org/ballerinalang/stdlib/internal/file/ToAbsolutePath.java
+++ stdlib/internal/src/main/java/org/ballerinalang/stdlib/internal/file/ToAbsolutePath.java
@@ -23,7 +23,6 @@ import org.ballerinalang.bre.bvm.BlockingNativeCallableUnit;
 import org.ballerinalang.connector.api.BLangConnectorSPIUtil;
 import org.ballerinalang.jvm.BallerinaValues;
 import org.ballerinalang.jvm.Strand;
-import org.ballerinalang.jvm.values.MapValue;
 import org.ballerinalang.jvm.values.ObjectValue;
 import org.ballerinalang.model.types.TypeKind;
 import org.ballerinalang.model.values.BMap;
