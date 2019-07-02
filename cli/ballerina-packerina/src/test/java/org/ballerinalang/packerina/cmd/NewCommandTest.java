29786eb1da2a7d8a23865d03478e87a3c4aa27e9
Joseph Fonseka <hevayo@gmail.com>
2019-07-02 13:16:00 +0530

    Fix issues identified in PR review

--- cli/ballerina-packerina/src/test/java/org/ballerinalang/packerina/cmd/NewCommandTest.java
+++ cli/ballerina-packerina/src/test/java/org/ballerinalang/packerina/cmd/NewCommandTest.java
@@ -1,19 +1,19 @@
 /*
- *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.ballerinalang.packerina.cmd;
@@ -30,10 +30,9 @@ import java.nio.file.Path;
 /**
  * Test cases for ballerina new command.
  *
- * @since 0.992.0
+ * @since 1.0.0
  */
 public class NewCommandTest extends CommandTest {
-
 
     @Test(description = "Initialize a new empty project")
     public void testNewCommand() throws IOException {
@@ -122,7 +121,6 @@ public class NewCommandTest extends CommandTest {
                 + tmpDir.resolve("exist").toString()
                 + "' already exists"));
     }
-
 
     @Test(description = "Test if initializing inside a sub directory")
     public void testNewCommandInsideProject() throws IOException {
