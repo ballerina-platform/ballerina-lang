9ae2cd168c29a3eec4738da7a007f9436f9f76b5
Sameera <sameera.madushan@gmail.com>
2019-07-08 23:39:28 -0700

    Update Java class comments

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/antlr4/BallerinaParserBaseListener.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/antlr4/BallerinaParserBaseListener.java
@@ -1,4 +1,4 @@
-// Generated from /Users/sameera/work/wso2/clones/sameera/jballerina_handle3/compiler/ballerina-lang/src/main/resources/grammar/BallerinaParser.g4 by ANTLR 4.5.3
+// Generated from BallerinaParser.g4 by ANTLR 4.5.3
 package org.wso2.ballerinalang.compiler.parser.antlr4;
 
 import org.antlr.v4.runtime.ParserRuleContext;
