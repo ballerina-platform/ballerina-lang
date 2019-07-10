13138ffc848e2e1858b4aedc2895c3f6d49100e1
srsharon <srsharondavid@gmail.com>
2019-07-10 11:23:32 +0530

    Update search icon

--- composer/packages/diagram/src/diagram/top-menu.tsx
+++ composer/packages/diagram/src/diagram/top-menu.tsx
@@ -35,81 +35,82 @@ export const TopMenu = (props: TopMenuProps) => {
 
     return (
         <div>{
-           !openedState ? <div>
+            !openedState ?
+            <div>
                 <div className="menu-icon">
-                    <Icon onClick={handleOpened} className="fw fw-ballerina"/>
+                    <Icon onClick={handleOpened} className="fw fw-search" />
                 </div>
             </div> :
-                <Grid className="menu-container">
-                        <Grid.Row className="menu-row" columns={3}>
-                            <Grid.Column className="menu-label" width={4}>
-                                    Zoom
-                            </Grid.Column>
-                            <Grid.Column className="selection-row" width={9}>
-                                <Icon onClick={handleZoomOut} className="fw fw-minus"/>
-                                <Dropdown className="menu-dropdown-small">
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item text="10%"/>
-                                        <Dropdown.Item text="20%"/>
-                                        <Dropdown.Item text="30%"/>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                                <Icon onClick={handleZoomIn} className="fw fw-add"/>
-                                <Icon onClick={handleFitClick} active={fitActive} className="fw fw-fit"/>
-                            </Grid.Column>
-                            <Grid.Column className="menu-control" width={3}>
-                                <Icon className="fw fw-refresh"/>
-                                <Icon onClick={handleClosed} className="fw fw-uncheck"/>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row className="menu-row" columns={3}>
-                            <Grid.Column className="menu-label" width={4}>
-                                    Depth
-                            </Grid.Column>
-                            <Grid.Column className="selection-row" width={9}>
-                            <Icon className="fw fw-minus"/>
-                                <Dropdown className="menu-dropdown-small">
-                                    <Dropdown.Menu>
-                                            <Dropdown.Item text={1} />
-                                            <Dropdown.Item text={2} />
-                                            <Dropdown.Item text={3} />
-                                            <Dropdown.Item text={4} />
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                                <Icon className="fw fw-add"/>
-                                <Icon className="fw fw-expand"/>
-                                <Icon className="fw fw-collapse"/>
-                            </Grid.Column>
-                            <Grid.Column width={3}/>
-                        </Grid.Row>
-                        <Grid.Row className="menu-row" columns={3}>
-                            <Grid.Column className="menu-label" width={4}>
-                                    Design
-                            </Grid.Column>
-                            <Grid.Column className="selection-row" width={9}>
-                            <Icon className="fw fw-minus"/>
-                                <Dropdown className="menu-dropdown-mid " text={selectedModeText}>
-                                    <Dropdown.Menu>
-                                    {
-                                        modes.map((mode) => (
-                                            <Dropdown.Item
-                                                text={mode.text}
-                                                value={mode.type}
-                                                onClick={handleModeChange}
-                                                data={{
-                                                    text: mode.text,
-                                                    type: mode.type,
-                                                }}
-                                            />
-                                        ))
-                                    }
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                                <Icon className="fw fw-add"/>
-                            </Grid.Column>
-                            <Grid.Column width={3}/>
-                        </Grid.Row>
-                    </Grid>
+            <Grid className="menu-container">
+                <Grid.Row className="menu-row" columns={3}>
+                    <Grid.Column className="menu-label" width={4}>
+                        Zoom
+                        </Grid.Column>
+                    <Grid.Column className="selection-row" width={9}>
+                        <Icon onClick={handleZoomOut} className="fw fw-minus" />
+                        <Dropdown className="menu-dropdown-small">
+                            <Dropdown.Menu>
+                                <Dropdown.Item text="10%" />
+                                <Dropdown.Item text="20%" />
+                                <Dropdown.Item text="30%" />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Icon onClick={handleZoomIn} className="fw fw-add" />
+                        <Icon onClick={handleFitClick} active={fitActive} className="fw fw-fit" />
+                    </Grid.Column>
+                    <Grid.Column className="menu-control" width={3}>
+                        <Icon className="fw fw-refresh" />
+                        <Icon onClick={handleClosed} className="fw fw-uncheck" />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row className="menu-row" columns={3}>
+                    <Grid.Column className="menu-label" width={4}>
+                        Depth
+                        </Grid.Column>
+                    <Grid.Column className="selection-row" width={9}>
+                        <Icon className="fw fw-minus" />
+                        <Dropdown className="menu-dropdown-small">
+                            <Dropdown.Menu>
+                                <Dropdown.Item text={1} />
+                                <Dropdown.Item text={2} />
+                                <Dropdown.Item text={3} />
+                                <Dropdown.Item text={4} />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Icon className="fw fw-add" />
+                        <Icon className="fw fw-expand" />
+                        <Icon className="fw fw-collapse" />
+                    </Grid.Column>
+                    <Grid.Column width={3} />
+                </Grid.Row>
+                <Grid.Row className="menu-row" columns={3}>
+                    <Grid.Column className="menu-label" width={4}>
+                        Design
+                        </Grid.Column>
+                    <Grid.Column className="selection-row" width={9}>
+                        <Icon className="fw fw-minus" />
+                        <Dropdown className="menu-dropdown-mid " text={selectedModeText}>
+                            <Dropdown.Menu>
+                                {
+                                    modes.map((mode) => (
+                                        <Dropdown.Item
+                                            text={mode.text}
+                                            value={mode.type}
+                                            onClick={handleModeChange}
+                                            data={{
+                                                text: mode.text,
+                                                type: mode.type,
+                                            }}
+                                        />
+                                    ))
+                                }
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Icon className="fw fw-add" />
+                    </Grid.Column>
+                    <Grid.Column width={3} />
+                </Grid.Row>
+            </Grid>
         }
         </div>
     );
