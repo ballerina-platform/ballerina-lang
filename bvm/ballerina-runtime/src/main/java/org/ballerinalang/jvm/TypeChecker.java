83e23769fc9f1f80970fdf4043336f81e31fff79
Supun Setunga <supun.setunga@gmail.com>
2019-07-12 00:14:10 +0530

    Fix checkstyle issues (#162)

--- bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/TypeChecker.java
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/TypeChecker.java
@@ -59,10 +59,8 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.ballerinalang.jvm.util.BLangConstants.BBYTE_MAX_VALUE;
