23bbc3861097d9f98869b4ac160bab52ea197f53
Imesh Chandrsiri <dimal.chandrasiri@gmail.com>
2019-07-11 16:58:43 +0530

    Add array type matching for OpenApi Tool

--- misc/openapi-ballerina/modules/openapi-to-ballerina-generator/src/main/java/org/ballerinalang/openapi/utils/SchemaParser.java
+++ misc/openapi-ballerina/modules/openapi-to-ballerina-generator/src/main/java/org/ballerinalang/openapi/utils/SchemaParser.java
@@ -1,5 +1,6 @@
 package org.ballerinalang.openapi.utils;
 
+import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.Schema;
 import org.apache.commons.lang3.StringUtils;
 import org.ballerinalang.openapi.typemodel.OpenApiPropertyType;
@@ -17,8 +18,26 @@ public class SchemaParser {
         openApiPropertyType.setPropertyType("string");
     }
 
+    public void parseBooleanSchema(Object string,  OpenApiPropertyType openApiPropertyType) {
+
+    }
+
+    public void parseObjectSchema(Object string,  OpenApiPropertyType openApiPropertyType) {
+        //TODO Implement object scehema 
+    }
+
     public void parseArraySchema(Object array, OpenApiPropertyType openApiPropertyType) {
-        //TODO: Process Array Schema
+        ArraySchema arrayObj = (ArraySchema) array;
+        if (arrayObj.getItems() != null) {
+            final Schema<?> items = arrayObj.getItems();
+            openApiPropertyType.setIsArray(true);
+            if (items.getType() != null) {
+                openApiPropertyType.setPropertyType(items.getType());
+            } else if (items.get$ref() != null) {
+                final String[] referenceType = items.get$ref().split("/");
+                openApiPropertyType.setPropertyType(referenceType[referenceType.length - 1]);
+            }
+        }
     }
 
     public void parseNumberSchema(Object number, OpenApiPropertyType openApiPropertyType) {
