b5c95b28870399f7587af11e86f04e8af0748871
MaryamZi <maryamziyadm@gmail.com>
2019-07-11 10:40:41 +0530

    Fix original type not being set

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
@@ -3521,6 +3521,7 @@ public class TypeChecker extends BLangNodeVisitor {
             actualType = checkRecordFieldAccessExpr(fieldAccessExpr, varRefType, fieldName);
 
             if (actualType != symTable.semanticError) {
+                fieldAccessExpr.originalType = actualType;
                 return actualType;
             }
 
@@ -3534,6 +3535,7 @@ public class TypeChecker extends BLangNodeVisitor {
             // If this is an LHS expression, check if there is a required and/ optional field by the specified field
             // name in all records.
             actualType = checkRecordFieldAccessLhsExpr(fieldAccessExpr, varRefType, fieldName);
+            fieldAccessExpr.originalType = actualType;
             if (actualType == symTable.semanticError) {
                 dlog.error(fieldAccessExpr.pos, DiagnosticCode.UNDEFINED_STRUCTURE_FIELD_WITH_TYPE, fieldName,
                            varRefType.tsymbol.type.getKind().typeName(), varRefType);
@@ -3559,6 +3561,7 @@ public class TypeChecker extends BLangNodeVisitor {
                 dlog.error(fieldAccessExpr.pos, DiagnosticCode.CANNOT_UPDATE_XML_SEQUENCE);
             }
             actualType = symTable.xmlType;
+            fieldAccessExpr.originalType = actualType;
         } else if (varRefType.tag != TypeTags.SEMANTIC_ERROR) {
             dlog.error(fieldAccessExpr.pos, DiagnosticCode.OPERATION_DOES_NOT_SUPPORT_FIELD_ACCESS, varRefType);
         }
@@ -3567,7 +3570,7 @@ public class TypeChecker extends BLangNodeVisitor {
     }
 
     private boolean hasLaxOriginalType(BLangFieldBasedAccess fieldBasedAccess) {
-        return fieldBasedAccess.originalType != null && types.isLax(fieldBasedAccess.originalType);
+        return types.isLax(fieldBasedAccess.originalType);
     }
 
     private BType getLaxFieldAccessType(BType exprType) {
@@ -3617,8 +3620,8 @@ public class TypeChecker extends BLangNodeVisitor {
                            DiagnosticCode.OPERATION_DOES_NOT_SUPPORT_OPTIONAL_FIELD_ACCESS_FOR_FIELD,
                            varRefType, fieldName);
             }
-            fieldAccessExpr.originalType = getSafeType(actualType, fieldAccessExpr);
             fieldAccessExpr.nilSafeNavigation = nillableExprType;
+            fieldAccessExpr.originalType = getSafeType(actualType, fieldAccessExpr);
         } else if (types.isLax(effectiveType)) {
             BType laxFieldAccessType = getLaxFieldAccessType(effectiveType);
             actualType = couldHoldNonMappingJson(effectiveType) ?
@@ -3733,6 +3736,7 @@ public class TypeChecker extends BLangNodeVisitor {
             }
 
             actualType = checkListIndexBasedAccess(indexBasedAccessExpr, varRefType);
+            indexBasedAccessExpr.originalType = actualType;
 
             if (actualType == symTable.semanticError) {
                 if (indexExpr.type.tag == TypeTags.INT && indexExpr.getKind() == NodeKind.NUMERIC_LITERAL) {
@@ -3752,6 +3756,7 @@ public class TypeChecker extends BLangNodeVisitor {
 
             checkExpr(indexExpr, this.env);
             actualType = symTable.xmlType;
+            indexBasedAccessExpr.originalType = actualType;
         } else if (varRefType == symTable.semanticError) {
             indexBasedAccessExpr.indexExpr.type = symTable.semanticError;
             return symTable.semanticError;
