97c6cb1b5c2adaad7d11b59841f0ec137b0f68b2
aashikam <arshika.nm@gmail.com>
2019-07-11 13:01:49 +0530

    Refactor compiler plugin

---
+++ stdlib/messaging/activemq-artemis/src/main/java/org/ballerinalang/messaging/artemis/ArtemisResourceValidator.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.messaging.artemis;
+
+import org.ballerinalang.util.diagnostic.Diagnostic;
+import org.ballerinalang.util.diagnostic.DiagnosticLog;
+import org.wso2.ballerinalang.compiler.semantics.model.types.BArrayType;
+import org.wso2.ballerinalang.compiler.semantics.model.types.BMapType;
+import org.wso2.ballerinalang.compiler.semantics.model.types.BType;
+import org.wso2.ballerinalang.compiler.tree.BLangFunction;
+import org.wso2.ballerinalang.compiler.tree.BLangSimpleVariable;
+import org.wso2.ballerinalang.compiler.util.TypeTags;
+
+import java.util.List;
+
+/**
+ * Validator for Artemis resource.
+ *
+ * @since 0.995.0
+ */
+public class ArtemisResourceValidator {
+
+    private static final String INVALID_RESOURCE_SIGNATURE_FOR = "Invalid resource signature for ";
+    private static final String RESOURCE_IN_SERVICE = " resource in service ";
+
+    public static void validate(BLangFunction resource, DiagnosticLog dlog, boolean resourceReturnsErrorOrNil,
+                                boolean onErrorAvailable) {
+        if (!resourceReturnsErrorOrNil) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, "Invalid return type: expected error?");
+        }
+        if (!resource.getName().getValue().equals(ArtemisConstants.ON_MESSAGE)
+                && !resource.getName().getValue().equals(ArtemisConstants.ON_ERROR)) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos,
+                    "Invalid resource name " + resource.getName().getValue() + " in service, only " +
+                            ArtemisConstants.ON_MESSAGE + " and " + ArtemisConstants.ON_ERROR + " are allowed");
+        }
+        switch (resource.getName().getValue()) {
+            case ArtemisConstants.ON_MESSAGE:
+                validateOnMessageResource(resource, dlog, onErrorAvailable);
+                break;
+            case ArtemisConstants.ON_ERROR:
+                validateOnErrorResource(resource, dlog);
+                break;
+            default:
+                dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos,
+                        "Invalid resource name " + resource.getName().getValue() + " in service, only " +
+                                ArtemisConstants.ON_MESSAGE + " and " + ArtemisConstants.ON_ERROR + " are allowed");
+        }
+    }
+
+    private static void validateOnErrorResource(BLangFunction resource, DiagnosticLog dlog) {
+        List<BLangSimpleVariable> paramDetails = resource.getParameters();
+        validateParamDetailsSize(paramDetails, 2, resource, dlog);
+        if (!ArtemisConstants.MESSAGE_OBJ_FULL_NAME.equals(paramDetails.get(0).type.toString())) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, INVALID_RESOURCE_SIGNATURE_FOR
+                    + resource.getName().getValue() + " resource: The first parameter should be an artemis:Message");
+        }
+        if (!"error".equals(paramDetails.get(1).type.toString())) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, String.format(
+                    "Invalid resource signature for %s resource in service : The second parameter should be " +
+                            "artemis:ArtemisError",
+                    resource.getName().getValue()));
+        }
+    }
+
+    private static void validateOnMessageResource(BLangFunction resource, DiagnosticLog dlog,
+                                                  boolean onErrorAvailable) {
+        List<BLangSimpleVariable> paramDetails = resource.getParameters();
+        validateParamDetailsSize(paramDetails, 2, 1, resource, dlog);
+        if (!ArtemisConstants.MESSAGE_OBJ_FULL_NAME.equals(paramDetails.get(0).type.toString())) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, INVALID_RESOURCE_SIGNATURE_FOR
+                    + resource.getName().getValue() + " resource: The first parameter should be an artemis:Message");
+        }
+        if (paramDetails.size() == 2) {
+            validateSecondParam(resource, paramDetails, dlog, onErrorAvailable);
+        }
+
+    }
+
+    private static void validateSecondParam(BLangFunction resource, List<BLangSimpleVariable> paramDetails,
+                                            DiagnosticLog dlog, boolean onErrorAvailable) {
+        if (!onErrorAvailable) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, "onError resource " +
+                    "function is not found; any errors that occur due to data binding will be discarded along " +
+                    "with the message");
+        }
+        BType secondParamType = paramDetails.get(1).type;
+        int secondParamTypeTag = secondParamType.tag;
+        if (secondParamTypeTag != TypeTags.STRING && secondParamTypeTag != TypeTags.JSON &&
+                secondParamTypeTag != TypeTags.XML && secondParamTypeTag != TypeTags.RECORD &&
+                secondParamTypeTag != TypeTags.MAP && checkArrayType(secondParamType)) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, INVALID_RESOURCE_SIGNATURE_FOR
+                    + resource.getName().getValue() + RESOURCE_IN_SERVICE +
+                    ": The second parameter should be a string, json, xml, byte[], map or a record type");
+        }
+        if (secondParamTypeTag == TypeTags.MAP && checkMapConstraint(secondParamType)) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, INVALID_RESOURCE_SIGNATURE_FOR
+                    + resource.getName().getValue() + RESOURCE_IN_SERVICE +
+                    ": The second parameter should be a map of string, int, float, byte, boolean or byte[]");
+        }
+
+    }
+
+    private static boolean checkArrayType(BType secondParamType) {
+        return secondParamType.tag != TypeTags.ARRAY || (secondParamType instanceof BArrayType &&
+                ((BArrayType) secondParamType).getElementType().tag != org.ballerinalang.model.types.TypeTags.BYTE_TAG);
+    }
+
+    private static boolean checkMapConstraint(BType paramType) {
+        if (paramType instanceof BMapType) {
+            BType constraintType = ((BMapType) paramType).constraint;
+            int constraintTypeTag = constraintType.tag;
+            return constraintTypeTag != TypeTags.STRING && constraintTypeTag != TypeTags.INT &&
+                    constraintTypeTag != TypeTags.FLOAT && constraintTypeTag != TypeTags.BYTE &&
+                    constraintTypeTag != TypeTags.BOOLEAN && checkArrayType(constraintType);
+        }
+        return false;
+    }
+
+    private static void validateParamDetailsSize(List<BLangSimpleVariable> paramDetails, int expectedSize,
+                                                 BLangFunction resource, DiagnosticLog dlog) {
+        if (paramDetails == null || paramDetails.size() != expectedSize) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, INVALID_RESOURCE_SIGNATURE_FOR
+                    + resource.getName().getValue() + RESOURCE_IN_SERVICE +
+                    ": Expected parameter count = " + expectedSize);
+        }
+    }
+
+    private static void validateParamDetailsSize(List<BLangSimpleVariable> paramDetails, int min, int max,
+                                                 BLangFunction resource, DiagnosticLog dlog) {
+        if (paramDetails == null || paramDetails.size() < min || paramDetails.size() > max) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, INVALID_RESOURCE_SIGNATURE_FOR
+                    + resource.getName().getValue() + RESOURCE_IN_SERVICE + ": Unexpected parameter count");
+        }
+    }
+
+    private ArtemisResourceValidator() {
+    }
+}
--- stdlib/messaging/activemq-artemis/src/main/java/org/ballerinalang/messaging/artemis/ArtemisServiceCompilerPlugin.java
+++ stdlib/messaging/activemq-artemis/src/main/java/org/ballerinalang/messaging/artemis/ArtemisServiceCompilerPlugin.java
@@ -22,12 +22,7 @@ import org.ballerinalang.model.tree.AnnotationAttachmentNode;
 import org.ballerinalang.model.tree.ServiceNode;
 import org.ballerinalang.util.diagnostic.Diagnostic;
 import org.ballerinalang.util.diagnostic.DiagnosticLog;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BArrayType;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BMapType;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BType;
 import org.wso2.ballerinalang.compiler.tree.BLangFunction;
-import org.wso2.ballerinalang.compiler.tree.BLangSimpleVariable;
-import org.wso2.ballerinalang.compiler.util.TypeTags;
 import org.wso2.ballerinalang.util.AbstractTransportCompilerPlugin;
 
 import java.util.List;
@@ -49,8 +44,9 @@ import java.util.List;
                 )})
 public class ArtemisServiceCompilerPlugin extends AbstractTransportCompilerPlugin {
 
-    private static final String INVALID_RESOURCE_SIGNATURE_FOR = "Invalid resource signature for ";
     private DiagnosticLog dlog = null;
+    private boolean onErrorAvailable = false;
+    private boolean onMessageAvailable = false;
 
     @Override
     public void init(DiagnosticLog diagnosticLog) {
@@ -65,7 +61,6 @@ public class ArtemisServiceCompilerPlugin extends AbstractTransportCompilerPlugi
                 count++;
             }
         }
-
         if (count == 0) {
             dlog.logDiagnostic(Diagnostic.Kind.ERROR, serviceNode.getPosition(),
                     "There has to be an artemis:ServiceConfig annotation declared for service");
@@ -73,90 +68,30 @@ public class ArtemisServiceCompilerPlugin extends AbstractTransportCompilerPlugi
 
         @SuppressWarnings(ArtemisConstants.UNCHECKED)
         List<BLangFunction> resources = (List<BLangFunction>) serviceNode.getResources();
+        // More than two resource functions are found
         if (resources.size() > 2) {
             dlog.logDiagnostic(Diagnostic.Kind.ERROR, serviceNode.getPosition(),
                     "Only maximum of two resources are allowed in the service");
         }
+        // No resource functions are found
+        if (resources.isEmpty()) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, serviceNode.getPosition(),
+                    "There has to be at least one resource function declared for the service");
+        }
+        for (BLangFunction res : resources) {
+            if (res.getName().getValue().equals(ArtemisConstants.ON_ERROR)) {
+                onErrorAvailable = true;
+            } else if (res.getName().getValue().equals(ArtemisConstants.ON_MESSAGE)) {
+                onMessageAvailable = false;
+            }
+        }
+        // Mandatory onMessage resource function not found
+        if (!onMessageAvailable) {
+            dlog.logDiagnostic(Diagnostic.Kind.ERROR, serviceNode.getPosition(),
+                    "There has to be onMessage resource declared for the service");
+        }
         resources.forEach(
-                res -> validate(res, resources)
+                res -> ArtemisResourceValidator.validate(res, dlog, isResourceReturnsErrorOrNil(res), onErrorAvailable)
         );
-    }
-
-    private void validate(BLangFunction resource, List<BLangFunction> resources) {
-        if (!resource.getName().getValue().equals(ArtemisConstants.ON_MESSAGE)
-                && !resource.getName().getValue().equals(ArtemisConstants.ON_ERROR)) {
-            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos,
-                    "Invalid resource name " + resource.getName().getValue() + " in service, only " +
-                            ArtemisConstants.ON_MESSAGE + " and " + ArtemisConstants.ON_ERROR + " are allowed");
-        }
-        if (!isResourceReturnsErrorOrNil(resource)) {
-            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, "Invalid return type: expected error?");
-        }
-        List<BLangSimpleVariable> paramDetails = resource.getParameters();
-        if (paramDetails == null || paramDetails.isEmpty() || paramDetails.size() > 2) {
-            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos,
-                    INVALID_RESOURCE_SIGNATURE_FOR + resource.getName().getValue() +
-                            " resource: Unexpected parameter count(expected parameter count 1 or 2)");
-            return;
-        }
-        if (!ArtemisConstants.MESSAGE_OBJ_FULL_NAME.equals(paramDetails.get(0).type.toString())) {
-            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, INVALID_RESOURCE_SIGNATURE_FOR
-                    + resource.getName().getValue() + " resource: The first parameter should be an artemis:Message");
-        }
-        if (paramDetails.size() == 2) {
-            validateSecondParam(resource, paramDetails, resources);
-        }
-    }
-
-    private void validateSecondParam(BLangFunction resource, List<BLangSimpleVariable> paramDetails,
-                                     List<BLangFunction> resources) {
-        if (resource.getName().getValue().equals(ArtemisConstants.ON_MESSAGE)) {
-            boolean isOnErrorAvailable = false;
-            for (BLangFunction res : resources) {
-                if (res.getName().getValue().equals(ArtemisConstants.ON_ERROR)) {
-                    isOnErrorAvailable = true;
-                }
-            }
-            if (!isOnErrorAvailable) {
-                dlog.logDiagnostic(Diagnostic.Kind.WARNING, resource.pos, "CAUTION: onError resource " +
-                        "function is not found; any errors that occur due to data binding will be discarded along " +
-                        "with the message.");
-            }
-            BType secondParamType = paramDetails.get(1).type;
-            int secondParamTypeTag = secondParamType.tag;
-            if (secondParamTypeTag != TypeTags.STRING && secondParamTypeTag != TypeTags.JSON &&
-                    secondParamTypeTag != TypeTags.XML && secondParamTypeTag != TypeTags.RECORD &&
-                    secondParamTypeTag != TypeTags.MAP && checkArrayType(secondParamType)) {
-                dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, INVALID_RESOURCE_SIGNATURE_FOR
-                        + resource.getName().getValue() + " resource in service " +
-                        ": The second parameter should be a string, json, xml, byte[], map or a record type");
-            }
-            if (secondParamTypeTag == TypeTags.MAP && checkMapConstraint(secondParamType)) {
-                dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, INVALID_RESOURCE_SIGNATURE_FOR
-                        + resource.getName().getValue() + " resource in service " +
-                        ": The second parameter should be a map of string, int, float, byte, boolean or byte[]");
-            }
-        } else if (ArtemisConstants.ON_ERROR.equals(resource.getName().getValue()) &&
-                !"error".equals(paramDetails.get(1).type.toString())) {
-            dlog.logDiagnostic(Diagnostic.Kind.ERROR, resource.pos, String.format(
-                    "Invalid resource signature for %s resource in service : The second parameter should be an error",
-                    resource.getName().getValue()));
-        }
-    }
-
-    private boolean checkArrayType(BType secondParamType) {
-        return secondParamType.tag != TypeTags.ARRAY || (secondParamType instanceof BArrayType &&
-                ((BArrayType) secondParamType).getElementType().tag != org.ballerinalang.model.types.TypeTags.BYTE_TAG);
-    }
-
-    private boolean checkMapConstraint(BType paramType) {
-        if (paramType instanceof BMapType) {
-            BType constraintType = ((BMapType) paramType).constraint;
-            int constraintTypeTag = constraintType.tag;
-            return constraintTypeTag != TypeTags.STRING && constraintTypeTag != TypeTags.INT &&
-                    constraintTypeTag != TypeTags.FLOAT && constraintTypeTag != TypeTags.BYTE &&
-                    constraintTypeTag != TypeTags.BOOLEAN && checkArrayType(constraintType);
-        }
-        return false;
     }
 }
