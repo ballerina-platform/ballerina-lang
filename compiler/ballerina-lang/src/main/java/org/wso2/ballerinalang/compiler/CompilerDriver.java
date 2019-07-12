36780e3bf931d9e4df684f3d90d3f040f194ad4a
Pubudu Fernando <pubuduf@wso2.com>
2019-07-12 14:36:21 +0530

    Fix issue with the error detail not being set properly

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/CompilerDriver.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/CompilerDriver.java
@@ -138,6 +138,7 @@ public class CompilerDriver {
         if (!LOAD_BUILTIN_FROM_SOURCE) {
             symbolTable.langInternalModuleSymbol = pkgLoader.loadPackageSymbol(INTERNAL, null, null);
             symbolTable.langAnnotationModuleSymbol = pkgLoader.loadPackageSymbol(ANNOTATIONS, null, null);
+            symResolver.reloadErrorType();
             symbolTable.langArrayModuleSymbol = pkgLoader.loadPackageSymbol(ARRAY, null, null);
             symbolTable.langDecimalModuleSymbol = pkgLoader.loadPackageSymbol(DECIMAL, null, null);
             symbolTable.langErrorModuleSymbol = pkgLoader.loadPackageSymbol(ERROR, null, null);
