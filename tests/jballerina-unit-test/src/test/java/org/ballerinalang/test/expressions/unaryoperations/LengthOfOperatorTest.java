bcab3783a71d38e45ee1eb1e7740d64545315943
Kishanthan <kshanth2101@gmail.com>
2019-07-14 16:56:07 +0530

    Fix checkstyle issues

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/unaryoperations/LengthOfOperatorTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/unaryoperations/LengthOfOperatorTest.java
@@ -21,7 +21,6 @@ import org.ballerinalang.model.values.BValue;
 import org.ballerinalang.test.util.BCompileUtil;
 import org.ballerinalang.test.util.BRunUtil;
 import org.ballerinalang.test.util.CompileResult;
-import org.ballerinalang.util.exceptions.BLangRuntimeException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
