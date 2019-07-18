0f66a8eab756a7163a82998ea7c7081681e367f8
wggihan <wggihan@gmail.com>
2019-07-18 13:19:50 +0530

    Remove deprecated class usage

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/socket/SocketBaseTest.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/socket/SocketBaseTest.java
@@ -18,7 +18,7 @@
 
 package org.ballerinalang.test.socket;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.ballerinalang.test.BaseTest;
 import org.ballerinalang.test.context.BServerInstance;
 import org.ballerinalang.test.context.BallerinaTestException;
