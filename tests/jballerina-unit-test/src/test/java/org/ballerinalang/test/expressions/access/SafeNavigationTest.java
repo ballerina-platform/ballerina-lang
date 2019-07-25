c7141fa73645510c4224c7d494c801da395adc8e
Dhananjaya Wickramasingha <dhananjayaw@wso2.com>
2019-07-25 09:08:59 +0530

    Fix and enabled brokenOnLangLibChange tests

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/access/SafeNavigationTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/access/SafeNavigationTest.java
@@ -17,18 +17,11 @@
  */
 package org.ballerinalang.test.expressions.access;
 
-import org.ballerinalang.model.util.JsonParser;
-import org.ballerinalang.model.values.BError;
-import org.ballerinalang.model.values.BInteger;
-import org.ballerinalang.model.values.BMap;
-import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BValue;
-import org.ballerinalang.model.values.BValueArray;
 import org.ballerinalang.test.util.BAssertUtil;
 import org.ballerinalang.test.util.BCompileUtil;
 import org.ballerinalang.test.util.BRunUtil;
 import org.ballerinalang.test.util.CompileResult;
-import org.ballerinalang.util.exceptions.BLangRuntimeException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -94,273 +87,20 @@ public class SafeNavigationTest {
     }
 
     @Test
-    public void testNonErrorPath() {
-        BValue[] returns = BRunUtil.invoke(result, "testNonErrorPath");
-        Assert.assertEquals(returns[0].stringValue(), "Colombo");
-    }
-
-    @Test
     public void testNotNilPath() {
         BValue[] returns = BRunUtil.invoke(result, "testNotNilPath");
         Assert.assertEquals(returns[0].stringValue(), "Colombo");
     }
 
     @Test
-    public void testErrorInMiddle() {
-        BValue[] returns = BRunUtil.invoke(result, "testErrorInMiddle");
-        Assert.assertTrue(returns[0] instanceof BError);
-        Assert.assertEquals(((BError) returns[0]).getReason(), "custom error");
-    }
-
-    @Test
-    public void testErrorInFirstVar() {
-        BValue[] returns = BRunUtil.invoke(result, "testErrorInFirstVar");
-        Assert.assertTrue(returns[0] instanceof BError);
-        Assert.assertEquals(((BError) returns[0]).getReason(), "custom error");
-    }
-
-    @Test
-    public void testNilInMiddle() {
-        BValue[] returns = BRunUtil.invoke(result, "testNilInMiddle");
-        Assert.assertEquals(returns[0], null);
-        Assert.assertEquals(returns[1], null);
-    }
-
-    @Test
     public void testNilInFirstVar() {
         BValue[] returns = BRunUtil.invoke(result, "testNilInFirstVar");
         Assert.assertEquals(returns[0], null);
-        Assert.assertEquals(returns[1], null);
-    }
-
-    @Test
-    public void testSafeNavigatingNilJSON_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigatingNilJSON_1");
-        Assert.assertEquals(returns[0], null);
-    }
-
-    @Test
-    public void testSafeNavigatingNilJSON_2() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigatingNilJSON_2");
-        Assert.assertEquals(returns[0], null);
-    }
-
-    @Test
-    public void testSafeNavigatingNilJSON_3() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigatingNilJSON_3");
-        Assert.assertEquals(returns[0], null);
-    }
-
-    @Test
-    public void testSafeNavigatingNilJSON_4() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigatingNilJSON_4");
-        Assert.assertEquals(returns[0], null);
-    }
-
-    @Test
-    public void testSafeNavigatingJSONWithNilInMiddle_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigatingJSONWithNilInMiddle_1");
-        Assert.assertEquals(returns[0], null);
-    }
-
-    @Test
-    public void testSafeNavigatingJSONWithNilInMiddle_2() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigatingJSONWithNilInMiddle_2");
-        Assert.assertEquals(returns[0], null);
-    }
-
-    @Test
-    public void testSafeNavigatingWithFuncInovc_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigatingWithFuncInovc_1");
-        Assert.assertEquals(returns[0], null);
-    }
-
-    @Test
-    public void testSafeNavigationOnObject_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigationOnObject_1");
-        Assert.assertEquals(returns[0].stringValue(), "John");
-    }
-
-    @Test
-    public void testSafeNavigationOnObject_2() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigationOnObject_2");
-        Assert.assertEquals(returns[0].stringValue(), "null name");
-    }
-
-    @Test
-    public void testSafeNavigationOnObject_3() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigationOnObject_3");
-        Assert.assertEquals(returns[0].stringValue(), "John");
-    }
-
-    @Test
-    public void testSafeNavigateArray_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigateArray_1");
-        Assert.assertNull(returns[0]);
-    }
-
-    @Test
-    public void testSafeNavigateArray_2() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigateArray_2");
-        Assert.assertNull(returns[0]);
-    }
-
-    @Test
-    public void testSafeNavigateOnErrorOrNull() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigateOnErrorOrNull");
-        Assert.assertNull(returns[0]);
-    }
-
-    @Test
-    public void testSafeNavigateOnJSONArrayOfArray() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigateOnJSONArrayOfArray");
-        Assert.assertEquals(returns[0].stringValue(), "Bob");
-    }
-
-    @Test
-    public void testJSONNilLiftingOnLHS_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testJSONNilLiftingOnLHS_1");
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{\"info\":{\"address1\":{\"city\":\"Colombo\"}}}");
-
-        Assert.assertTrue(returns[1] instanceof BMap);
-        Assert.assertEquals(returns[1].stringValue(), "{\"info\":{\"address2\":{\"city\":\"Kandy\"}}}");
-
-        Assert.assertTrue(returns[2] instanceof BMap);
-        Assert.assertEquals(returns[2].stringValue(), "{\"info\":{\"address3\":{\"city\":\"Galle\"}}}");
-
-        Assert.assertTrue(returns[3] instanceof BMap);
-        Assert.assertEquals(returns[3].stringValue(), "{\"info\":{\"address4\":{\"city\":\"Jaffna\"}}}");
-    }
-
-    @Test(expectedExceptions = {BLangRuntimeException.class},
-            expectedExceptionsMessageRegExp = "error: failed to get element from json: " +
-                    "array index out of range: index: 2, size: 0.*")
-    public void testJSONNilLiftingOnLHS_2() {
-        BRunUtil.invoke(result, "testJSONNilLiftingOnLHS_2");
     }
 
     @Test
     public void testNonExistingMapKeyWithIndexAccess() {
         BValue[] returns = BRunUtil.invoke(result, "testNonExistingMapKeyWithIndexAccess");
-        Assert.assertNull(returns[0]);
-    }
-
-    @Test(expectedExceptions = {BLangRuntimeException.class},
-            expectedExceptionsMessageRegExp = "error: \\{ballerina\\}KeyNotFound \\{\"message\":\"cannot find key " +
-                    "'a'\"\\}.*")
-    public void testNonExistingMapKeyWithFieldAccess() {
-        BValue[] returns = BRunUtil.invoke(result, "testNonExistingMapKeyWithFieldAccess");
-        Assert.assertEquals(returns[0].stringValue(), "Bob");
-    }
-
-    @Test
-    public void testMapNilLiftingOnLHS_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testMapNilLiftingOnLHS_1");
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{\"name\":\"John\"}");
-    }
-
-    @Test
-    public void testMapNilLiftingOnLHS_2() {
-        BValue[] returns = BRunUtil.invoke(result, "testMapNilLiftingOnLHS_2");
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{\"name\":{\"fname\":\"John\"}}");
-    }
-
-    @Test
-    public void testMapNilLiftingOnLHS_3() {
-        BValue[] returns = BRunUtil.invoke(result, "testMapNilLiftingOnLHS_3");
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{\"name\":{\"fname\":\"John\"}}");
-    }
-
-    @Test
-    public void testMapNilLiftingOnLHS_4() {
-        BValue[] returns = BRunUtil.invoke(result, "testMapNilLiftingOnLHS_4");
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{\"name\":{foo:(), fname:\"John\"}}");
-    }
-
-    @Test(expectedExceptions = {BLangRuntimeException.class},
-            expectedExceptionsMessageRegExp = "error: NullReferenceException.*")
-    public void testMapNilLiftingOnLHS_5() {
-        BRunUtil.invoke(result, "testMapNilLiftingOnLHS_5");
-    }
-
-    @Test
-    public void testMapInRecordNilLiftingOnLHS_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testMapInRecordNilLiftingOnLHS_1");
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{foo:{\"name\":\"Doe\"}}");
-    }
-
-    @Test(expectedExceptions = {BLangRuntimeException.class},
-            expectedExceptionsMessageRegExp = "error: NullReferenceException.*")
-    public void testMapInRecordNilLiftingOnLHS_2() {
-        BRunUtil.invoke(result, "testMapInRecordNilLiftingOnLHS_2");
-    }
-
-    @Test
-    public void testFunctionInvocOnJsonNonExistingField() {
-        BValue[] vals = {JsonParser.parse("\"hello\"")};
-        BValue[] returns = BRunUtil.invoke(result, "testFunctionInvocOnJsonNonExistingField", vals);
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{\"name\":\"John\"}");
-
-        Assert.assertTrue(returns[1] instanceof BString);
-        Assert.assertEquals(returns[1].stringValue(), "null");
-
-        Assert.assertTrue(returns[2] instanceof BValueArray);
-        Assert.assertEquals(returns[2].stringValue(), "[]");
-    }
-
-    @Test
-    public void testFunctionInvocOnNullabeType() {
-        BValue[] returns = BRunUtil.invoke(result, "testFunctionInvocOnNullabeType");
-        Assert.assertTrue(returns[0] instanceof BInteger);
-        Assert.assertEquals(((BInteger) returns[0]).intValue(), 61);
-    }
-
-    @Test
-    public void testUpdatingNullableRecordField_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testUpdatingNullableRecordField_1");
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{a:0, fname:\"John\", lname:\"\", info1:(), " +
-                "info2:{address1:(), address2:{street:\"Palm Grove\", city:\"Kandy\", country:\"Sri Lanka\"}}}");
-    }
-
-    @Test
-    public void testUpdatingNullableRecordField_2() {
-        BValue[] returns = BRunUtil.invoke(result, "testUpdatingNullableRecordField_2");
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{a:0, fname:\"John\", lname:\"\", info1:(), " +
-                "info2:{address1:(), address2:{street:\"\", city:\"Kandy\", country:\"Sri Lanka\"}}}");
-    }
-
-    @Test
-    public void testUpdatingNullableObjectField_1() {
-        BValue[] returns = BRunUtil.invoke(result, "testUpdatingNullableObjectField_1");
-        Assert.assertTrue(returns[0] instanceof BMap);
-        Assert.assertEquals(returns[0].stringValue(), "{a:0, fname:\"John\", lname:\"\", info1:(), " +
-                "info2:{address1:(), address2:{street:\"Palm Grove\", city:\"Kandy\", country:\"Sri Lanka\"}}}");
-    }
-
-    @Test(expectedExceptions = {BLangRuntimeException.class},
-            expectedExceptionsMessageRegExp = "error: NullReferenceException.*")
-    public void testUpdatingNullableObjectField_2() {
-        BRunUtil.invoke(result, "testUpdatingNullableObjectField_2");
-    }
-
-    @Test
-    public void testSafeNavigationOnFieldAccess() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigationOnFieldAccess");
-        Assert.assertNull(returns[0]);
-    }
-
-    @Test
-    public void testSafeNavigationOnIndexBasedAccess() {
-        BValue[] returns = BRunUtil.invoke(result, "testSafeNavigationOnIndexBasedAccess");
         Assert.assertNull(returns[0]);
     }
 }
--- tests/jballerina-unit-test/src/test/resources/test-src/expressions/access/safe_navigation.bal
+++ tests/jballerina-unit-test/src/test/resources/test-src/expressions/access/safe_navigation.bal
@@ -19,100 +19,42 @@ type Address record {
     string country = "Sri Lanka";
 };
 
-function testNonErrorPath () returns any|error {
-    Address adrs = {city:"Colombo"};
-    Info inf = {address1 : adrs};
-    Person prsn = {info1 : inf};
-    Person|error p = prsn;
-    string|error|() x = p!info1!address1!city;
-    return x;
-}
-
 function testNotNilPath () returns any|error {
     Address adrs = {city:"Colombo"};
     Info inf = {address2 : adrs};
     Person prsn = {info2 : inf};
     Person|() p = prsn;
-    string|error|() x = p.info2.address2.city;
+    string|error|() x = p?.info2?.address2?.city;
     return x;
 }
 
-function testErrorInMiddle () returns any|error {
-    error e = error("custom error");
-    Info inf = {address1 : e};
-    Person prsn = {info1 : inf};
-    Person|error p = prsn;
-    string|error|() x = p!info1!address1!city;
-    return x;
-}
-
-function testErrorInFirstVar () returns any|error {
-    error e = error("custom error");
-    Person|error p = e;
-    string|error|() x = p!info1!address1!city;
-    return x;
-}
-
-function testNilInMiddle () returns [any|error, any|error] {
+function testNilInMiddle () returns any|error {
     Info inf = {address2 : ()};
     Person prsn = {info2 : inf};
     Person|() p = prsn;
-    string|error|() x = p.info1!address1!city;
-    string|error|() y = p.info2.address2.city;
-    return [x, y];
+    string|error|() y = p?.info2?.address2?.city;
+    return y;
 }
 
-function testNilInFirstVar () returns [any|error, any|error] {
+function testNilInFirstVar () returns any|error {
     Person|() p = ();
-    string|error|() x = p.info1!address1!city;
-    string|error|() y = p.info2.address2.city;
-    return [x, y];
+    string|error|() y = p?.info2?.address2?.city;
+    return y;
 }
 
-function testSafeNavigatingNilJSON_1 () returns any {
+function testSafeNavigatingNilJSON_1 () returns any|error {
     json j = {};
     return j.foo;
 }
 
-function testSafeNavigatingNilJSON_2 () returns any {
-    json j = {};
-    return j["foo"];
-}
-
-function testSafeNavigatingNilJSON_3 () returns any {
+function testSafeNavigatingNilJSON_3 () returns any|error {
     json j = {};
     return j.foo.bar;
-}
-
-function testSafeNavigatingNilJSON_4 () returns any {
-    json j = {};
-    return j["foo"]["bar"];
-}
-
-function testSafeNavigatingJSONWithNilInMiddle_1 () returns any {
-    json j = {name:"hello"};
-    return j.info["name"].fname;
-}
-
-function testSafeNavigatingJSONWithNilInMiddle_2 () returns any {
-    json j = {name:"hello"};
-    return j["info"].name["fname"];
 }
 
 function testSafeNavigatingNilMap () returns any {
     map<any> m = {};
     return m["foo"];
-}
-
-function testSafeNavigatingWithFuncInovc_1 () returns any|error {
-    string|error|() x = getNullablePerson().info1!address1!city;
-    return x;
-}
-
-function testSafeNavigatingWithFuncInovc_2 () returns string|() {
-    json j = {};
-    string|() x = j.getKeys()[0];
-    return x;
 }
 
 function getNullablePerson() returns Person|() {
@@ -128,137 +70,14 @@ type Employee object {
   }
 };
 
-function testSafeNavigationOnObject_1() returns string {
-  Employee? p;
-  p = new Employee();
-    return p.getName() ?: "null name";
-}
-
-function testSafeNavigationOnObject_2() returns string {
-  Employee? p = ();
-  return p.getName() ?: "null name";
-}
-
-function testSafeNavigationOnObject_3() returns string {
-  Employee?|error p = ();
-  p = new Employee();
-  string|error? name = p!getName();
-  return name is string ? name : "null name";
-}
-
-function testSafeNavigationOnObject_4() returns string {
-  error e = error("");
-  Employee|error p = e;
-  string|error name = p!getName();
-  return name is string ? name : "error name";
-}
-
-function testSafeNavigateArray_1() returns Person? {
-    Person[]|() p = ();
-    return p[0];
-}
-
-function testSafeNavigateArray_2() returns string? {
-    Person[]|() p = ();
-    return p[0].info2.address2.city;
-}
-
-function testSafeNavigateOnErrorOrNull() returns string? {
-    error|() e = ();
-    return e.reason();
-}
-
-function testSafeNavigateOnJSONArrayOfArray() returns json {
-    json j = {"values" : [ ["Alex", "Bob"] ] };
-    return j.values[0][1];
-}
-
-function testJSONNilLiftingOnLHS_1() returns [json, json, json, json] {
-    json j1 = {};
-    j1.info["address1"].city = "Colombo";
-
-    json j2 = { info:{} };
-    j2.info["address2"].city = "Kandy";
-
-    json j3 = { info : { address3: {} } };
-    j3.info["address3"].city = "Galle";
-
-    json j4 = {};
-    j4.info["address4"].city = "Jaffna";
-
-    return [j1, j2, j3, j4];
-}
-
-function testJSONNilLiftingOnLHS_2() returns json {
-    json j = [];
-    j[2].address[3].city = "Colombo";
-    return j;
-}
-
 function testNonExistingMapKeyWithIndexAccess() returns string? {
     map<string> m = {};
     return m["a"];
 }
 
-function testNonExistingMapKeyWithFieldAccess() returns string {
-    map<string> m = {};
-    return m.a;
-}
-
-function testMapNilLiftingOnLHS_1() returns map<any> {
-    map<any> m = {};
-    m.name = "John";
-    return m;
-}
-
-function testMapNilLiftingOnLHS_2() returns map<any> {
-    map<json> m = {};
-    m["name"].fname = "John";
-    return m;
-}
-
-function testMapNilLiftingOnLHS_3() returns map<map<any>?> {
-    map<map<any>?> m = {};
-    m["name"].fname = "John";
-    return m;
-}
-
-function testMapNilLiftingOnLHS_4() returns map<A?> {
-    map<A?> m = {};
-    m["name"].fname = "John";
-    return m;
-}
-
-function testMapNilLiftingOnLHS_5() returns map<string>? {
-    map<string>? m = ();
-    m["name"] = "John";
-    return m;
-}
-
 type A record {
     map<any>? foo = ();
 };
-
-function testMapInRecordNilLiftingOnLHS_1() returns A {
-    A a = {};
-    a.foo = {"name" : "John"};
-    a.foo.name = "Doe";
-    return a;
-}
-
-function testMapInRecordNilLiftingOnLHS_2() returns A? {
-    A? a = ();
-    a.foo = {"name" : "John"};
-    a.foo.name = "Doe";
-    return a;
-}
-
-function testFunctionInvocOnJsonNonExistingField (json inputJson) returns [json, string, string[]] {
-    json j = {name:"John"};
-    string s = j.foo.bar.toString();
-    string[] keys = j.foo.bar.getKeys();
-    return [j, s, keys];
-}
 
 type Student object {
     public string name = "";
@@ -268,29 +87,6 @@ type Student object {
         self.marks = self.marks + 1;
     }
 };
-
-function testFunctionInvocOnNullabeType() returns int {
-    Student s1 = new;
-    Student|() s2 = s1;
-    s2.increaseMarks();
-    return s2.marks ?: -1;
-}
-
-function testUpdatingNullableRecordField_1() returns any {
-    Address adrs = {street:"Palm Grove"};
-    Info inf = {address2 : adrs};
-    Person prsn = {info2 : inf};
-    Person|() p = prsn;
-    p.info2.address2.city = "Kandy";
-    return p;
-}
-
-function testUpdatingNullableRecordField_2() returns any {
-    Person prsn = {};
-    Person|() p = prsn;
-    p.info2.address2.city = "Kandy";
-    return p;
-}
 
 type PersonObject object {
     public int a = 0;
@@ -323,30 +119,7 @@ type AddressObject object {
     }
 };
 
-function testUpdatingNullableObjectField_1() returns any {
-    AddressObject adrs = new("Palm Grove");
-    InfoObject inf = new(adrs);
-    PersonObject prsn = new(inf);
-    PersonObject|() p = prsn;
-    p.info2.address2.city = "Kandy";
-    return p;
-}
-
-function testUpdatingNullableObjectField_2() returns any {
-    PersonObject|() p = new PersonObject(());
-    p.info2.address2.city = "Kandy";
-    return p;
-}
-
 function getJsonValue() returns json|error {
     return 10;
 }
 
-function testSafeNavigationOnFieldAccess() returns json|error {
-    return getJsonValue()!foo;
-}
-
-function testSafeNavigationOnIndexBasedAccess() returns json|error {
-    (json|error)?[] data = [getJsonValue()];
-    return data[0]!foo;
-}
