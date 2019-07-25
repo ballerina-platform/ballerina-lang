ad07937f25ed58cfdc58eddeceb52b4e5aa4c6d2
Hemika Kodikara <hemika.kodikara@gmail.com>
2019-07-25 11:22:41 +0530

    refator packerina build context
    Add Balo toml model to packerina. Add ModuleWrite to packerina. Use enum
    for build context fields. Add implementation for balo gen.

--- cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/writer/ModuleFileWriter.java
+++ cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/writer/ModuleFileWriter.java
@@ -1,28 +1,29 @@
 /*
- *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-package org.wso2.ballerinalang.compiler;
+package org.ballerinalang.packerina.writer;
 
 import com.moandjiezana.toml.TomlWriter;
 import org.ballerinalang.compiler.BLangCompilerException;
-import org.ballerinalang.toml.model.Balo;
+import org.ballerinalang.packerina.model.BaloToml;
 import org.ballerinalang.toml.model.Manifest;
 import org.ballerinalang.toml.model.Module;
 import org.ballerinalang.toml.parser.ManifestProcessor;
+import org.wso2.ballerinalang.compiler.SourceDirectory;
 import org.wso2.ballerinalang.compiler.tree.BLangPackage;
 import org.wso2.ballerinalang.compiler.util.CompilerContext;
 import org.wso2.ballerinalang.compiler.util.ProjectDirConstants;
@@ -292,7 +293,7 @@ public class ModuleFileWriter {
 
         TomlWriter writer = new TomlWriter();
         // Write to BALO.toml
-        String baloToml = writer.write(new Balo());
+        String baloToml = writer.write(new BaloToml());
         Files.write(baloMetaFile, baloToml.getBytes());
 
         // Write to MODULE.toml
