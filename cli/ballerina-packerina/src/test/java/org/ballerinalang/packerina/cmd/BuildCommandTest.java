ab686132193d0c85537af52e3d5ce6408e13d627
Joseph Fonseka <hevayo@gmail.com>
2019-07-29 17:01:26 +0530

    Add test skip flag support

--- cli/ballerina-packerina/src/test/java/org/ballerinalang/packerina/cmd/BuildCommandTest.java
+++ cli/ballerina-packerina/src/test/java/org/ballerinalang/packerina/cmd/BuildCommandTest.java
@@ -82,7 +82,7 @@ public class BuildCommandTest extends CommandTest {
         zipFile(libs.resolve("json.jar").toFile(), "json.class");
 
         // Build the project
-        String[] compileArgs = {"--skip-tests", "-c"};
+        String[] compileArgs = {};
         BuildCommand buildCommand = new BuildCommand(projectDirectory, printStream, false);
         new CommandLine(buildCommand).parse(compileArgs);
         buildCommand.execute();
