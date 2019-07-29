c97112aea35fc6ff737217cd31a847fd029e13e1
Pubudu Fernando <pubuduf@wso2.com>
2019-07-29 12:28:24 +0530

    Fix record var reference test

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/matchstmt/MatchStructuredRecordPatternsTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/matchstmt/MatchStructuredRecordPatternsTest.java
@@ -26,7 +26,6 @@ import org.ballerinalang.test.util.BRunUtil;
 import org.ballerinalang.test.util.CompileResult;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 /**
