34ef666699989bf0513ad8a6f6d556a6352ba30c
Anoukh <anoukh.ashley@gmail.com>
2019-07-30 10:34:53 +0530

    Minor refactor and fix integration test case order

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/observability/tracing/TracingTestCase.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/observability/tracing/TracingTestCase.java
@@ -156,36 +156,6 @@ public class TracingTestCase extends BaseTest {
                 .count(), 3, "Mismatch in number of root spans.");
     }
 
-    // 1. helloWorld_service_0 -> resourceOne (Root Span)
-    // 2. helloWorld_service_0 -> ballerina/http/Client:get
-    // 3. helloWorld_service_0 -> ballerina/http/HttpClient:get
-    // 4. helloWorld_service_0 -> resourceTwo
-    // 5. helloWorld_service_0 -> ballerina/http/Caller:respond
-    // 6. helloWorld_service_0 -> ballerina/http/Caller:respond
-
-    // 7. helloWorld_service_0 -> getMockTracers (Root Span)
-    // 8. helloWorld_service_0 -> ballerina/http/Caller:respond
-
-    // 9. helloWorld_service_0 -> resourceOne (Root Span)
-    // 10. helloWorld_service_0 -> uSpanOne
-    // 11. helloWorld_service_0 -> uSpanTwo
-    // 12. helloWorld_service_0 -> ballerina/http/Client:get
-    // 13. helloWorld_service_0 -> ballerina/http/HttpClient:get
-    // 14. helloWorld_service_0 -> resourceTwo
-    // 15. helloWorld_service_0 -> ballerina/http/Caller:respond
-    // 16. helloWorld_service_0 -> ballerina/http/Caller:respond
-
-    // 17. helloWorld_service_0 -> getMockTracers (Root Span)
-    // 18. helloWorld_service_0 -> ballerina/http/Caller:respond
-
-    // 19. helloWorld_service_0 -> resourceOne (Root Span)
-    // 20. helloWorld_service_0 -> ballerina/http/Client:get
-    // 21. helloWorld_service_0 -> ballerina/http/HttpClient:get
-    // 22. helloWorld_service_0 -> resourceTwo
-    // 23. helloWorld_service_0 -> ballerina/http/Caller:respond
-    // 24. helloWorld_service_0 -> ballerina/http/Caller:respond
-    // 24. helloWorld_service_0 -> uSpanThree (Root Span)
-    // 24. helloWorld_service_0 -> uSpanFour
     @Test
     public void testObservePackageUserTraceTrue() throws Exception {
         final String service = "http://localhost:9092/echoService/";
@@ -198,6 +168,36 @@ public class TracingTestCase extends BaseTest {
         out.println(data);
         List<BMockSpan> mockSpans = new Gson().fromJson(data, type);
 
+        // 1. helloWorld_service_0 -> resourceOne (Root Span)
+        // 2. helloWorld_service_0 -> ballerina/http/Client:get
+        // 3. helloWorld_service_0 -> ballerina/http/HttpClient:get
+        // 4. helloWorld_service_0 -> resourceTwo
+        // 5. helloWorld_service_0 -> ballerina/http/Caller:respond
+        // 6. helloWorld_service_0 -> ballerina/http/Caller:respond
+
+        // 7. helloWorld_service_0 -> getMockTracers (Root Span)
+        // 8. helloWorld_service_0 -> ballerina/http/Caller:respond
+
+        // 9. helloWorld_service_0 -> resourceOne (Root Span)
+        // 10. helloWorld_service_0 -> uSpanOne
+        // 11. helloWorld_service_0 -> uSpanTwo
+        // 12. helloWorld_service_0 -> ballerina/http/Client:get
+        // 13. helloWorld_service_0 -> ballerina/http/HttpClient:get
+        // 14. helloWorld_service_0 -> resourceTwo
+        // 15. helloWorld_service_0 -> ballerina/http/Caller:respond
+        // 16. helloWorld_service_0 -> ballerina/http/Caller:respond
+
+        // 17. helloWorld_service_0 -> getMockTracers (Root Span)
+        // 18. helloWorld_service_0 -> ballerina/http/Caller:respond
+
+        // 19. helloWorld_service_0 -> resourceOne (Root Span)
+        // 20. helloWorld_service_0 -> ballerina/http/Client:get
+        // 21. helloWorld_service_0 -> ballerina/http/HttpClient:get
+        // 22. helloWorld_service_0 -> resourceTwo
+        // 23. helloWorld_service_0 -> ballerina/http/Caller:respond
+        // 24. helloWorld_service_0 -> ballerina/http/Caller:respond
+        // 24. helloWorld_service_0 -> uSpanThree (Root Span)
+        // 24. helloWorld_service_0 -> uSpanFour
         Assert.assertEquals(mockSpans.size(), 26, "Mismatch in number of spans reported.");
 
         Assert.assertEquals(mockSpans.stream()
