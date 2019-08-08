297d84e5318465868c6d4b9dc6273de0828f80bf
Kavith Lokuhewage <kaviththiranga@gmail.com>
2019-08-08 10:35:25 +0530

    Add temp instrumentation key for telemetry testing

--- tool-plugins/vscode/src/telemetry/index.ts
+++ tool-plugins/vscode/src/telemetry/index.ts
@@ -1,8 +1,8 @@
 import TelemetryReporter from "vscode-extension-telemetry";
 import { BallerinaExtension } from "../core";
 
-// TODO generate a working instrumentation key from azure
-const INSTRUMENTATION_KEY = "ballerina.ext.instrumentation.key";
+// TODO generate a instrumentation key from azure with a production account
+const INSTRUMENTATION_KEY = "d1155177-d396-4504-ad0f-c3570da84c3e";
 
 export function createTelemetryReporter(ext: BallerinaExtension): TelemetryReporter {
     const reporter = new TelemetryReporter(ext.getID(), ext.getVersion(), INSTRUMENTATION_KEY); 
