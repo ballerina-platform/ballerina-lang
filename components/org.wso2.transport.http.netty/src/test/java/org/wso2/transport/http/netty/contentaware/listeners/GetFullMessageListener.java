e83ea86b3234a67135d4f6cef8ad33f80f8e8098
shafreen <shafreen@wso2.com>
2019-08-13 07:57:41 +0530

    Update license

--- components/org.wso2.transport.http.netty/src/test/java/org/wso2/transport/http/netty/contentaware/listeners/GetFullMessageListener.java
+++ components/org.wso2.transport.http.netty/src/test/java/org/wso2/transport/http/netty/contentaware/listeners/GetFullMessageListener.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *  WSO2 Inc. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -14,7 +14,6 @@
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
  *  under the License.
- *
  */
 
 package org.wso2.transport.http.netty.contentaware.listeners;
@@ -39,7 +38,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * A Message processor which echos the incoming message.
+ * This class implements GetFullMessageListener. GetFullMessageListener is responsible for echoing back the
+ * request payload using getFullHttpCarbonMessage API.
  */
 public class GetFullMessageListener implements HttpConnectorListener {
     private static final Logger LOG = LoggerFactory.getLogger(GetFullMessageListener.class);
