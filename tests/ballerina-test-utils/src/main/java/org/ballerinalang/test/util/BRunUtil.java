0fcef4df3a030026a1fa581ddd08d34d5bc02b74
Vinod Kavinda <vinod@wso2.com>
2019-08-13 09:18:10 +0530

    Fix checkstyle issue

--- tests/ballerina-test-utils/src/main/java/org/ballerinalang/test/util/BRunUtil.java
+++ tests/ballerina-test-utils/src/main/java/org/ballerinalang/test/util/BRunUtil.java
@@ -432,7 +432,7 @@ public class BRunUtil {
                 }
             };
 
-            Scheduler scheduler = new Scheduler( false);
+            Scheduler scheduler = new Scheduler(false);
             FutureValue futureValue = scheduler.schedule(jvmArgs, func, null, null, new HashMap<>());
             scheduler.start();
             if (futureValue.panic instanceof RuntimeException) {
