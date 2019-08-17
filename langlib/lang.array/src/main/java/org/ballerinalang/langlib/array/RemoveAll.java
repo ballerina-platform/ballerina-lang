5dc5c34d6c1016d090372a26309d27081e1fcec8
Dhananjaya Wickramasingha <dhananjayaw@wso2.com>
2019-08-17 10:38:18 +0530

    Remove unused import

--- langlib/lang.array/src/main/java/org/ballerinalang/langlib/array/RemoveAll.java
+++ langlib/lang.array/src/main/java/org/ballerinalang/langlib/array/RemoveAll.java
@@ -18,7 +18,6 @@
 
 package org.ballerinalang.langlib.array;
 
-import org.ballerinalang.jvm.BallerinaErrors;
 import org.ballerinalang.jvm.scheduling.Strand;
 import org.ballerinalang.jvm.values.ArrayValue;
 import org.ballerinalang.model.types.TypeKind;
