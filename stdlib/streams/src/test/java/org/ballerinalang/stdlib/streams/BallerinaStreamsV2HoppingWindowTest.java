5163b7ca6bf012bbf736ca352e1179bbba42005c
gimantha <gimantha@wso2.com>
2019-08-17 18:24:15 +0530

    Enable streams tests except for time based ones

--- stdlib/streams/src/test/java/org/ballerinalang/stdlib/streams/BallerinaStreamsV2HoppingWindowTest.java
+++ stdlib/streams/src/test/java/org/ballerinalang/stdlib/streams/BallerinaStreamsV2HoppingWindowTest.java
@@ -33,6 +33,7 @@ import org.testng.annotations.Test;
  *
  * @since 0.990.3
  */
+@Test(groups = { "TakesTooMuchTime" })
 public class BallerinaStreamsV2HoppingWindowTest {
     private CompileResult result1, result2, result3;
 
