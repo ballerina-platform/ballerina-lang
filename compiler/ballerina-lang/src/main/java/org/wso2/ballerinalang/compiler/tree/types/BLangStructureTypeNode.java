55dd426aafef611197e92b9d0f1b83e8e3bdc44f
Pubudu Fernando <pubuduf@wso2.com>
2019-08-18 21:05:05 +0530

    Define local anonymous records locally

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/tree/types/BLangStructureTypeNode.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/tree/types/BLangStructureTypeNode.java
@@ -38,6 +38,7 @@ public abstract class BLangStructureTypeNode extends BLangType implements Struct
     public List<BLangSimpleVariable> fields;
     public BLangFunction initFunction;
     public boolean isAnonymous;
+    public boolean isLocal;
     public boolean isFieldAnalyseRequired;
     public List<BLangType> typeRefs;
     public BSymbol symbol;
