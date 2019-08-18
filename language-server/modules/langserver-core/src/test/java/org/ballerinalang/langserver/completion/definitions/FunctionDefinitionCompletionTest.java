e425f03c81ef8667130d58586f46d40bf10404ea
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2019-08-18 19:03:06 +0530

    Fix typeof keyword suggestion on anon record

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/definitions/FunctionDefinitionCompletionTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/definitions/FunctionDefinitionCompletionTest.java
@@ -145,7 +145,8 @@ public class FunctionDefinitionCompletionTest extends CompletionTest {
                 {"assignmentStmt4.json", "function"},
                 {"assignmentStmt5.json", "function"},
                 {"assignmentStmt6.json", "function"},
-                {"typeofKWSuggestion.json", "function"}
+                {"typeofKWSuggestion.json", "function"},
+                {"typeofKWSuggestion2.json", "function"}
         };
     }
 }
