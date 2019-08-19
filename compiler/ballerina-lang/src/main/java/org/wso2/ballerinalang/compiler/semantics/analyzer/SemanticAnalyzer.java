69206eb721b94e8a5876ba607166a454284d6232
Dhananjaya Wickramasingha <dhananjayaw@wso2.com>
2019-08-19 13:55:32 +0530

    Fix error var ref optional detail type checking

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SemanticAnalyzer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SemanticAnalyzer.java
@@ -1738,6 +1738,8 @@ public class SemanticAnalyzer extends BLangNodeVisitor {
                 } else {
                     matchedType = BUnionType.create(null, symTable.nilType, rhsDetailType.restFieldType);
                 }
+            } else if (Symbols.isOptional(matchedDetailItem.symbol)) {
+                matchedType = BUnionType.create(null, symTable.nilType, matchedDetailItem.type);
             } else {
                 matchedType = matchedDetailItem.type;
             }
