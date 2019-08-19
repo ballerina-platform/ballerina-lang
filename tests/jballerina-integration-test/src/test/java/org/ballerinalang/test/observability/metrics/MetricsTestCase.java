8625842d43e33d58de4110995b8c685e2b2a9718
Rajith <lanka.vitharana@gmail.com>
2019-08-19 10:29:48 +0530

    Disable main function error return tests

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/observability/metrics/MetricsTestCase.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/observability/metrics/MetricsTestCase.java
@@ -46,8 +46,7 @@ import static org.ballerinalang.jvm.observability.ObservabilityConstants.CONFIG_
 /**
  * Integration test for observability of metrics.
  */
-//@Test(groups = "metrics-test")
-@Test(groups = { "broken" })
+@Test(groups = "metrics-test")
 public class MetricsTestCase extends BaseTest {
     private static BServerInstance serverInstance;
 
