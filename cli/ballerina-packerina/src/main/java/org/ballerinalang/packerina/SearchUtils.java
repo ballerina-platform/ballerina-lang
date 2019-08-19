77bad63b9f80bbbfbf0b628943c04e47c326df0e
Hemika Kodikara <hemika.kodikara@gmail.com>
2019-08-20 02:20:10 +0530

    fix error handling central search command

--- cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/SearchUtils.java
+++ cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/SearchUtils.java
@@ -46,9 +46,19 @@ public class SearchUtils {
         String urlWithModulePath = URI.create(RepoUtils.getRemoteRepoURL()).resolve("/modules/").toString();
         String proxyPortAsString = proxy.getPort() == 0 ? "" : Integer.toString(proxy.getPort());
         
-        Optional<RuntimeException> executionResult = executor.executeMainFunction("module_search",
+        Optional<RuntimeException> exception = executor.executeMainFunction("module_search",
                 urlWithModulePath, query, proxy.getHost(), proxyPortAsString, proxy.getUserName(), proxy.getPassword(),
                 RepoUtils.getTerminalWidth());
-        executionResult.ifPresent(e -> ERROR_STREAM.println(e.getMessage()));
+        if (exception.isPresent()) {
+            String errorMessage = exception.get().getMessage();
+            if (null != errorMessage && !"".equals(errorMessage.trim())) {
+                // removing the error stack
+                if (errorMessage.contains("\n\tat")) {
+                    errorMessage = errorMessage.substring(0, errorMessage.indexOf("\n\tat"));
+                }
+    
+                ERROR_STREAM.println(errorMessage);
+            }
+        }
     }
 }
