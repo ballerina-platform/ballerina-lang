c7e446c0833b993ab55b09c80fbc2607dcbaa460
Hemika Kodikara <hemika.kodikara@gmail.com>
2019-08-20 02:05:21 +0530

    add new test case for path dependency test cases

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/packaging/PathDependencyTestCase.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/packaging/PathDependencyTestCase.java
@@ -18,6 +18,7 @@
 
 package org.ballerinalang.test.packaging;
 
+import org.awaitility.Duration;
 import org.ballerinalang.test.BaseTest;
 import org.ballerinalang.test.context.BMainInstance;
 import org.ballerinalang.test.context.BallerinaTestException;
@@ -33,9 +34,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.given;
 import static org.ballerinalang.test.packaging.PackerinaTestUtils.deleteFiles;
 import static org.wso2.ballerinalang.compiler.util.ProjectDirConstants.BLANG_COMPILED_JAR_EXT;
 import static org.wso2.ballerinalang.compiler.util.ProjectDirConstants.BLANG_COMPILED_PKG_BINARY_EXT;
@@ -49,6 +55,7 @@ public class PathDependencyTestCase extends BaseTest {
     private Path tempTestResources;
     private Map<String, String> envVariables;
     private BMainInstance balClient;
+    private String orgName = "bcintegrationtest";
     
     @BeforeClass()
     public void setUp() throws IOException, BallerinaTestException {
@@ -176,6 +183,145 @@ public class PathDependencyTestCase extends BaseTest {
         balClient.runMain("run", new String[] {bazBuildMsg}, envVariables, new String[0],
                 new LogLeecher[]{bazRunLeecher}, caseResources.resolve("TestProject2").toString());
         bazRunLeecher.waitForText(10000);
+    }
+    
+    /**
+     * Case4: 1. Build TestProject1 and push bee to central.
+     * 2. Build TestProject2 and push to central where module fee depends on bee.
+     * 3. Build TestProject3 which imports fee.
+     * 4. Run the jar if fee. Check output.
+     * 5. Modify and build bee.
+     * 6. Set dependency path to bee in TestProject3.
+     * 7. Check if output has changes.
+     *
+     * @throws BallerinaTestException Error when executing the commands.
+     */
+    @Test()
+    public void testBaloPathCase4() throws BallerinaTestException, IOException {
+        Path caseResources = tempTestResources.resolve("case4");
+        // Build bee module of TestProject1
+        //// change module name
+        String beeModuleName = "bee" + PackerinaTestUtils.randomModuleName(10);
+        Path testProjBeeModulePath = caseResources.resolve("TestProject1").resolve("src").resolve(beeModuleName);
+        Files.createDirectories(caseResources.resolve("TestProject1").resolve("src").resolve(beeModuleName));
+        copyFolder(caseResources.resolve("TestProject1").resolve("src").resolve("bee"), testProjBeeModulePath);
+        deleteFiles(caseResources.resolve("TestProject1").resolve("src").resolve("bee"));
+        
+        String beeModuleBaloFileName = beeModuleName + "-" + ProgramFileConstants.IMPLEMENTATION_VERSION + "-any-1.2.0"
+                                       + BLANG_COMPILED_PKG_BINARY_EXT;
+        
+        String module1BuildMsg = "target" + File.separator + "balo" + File.separator + beeModuleBaloFileName;
+        LogLeecher beeModuleBuildLeecher = new LogLeecher(module1BuildMsg);
+        balClient.runMain("build", new String[]{"-c"}, envVariables, new String[]{},
+                new LogLeecher[]{beeModuleBuildLeecher}, caseResources.resolve("TestProject1").toString());
+        beeModuleBuildLeecher.waitForText(5000);
+    
+        // Push bee module of TestProject1
+        String beePushMsg = orgName + "/" + beeModuleName + ":1.2.0 [project repo -> central]";
+        LogLeecher beePushLeecher = new LogLeecher(beePushMsg);
+        balClient.runMain("push", new String[]{}, envVariables, new String[]{},
+                new LogLeecher[]{beePushLeecher}, caseResources.resolve("TestProject1").toString());
+        beePushLeecher.waitForText(5000);
+    
+        
+        // Build fee module of TestProject2
+        String feeModuleName = "fee" + PackerinaTestUtils.randomModuleName(10);
+        //// replace import
+        Path feeBalPath = caseResources.resolve("TestProject2").resolve("src").resolve("fee").resolve("impl.bal");
+        Stream<String> lines = Files.lines(feeBalPath);
+        List<String> replaced = lines.map(line -> line.replaceAll("bee", beeModuleName))
+                .collect(Collectors.toList());
+        Files.write(feeBalPath, replaced);
+        
+        //// change module name
+        Path testProjFeeModulePath = caseResources.resolve("TestProject2").resolve("src").resolve(feeModuleName);
+        Files.createDirectories(caseResources.resolve("TestProject2").resolve("src").resolve(feeModuleName));
+        copyFolder(caseResources.resolve("TestProject2").resolve("src").resolve("fee"), testProjFeeModulePath);
+        deleteFiles(caseResources.resolve("TestProject2").resolve("src").resolve("fee"));
+    
+        String feeModuleBaloFileName = feeModuleName + "-" + ProgramFileConstants.IMPLEMENTATION_VERSION + "-any-2.0.0"
+                                       + BLANG_COMPILED_PKG_BINARY_EXT;
+    
+        String feeBaloFilePath = "target" + File.separator + "balo" + File.separator + feeModuleBaloFileName;
+        given().with().pollInterval(Duration.TEN_SECONDS).and()
+                .with().pollDelay(Duration.FIVE_SECONDS)
+                .await().atMost(120, SECONDS).until(() -> {
+            balClient.runMain("build", new String[]{"-c"}, envVariables, new String[]{},
+                    new LogLeecher[]{}, caseResources.resolve("TestProject2").toString());
+            return Files.exists(caseResources.resolve("TestProject2").resolve(Paths.get(feeBaloFilePath)));
+        });
+    
+        // Push fee module of TestProject2
+        String feePushMsg = orgName + "/" + feeModuleName + ":2.0.0 [project repo -> central]";
+        LogLeecher feePushLeecher = new LogLeecher(feePushMsg);
+        balClient.runMain("push", new String[]{}, envVariables, new String[]{},
+                new LogLeecher[]{feePushLeecher}, caseResources.resolve("TestProject2").toString());
+        feePushLeecher.waitForText(5000);
+
+        // Build jee module of TestProject3
+        //// replace import
+        Path jeeBalPath = caseResources.resolve("TestProject3").resolve("src").resolve("jee").resolve("impl.bal");
+        lines = Files.lines(jeeBalPath);
+        replaced = lines.map(line -> line.replaceAll("fee", feeModuleName))
+                .collect(Collectors.toList());
+        Files.write(jeeBalPath, replaced);
+    
+        String jeeModuleBaloFileName = "jee" + EXEC_SUFFIX + BLANG_COMPILED_JAR_EXT;
+    
+        String jeeExecutableFilePath = "target" + File.separator + "bin" + File.separator + jeeModuleBaloFileName;
+        given().with().pollInterval(Duration.TEN_SECONDS).and()
+                .with().pollDelay(Duration.FIVE_SECONDS)
+                .await().atMost(120, SECONDS).until(() -> {
+            balClient.runMain("build", new String[]{}, envVariables, new String[]{},
+                    new LogLeecher[]{}, caseResources.resolve("TestProject3").toString());
+            return Files.exists(caseResources.resolve("TestProject3").resolve(Paths.get(jeeExecutableFilePath)));
+        });
+    
+        // Run and see output
+        LogLeecher beeRunLeecher = new LogLeecher("bzzz");
+        LogLeecher feeRunLeecher = new LogLeecher("feeeee");
+        balClient.runMain("run", new String[] {jeeExecutableFilePath}, envVariables, new String[0],
+                new LogLeecher[]{beeRunLeecher, feeRunLeecher}, caseResources.resolve("TestProject3").toString());
+        beeRunLeecher.waitForText(10000);
+        feeRunLeecher.waitForText(10000);
+    
+        // Update and build bee module of TestProject1
+        //// replace code
+        Path beeBalPath = caseResources.resolve("TestProject1").resolve("src").resolve(beeModuleName)
+                .resolve("say.bal");
+        lines = Files.lines(beeBalPath);
+        replaced = lines.map(line -> line.replaceAll("bzzz", "buzzz")).collect(Collectors.toList());
+        Files.write(beeBalPath, replaced);
+    
+        beeModuleBuildLeecher = new LogLeecher(module1BuildMsg);
+        balClient.runMain("build", new String[]{"-c"}, envVariables, new String[]{},
+                new LogLeecher[]{beeModuleBuildLeecher}, caseResources.resolve("TestProject1").toString());
+        beeModuleBuildLeecher.waitForText(5000);
+    
+        // Build and run TestProject3 with path set to bee balo in TestProject1
+        //// replace Ballerina.toml
+        Path ballerinaToml = caseResources.resolve("TestProject3").resolve("Ballerina.toml");
+        lines = Files.lines(ballerinaToml);
+        replaced = lines.map(line -> line.replaceAll("#", ""))
+                .map(line -> line.replaceAll("bee", beeModuleName))
+                .collect(Collectors.toList());
+        Files.write(ballerinaToml, replaced);
+        
+        given().with().pollInterval(Duration.TEN_SECONDS).and()
+                .with().pollDelay(Duration.FIVE_SECONDS)
+                .await().atMost(120, SECONDS).until(() -> {
+            balClient.runMain("build", new String[]{}, envVariables, new String[]{},
+                    new LogLeecher[]{}, caseResources.resolve("TestProject3").toString());
+            return Files.exists(caseResources.resolve("TestProject3").resolve(Paths.get(jeeExecutableFilePath)));
+        });
+    
+        // Run and see output
+        beeRunLeecher = new LogLeecher("buzzz");
+        feeRunLeecher = new LogLeecher("feeeee");
+        balClient.runMain("run", new String[] {jeeExecutableFilePath}, envVariables, new String[0],
+                new LogLeecher[]{beeRunLeecher, feeRunLeecher}, caseResources.resolve("TestProject3").toString());
+        beeRunLeecher.waitForText(10000);
+        feeRunLeecher.waitForText(10000);
     }
     
     /**
