e0157ae36fc1354518ada4969df34ce22b758559
rukshani <rukz.atha@gmail.com>
2019-09-17 13:58:33 +0530

    Address review suggestions

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/http/sample/SerializeComplexXmlTestCase.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/http/sample/SerializeComplexXmlTestCase.java
@@ -19,6 +19,7 @@
 package org.ballerinalang.test.service.http.sample;
 
 import io.netty.handler.codec.http.HttpHeaderNames;
+import org.apache.commons.text.StringEscapeUtils;
 import org.ballerinalang.test.service.http.HttpBaseTest;
 import org.ballerinalang.test.util.HttpClientRequest;
 import org.ballerinalang.test.util.HttpResponse;
@@ -26,7 +27,10 @@ import org.ballerinalang.test.util.TestConstant;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 
 import static org.ballerinalang.jvm.XMLFactory.isEqual;
 import static org.ballerinalang.jvm.XMLFactory.parse;
@@ -36,56 +40,6 @@ import static org.ballerinalang.jvm.XMLFactory.parse;
  */
 @Test(groups = "http-test")
 public class SerializeComplexXmlTestCase extends HttpBaseTest {
-    private static final String XML_DATA = "<?xml version=\"1.0\"?>\n" +
-            "<?xml-stylesheet href=\"catalog.xsl\" type=\"text/xsl\"?>\n" +
-            "<!--<!DOCTYPE catalog SYSTEM \"catalog.dtd\">-->\n" +
-            "<!-- Single line comment.-->\n" +
-            "<catalog>\n" +
-            "    <product description=\"Cardigan Sweater\" product_image=\"cardigan.jpg\">\n" +
-            "        <catalog_item gender=\"Men's\">\n" +
-            "            <item_number>QWZ5671</item_number>\n" +
-            "            <price>39.95</price>\n" +
-            "            <size description=\"Medium\">\n" +
-            "                <color_swatch image=\"red_cardigan.jpg\">Red</color_swatch>\n" +
-            "                <color_swatch image=\"burgundy_cardigan.jpg\">Burgundy</color_swatch>\n" +
-            "            </size>\n" +
-            "\n" +
-            "            <size description=\"Large\">\n" +
-            "                <color_swatch image=\"red_cardigan.jpg\">Red</color_swatch>\n" +
-            "                <color_swatch image=\"burgundy_cardigan.jpg\">Burgundy</color_swatch>\n" +
-            "            </size>\n" +
-            "        </catalog_item>\n" +
-            "        <catalog_item gender=\"Women's\">\n" +
-            "            <item_number>RRX9856</item_number>\n" +
-            "            <price>42.50</price>\n" +
-            "            <!-- Multi line comment.\n" +
-            "               For each property, a random number is created.\n" +
-            "               This number is made of two parts that each\n" +
-            "               ranges from 100 to 999.\n" +
-            "               Both parts are separated by a dash.\n" +
-            "           -->\n" +
-            "            <size description=\"Small\">\n" +
-            "                <color_swatch image=\"red_cardigan.jpg\">Red</color_swatch>\n" +
-            "                <color_swatch image=\"navy_cardigan.jpg\">Navy</color_swatch>\n" +
-            "                <color_swatch image=\"burgundy_cardigan.jpg\">Burgundy</color_swatch>\n" +
-            "            </size>\n" +
-            "            <size description=\"Medium\">\n" +
-            "                <color_swatch image=\"red_cardigan.jpg\">Red</color_swatch>\n" +
-            "                <color_swatch image=\"navy_cardigan.jpg\">Navy</color_swatch>\n" +
-            "                <color_swatch image=\"burgundy_cardigan.jpg\">Burgundy</color_swatch>\n" +
-            "                <color_swatch image=\"black_cardigan.jpg\">Black</color_swatch>\n" +
-            "            </size>\n" +
-            "            <size description=\"Large\">\n" +
-            "                <color_swatch image=\"navy_cardigan.jpg\">Navy</color_swatch>\n" +
-            "                <color_swatch image=\"black_cardigan.jpg\">Black</color_swatch>\n" +
-            "            </size>\n" +
-            "            <size description=\"Extra Large\">\n" +
-            "                <color_swatch image=\"burgundy_cardigan.jpg\">Burgundy</color_swatch>\n" +
-            "                <color_swatch image=\"black_cardigan.jpg\">Black</color_swatch>\n" +
-            "            </size>\n" +
-            "        </catalog_item>\n" +
-            "    </product>\n" +
-            "</catalog>\n";
 
     @Test
     public void testXmlSerialization() throws IOException {
@@ -95,6 +49,13 @@ public class SerializeComplexXmlTestCase extends HttpBaseTest {
         Assert.assertEquals(response.getResponseCode(), 200, "Response code mismatched");
         Assert.assertEquals(response.getHeaders().get(HttpHeaderNames.CONTENT_TYPE.toString()),
                             TestConstant.CONTENT_TYPE_XML, "Content-Type mismatched");
-        Assert.assertTrue(isEqual(parse(response.getData()), parse(XML_DATA)), "Message content mismatched");
+        Assert.assertTrue(isEqual(parse(response.getData()), parse(getInputStream()).elements()),
+                          "Message content mismatched");
+    }
+
+    public InputStream getInputStream() throws IOException {
+        String filePath = StringEscapeUtils.escapeJava(
+                Paths.get("src", "test", "resources", "http", "ComplexTestXmlSample.xml").toAbsolutePath().toString());
+        return new FileInputStream(filePath);
     }
 }
