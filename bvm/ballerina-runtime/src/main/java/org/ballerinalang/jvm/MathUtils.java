64021ddd554488516ce48c9e67f44bfeab8fff0e
Vinod Kavinda <vinod@wso2.com>
2019-10-09 16:38:50 +0530

    Add overflow error after devision

--- bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/MathUtils.java
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/MathUtils.java
@@ -28,6 +28,11 @@ public class MathUtils {
 
     public static long divide(long numerator, long denominator) {
         try {
+            if (numerator == Math.pow(-2, 63) && denominator == -1) {
+                // a panic will occur on division by zero or overflow,
+                // which happens if the first operand is -2^63 and the second operand is -1
+                throw BallerinaErrors.createError(BallerinaErrorReasons.Number_OVERFLOW, " int range overflow");
+            }
             return numerator / denominator;
         } catch (ArithmeticException e) {
             if (denominator == 0) {
