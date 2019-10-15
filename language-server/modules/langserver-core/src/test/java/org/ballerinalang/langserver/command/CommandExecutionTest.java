381ca4422ecedcc2ee8076e4792d6230f13a906e
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2019-10-15 09:03:46 +0530

    Fix checkstyle issues

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/command/CommandExecutionTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/command/CommandExecutionTest.java
@@ -46,7 +46,6 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.wso2.ballerinalang.compiler.semantics.model.types.BServiceType;
