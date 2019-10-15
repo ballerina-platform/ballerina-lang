cbd8988f690ddc27ddad7c4ee56b0136e3c91ab4
Grainier <grainier@wso2.com>
2019-10-15 10:43:10 +0530

    Remove redundant variable

--- stdlib/log-api/src/main/java/org/ballerinalang/stdlib/log/AbstractLogFunction.java
+++ stdlib/log-api/src/main/java/org/ballerinalang/stdlib/log/AbstractLogFunction.java
@@ -77,8 +77,7 @@ public abstract class AbstractLogFunction {
                 return msg;
             }
         };
-        boolean logEnabled = LOG_MANAGER.getPackageLogLevel(pckg).value() <= logLevel.value();
-        if (logEnabled) {
+        if (LOG_MANAGER.getPackageLogLevel(pckg).value() <= logLevel.value()) {
             consumer.accept(pckg, logMessage.get());
             ObserveUtils.logMessageToActiveSpan(strand, logLevel.name(), logMessage, logLevel == BLogLevel.ERROR);
         }
