5435bb221f25fc9c0cc9799e5a5c03b2fd5df6ee
Dhananjaya Wickramasingha <dhananjayaw@wso2.com>
2019-10-22 15:25:38 +0530

    Remove unused code

--- bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/scheduling/Scheduler.java
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/scheduling/Scheduler.java
@@ -73,7 +73,6 @@ public class Scheduler {
     private static int poolSize = Runtime.getRuntime().availableProcessors() * 2;
 
     private Semaphore mainBlockSem;
-    private static LogicalProcessorCount logicalProcessorCount = new LogicalProcessorCount();
 
     public Scheduler(boolean immortal) {
         try {
@@ -370,17 +369,6 @@ public class Scheduler {
     public void poison() {
         for (int i = 0; i < numThreads; i++) {
             runnableList.add(POISON_PILL);
-        }
-    }
-
-    private static class LogicalProcessorCount {
-        private int threadCount;
-        LogicalProcessorCount() {
-            threadCount = Runtime.getRuntime().availableProcessors() * 2;
-        }
-
-        int getAllowedThreadCount() {
-            return threadCount;
         }
     }
 }
