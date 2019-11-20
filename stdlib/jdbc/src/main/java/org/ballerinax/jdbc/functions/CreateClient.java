b46360b1727f7fa0d0d9ab4e7a0c35460d8b5118
Anupama Pathirage <anupama@Anupamas-MacBook-Pro.local>
2019-11-20 12:49:31 +0530

    Improve JDBC Client implementation

--- stdlib/jdbc/src/main/java/org/ballerinax/jdbc/functions/CreateClient.java
+++ stdlib/jdbc/src/main/java/org/ballerinax/jdbc/functions/CreateClient.java
@@ -37,15 +37,14 @@ import java.util.UUID;
  * @since 0.970
  */
 
-@BallerinaFunction(
-        orgName = "ballerinax", packageName = "java.jdbc",
-        functionName = "createClient",
-        isPublic = true
-)
+@BallerinaFunction(orgName = "ballerinax",
+                   packageName = "java.jdbc",
+                   functionName = "createClient",
+                   isPublic = true)
 public class CreateClient {
 
-    public static ObjectValue createClient(Strand strand, MapValue<String, Object> config, MapValue<String,
-                Object> globalPoolOptions) {
+    public static void createClient(Strand strand, ObjectValue client, MapValue<String, Object> config,
+            MapValue<String, Object> globalPoolOptions) {
         String url = config.getStringValue(Constants.EndpointConfig.URL);
 
         if (!isJdbcUrlValid(url)) {
@@ -72,10 +71,8 @@ public class CreateClient {
 
         SQLDatasource sqlDatasource = sqlDatasourceParams.getPoolOptionsWrapper()
                 .retrieveDatasource(sqlDatasourceParams);
-        ObjectValue sqlClient = BallerinaValues.createObjectValue(Constants.JDBC_PACKAGE_ID, Constants.JDBC_CLIENT);
-        sqlClient.addNativeData(Constants.JDBC_CLIENT, sqlDatasource);
-        sqlClient.addNativeData(Constants.CONNECTOR_ID_KEY, UUID.randomUUID().toString());
-        return sqlClient;
+        client.addNativeData(Constants.JDBC_CLIENT, sqlDatasource);
+        client.addNativeData(Constants.CONNECTOR_ID_KEY, UUID.randomUUID().toString());
     }
 
     // Unable to perform a complete validation since URL differs based on the database.
