aaec0b0dc74dbc381ae10d58d752d9fec85b190d
Supun Setunga <supun.setunga@gmail.com>
2019-11-22 10:58:11 +0530

    Revert sync string value changes to backend jmv old

--- stdlib/jvm-old/src/main/java/org/ballerinalang/nativeimpl/jvm/interop/JMethodResolver.java
+++ stdlib/jvm-old/src/main/java/org/ballerinalang/nativeimpl/jvm/interop/JMethodResolver.java
@@ -28,7 +28,6 @@ import org.ballerinalang.jvm.values.ErrorValue;
 import org.ballerinalang.jvm.values.MapValue;
 import org.ballerinalang.jvm.values.ObjectValue;
 import org.ballerinalang.jvm.values.StreamValue;
-import org.ballerinalang.jvm.values.StringValue;
 import org.ballerinalang.jvm.values.TableValue;
 import org.ballerinalang.jvm.values.XMLValue;
 
@@ -259,7 +258,8 @@ class JMethodResolver {
                 case TypeTags.DECIMAL_TAG:
                     return this.classLoader.loadClass(BigDecimal.class.getCanonicalName()).isAssignableFrom(jType);
                 case TypeTags.STRING_TAG:
-                    return this.classLoader.loadClass(StringValue.class.getCanonicalName()).isAssignableFrom(jType);
+                    // Currently no java type matches to ballerina string type.
+                    return false;
                 case TypeTags.MAP_TAG:
                 case TypeTags.RECORD_TYPE_TAG:
                     return this.classLoader.loadClass(MapValue.class.getCanonicalName()).isAssignableFrom(jType);
