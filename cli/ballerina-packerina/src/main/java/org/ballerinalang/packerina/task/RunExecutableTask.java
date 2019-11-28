695aaf2e286e045f4af16a8c6d04467e43854d7d
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2019-11-28 10:56:45 +0530

    Fix debugger regression caused by packerina changes

--- cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/task/RunExecutableTask.java
+++ cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/task/RunExecutableTask.java
@@ -44,6 +44,7 @@ import java.util.jar.Attributes;
 import java.util.jar.JarInputStream;
 import java.util.jar.Manifest;
 
+import static org.ballerinalang.jvm.runtime.RuntimeConstants.SYSTEM_PROP_BAL_DEBUG;
 import static org.ballerinalang.jvm.util.BLangConstants.MODULE_INIT_CLASS_NAME;
 import static org.ballerinalang.tool.LauncherUtils.createLauncherException;
 import static org.wso2.ballerinalang.compiler.util.ProjectDirConstants.BLANG_COMPILED_JAR_EXT;
@@ -54,21 +55,25 @@ import static org.wso2.ballerinalang.compiler.util.ProjectDirConstants.MAIN_CLAS
  * Task for running the executable.
  */
 public class RunExecutableTask implements Task {
-    
+
     private final String[] args;
     private Path executablePath;
     private boolean isGeneratedExecutable = false;
-    
+    private boolean isInDebugMode = false;
+    private static final String DEBUG_ARGS_JAVA8 = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=";
+
     /**
      * Create a task to run the executable. This requires {@link CreateExecutableTask} to be completed.
      *
-     * @param args           Arguments for the executable.
+     * @param args          Arguments for the executable.
+     * @param isInDebugMode Flag to notify whether the executable jar should be run in the debug mode.
      */
-    public RunExecutableTask(String[] args) {
+    public RunExecutableTask(String[] args, boolean isInDebugMode) {
         this(null, args);
         this.isGeneratedExecutable = true;
+        this.isInDebugMode = isInDebugMode;
     }
-    
+
     /**
      * Create a task to run an executable from a given path.
      *
@@ -79,7 +84,7 @@ public class RunExecutableTask implements Task {
         this.executablePath = executablePath;
         this.args = args;
     }
-    
+
     @Override
     public void execute(BuildContext buildContext) {
         Path sourceRootPath = buildContext.get(BuildContextField.SOURCE_ROOT);
@@ -98,50 +103,50 @@ public class RunExecutableTask implements Task {
                     break;
                 }
             }
-            
+
             // check if executable found with entry points.
             if (null == this.executablePath) {
                 switch (buildContext.getSourceType()) {
                     case SINGLE_BAL_FILE:
                         SingleFileContext singleFileContext = buildContext.get(BuildContextField.SOURCE_CONTEXT);
                         throw createLauncherException("no entry points found in '" + singleFileContext.getBalFile() +
-                                                      "'.");
+                                "'.");
                     case SINGLE_MODULE:
                         SingleModuleContext singleModuleContext = buildContext.get(BuildContextField.SOURCE_CONTEXT);
                         throw createLauncherException("no entry points found in '" +
-                                                         singleModuleContext.getModuleName() + "'.");
+                                singleModuleContext.getModuleName() + "'.");
                     default:
                         throw createLauncherException("unknown source type found when running executable.");
                 }
             }
         }
-        
+
         if (!this.executablePath.isAbsolute()) {
             this.executablePath = sourceRootPath.resolve(executablePath);
         }
-        
+
         // clean up the path and get absolute path
         this.executablePath = this.executablePath.toAbsolutePath().normalize();
-        
+
         // if the executable does not exist
         if (Files.notExists(this.executablePath)) {
             throw createLauncherException("cannot run '" + this.executablePath.toAbsolutePath().toString() +
-                                             "' as it does not exist.");
+                    "' as it does not exist.");
         }
-    
+
         // if the executable is not a file and not an extension with .jar
         if (!(Files.isRegularFile(this.executablePath) &&
-            this.executablePath.toString().endsWith(BLANG_COMPILED_JAR_EXT))) {
-    
+                this.executablePath.toString().endsWith(BLANG_COMPILED_JAR_EXT))) {
+
             throw createLauncherException("cannot run '" + this.executablePath.toAbsolutePath().toString() +
-                                             "' as it is not an executable with .jar extension.");
+                    "' as it is not an executable with .jar extension.");
         }
-    
+
         // set the source root path relative to the source path i.e. set the parent directory of the source path
-        System.setProperty(ProjectDirConstants.BALLERINA_SOURCE_ROOT, sourceRootPath.toString());;
+        System.setProperty(ProjectDirConstants.BALLERINA_SOURCE_ROOT, sourceRootPath.toString());
         this.runGeneratedExecutable(executableModule, buildContext);
     }
-    
+
     /**
      * Run an executable that is generated from 'run' command.
      *
@@ -151,13 +156,16 @@ public class RunExecutableTask implements Task {
 
         ExecutableJar executableJar = buildContext.moduleDependencyPathMap.get(executableModule.packageID);
         String initClassName = BFileUtil.getQualifiedClassName(executableModule.packageID.orgName.value,
-                                                               executableModule.packageID.name.value,
-                                                               MODULE_INIT_CLASS_NAME);
+                executableModule.packageID.name.value, MODULE_INIT_CLASS_NAME);
         try {
             List<String> commands = new ArrayList<>();
             commands.add("java");
             commands.add("-cp");
             commands.add(getClassPath(executableJar));
+            if (isInDebugMode) {
+                commands.add(String.format("%s,address=%s", DEBUG_ARGS_JAVA8,
+                        System.getProperty(SYSTEM_PROP_BAL_DEBUG)));
+            }
             commands.add(initClassName);
             commands.addAll(Lists.of(args));
 
@@ -170,16 +178,16 @@ public class RunExecutableTask implements Task {
             throw createLauncherException("Error occurred while running the executable ", e.getCause());
         }
     }
-    
+
     /**
      * Run a given executable .jar file.
      */
     private void runExecutable() {
         String initClassName = null;
         try {
-            URLClassLoader classLoader = new URLClassLoader(new URL[] { this.executablePath.toUri().toURL() },
+            URLClassLoader classLoader = new URLClassLoader(new URL[]{this.executablePath.toUri().toURL()},
                     ClassLoader.getSystemClassLoader());
-            
+
             initClassName = getModuleInitClassName(this.executablePath);
             Class<?> initClazz = classLoader.loadClass(initClassName);
             Method mainMethod = initClazz.getDeclaredMethod(JAVA_MAIN, String[].class);
@@ -199,7 +207,7 @@ public class RunExecutableTask implements Task {
             throw createLauncherException("invoking main method failed due to ", e.getCause());
         }
     }
-    
+
     /**
      * Get the clazz name with the main method. The name of the clazz is found through the Manifest file in the .jar.
      *
