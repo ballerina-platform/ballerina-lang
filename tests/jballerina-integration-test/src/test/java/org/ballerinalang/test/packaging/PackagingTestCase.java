e38598ba0a8c82473f39dff9dc008272069d81d5
Suganya <suganyasuven@gmail.com>
2019-12-09 12:19:36 +0530

    handle the fail case for testPullCount test

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/packaging/PackagingTestCase.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/packaging/PackagingTestCase.java
@@ -175,8 +175,12 @@ public class PackagingTestCase extends BaseTest {
                 if (payload instanceof MapValue) {
                     long pullCount = ((MapValue) payload).getIntValue("totalPullCount");
                     Assert.assertEquals(pullCount, 1);
+                } else {
+                    Assert.fail("error: invalid response received");
                 }
             }
+        } else {
+            Assert.fail("error: could not connect to remote repository to find the latest version of module");
         }
     }
 
