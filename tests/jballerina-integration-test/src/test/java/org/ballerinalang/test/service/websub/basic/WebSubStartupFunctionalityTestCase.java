8bfaf3f4c9af4438671be7e9b6c0fa1c1d66a68d
chamil321 <chamilmadawa8@gmail.com>
2019-12-18 15:17:02 +0530

    Refactor test flow

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/websub/basic/WebSubStartupFunctionalityTestCase.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/websub/basic/WebSubStartupFunctionalityTestCase.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *  WSO2 Inc. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -15,6 +15,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
+
 package org.ballerinalang.test.service.websub.basic;
 
 import org.ballerinalang.test.BaseTest;
