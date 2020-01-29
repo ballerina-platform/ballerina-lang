d06049afc9cddc18776f8daed5dc24e384750ac8
gimantha <gimantha@wso2.com>
2020-01-29 14:48:53 +0530

    Add Streamvalue function filter and toStream functions

--- langlib/lang.stream/src/main/ballerina/src/lang.stream/stream.bal
+++ langlib/lang.stream/src/main/ballerina/src/lang.stream/stream.bal
@@ -13,3 +13,23 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+//TODO: stream<Type, E>, supporting E is not implemented yet.
+# A type parameter that is a subtype of `anydata|error`.
+# Has the special semantic that when used in a declaration
+# all uses in the declaration must refer to same type.
+@typeParam
+type PureType anydata | error;
+
+# Selects the members from an array for which a function returns true.
+#
+# + strm - The stream
+# + func - a predicate to apply to each member to test whether it should be selected
+# + return - new stream only containing members of `strm` for which `func` evaluates to true
+public function filter(stream<PureType> strm, function(PureType val) returns boolean func) returns stream<PureType> = external;
+
+# Returns the next element in the stream wrapped in a record or () if the stream ends.
+#
+# + strm - The stream
+# + return - If the stream has elements, return the element wrapped in a record with single field called `value`,
+#            otherwise returns ()
+public function next(stream<PureType> strm) returns record {PureType value;}? = external;
---
+++ langlib/lang.stream/src/main/java/org/ballerinalang/langlib/stream/Filter.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.langlib.stream;
+
+import org.ballerinalang.jvm.scheduling.Strand;
+import org.ballerinalang.jvm.values.FPValue;
+import org.ballerinalang.jvm.values.StreamValue;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaFunction;
+import org.ballerinalang.natives.annotations.ReturnType;
+
+/**
+ * Native implementation of lang.stream:filter(stream<Type>, function).
+ *
+ * @since 1.2
+ */
+@BallerinaFunction(
+        orgName = "ballerina", packageName = "lang.stream", functionName = "filter",
+        args = {@Argument(name = "strm", type = TypeKind.STREAM), @Argument(name = "func", type = TypeKind.FUNCTION)},
+        returnType = {@ReturnType(type = TypeKind.STREAM)},
+        isPublic = true
+)
+public class Filter {
+
+    public static StreamValue filter(Strand strand, StreamValue strm, FPValue<Object, Boolean> func) {
+        return (StreamValue) strm.filter(strm, func);
+    }
+}
