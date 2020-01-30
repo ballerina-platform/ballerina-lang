27c9ee4c84063201013121701d87ecec9fc7bb17
gimantha <gimantha@wso2.com>
2020-01-30 18:40:01 +0530

    Fix laziness

---
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/values/BStreamIteratorImpl.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.ballerinalang.jvm.values;
+
+import org.ballerinalang.jvm.scheduling.Strand;
+import org.ballerinalang.jvm.values.api.BStreamIterator;
+
+/**
+ * <p>
+ * The {@link BStreamIteratorImpl} represents the stream iterator class for streams in Ballerina.
+ * </p>
+ * <p>
+ * <i>Note: This is an internal API and may change in future versions.</i>
+ * </p>
+ *
+ * @since 1.2
+ */
+public class BStreamIteratorImpl implements BStreamIterator {
+
+    private IteratorValue iterator;
+    public BStreamIteratorImpl(IteratorValue iterator) {
+        this.iterator = iterator;
+    }
+    @Override
+    public Object next(Strand strand) {
+        if (iterator.hasNext()) {
+            return iterator.next();
+        }
+
+        return null;
+    }
+}
--- langlib/lang.array/src/main/java/org/ballerinalang/langlib/array/ToStream.java
+++ langlib/lang.array/src/main/java/org/ballerinalang/langlib/array/ToStream.java
@@ -20,8 +20,7 @@ package org.ballerinalang.langlib.array;
 
 import org.ballerinalang.jvm.scheduling.Strand;
 import org.ballerinalang.jvm.types.BStreamType;
-import org.ballerinalang.jvm.values.ArrayValue;
-import org.ballerinalang.jvm.values.StreamValue;
+import org.ballerinalang.jvm.values.*;
 import org.ballerinalang.model.types.TypeKind;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaFunction;
@@ -41,6 +40,7 @@ import org.ballerinalang.natives.annotations.ReturnType;
 public class ToStream {
 
     public static StreamValue toStream(Strand strand, ArrayValue arr) {
-        return new StreamValue(new BStreamType(arr.getElementType()), arr.getIterator(), null, null);
+        return new StreamValue(new BStreamType(arr.getElementType()), new BStreamIteratorImpl(arr.getIterator()),
+                null, null);
     }
 }
