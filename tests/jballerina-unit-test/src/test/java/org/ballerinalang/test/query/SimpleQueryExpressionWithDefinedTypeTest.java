7b32d7cea7811c46e90d6257e60a66097199e1f2
Mohan <mohan.vive@gmail.com>
2020-02-05 14:05:08 +0530

    Fix for the checkstyle issues

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/query/SimpleQueryExpressionWithDefinedTypeTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/query/SimpleQueryExpressionWithDefinedTypeTest.java
@@ -153,17 +153,16 @@ public class SimpleQueryExpressionWithDefinedTypeTest {
         BValue[] returnValues = BRunUtil.invoke(result, "testMapWithArity");
         Assert.assertNotNull(returnValues);
         Assert.assertEquals(returnValues.length, 1);
-        Assert.assertEquals(returnValues[0].stringValue(),"1A");
+        Assert.assertEquals(returnValues[0].stringValue(), "1A");
     }
-
 
     @Test(description = "Test selecting values in a JSON array from query expression")
     public void testJSONArrayWithArity() {
         BValue[] returnValues = BRunUtil.invoke(result, "testJSONArrayWithArity");
         Assert.assertNotNull(returnValues);
         Assert.assertEquals(returnValues.length, 2);
-        Assert.assertEquals(returnValues[0].stringValue(),"bob");
-        Assert.assertEquals(returnValues[1].stringValue(),"tom");
+        Assert.assertEquals(returnValues[0].stringValue(), "bob");
+        Assert.assertEquals(returnValues[1].stringValue(), "tom");
     }
 
     @Test(description = "Test filtering values in a tuple from query expression")
@@ -171,6 +170,6 @@ public class SimpleQueryExpressionWithDefinedTypeTest {
         BValue[] returnValues = BRunUtil.invoke(result, "testArrayWithTuple");
         Assert.assertNotNull(returnValues);
         Assert.assertEquals(returnValues.length, 1);
-        Assert.assertEquals(returnValues[0].stringValue(),"[\"C\"]");
+        Assert.assertEquals(returnValues[0].stringValue(), "[\"C\"]");
     }
 }
