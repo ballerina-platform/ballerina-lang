f9dee3f3ea284f0ce02c6e83d722b20c273e3f9b
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2020-02-07 12:02:19 +0530

    fix object init completion suggestion

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/spi/LSCompletionProvider.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/spi/LSCompletionProvider.java
@@ -499,7 +499,8 @@ public abstract class LSCompletionProvider {
         
         BObjectTypeSymbol objectTypeSymbol = (BObjectTypeSymbol) bSymbol;
         BAttachedFunction initFunction = objectTypeSymbol.initializerFunc;
-        
+        BInvokableSymbol initFunctionSymbol = initFunction == null ? null : initFunction.symbol;
+
         if (newTokenIndex > -1 && firstColonIndex > -1 && lastColonIndex == firstColonIndex) {
             /*
             ex: modName:ObjectName = new <cursor>
@@ -511,9 +512,9 @@ public abstract class LSCompletionProvider {
             ex: modName:ObjectName = new modName:
                 ObjectName = new 
              */
-            Pair<String, String> newWithTypeSign = getFunctionInvocationSignature(initFunction.symbol, typeName,
+            Pair<String, String> newWithTypeSign = getFunctionInvocationSignature(initFunctionSymbol, typeName,
                     context);
-            completionItems.add(BFunctionCompletionItemBuilder.build(initFunction.symbol, newWithTypeSign.getRight(),
+            completionItems.add(BFunctionCompletionItemBuilder.build(initFunctionSymbol, newWithTypeSign.getRight(),
                     newWithTypeSign.getLeft(), context));
             return completionItems;
         } else if (invocationType > -1) {
