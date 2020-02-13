829f9a73ba3e089d2a8f298ca524f2890184a070
Pubudu Fernando <pubuduf@wso2.com>
2020-02-13 15:36:18 +0530

    Add test cases

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/functions/ExprBodiedFunctionTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/functions/ExprBodiedFunctionTest.java
@@ -20,8 +20,19 @@ package org.ballerinalang.test.functions;
 import org.ballerinalang.test.util.BCompileUtil;
 import org.ballerinalang.test.util.BRunUtil;
 import org.ballerinalang.test.util.CompileResult;
+import org.ballerinalang.util.diagnostic.Diagnostic;
+import org.ballerinalang.util.exceptions.BLangRuntimeException;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.ballerinalang.test.util.BAssertUtil.validateError;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Test cases for expression bodied functions.
@@ -38,17 +49,61 @@ public class ExprBodiedFunctionTest {
     }
 
     @Test
-    public void testRecordAsAnExpr() {
-        BRunUtil.invoke(compileResult, "testRecordAsAnExpr");
+    public void testSyntaxErrors() {
+        CompileResult result = BCompileUtil.compile("test-src/functions/expr_bodied_functions_negative.bal");
+        Set<Integer> errorLines = new HashSet<>(Arrays.asList(19, 23, 26));
+        for (Diagnostic diagnostic : result.getDiagnostics()) {
+            if (!errorLines.contains(diagnostic.getPosition().getStartLine())) {
+                Assert.fail("Unexpected error at line: " + diagnostic.getPosition().getStartLine());
+            }
+        }
     }
 
     @Test
-    public void testBinaryExprs() {
-        BRunUtil.invoke(compileResult, "testBinaryExprs");
+    public void testTaintChecking() {
+        CompileResult result = BCompileUtil.compile("test-src/functions/expr_bodied_functions_taint.bal");
+        int index = 0;
+        validateError(result, index++, "tainted value passed to untainted parameter 'param'", 25, 32);
+        validateError(result, index++, "tainted value passed to untainted parameter 'param'", 32, 32);
+        validateError(result, index++, "tainted value passed to untainted parameter 'param'", 41, 32);
+        assertEquals(result.getErrorCount(), index);
+    }
+
+    @Test(dataProvider = "FunctionList")
+    public void testExprBodiedFunctions(String funcName) {
+        BRunUtil.invoke(compileResult, funcName);
     }
 
     @Test
     public void testClosures() {
         BRunUtil.invoke(compileResult, "testClosures", new Object[]{10});
+    }
+
+    @Test(expectedExceptions = BLangRuntimeException.class,
+          expectedExceptionsMessageRegExp = ".*NumberParsingError message='string' value " +
+                  "'invalid' cannot be converted to 'int'.*")
+    public void testCheckPanic() {
+        BRunUtil.invoke(compileResult, "testCheckPanic");
+    }
+
+    @DataProvider(name = "FunctionList")
+    public Object[][] getTestFunctions() {
+        return new Object[][]{
+                {"testReturningLiterals"},
+                {"testReturningLists"},
+                {"testBinaryExprs"},
+                {"testNilReturningFunctions"},
+                {"testRecordAsAnExpr"},
+                {"testSameVarRefAsExpr"},
+                {"testFunctionInvocation"},
+                {"testFunctionInvocationAsLambdas"},
+                {"testExprsBodiesInMethods"},
+                {"testObjectInitBodyAsAnExpr"},
+                {"testObjectsAsExprBody"},
+                {"testAnonFuncsAsExprBody"},
+                {"testReturningXML"},
+                {"testReturningStringTemplate"},
+                {"testReturningServiceConstructors"},
+        };
     }
 }
