ba6e356bc9bcee6571199efe6acdbf94f2d9282f
Mohan <mohan.vive@gmail.com>
2020-02-18 16:13:56 +0530

    Remove redundant type checks from query desugar

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/desugar/QueryDesugar.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/desugar/QueryDesugar.java
@@ -113,15 +113,9 @@ public class QueryDesugar extends BLangNodeVisitor {
         BLangForeach leafForeach = buildFromClauseBlock(fromClauseList);
         BLangBlockStmt foreachBody = ASTBuilderUtil.createBlockStmt(pos);
 
-        BType outputArrayType;
-        if (selectClause.expression != null && selectClause.expression.type != null) {
-            outputArrayType = new BArrayType(selectClause.expression.type);
-        } else {
-            outputArrayType = fromClause.varType;
-        }
-
+        BArrayType outputArrayType = new BArrayType(selectClause.expression.type);
         BLangListConstructorExpr emptyArrayExpr = ASTBuilderUtil.createEmptyArrayLiteral(pos,
-                (BArrayType) outputArrayType);
+                outputArrayType);
         BVarSymbol emptyArrayVarSymbol = new BVarSymbol(0, new Name("$outputDataArray$"),
                 env.scope.owner.pkgID, outputArrayType, env.scope.owner);
         BLangSimpleVariable outputArrayVariable =
@@ -140,10 +134,6 @@ public class QueryDesugar extends BLangNodeVisitor {
         //         firstName: person.firstName,
         //         lastName: person.lastName
         //      };
-
-        if (selectClause.expression.type == null) {
-            selectClause.expression.type = fromClause.varType;
-        }
 
         BLangInvocation lengthInvocation = createLengthInvocation(selectClause.pos, outputArrayVariable.symbol);
         lengthInvocation.expr = outputVarRef;
