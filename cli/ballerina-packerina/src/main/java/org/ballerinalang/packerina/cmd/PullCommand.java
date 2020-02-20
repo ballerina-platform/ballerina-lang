2ca55eaf4ea0f2fb22307f2522b60e7ac19287dd
Suganya <suganyasuven@gmail.com>
2020-02-20 12:40:39 +0530

    Remove the condition added to check if org name is reserved one

--- cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/cmd/PullCommand.java
+++ cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/cmd/PullCommand.java
@@ -100,10 +100,6 @@ public class PullCommand implements BLauncherCmd {
         String[] moduleInfo = resourceName.split("/");
         orgName = moduleInfo[0];
         packageName = moduleInfo[1];
-        if (orgName.equals("ballerina") || orgName.equals("ballerinax")) {
-            throw LauncherUtils.createLauncherException("`ballerina` and `ballerinax` are reserved organization" +
-                    " names used by ballerina");
-        }
 
         // Get the name of the module.
         String[] packageInfo = packageName.split(":");
