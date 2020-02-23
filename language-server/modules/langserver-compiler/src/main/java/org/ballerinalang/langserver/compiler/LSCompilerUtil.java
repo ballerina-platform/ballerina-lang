139ba5ce01ce598694e49fbf0370a697d120fafa
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2020-02-23 18:53:39 +0530

    fix language server compiler phases and visit const reference

--- language-server/modules/langserver-compiler/src/main/java/org/ballerinalang/langserver/compiler/LSCompilerUtil.java
+++ language-server/modules/langserver-compiler/src/main/java/org/ballerinalang/langserver/compiler/LSCompilerUtil.java
@@ -126,10 +126,7 @@ public class LSCompilerUtil {
         if (null == compilerPhase) {
             throw new AssertionError("Compiler Phase can not be null.");
         }
-        String phase = compilerPhase.toString().equals(CompilerPhase.COMPILER_PLUGIN.toString()) ? "annotationProcess"
-                : compilerPhase.toString();
-
-        options.put(COMPILER_PHASE, phase);
+        options.put(COMPILER_PHASE, compilerPhase.toString());
         options.put(PRESERVE_WHITESPACE, Boolean.TRUE.toString());
         options.put(TEST_ENABLED, String.valueOf(true));
         options.put(SKIP_TESTS, String.valueOf(false));
