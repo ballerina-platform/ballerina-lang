83ed4f43d56fae509cb82bfc51ea2125114a0c87
gimantha <gimantha@wso2.com>
2020-02-25 11:33:04 +0530

    Fix test failures

--- bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/values/CloneUtils.java
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/values/CloneUtils.java
@@ -47,6 +47,13 @@ public class CloneUtils {
         return refValue.copy(new HashMap<>());
     }
 
+    /**
+     * Returns a clone of `value` which is immutable. A clone is a deep copy that does not copy immutable subtrees.
+     * A clone can therefore safely be used concurrently with the original. It corresponds to the ImmutableClone(v)
+     * abstract operation, defined in the Ballerina Language Specification.
+     * @param value The value on which the function is invoked
+     * @return String value of the value
+     */
     public static Object cloneReadOnly(Object value) {
         if (value == null) {
             return null;
@@ -57,6 +64,6 @@ public class CloneUtils {
         }
 
         RefValue refValue = (RefValue) value;
-        return refValue.copy(new HashMap<>());
+        return refValue.frozenCopy(new HashMap<>());
     }
 }
