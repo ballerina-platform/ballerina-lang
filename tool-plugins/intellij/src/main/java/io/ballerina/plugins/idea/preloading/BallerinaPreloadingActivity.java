28b12bb642ac40a952547e3902506c770d0f55fa
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2020-02-26 18:27:01 +0530

    Fix licence headers

--- tool-plugins/intellij/src/main/java/io/ballerina/plugins/idea/preloading/BallerinaPreloadingActivity.java
+++ tool-plugins/intellij/src/main/java/io/ballerina/plugins/idea/preloading/BallerinaPreloadingActivity.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 package io.ballerina.plugins.idea.preloading;
 
 import com.intellij.openapi.application.ApplicationManager;
