780e15d38c8d0a4e3f3b31ee28a22584f771c0cd
Maninda Edirisooriya <mwe163@gmail.com>
2020-02-28 16:47:46 +0530

    Correct the grammar.
    Co-Authored-By: praneesha <praneesha@wso2.com>

--- stdlib/email/src/main/java/org/ballerinalang/stdlib/email/client/PopClient.java
+++ stdlib/email/src/main/java/org/ballerinalang/stdlib/email/client/PopClient.java
@@ -88,7 +88,7 @@ public class PopClient {
      * @param clientConnector Represents the POP Client class
      * @param filter Criteria, which is used to read emails
      * @param isPop True if the protocol is POP3 and false otherwise (if protocol is IMAP)
-     * @return MapValue Returns the type supported to Ballerina
+     * @return MapValue Returns the type supported by Ballerina
      * @throws BallerinaPopException If an error occurs in POP client
      */
     public static MapValue readMessage(ObjectValue clientConnector, MapValue<Object, Object> filter, boolean isPop)
