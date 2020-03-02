405b8072fddf3ad668d1d76e0d0a4275c511fc2b
Buddhi <b.wathsala.bw@gmail.com>
2020-03-02 14:07:50 +0530

    to lower case using default encoding of the platform

--- stdlib/grpc/src/main/java/org/ballerinalang/net/grpc/builder/components/Message.java
+++ stdlib/grpc/src/main/java/org/ballerinalang/net/grpc/builder/components/Message.java
@@ -22,6 +22,7 @@ import com.google.protobuf.DescriptorProtos;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import static org.ballerinalang.net.grpc.builder.utils.BalGenerationUtils.toCamelCase;
@@ -112,7 +113,7 @@ public class Message {
                         nestedMessage.setMessageName(
                                 nestedMessage.getMessageName().substring(
                                         0, nestedMessage.getMessageName().length() - 5
-                                ).toLowerCase()
+                                ).toLowerCase(Locale.getDefault())
                         );
                     }
                     mapList.add(nestedMessage);
