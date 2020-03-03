7a736b6079a0b5d67dd613fea16519608d3d522d
manuranga perera <manu@wso2.com>
2020-03-03 13:47:28 +0530

    Move bstring test to a separate module

--- misc/lib-creator/src/main/java/org/ballerinalang/stdlib/utils/GenerateBalo.java
+++ misc/lib-creator/src/main/java/org/ballerinalang/stdlib/utils/GenerateBalo.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing,
-*  software distributed under the License is distributed on an
-*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-*  KIND, either express or implied.  See the License for the
-*  specific language governing permissions and limitations
-*  under the License.
-*/
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.ballerinalang.stdlib.utils;
 
 import org.ballerinalang.compiler.BLangCompilerException;
@@ -92,7 +92,7 @@ public class GenerateBalo {
     }
 
     private static void genBalo(String targetDir, String sourceRootDir, boolean reportWarnings, boolean jvmTarget)
-            throws IOException {
+    throws IOException {
         Files.createDirectories(Paths.get(targetDir));
 
         CompilerContext context = new CompilerContext();
@@ -128,7 +128,12 @@ public class GenerateBalo {
         compiler.write(buildPackages);
 
         for (BLangPackage pkg : buildPackages) {
-            Path jarOutput = Paths.get("./build/generated-bir-jar/" + pkg.packageID.name + ".jar");
+            String suffix = "";
+            String bStringProp = System.getProperty("ballerina.bstring");
+            if (bStringProp != null && !"".equals(bStringProp)) {
+                suffix = "-bstring";
+            }
+            Path jarOutput = Paths.get("./build/generated-bir-jar/" + pkg.packageID.name + suffix + ".jar");
             Path parent = jarOutput.getParent();
             if (parent != null) {
                 Files.createDirectories(parent);
