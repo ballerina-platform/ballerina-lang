4c040b78cbf182c9d49e0d55aff5c5ee168a80f5
Gigara <info@gigara.info>
2020-03-10 15:55:52 +0530

    Move code action util to executors package

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/command/executors/openAPI/OpenApiCodeActionUtil.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/command/executors/openAPI/OpenApiCodeActionUtil.java
@@ -1,4 +1,19 @@
-package org.ballerinalang.langserver.codeaction.providers.openAPI;
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ballerinalang.langserver.command.executors.openAPI;
 
 import org.ballerinalang.langserver.commons.LSContext;
 import org.ballerinalang.langserver.commons.workspace.LSDocumentIdentifier;
@@ -55,7 +70,7 @@ public class OpenApiCodeActionUtil {
                                 DiagnosticPos fieldPosition =
                                         ((BLangSimpleVarRef) ((BLangRecordLiteral.BLangRecordKeyValueField) field).key.expr).pos;
                                 if (fieldPosition.sLine == position.getLine() + 1 &&
-                                        fieldPosition.sCol == position.getCharacter() + 1) {
+                                        fieldPosition.sCol == position.getCharacter()) {
                                     return resourceFunction;
                                 }
 
