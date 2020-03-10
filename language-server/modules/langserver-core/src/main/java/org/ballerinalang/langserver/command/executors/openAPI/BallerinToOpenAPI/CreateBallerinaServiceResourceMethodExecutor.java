1e2d72d67ae6e3b8966582e694f73a8788731536
Gigara <info@gigara.info>
2020-03-10 15:54:34 +0530

    Add license details

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/command/executors/openAPI/BallerinToOpenAPI/CreateBallerinaServiceResourceMethodExecutor.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/command/executors/openAPI/BallerinToOpenAPI/CreateBallerinaServiceResourceMethodExecutor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ballerinalang.langserver.command.executors.openAPI.BallerinToOpenAPI;
 
 import com.google.gson.JsonObject;
@@ -90,8 +105,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.ballerinalang.langserver.codeaction.providers.openAPI.OpenApiCodeActionUtil.getBLangFunction;
-import static org.ballerinalang.langserver.codeaction.providers.openAPI.OpenApiCodeActionUtil.getBLangPkg;
+import static org.ballerinalang.langserver.command.executors.openAPI.OpenApiCodeActionUtil.getBLangFunction;
+import static org.ballerinalang.langserver.command.executors.openAPI.OpenApiCodeActionUtil.getBLangPkg;
 
 /**
  * Represents the command executor for creating a openAPI service resource method in contract file.
