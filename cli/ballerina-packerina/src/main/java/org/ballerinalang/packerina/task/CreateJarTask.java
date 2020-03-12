da45573b6a57a24c9465e0364b8983a97cc3f4e5
warunalakshitha <warudanu@gmail.com>
2020-03-12 13:49:54 +0530

    Remove unused import

--- cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/task/CreateJarTask.java
+++ cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/task/CreateJarTask.java
@@ -18,7 +18,6 @@
 
 package org.ballerinalang.packerina.task;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.ballerinalang.config.ConfigRegistry;
 import org.ballerinalang.model.elements.PackageID;
 import org.ballerinalang.packerina.buildcontext.BuildContext;
