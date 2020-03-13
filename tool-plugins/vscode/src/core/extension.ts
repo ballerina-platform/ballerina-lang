57c75f20b93c7dc1fc340b068a657676c42cebae
Rasika <info.rasika@gmail.com>
2020-03-13 15:58:14 +0530

    Fix VSCode prints ""<--command--> failed"
    Signed-off-by: Rasika <info.rasika@gmail.com>

--- tool-plugins/vscode/src/core/extension.ts
+++ tool-plugins/vscode/src/core/extension.ts
@@ -29,7 +29,7 @@ import {
 } from "./messages";
 import * as path from 'path';
 import * as fs from 'fs';
-import { exec, execSync } from 'child_process';
+import { exec, spawnSync } from 'child_process';
 import { LanguageClientOptions, State as LS_STATE, RevealOutputChannelOn, ServerOptions } from "vscode-languageclient";
 import { getServerOptions, getOldServerOptions, getOldCliServerOptions } from '../server/server';
 import { ExtendedLangClient } from './extended-language-client';
@@ -461,7 +461,7 @@ export class BallerinaExtension {
             isBallerinaNotFound = false,
             isOldBallerinaDist = false;
         try {
-            balHomeOutput = execSync(this.getBallerinaCmd() + ' home').toString().trim();
+            balHomeOutput = spawnSync(this.getBallerinaCmd(), ['home']).toString().trim();
             // specially handle unknown ballerina command scenario for windows
             if (balHomeOutput === "" && process.platform === "win32") {
                 isOldBallerinaDist = true;
