7026ba598fb43755656b906190b05471df4c23b1
Ayodhya <140597V@uom,lk>
2020-03-14 12:14:57 +0530

    [data-mapper] Add a script to build the project

---
+++ misc/data-mapper/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Cloning ballerina-lang"
+git clone https://github.com/Ayodhya94/ballerina-lang.git
+echo "Download complete"
+cd ballerina-lang/
+
+echo $PWD
+./gradlew clean build
+./gradlew publishToMavenLocal
+cd ..
+
+echo $PWD
+./gradlew clean build
+echo "complete"
