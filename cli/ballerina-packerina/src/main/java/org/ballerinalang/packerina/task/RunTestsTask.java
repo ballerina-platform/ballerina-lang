f0c81703464f513b8ea3dc5fe12797389633a146
Asma Jabir <azinneera@gmail.com>
2020-03-16 21:41:27 +0530

    Generate html test report

--- cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/task/RunTestsTask.java
+++ cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/task/RunTestsTask.java
@@ -19,7 +19,6 @@
 package org.ballerinalang.packerina.task;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.ballerinalang.packerina.OsUtils;
 import org.ballerinalang.packerina.buildcontext.BuildContext;
 import org.ballerinalang.packerina.buildcontext.BuildContextField;
@@ -42,6 +41,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.Writer;
@@ -54,6 +55,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
+import static org.ballerinalang.test.runtime.util.TesterinaConstants.FILE_PROTOCOL;
+import static org.ballerinalang.test.runtime.util.TesterinaConstants.HTML_RESOURCE_FILE;
 import static org.ballerinalang.test.runtime.util.TesterinaConstants.TEST_RESULTS_FILE;
 import static org.ballerinalang.test.runtime.util.TesterinaConstants.TEST_RUNTIME_JAR_PREFIX;
 import static org.ballerinalang.tool.LauncherUtils.createLauncherException;
@@ -154,7 +157,7 @@ public class RunTestsTask implements Task {
             }
         }
         testReport.finalizeTestResults(coverage);
-        writeReportToJson(buildContext.out(), testReport, targetDir);
+        generateHtmlReport(buildContext.out(), testReport, targetDir);
         if (result != 0) {
             throw createLauncherException("there are test failures");
         }
@@ -232,18 +235,41 @@ public class RunTestsTask implements Task {
      * @param out PrintStream object to print messages to console
      * @param testReport Data that are parsed to the json
      */
-    private void writeReportToJson(PrintStream out, TestReport testReport, Path jsonPath) {
+    private void generateHtmlReport(PrintStream out, TestReport testReport, Path jsonPath) {
         out.println();
         out.println("Generating Test Report");
+
+        Gson gson = new Gson();
+        String json = gson.toJson(testReport).replaceAll("\\\\\\(", "(");
         File jsonFile = new File(jsonPath.resolve(TEST_RESULTS_FILE).toString());
+        String content;
+
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream(HTML_RESOURCE_FILE)) {
+                content = readFromInputStream(in);
+                content = content.replaceAll("__data__", "'" + json + "'");
+                content = content.replace("\"'", "'").replace("'\"", "'");
+        } catch (IOException e) {
+            throw LauncherUtils.createLauncherException("couldn't read content from the html file : " + e.toString());
+        }
         try (Writer writer = new OutputStreamWriter(new FileOutputStream(jsonFile), StandardCharsets.UTF_8)) {
-            Gson gson = new Gson();
-            String json = gson.toJson(testReport);
-            writer.write(new String(json.getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8));
-            out.println("\t" + Paths.get("").toAbsolutePath().relativize(jsonFile.toPath()));
+            writer.write(new String(content.getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8));
+            out.println("\tView the test report at: " + FILE_PROTOCOL + jsonFile.getAbsolutePath());
         } catch (IOException e) {
             throw LauncherUtils.createLauncherException("couldn't read data from the Json file : " + e.toString());
         }
+    }
+
+    private String readFromInputStream(InputStream inputStream)
+            throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
     }
 
     private int runTestSuit(Path jsonPath, BuildContext buildContext, HashSet<Path> testDependencies,
