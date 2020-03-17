7cff34ce07d27e1440e61a94c980a77acbdee536
kalaiyarasiganeshalingam <ashakalai31@gmail.com>
2020-03-17 12:48:42 +0530

    Revert "Implement get generated keys only if supported by JDBC"

--- stdlib/jdbc/src/main/java/org/ballerinax/jdbc/statement/BatchUpdateStatement.java
+++ stdlib/jdbc/src/main/java/org/ballerinax/jdbc/statement/BatchUpdateStatement.java
@@ -55,25 +55,22 @@ public class BatchUpdateStatement extends AbstractSQLStatement {
     private final String query;
     private final ArrayValue[] parameters;
     private final boolean rollbackAllInFailure;
-    private final boolean returnGeneratedKeys;
 
     public BatchUpdateStatement(ObjectValue client, SQLDatasource datasource, String query,
-                                boolean rollbackAllInFailure, Strand strand, boolean returnGeneratedKeys,
-                                ArrayValue... parameters) {
+                                boolean rollbackAllInFailure, Strand strand, ArrayValue... parameters) {
         super(strand);
         this.client = client;
         this.datasource = datasource;
         this.query = query;
         this.parameters = parameters;
         this.rollbackAllInFailure = rollbackAllInFailure;
-        this.returnGeneratedKeys = returnGeneratedKeys;
     }
 
     @Override
     public MapValue<String, Object> execute() {
         Connection conn = null;
         PreparedStatement stmt = null;
-        ResultSet rs = null;
+        ResultSet rs;
         MapValue<String, ArrayValue> generatedKeys = new MapValueImpl<>();
         int[] updatedCount;
         int paramArrayCount = 0;
@@ -86,7 +83,8 @@ public class BatchUpdateStatement extends AbstractSQLStatement {
         String errorMessagePrefix = "failed to execute batch update";
         try {
             conn = getDatabaseConnection(strand, client, datasource);
-            if (returnGeneratedKeys) {
+            boolean generatedKeyReturningSupported = isGeneratedKeyReturningSupported();
+            if (generatedKeyReturningSupported) {
                 stmt = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
             } else {
                 stmt = conn.prepareStatement(query);
@@ -104,7 +102,7 @@ public class BatchUpdateStatement extends AbstractSQLStatement {
                 stmt.addBatch();
             }
             updatedCount = stmt.executeBatch();
-            if (returnGeneratedKeys) {
+            if (generatedKeyReturningSupported) {
                 rs = stmt.getGeneratedKeys();
                 //This result set contains the auto generated keys.
                 generatedKeys = getGeneratedKeysFromBatch(rs);
@@ -145,8 +143,17 @@ public class BatchUpdateStatement extends AbstractSQLStatement {
             return createFrozenBatchUpdateResultRecord(createUpdatedCountArray(null, paramArrayCount),
                     generatedKeys, ErrorGenerator.getSQLApplicationError(e, errorMessagePrefix + ": "));
         } finally {
-            cleanupResources(rs, stmt, conn, !isInTransaction);
+            cleanupResources(stmt, conn, !isInTransaction);
         }
+    }
+
+    // It has been identified that Oracle and MS SQL Server does not support returning generated keys along with
+    // batch update. And such effort would result in an exception causing batch update failure.
+    // If such other databases are identified they can be included here.
+    // The name of the database is being checked because there is no way to identify through the API.
+    private boolean isGeneratedKeyReturningSupported() {
+        return !Constants.DatabaseNames.ORACLE.equals(datasource.getDatabaseProductName())
+                && !Constants.DatabaseNames.MSSQL_SERVER.equals(datasource.getDatabaseProductName());
     }
 
     private ArrayValue createUpdatedCountArray(int[] updatedCounts, int paramArrayCount) {
