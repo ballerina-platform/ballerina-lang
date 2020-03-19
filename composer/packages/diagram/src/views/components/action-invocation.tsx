b4f74ea15f462a71bfd99d634efa7fbfd1070219
Sharon David <srsharondavid@gmail.com>
2020-03-19 15:16:37 +0530

    Add states wrapper on the invocation arrow

--- composer/packages/diagram/src/views/components/action-invocation.tsx
+++ composer/packages/diagram/src/views/components/action-invocation.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { Popup } from "semantic-ui-react";
 import { DiagramConfig } from "../../config/default";
 import { DiagramUtils } from "../../diagram/diagram-utils";
+// import { getCodePoint } from "../../utils";
 import { StmntViewState } from "../../view-model/index";
 import { ArrowHead } from "./arrow-head";
 import { SourceLinkedLabel } from "./source-linked-label";
@@ -10,13 +11,21 @@ import { SourceLinkedLabel } from "./source-linked-label";
 const config: DiagramConfig = DiagramUtils.getConfig();
 
 export const ActionInvocation: React.StatelessComponent<{
-    model: StmntViewState, action: string, astModel?: ASTNode
+    model: StmntViewState,
+    action: string,
+    astModel?: ASTNode,
+    // icon: string,
 }> = ({
-    model, action, astModel
+    model, action, astModel,
 }) => {
+        // const topIcon = { x: 0, y: 0, className: "alert" };
         const sendLine = { x1: 0, y1: 0, x2: 0, y2: 0 };
         const receiveLine = { x1: 0, y1: 0, x2: 0, y2: 0 };
         const actionProps = { x: 0, y: 0 };
+        const statusRect = { x: 0, y: 0 , width: 55, height: 20, rx: 5, ry: 5 };
+        const succesText = { x: 0, y: 0 , width: 0, height: 0 };
+        const errorText = { x: 0, y: 0 , width: 0, height: 0 };
+        const msText = { x: 0, y: 0 , width: 0, height: 0 };
 
         sendLine.x1 = model.bBox.x;
         sendLine.y1 = sendLine.y2 = model.bBox.y + config.statement.height ;
@@ -29,10 +38,24 @@ export const ActionInvocation: React.StatelessComponent<{
         actionProps.x = model.bBox.x + config.statement.padding.left;
         actionProps.y = model.bBox.y + (config.statement.height / 2);
 
+        statusRect.x = model.bBox.x + (config.statement.expanded.rightMargin * 7);
+        statusRect.y = sendLine.y1 - (config.statement.expanded.topMargin * 2) - 5 ;
+
+        succesText.y = statusRect.y + (config.statement.height / 2);
+        succesText.x = statusRect.x + 5;
+        errorText.y = statusRect.y + (config.statement.height / 2);
+        errorText.x = succesText.x + config.statement.expanded.rightMargin - 5;
+        msText.y = errorText.y + config.statement.height + (config.statement.expanded.topMargin / 2) ;
+        msText.x = errorText.x - config.statement.expanded.leftMargin - 5;
+
         const fullExpression = (astModel) ? ASTUtil.genSource(astModel) : action;
 
         return (
             <g className="action-invocation">
+                <rect className = {"action-status"} {...statusRect} />
+                <text className= {"action-status-text-sucess"} {...succesText}>75%</text>
+                <text className= {"action-status-text-error"}  {...errorText}>25%</text>
+                <text className= {"action-status-text-ms"}  {...msText}>20ms</text>
                 <line {...sendLine} className="invoke-line" />
                 <ArrowHead direction="right" x={sendLine.x2} y={sendLine.y2} className="invoke-arrowhead" />
                 <line {...receiveLine} strokeDasharray={5} className="invoke-line" />
