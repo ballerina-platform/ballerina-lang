cec97b6bfe9eecb71f3c90876d93d1edb5b06b06
Tharik Kanaka <tharik.kanaka@gmail.com>
2020-03-20 07:30:33 +0530

    Update misc/docerina/src/main/java/org/ballerinalang/docgen/Generator.java

--- misc/docerina/src/main/java/org/ballerinalang/docgen/Generator.java
+++ misc/docerina/src/main/java/org/ballerinalang/docgen/Generator.java
@@ -160,7 +160,7 @@ public class Generator {
         } else if (kind == NodeKind.FUNCTION_TYPE) {
             // TODO: handle function type nodes
             added = true;
-        } else if (kind == NodeKind.BUILT_IN_REF_TYPE){
+        } else if (kind == NodeKind.BUILT_IN_REF_TYPE) {
             // TODO: handle built in ref type
             added = true;
         }
