aefa3d0f15d316658aef9828dd5fda8a4842ece5
ramithaYO <ramithagithub@gmail.com>
2020-03-26 11:22:17 +0530

    Fix checkstyleMain issue

--- langlib/lang.compilertest/src/main/java/org/ballerinalang/langlib/compilertest/AssertValueEqual.java
+++ langlib/lang.compilertest/src/main/java/org/ballerinalang/langlib/compilertest/AssertValueEqual.java
@@ -1,15 +1,39 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.ballerinalang.langlib.compilertest;
 
 import org.ballerinalang.jvm.BallerinaErrors;
+import org.ballerinalang.jvm.TypeChecker;
 import org.ballerinalang.jvm.scheduling.Strand;
 import org.ballerinalang.model.types.TypeKind;
-import org.ballerinalang.jvm.TypeChecker;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaFunction;
 
+/**
+ * Native implementation of assertValueEqual(anydata expected, anydata actual).
+ *
+ * @since 2.0
+ */
 @BallerinaFunction(
         orgName = "ballerina", packageName = "lang.compilertest", functionName = "assertValueEqual",
-        args = {@Argument(name = "expected", type = TypeKind.ANYDATA), @Argument(name = "actual", type = TypeKind.ANYDATA)},
+        args = {@Argument(name = "expected", type = TypeKind.ANYDATA),
+                @Argument(name = "actual", type = TypeKind.ANYDATA)},
         isPublic = true
 )
 public class AssertValueEqual {
