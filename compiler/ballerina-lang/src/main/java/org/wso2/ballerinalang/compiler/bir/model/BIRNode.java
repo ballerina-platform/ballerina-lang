750bcd20a59d61f40a72f53a92f58f354598a1ab
dulvin <dulvinw@gmail.com>
2020-03-30 22:43:48 +0530

    Fix spotbugs and checkstyle

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/model/BIRNode.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/model/BIRNode.java
@@ -31,7 +31,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Stack;
 
 /**
  * Root class of Ballerina intermediate representation-BIR.
