d92d0173e4f07bc39b4fcb5872e6b58522a09f94
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2020-03-30 11:41:56 +0530

    Update copyright header

--- misc/observerability-symbol-collector/src/main/java/org/ballerinalang/observability/anaylze/JsonASTHolder.java
+++ misc/observerability-symbol-collector/src/main/java/org/ballerinalang/observability/anaylze/JsonASTHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
