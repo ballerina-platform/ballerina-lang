4d9a19bdb3630847b2bb7c0a3ff251c1a5448e7c
gimantha <gimantha@wso2.com>
2020-04-16 13:47:04 +0530

    Fix checkstyle issues

--- bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/values/MapValueImpl.java
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/values/MapValueImpl.java
@@ -50,7 +50,6 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
@@ -272,11 +271,24 @@ public class MapValueImpl<K, V> extends LinkedHashMap<K, V> implements RefValue,
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+           return false;
+        }
+
         MapValueImpl<?, ?> mapValue = (MapValueImpl<?, ?>) o;
-        if (mapValue.type.getTag() != this.type.getTag()) return false;
-        if (this.entrySet().size() != mapValue.entrySet().size()) return false;
+
+        if (mapValue.type.getTag() != this.type.getTag()) {
+            return false;
+        }
+
+        if (this.entrySet().size() != mapValue.entrySet().size()) {
+            return false;
+        }
+
         return entrySet().equals(mapValue.entrySet());
     }
 
