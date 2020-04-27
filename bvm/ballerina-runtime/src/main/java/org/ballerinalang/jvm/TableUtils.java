787f779403ab1bc936ceee3327fc50fd6f8fb102
gimantha <gimantha@wso2.com>
2020-04-27 14:36:09 +0530

    Refactor Code

--- bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/TableUtils.java
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/TableUtils.java
@@ -20,6 +20,9 @@ package org.ballerinalang.jvm;
 import org.ballerinalang.jvm.types.BTupleType;
 import org.ballerinalang.jvm.types.BType;
 import org.ballerinalang.jvm.types.TypeTags;
+import org.ballerinalang.jvm.util.exceptions.BLangExceptionHelper;
+import org.ballerinalang.jvm.util.exceptions.BallerinaException;
+import org.ballerinalang.jvm.util.exceptions.RuntimeErrors;
 import org.ballerinalang.jvm.values.ArrayValue;
 import org.ballerinalang.jvm.values.IteratorValue;
 import org.ballerinalang.jvm.values.MapValue;
@@ -27,8 +30,11 @@ import org.ballerinalang.jvm.values.RefValue;
 import org.ballerinalang.jvm.values.TableValue;
 import org.ballerinalang.jvm.values.TupleValueImpl;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
+import static org.ballerinalang.jvm.util.exceptions.BallerinaErrorReasons.CONSTRUCT_FROM_CYCLIC_VALUE_REFERENCE_ERROR;
 
 /**
  * This class contains the utility methods required by the table implementation.
@@ -40,11 +46,21 @@ public class TableUtils {
 
     /**
      * Generates a hash value which is same for the same shape.
+     *
      * @param obj Ballerina value which the hash is generated from
      * @return The hash value
      */
-    public static Integer hash(Object obj) {
+    public static Integer hash(Object obj, List<Object> checkedValues) {
         int result;
+
+        if (checkedValues.contains(obj)) {
+            throw new BallerinaException(CONSTRUCT_FROM_CYCLIC_VALUE_REFERENCE_ERROR,
+                    BLangExceptionHelper.getErrorMessage(RuntimeErrors.CYCLIC_VALUE_REFERENCE,
+                            TypeChecker.getType(obj)));
+        }
+
+        checkedValues.add(obj);
+
         if (obj instanceof RefValue) {
             RefValue refValue = (RefValue) obj;
             BType refType = refValue.getType();
@@ -52,8 +68,9 @@ public class TableUtils {
                 MapValue mapValue = (MapValue) refValue;
                 result = mapValue.getType().hashCode();
                 for (Object entry : mapValue.entrySet()) {
-                    result = 31 * result + hash(((Map.Entry) entry).getKey()) +
-                            (((Map.Entry) entry).getValue() == null ? 0 : hash(((Map.Entry) entry).getValue()));
+                    result = 31 * result + hash(((Map.Entry) entry).getKey(), checkedValues) +
+                            (((Map.Entry) entry).getValue() == null ? 0 : hash(((Map.Entry) entry).getValue(),
+                                    checkedValues));
                 }
                 return result;
             } else if (refType.getTag() == TypeTags.ARRAY_TAG) {
@@ -61,7 +78,7 @@ public class TableUtils {
                 result = Objects.hash(refType, arrayValue.getElementType());
                 IteratorValue arrayIterator = arrayValue.getIterator();
                 while (arrayIterator.hasNext()) {
-                    result = 31 * result + hash(arrayIterator.next());
+                    result = 31 * result + hash(arrayIterator.next(), checkedValues);
                 }
                 return result;
             } else if (refType.getTag() == TypeTags.TUPLE_TAG) {
@@ -69,7 +86,7 @@ public class TableUtils {
                 result = Objects.hash(refType, ((BTupleType) refType).getTupleTypes());
                 IteratorValue arrayIterator = arrayValue.getIterator();
                 while (arrayIterator.hasNext()) {
-                    result = 31 * result + hash(arrayIterator.next());
+                    result = 31 * result + hash(arrayIterator.next(), checkedValues);
                 }
                 return result;
             } else {
@@ -82,9 +99,10 @@ public class TableUtils {
 
     /**
      * Handles table insertion/store functionality.
+     *
      * @param tableValue Table value which the values are inserted to
-     * @param key The key associated with the value
-     * @param value The value being inserted
+     * @param key        The key associated with the value
+     * @param value      The value being inserted
      */
     public static void handleTableStore(TableValue<Object, Object> tableValue, Object key, Object value) {
         tableValue.put(key, value);
