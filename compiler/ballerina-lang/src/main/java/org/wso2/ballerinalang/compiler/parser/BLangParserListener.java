4611afcfe3fec5e83f462df8179947275c164568
dulvin <dulvinw@gmail.com>
2020-04-29 18:28:25 +0530

    Fixed an issue where anonumous records were not getting created for type definitions

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangParserListener.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangParserListener.java
@@ -868,7 +868,28 @@ public class BLangParserListener extends BallerinaParserBaseListener {
 
         boolean isAnonymous = !(ctx.parent.parent instanceof BallerinaParser.FiniteTypeUnitContext);
 
+        // Validate type AB "A"| record { string f; };
+        if (!isAnonymous) {
+                isAnonymous = checkIfAnonymousInTypeDef(ctx);
+        }
+
         this.pkgBuilder.addRecordType(getCurrentPos(ctx), getWS(ctx), isAnonymous, false, false);
+    }
+
+    /**
+     * Validate if this is a anonymous record type describe in union with a type descriptor
+     *
+     * @param ctx the current context to be validated. can be InclusiveRecordTypeDescriptor or ExclusiveRecordTypeDes.
+     * @return true if is part of a union type descriptor with on the fly definition
+     */
+    private boolean checkIfAnonymousInTypeDef(ParserRuleContext ctx) {
+        if (!(ctx instanceof BallerinaParser.InclusiveRecordTypeDescriptorContext ||
+            ctx instanceof BallerinaParser.ExclusiveRecordTypeDescriptorContext)) {
+            return false;
+        }
+
+        return (ctx.parent.parent.parent instanceof BallerinaParser.FiniteTypeContext &&
+            ctx.parent.parent.parent.getChildCount() > 1);
     }
 
     @Override
@@ -887,6 +908,11 @@ public class BLangParserListener extends BallerinaParserBaseListener {
         }
 
         boolean isAnonymous = !(ctx.parent.parent instanceof BallerinaParser.FiniteTypeUnitContext);
+
+        // Validate type AB "A"| record {| string f; |};
+        if (!isAnonymous) {
+            isAnonymous = checkIfAnonymousInTypeDef(ctx);
+        }
 
         boolean hasRestField = ctx.recordRestFieldDefinition() != null;
 
