9ba7c59b3ddadddf6d5943baea9ce17be8f4b3f0
shehan360 <shehanpa@wso2.com>
2020-04-29 18:03:18 +0530

    Fix unknown links

--- misc/docerina/src/main/java/org/ballerinalang/docgen/Writer.java
+++ misc/docerina/src/main/java/org/ballerinalang/docgen/Writer.java
@@ -167,7 +167,7 @@ public class Writer {
             label = "<span class=\"array-type\">" + getTypeLabel(type.elementType, context) + getSuffixes(type)
                     + "</span>";
         } else if ("builtin".equals(type.category) || "lang.annotations".equals(type.moduleName)
-                || !type.generateUserDefinedTypeLink) {
+                || !type.generateUserDefinedTypeLink || "UNKNOWN".equals(type.category)) {
             label = "<span class=\"builtin-type\">" + type.name + getSuffixes(type) + "</span>";
         } else {
             label = getHtmlLink(type, root);
