8e9801399953dc94a421968bc9d7b37bb6a0d3a2
lochana-chathura <39232462+lochana-chathura@users.noreply.github.com>
2020-04-29 17:26:45 +0530

    Add test cases for error recovery

--- compiler/ballerina-parser/src/test/java/io/ballerinalang/compiler/parser/test/syntax/expressions/TypeCastExpressionTest.java
+++ compiler/ballerina-parser/src/test/java/io/ballerinalang/compiler/parser/test/syntax/expressions/TypeCastExpressionTest.java
@@ -43,10 +43,35 @@ public class TypeCastExpressionTest extends AbstractExpressionsTest {
 
     // Recovery tests
 
-//    @Test
-//    public void testTypeCastWithMissingExpr() {
-//        test("<int>", "type-cast-expr/type_cast_expr_assert_08.json");
-//        test("<@foo{}>", "type-cast-expr/type_cast_expr_assert_09.json");
-//        test("<@foo{} int>", "type-cast-expr/type_cast_expr_assert_10.json");
-//    }
+    @Test
+    public void testTypeCastWithMissingExpr() {
+        test("<int>", "type-cast-expr/type_cast_expr_assert_08.json");
+        test("<@foo{}>", "type-cast-expr/type_cast_expr_assert_09.json");
+        test("<@foo{} int>", "type-cast-expr/type_cast_expr_assert_10.json");
+    }
+
+    @Test
+    public void testTypeCastWithMissingParam() {
+        test("< > b", "type-cast-expr/type_cast_expr_assert_11.json");
+    }
+
+    @Test
+    public void testTypeCastWithMissingParamAndExpr() {
+        test("< >", "type-cast-expr/type_cast_expr_assert_12.json");
+    }
+
+    @Test
+    public void testTypeCastWithMissingGTToken() {
+        test("<int b", "type-cast-expr/type_cast_expr_assert_13.json");
+        // Here first b will be captured as a type-descriptor
+        test("<@foo{} b b", "type-cast-expr/type_cast_expr_assert_14.json");
+        test("<@foo{} b ", "type-cast-expr/type_cast_expr_assert_15.json");
+    }
+
+    @Test
+    public void testTypeCastWithMissingLTToken() {
+        test("int> b", "type-cast-expr/type_cast_expr_assert_16.json");
+        test("@foo{}> b", "type-cast-expr/type_cast_expr_assert_17.json");
+        test("@foo{} int> b", "type-cast-expr/type_cast_expr_assert_18.json");
+    }
 }
