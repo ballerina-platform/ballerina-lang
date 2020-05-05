31d1c78597877704a1649316aa2b79c7d9b0be84
Danesh Kuruppu <dknkuruppu@gmail.com>
2020-05-05 14:54:46 +0530

    JDK 11 migration - Fix compile failure due to remove in toml-parser

--- bvm/ballerina-config/src/main/java/module-info.java
+++ bvm/ballerina-config/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module ballerina.config {
     requires antlr4.runtime;
-    requires ballerina.toml.parser;
     requires org.apache.commons.lang3;
     exports org.ballerinalang.config;
     exports org.ballerinalang.config.cipher;
