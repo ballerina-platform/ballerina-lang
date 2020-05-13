ac860e0aff2a4a4bbc2ae3da8b8bd350355a1bfd
chiranSachintha <33259160+chiranSachintha@users.noreply.github.com>
2020-05-13 17:51:55 +0530

    Fix review suggestions

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolResolver.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolResolver.java
@@ -814,8 +814,8 @@ public class SymbolResolver extends BLangNodeVisitor {
             symTable.anyOrErrorType = BUnionType.create(null, symTable.anyType, symTable.errorType);
             symTable.mapAllType = new BMapType(TypeTags.MAP, symTable.anyOrErrorType, null);
             symTable.arrayAllType = new BArrayType(symTable.anyOrErrorType);
-            ((BTypedescType) symTable.typeDesc).constraint = symTable.anyOrErrorType;
-            ((BFutureType) symTable.futureType).constraint = symTable.anyOrErrorType;
+            symTable.typeDesc.constraint = symTable.anyOrErrorType;
+            symTable.futureType.constraint = symTable.anyOrErrorType;
             return;
         }
         throw new IllegalStateException("built-in error not found ?");
