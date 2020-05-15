e6f85f613836661295b53df249dddd54031b3f11
Kishanthan <kshanth2101@gmail.com>
2020-05-15 13:29:14 +0530

    Refactor CodeGenerator to run as part of compilation task
    This change will make codegen to run as part of compilation task
    now and with this codegen is merged with compile task and will
    be executed by the compile driver for each module.

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/CodeGenerator.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/CodeGenerator.java
@@ -18,31 +18,62 @@
 package org.wso2.ballerinalang.compiler.bir.codegen;
 
 import org.ballerinalang.compiler.BLangCompilerException;
+import org.ballerinalang.compiler.CompilerOptionName;
+import org.ballerinalang.compiler.CompilerPhase;
+import org.ballerinalang.model.elements.PackageID;
+import org.ballerinalang.toml.model.Library;
+import org.ballerinalang.toml.model.Manifest;
+import org.ballerinalang.toml.parser.ManifestProcessor;
+import org.wso2.ballerinalang.compiler.PackageCache;
 import org.wso2.ballerinalang.compiler.bir.codegen.internal.JarFile;
 import org.wso2.ballerinalang.compiler.bir.codegen.interop.InteropValidator;
+import org.wso2.ballerinalang.compiler.bir.emit.BIREmitter;
 import org.wso2.ballerinalang.compiler.bir.model.BIRNode;
 import org.wso2.ballerinalang.compiler.semantics.model.SymbolTable;
+import org.wso2.ballerinalang.compiler.semantics.model.symbols.BPackageSymbol;
+import org.wso2.ballerinalang.compiler.tree.BLangPackage;
+import org.wso2.ballerinalang.compiler.tree.BLangTestablePackage;
 import org.wso2.ballerinalang.compiler.util.CompilerContext;
+import org.wso2.ballerinalang.compiler.util.CompilerOptions;
+import org.wso2.ballerinalang.compiler.util.FileUtils;
+import org.wso2.ballerinalang.compiler.util.ProjectDirConstants;
+import org.wso2.ballerinalang.compiler.util.SourceType;
+import org.wso2.ballerinalang.compiler.util.diagnotic.BLangDiagnosticLogHelper;
+import org.wso2.ballerinalang.programfile.ProgramFileConstants;
+import org.wso2.ballerinalang.util.RepoUtils;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
-import java.util.jar.Manifest;
+import java.util.stream.Collectors;
+
+import static org.wso2.ballerinalang.compiler.util.ProjectDirConstants.BALLERINA_HOME;
+import static org.wso2.ballerinalang.compiler.util.ProjectDirConstants.BALLERINA_HOME_BRE;
+import static org.wso2.ballerinalang.compiler.util.ProjectDirConstants.BALLERINA_HOME_LIB;
+import static org.wso2.ballerinalang.compiler.util.ProjectDirConstants.BLANG_COMPILED_JAR_EXT;
+import static org.wso2.ballerinalang.compiler.util.SourceType.ALL_MODULES;
+import static org.wso2.ballerinalang.compiler.util.SourceType.SINGLE_MODULE;
 
 /**
  * JVM byte code generator from BIR model.
@@ -52,18 +83,34 @@ import java.util.jar.Manifest;
 public class CodeGenerator {
 
     private static final CompilerContext.Key<CodeGenerator> CODE_GEN = new CompilerContext.Key<>();
-
+    private final Set<Path> projectDependencies = new HashSet<>();
     private SymbolTable symbolTable;
-
-    private Map<String, BIRNode.BIRPackage> compiledPkgCache = new HashMap<>();
-
-    private JvmPackageGen jvmPackageGen;
+    private PackageCache packageCache;
+    private BLangDiagnosticLogHelper dlog;
+    private BIREmitter birEmitter;
+    private Path jarCachePath;
+    private Path projectRoot;
+    private boolean isBaloGen;
+    private SourceType sourceType;
+    private Path sourcePath;
+    private Manifest manifest;
+    private boolean skipTests;
+    private boolean dumbBIR;
+    private boolean skipAddDependencies;
+    private Path ballerinaHome = Paths.get(System.getProperty(BALLERINA_HOME));
+    private List<String> supportedPlatforms = Arrays.stream(ProgramFileConstants.SUPPORTED_PLATFORMS).
+            collect(Collectors.toList());
 
     private CodeGenerator(CompilerContext context) {
 
         context.put(CODE_GEN, this);
-        symbolTable = SymbolTable.getInstance(context);
-        jvmPackageGen = JvmPackageGen.getInstance(context);
+        this.symbolTable = SymbolTable.getInstance(context);
+        this.packageCache = PackageCache.getInstance(context);
+        this.dlog = BLangDiagnosticLogHelper.getInstance(context);
+        this.birEmitter = BIREmitter.getInstance(context);
+        this.manifest = ManifestProcessor.getInstance(context).getManifest();
+
+        init(CompilerOptions.getInstance(context));
     }
 
     public static CodeGenerator getInstance(CompilerContext context) {
@@ -76,25 +123,188 @@ public class CodeGenerator {
         return codeGenerator;
     }
 
-    public void generate(BIRNode.BIRPackage entryMod, Path target, Set<Path> moduleDependencies) {
+    private void init(CompilerOptions compilerOptions) {
 
-        if (compiledPkgCache.containsValue(entryMod)) {
+        // skip initialization of this instance if the phase is not codegen
+        if (compilerOptions.getCompilerPhase().compareTo(CompilerPhase.CODE_GEN) < 0) {
             return;
         }
 
-        compiledPkgCache.put(entryMod.org.value + entryMod.name.value, entryMod);
+        Path targetDirPath = Paths.get(compilerOptions.get(CompilerOptionName.TARGET_BINARY_PATH));
+        this.jarCachePath = targetDirPath.resolve(ProjectDirConstants.CACHES_DIR_NAME)
+                .resolve(ProjectDirConstants.JAR_CACHE_DIR_NAME);
+        this.projectRoot = Paths.get(compilerOptions.get(CompilerOptionName.PROJECT_DIR));
+        this.sourceType = SourceType.valueOf(compilerOptions.get(CompilerOptionName.SOURCE_TYPE));
+        this.sourcePath = Paths.get(compilerOptions.get(CompilerOptionName.SOURCE_PATH));
+        this.skipTests = getBooleanValueIfSet(compilerOptions, CompilerOptionName.SKIP_TESTS);
+        this.isBaloGen = getBooleanValueIfSet(compilerOptions, CompilerOptionName.BALO_GENERATION);
+        this.dumbBIR = getBooleanValueIfSet(compilerOptions, CompilerOptionName.DUMP_BIR);
+        this.skipAddDependencies = getBooleanValueIfSet(compilerOptions, CompilerOptionName.SKIP_ADD_DEPENDENCIES);
+        this.supportedPlatforms.add(ProgramFileConstants.ANY_PLATFORM);
+        findProjectDependenciesInManifest();
+    }
 
-        populateExternalMap();
+    private boolean getBooleanValueIfSet(CompilerOptions compilerOptions, CompilerOptionName optionName) {
 
-        ClassLoader classLoader = makeClassLoader(moduleDependencies);
-        InteropValidator interopValidator = new InteropValidator(classLoader, symbolTable);
+        return compilerOptions.isSet(optionName) && Boolean.parseBoolean(compilerOptions.get(optionName));
+    }
+
+    public void generate(BLangPackage entryMod) {
+
+        if (dumbBIR) {
+            birEmitter.emit(entryMod.symbol.bir);
+        }
+
+        // find target file path
+        Path targetJarName = findTargetJarName(entryMod);
+
+        // find module dependencies for classpath
+        Set<Path> moduleDependencies = findDependencies(entryMod.symbol);
+
+        // generate module jar
+        generate(entryMod.symbol.bir, targetJarName, moduleDependencies);
+
+        if (skipTests || !entryMod.hasTestablePackage()) {
+            return;
+        }
+
+        entryMod.getTestablePkgs().forEach(testablePackage -> {
+            // find target test file path
+            Path targetTestJarName = findTargetTestJarName(testablePackage);
+
+            // generate test module jar
+            generate(testablePackage.symbol.bir, targetTestJarName, moduleDependencies);
+        });
+    }
+
+    private void generate(BIRNode.BIRPackage entryMod, Path target, Set<Path> moduleDependencies) {
+
+        final JvmPackageGen jvmPackageGen = new JvmPackageGen(symbolTable, packageCache, dlog);
+
+        populateExternalMap(jvmPackageGen);
+
+        ClassLoader interopValidationClassLoader = makeClassLoader(moduleDependencies);
+        InteropValidator interopValidator = new InteropValidator(interopValidationClassLoader, symbolTable);
         JarFile jarFile = jvmPackageGen.generate(entryMod, interopValidator, true);
         writeJarFile(jarFile, target);
     }
 
+    private Path calculateTargetFilePath(PackageID moduleID) {
+
+        try {
+            Files.createDirectories(jarCachePath);
+            switch (sourceType) {
+                case SINGLE_BAL_FILE:
+                    String birFileName = FileUtils.geFileNameWithoutExtension(sourcePath);
+                    return jarCachePath.resolve(birFileName);
+                case SINGLE_MODULE:
+                case ALL_MODULES:
+                    Path moduleBirCacheDir = Files.createDirectories(jarCachePath
+                            .resolve(moduleID.orgName.value)
+                            .resolve(moduleID.name.value)
+                            .resolve(moduleID.version.value));
+                    return moduleBirCacheDir.resolve(moduleID.orgName.value + "-" + moduleID.name.value + "-" +
+                            moduleID.version.value);
+                default:
+                    throw new BLangCompilerException("unknown source type found: " + sourceType);
+            }
+
+        } catch (IOException e) {
+            throw new BLangCompilerException("error creating jar_cache directories for module(s): " + moduleID, e);
+        }
+    }
+
+    private Path findTargetJarName(BLangPackage bLangPackage) {
+
+        Path testFilePath = calculateTargetFilePath(bLangPackage.packageID);
+
+        return Paths.get(testFilePath.toString(), BLANG_COMPILED_JAR_EXT);
+    }
+
+    private Path findTargetTestJarName(BLangTestablePackage testablePackage) {
+
+        Path testFilePath = calculateTargetFilePath(testablePackage.packageID);
+
+        return Paths.get(testFilePath.toString(), "-testable", BLANG_COMPILED_JAR_EXT);
+    }
+
+    private Set<Path> findDependencies(BPackageSymbol packageSymbol) {
+
+        Set<Path> moduleDependencies = new HashSet<>();
+
+        if (skipAddDependencies) {
+            return moduleDependencies;
+        }
+
+        if (projectDependencies.size() > 0) {
+            moduleDependencies.addAll(projectDependencies);
+        }
+
+        if (isBaloGen) {
+            moduleDependencies.addAll(readInteropDependencies());
+        }
+
+        // setting the moduleDependencies to package symbol which will be used with final executable jar creation
+        packageSymbol.moduleDependencies = moduleDependencies;
+
+        return moduleDependencies;
+    }
+
+    private void findProjectDependenciesInManifest() {
+
+        // add runtime all jar to the classpath
+        projectDependencies.add(getRuntimeAllJarPath());
+
+        if (!SINGLE_MODULE.equals(sourceType) && !ALL_MODULES.equals(sourceType)) {
+            return;
+        }
+
+        // add platform libs, if they are defined
+        List<Library> libraries = manifest.getPlatform().libraries;
+        if (libraries != null && libraries.size() > 0) {
+            for (Library library : libraries) {
+
+                if (library.getPath() == null) {
+                    continue;
+                }
+
+                Path libFilePath = Paths.get(library.getPath());
+                Path libFile = projectRoot.resolve(libFilePath);
+
+                if (!libFile.toFile().exists()) {
+                    continue;
+                }
+                Path path = Paths.get(libFile.toUri());
+                projectDependencies.add(path);
+            }
+        }
+    }
+
+    private Path getRuntimeAllJarPath() {
+
+        String ballerinaVersion = RepoUtils.getBallerinaVersion();
+        String runtimeJarName = "ballerina-rt-" + ballerinaVersion + BLANG_COMPILED_JAR_EXT;
+        return Paths.get(ballerinaHome.toString(), BALLERINA_HOME_BRE, BALLERINA_HOME_LIB, runtimeJarName);
+    }
+
+    private HashSet<Path> readInteropDependencies() {
+
+        HashSet<Path> interopDependencies = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                new FileInputStream("build/interopJars.txt"), Charset.forName("UTF-8")))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                interopDependencies.add(Paths.get(line));
+            }
+        } catch (IOException e) {
+            throw new BLangCompilerException("error reading interop jar file names", e);
+        }
+        return interopDependencies;
+    }
+
     private ClassLoader makeClassLoader(Set<Path> moduleDependencies) {
 
-        if (moduleDependencies == null) {
+        if (moduleDependencies == null || moduleDependencies.size() == 0) {
             return Thread.currentThread().getContextClassLoader();
         }
         List<URL> dependentJars = new ArrayList<>();
@@ -109,7 +319,7 @@ public class CodeGenerator {
         return new URLClassLoader(dependentJars.toArray(new URL[]{}), null);
     }
 
-    private void populateExternalMap() {
+    private void populateExternalMap(JvmPackageGen jvmPackageGen) {
 
         String nativeMap = System.getenv("BALLERINA_NATIVE_MAP");
         if (nativeMap == null) {
@@ -137,7 +347,7 @@ public class CodeGenerator {
 
     private void writeJarFile(JarFile jarFile, Path targetPath) {
 
-        Manifest manifest = new Manifest();
+        java.util.jar.Manifest manifest = new java.util.jar.Manifest();
         Attributes mainAttributes = manifest.getMainAttributes();
         mainAttributes.put(Attributes.Name.MANIFEST_VERSION, "1.0");
         jarFile.getMainClassName().ifPresent(mainClassName ->
