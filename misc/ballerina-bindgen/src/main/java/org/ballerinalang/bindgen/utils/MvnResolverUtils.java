68853c4db8e5cd0d82ac6cf804fbd1f73f03654a
IrushiL <irushi@wso2.com>
2020-05-25 10:34:35 +0530

    Improve and add updating of Ballerina.toml component

--- misc/ballerina-bindgen/src/main/java/org/ballerinalang/bindgen/utils/MvnResolverUtils.java
+++ misc/ballerina-bindgen/src/main/java/org/ballerinalang/bindgen/utils/MvnResolverUtils.java
@@ -4,13 +4,20 @@ import org.ballerinalang.bindgen.exceptions.BindgenException;
 import org.ballerinalang.maven.Dependency;
 import org.ballerinalang.maven.MavenResolver;
 import org.ballerinalang.maven.exceptions.MavenResolverException;
+import org.ballerinalang.toml.model.Library;
+import org.ballerinalang.toml.model.Manifest;
+import org.ballerinalang.toml.model.Platform;
+import org.wso2.ballerinalang.util.TomlParserUtils;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.ballerinalang.bindgen.command.BindingsGenerator.getOutputPath;
 import static org.ballerinalang.bindgen.command.BindingsGenerator.setClassPaths;
+import static org.ballerinalang.bindgen.utils.BindgenConstants.BALLERINA_TOML;
 import static org.ballerinalang.bindgen.utils.BindgenConstants.MVN_REPO;
 import static org.ballerinalang.bindgen.utils.BindgenConstants.TARGET_DIR;
 import static org.ballerinalang.bindgen.utils.BindgenConstants.USER_DIR;
@@ -32,31 +39,67 @@ public class MvnResolverUtils {
         } else {
             mvnRepository = Paths.get(projectRoot.toString(), TARGET_DIR, MVN_REPO);
         }
-        populateBallerinaToml(groupId, artifactId, version, mvnRepository.toString(), false);
+        handleDependency(groupId, artifactId, version, mvnRepository.toString(), projectRoot, false);
         MavenResolver resolver = new MavenResolver(mvnRepository.toString());
         try {
             Dependency dependency = resolver.resolve(groupId, artifactId, version, true);
-            dependencyTraversal(dependency, mvnRepository.toString());
+            dependencyTraversal(dependency, mvnRepository.toString(), projectRoot);
         } catch (MavenResolverException e) {
             throw new BindgenException(e.getMessage());
         }
     }
 
-    private static void dependencyTraversal(Dependency dependency, String mvnRepository) {
+    private static void dependencyTraversal(Dependency dependency, String mvnRepository, Path projectRoot) throws BindgenException {
         if (dependency.getDepedencies() == null) {
             return;
         }
         for (Dependency transitive: dependency.getDepedencies()) {
-            populateBallerinaToml(transitive.getGroupId(), transitive.getArtifactId(), transitive.getVersion(),
-                    mvnRepository, true);
-            dependencyTraversal(transitive, mvnRepository);
+            handleDependency(transitive.getGroupId(), transitive.getArtifactId(), transitive.getVersion(),
+                    mvnRepository, projectRoot, true);
+            dependencyTraversal(transitive, mvnRepository, projectRoot);
         }
     }
 
-    private static void populateBallerinaToml(String groupId, String artifactId, String version, String mvnRepository,
-                                              Boolean isTransitive) {
+    private static void handleDependency(String groupId, String artifactId, String version, String mvnRepository,
+                                         Path projectRoot, Boolean isTransitive) throws BindgenException {
         Path mvnPath = Paths.get(mvnRepository, getPathFromGroupId(groupId), artifactId, version);
         setClassPaths(mvnPath.toString());
+        if (projectRoot != null) {
+            File tomlFile = new File(Paths.get(projectRoot.toString(), BALLERINA_TOML).toString());
+            if(tomlFile.exists() && !tomlFile.isDirectory()) {
+                populateBallerinaToml(groupId, artifactId, version, tomlFile, projectRoot, isTransitive);
+            }
+        }
+    }
+
+    private static void populateBallerinaToml(String groupId, String artifactId, String version, File tomlFile,
+                                              Path projectRoot, boolean isTransitive) throws BindgenException {
+        Manifest manifest = TomlParserUtils.getManifest(projectRoot);
+        try (FileWriter fileWriter = new FileWriter(tomlFile, true)) {
+            Platform platform = manifest.getPlatform();
+            if (platform.target == null && platform.libraries == null) {
+                fileWriter.write("\n\n[platform]\n");
+                fileWriter.write("target = \"java8\"\n");
+            } else {
+                for (Library library:platform.getLibraries()) {
+                    if (library.groupId != null && library.artifactId != null && library.version != null &&
+                            library.groupId.equals(groupId) && library.artifactId.equals(artifactId)
+                                && library.version.equals(version)) {
+                        return;
+                    }
+                }
+            }
+            fileWriter.write("\n");
+            if (isTransitive) {
+                fileWriter.write("# Transitive Dependency\n");
+            }
+            fileWriter.write("[[platform.libraries]]\n");
+            fileWriter.write("groupId = \"" + groupId + "\"\n");
+            fileWriter.write("artifactId = \"" + artifactId + "\"\n");
+            fileWriter.write("version = \"" + version + "\"\n");
+        } catch (IOException io) {
+            throw new BindgenException("Error while updating the Ballerina.toml file.");
+        }
     }
 
     private static String getPathFromGroupId(String groupId) {
