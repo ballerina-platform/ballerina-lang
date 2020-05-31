57721546efd45f8b89757d9497d85311423288bf
shafreenAnfar <www.call911@gmail.com>
2020-05-31 11:04:42 +0530

    Fix checkstyle issue

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/worker/WorkerTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/worker/WorkerTest.java
@@ -275,7 +275,7 @@ public class WorkerTest {
 
     @Test(expectedExceptions = BLangRuntimeException.class,
           expectedExceptionsMessageRegExp = ".*error: \\{ballerina/lang.future\\}FutureAlreadyCancelled.*",
-          enabled=false)
+          enabled = false)
     public void workerWithFutureTest1() {
         BValue[] returns = BRunUtil.invoke(result, "workerWithFutureTest1");
         Assert.assertEquals(returns.length, 1);
