4939db5f7404ae921b2c0ed7d15d3c43f0dd7587
sinthuja <s.rajan.sinthu@gmail.com>
2020-06-12 14:58:04 +0530

    Add sql local transactions tests

--- stdlib/database/sql/src/test/java/org/ballerinalang/sql/transaction/LocalTransactionsTest.java
+++ stdlib/database/sql/src/test/java/org/ballerinalang/sql/transaction/LocalTransactionsTest.java
@@ -19,7 +19,6 @@ package org.ballerinalang.sql.transaction;
 
 import org.ballerinalang.model.values.BBoolean;
 import org.ballerinalang.model.values.BInteger;
-import org.ballerinalang.model.values.BMap;
 import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BValue;
 import org.ballerinalang.model.values.BValueArray;
@@ -51,111 +50,113 @@ public class LocalTransactionsTest {
                 "local-transaction-test-data.sql"));
     }
 
-//    @Test
-//    public void testLocalTransaction() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testLocalTransaction", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 2);
-//        Assert.assertTrue(((BBoolean) bArray.getBValue(2)).booleanValue());
-//    }
-//
-//    @Test
-//    public void testTransactionRollbackWithCheck() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionRollbackWithCheck", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 1);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 0);
-//        Assert.assertFalse(((BBoolean) bArray.getBValue(2)).booleanValue());
-//    }
-//
-//    @Test
-//    public void testTransactionRollbackWithRollback() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionRollbackWithRollback", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 0);
-//        Assert.assertTrue(((BBoolean) bArray.getBValue(2)).booleanValue());
-//    }
-//
-//    @Test
-//    public void testLocalTransactionUpdateWithGeneratedKeys() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testLocalTransactionUpdateWithGeneratedKeys", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 2);
-//    }
-//
-//    @Test
-//    public void testLocalTransactionRollbackWithGeneratedKeys() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testLocalTransactionRollbackWithGeneratedKeys", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 1);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 0);
-//    }
-//
-//    @Test
-//    public void testTransactionAbort() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionAbort", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), -1);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(2)).intValue(), 0);
-//    }
-//
-//    @Test
-//    public void testTransactionErrorPanic() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionErrorPanic", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 1);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), -1);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(2)).intValue(), 0);
-//    }
-//
-//    @Test
-//    public void testTransactionErrorPanicAndTrap() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionErrorPanicAndTrap", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), -1);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(2)).intValue(), 1);
-//    }
-//
-//    @Test
-//    public void testTwoTransactions() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTwoTransactions", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 1);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 1);
-//        Assert.assertEquals(((BInteger) bArray.getBValue(2)).intValue(), 4);
-//    }
-//
-//    @Test
-//    public void testTransactionWithoutHandlers() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionWithoutHandlers", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 2);
-//    }
+    @Test
+    public void testLocalTransaction() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result, "testLocalTransaction", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 2);
+        Assert.assertTrue(((BBoolean) bArray.getBValue(2)).booleanValue());
+    }
+
+    @Test
+    public void testTransactionRollbackWithCheck() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionRollbackWithCheck", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 1);
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 0);
+        Assert.assertFalse(((BBoolean) bArray.getBValue(2)).booleanValue());
+    }
+
+    @Test
+    public void testTransactionRollbackWithRollback() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionRollbackWithRollback", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 0);
+        Assert.assertTrue(((BBoolean) bArray.getBValue(2)).booleanValue());
+    }
+
+    @Test
+    public void testLocalTransactionUpdateWithGeneratedKeys() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result,
+                "testLocalTransactionUpdateWithGeneratedKeys", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 2);
+    }
+
+    @Test
+    public void testLocalTransactionRollbackWithGeneratedKeys() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result,
+                "testLocalTransactionRollbackWithGeneratedKeys", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 1);
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 0);
+    }
+
+    @Test
+    public void testTransactionAbort() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionAbort", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), -1);
+        Assert.assertEquals(((BInteger) bArray.getBValue(2)).intValue(), 0);
+    }
+
+    @Test(enabled = false)
+    public void testTransactionErrorPanic() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionErrorPanic", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 1);
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), -1);
+        Assert.assertEquals(((BInteger) bArray.getBValue(2)).intValue(), 0);
+    }
+
+    @Test
+    public void testTransactionErrorPanicAndTrap() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionErrorPanicAndTrap", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 0);
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), -1);
+        Assert.assertEquals(((BInteger) bArray.getBValue(2)).intValue(), 1);
+    }
+
+    @Test(enabled = false)
+    public void testTwoTransactions() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTwoTransactions", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 1);
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 1);
+        Assert.assertEquals(((BInteger) bArray.getBValue(2)).intValue(), 4);
+    }
+
+    @Test
+    public void testTransactionWithoutHandlers() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result, "testTransactionWithoutHandlers", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BInteger) bArray.getBValue(0)).intValue(), 2);
+    }
 
     @Test
     public void testLocalTransactionFailed() {
@@ -168,15 +169,15 @@ public class LocalTransactionsTest {
         Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 0);
     }
 
-//    @Test
-//    public void testLocalTransactionSuccessWithFailed() {
-//        BValue[] returnVal = BRunUtil.invokeFunction(result, "testLocalTransactionSuccessWithFailed", args);
-//        SQLDBUtils.assertNotError(returnVal[0]);
-//        Assert.assertTrue(returnVal[0] instanceof BValueArray);
-//        BValueArray bArray = (BValueArray) returnVal[0];
-//        Assert.assertEquals(((BString) bArray.getBValue(0)).stringValue(),
-//                "beforetx inTrx inTrx inTrx committed afterTrx");
-//        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 2);
-//    }
+    @Test
+    public void testLocalTransactionSuccessWithFailed() {
+        BValue[] returnVal = BRunUtil.invokeFunction(result, "testLocalTransactionSuccessWithFailed", args);
+        SQLDBUtils.assertNotError(returnVal[0]);
+        Assert.assertTrue(returnVal[0] instanceof BValueArray);
+        BValueArray bArray = (BValueArray) returnVal[0];
+        Assert.assertEquals(((BString) bArray.getBValue(0)).stringValue(),
+                "beforetx inTrx inTrx inTrx committed afterTrx");
+        Assert.assertEquals(((BInteger) bArray.getBValue(1)).intValue(), 2);
+    }
 
 }
