ed317cf6471b0ddc5cf2b14b2bcf71a83c3c724d
gimantha <gimantha@wso2.com>
2020-06-13 15:58:43 +0530

    Fix imports order

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/transaction/RetryTransactionStmtTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/transaction/RetryTransactionStmtTest.java
@@ -22,8 +22,8 @@ import org.ballerinalang.model.values.BValue;
 import org.ballerinalang.test.util.BCompileUtil;
 import org.ballerinalang.test.util.BRunUtil;
 import org.ballerinalang.test.util.CompileResult;
-import org.testng.Assert;
 import org.ballerinalang.util.exceptions.BLangRuntimeException;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
