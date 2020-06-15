8dea1e3f11a6be3093005fc0ea9a8080563b8575
Supun Setunga <supun.setunga@gmail.com>
2020-06-15 10:57:00 +0530

    Disable RetryStmtTest

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/retrystmt/RetryStmtTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/retrystmt/RetryStmtTest.java
@@ -29,6 +29,7 @@ import org.testng.annotations.Test;
 /**
  * Test cases for flow validation in Retry Statement.
  */
+@Test(groups = { "disableOnOldParser" })
 public class RetryStmtTest {
 
     private CompileResult programFile, managerNegative, retryStmtNegative;
