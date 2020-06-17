fdb0eadca89d011beea7eb524c5d63a8bfa46847
Saif Abdul Cassim <saif.15@cse.mrt.ac.lk>
2020-06-17 12:41:47 +0530

    Enable test

--- langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibErrorTest.java
+++ langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibErrorTest.java
@@ -55,7 +55,7 @@ public class LangLibErrorTest {
         assertEquals(returns[1].stringValue(), "{message:\"Test union of errors with type test\"}");
     }
 
-    @Test(enabled = false)
+    @Test
     public void testErrorCause() {
         CompileResult errorCtor = BCompileUtil.compile("test-src/errorlib_error_ctor_test.bal");
         BValue[] returns = BRunUtil.invoke(errorCtor, "testErrorCause");
@@ -64,7 +64,7 @@ public class LangLibErrorTest {
         assertEquals(returns[2].stringValue(), "This is the cause {}");
     }
 
-    @Test(enabled = false)
+    @Test
     public void testErrorDestructureWithCause() {
         CompileResult errorCtor = BCompileUtil.compile("test-src/errorlib_error_ctor_test.bal");
         BValue[] returns = BRunUtil.invoke(errorCtor, "testErrorDestructureWithCause");
