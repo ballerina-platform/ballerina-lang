3d817a5d82430b6789d8a55bff36374d2328200e
Rasika <info.rasika@gmail.com>
2020-06-18 10:02:14 +0530

    Revert transformer changes
    Signed-off-by: Rasika <info.rasika@gmail.com>

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeTransformer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeTransformer.java
@@ -2554,7 +2554,6 @@ public class BLangNodeTransformer extends NodeTransformer<BLangNode> {
         }
 
         invocation.async = true;
-        invocation.pos = getPosition(startActionNode);
         invocation.annAttachments = applyAll(startActionNode.annotations());
         return invocation;
     }
@@ -2983,7 +2982,6 @@ public class BLangNodeTransformer extends NodeTransformer<BLangNode> {
 
         bLangActionInvocation.expr = createExpression(remoteMethodCallActionNode.expression());
         bLangActionInvocation.argExprs = applyAll(remoteMethodCallActionNode.arguments());
-        bLangActionInvocation.pos = getPosition(remoteMethodCallActionNode);
 
         BLangNameReference nameReference = createBLangNameReference(remoteMethodCallActionNode.methodName().name());
         bLangActionInvocation.name = (BLangIdentifier) nameReference.name;
