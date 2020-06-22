1fd44be3bff05a3c39099c5d14a7622379adb80a
Chiran <pcnfernando@gmail.com>
2020-06-23 01:37:02 +0530

    Fix checkstyle errors

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
@@ -2730,11 +2730,11 @@ public class TypeChecker extends BLangNodeVisitor {
                     return;
                 }
                 if (types.getErrorType(nextReturnType) == null && (types.getErrorType(expectedReturnType) != null)) {
-                    dlog.error(iteratorExpr.pos, DiagnosticCode.INVALID_STREAM_CONSTRUCTOR_EXP_TYPE,
-                            iteratorExpr);
+                    dlog.error(iteratorExpr.pos, DiagnosticCode.INVALID_STREAM_CONSTRUCTOR_EXP_TYPE, iteratorExpr);
                     resultType = symTable.semanticError;
                     return;
                 }
+
                 types.checkType(iteratorExpr.pos, nextReturnType, expectedReturnType,
                         DiagnosticCode.INCOMPATIBLE_TYPES);
                 resultType = actualType;
