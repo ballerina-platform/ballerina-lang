b841ca8540f4c0305edbb1cc42721cd05bdf74ba
Pubudu Fernando <pubuduf@wso2.com>
2020-07-06 17:21:18 +0530

    Use original caller request as the cache validation request

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/http/sample/HTTPCachingTestCase.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/http/sample/HTTPCachingTestCase.java
@@ -17,14 +17,18 @@
  */
 package org.ballerinalang.test.service.http.sample;
 
+import io.netty.handler.codec.http.HttpHeaderNames;
 import org.ballerinalang.test.service.http.HttpBaseTest;
 import org.ballerinalang.test.util.HttpClientRequest;
 import org.ballerinalang.test.util.HttpResponse;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 
 /**
  * Test cases for HTTP caching.
@@ -109,5 +113,48 @@ public class HTTPCachingTestCase extends HttpBaseTest {
         assertEquals(response.getData(), payload);
         assertEquals(response.getHeaders().get(serviceHitCount), "2");
         assertEquals(response.getHeaders().get(proxyHitCount), "3");
+    }
+
+    @Test(description = "Test preservation of caller request headers in the validation request")
+    public void testCallerRequestHeaderPreservation() throws IOException, InterruptedException {
+        final String callerReqHeader = "x-caller-req-header";
+        Map<String, String> headers = new HashMap<>();
+        headers.put(callerReqHeader, "First Request");
+
+        HttpResponse response = HttpClientRequest.doGet(serverInstance.getServiceURLHttp(9259, "validation-request"),
+                                                        headers);
+        assertEquals(response.getData(), payload);
+        assertEquals(response.getHeaders().get(callerReqHeader), "First Request");
+        assertFalse(response.getHeaders().containsKey(HttpHeaderNames.IF_NONE_MATCH.toString()));
+        assertFalse(response.getHeaders().containsKey(HttpHeaderNames.IF_MODIFIED_SINCE.toString()));
+
+        // Since this request gets served straight from the cache, the value of 'x-caller-req-header' doesn't change.
+        headers.put(callerReqHeader, "Second Request");
+        response = HttpClientRequest.doGet(serverInstance.getServiceURLHttp(9259, "validation-request"), headers);
+        assertEquals(response.getData(), payload);
+        assertEquals(response.getHeaders().get(callerReqHeader), "First Request");
+        assertFalse(response.getHeaders().containsKey(HttpHeaderNames.IF_NONE_MATCH.toString()));
+        assertFalse(response.getHeaders().containsKey(HttpHeaderNames.IF_MODIFIED_SINCE.toString()));
+
+        Thread.sleep(3000);
+
+        headers.put(callerReqHeader, "Third Request");
+        response = HttpClientRequest.doGet(serverInstance.getServiceURLHttp(9259, "validation-request"), headers);
+        assertEquals(response.getData(), payload);
+        assertEquals(response.getHeaders().get(callerReqHeader), "Third Request");
+        assertFalse(response.getHeaders().containsKey(HttpHeaderNames.IF_NONE_MATCH.toString()));
+        assertFalse(response.getHeaders().containsKey(HttpHeaderNames.IF_MODIFIED_SINCE.toString()));
+    }
+
+    @Test(description = "Test preservation of caller request headers in the validation request")
+    public void testCallerRequestHeaderPreservation2() throws IOException, InterruptedException {
+        Map<String, String> headers = new HashMap<>();
+        headers.put(HttpHeaderNames.IF_NONE_MATCH.toString(), "c854ce2c");
+
+        HttpResponse response = HttpClientRequest.doGet(serverInstance.getServiceURLHttp(9259, "validation-request"),
+                                                        headers);
+        assertEquals(response.getResponseCode(), 304);
+        assertEquals(response.getResponseMessage(), "Not Modified");
+        assertEquals(response.getData(), "");
     }
 }
