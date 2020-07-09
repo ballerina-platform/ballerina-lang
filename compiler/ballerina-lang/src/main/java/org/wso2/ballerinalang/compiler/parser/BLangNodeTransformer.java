3fd9b6c8d501850fc7fb98efd7167f502bcf3c4a
lochana-chathura <39232462+lochana-chathura@users.noreply.github.com>
2020-07-09 21:51:35 +0530

    Enable DocumentationAnalyzer for new parser and all documentation unit tests

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeTransformer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeTransformer.java
@@ -4208,7 +4208,7 @@ public class BLangNodeTransformer extends NodeTransformer<BLangNode> {
                             new BLangMarkDownDeprecationDocumentation();
                     DocumentationLineNode deprecationDocLineNode = (DocumentationLineNode) singleDocLine;
 
-                    String lineText = ((Token)deprecationDocLineNode.documentElements().get(0)).text();
+                    String lineText = ((Token) deprecationDocLineNode.documentElements().get(0)).text();
                     bLangDeprecationDoc.addDeprecationLine("#" + lineText);
                     bLangDeprecationDoc.pos = getPosition(deprecationDocLineNode);
                     doc.deprecationDocumentation = bLangDeprecationDoc;
