35174a4c2b0191df990b1bb88de866b965f5af27
Shehan Avishka Panditharatne <shehanpa@wso2.com>
2020-07-14 16:56:30 +0530

    Add support for map constraint

--- misc/docerina/src/main/java/org/ballerinalang/docgen/Writer.java
+++ misc/docerina/src/main/java/org/ballerinalang/docgen/Writer.java
@@ -205,6 +205,7 @@ public class Writer {
             label = "<span class=\"array-type\">" + getTypeLabel(type.elementType, context) + getSuffixes(type)
                     + "</span>";
         } else if ("map".equals(type.category) && type.constraint != null) {
+            // set map constraint
             label = "<span class=\"builtin-type\">" + type.name + "</span><" +
                     getTypeLabel(type.constraint, context) + ">";
         } else if ("stream".equals(type.category)) {
