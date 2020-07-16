a621de8001be1663544d7ff2cc22fdaee994aedc
bhashinee <bhashineen@gmail.com>
2020-07-16 14:20:09 +0530

    Enable all the test cases

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/websocket/RetryClientTest.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/websocket/RetryClientTest.java
@@ -51,7 +51,7 @@ public class RetryClientTest extends WebSocketTestCommons {
     }
 
     @Test(description = "Tests the retry function with the maximum count using the WebSocket client (" +
-            "Restart the server twice and send the data for every restart)", enabled = false)
+            "Restart the server twice and send the data for every restart)")
     public void testMultipleRetryAttempts() throws URISyntaxException, InterruptedException, BallerinaTestException {
         remoteServer = initiateServer();
         client = initiateClient(URL);
@@ -66,7 +66,7 @@ public class RetryClientTest extends WebSocketTestCommons {
     }
 
     @Test(description = "Tests the retry function using the WebSocket client (Restart the server and " +
-            "check the maximum count", enabled = false)
+            "check the maximum count")
     public void testBinaryFrameForRetryWithMaxCount() throws URISyntaxException, InterruptedException,
             BallerinaTestException {
         remoteServer = initiateServer();
@@ -82,7 +82,7 @@ public class RetryClientTest extends WebSocketTestCommons {
     }
 
     @Test(description = "Tests the `countDownLatch` for the retry function using the WebSocket client (" +
-            "Restart the server and check the countDownLatch for handshake)", enabled = false)
+            "Restart the server and check the countDownLatch for handshake)")
     public void testCountdownLatchForRetry() throws URISyntaxException, InterruptedException, BallerinaTestException {
         remoteServer = initiateServer();
         client = initiateClient(URL);
