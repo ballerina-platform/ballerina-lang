45f11b0129ba83f36bd6a977743eefda236994a6
IrushiL <irushi@wso2.com>
2020-07-17 15:47:24 +0530

    Get transitive dependencies

--- misc/ballerina-bindgen/src/main/java/org/ballerinalang/bindgen/utils/BindgenMvnResolver.java
+++ misc/ballerina-bindgen/src/main/java/org/ballerinalang/bindgen/utils/BindgenMvnResolver.java
@@ -89,6 +89,9 @@ public class BindgenMvnResolver {
         for (Dependency transitive : dependency.getDepedencies()) {
             handleDependency(transitive.getGroupId(), transitive.getArtifactId(), transitive.getVersion(),
                     mvnRepository, projectRoot, parent);
+            if (transitive.getDepedencies() != null) {
+                dependencyTraversal(transitive, mvnRepository, projectRoot);
+            }
         }
     }
 
