4500b550b4b38ebcf432e84af6615fdf37f8c7bc
lasinicl <lasini.1021@gmail.com>
2020-07-22 14:46:07 +0530

    Resolve conflicts

--- compiler/ballerina-parser/src/main/java/io/ballerinalang/compiler/internal/diagnostics/DiagnosticErrorCode.java
+++ compiler/ballerina-parser/src/main/java/io/ballerinalang/compiler/internal/diagnostics/DiagnosticErrorCode.java
@@ -216,6 +216,7 @@ public enum DiagnosticErrorCode implements DiagnosticCode {
     ERROR_MISSING_KEY_EXPR_IN_MEMBER_ACCESS_EXPR("BCE109", "error.missing.key.expr.in.member.access.expr"),
     ONLY_TYPE_REFERENCE_ALLOWED_HERE_AS_TYPE_INCLUSIONS("BCE110",
             "error.only.type.reference.allowed.here.as.type.inclusions"),
+    ERROR_MISSING_ORDER_KEY("BCE110", "error.missing.order.key"),
 
     ERROR_MISSING_ANNOTATION_ATTACH_POINT("BCE200", "error.missing.annotation.attach.point"),
     ERROR_MISSING_LET_VARIABLE_DECLARATION("BCE201", "error.missing.let.variable.declaration"),
