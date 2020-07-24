1459491394e25f2ef083e31a8873495a08727b11
Ayodhya94 <140597V@uom.lk>
2020-07-24 11:12:39 +0530

    Add PowerMockito to mock backend service

--- misc/ballerinalang-data-mapper/src/test/java/org/ballerinalang/datamapper/codeaction/CodeActionTest.java
+++ misc/ballerinalang-data-mapper/src/test/java/org/ballerinalang/datamapper/codeaction/CodeActionTest.java
@@ -1,19 +1,13 @@
 /*
- *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
  */
 package org.ballerinalang.datamapper.codeaction;
 
@@ -21,6 +15,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.ballerinalang.datamapper.AIDataMapperNetworkUtil;
 import org.ballerinalang.datamapper.util.FileUtils;
 import org.ballerinalang.datamapper.util.TestUtil;
 import org.ballerinalang.langserver.codeaction.CodeActionUtil;
@@ -30,6 +25,12 @@ import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.jsonrpc.Endpoint;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.testng.PowerMockTestCase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -47,9 +48,12 @@ import java.util.List;
 /**
  * Test Cases for CodeActions.
  *
- * @since 0.982.0
+ * @since 2.0.0
  */
-public class CodeActionTest {
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(AIDataMapperNetworkUtil.class)
+public class CodeActionTest extends PowerMockTestCase {
+
     private Endpoint serviceEndpoint;
 
     private JsonParser parser = new JsonParser();
@@ -66,10 +70,15 @@ public class CodeActionTest {
         TestUtil.setWorkspaceConfig(serviceEndpoint, configs);
     }
 
-
     @Test(dataProvider = "codeAction-data-mapper-data-provider")
     public void testDataMapperCodeAction(String config, String source)
             throws IOException, CompilationFailedException {
+        PowerMockito.mockStatic(AIDataMapperNetworkUtil.class);
+        Mockito.when(AIDataMapperNetworkUtil.getMapping())
+                .thenReturn("\nfunction mapStudentToGrades (Student student) returns Grades " +
+                        "{\n// Some record fields might be missing in the AI based mapping.\n\tGrades grades = " +
+                        "{maths: student.grades.maths, chemistry: student.grades.chemistry, physics: student.grades.physics};" +
+                        "\n\treturn grades;\n}");
         String configJsonPath = "codeaction" + File.separator + config;
         Path sourcePath = sourcesPath.resolve("source").resolve(source);
         JsonObject configJsonObject = FileUtils.fileContentAsObject(configJsonPath);
@@ -79,9 +88,10 @@ public class CodeActionTest {
                 CodeActionUtil.toDiagnostics(TestUtil.compileAndGetDiagnostics(sourcePath)));
         CodeActionContext codeActionContext = new CodeActionContext(diagnostics);
         Position position = new Position(configJsonObject.get("line").getAsInt(),
-                                    configJsonObject.get("character").getAsInt());
+                configJsonObject.get("character").getAsInt());
         Range range = new Range(position, position);
-        String response = TestUtil.getCodeActionResponse(serviceEndpoint, sourcePath.toString(), range, codeActionContext);
+        String response =
+                TestUtil.getCodeActionResponse(serviceEndpoint, sourcePath.toString(), range, codeActionContext);
         System.out.println(response);
         JsonObject expectedResponse = configJsonObject.get("expected").getAsJsonObject();
         String title = expectedResponse.get("title").getAsString();
@@ -106,7 +116,7 @@ public class CodeActionTest {
         }
         String cursorStr = range.getStart().getLine() + ":" + range.getEnd().getCharacter();
         Assert.assertTrue(codeActionFound,
-                          "Cannot find expected Code Action for: " + title + ", cursor at " + cursorStr);
+                "Cannot find expected Code Action for: " + title + ", cursor at " + cursorStr);
         TestUtil.closeDocument(this.serviceEndpoint, sourcePath);
     }
 
@@ -122,15 +132,9 @@ public class CodeActionTest {
         };
     }
 
-
     @AfterClass
     public void cleanupLanguageServer() {
         TestUtil.shutdownLanguageServer(this.serviceEndpoint);
-    }
-
-    @AfterClass
-    public void shutDownMockService() throws IOException{
-        codeActionTestUtil.shutDownMockService();
     }
 
     private JsonObject getResponseJson(String response) {
--- misc/ballerinalang-data-mapper/src/test/java/org/ballerinalang/datamapper/util/FileUtils.java
+++ misc/ballerinalang-data-mapper/src/test/java/org/ballerinalang/datamapper/util/FileUtils.java
@@ -1,20 +1,14 @@
 /*
-*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing,
-*  software distributed under the License is distributed on an
-*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-*  KIND, either express or implied.  See the License for the
-*  specific language governing permissions and limitations
-*  under the License.
-*/
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ */
 package org.ballerinalang.datamapper.util;
 
 import com.google.gson.JsonObject;
