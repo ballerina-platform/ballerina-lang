bd95466f2c8e3aa4840cc6e2867ae562ea8be302
sameerajayasoma <sameera.madushan@gmail.com>
2020-07-29 17:16:13 -0700

    Improve the invalid node minutiae capturing logic
    Break the invalid node into tokens and store each token as a separate Minutiae node

--- compiler/ballerina-parser/src/main/java/io/ballerinalang/compiler/internal/parser/BallerinaLexer.java
+++ compiler/ballerina-parser/src/main/java/io/ballerinalang/compiler/internal/parser/BallerinaLexer.java
@@ -1010,7 +1010,7 @@ public class BallerinaLexer extends AbstractLexer {
         }
 
         String tokenText = getLexeme();
-        STNode invalidToken = STNodeFactory.createInvalidToken(tokenText);
+        STToken invalidToken = STNodeFactory.createInvalidToken(tokenText);
         STNode invalidNodeMinutiae = STNodeFactory.createInvalidNodeMinutiae(invalidToken);
         this.leadingTriviaList.add(invalidNodeMinutiae);
     }
