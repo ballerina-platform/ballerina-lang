7b77f136ae53f4a9b11a6a79fff2fe2e5231d11b
lochana-chathura <39232462+lochana-chathura@users.noreply.github.com>
2020-08-06 16:23:32 +0530

    Update io.ballerinalang.compiler.parser.test.tree test cases

--- compiler/ballerina-parser/src/test/java/io/ballerinalang/compiler/parser/test/tree/TreeTraversalAPITest.java
+++ compiler/ballerina-parser/src/test/java/io/ballerinalang/compiler/parser/test/tree/TreeTraversalAPITest.java
@@ -234,7 +234,6 @@ public class TreeTraversalAPITest extends AbstractSyntaxTreeAPITest {
 
         List<SyntaxKind> actualChildNodeKindList = new ArrayList<>();
         List<SyntaxKind> expectedChildNodeKindList = Arrays.asList(
-                SyntaxKind.METADATA,
                 SyntaxKind.PUBLIC_KEYWORD,
                 SyntaxKind.FUNCTION_KEYWORD,
                 SyntaxKind.IDENTIFIER_TOKEN,
