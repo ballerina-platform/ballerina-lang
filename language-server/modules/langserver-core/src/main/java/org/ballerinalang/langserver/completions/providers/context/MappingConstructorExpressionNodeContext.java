b50adefdfed0182d1a8e06777c91b1f5ed866ba3
IrushiL <irushi@wso2.com>
2020-08-27 17:33:22 +0530

    Fix compilation errors

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/providers/context/MappingConstructorExpressionNodeContext.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/providers/context/MappingConstructorExpressionNodeContext.java
@@ -124,7 +124,7 @@ public class MappingConstructorExpressionNodeContext extends
         Token colon = null;
 
         if (evalNodeAtCursor.kind() == SyntaxKind.SPECIFIC_FIELD) {
-            colon = ((SpecificFieldNode) evalNodeAtCursor).colon();
+            colon = ((SpecificFieldNode) evalNodeAtCursor).colon().orElse(null);
         } else if (evalNodeAtCursor.kind() == SyntaxKind.COMPUTED_NAME_FIELD) {
             colon = ((ComputedNameFieldNode) evalNodeAtCursor).colonToken();
         }
