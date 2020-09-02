f838a70ba26890edd7c319b862e7bed6a8321e2f
dulmina <rdulmina@gmail.com>
2020-09-02 19:13:24 +0530

    Remove println.

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/grpc/sample/ServiceUnavailableTestCase.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/service/grpc/sample/ServiceUnavailableTestCase.java
@@ -30,7 +30,6 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.PrintStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -55,8 +54,6 @@ public class ServiceUnavailableTestCase extends GrpcBaseTest {
 
         BValue[] responses = BRunUtil.invoke(result, "testUnaryBlockingClient",
                 new Object[] { StringUtils.fromString("WSO2") });
-        PrintStream out = System.out;
-        out.println(responses[0].stringValue());
         Assert.assertEquals(responses.length, 1);
         Assert.assertTrue(responses[0] instanceof BString);
         Assert.assertTrue(responses[0].stringValue().contains(expectedMsg));
