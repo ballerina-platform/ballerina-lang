e10ac3fca738a9d42928daf3dfedd0d5f271f731
Buddhi <b.wathsala.bw@gmail.com>
2020-09-03 15:51:45 +0530

    Disable docerina tests

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/docerina/BallerinaApiDocsTestCase.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/docerina/BallerinaApiDocsTestCase.java
@@ -40,13 +40,13 @@ public class BallerinaApiDocsTestCase extends BaseTest {
     private static final String DOCS = "docs";
     private static final String INDEX_HTML = "index.html";
 
-    @Test(description = "Test docs directory generated in the server home")
+    @Test(enabled = false, description = "Test docs directory generated in the server home")
     public void testDocsDirectory() {
         Assert.assertTrue(Files.exists(Paths.get(balServer.getServerHome(), DOCS)));
         Assert.assertTrue(Files.exists(Paths.get(balServer.getServerHome(), DOCS, INDEX_HTML)));
     }
 
-    @Test(description = "Test docs directories of bir-cache modules", dependsOnMethods = "testDocsDirectory")
+    @Test(enabled = false, description = "Test docs directories of bir-cache modules", dependsOnMethods = "testDocsDirectory")
     public void testBirCacheModuleDocs() throws BallerinaTestException {
         PrintStream out = System.out;
         List<String> skipModules = new ArrayList<>(
@@ -69,7 +69,7 @@ public class BallerinaApiDocsTestCase extends BaseTest {
         }
     }
 
-    @Test(description = "Test auth module docs", dependsOnMethods = "testBirCacheModuleDocs")
+    @Test(enabled = false, description = "Test auth module docs", dependsOnMethods = "testBirCacheModuleDocs")
     public void testAuthModuleDocs() {
         Path authDocsDir = Paths.get(balServer.getServerHome(), DOCS, "auth");
         Assert.assertTrue(Files.exists(authDocsDir));
