fffa13cbab56fe839343cb65629b3860230340c2
Danesh Kuruppu <dknkuruppu@gmail.com>
2020-09-04 00:30:49 +0530

    Fix build failure

--- stdlib/mime/src/main/java/module-info.java
+++ stdlib/mime/src/main/java/module-info.java
@@ -3,8 +3,6 @@ module io.ballerina.mime {
     exports org.ballerinalang.mime.nativeimpl;
     requires io.ballerina.jvm;
     requires slf4j.api;
-    requires org.wso2.transport.http.netty;
-    requires io.netty.codec.http;
     requires io.ballerina.io;
     requires io.ballerina.lang;
     requires org.jvnet.mimepull;
