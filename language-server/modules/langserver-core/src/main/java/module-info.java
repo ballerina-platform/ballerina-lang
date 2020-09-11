59fe4c0837aeeca13297ea2a152b3828838b277c
Tharik Kanaka <tharik.kanaka@gmail.com>
2020-09-11 20:20:24 +0530

    Remove commented require module

--- language-server/modules/langserver-core/src/main/java/module-info.java
+++ language-server/modules/langserver-core/src/main/java/module-info.java
@@ -12,7 +12,6 @@ module io.ballerina.language.server.core {
     requires org.eclipse.lsp4j.jsonrpc;
     requires io.ballerina.lang;
     requires io.ballerina.jvm;
-//    requires slf4j.api;
     requires org.apache.commons.io;
     requires io.ballerina.openapi.convertor;
     requires handlebars;
