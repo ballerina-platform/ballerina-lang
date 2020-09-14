6226b90578a9879df5847c37cd40c601041ac49f
lnash94 <sumudunissanke@gmail.com>
2020-09-14 13:02:59 +0530

    Fix warnings

--- misc/openapi-ballerina/modules/openapi-to-ballerina-generator/src/main/java/org/ballerinalang/openapi/CodeGenerator.java
+++ misc/openapi-ballerina/modules/openapi-to-ballerina-generator/src/main/java/org/ballerinalang/openapi/CodeGenerator.java
@@ -125,6 +125,7 @@ public class CodeGenerator {
      * @param executionPath  Command execution path
      * @param definitionPath Input Open Api Definition file path
      * @param serviceName    Name of the service
+     * @param filter         Tags and Operations to be documented
      * @param outPath        Destination file path to save generated source files. If not provided
      *                       {@code definitionPath} will be used as the default destination path
      * @throws IOException               when file operations fail
