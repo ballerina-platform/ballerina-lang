96996a54bebc9a3b157df90821b80dabcfe6b82e
Fathima Dilhasha <dilhasha.uom@gmail.com>
2020-09-16 09:05:58 +0530

    Add Diff row evaluation

--- misc/testerina/modules/testerina-core/src/main/java/org/ballerinalang/testerina/core/AssertionDiffEvaluator.java
+++ misc/testerina/modules/testerina-core/src/main/java/org/ballerinalang/testerina/core/AssertionDiffEvaluator.java
@@ -1,22 +1,32 @@
 package org.ballerinalang.testerina.core;
 
-import com.github.difflib.DiffUtils;
 import com.github.difflib.algorithm.DiffException;
-import com.github.difflib.patch.AbstractDelta;
-import com.github.difflib.patch.Patch;
+import com.github.difflib.text.DiffRow;
+import com.github.difflib.text.DiffRowGenerator;
 import org.ballerinalang.model.values.BString;
 import org.ballerinalang.util.exceptions.BallerinaException;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Evaluates assertion values to find the difference inline.
  */
 public class AssertionDiffEvaluator {
     public static BString getStringDiff(String actual, String expected) throws BallerinaException {
-        String value = "";
-        try {
-            Patch<String> patch = DiffUtils.diffInline(actual, expected);
-            for (AbstractDelta<String> delta : patch.getDeltas()) {
-                value = value.concat(delta.toString());
+        String value = "Diff\t:\n~expected~\n**actual**\n";
+        DiffRowGenerator generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .inlineDiffByWord(false)
+                .oldTag(f -> "~")
+                .newTag(f -> "**")
+                .build();
+        try {   
+            List<DiffRow> rows = generator.generateDiffRows(
+                    Arrays.asList(expected),
+                    Arrays.asList(actual));
+            for (DiffRow row : rows) {
+                value = value.concat(row.getOldLine() + "\n" + row.getNewLine() + "\n");
             }
         } catch (DiffException e) {
             throw new BallerinaException("Diff inline failed");
@@ -24,3 +34,5 @@ public class AssertionDiffEvaluator {
         return new BString(value);
     }
 }
+
+
