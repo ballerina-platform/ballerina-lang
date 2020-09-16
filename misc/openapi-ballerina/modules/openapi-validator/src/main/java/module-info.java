9e07b4976d71ac67ef7111ff3895f9cd5eaf0c52
lnash94 <sumudunissanke@gmail.com>
2020-09-16 09:31:10 +0530

    Remove openapi from misc

--- misc/openapi-ballerina/modules/openapi-validator/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module io.ballerina.openapi.validator {
-    requires io.ballerina.lang;
-    requires io.swagger.v3.oas.models;
-    requires swagger.parser.v3;
-    requires swagger.parser.core;
-}
