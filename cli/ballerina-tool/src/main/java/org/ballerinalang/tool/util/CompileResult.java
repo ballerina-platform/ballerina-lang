70ec981218247c59f9718e514d439add83d30a83
dulaj <balasuriyadulaj@gmail.com>
2020-09-18 08:57:37 +0530

    Remove CompileResultDiagnosticListener from cli/ballerina-tool/util and align with new diagnostic API

--- cli/ballerina-tool/src/main/java/org/ballerinalang/tool/util/CompileResult.java
+++ cli/ballerina-tool/src/main/java/org/ballerinalang/tool/util/CompileResult.java
@@ -19,39 +19,57 @@ package org.ballerinalang.tool.util;
 import org.ballerinalang.model.tree.PackageNode;
 import org.ballerinalang.util.diagnostic.Diagnostic;
 import org.ballerinalang.util.diagnostic.DiagnosticListener;
+import org.wso2.ballerinalang.compiler.PackageCache;
+import org.wso2.ballerinalang.compiler.diagnostic.DiagnosticComparator;
+import org.wso2.ballerinalang.compiler.semantics.model.symbols.BPackageSymbol;
+import org.wso2.ballerinalang.compiler.tree.BLangImportPackage;
+import org.wso2.ballerinalang.compiler.tree.BLangPackage;
+import org.wso2.ballerinalang.compiler.util.CompilerContext;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
  * Represents the result of a ballerina file compilation.
- * 
+ *
  * @since 0.94
  */
 public class CompileResult {
 
     private PackageNode pkgNode;
-    private CompileResultDiagnosticListener diagnosticListener;
+    private CompilerContext context;
+    private io.ballerina.tools.diagnostics.Diagnostic[] diagnostics = null;
+    private int errorCount = 0;
+    private int warnCount = 0;
 
-    public CompileResult(CompileResultDiagnosticListener diagnosticListener) {
-        this.diagnosticListener = diagnosticListener;
+    public CompileResult(CompilerContext context, BLangPackage packageNode) {
+        this.pkgNode = packageNode;
+        this.context = context;
     }
 
-    public Diagnostic[] getDiagnostics() {
-        List<Diagnostic> diagnostics = this.diagnosticListener.getDiagnostics();
-        diagnostics.sort(Comparator.comparing((Diagnostic d) -> d.getSource().getCompilationUnitName()).
-                thenComparingInt(d -> d.getPosition().getStartLine()));
-        return diagnostics.toArray(new Diagnostic[diagnostics.size()]);
+    public io.ballerina.tools.diagnostics.Diagnostic[] getDiagnostics() {
+        if (this.diagnostics == null) {
+            populateDiagnostics();
+        }
+
+        return diagnostics;
     }
 
     public int getErrorCount() {
-        return this.diagnosticListener.errorCount;
+        if (diagnostics == null) {
+            populateDiagnostics();
+        }
+
+        return this.errorCount;
     }
 
     public int getWarnCount() {
-        return this.diagnosticListener.warnCount;
+        if (diagnostics == null) {
+            populateDiagnostics();
+        }
+
+        return this.warnCount;
     }
 
     public PackageNode getAST() {
@@ -65,15 +83,44 @@ public class CompileResult {
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        if (this.diagnosticListener.errorCount == 0) {
+        if (getErrorCount() == 0) {
             builder.append("Compilation Successful");
         } else {
             builder.append("Compilation Failed:\n");
-            for (Diagnostic diag : this.getDiagnostics()) {
+            for (io.ballerina.tools.diagnostics.Diagnostic diag : this.getDiagnostics()) {
                 builder.append(diag + "\n");
             }
         }
         return builder.toString();
+    }
+
+    private void populateDiagnostics() {
+        BLangPackage bLangPkg = (BLangPackage) this.pkgNode;
+        this.errorCount = bLangPkg.getErrorCount();
+        this.warnCount = bLangPkg.getWarnCount();
+
+        // Get diagnostics for imported packages
+        List<io.ballerina.tools.diagnostics.Diagnostic> diagList = bLangPkg.getDiagnostics();
+        PackageCache packageCache = PackageCache.getInstance(context);
+
+        for (BLangImportPackage importedPackage : bLangPkg.getImports()) {
+            BPackageSymbol pkgSymbol = importedPackage.symbol;
+            if (pkgSymbol == null) {
+                continue;
+            }
+
+            BLangPackage pkg = packageCache.get(pkgSymbol.pkgID);
+            if (pkg == null) {
+                continue;
+            }
+
+            diagList.addAll(pkg.getDiagnostics());
+            this.errorCount += pkg.getErrorCount();
+            this.warnCount += pkg.getWarnCount();
+        }
+
+        Collections.sort(diagList, new DiagnosticComparator());
+        this.diagnostics = diagList.toArray(new io.ballerina.tools.diagnostics.Diagnostic[diagList.size()]);
     }
 
     /**
