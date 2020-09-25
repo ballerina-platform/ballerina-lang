de8ca44803c8c0916a5d3c7e190a0afd705e4d14
Asma Jabir <azinneera@gmail.com>
2020-09-26 00:44:48 +0530

    Remove DocumentData, ModuleData and PackageData classes

--- project-api/ballerina-projects/src/main/java/io/ballerina/projects/directory/PackageLoader.java
+++ project-api/ballerina-projects/src/main/java/io/ballerina/projects/directory/PackageLoader.java
@@ -25,10 +25,20 @@ import io.ballerina.projects.ModuleName;
 import io.ballerina.projects.PackageConfig;
 import io.ballerina.projects.PackageId;
 import io.ballerina.projects.PackageName;
+import io.ballerina.projects.utils.ProjectUtils;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Contains a set of utility methods that creates the config hierarchy from the project directory.
@@ -36,74 +46,151 @@ import java.util.stream.Collectors;
  * @since 2.0.0
  */
 public class PackageLoader {
+    private static final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**.bal");
 
     public static PackageConfig loadPackage(String packageDir, boolean isSingleFile) {
-        final PackageData packageData = ProjectFiles.loadPackageData(packageDir, isSingleFile);
-        return createPackageConfig(packageData);
+        return createPackageConfig(Paths.get(packageDir).toAbsolutePath(), isSingleFile);
     }
 
-    protected static PackageConfig createPackageConfig(PackageData packageData) {
+    private static PackageConfig createPackageConfig(Path packagePath, boolean isSingleFile) {
         // TODO PackageData should contain the packageName. This should come from the Ballerina.toml file.
         // TODO For now, I take the directory name as the project name. I am not handling the case where the
         //  directory name is not a valid Ballerina identifier.
-        Path packagePath = packageData.packagePath();
+
+        // Validate that the provided package path exists
+        if (packagePath == null) {
+            throw new IllegalArgumentException("packageDir cannot be null");
+        }
         Path fileName = packagePath.getFileName();
         if (fileName == null) {
             // TODO Proper error handling
             throw new IllegalStateException("This branch cannot be reached");
         }
+
         PackageName packageName = PackageName.from(fileName.toString());
         PackageId packageId = PackageId.create(packageName.toString());
+        List<ModuleConfig> moduleConfigs = new ArrayList<>();
 
-        List<ModuleConfig> moduleConfigs = packageData.otherModules()
-                .stream()
-                .map(moduleData -> createModuleConfig(packageName, moduleData, packageId))
-                .collect(Collectors.toList());
-        ModuleConfig defaultModuleConfig = createDefaultModuleData(packageName,
-                packageData.defaultModule(), packageId);
-        moduleConfigs.add(defaultModuleConfig);
+        if (isSingleFile) {
+            moduleConfigs.add(createDefaultModuleConfig(packageName, packagePath, packageId));
+        } else {
+            // Check whether the directory exists
+            if (!packagePath.toFile().canRead()
+                    || !packagePath.toFile().canWrite() || !packagePath.toFile().canExecute()) {
+                throw new RuntimeException("insufficient privileges to path: " + packagePath);
+            }
+            if (!Files.exists(packagePath)) {
+                // TODO handle the error
+                // TODO use a custom runtime error
+                throw new RuntimeException("directory does not exists: " + packagePath);
+            }
+
+            if (!Files.isDirectory(packagePath)) {
+                throw new RuntimeException("Not a directory: " + packagePath);
+            }
+
+            List<Path> otherModulePaths = loadOtherModulePaths(packagePath);
+            moduleConfigs = otherModulePaths
+                    .stream()
+                    .map(moduleDirPath -> createModuleConfig(packageName, moduleDirPath, packageId))
+                    .collect(Collectors.toList());
+            ModuleConfig defaultModuleConfig = createDefaultModuleConfig(packageName,
+                    packagePath, packageId);
+            moduleConfigs.add(defaultModuleConfig);
+        }
+
         return PackageConfig.from(packageId, packageName, packagePath, moduleConfigs);
     }
 
-    private static ModuleConfig createDefaultModuleData(PackageName packageName,
-                                                        ModuleData moduleData,
-                                                        PackageId packageId) {
+    private static ModuleConfig createDefaultModuleConfig(PackageName packageName,
+                                                          Path moduleDirPath,
+                                                          PackageId packageId) {
         ModuleName moduleName = ModuleName.from(packageName);
-        return createModuleConfig(moduleName, moduleData, packageId);
+        return createModuleConfig(moduleName, moduleDirPath, packageId);
     }
 
     private static ModuleConfig createModuleConfig(PackageName packageName,
-                                                   ModuleData moduleData,
+                                                   Path moduleDirPath,
                                                    PackageId packageId) {
-        Path fileName = moduleData.moduleDirectoryPath().getFileName();
+        Path fileName = moduleDirPath.getFileName();
         if (fileName == null) {
             // TODO Proper error handling
             throw new IllegalStateException("This branch cannot be reached");
         }
+        if (!ProjectUtils.validateModuleName(fileName.toString())) {
+            throw new RuntimeException("Invalid module name : '" + fileName.toString() + "' :\n" +
+                    "Module name can only contain alphanumerics, underscores and periods " +
+                    "and the maximum length is 256 characters");
+        }
         ModuleName moduleName = ModuleName.from(packageName, fileName.toString());
-        return createModuleConfig(moduleName, moduleData, packageId);
+        return createModuleConfig(moduleName, moduleDirPath, packageId);
     }
 
     private static ModuleConfig createModuleConfig(ModuleName moduleName,
-                                                   ModuleData moduleData,
+                                                   Path moduleDirPath,
                                                    PackageId packageId) {
-        Path moduleDirPath = moduleData.moduleDirectoryPath();
         ModuleId moduleId = ModuleId.create(moduleName.toString(), packageId);
 
-        List<DocumentConfig> srcDocs = getDocumentConfigs(moduleId, moduleData.sourceDocs());
-        List<DocumentConfig> testSrcDocs = getDocumentConfigs(moduleId, moduleData.testSourceDocs());
+        List<Path> srcDocPaths = loadDocuments(moduleDirPath);
+        List<Path> testSrcDocPaths;
+        Path testDirPath = moduleDirPath.resolve("tests");
+        if (Files.isDirectory(testDirPath)) {
+            testSrcDocPaths = loadDocuments(testDirPath);
+        } else {
+            testSrcDocPaths = Collections.emptyList();
+        }
+
+        List<DocumentConfig> srcDocs = getDocumentConfigs(moduleId, srcDocPaths);
+        List<DocumentConfig> testSrcDocs = getDocumentConfigs(moduleId, testSrcDocPaths);
         return ModuleConfig.from(moduleId, moduleName, moduleDirPath, srcDocs, testSrcDocs);
     }
 
-    private static List<DocumentConfig> getDocumentConfigs(ModuleId moduleId, List<DocumentData> documentData) {
-        return documentData
+    private static List<DocumentConfig> getDocumentConfigs(ModuleId moduleId, List<Path> documentPathList) {
+        return documentPathList
                 .stream()
                 .map(srcDoc -> createDocumentConfig(srcDoc, moduleId))
                 .collect(Collectors.toList());
     }
 
-    static DocumentConfig createDocumentConfig(DocumentData documentData, ModuleId moduleId) {
-        final DocumentId documentId = DocumentId.create(documentData.name(), moduleId);
-        return DocumentConfig.from(documentId, documentData.content(), documentData.name());
+    private static DocumentConfig createDocumentConfig(Path documentPath, ModuleId moduleId) {
+        if (documentPath == null) {
+            throw new RuntimeException("document path cannot be null");
+        }
+
+        String name = documentPath.toFile().getName();
+        String content;
+        try {
+            content = new String(Files.readAllBytes(documentPath), Charset.defaultCharset());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        final DocumentId documentId = DocumentId.create(name, moduleId);
+        return DocumentConfig.from(documentId, content, name);
+    }
+
+    private static List<Path> loadDocuments(Path dirPath) {
+        try (Stream<Path> pathStream = Files.walk(dirPath, 1)) {
+            return pathStream
+                    .filter(matcher::matches)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<Path> loadOtherModulePaths(Path packageDirPath) {
+        Path modulesDirPath = packageDirPath.resolve("modules");
+        if (!Files.isDirectory(modulesDirPath)) {
+            return Collections.emptyList();
+        }
+
+        try (Stream<Path> pathStream = Files.walk(modulesDirPath, 1)) {
+            return pathStream
+                    .filter(path -> !path.equals(modulesDirPath))
+                    .filter(Files::isDirectory)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
