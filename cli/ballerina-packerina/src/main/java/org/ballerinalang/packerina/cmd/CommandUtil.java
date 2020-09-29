85eeb8dd999d94321ae29bea3598a5e2f6dfa461
Suganya <suganyasuven@gmail.com>
2020-09-29 12:44:43 +0530

    Refactor code

--- cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/cmd/CommandUtil.java
+++ cli/ballerina-packerina/src/main/java/org/ballerinalang/packerina/cmd/CommandUtil.java
@@ -38,7 +38,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
