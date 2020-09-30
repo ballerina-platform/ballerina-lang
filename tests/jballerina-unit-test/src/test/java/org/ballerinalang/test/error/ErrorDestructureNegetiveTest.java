bf901bccc011e09d753616e3a3d9ba32b52f3c34
dulmina <rdulmina@gmail.com>
2020-09-30 15:44:11 +0530

    Update error line position

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/error/ErrorDestructureNegetiveTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/error/ErrorDestructureNegetiveTest.java
@@ -35,7 +35,7 @@ public class ErrorDestructureNegetiveTest {
         CompileResult destructuringResult = BCompileUtil.
                 compile("test-src/error/error_destructure_binding_pattern_error_cause.bal");
         BAssertUtil.validateError(destructuringResult, 0,
-                "incompatible types: expected 'error?', found 'error'", 4, 21);
+                "incompatible types: expected 'error?', found 'error'", 20, 21);
         Assert.assertEquals(1, destructuringResult.getErrorCount());
     }
 }
