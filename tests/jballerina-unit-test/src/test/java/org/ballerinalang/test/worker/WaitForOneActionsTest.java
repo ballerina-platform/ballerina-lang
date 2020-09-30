5d3083e35da99aa0a56733eb69a14caa00333116
Tharik Kanaka <tharik.kanaka@gmail.com>
2020-09-30 13:32:24 +0530

    Enable unit tests

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/worker/WaitForOneActionsTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/worker/WaitForOneActionsTest.java
@@ -16,94 +16,94 @@
  */
 package org.ballerinalang.test.worker;
 
-//import org.ballerinalang.core.model.values.BValue;
-//import org.ballerinalang.core.util.exceptions.BLangRuntimeException;
-//import org.ballerinalang.test.util.BCompileUtil;
-//import org.ballerinalang.test.util.BRunUtil;
-//import org.ballerinalang.test.util.CompileResult;
-//import org.testng.Assert;
-//import org.testng.annotations.BeforeClass;
-//import org.testng.annotations.Test;
+import org.ballerinalang.core.model.values.BValue;
+import org.ballerinalang.core.util.exceptions.BLangRuntimeException;
+import org.ballerinalang.test.util.BCompileUtil;
+import org.ballerinalang.test.util.BRunUtil;
+import org.ballerinalang.test.util.CompileResult;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 /**
  * Wait for any action related tests.
  */
 public class WaitForOneActionsTest {
-//
-//    private CompileResult result;
-//
-//    @BeforeClass
-//    public void setup() {
-//        this.result = BCompileUtil.compile("test-src/workers/wait-for-one-actions.bal");
-//        Assert.assertEquals(result.getErrorCount(), 0, "Wait for any actions test error count");
-//    }
-//
-//    @Test
-//    public void waitTest1() {
-//        BValue[] vals = BRunUtil.invoke(result, "waitTest1", new BValue[0]);
-//        Assert.assertEquals(vals.length, 1);
-//        Assert.assertEquals(vals[0].stringValue(), "7");
-//    }
-//
-//    @Test
-//    public void waitTest2() {
-//        BValue[] vals = BRunUtil.invoke(result, "waitTest2", new BValue[0]);
-//        Assert.assertEquals(vals.length, 1);
-//        Assert.assertEquals(vals[0].stringValue(), "true");
-//    }
-//
-//    @Test
-//    public void waitTest3() {
-//        BValue[] vals = BRunUtil.invoke(result, "waitTest3", new BValue[0]);
-//        Assert.assertEquals(vals.length, 1);
-//        Assert.assertEquals(vals[0].stringValue(), "hello foo");
-//    }
-//
-//    @Test
-//    public void waitTest4() { // Need to check
-//        BValue[] vals = BRunUtil.invoke(result, "waitTest4", new BValue[0]);
-//        Assert.assertEquals(vals.length, 3);
-//        Assert.assertEquals(vals[0].stringValue(), "22");
-//        Assert.assertEquals(vals[1].stringValue(), "hello bar");
-//        Assert.assertEquals(vals[2].stringValue(), "hello xyz");
-//    }
-//
-//    @Test
-//    public void waitTest5() {
-//        BValue[] vals = BRunUtil.invoke(result, "waitTest5", new BValue[0]);
-//        Assert.assertEquals(vals.length, 1);
-//        Assert.assertEquals(vals[0].stringValue(), "{\"fname\":\"foo\", \"lname\":\"bar\"}");
-//    }
-//
-//    @Test
-//    public void waitTest6() {
-//        BValue[] vals = BRunUtil.invoke(result, "waitTest6", new BValue[0]);
-//        Assert.assertEquals(vals.length, 1);
-//        Assert.assertEquals(vals[0].stringValue(), "99");
-//    }
-//
-//    @Test
-//    public void waitTest7() {
-//        BValue[] vals = BRunUtil.invoke(result, "waitTest7", new BValue[0]);
-//        Assert.assertEquals(vals.length, 1);
-//        Assert.assertEquals(vals[0].stringValue(), "66");
-//    }
-//
-//    @Test (expectedExceptions = {BLangRuntimeException.class},
-//            expectedExceptionsMessageRegExp = "error: err from panic.*")
-//    public void waitTest8() {
-//        BRunUtil.invoke(result, "waitTest8");
-//    }
-//
-//    @Test
-//    public void waitTest9() {
-//        BValue[] vals = BRunUtil.invoke(result, "waitTest9", new BValue[0]);
-//        Assert.assertEquals(vals.length, 1);
-//        Assert.assertNull(vals[0]);
-//    }
-//
-//    @Test
-//    public void asyncObjectCreationTest() {
-//        BRunUtil.invoke(result, "asyncObjectCreationTest", new BValue[0]);
-//    }
+
+    private CompileResult result;
+
+    @BeforeClass
+    public void setup() {
+        this.result = BCompileUtil.compile("test-src/workers/wait-for-one-actions.bal");
+        Assert.assertEquals(result.getErrorCount(), 0, "Wait for any actions test error count");
+    }
+
+    @Test
+    public void waitTest1() {
+        BValue[] vals = BRunUtil.invoke(result, "waitTest1", new BValue[0]);
+        Assert.assertEquals(vals.length, 1);
+        Assert.assertEquals(vals[0].stringValue(), "7");
+    }
+
+    @Test
+    public void waitTest2() {
+        BValue[] vals = BRunUtil.invoke(result, "waitTest2", new BValue[0]);
+        Assert.assertEquals(vals.length, 1);
+        Assert.assertEquals(vals[0].stringValue(), "true");
+    }
+
+    @Test
+    public void waitTest3() {
+        BValue[] vals = BRunUtil.invoke(result, "waitTest3", new BValue[0]);
+        Assert.assertEquals(vals.length, 1);
+        Assert.assertEquals(vals[0].stringValue(), "hello foo");
+    }
+
+    @Test
+    public void waitTest4() { // Need to check
+        BValue[] vals = BRunUtil.invoke(result, "waitTest4", new BValue[0]);
+        Assert.assertEquals(vals.length, 3);
+        Assert.assertEquals(vals[0].stringValue(), "22");
+        Assert.assertEquals(vals[1].stringValue(), "hello bar");
+        Assert.assertEquals(vals[2].stringValue(), "hello xyz");
+    }
+
+    @Test
+    public void waitTest5() {
+        BValue[] vals = BRunUtil.invoke(result, "waitTest5", new BValue[0]);
+        Assert.assertEquals(vals.length, 1);
+        Assert.assertEquals(vals[0].stringValue(), "{\"fname\":\"foo\", \"lname\":\"bar\"}");
+    }
+
+    @Test
+    public void waitTest6() {
+        BValue[] vals = BRunUtil.invoke(result, "waitTest6", new BValue[0]);
+        Assert.assertEquals(vals.length, 1);
+        Assert.assertEquals(vals[0].stringValue(), "99");
+    }
+
+    @Test
+    public void waitTest7() {
+        BValue[] vals = BRunUtil.invoke(result, "waitTest7", new BValue[0]);
+        Assert.assertEquals(vals.length, 1);
+        Assert.assertEquals(vals[0].stringValue(), "66");
+    }
+
+    @Test (expectedExceptions = {BLangRuntimeException.class},
+            expectedExceptionsMessageRegExp = "error: err from panic.*")
+    public void waitTest8() {
+        BRunUtil.invoke(result, "waitTest8");
+    }
+
+    @Test
+    public void waitTest9() {
+        BValue[] vals = BRunUtil.invoke(result, "waitTest9", new BValue[0]);
+        Assert.assertEquals(vals.length, 1);
+        Assert.assertNull(vals[0]);
+    }
+
+    @Test
+    public void asyncObjectCreationTest() {
+        BRunUtil.invoke(result, "asyncObjectCreationTest", new BValue[0]);
+    }
 }
