6d2a6318ceb8f9e3cb68ca090f7835e1d030043a
dulmina <rdulmina@gmail.com>
2020-09-30 12:15:50 +0530

    Remove unused imports.

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/error/ErrorDestructureNegetiveTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/error/ErrorDestructureNegetiveTest.java
@@ -17,10 +17,8 @@
  */
 package org.ballerinalang.test.error;
 
-import org.ballerinalang.model.values.BValue;
 import org.ballerinalang.test.util.BAssertUtil;
 import org.ballerinalang.test.util.BCompileUtil;
-import org.ballerinalang.test.util.BRunUtil;
 import org.ballerinalang.test.util.CompileResult;
 import org.testng.Assert;
 import org.testng.annotations.Test;
