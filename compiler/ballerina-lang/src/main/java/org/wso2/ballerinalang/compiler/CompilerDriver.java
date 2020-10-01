cceace97332de3db6766baf4f5107edd0f6af1ce
suleka96 <sulekahelmini96comp@gmail.com>
2020-10-01 22:06:03 +0530

    Remove functional constructor related code

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/CompilerDriver.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/CompilerDriver.java
@@ -169,7 +169,6 @@ public class CompilerDriver {
             symbolTable.langQueryModuleSymbol = pkgLoader.loadPackageSymbol(QUERY, null, null);
             symbolTable.langTransactionModuleSymbol = pkgLoader.loadPackageSymbol(TRANSACTION, null, null);
             symbolTable.loadPredeclaredModules();
-            symResolver.loadFunctionalConstructors();
             return;
         }
 
---
+++ langlib/lang.xml/src/main/java/org/ballerinalang/langlib/xml/CreateText.java
@@ -0,0 +1,48 @@
+/*
+ *   Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ballerinalang.langlib.xml;
+
+import org.ballerinalang.jvm.XMLFactory;
+import org.ballerinalang.jvm.api.values.BString;
+import org.ballerinalang.jvm.scheduling.Strand;
+import org.ballerinalang.jvm.values.XMLValue;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaFunction;
+import org.ballerinalang.natives.annotations.ReturnType;
+
+import static org.ballerinalang.util.BLangCompilerConstants.XML_VERSION;
+
+/**
+ * Create XML text value from string.
+ *
+ * @since Swan Lake
+ */
+@BallerinaFunction(
+        orgName = "ballerina", packageName = "lang.xml", version = XML_VERSION,
+        functionName = "createText",
+        args = {@Argument(name = "string", type = TypeKind.STRING)},
+        returnType = {@ReturnType(type = TypeKind.XML)},
+        isPublic = true
+)
+public class CreateText {
+
+    public static XMLValue createText(Strand strand, BString string) {
+        return XMLFactory.createXMLText(string);
+    }
+}
