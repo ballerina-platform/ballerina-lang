24f6e2eb6a69ed6e2d3ec0e9e92b5d3ffbfeae0b
warunalakshitha <warudanu@gmail.com>
2020-10-08 16:52:27 +0530

    Fix the licence year in new type interfaces

--- bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/api/Types.java
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/api/Types.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *  WSO2 Inc. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -62,7 +62,7 @@ import static org.ballerinalang.jvm.util.BLangConstants.XML_LANG_LIB;
 /**
  * This class contains various methods manipulate {@link BType}s in Ballerina.
  *
- * @since 0.995.0
+ * @since 2.0.0
  */
 public class Types {
 
--- bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/api/types/MapType.java
+++ bvm/ballerina-runtime/src/main/java/org/ballerinalang/jvm/api/types/MapType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 Inc. (http://wso2.com) All Rights Reserved.
  * <p>
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
