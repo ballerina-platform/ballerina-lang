87834e8f4801163ed05584973d11e2c19915dcae
sameerajayasoma <sameera.madushan@gmail.com>
2020-10-09 06:39:38 -0700

    Make toml parser a Java module

---
+++ misc/toml-parser/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module io.ballerina.toml.parser {
+    requires io.ballerina.tools.api;
+    exports io.ballerina.toml.syntax.tree;
+    exports io.ballerina.toml.ast;
+    exports io.ballerina.toml;
+}
