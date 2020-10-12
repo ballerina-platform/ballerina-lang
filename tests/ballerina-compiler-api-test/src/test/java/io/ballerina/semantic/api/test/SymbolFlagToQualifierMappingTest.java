68ffacb0d6abd220fdb7976aad638758633fe065
Pubudu Fernando <pubuduf@wso2.com>
2020-10-12 12:49:57 +0530

    Move symbol mapping test to new test module

--- tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/SymbolFlagToQualifierMappingTest.java
+++ tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/SymbolFlagToQualifierMappingTest.java
@@ -18,11 +18,11 @@
 package io.ballerina.semantic.api.test;
 
 import io.ballerina.compiler.api.SemanticModel;
+import io.ballerina.compiler.api.impl.BallerinaSemanticModel;
 import io.ballerina.compiler.api.symbols.Qualifiable;
 import io.ballerina.compiler.api.symbols.Qualifier;
 import io.ballerina.compiler.api.symbols.Symbol;
 import io.ballerina.compiler.api.symbols.SymbolKind;
-import io.ballerina.compiler.impl.BallerinaSemanticModel;
 import io.ballerina.tools.text.LinePosition;
 import org.ballerinalang.test.util.CompileResult;
 import org.testng.annotations.BeforeClass;
