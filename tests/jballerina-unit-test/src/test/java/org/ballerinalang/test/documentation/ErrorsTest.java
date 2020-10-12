ce3da426e1469cfa81661724cf1fb9c3bd65f15c
Shehan <shehanpa@wso2.com>
2020-10-12 10:31:24 +0530

    Add license header

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/documentation/ErrorsTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/documentation/ErrorsTest.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.ballerinalang.test.documentation;
 
 import org.ballerinalang.docgen.docs.BallerinaDocGenerator;
@@ -22,6 +40,8 @@ import java.util.Map;
 
 /**
  * Test cases related to error types in docs.
+ *
+ * @since 2.0
  */
 public class ErrorsTest {
     private Module testModule;
@@ -35,7 +55,7 @@ public class ErrorsTest {
         List<BLangPackage> modules = new LinkedList<>();
         modules.add((BLangPackage) result.getAST());
         Map<String, ModuleDoc> docsMap = BallerinaDocGenerator.generateModuleDocs(
-                Paths.get("src/test/resources", sourceRoot).toAbsolutePath().toString(), modules);
+                Paths.get("src", "test", "resources", sourceRoot).toAbsolutePath().toString(), modules);
         List<ModuleDoc> moduleDocList = new ArrayList<>(docsMap.values());
         moduleDocList.sort(Comparator.comparing(pkg -> pkg.bLangPackage.packageID.toString()));
 
