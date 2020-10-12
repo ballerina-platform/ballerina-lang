7566f870cfb29ae8eb3e90290593eb5dd0193b2e
sameerajayasoma <sameera.madushan@gmail.com>
2020-10-12 08:05:01 -0700

    Remove toml parser from the proj_api branch

--- misc/toml-parser/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module io.ballerina.toml {
-    requires io.ballerina.tools.api;
-    exports io.ballerina.toml.api;
-    exports io.ballerina.toml.syntax.tree;
-    exports io.ballerina.toml.ast;
-}
