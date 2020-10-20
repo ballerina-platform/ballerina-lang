2f2adc278f705b6476d857752008b01dc0c7bcc5
dulaj <balasuriyadulaj@gmail.com>
2020-10-20 14:05:44 +0530

    Remove constructor since no requirement yet

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/diagnostic/BLangDiagnosticLocation.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/diagnostic/BLangDiagnosticLocation.java
@@ -34,16 +34,10 @@ public class BLangDiagnosticLocation implements Location {
     private LineRange lineRange;
     private TextRange textRange;
 
-    @Deprecated
     public BLangDiagnosticLocation(String filePath, int startLine, int endLine, int startColumn, int endColumn) {
         this.lineRange = LineRange.from(filePath, LinePosition.from(startLine, startColumn),
                 LinePosition.from(endLine, endColumn));
         this.textRange = TextRange.from(0, 0);
-    }
-
-    public BLangDiagnosticLocation(LineRange lineRange, TextRange textRange) {
-        this.lineRange = lineRange;
-        this.textRange = textRange;
     }
 
     @Override
