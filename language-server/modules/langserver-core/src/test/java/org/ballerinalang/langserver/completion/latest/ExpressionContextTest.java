e48554d822d8bf0efb65acf01bad6fbf5408d098
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2020-10-21 16:12:08 +0530

    enable more lang server tests

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/latest/ExpressionContextTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/latest/ExpressionContextTest.java
@@ -94,7 +94,7 @@ public class ExpressionContextTest extends CompletionTestNew {
                 {"new_expr_ctx_config3.json", this.getTestResourceDir()},
                 {"new_expr_ctx_config4.json", this.getTestResourceDir()},
                 {"new_expr_ctx_config5.json", this.getTestResourceDir()},        
-                {"new_expr_ctx_config6.json", this.getTestResourceDir()},     // blocked by 26309       
+                {"new_expr_ctx_config6.json", this.getTestResourceDir()},     
 //                {"new_expr_ctx_config7.json", this.getTestResourceDir()},   // blocked due to the typeref issue     
 //                {"new_expr_ctx_config8.json", this.getTestResourceDir()},   // blocked due to the typeref issue     
 //                {"new_expr_ctx_config9.json", this.getTestResourceDir()},   // blocked due to the typeref issue     
