f1471e87bf793283f71f2f3d0fce14e3f13a7606
kavindu <kavindu@wso2.com>
2020-10-28 15:53:45 +0530

    Change the test cases

---
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/matchstmt/varbindingpatternmatchpattern/ListBindingPatternTest.java
@@ -0,0 +1,157 @@
+package org.ballerinalang.test.statements.matchstmt.varbindingpatternmatchpattern;
+
+import org.ballerinalang.test.util.BAssertUtil;
+import org.ballerinalang.test.util.BCompileUtil;
+import org.ballerinalang.test.util.BRunUtil;
+import org.ballerinalang.test.util.CompileResult;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Test cases to verify the behaviour of the var binding-pattern list-binding-pattern.
+ *
+ * @since 2.0.0
+ */
+@Test(groups = { "disableOnOldParser" })
+public class ListBindingPatternTest {
+    private CompileResult result, restMatchPatternResult, resultNegative;
+    private String patternNotMatched = "pattern will not be matched";
+    private String unreachablePattern = "unreachable pattern";
+    private String unreachableCode = "unreachable code";
+
+    @BeforeClass
+    public void setup() {
+        result = BCompileUtil.compile("test-src/statements/matchstmt/varbindingpatternmatchpattern/list-binding" +
+                "-pattern.bal");
+        restMatchPatternResult = BCompileUtil.compile("test-src/statements/matchstmt/varbindingpatternmatchpattern/list-binding-pattern-with-rest-binding-pattern.bal");
+        resultNegative = BCompileUtil.compile("test-src/statements/matchstmt/varbindingpatternmatchpattern/list" +
+                "-binding-pattern-negative.bal");
+    }
+
+    @Test
+    public void testListBindingPattern1() {
+        BRunUtil.invoke(result, "testListBindingPattern1");
+    }
+
+    @Test
+    public void testListBindingPattern2() {
+        BRunUtil.invoke(result, "testListBindingPattern2");
+    }
+
+    @Test
+    public void testListBindingPattern3() {
+        BRunUtil.invoke(result, "testListBindingPattern3");
+    }
+
+    @Test
+    public void testListBindingPattern4() {
+        BRunUtil.invoke(result, "testListBindingPattern4");
+    }
+
+    @Test
+    public void testListBindingPattern5() {
+        BRunUtil.invoke(result, "testListBindingPattern5");
+    }
+
+    @Test
+    public void testListBindingPattern6() {
+        BRunUtil.invoke(result, "testListBindingPattern6");
+    }
+
+    @Test
+    public void testListBindingPattern7() {
+        BRunUtil.invoke(result, "testListBindingPattern7");
+    }
+
+    @Test
+    public void testListBindingPattern8() {
+        BRunUtil.invoke(result, "testListBindingPattern8");
+    }
+
+    @Test
+    public void testListBindingPattern9() {
+        BRunUtil.invoke(result, "testListBindingPattern9");
+    }
+
+    @Test
+    public void testListBindingPattern10() {
+        BRunUtil.invoke(result, "testListBindingPattern10");
+    }
+
+    @Test
+    public void testListBindingPattern11() {
+        BRunUtil.invoke(result, "testListBindingPattern11");
+    }
+
+    @Test
+    public void testListBindingPattern12() {
+        BRunUtil.invoke(result, "testListBindingPattern12");
+    }
+
+    @Test
+    public void testListBindingPattern13() {
+        BRunUtil.invoke(result, "testListBindingPattern13");
+    }
+
+    @Test
+    public void testListBindingPattern14() {
+        BRunUtil.invoke(result, "testListBindingPattern14");
+    }
+
+    @Test
+    public void testRestBindingPattern1() {
+        BRunUtil.invoke(restMatchPatternResult, "testListBindingPatternWithRest1");
+    }
+
+    @Test
+    public void testRestBindingPattern2() {
+        BRunUtil.invoke(restMatchPatternResult, "testListBindingPatternWithRest2");
+    }
+
+    @Test
+    public void testRestBindingPattern3() {
+        BRunUtil.invoke(restMatchPatternResult, "testListBindingPatternWithRest3");
+    }
+
+    @Test
+    public void testRestBindingPattern4() {
+        BRunUtil.invoke(restMatchPatternResult, "testListBindingPatternWithRest4");
+    }
+
+    @Test
+    public void testRestBindingPattern5() {
+        BRunUtil.invoke(restMatchPatternResult, "testListBindingPatternWithRest5");
+    }
+
+    @Test
+    public void testRestBindingPattern6() {
+        BRunUtil.invoke(restMatchPatternResult, "testListBindingPatternWithRest6");
+    }
+
+    @Test
+    public void testListBindingPatternNegative() {
+        Assert.assertEquals(resultNegative.getErrorCount(), 16);
+
+        int i = -1;
+        BAssertUtil.validateError(resultNegative, ++i, unreachablePattern, 23, 9);
+        BAssertUtil.validateError(resultNegative, ++i, unreachableCode, 28, 5);
+        BAssertUtil.validateError(resultNegative, ++i, "all match patterns should contain the same set of variables",
+                30, 9);
+        BAssertUtil.validateError(resultNegative, ++i, unreachablePattern, 30, 17);
+        BAssertUtil.validateError(resultNegative, ++i, patternNotMatched, 37, 9);
+        BAssertUtil.validateError(resultNegative, ++i, patternNotMatched, 40, 9);
+        BAssertUtil.validateError(resultNegative, ++i, unreachablePattern, 47, 13);
+        BAssertUtil.validateError(resultNegative, ++i, "all match patterns should contain the same set of variables",
+                58, 9);
+        BAssertUtil.validateError(resultNegative, ++i, "all match patterns should contain the same set of variables",
+                65, 9);
+        BAssertUtil.validateError(resultNegative, ++i, patternNotMatched, 65, 35);
+        BAssertUtil.validateError(resultNegative, ++i, unreachablePattern, 79, 22);
+        BAssertUtil.validateError(resultNegative, ++i, unreachablePattern, 83, 9);
+        BAssertUtil.validateError(resultNegative, ++i, patternNotMatched, 85, 9);
+        BAssertUtil.validateError(resultNegative, ++i, patternNotMatched, 87, 9);
+        BAssertUtil.validateError(resultNegative, ++i, patternNotMatched, 89, 9);
+        BAssertUtil.validateError(resultNegative, ++i, unreachablePattern, 89, 9);
+    }
+}
