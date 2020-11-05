dfd3f1192a5de3e78987982b22e2e6b4bdbd7e29
warunalakshitha <warudanu@gmail.com>
2020-11-05 10:49:34 +0530

    Rename XML and JSON classes

---
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/api/utils/JsonUtils.java
@@ -0,0 +1,267 @@
+/*
+ *   Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.ballerina.runtime.api.utils;
+
+import io.ballerina.runtime.JSONGenerator;
+import io.ballerina.runtime.JSONParser;
+import io.ballerina.runtime.api.types.JSONType;
+import io.ballerina.runtime.api.types.MapType;
+import io.ballerina.runtime.api.types.StructureType;
+import io.ballerina.runtime.api.types.Type;
+import io.ballerina.runtime.api.values.BArray;
+import io.ballerina.runtime.api.values.BError;
+import io.ballerina.runtime.api.values.BMap;
+import io.ballerina.runtime.api.values.BString;
+import io.ballerina.runtime.values.ErrorValue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+
+/**
+ * Class @{@link JSONParser} provides APIs to handle json values.
+ *
+ * @since 2.0.0
+ */
+public class JSONUtils {
+
+    /**
+     * Parses the contents in the given {@link InputStream} and returns a json.
+     *
+     * @param in input stream which contains the JSON content
+     * @return JSON structure
+     * @throws BError for any parsing error
+     */
+    public static Object parse(InputStream in) throws BError {
+        return JSONParser.parse(in);
+    }
+
+    /**
+     * Parses the contents in the given {@link InputStream} and returns a json.
+     *
+     * @param in          input stream which contains the JSON content
+     * @param charsetName the character set name of the input stream
+     * @return JSON structure
+     * @throws BError for any parsing error
+     */
+    public static Object parse(InputStream in, String charsetName) throws BError {
+        return JSONParser.parse(in, charsetName);
+    }
+
+    /**
+     * Parses the contents in the given string and returns a json.
+     *
+     * @param jsonStr the string which contains the JSON content
+     * @return JSON structure
+     * @throws BError for any parsing error
+     */
+    public static Object parse(BString jsonStr) throws BError {
+        return JSONParser.parse(jsonStr.getValue());
+    }
+
+    /**
+     * Parses the contents in the given string and returns a json.
+     *
+     * @param jsonStr the string which contains the JSON content
+     * @param mode    the mode to use when processing numeric values
+     * @return JSON   value if parsing is successful
+     * @throws BError for any parsing error
+     */
+    public static Object parse(BString jsonStr, JSONUtils.NonStringValueProcessingMode mode) throws BError {
+        return JSONParser.parse(jsonStr.getValue(), mode);
+    }
+
+    /**
+     * Parses the contents in the given string and returns a json.
+     *
+     * @param jsonStr the string which contains the JSON content
+     * @return JSON structure
+     * @throws BError for any parsing error
+     */
+    public static Object parse(String jsonStr) throws BError {
+        return JSONParser.parse(jsonStr);
+    }
+
+    /**
+     * Parses the contents in the given string and returns a json.
+     *
+     * @param jsonStr the string which contains the JSON content
+     * @param mode    the mode to use when processing numeric values
+     * @return JSON   value if parsing is successful
+     * @throws BError for any parsing error
+     */
+    public static Object parse(String jsonStr, JSONUtils.NonStringValueProcessingMode mode) throws BError {
+        return JSONParser.parse(jsonStr, mode);
+    }
+
+    /**
+     * Parses the contents in the given {@link Reader} and returns a json.
+     *
+     * @param reader reader which contains the JSON content
+     * @param mode   the mode to use when processing numeric values
+     * @return JSON structure
+     * @throws BError for any parsing error
+     */
+    public static Object parse(Reader reader, JSONUtils.NonStringValueProcessingMode mode) throws BError {
+        return JSONParser.parse(reader, mode);
+    }
+
+    /**
+     * Convert {@link BArray} to JSON.
+     *
+     * @param bArray {@link BArray} to be converted to JSON
+     * @return JSON representation of the provided bArray
+     */
+    public static BArray convertArrayToJSON(BArray bArray) {
+        return io.ballerina.runtime.JSONUtils.convertArrayToJSON(bArray);
+    }
+
+    /**
+     * Convert map value to JSON.
+     *
+     * @param map        value {@link BMap} to be converted to JSON
+     * @param targetType the target JSON type to be convert to
+     * @return JSON representation of the provided array
+     */
+    public static Object convertMapToJSON(BMap<BString, ?> map, JSONType targetType) {
+        return io.ballerina.runtime.JSONUtils.convertMapToJSON(map, targetType);
+    }
+
+    /**
+     * Convert a JSON node to a map.
+     *
+     * @param json    JSON to convert
+     * @param mapType MapType which the JSON is converted to.
+     * @return If the provided JSON is of object-type, this method will return a {@link BMap} containing the
+     * values of the JSON object. Otherwise a {@link BError} will be thrown.
+     * @throws BError If conversion fails.
+     */
+    public static BMap<BString, ?> convertJSONToMap(Object json, MapType mapType) throws BError {
+        return io.ballerina.runtime.JSONUtils.jsonToMap(json, mapType);
+    }
+
+    /**
+     * Convert a BJSON to a user defined record.
+     *
+     * @param record       JSON to convert
+     * @param structType Type (definition) of the target record
+     * @return If the provided JSON is of object-type, this method will return a {@link BMap} containing the
+     * values of the JSON object. Otherwise the method will throw a {@link BError}.
+     * @throws BError If conversion fails.
+     */
+    public static BMap<BString, Object> convertJSONToRecord(Object record, StructureType structType) throws BError {
+        return io.ballerina.runtime.JSONUtils.convertJSONToRecord(record, structType);
+    }
+
+    /**
+     * Convert a BJSON to a user given target type.
+     *
+     * @param source       JSON to convert
+     * @param targetType Type (definition) of the target record
+     * @return If the conversion is successes this will return JSON object.
+     * @throws BError If conversion fails.
+     */
+    public static Object convertJSON(Object source, Type targetType) throws BError {
+        return io.ballerina.runtime.JSONUtils.convertJSON(source, targetType);
+    }
+
+    /**
+     * Convert a union type value to a user given target type.
+     *
+     * @param source     Union type value to convert
+     * @param targetType Type (definition).
+     * @return If the conversion is successes this will return JSON object.
+     * @throws BError If conversions fails.
+     */
+    public static Object convertUnionTypeToJSON(Object source, JSONType targetType) throws BError {
+        return io.ballerina.runtime.JSONUtils.convertUnionTypeToJSON(source, targetType);
+    }
+
+    /**
+     * Serialize the JSON constructs to be written out to a given {@link OutputStream}
+     *
+     * @param json JSON construct
+     * @param out  Output source
+     * @throws BError If error occur while serialize json construct.
+     */
+    public void serialize(Object json, OutputStream out) throws BError {
+        try {
+            new JSONGenerator(out, Charset.defaultCharset()).serialize(json);
+        } catch (IOException e) {
+            throw new ErrorValue(StringUtils.fromString(e.getMessage()), e);
+        }
+    }
+
+    /**
+     * Serialize the JSON constructs to be written out to a given {@link OutputStream}
+     *
+     * @param json    JSON construct
+     * @param out     Output source
+     * @param charset Character set
+     * @throws BError If error occur while serialize json construct.
+     */
+    public void serialize(Object json, OutputStream out, Charset charset) throws BError {
+        try {
+            new JSONGenerator(out, charset).serialize(json);
+        } catch (IOException e) {
+            throw new ErrorValue(StringUtils.fromString(e.getMessage()), e);
+        }
+    }
+
+    /**
+     * Serialize the JSON constructs to be written out to a given {@link Writer}
+     *
+     * @param json   JSON construct
+     * @param writer Output writer
+     * @throws BError If error occur while serialize json construct.
+     */
+    public void serialize(Object json, Writer writer) throws BError {
+        try {
+            new JSONGenerator(writer).serialize(json);
+        } catch (IOException e) {
+            throw new ErrorValue(StringUtils.fromString(e.getMessage()), e);
+        }
+    }
+
+    /**
+     * Represents the modes which process numeric values while converting a string to JSON.
+     */
+    public enum NonStringValueProcessingMode {
+        /**
+         * FROM_JSON_STRING converts a numeric value that
+         * - starts with the negative sign (-) and is numerically equal to zero (0) to `-0.0f`
+         * - is syntactically an integer to an `int`
+         * - doesn't belong to the above to decimal.
+         */
+        FROM_JSON_STRING,
+
+        /**
+         * FROM_JSON_FLOAT_STRING converts all numerical values to float.
+         */
+        FROM_JSON_FLOAT_STRING,
+
+        /**
+         * FROM_JSON_DECIMAL_STRING converts all numerical values to decimal.
+         */
+        FROM_JSON_DECIMAL_STRING
+    }
+
+}
---
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/values/XmlSequence.java
@@ -0,0 +1,608 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.ballerina.runtime.values;
+
+import io.ballerina.runtime.CycleUtils;
+import io.ballerina.runtime.api.creators.ErrorCreator;
+import io.ballerina.runtime.api.PredefinedTypes;
+import io.ballerina.runtime.api.utils.StringUtils;
+import io.ballerina.runtime.api.types.XmlNodeType;
+import io.ballerina.runtime.api.values.BLink;
+import io.ballerina.runtime.api.values.BMap;
+import io.ballerina.runtime.api.values.BString;
+import io.ballerina.runtime.api.values.BXml;
+import io.ballerina.runtime.api.values.BXMLSequence;
+import io.ballerina.runtime.types.BArrayType;
+import io.ballerina.runtime.util.BLangConstants;
+import io.ballerina.runtime.util.exceptions.BallerinaErrorReasons;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static io.ballerina.runtime.util.BLangConstants.STRING_EMPTY_VALUE;
+import static io.ballerina.runtime.util.BLangConstants.XML_LANG_LIB;
+
+/**
+ * <p>
+ * {@code BXMLSequence} represents a sequence of {@link XMLItem}s in Ballerina.
+ * </p>
+ * <p>
+ * <i>Note: This is an internal API and may change in future versions.</i>
+ * </p>
+ * 
+ * @since 0.995.0
+ */
+public final class XMLSequence extends XMLValue implements BXMLSequence {
+
+    List<BXml> children;
+
+    /**
+     * Create an empty xml sequence.
+     */
+    public XMLSequence() {
+        children = new ArrayList<>();
+    }
+
+    public XMLSequence(List<BXml> children) {
+        this.children = children;
+    }
+
+    public XMLSequence(BXml child) {
+        this.children = new ArrayList<>();
+        if (!child.isEmpty()) {
+            this.children.add(child);
+        }
+    }
+
+    public List<BXml> getChildrenList() {
+        return children;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XmlNodeType getNodeType() {
+        return XmlNodeType.SEQUENCE;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isEmpty() {
+        return children.isEmpty();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isSingleton() {
+        return children.size() == 1 && children.get(0).isSingleton();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getItemType() {
+        if (isSingleton()) {
+            return children.get(0).getItemType();
+        }
+
+        return XmlNodeType.SEQUENCE.value();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getElementName() {
+        if (isSingleton()) {
+            return children.get(0).getElementName();
+        }
+        return STRING_EMPTY_VALUE.getValue();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getTextValue() {
+        StringBuilder seqTextBuilder = new StringBuilder();
+        for (BXml x : children) {
+            if (x.getNodeType() == XmlNodeType.ELEMENT || x.getNodeType() == XmlNodeType.TEXT) {
+                seqTextBuilder.append(x.getTextValue());
+            }
+        }
+        return seqTextBuilder.toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public BString getAttribute(String localName, String namespace) {
+        if (isSingleton()) {
+            return children.get(0).getAttribute(localName, namespace);
+        }
+
+        return BLangConstants.BSTRING_NULL_VALUE;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public BString getAttribute(String localName, String namespace, String prefix) {
+        if (isSingleton()) {
+            return children.get(0).getAttribute(localName, namespace, prefix);
+        }
+
+        return BLangConstants.BSTRING_NULL_VALUE;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setAttribute(String localName, String namespace, String prefix, String value) {
+        if (this.isFrozen()) {
+            ReadOnlyUtils.handleInvalidUpdate(XML_LANG_LIB);
+        }
+
+        if (isSingleton()) {
+            children.get(0).setAttribute(localName, namespace, prefix, value);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public MapValue<BString, BString> getAttributesMap() {
+        if (isSingleton()) {
+            return (MapValue<BString, BString>) children.get(0).getAttributesMap();
+        }
+
+        return null;
+    }
+
+    @Override
+    @Deprecated
+    public void setAttributes(BMap<BString, ?> attributes) {
+        if (this.isFrozen()) {
+            ReadOnlyUtils.handleInvalidUpdate(XML_LANG_LIB);
+        }
+
+        if (isSingleton()) {
+            children.get(0).setAttributes(attributes);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XMLValue elements() {
+        List elementsSeq = new ArrayList<XMLValue>();
+        for (BXml child : children) {
+            if (child.getNodeType() == XmlNodeType.ELEMENT) {
+                elementsSeq.add(child);
+            }
+        }
+        return new XMLSequence(elementsSeq);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XMLValue elements(String qname) {
+        List<BXml> elementsSeq = new ArrayList<>();
+        String qnameStr = getQname(qname).toString();
+        for (BXml child : children) {
+            if (child.getNodeType() == XmlNodeType.ELEMENT && child.getElementName().equals(qnameStr)) {
+                elementsSeq.add(child);
+            }
+        }
+        return new XMLSequence(elementsSeq);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XMLValue children() {
+        if (children.size() == 1) {
+            return (XMLValue) children.get(0).children();
+        }
+        return new XMLSequence(new ArrayList<>(children));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XMLValue children(String qname) {
+        List<BXml> selected = new ArrayList<>();
+        if (this.children.size() == 1) {
+            BXml bxml = this.children.get(0);
+            return (XMLValue) bxml.children(qname);
+        }
+
+        for (BXml elem : this.children) {
+            XMLSequence elements = (XMLSequence) elem.children().elements(qname);
+            List<BXml> childrenList = elements.getChildrenList();
+            if (childrenList.size() == 1) {
+                selected.add(childrenList.get(0));
+            } else if (childrenList.size() > 1) {
+                selected.addAll(childrenList);
+            }
+        }
+
+        if (selected.size() == 1) {
+            return (XMLValue) selected.get(0);
+        }
+
+        return new XMLSequence(selected);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setChildren(BXml seq) {
+        if (this.isFrozen()) {
+            ReadOnlyUtils.handleInvalidUpdate(XML_LANG_LIB);
+        }
+
+        if (children.size() != 1) {
+            throw ErrorCreator.createError(StringUtils.fromString(("not an " + XmlNodeType.ELEMENT)));
+        }
+
+        children.get(0).setChildren(seq);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @Deprecated
+    public void addChildren(BXml seq) {
+        if (children.size() != 1) {
+            throw ErrorCreator.createError(StringUtils.fromString(("not an " + XmlNodeType.ELEMENT)));
+        }
+
+        children.get(0).addChildren(seq);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XMLValue strip() {
+        List<BXml> elementsSeq = new ArrayList<>();
+        boolean prevChildWasATextNode = false;
+        String prevConsecutiveText = null;
+
+        for (BXml x : children) {
+            XMLValue item = (XMLValue) x;
+            if (item.getNodeType() == XmlNodeType.TEXT) {
+                if (prevChildWasATextNode) {
+                    prevConsecutiveText += x.getTextValue();
+                } else {
+                    prevConsecutiveText = x.getTextValue();
+                }
+                prevChildWasATextNode = true;
+            } else if (item.getNodeType() == XmlNodeType.ELEMENT) {
+                // Previous child was a text node and now we see a element node, we need to add last child to the list
+                if (prevChildWasATextNode && !prevConsecutiveText.trim().isEmpty()) {
+                    elementsSeq.add(new XMLText(prevConsecutiveText));
+                    prevConsecutiveText = null;
+                }
+                prevChildWasATextNode = false;
+                elementsSeq.add(x);
+            }
+        }
+        if (prevChildWasATextNode && !prevConsecutiveText.trim().isEmpty()) {
+            elementsSeq.add(new XMLText(prevConsecutiveText));
+        }
+
+        return new XMLSequence(elementsSeq);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(children);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XMLValue slice(long startIndex, long endIndex) {
+        if (startIndex > this.children.size() || endIndex > this.children.size() || startIndex < -1 || endIndex < -1) {
+            throw ErrorCreator
+                    .createError(
+                            StringUtils.fromString(("index out of range: [" + startIndex + "," + endIndex + "]")));
+        }
+
+        if (startIndex == -1) {
+            startIndex = 0;
+        }
+
+        if (endIndex == -1) {
+            endIndex = children.size();
+        }
+
+        if (startIndex == endIndex) {
+            return new XMLSequence();
+        }
+
+        if (startIndex > endIndex) {
+            throw ErrorCreator
+                    .createError(StringUtils.fromString(("invalid indices: " + startIndex + " < " + endIndex)));
+        }
+
+        int j = 0;
+        List<BXml> elementsSeq = new ArrayList<>();
+        for (int i = (int) startIndex; i < endIndex; i++) {
+            elementsSeq.add(j++, children.get(i));
+        }
+
+        return new XMLSequence(elementsSeq);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XMLValue descendants(List<String> qnames) {
+        List<BXml> descendants = new ArrayList<>();
+        for (BXml child : children) {
+            if (child.getNodeType() == XmlNodeType.ELEMENT) {
+                XMLItem element = (XMLItem) child;
+                String name = element.getQName().toString();
+                if (qnames.contains(name)) {
+                    descendants.add(element);
+                }
+                addDescendants(descendants, element, qnames);
+            }
+        }
+
+        return new XMLSequence(descendants);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object value() {
+        BArrayType bArrayType = new BArrayType(PredefinedTypes.TYPE_XML);
+        return new ArrayValueImpl(children.toArray(), bArrayType);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        try {
+            return stringValue(null);
+        } catch (Throwable t) {
+            handleXmlException("failed to get xml as string: ", t);
+        }
+        return BLangConstants.STRING_NULL_VALUE;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param parent The link to the parent node
+     */
+    @Override
+    public String stringValue(BLink parent) {
+        try {
+            StringBuilder sb = new StringBuilder();
+            for (BXml child : children) {
+                sb.append(child.stringValue(new CycleUtils.Node(this, parent)));
+            }
+            return sb.toString();
+        } catch (Throwable t) {
+            handleXmlException("failed to get xml as string: ", t);
+        }
+        return BLangConstants.STRING_NULL_VALUE;
+    }
+
+    @Override
+    public String informalStringValue(BLink parent) {
+        return "`" + stringValue(parent) + "`";
+    }
+
+    @Override
+    public String expressionStringValue(BLink parent) {
+        return "xml`" + toString() + "`";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object copy(Map<Object, Object> refs) {
+        if (isFrozen()) {
+            return this;
+        }
+
+        if (refs.containsKey(this)) {
+            return refs.get(this);
+        }
+
+        ArrayList<BXml> copiedChildrenList = new ArrayList<>(children.size());
+        XMLSequence copiedSeq = new XMLSequence(copiedChildrenList);
+        refs.put(this, copiedSeq);
+        for (BXml child : children) {
+            copiedChildrenList.add((XMLValue) child.copy(refs));
+        }
+
+        return copiedSeq;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object frozenCopy(Map<Object, Object> refs) {
+        XMLSequence copy = (XMLSequence) copy(refs);
+        if (!copy.isFrozen()) {
+            copy.freezeDirect();
+        }
+        return copy;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public XMLValue getItem(int index) {
+        try {
+            if (index >= this.children.size()) {
+                return new XMLSequence();
+            }
+            return (XMLValue) this.children.get(index);
+        } catch (Exception e) {
+            throw ErrorCreator.createError(BallerinaErrorReasons.XML_OPERATION_ERROR,
+                                           StringUtils.fromString(e.getMessage()));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int size() {
+        return this.children.size();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void build() {
+        for (BXml child : children) {
+            child.build();
+        }
+    }
+
+    @Override
+    protected void setAttributesOnInitialization(BMap<BString, ?> attributes) {
+        if (isSingleton()) {
+            ((XMLValue) children.get(0)).setAttributesOnInitialization(attributes);
+        }
+    }
+
+    @Override
+    protected void setAttributeOnInitialization(String localName, String namespace, String prefix, String value) {
+        ((XMLValue) children.get(0)).setAttributeOnInitialization(localName, namespace, prefix, value);
+    }
+
+    @Override
+    public void removeAttribute(String qname) {
+        if (this.isFrozen()) {
+            ReadOnlyUtils.handleInvalidUpdate(XML_LANG_LIB);
+        }
+
+        if (children.size() != 1) {
+            throw ErrorCreator.createError(StringUtils.fromString(("not an " + XmlNodeType.ELEMENT)));
+        }
+
+        children.get(0).removeAttribute(qname);
+    }
+
+    @Override
+    @Deprecated
+    public void removeChildren(String qname) {
+        if (this.isFrozen()) {
+            ReadOnlyUtils.handleInvalidUpdate(XML_LANG_LIB);
+        }
+
+        if (children.size() != 1) {
+            throw ErrorCreator.createError(StringUtils.fromString(("not an " + XmlNodeType.ELEMENT)));
+        }
+
+        children.get(0).removeChildren(qname);
+    }
+
+    @Override
+    public void freezeDirect() {
+        this.type = ReadOnlyUtils.setImmutableTypeAndGetEffectiveType(this.type);
+        for (BXml elem : children) {
+            elem.freezeDirect();
+        }
+    }
+
+    @Override
+    public boolean isFrozen() {
+        if (this.type.isReadOnly()) {
+            return true;
+        }
+
+        for (BXml child : this.children) {
+            if (!child.isFrozen()) {
+                return false;
+            }
+        }
+        freezeDirect();
+        return true;
+    }
+
+    @Override
+    public IteratorValue getIterator() {
+        return new IteratorValue() {
+            Iterator<BXml> iterator = children.iterator();
+
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            @Override
+            public Object next() {
+                return iterator.next();
+            }
+        };
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj instanceof XMLSequence) {
+            XMLSequence that = (XMLSequence) obj;
+            return that.children.equals(this.children);
+        }
+        if (obj instanceof XMLItem) {
+            return this.children.size() == 1 && this.children.get(0).equals(obj);
+        }
+        return false;
+    }
+}
