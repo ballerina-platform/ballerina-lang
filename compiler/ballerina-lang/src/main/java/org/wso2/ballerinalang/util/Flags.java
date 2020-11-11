b318635414e27b52ab65d126e1135aa71c0fc56e
chiranSachintha <chiransachintha1994@gmail.com>
2020-11-11 19:51:41 +0530

    Introduce flag `included` for included record parameters

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/util/Flags.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/util/Flags.java
@@ -56,6 +56,7 @@ public class Flags {
     public static final int DISTINCT = PARAMETERIZED << 1;
     public static final int CLASS = DISTINCT << 1;
     public static final int ISOLATED = CLASS << 1;
+    public static final int INCLUDED = ISOLATED << 1;
 
     public static int asMask(Set<Flag> flagSet) {
         int mask = 0;
@@ -145,6 +146,8 @@ public class Flags {
                 case ISOLATED:
                     mask |= ISOLATED;
                     break;
+                case INCLUDED:
+                    mask |= INCLUDED;
             }
         }
         return mask;
@@ -232,6 +235,9 @@ public class Flags {
                     break;
                 case ISOLATED:
                     flagVal = ISOLATED;
+                    break;
+                case INCLUDED:
+                    flagVal = INCLUDED;
                     break;
                 default:
                     continue;
