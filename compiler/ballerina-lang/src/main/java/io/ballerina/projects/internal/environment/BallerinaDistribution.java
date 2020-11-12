1b697ba5c1f5ff845d18900b76d1b34bde987748
sameerajayasoma <sameera.madushan@gmail.com>
2020-11-11 17:20:19 -0800

    Add ballerina distribution validation logic

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/environment/BallerinaDistribution.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/environment/BallerinaDistribution.java
@@ -18,11 +18,13 @@
 package io.ballerina.projects.internal.environment;
 
 import io.ballerina.projects.Bootstrap;
+import io.ballerina.projects.ProjectException;
 import io.ballerina.projects.environment.Environment;
 import io.ballerina.projects.environment.GlobalPackageCache;
 import io.ballerina.projects.repos.BallerinaDistributionRepository;
 import org.wso2.ballerinalang.compiler.util.CompilerContext;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -34,13 +36,14 @@ import java.nio.file.Paths;
  */
 public final class BallerinaDistribution {
     private static final String BALLERINA_HOME_KEY = "ballerina.home";
+    private static final String langLibBootstrapPhase = System.getProperty("BOOTSTRAP_LANG_LIB");
 
     private final Path ballerinaHomeDirPath;
     private final BallerinaDistributionRepository distributionRepository;
 
     private BallerinaDistribution(Environment environment, Path ballerinaHomeDirPath) {
         this.ballerinaHomeDirPath = ballerinaHomeDirPath;
-        this.distributionRepository = new BallerinaDistributionRepository(environment, this.ballerinaHomeDirPath);
+        this.distributionRepository = BallerinaDistributionRepository.from(environment, this.ballerinaHomeDirPath);
     }
 
     public static BallerinaDistribution from(Environment environment) {
@@ -61,14 +64,27 @@ public final class BallerinaDistribution {
     }
 
     public void loadLangLibPackages(CompilerContext compilerContext, GlobalPackageCache packageCache) {
-        String bootstrapLangLibName = System.getProperty("BOOTSTRAP_LANG_LIB");
-        if (bootstrapLangLibName == null) {
+        if (langLibBootstrapPhase == null) {
             Bootstrap bootstrap = new Bootstrap(new LangLibResolver(distributionRepository, packageCache));
             bootstrap.loadLangLibSymbols(compilerContext);
         }
     }
 
     private static void validateBallerinaHomeDir(Path ballerinaHomeDirPath) {
-        // TODO
+        if (Files.notExists(ballerinaHomeDirPath)) {
+            throw new ProjectException("Ballerina distribution directory does not exists in `" +
+                    ballerinaHomeDirPath + "'");
+        }
+
+        if (!Files.isDirectory(ballerinaHomeDirPath)) {
+            throw new ProjectException("Invalid Ballerina distribution directory: " + ballerinaHomeDirPath);
+        }
+
+        if (langLibBootstrapPhase != null) {
+            return;
+        }
+
+        // TODO Validate whether the runtime jar exists this validation
+        // TODO At the moment runtime jar is loaded from the class path during the Gradle build. It is wrong!
     }
 }
