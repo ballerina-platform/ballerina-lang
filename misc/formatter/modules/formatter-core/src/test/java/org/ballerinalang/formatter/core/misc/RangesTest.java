a7de036d9d594b19849a019844827926f53b2ce5
sanjana <anjetnet@gmail.com>
2020-11-22 15:19:27 +0530

    Add more range test cases

--- misc/formatter/modules/formatter-core/src/test/java/org/ballerinalang/formatter/core/misc/RangesTest.java
+++ misc/formatter/modules/formatter-core/src/test/java/org/ballerinalang/formatter/core/misc/RangesTest.java
@@ -46,13 +46,46 @@ public class RangesTest extends RangeFormatter {
 
         return new Object[][]{
                 {
-                        new String[] {"misc/ranges", "ranges_1.bal"}, new int[][] {new int[] {5, 30, 9, 34},
-                        new int[] {12, 31, 16, 29},
-                        new int[] {16, 20, 22, 38},
-                        new int[] {24, 10, 31, 36},
-                        new int[] {31, 53, 31, 88},
-                        new int[] {37, 26, 37, 33},
+                        new String[] {"misc/ranges", "ranges_1.bal"}, new int[][] {
+                            new int[] {5, 30, 9, 34},
+                            new int[] {12, 31, 16, 29},
+                            new int[] {16, 20, 22, 38},
+                            new int[] {24, 10, 31, 36},
+                            new int[] {31, 53, 31, 88},
+                            new int[] {37, 26, 37, 33},
                         }
+                },
+                {
+                        new String[] {"misc/ranges", "ranges_2.bal"}, new int[][] {
+                                new int[] {0, 1, 1, 0},
+                                new int[] {2, 52, 14, 12},
+                                new int[] {7, 22, 25, 8},
+                                new int[] {27, 18, 27, 94},
+                                new int[] {35, 12, 31, 88},
+                                new int[] {32, 7, 32, 25},
+                                new int[] {34, 12, 40, 7},
+                        }
+                },
+                {
+                        new String[] {"misc/ranges", "ranges_3.bal"}, new int[][] {
+                        new int[] {0, 6, 9, 16},
+                        new int[] {5, 15, 27, 8},
+                        new int[] {26, 37, 35, 30},
+                        new int[] {45, 4, 48, 12},
+                        new int[] {48, 4, 48, 44},
+                        }
+                },
+                {
+                        new String[] {"misc/ranges", "ranges_4.bal"}, new int[][] {
+                        new int[] {2, 14, 12, 25},
+                        new int[] {9, 15, 23, 8},
+                        new int[] {18, 8, 22, 1},
+                        }
+                },
+                {
+                        new String[] {"misc/ranges", "ranges_5.bal"}, new int[][] {
+                        new int[] {1, 1, 129, 1},
+                }
                 },
         };
     }
