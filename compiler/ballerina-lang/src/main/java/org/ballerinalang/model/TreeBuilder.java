a7218698e12c524f5953ac7ca46c025a3635ca3f
chiranSachintha <chiransachintha1994@gmail.com>
2020-11-24 19:53:24 +0530

    Fix review suggestions

--- compiler/ballerina-lang/src/main/java/org/ballerinalang/model/TreeBuilder.java
+++ compiler/ballerina-lang/src/main/java/org/ballerinalang/model/TreeBuilder.java
@@ -450,10 +450,6 @@ public class TreeBuilder {
         return new BLangRecordLiteral();
     }
 
-    public static BLangRecordLiteral createBLangRecordLiteralNode() {
-        return new BLangRecordLiteral();
-    }
-
     public static RecordLiteralNode.RecordKeyValueFieldNode createRecordKeyValue() {
         return new BLangRecordLiteral.BLangRecordKeyValueField();
     }
