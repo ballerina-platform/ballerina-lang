40937954c9d43a352e8725d73703cfcbd361998a
KRVPerera <rukshan.viduranga@gmail.com>
2020-12-08 03:32:14 +0530

    Resolve conflict with master

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/model/SymbolTable.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/model/SymbolTable.java
@@ -128,6 +128,7 @@ public class SymbolTable {
     public final BType handleType = new BHandleType(TypeTags.HANDLE, null);
     public final BTypedescType typeDesc = new BTypedescType(this.anyType, null);
     public final BType readonlyType = new BReadonlyType(TypeTags.READONLY, null);
+    public final BType intStringFloatOrBoolean = BUnionType.create(null, intType, stringType, floatType, booleanType);
 
     public final BType semanticError = new BType(TypeTags.SEMANTIC_ERROR, null);
     public final BType nullSet = new BType(TypeTags.NULL_SET, null);
