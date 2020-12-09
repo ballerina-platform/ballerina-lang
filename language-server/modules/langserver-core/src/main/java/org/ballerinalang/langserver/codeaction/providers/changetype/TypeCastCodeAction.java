7650d9b6049e3dbc482e10f54ead45c5151a6dd3
Rasika <info.rasika@gmail.com>
2020-12-09 14:30:10 +0530

    Add type-cast code-action support for module vars
    Signed-off-by: Rasika <info.rasika@gmail.com>

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/codeaction/providers/changetype/TypeCastCodeAction.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/codeaction/providers/changetype/TypeCastCodeAction.java
@@ -16,9 +16,12 @@
 package org.ballerinalang.langserver.codeaction.providers.changetype;
 
 import io.ballerina.compiler.syntax.tree.ExpressionNode;
+import io.ballerina.compiler.syntax.tree.ModuleVariableDeclarationNode;
+import io.ballerina.compiler.syntax.tree.Node;
 import io.ballerina.compiler.syntax.tree.SyntaxKind;
 import io.ballerina.compiler.syntax.tree.VariableDeclarationNode;
 import org.ballerinalang.annotation.JavaSPIService;
+import org.ballerinalang.langserver.codeaction.CodeActionUtil;
 import org.ballerinalang.langserver.codeaction.providers.AbstractCodeActionProvider;
 import org.ballerinalang.langserver.common.constants.CommandConstants;
 import org.ballerinalang.langserver.common.utils.CommonUtil;
@@ -33,7 +36,6 @@ import org.eclipse.lsp4j.TextEdit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
 
 /**
@@ -50,22 +52,37 @@ public class TypeCastCodeAction extends AbstractCodeActionProvider {
     @Override
     public List<CodeAction> getDiagBasedCodeActions(Diagnostic diagnostic,
                                                     CodeActionContext context) {
-        if (!(diagnostic.getMessage().toLowerCase(Locale.ROOT).contains(CommandConstants.INCOMPATIBLE_TYPES))) {
+        if (!(diagnostic.getMessage().contains(CommandConstants.INCOMPATIBLE_TYPES))) {
             return Collections.emptyList();
         }
         PositionDetails positionDetails = context.positionDetails();
-        if (positionDetails.matchedNode().kind() != SyntaxKind.LOCAL_VAR_DECL) {
+        if (positionDetails.matchedNode().kind() != SyntaxKind.LOCAL_VAR_DECL &&
+                positionDetails.matchedNode().kind() != SyntaxKind.MODULE_VAR_DECL) {
             return Collections.emptyList();
         }
-        Optional<ExpressionNode> initializer = ((VariableDeclarationNode) positionDetails.matchedNode()).initializer();
+        Optional<ExpressionNode> initializer = getInitializer(positionDetails.matchedNode());
         if (initializer.isEmpty() || initializer.get().kind() == SyntaxKind.TYPE_CAST_EXPRESSION) {
             return Collections.emptyList();
         }
         Position editPos = CommonUtil.toPosition(initializer.get().lineRange().startLine());
-        String editText = "<" + positionDetails.matchedExprType().signature() + "> ";
         List<TextEdit> edits = new ArrayList<>();
+        Optional<String> typeName = CodeActionUtil.getPossibleType(positionDetails.matchedExprType(), edits, context);
+        if (typeName.isEmpty()) {
+            return Collections.emptyList();
+        }
+        String editText = "<" + typeName.get() + "> ";
         edits.add(new TextEdit(new Range(editPos, editPos), editText));
         String commandTitle = CommandConstants.ADD_TYPE_CAST_TITLE;
         return Collections.singletonList(createQuickFixCodeAction(commandTitle, edits, context.fileUri()));
+    }
+
+    private Optional<ExpressionNode> getInitializer(Node node) {
+        if (node.kind() == SyntaxKind.LOCAL_VAR_DECL) {
+            return ((VariableDeclarationNode) node).initializer();
+        } else if (node.kind() == SyntaxKind.MODULE_VAR_DECL) {
+            return ((ModuleVariableDeclarationNode) node).initializer();
+        } else {
+            return Optional.empty();
+        }
     }
 }
