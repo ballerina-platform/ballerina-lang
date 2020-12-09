7bd6b3dafc07361fc4fbca0df4cc791053fc9f2c
Kishanthan <kshanth2101@gmail.com>
2020-12-10 01:13:26 +0530

    Enable SymbolBIRTest

--- tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/SymbolBIRTest.java
+++ tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/SymbolBIRTest.java
@@ -64,7 +64,7 @@ public class SymbolBIRTest {
         }
     }
 
-    @Test(enabled = false)
+    @Test
     public void testSymbolLookupInBIR() {
         Project project = BCompileUtil.loadProject("test-src/symbol_lookup_with_imports_test.bal");
         Package currentPackage = project.currentPackage();
