e4682b3f6f9b1fbeb48a2ffb6874a973e050f1eb
Tharik Kanaka <tharik.kanaka@gmail.com>
2020-12-14 22:52:48 +0530

    Remove ballerina tool reference in runtime exports

--- bvm/ballerina-runtime/src/main/java/module-info.java
+++ bvm/ballerina-runtime/src/main/java/module-info.java
@@ -38,7 +38,7 @@ module io.ballerina.runtime {
     exports io.ballerina.runtime.internal to io.ballerina.testerina.core, io.ballerina.testerina.runtime,
             io.ballerina.lang, io.ballerina.lang.map, io.ballerina.lang.test, io.ballerina.lang.array,
             io.ballerina.lang.table, io.ballerina.lang.value, io.ballerina.lang.xml, ballerina.debug.adapter.core,
-            io.ballerina.tool, io.ballerina.cli;
+            io.ballerina.cli;
     exports io.ballerina.runtime.internal.commons to io.ballerina.lang.value;
     exports io.ballerina.runtime.internal.launch to io.ballerina.testerina.runtime, io.ballerina.packerina,
             ballerina.test.listener, io.ballerina.cli;
@@ -46,7 +46,7 @@ module io.ballerina.runtime {
             io.ballerina.lang.array, io.ballerina.lang.error, io.ballerina.lang.internal, io.ballerina.lang.map,
             io.ballerina.lang.table, io.ballerina.lang.transaction, io.ballerina.lang.value, io.ballerina.lang.xml,
             io.ballerina.log.api, io.ballerina.testerina.core, io.ballerina.testerina.runtime;
-    exports io.ballerina.runtime.internal.util to io.ballerina.testerina.runtime, io.ballerina.lang, io.ballerina.tool,
+    exports io.ballerina.runtime.internal.util to io.ballerina.testerina.runtime, io.ballerina.lang,
             io.ballerina.lang.integer, io.ballerina.lang.floatingpoint, io.ballerina.lang.array,
             io.ballerina.lang.table, io.ballerina.java, io.ballerina.lang.map, io.ballerina.lang.string,
             io.ballerina.lang.xml, io.ballerina.lang.bool, io.ballerina.lang.error, io.ballerina.lang.internal,
