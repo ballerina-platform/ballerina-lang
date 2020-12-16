f8c9b0b2ce39e24fc81df43389975abc0b98a495
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2020-12-16 12:39:21 +0530

    Improve langlib invocations using semantic API

--- misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/evaluation/engine/MethodCallExpressionEvaluator.java
+++ misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/evaluation/engine/MethodCallExpressionEvaluator.java
@@ -22,8 +22,10 @@ import com.sun.jdi.ReferenceType;
 import com.sun.jdi.Value;
 import io.ballerina.compiler.api.SemanticModel;
 import io.ballerina.compiler.api.symbols.ClassSymbol;
+import io.ballerina.compiler.api.symbols.FunctionSymbol;
+import io.ballerina.compiler.api.symbols.FunctionTypeSymbol;
 import io.ballerina.compiler.api.symbols.MethodSymbol;
-import io.ballerina.compiler.api.symbols.Symbol;
+import io.ballerina.compiler.api.symbols.ModuleSymbol;
 import io.ballerina.compiler.api.symbols.SymbolKind;
 import io.ballerina.compiler.syntax.tree.MethodCallExpressionNode;
 import io.ballerina.tools.text.LinePosition;
@@ -39,10 +41,15 @@ import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.ballerinalang.debugadapter.evaluation.engine.FunctionInvocationExpressionEvaluator.modifyName;
 import static org.ballerinalang.debugadapter.evaluation.engine.InvocationArgProcessor.generateNamedArgs;
+import static org.ballerinalang.debugadapter.evaluation.utils.LangLibUtils.LANG_LIB_PACKAGE_PREFIX;
+import static org.ballerinalang.debugadapter.evaluation.utils.LangLibUtils.LANG_LIB_VALUE;
+import static org.ballerinalang.debugadapter.evaluation.utils.LangLibUtils.getAssociatedLangLibName;
+import static org.ballerinalang.debugadapter.evaluation.utils.LangLibUtils.getLangLibDefinition;
+import static org.ballerinalang.debugadapter.evaluation.utils.LangLibUtils.getLangLibFunctionDefinition;
+import static org.ballerinalang.debugadapter.evaluation.utils.LangLibUtils.getQualifiedLangLibClassName;
 import static org.ballerinalang.debugadapter.evaluation.utils.LangLibUtils.loadLangLibMethod;
 
 /**
@@ -69,50 +76,34 @@ public class MethodCallExpressionEvaluator extends Evaluator {
     @Override
     public BExpressionValue evaluate() throws EvaluationException {
         try {
+            // If the static type of expression is a subtype of object, and the object type includes a method named
+            // method-name, then the method-call-expr is executed by calling that method on v.
+            //
+            // Otherwise, the method-call-expr will be turned into a call to a function in the lang library M,
+            // where M is selected as follows.
+            //  - If the static type of expression is a subtype of some basic type with identifier B, and the module
+            //      lang.B contains a function method-name then M is B. The identifier for a basic type is the reserved
+            //      identifier used in type descriptors for subtypes of that basic type, as listed in the Lang library
+            //      section.
+            //  - Otherwise, if the static type of expression is xml:Text and the module lang.string contains a
+            //      function method-name, then M is string, and the result of evaluating expression is implicitly
+            //      converted to a string before the function is called.
+            //  - Otherwise, M is value.
+
+            Value invocationResult = null;
             BExpressionValue result = objectExpressionEvaluator.evaluate();
             BVariable resultVar = VariableFactory.getVariable(context, result.getJdiValue());
-            Value invocationResult = null;
-            boolean isFoundObjectMethod = false;
-            // If the expression result is an object, search for object methods.
-            if (resultVar.getBType() == BVariableType.OBJECT) {
-                try {
-                    String className = resultVar.getDapVariable().getValue();
-                    Optional<ClassSymbol> classDef = findClassDefWithinModule(className);
-                    if (classDef.isEmpty()) {
-                        throw new EvaluationException(String.format(EvaluationExceptionKind.CLASS_NOT_FOUND.getString(),
-                                className));
-                    }
 
-                    Optional<MethodSymbol> objectMethodDef = findObjectMethodInClass(classDef.get(), methodName);
-                    if (objectMethodDef.isEmpty()) {
-                        throw new EvaluationException(
-                                String.format(EvaluationExceptionKind.OBJECT_METHOD_NOT_FOUND.getString(),
-                                        syntaxNode.methodName().toString().trim(), className));
-                    }
-
-                    isFoundObjectMethod = true;
-                    GeneratedInstanceMethod objectMethod = getObjectMethodByName(resultVar, methodName);
-                    objectMethod.setNamedArgValues(generateNamedArgs(context, methodName, objectMethodDef.get().
-                            typeDescriptor(), argEvaluators));
-                    invocationResult = objectMethod.invoke();
-                } catch (EvaluationException e) {
-                    // If the object method is not found, we have to ignore the Evaluation Exception and try find any
-                    // matching lang library functions.
-                    if (isFoundObjectMethod) {
-                        throw e;
-                    }
-                }
+            // If the expression result is an object, try invoking as an object method invocation.
+            if (result.getType() == BVariableType.OBJECT) {
+                invocationResult = invokeObjectMethod(resultVar);
             }
-            // Otherwise, search for matching lang-lib methods.
+
+            // Otherwise, try matching lang-lib methods.
             if (invocationResult == null) {
-                GeneratedStaticMethod langLibMethod = loadLangLibMethod(context, result, methodName);
-                argEvaluators.add(0, new AbstractMap.SimpleEntry<>("", objectExpressionEvaluator));
-                // Todo - IMPORTANT Enable after having a way to resolve lang lib method definition signature
-                //  information.
-                // langLibMethod.setNamedArgValues(validateAndProcessArguments(context, methodName, argEvaluators));
-                langLibMethod.setArgEvaluators(argEvaluators);
-                invocationResult = langLibMethod.invoke();
+                invocationResult = invokeLangLibMethod(result);
             }
+
             return new BExpressionValue(context, invocationResult);
         } catch (EvaluationException e) {
             throw e;
@@ -122,18 +113,90 @@ public class MethodCallExpressionEvaluator extends Evaluator {
         }
     }
 
+    private Value invokeObjectMethod(BVariable resultVar) throws EvaluationException {
+        boolean isFoundObjectMethod = false;
+        try {
+            String className = resultVar.getDapVariable().getValue();
+            Optional<ClassSymbol> classDef = findClassDefWithinModule(className);
+            if (classDef.isEmpty()) {
+                throw new EvaluationException(String.format(EvaluationExceptionKind.CLASS_NOT_FOUND.getString(),
+                        className));
+            }
+
+            Optional<MethodSymbol> objectMethodDef = findObjectMethodInClass(classDef.get(), methodName);
+            if (objectMethodDef.isEmpty()) {
+                throw new EvaluationException(
+                        String.format(EvaluationExceptionKind.OBJECT_METHOD_NOT_FOUND.getString(),
+                                syntaxNode.methodName().toString().trim(), className));
+            }
+
+            isFoundObjectMethod = true;
+            GeneratedInstanceMethod objectMethod = getObjectMethodByName(resultVar, methodName);
+            objectMethod.setNamedArgValues(generateNamedArgs(context, methodName, objectMethodDef.get().
+                    typeDescriptor(), argEvaluators));
+            return objectMethod.invoke();
+        } catch (EvaluationException e) {
+            // If the object method is not found, we have to ignore the Evaluation Exception and try find any
+            // matching lang library functions.
+            if (isFoundObjectMethod) {
+                throw e;
+            }
+        }
+        return null;
+    }
+
+    private Value invokeLangLibMethod(BExpressionValue resultVar) throws EvaluationException {
+
+        FunctionSymbol langLibFunctionDef = null;
+        GeneratedStaticMethod langLibMethod = null;
+
+        // Tries to use the dedicated lang library functions.
+        String langLibName = getAssociatedLangLibName(resultVar.getType());
+        Optional<ModuleSymbol> langLibDef = getLangLibDefinition(context, langLibName);
+        if (langLibDef.isPresent()) {
+            Optional<FunctionSymbol> functionDef = getLangLibFunctionDefinition(context, langLibDef.get(), methodName);
+            if (functionDef.isPresent()) {
+                String langLibCls = getQualifiedLangLibClassName(context, langLibName);
+                langLibFunctionDef = functionDef.get();
+                langLibMethod = loadLangLibMethod(context, resultVar, langLibCls, methodName);
+            }
+        }
+
+        // Tries to use "value" lang library functions.
+        if (langLibMethod == null) {
+            Optional<ModuleSymbol> valueLibDef = getLangLibDefinition(context, LANG_LIB_VALUE);
+            if (valueLibDef.isEmpty()) {
+                throw new EvaluationException(String.format(EvaluationExceptionKind.LANG_LIB_NOT_FOUND.getString(),
+                        LANG_LIB_PACKAGE_PREFIX + langLibName + ", " + LANG_LIB_PACKAGE_PREFIX + LANG_LIB_VALUE));
+            }
+
+            Optional<FunctionSymbol> functionDef = getLangLibFunctionDefinition(context, valueLibDef.get(), methodName);
+            if (functionDef.isEmpty()) {
+                throw new EvaluationException(String.format(EvaluationExceptionKind.LANG_LIB_METHOD_NOT_FOUND.
+                        getString(), methodName, langLibName));
+            }
+
+            String langLibCls = getQualifiedLangLibClassName(context, LANG_LIB_VALUE);
+            langLibFunctionDef = functionDef.get();
+            langLibMethod = loadLangLibMethod(context, resultVar, langLibCls, methodName);
+        }
+
+        argEvaluators.add(0, new AbstractMap.SimpleEntry<>("", objectExpressionEvaluator));
+        FunctionTypeSymbol functionTypeDesc = langLibFunctionDef.typeDescriptor();
+        Map<String, Value> argValueMap = generateNamedArgs(context, methodName, functionTypeDesc, argEvaluators);
+        langLibMethod.setNamedArgValues(argValueMap);
+        return langLibMethod.invoke();
+    }
+
     private Optional<ClassSymbol> findClassDefWithinModule(String className) {
         SemanticModel semanticContext = context.getDebugCompiler().getSemanticInfo();
         LinePosition position = LinePosition.from(context.getLineNumber(), 0);
-        List<Symbol> classMatches = semanticContext.visibleSymbols(context.getFileNameWithExt().get(), position)
+
+        return semanticContext.visibleSymbols(context.getFileNameWithExt().get(), position)
                 .stream()
                 .filter(symbol -> symbol.kind() == SymbolKind.CLASS && modifyName(symbol.name()).equals(className))
-                .collect(Collectors.toList());
-        if (classMatches.isEmpty()) {
-            return Optional.empty();
-        }
-
-        return Optional.ofNullable((ClassSymbol) classMatches.get(0));
+                .findFirst()
+                .map(symbol -> (ClassSymbol) symbol);
     }
 
     private Optional<MethodSymbol> findObjectMethodInClass(ClassSymbol classDef, String methodName) {
