877674d87fbbf61456d6af267ae94bedbdcc2268
warunalakshitha <warudanu@gmail.com>
2020-12-16 20:47:51 +0530

    Enable dump bir and dump file file options

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/cmd/BuildCommand.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/cmd/BuildCommand.java
@@ -118,6 +118,12 @@ public class BuildCommand implements BLauncherCmd {
     @CommandLine.Parameters
     private List<String> argList;
 
+    @CommandLine.Option(names = "--dump-bir", hidden = true)
+    private boolean dumpBIR;
+
+    @CommandLine.Option(names = "--dump-bir-file", hidden = true)
+    private String dumpBIRFile;
+
     @CommandLine.Option(names = {"--help", "-h"}, hidden = true)
     private boolean helpFlag;
 
@@ -249,6 +255,8 @@ public class BuildCommand implements BLauncherCmd {
                 .skipTests(skipTests)
                 .testReport(testReport)
                 .observabilityIncluded(observabilityIncluded)
+                .dumpBir(dumpBIR)
+                .dumpBirFile(dumpBIRFile)
                 .build();
     }
 
