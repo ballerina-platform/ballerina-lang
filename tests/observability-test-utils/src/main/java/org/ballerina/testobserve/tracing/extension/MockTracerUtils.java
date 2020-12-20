007e574ef0c2ed537bcca9b7a3d14995aaa01a63
sachiniSam <sachiniprof@gmail.com>
2020-12-20 09:15:26 +0530

    Add checkpoint integration test for main function

--- tests/observability-test-utils/src/main/java/org/ballerina/testobserve/tracing/extension/MockTracerUtils.java
+++ tests/observability-test-utils/src/main/java/org/ballerina/testobserve/tracing/extension/MockTracerUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -42,7 +42,8 @@ public class MockTracerUtils {
                             mockSpan.context().traceId(),
                             mockSpan.context().spanId(),
                             mockSpan.parentId(),
-                            mockSpan.tags()))
+                            mockSpan.tags(),
+                            mockSpan.logEntries()))
                     .collect(Collectors.toList());
         }
         return JsonUtils.parse(new Gson().toJson(mockSpans));
