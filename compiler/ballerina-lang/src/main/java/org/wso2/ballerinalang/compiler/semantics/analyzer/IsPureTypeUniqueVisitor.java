ba547b1cc21be945f431316cf6f9051acde186ac
KRVPerera <rukshan.viduranga@gmail.com>
2020-12-21 12:27:39 +0530

    Add readonly as pure type for `==` operator

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/IsPureTypeUniqueVisitor.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/IsPureTypeUniqueVisitor.java
@@ -82,7 +82,7 @@ public class IsPureTypeUniqueVisitor implements UniqueTypeVisitor<Boolean> {
     }
 
     private boolean isPureType(BType type) {
-        return type.tag == TypeTags.ERROR || isAnydata(type);
+        return type.tag == TypeTags.ERROR || type.tag == TypeTags.READONLY || isAnydata(type);
     }
 
     @Override
