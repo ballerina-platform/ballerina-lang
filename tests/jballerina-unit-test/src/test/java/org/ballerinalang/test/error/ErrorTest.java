ffc53976feb2abf313a31f0cbb0c08c0b3fdc4b5
KRVPerera <rukshan.viduranga@gmail.com>
2021-01-07 11:32:02 +0530

    Fix test case updates for line numbers

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/error/ErrorTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/error/ErrorTest.java
@@ -361,7 +361,7 @@ public class ErrorTest {
         String message = expectedException.getMessage();
         Assert.assertEquals(message, "error: array index out of range: index: 4, size: 2\n\t" +
                 "at ballerina.lang.array.1_1_0:slice(array.bal:126)\n\t" +
-                "   error_test:testStackTraceInNative(error_test.bal:276)");
+                "   error_test:testStackTraceInNative(error_test.bal:277)");
     }
 
     @Test
