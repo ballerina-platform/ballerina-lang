0e2e57b55179a3d2d33f55f891bae3aa2fe37b45
Anjana Supun <anjanas@wso2.com>
2021-01-09 09:49:31 +0530

    Refacotr Visitor

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/toml/C2CSchemaVisitor.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/toml/C2CSchemaVisitor.java
@@ -2,9 +2,12 @@ package org.ballerinalang.langserver.completions.toml;
 
 import io.ballerina.toml.validator.schema.AbstractSchema;
 import io.ballerina.toml.validator.schema.ArraySchema;
+import io.ballerina.toml.validator.schema.BooleanSchema;
+import io.ballerina.toml.validator.schema.NumericSchema;
 import io.ballerina.toml.validator.schema.ObjectSchema;
 import io.ballerina.toml.validator.schema.Schema;
 import io.ballerina.toml.validator.schema.SchemaVisitor;
+import io.ballerina.toml.validator.schema.StringSchema;
 import io.ballerina.toml.validator.schema.Type;
 import org.eclipse.lsp4j.CompletionItem;
 import org.eclipse.lsp4j.CompletionItemKind;
@@ -53,78 +56,128 @@ public class C2CSchemaVisitor extends SchemaVisitor {
             String key = entry.getKey();
             AbstractSchema value = entry.getValue();
             if (value.type() == Type.OBJECT) {
-                String oldParentKey = this.parentKey;
+//                String oldParentKey = this.parentKey;
                 key = this.parentKey + "." + key;
-                this.parentKey = key;
-                Table objectNode = new Table(key);
-                item.setInsertText(objectNode.prettyPrint());
-                item.setDetail("Table");
-                item.setLabel(key);
-                item.setKind(CompletionItemKind.Snippet);
-                item.setInsertTextFormat(InsertTextFormat.Snippet);
+//                this.parentKey = key;
+                generateTableSnippet(item, key);
                 completionStore.put(key,item);
 
                 Map<String,CompletionItem> store = new HashMap<>();
-                this.parentStore = store;
-                visitNode(value);
-                map.put(this.parentKey,store);
-                this.parentKey = oldParentKey;
+//                this.parentStore = store;
+                visitNode(value,key,store);
+                map.put(key,store);
+//                this.parentKey = oldParentKey;
             }
             else if (value.type() == Type.ARRAY) {
-                String oldParentKey = this.parentKey;
                 key = this.parentKey + "." + key;
-                this.parentKey = key;
-                TableArray objectNode = new TableArray(key);
-                item.setInsertText(objectNode.prettyPrint());
-                item.setLabel(key);
-                item.setKind(CompletionItemKind.Snippet);
-                item.setInsertTextFormat(InsertTextFormat.Snippet);
-                item.setDetail("Table Array");
-                completionStore.put(key,item);
-
-                ArraySchema arraySchema = (ArraySchema) value;
-                AbstractSchema items = arraySchema.items();
-
-                Map<String,CompletionItem> store = new HashMap<>();
-                this.parentStore = store;
-                visitNode(items);
-                map.put(this.parentKey,store);
-                this.parentKey = oldParentKey;
-
+                visitNode(value, key,completionStore);
             }
-            else if (value.type() == Type.STRING) {
-                KeyValuePair objectNode = new KeyValuePair(key, ValueType.STRING);
-                item.setInsertText(objectNode.prettyPrint());
-                item.setLabel(key);
-                item.setKind(CompletionItemKind.Snippet);
-                item.setInsertTextFormat(InsertTextFormat.Snippet);
-                item.setDetail("String");
-                completionStore.put(key,item);
-            }
-            else if (value.type() == Type.NUMBER || value.type() == Type.INTEGER) {
-                KeyValuePair objectNode = new KeyValuePair(key, ValueType.NUMBER);
-                item.setInsertText(objectNode.prettyPrint());
-                item.setLabel(key);
-                item.setKind(CompletionItemKind.Snippet);
-                item.setInsertTextFormat(InsertTextFormat.Snippet);
-                item.setDetail("Number");
-                completionStore.put(key,item);
-            }
-            else if (value.type() == Type.BOOLEAN) {
-                KeyValuePair objectNode = new KeyValuePair(key, ValueType.NUMBER);
-                item.setInsertText(objectNode.prettyPrint());
-                item.setLabel(key);
-                item.setKind(CompletionItemKind.Snippet);
-                item.setInsertTextFormat(InsertTextFormat.Snippet);
-                item.setDetail("Boolean");
-                completionStore.put(key,item);
+            else {
+                visitNode(value, key, completionStore);
             }
         }
+    }
+
+    private void generateTableSnippet(CompletionItem item, String key) {
+        Table objectNode = new Table(key);
+        item.setInsertText(objectNode.prettyPrint());
+        item.setDetail("Table");
+        item.setLabel(key);
+        item.setKind(CompletionItemKind.Snippet);
+        item.setInsertTextFormat(InsertTextFormat.Snippet);
+    }
+
+    @Override
+    public void visit(ArraySchema arraySchema) {
+        String key = this.parentKey;
+        Map<String, CompletionItem> parentStore = this.parentStore;
+        CompletionItem item = generateArraySnippet(key);
+        parentStore.put(key,item);
+
+        AbstractSchema items = arraySchema.items();
+        Map<String,CompletionItem> store = new HashMap<>();
+        visitNode(items,key,store);
+        this.map.put(key,store);
+    }
+
+    private CompletionItem generateArraySnippet(String key) {
+        CompletionItem item = new CompletionItem();
+        TableArray objectNode = new TableArray(key);
+        item.setInsertText(objectNode.prettyPrint());
+        item.setLabel(key);
+        item.setKind(CompletionItemKind.Snippet);
+        item.setInsertTextFormat(InsertTextFormat.Snippet);
+        item.setDetail("Table Array");
+        return item;
+    }
+
+    @Override
+    public void visit(BooleanSchema booleanSchema) {
+        String key = this.parentKey;
+        CompletionItem item = generateBooleanSnippet(key);
+        this.parentStore.put(key,item);
+    }
+
+    private CompletionItem generateBooleanSnippet(String key) {
+        CompletionItem item = new CompletionItem();
+        KeyValuePair objectNode = new KeyValuePair(key, ValueType.NUMBER);
+        item.setInsertText(objectNode.prettyPrint());
+        item.setLabel(key);
+        item.setKind(CompletionItemKind.Snippet);
+        item.setInsertTextFormat(InsertTextFormat.Snippet);
+        item.setDetail("Boolean");
+        return item;
+    }
+
+    @Override
+    public void visit(NumericSchema numericSchema) {
+        String key = this.parentKey;
+        CompletionItem item = generateNumericSnippet(key);
+        this.parentStore.put(key,item);
+    }
+
+    private CompletionItem generateNumericSnippet(String key) {
+        KeyValuePair objectNode = new KeyValuePair(key, ValueType.NUMBER);
+        CompletionItem item = new CompletionItem();
+        item.setInsertText(objectNode.prettyPrint());
+        item.setLabel(key);
+        item.setKind(CompletionItemKind.Snippet);
+        item.setInsertTextFormat(InsertTextFormat.Snippet);
+        item.setDetail("Number");
+        return item;
+    }
+
+    @Override
+    public void visit(StringSchema stringSchema) {
+        String key = this.parentKey;
+        CompletionItem item = generateStringSnippet(key);
+        this.parentStore.put(key,item);
+    }
+
+    private CompletionItem generateStringSnippet(String key) {
+        CompletionItem item = new CompletionItem();
+        KeyValuePair objectNode = new KeyValuePair(key, ValueType.STRING);
+        item.setInsertText(objectNode.prettyPrint());
+        item.setLabel(key);
+        item.setKind(CompletionItemKind.Snippet);
+        item.setInsertTextFormat(InsertTextFormat.Snippet);
+        item.setDetail("String");
+        return item;
     }
 
     private void visitNode(AbstractSchema schema) {
         String oldKey = this.parentKey;
         Map<String,CompletionItem> oldStore = this.parentStore;
+        schema.accept(this);
+        this.parentKey = oldKey;
+        this.parentStore = oldStore;
+    }
+
+    private void visitNode(AbstractSchema schema,String newKey, Map<String,CompletionItem> newParentStore) {
+        String oldKey = this.parentKey;
+        Map<String,CompletionItem> oldStore = this.parentStore;
+        this.parentKey = newKey;
+        this.parentStore = newParentStore;
         schema.accept(this);
         this.parentKey = oldKey;
         this.parentStore = oldStore;
