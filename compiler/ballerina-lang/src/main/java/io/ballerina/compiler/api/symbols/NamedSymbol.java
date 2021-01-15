fbd19d6670d183d4cea624683e9179a434b13fae
Pubudu Fernando <pubuduf@wso2.com>
2021-01-15 23:42:38 +0530

    Introduce a new interface for named symbols

---
+++ compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/symbols/NamedSymbol.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.ballerina.compiler.api.symbols;
+
+import io.ballerina.compiler.api.ModuleID;
+import io.ballerina.tools.diagnostics.Location;
+
+/**
+ * Represents a symbol which is associated with a name and can be located in a source file.
+ *
+ * @since 2.0.0
+ */
+public interface NamedSymbol extends Symbol {
+
+    /**
+     * Get the symbol name.
+     *
+     * @return {@link String} name of the symbol
+     */
+    String name();
+
+    /**
+     * Get the moduleID of the symbol.
+     *
+     * @return {@link ModuleID} of the symbol
+     */
+    ModuleID moduleID();
+
+    /**
+     * This retrieves position information of the symbol in the source code. The position given here is the location of
+     * the definition of the symbol.
+     *
+     * @return The position information of the symbol
+     */
+    Location location();
+}
