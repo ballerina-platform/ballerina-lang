35a8adee87b572fba7b48b9cc46ed4ec3523d0e6
sameerajayasoma <sameera.madushan@gmail.com>
2021-01-20 00:36:12 -0800

    Add classes to represent import module request and response

---
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/ImportModuleResponse.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package io.ballerina.projects.internal;
+
+import io.ballerina.projects.PackageName;
+import io.ballerina.projects.PackageOrg;
+
+/**
+ * Represents a resolved imported module.
+ *
+ * @since 2.0.0
+ */
+public class ImportModuleResponse {
+    // TODO can we replace this with Vertex?
+    private final PackageOrg packageOrg;
+    private final PackageName packageName;
+
+    public ImportModuleResponse(PackageOrg packageOrg, PackageName packageName) {
+        this.packageOrg = packageOrg;
+        this.packageName = packageName;
+    }
+
+    public PackageOrg packageOrg() {
+        return packageOrg;
+    }
+
+    public PackageName packageName() {
+        return packageName;
+    }
+}
