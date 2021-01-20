6c46f5d4876925784964b81c8b0747ea928a2e2b
Nadun De Silva <nadunrds@gmail.com>
2021-01-20 15:30:40 +0530

    Update interop calls in ballerina/observe module to use ocnfigurable feature

--- observelib/observe/src/main/java/org/ballerinalang/observe/nativeimpl/OpenTracerBallerinaWrapper.java
+++ observelib/observe/src/main/java/org/ballerinalang/observe/nativeimpl/OpenTracerBallerinaWrapper.java
@@ -37,14 +37,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
-import static io.ballerina.runtime.observability.ObservabilityConstants.CONFIG_TRACING_ENABLED;
 import static io.ballerina.runtime.observability.ObservabilityConstants.UNKNOWN_SERVICE;
 
 /**
  * This class wraps opentracing apis and exposes extern functions to use within ballerina.
  */
 public class OpenTracerBallerinaWrapper {
-    private static Logger log = LoggerFactory.getLogger(OpenTracerBallerinaWrapper.class);
+    private static final Logger log = LoggerFactory.getLogger(OpenTracerBallerinaWrapper.class);
 
     private static final OpenTracerBallerinaWrapper instance = new OpenTracerBallerinaWrapper();
     private final TracersStore tracerStore;
@@ -56,7 +55,7 @@ public class OpenTracerBallerinaWrapper {
     static final int ROOT_SPAN_INDICATOR = -2;
 
     private OpenTracerBallerinaWrapper() {
-        enabled = ConfigRegistry.getInstance().getAsBoolean(CONFIG_TRACING_ENABLED);
+        enabled = ObserveUtils.isTracingEnabled();
         tracerStore = TracersStore.getInstance();
     }
 
