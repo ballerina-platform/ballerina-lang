7a9d4db4dd5a1b9c9af1820e87faf73950fffdc9
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2021-01-22 20:39:47 +0530

    Change shell cli tests to clean the inputs
    Shell cli tests depend on the cli output. But some terminals
    cause the cli output to have control characters unexpected by the test.
    This cause cli tests to fail.

--- ballerina-shell/modules/shell-cli/src/test/java/io/ballerina/shell/cli/test/base/TestIntegrator.java
+++ ballerina-shell/modules/shell-cli/src/test/java/io/ballerina/shell/cli/test/base/TestIntegrator.java
@@ -19,6 +19,7 @@
 package io.ballerina.shell.cli.test.base;
 
 import io.ballerina.shell.cli.PropertiesLoader;
+import io.ballerina.shell.cli.utils.FileUtils;
 import org.testng.Assert;
 
 import java.io.BufferedReader;
@@ -29,9 +30,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+import static io.ballerina.shell.cli.PropertiesLoader.HEADER_FILE;
 import static io.ballerina.shell.cli.PropertiesLoader.REPL_PROMPT;
 
 /**
@@ -41,13 +45,16 @@ import static io.ballerina.shell.cli.PropertiesLoader.REPL_PROMPT;
  * @since 2.0.0
  */
 public class TestIntegrator extends Thread {
+    private static final String[] SPECIAL_CONTROL_STRINGS = {"\\x1B>", "\\x1B=", "\\x08"};
     private final InputStream inputStream;
     private final OutputStream outputStream;
     private final ByteArrayOutputStream stdoutStream;
     private final List<TestCase> testCases;
+    private final String shellPrompt;
 
-    public TestIntegrator(InputStream inputStream, OutputStream outputStream,
-                          ByteArrayOutputStream stdoutStream, List<TestCase> testCases) {
+    public TestIntegrator(InputStream inputStream, OutputStream outputStream, ByteArrayOutputStream stdoutStream,
+                          List<TestCase> testCases) {
+        this.shellPrompt = PropertiesLoader.getProperty(REPL_PROMPT);
         this.inputStream = inputStream;
         this.outputStream = outputStream;
         this.stdoutStream = stdoutStream;
@@ -57,61 +64,95 @@ public class TestIntegrator extends Thread {
     @Override
     public void run() {
         try {
-            String shellPrompt = PropertiesLoader.getProperty(REPL_PROMPT);
             PrintStream testPrint = new PrintStream(outputStream, true, Charset.defaultCharset());
             InputStreamReader inStreamReader = new InputStreamReader(inputStream, Charset.defaultCharset());
             BufferedReader testReader = new BufferedReader(inStreamReader);
 
+            // Assert welcome text
+            String headerText = FileUtils.readResource(PropertiesLoader.getProperty(HEADER_FILE));
+            sendRequest(testPrint, "");
+            String response = readResponse(testReader);
+            String expectedResponse = String.format("%n%n%s%n%s%n", headerText, shellPrompt);
+            Assert.assertEquals(response, expectedResponse);
+
+            readResponse(testReader);
+
             for (TestCase testCase : testCases) {
-                // Give input and record shell in/out.
-                testPrint.println(testCase.getCode() + System.lineSeparator());
+                sendRequest(testPrint, testCase.getCode());
+                String exprResponse = stripTillPrompt(readResponse(testReader));
+                // Expected format: [PROMPT][INPUT]\n\n[OUTPUT]\n\n[PROMPT]\n
+                String expectedExprResponse = String.format("%s%s%n%n%s%n%s%n",
+                        shellPrompt, testCase.getCode(), testCase.getExpr(), shellPrompt);
 
-                String recordedContent;
-                while (true) {
-                    StringBuilder recordedInput = new StringBuilder();
-                    while (true) {
-                        String line = Objects.requireNonNull(testReader.readLine());
-                        recordedInput.append(line).append(System.lineSeparator());
-                        if (line.endsWith(shellPrompt)) {
-                            break;
-                        }
-                    }
-
-                    // The input will be in format "[GARBAGE][PROMPT][INPUT][OUTPUT][PROMPT][GARBAGE]".
-                    recordedContent = filteredString(recordedInput.toString());
-                    // Remove all unnecessary prefix/prompt strings. (Remove GARBAGE and PROMPT)
-                    // recordedContent = [INPUT][OUTPUT][PROMPT][GARBAGE]
-                    recordedContent = recordedContent.substring(recordedContent.indexOf(shellPrompt) +
-                            shellPrompt.length());
-                    if (recordedContent.indexOf(shellPrompt) <= 0) {
-                        continue;
-                    }
-
-                    // recordedContent = [INPUT][OUTPUT]
-                    recordedContent = recordedContent.substring(0, recordedContent.indexOf(shellPrompt));
-                    break;
-                }
-
-                // Extract INPUT and verify.
-                String recordedContentInput = recordedContent.substring(0, testCase.getCode().length());
-                Assert.assertEquals(recordedContentInput, testCase.getCode(), testCase.getDescription());
-
-                // Extract OUTPUT and test.
-                String shellOutput = recordedContent.substring(testCase.getCode().length()).trim();
-                String expectedOutput = Objects.requireNonNullElse(testCase.getExpr(), "");
-                Assert.assertEquals(shellOutput.trim(), expectedOutput.trim(), testCase.getDescription());
-
-                Assert.assertEquals(stdoutStream.toString().trim(), testCase.getStdout().trim(),
+                Assert.assertEquals(replaceNewLine(exprResponse), replaceNewLine(expectedExprResponse),
+                        errorMessage(exprResponse, expectedExprResponse, testCase.getDescription()));
+                Assert.assertEquals(replaceNewLine(stdoutStream.toString()), replaceNewLine(testCase.getStdout()),
                         testCase.getDescription());
                 stdoutStream.reset();
             }
-        } catch (IOException ignored) {
+        } catch (IOException | InterruptedException ignored) {
         }
     }
 
-    private String filteredString(String rawString) {
-        return rawString
-                .replaceAll("(\\x9B|\\x1B\\[)[0-?]*[ -/]*[@-~]", "")
-                .replace("\r\n", "\n");
+    /**
+     * Reads data from the stream specified until if finds shell prompt as the EOL.
+     */
+    private String readResponse(BufferedReader stream) throws IOException {
+        String line = "";
+        StringBuilder data = new StringBuilder();
+        while (!line.endsWith(shellPrompt)) {
+            line = Objects.requireNonNull(stream.readLine());
+            data.append(line).append(System.lineSeparator());
+        }
+        return filteredString(data.toString());
+    }
+
+    /**
+     * Send the data given to the specific stream.
+     */
+    private void sendRequest(PrintStream stream, String string) throws InterruptedException {
+        stream.append(string);
+        stream.println(System.lineSeparator());
+        stream.flush();
+    }
+
+    /**
+     * Remove invisible characters from the string.
+     * The removed characters are ansi characters that are added by jline.
+     */
+    private String filteredString(String string) {
+        // Remove all ANSI codes
+        string = string.replaceAll("(\\x9B|\\x1B\\[)[0-?]*[ -/]*[@-~]", "");
+        // Disable ANSI escape characters and Backspace character
+        string = string
+                .replaceAll("\\x9B", "\\\\x9B")
+                .replaceAll("\\x1B", "\\\\x1B")
+                .replaceAll("\\x08", "\\\\x08");
+        // Remove any special sequences known
+        for (String controlString : SPECIAL_CONTROL_STRINGS) {
+            string = string.replace(controlString, "");
+        }
+        return string;
+    }
+
+    /**
+     * Strip string till the first appearance of prompt.
+     */
+    private String stripTillPrompt(String string) {
+        return string.substring(string.indexOf(shellPrompt));
+    }
+
+    /**
+     * Replace new line with escaped newline.
+     */
+    private String replaceNewLine(String string) {
+        return string.replace("\r\n", "\n")
+                .replace("\n", "\\n");
+    }
+
+    private String errorMessage(String original, String expected, String description) {
+        return description + " failed because " +
+                Arrays.toString(replaceNewLine(original).getBytes(StandardCharsets.UTF_8)) + "!=" +
+                Arrays.toString(replaceNewLine(expected).getBytes(StandardCharsets.UTF_8));
     }
 }
