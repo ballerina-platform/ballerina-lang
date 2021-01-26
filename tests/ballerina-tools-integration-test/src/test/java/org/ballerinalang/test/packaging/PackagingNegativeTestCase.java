d5e086d42a4446b7be2dc4c3e07736d91ebaaedb
Suganya <suganyasuven@gmail.com>
2021-01-26 12:59:49 +0530

    Update ballerina to bal

--- tests/ballerina-tools-integration-test/src/test/java/org/ballerinalang/test/packaging/PackagingNegativeTestCase.java
+++ tests/ballerina-tools-integration-test/src/test/java/org/ballerinalang/test/packaging/PackagingNegativeTestCase.java
@@ -261,28 +261,28 @@ public class PackagingNegativeTestCase extends BaseTest {
 
     @Test(description = "Test build with too many arguments")
     public void testBuildWithTooManyArgs() throws Exception {
-        String msg = "ballerina: too many arguments\n Run 'ballerina help' for usage.";
+        String msg = "ballerina: too many arguments\n Run 'bal help' for usage.";
         balClient.runMain("build", new String[] {"hello", "world"}, envVariables, new String[0],
                 new LogLeecher[]{new LogLeecher(msg)}, balServer.getServerHome());
     }
 
     @Test(description = "Test install with too many arguments")
     public void testInstallWithTooManyArgs() throws Exception {
-        String msg = "ballerina: too many arguments\n Run 'ballerina help' for usage.";
+        String msg = "ballerina: too many arguments\n Run 'bal help' for usage.";
         balClient.runMain("install", new String[] {"foo", "bar"}, envVariables, new String[0],
                 new LogLeecher[]{new LogLeecher(msg)}, balServer.getServerHome());
     }
 
     @Test(description = "Test push with too many arguments")
     public void testPushWithTooManyArgs() throws Exception {
-        String msg = "ballerina: too many arguments\n Run 'ballerina help' for usage.";
+        String msg = "ballerina: too many arguments\n Run 'bal help' for usage.";
         balClient.runMain("push", new String[] {"bcintegrationtest", "foo"}, envVariables, new String[0],
                 new LogLeecher[]{new LogLeecher(msg)}, balServer.getServerHome());
     }
 
     @Test(description = "Test list with too many arguments")
     public void testSearchWithTooManyArgs() throws Exception {
-        String msg = "ballerina: too many arguments\n Run 'ballerina help' for usage.";
+        String msg = "ballerina: too many arguments\n Run 'bal help' for usage.";
         balClient.runMain("list", new String[] {"wso2", "twitter"}, envVariables, new String[0],
                 new LogLeecher[]{new LogLeecher(msg)}, balServer.getServerHome());
     }
@@ -296,7 +296,7 @@ public class PackagingNegativeTestCase extends BaseTest {
 
     @Test(description = "Test uninstall with too many arguments")
     public void testUninstallWithTooManyArgs() throws Exception {
-        String msg = "ballerina: too many arguments\n Run 'ballerina help' for usage.";
+        String msg = "ballerina: too many arguments\n Run 'bal help' for usage.";
         balClient.runMain("uninstall", new String[] {"bcintegrationtest", "testxyz"}, envVariables, new String[0],
                           new LogLeecher[]{new LogLeecher(msg)}, balServer.getServerHome());
     }
@@ -342,7 +342,7 @@ public class PackagingNegativeTestCase extends BaseTest {
         Path projectPath = tempProjectDirectory.resolve("projectxyz");
         initProject(projectPath);
         String msg = "error: you are trying to run a ballerina file inside a package within a project. Try running " +
-                "'ballerina run <package-name>'";
+                "'bal run <package-name>'";
         String sourcePath = Paths.get(moduleName, "main.bal").toString();
         balClient.runMain("run", new String[] {sourcePath}, envVariables, new String[0],
                           new LogLeecher[]{new LogLeecher(msg)}, projectPath.toString());
