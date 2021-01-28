ea43fb91d3b44c0f480041281b51cde448e45357
KRVPerera <rukshan.viduranga@gmail.com>
2021-01-29 01:10:15 +0530

    Fix ErrorTest.java merge conflict

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/error/ErrorTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/error/ErrorTest.java
@@ -357,7 +357,7 @@ public class ErrorTest {
         String message = expectedException.getMessage();
         Assert.assertEquals(message, "error: array index out of range: index: 4, size: 2\n\t" +
                 "at ballerina.lang.array.1_1_0:slice(array.bal:132)\n\t" +
-                "   error_test:testStackTraceInNative(error_test.bal:277)");
+                "   error_test:testStackTraceInNative(error_test.bal:278)");
     }
 
     @Test
