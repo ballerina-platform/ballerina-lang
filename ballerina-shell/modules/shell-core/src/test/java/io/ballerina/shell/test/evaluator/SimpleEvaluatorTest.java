e971cf80b7b6a7e2b9649df73eb25607a30ba129
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2021-02-01 17:55:10 +0530

    Fix testcases related to byte array toBalString

--- ballerina-shell/modules/shell-core/src/test/java/io/ballerina/shell/test/evaluator/SimpleEvaluatorTest.java
+++ ballerina-shell/modules/shell-core/src/test/java/io/ballerina/shell/test/evaluator/SimpleEvaluatorTest.java
@@ -32,9 +32,6 @@ public class SimpleEvaluatorTest extends AbstractEvaluatorTest {
 
     @Test
     public void testEvaluateBasic() throws BallerinaShellException {
-        // TODO: Fix the test cases after #28302,
-        // TODO: toBalString of [5,24,56,243]
-        // TODO: and [174,238,205,239,171,205,18,52,85,103,136,136,34]
         testEvaluate(BASIC_EVALUATOR_TESTCASE);
     }
 
