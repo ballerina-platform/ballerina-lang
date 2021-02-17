42613ad721e71f1738f00d0a9cfb255baa35888a
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2021-02-17 13:56:24 +0530

    Add more improvements

--- misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/JDIEventProcessor.java
+++ misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/JDIEventProcessor.java
@@ -61,7 +61,9 @@ public class JDIEventProcessor {
     private final Map<String, Breakpoint[]> breakpointsList = new HashMap<>();
     private final List<EventRequest> stepEventRequests = new ArrayList<>();
     private static final Logger LOGGER = LoggerFactory.getLogger(JBallerinaDebugServer.class);
-    private static final String JBAL_STRAND_PREFIX = "jbal-strand-exec";
+    private static final String BALLERINA_ORG_PREFIX = "ballerina";
+    private static final String BALLERINAX_ORG_PREFIX = "ballerinax";
+    private static final String NOTIFICATION_PREFIX = "NOTIFICATION: ";
 
     JDIEventProcessor(ExecutionContext context) {
         this.context = context;
@@ -108,26 +110,28 @@ public class JDIEventProcessor {
             context.getEventManager().deleteEventRequests(stepEventRequests);
         } else if (event instanceof StepEvent) {
             StepEvent e = (StepEvent) event;
-            if (isBalSourceAttached(e)) {
+            long threadId = e.thread().uniqueID();
+            if (isBallerinaSource(e.location())) {
+                if (isExternalLibSource(e.location())) {
+                    // If the current step-in event is related to an external source (i.e. lang library, standard
+                    // library, connector module), notifies the user and rolls back to the previous state.
+                    // Todo - add support for external libraries
+                    context.getAdapter().sendOutput(NOTIFICATION_PREFIX + "Stepping into ballerina external modules " +
+                            "is not supported.", STDOUT);
+                    context.getAdapter().stepOut(threadId);
+                    return true;
+                }
                 // If the current step event is related to a ballerina source, suspends all threads and notifies the
                 // client that the debuggee is stopped.
-                context.getEventManager().deleteEventRequests(stepEventRequests);
                 StoppedEventArguments stoppedEventArguments = new StoppedEventArguments();
                 stoppedEventArguments.setReason(StoppedEventArgumentsReason.STEP);
                 stoppedEventArguments.setThreadId(e.thread().uniqueID());
                 stoppedEventArguments.setAllThreadsStopped(true);
                 context.getClient().stopped(stoppedEventArguments);
+                context.getEventManager().deleteEventRequests(stepEventRequests);
             } else {
-                long threadId = e.thread().uniqueID();
-                if (context.getAdapter().getContext().getLastInstruction() == DebugInstruction.STEP_IN) {
-                    // If the current step-in event is not related to a ballerina source, sends step-out request to
-                    // rollback into the last stable state.
-                    context.getAdapter().sendOutput("WARNING: Trying to step-in into an unsupported source", STDOUT);
-                    context.getAdapter().stepOut(threadId);
-                } else {
-                    int stepType = ((StepRequest) event.request()).depth();
-                    sendStepRequest(threadId, stepType);
-                }
+                int stepType = ((StepRequest) event.request()).depth();
+                sendStepRequest(threadId, stepType);
             }
         } else if (event instanceof VMDisconnectEvent
                 || event instanceof VMDeathEvent
@@ -139,10 +143,16 @@ public class JDIEventProcessor {
         return true;
     }
 
-    private boolean isBalSourceAttached(StepEvent event) {
+    /**
+     * Validates whether the given location is related to a ballerina source.
+     *
+     * @param location location
+     * @return true if the given step event is related to a ballerina source
+     */
+    private boolean isBallerinaSource(Location location) {
         try {
-            String sourceName = event.location().sourceName();
-            int sourceLine = event.location().lineNumber();
+            String sourceName = location.sourceName();
+            int sourceLine = location.lineNumber();
             return sourceName.endsWith(BAL_FILE_EXT) && sourceLine > 0;
         } catch (AbsentInformationException e) {
             return false;
@@ -238,25 +248,40 @@ public class JDIEventProcessor {
         }
     }
 
-    void createStepRequest(long threadId, int stepType) {
+    private void createStepRequest(long threadId, int stepType) {
         context.getEventManager().deleteEventRequests(stepEventRequests);
-        ThreadReferenceProxyImpl threadReference = context.getAdapter().getAllThreads().get(threadId);
-        if (threadReference == null || threadReference.getThreadReference() == null) {
+        ThreadReferenceProxyImpl proxy = context.getAdapter().getAllThreads().get(threadId);
+        if (proxy == null || proxy.getThreadReference() == null) {
             return;
         }
 
-        StepRequest request = context.getEventManager().createStepRequest(threadReference.getThreadReference(),
+        StepRequest request = context.getEventManager().createStepRequest(proxy.getThreadReference(),
                 StepRequest.STEP_LINE, stepType);
         request.setSuspendPolicy(StepRequest.SUSPEND_ALL);
         // Todo - Replace with a class inclusive filter.
         request.addClassExclusionFilter("io.*");
         request.addClassExclusionFilter("com.*");
         request.addClassExclusionFilter("org.*");
-        request.addClassExclusionFilter("ballerina.*");
         request.addClassExclusionFilter("java.*");
         request.addClassExclusionFilter("$lambda$main$");
         stepEventRequests.add(request);
         request.addCountFilter(1);
+        stepEventRequests.add(request);
         request.enable();
+    }
+
+    /**
+     * Checks whether the given source location lies within an external module source (i.e. lang library, standard
+     * library, connector module).
+     *
+     * @param location source location
+     */
+    private static boolean isExternalLibSource(Location location) {
+        try {
+            String srcPath = location.sourcePath();
+            return srcPath.startsWith(BALLERINA_ORG_PREFIX) || srcPath.startsWith(BALLERINAX_ORG_PREFIX);
+        } catch (AbsentInformationException e) {
+            return false;
+        }
     }
 }
