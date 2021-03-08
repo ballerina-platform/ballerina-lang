3a0bb5c502d9a98b95c0608d167d078a9ee98dda
azinneera <azinneera@gmail.com>
2021-03-09 01:07:25 +0530

    Rebase with ballerina-platform

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/task/RunTestsTask.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/task/RunTestsTask.java
@@ -221,7 +221,7 @@ public class RunTestsTask implements Task {
                         testReport.addModuleStatus(moduleName, moduleStatus);
                     }
                     try {
-                        generateCoverage(project, jarResolver, jBallerinaBackend);
+                        generateCoverage(project, jBallerinaBackend);
                         generateHtmlReport(project, this.out, testReport, target);
                     } catch (IOException e) {
                         cleanTempCache(project, cachesRoot);
@@ -243,10 +243,10 @@ public class RunTestsTask implements Task {
         cleanTempCache(project, cachesRoot);
     }
 
-    private void generateCoverage(Project project, JarResolver jarResolver, JBallerinaBackend jBallerinaBackend)
+    private void generateCoverage(Project project, JBallerinaBackend jBallerinaBackend)
             throws IOException {
         // Generate code coverage
-        if (!coverage) {
+        if (testReport == null) {
             return;
         }
         Map<String, ModuleCoverage> moduleCoverageMap = initializeCoverageMap(project);
@@ -375,10 +375,8 @@ public class RunTestsTask implements Task {
         // Adds arguments to be read at the Test Runner
         // Index [0 - 3...]
         cmdArgs.add(testCachePath.toString());
-        cmdArgs.add(target.path().toString());
         cmdArgs.add(Boolean.toString(report));
         cmdArgs.add(Boolean.toString(coverage));
-        cmdArgs.addAll(args);
 
         ProcessBuilder processBuilder = new ProcessBuilder(cmdArgs).inheritIO();
         Process proc = processBuilder.start();
