a16ca501e0ddb9f779b12e3a2525f5aa31d00047
pramodya1994 <bpmmendis94@gmail.com>
2021-03-08 14:53:27 +0530

    Add compilerPluginToml to Package

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/BalaFiles.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/BalaFiles.java
@@ -86,7 +86,7 @@ public class BalaFiles {
                     .resolve(ProjectConstants.PACKAGE_MD_FILE_NAME));
             // load other modules
             List<ModuleData> otherModules = loadOtherModules(pkgName, packageRoot);
-            return PackageData.from(balaPath, defaultModule, otherModules, null, null, null, packageMd);
+            return PackageData.from(balaPath, defaultModule, otherModules, null, null, null, null, packageMd);
         } catch (IOException e) {
             throw new ProjectException("Failed to read bala file:" + balaPath);
         }
--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/ProjectFiles.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/ProjectFiles.java
@@ -59,7 +59,7 @@ public class ProjectFiles {
         ModuleData defaultModule = ModuleData
                 .from(filePath, DOT, Collections.singletonList(documentData), Collections.emptyList(), null);
         return PackageData.from(filePath, defaultModule, Collections.emptyList(),
-                null, null, null, null);
+                null, null, null, null, null);
     }
 
     public static PackageData loadBuildProjectPackageData(Path packageDirPath) {
@@ -69,10 +69,11 @@ public class ProjectFiles {
         DocumentData ballerinaToml = loadDocument(packageDirPath.resolve(ProjectConstants.BALLERINA_TOML));
         DocumentData dependenciesToml = loadDocument(packageDirPath.resolve(ProjectConstants.DEPENDENCIES_TOML));
         DocumentData cloudToml = loadDocument(packageDirPath.resolve(ProjectConstants.CLOUD_TOML));
+        DocumentData compilerPluginToml = loadDocument(packageDirPath.resolve(ProjectConstants.COMPILER_PLUGIN_TOML));
         DocumentData packageMd = loadDocument(packageDirPath.resolve(ProjectConstants.PACKAGE_MD_FILE_NAME));
 
         return PackageData.from(packageDirPath, defaultModule, otherModules,
-                ballerinaToml, dependenciesToml, cloudToml, packageMd);
+                ballerinaToml, dependenciesToml, cloudToml, compilerPluginToml, packageMd);
     }
 
     private static List<ModuleData> loadOtherModules(Path packageDirPath) {
