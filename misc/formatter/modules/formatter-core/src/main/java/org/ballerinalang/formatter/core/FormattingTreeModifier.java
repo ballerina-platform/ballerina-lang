00571e78af927460c85a525344139e3c74a360a9
IrushiL <irushi@wso2.com>
2021-03-09 14:08:06 +0530

    Improve the missing node filter logic

--- misc/formatter/modules/formatter-core/src/main/java/org/ballerinalang/formatter/core/FormattingTreeModifier.java
+++ misc/formatter/modules/formatter-core/src/main/java/org/ballerinalang/formatter/core/FormattingTreeModifier.java
@@ -1664,27 +1664,25 @@ public class FormattingTreeModifier extends TreeModifier {
         Token minusToken = markdownParameterDocumentationLineNode.minusToken();
         NodeList<Node> documentElements = markdownParameterDocumentationLineNode.documentElements();
 
-        // handle a scenario when the plus token is the last token in the documentation line
         if (parameterName == null && minusToken == null && documentElements.isEmpty()) {
+            // handle a scenario when the plus token is the last token in the documentation line
             plusToken = formatToken(plusToken, env.trailingWS, env.trailingNL);
         } else {
             plusToken = formatToken(plusToken, 1, 0);
-        }
-
-        // handle a scenario when the parameter name is the last token in the documentation line
-        if (minusToken == null && documentElements.isEmpty()) {
-            parameterName = formatToken(parameterName, env.trailingWS, env.trailingNL);
-        } else {
-            parameterName = formatToken(parameterName, 1, 0);
-        }
-
-        // handle a scenario when the minus token is the last token in the documentation line
-        if (documentElements.isEmpty()) {
-            minusToken = formatToken(minusToken, env.trailingWS, env.trailingNL);
-        } else {
-            minusToken = formatToken(minusToken, 1, 0);
-            documentElements = formatNodeList(markdownParameterDocumentationLineNode.documentElements(),
-                    0, 0, env.trailingWS, env.trailingNL);
+            if (minusToken == null && documentElements.isEmpty()) {
+                // handle a scenario when the parameter name is the last token in the documentation line
+                parameterName = formatToken(parameterName, env.trailingWS, env.trailingNL);
+            } else {
+                parameterName = formatToken(parameterName, 1, 0);
+                if (documentElements.isEmpty()) {
+                    // handle a scenario when the minus token is the last token in the documentation line
+                    minusToken = formatToken(minusToken, env.trailingWS, env.trailingNL);
+                } else {
+                    minusToken = formatToken(minusToken, 1, 0);
+                    documentElements = formatNodeList(markdownParameterDocumentationLineNode.documentElements(),
+                            0, 0, env.trailingWS, env.trailingNL);
+                }
+            }
         }
 
         return markdownParameterDocumentationLineNode.modify()
