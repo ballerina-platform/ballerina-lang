e7cd9a5b607ed355d4f2b5439b77aa5f3219b29f
Fathima Dilhasha <dilhasha.uom@gmail.com>
2021-03-12 12:34:34 +0530

    Enable cli tests in windows workflow

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/model/Target.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/model/Target.java
@@ -178,11 +178,11 @@ public class Target {
      */
     public void clean() throws IOException {
         // Remove from cache
-        FileUtils.deleteDirectory(this.cache.toFile());
+        ProjectUtils.deleteDirectory(this.cache);
         // Remove any generated bala
-        FileUtils.deleteDirectory(this.balaCachePath.toFile());
-        FileUtils.deleteDirectory(this.binPath.toFile());
-        FileUtils.deleteDirectory(this.docPath.toFile());
-        FileUtils.deleteDirectory(this.reportPath.toFile());
+        ProjectUtils.deleteDirectory(this.balaCachePath);
+        ProjectUtils.deleteDirectory(this.binPath);
+        ProjectUtils.deleteDirectory(this.docPath);
+        ProjectUtils.deleteDirectory(this.reportPath);
     }
 }
