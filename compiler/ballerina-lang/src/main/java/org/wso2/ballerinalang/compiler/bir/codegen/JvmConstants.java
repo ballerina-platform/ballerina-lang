b99553d2448b1744a6e334947dce08276e793c9d
riyafa <riyafa.12@cse.mrt.ac.lk>
2021-03-16 16:59:38 +0530

    Add support for command line argument parsing
    Support parsing of options and operands.
    Related issue https://github.com/ballerina-platform/ballerina-lang/issues/28893

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/JvmConstants.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/JvmConstants.java
@@ -171,6 +171,9 @@ public class JvmConstants {
     public static final String ERROR_UTILS = "io/ballerina/runtime/internal/ErrorUtils";
     public static final String ERROR_CREATOR = "io/ballerina/runtime/api/ErrorCreator";
     public static final String RUNTIME_UTILS = "io/ballerina/runtime/internal/util/RuntimeUtils";
+    public static final String OPTION = "io/ballerina/runtime/internal/cli/Option";
+    public static final String OPERAND = "io/ballerina/runtime/internal/cli/Operand";
+    public static final String CLI_SPEC = "io/ballerina/runtime/internal/cli/CliSpec";
     public static final String ARGUMENT_PARSER = "io/ballerina/runtime/internal/util/ArgumentParser";
     public static final String LAUNCH_UTILS = "io/ballerina/runtime/internal/launch/LaunchUtils";
     public static final String MATH_UTILS = "io/ballerina/runtime/internal/MathUtils";
