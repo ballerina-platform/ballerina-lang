5ff76908b5b9998b67e9d7755fbcdfa536b942fb
Dhananjaya Wickramasingha <dhananjayaw@wso2.com>
2021-03-24 17:09:35 +0530

    Fix module-info

--- project-api/test-artifacts/function-node-analyzer-compiler-plugin/src/main/java/module-info.java
+++ project-api/test-artifacts/function-node-analyzer-compiler-plugin/src/main/java/module-info.java
@@ -4,5 +4,6 @@ module function.node.analyzer.compiler.plugin {
     requires io.ballerina.tools.api;
     requires compiler.plugin.test.diagnostic.utils.lib;
 
+    exports io.samjs.plugins.funcnodeanalyzer;
     exports io.samjs.plugins.lifecycle;
 }
