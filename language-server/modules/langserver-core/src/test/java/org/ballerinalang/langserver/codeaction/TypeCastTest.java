c938bd90682425bc73f19d24b2a79057273382ea
Imesha Sudasingha <imesha.sudasingha@gmail.com>
2021-03-26 23:04:11 +0530

    Fix type cast suggestion for unions with error members

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/TypeCastTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/TypeCastTest.java
@@ -41,6 +41,12 @@ public class TypeCastTest extends AbstractCodeActionTest {
         super.test(config, source);
     }
 
+    @Override
+    @Test(dataProvider = "negative-test-data-provider")
+    public void negativeTest(String config, String source) throws IOException, WorkspaceDocumentException {
+        super.negativeTest(config, source);
+    }
+
     @DataProvider(name = "codeaction-data-provider")
     @Override
     public Object[][] dataProvider() {
@@ -52,6 +58,13 @@ public class TypeCastTest extends AbstractCodeActionTest {
                 {"typeCast5.json", "typeCast.bal"},
                 {"typeCast6.json", "typeCast.bal"},
                 {"nilTypeCast.json", "typeCast.bal"},
+        };
+    }
+
+    @DataProvider(name = "negative-test-data-provider")
+    public Object[][] negativeDataProvider() {
+        return new Object[][]{
+                {"typeCastNegative1.json", "typeCast2.bal"},
         };
     }
 }
