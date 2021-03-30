8a3b2b415621a681e3d9d8e5c34fe6e7c84e39ac
CharukaK <charukakarunanayake@gmail.com>
2021-03-30 16:01:34 +0530

    Remove commented out code segments

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/extensions/ballerina/connector/BallerinaConnectorServiceImpl.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/extensions/ballerina/connector/BallerinaConnectorServiceImpl.java
@@ -192,8 +192,6 @@ public class BallerinaConnectorServiceImpl implements BallerinaConnectorService 
                     if (jsonST instanceof JsonObject && ((JsonObject) jsonST).has("typeData")) {
                         JsonElement recordsJson = gson.toJsonTree(connectorRecords);
                         ((JsonObject) ((JsonObject) jsonST).get("typeData")).add("records", recordsJson);
-//                        ((JsonObject) ((JsonObject) jsonST).get("typeData")).add("classes", classesJson);
-//                        ((JsonObject) ((JsonObject) jsonST).get("typeData")).add("classes", classesJson);
                     }
                     connectorCache.addConnectorConfig(request.getOrg(), request.getModule(),
                             request.getVersion(), connector.className().text(), jsonST);
@@ -284,7 +282,7 @@ public class BallerinaConnectorServiceImpl implements BallerinaConnectorService 
                 }
 
                 if (classDefinitions.containsKey(paramType.get().signature())) {
-                    ClassDefinitionNode classDefinition= classDefinitions.get(paramType.get().signature());
+                    ClassDefinitionNode classDefinition = classDefinitions.get(paramType.get().signature());
                     connectorRecords.put(paramType.get().signature(),
                             DiagramUtil.getClassDefinitionSyntaxJson(classDefinition, semanticModel));
                 }
