852fc63876ec7053d9bc0f6ca51ad4930a27a247
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2021-03-31 17:11:29 +0530

    Add definition support to external modules

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/common/utils/CommonUtil.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/common/utils/CommonUtil.java
@@ -43,8 +43,6 @@ import io.ballerina.compiler.syntax.tree.Token;
 import io.ballerina.projects.Module;
 import io.ballerina.projects.Package;
 import io.ballerina.projects.Project;
-import io.ballerina.projects.ProjectKind;
-import io.ballerina.projects.util.ProjectConstants;
 import io.ballerina.tools.diagnostics.Location;
 import io.ballerina.tools.text.LinePosition;
 import io.ballerina.tools.text.LineRange;
@@ -194,7 +192,7 @@ public class CommonUtil {
                 + (!orgName.isEmpty() ? orgName + SLASH_KEYWORD_KEY : orgName)
                 + pkgName + SEMI_COLON_SYMBOL_KEY
                 + CommonUtil.LINE_SEPARATOR;
-        
+
         return Collections.singletonList(new TextEdit(new Range(start, start), importStatement));
     }
 
@@ -484,7 +482,7 @@ public class CommonUtil {
     /**
      * Get the completion item insert text for a BField.
      *
-     * @param bField BField to evaluate
+     * @param bField  BField to evaluate
      * @param parents Parent record field symbols
      * @return {@link String} Insert text
      */
@@ -644,7 +642,7 @@ public class CommonUtil {
     public static boolean isLangLib(ModuleID moduleID) {
         return isLangLib(moduleID.orgName(), moduleID.moduleName());
     }
-    
+
     public static boolean isLangLib(String orgName, String moduleName) {
         return orgName.equals("ballerina") && moduleName.startsWith("lang.");
     }
@@ -993,7 +991,7 @@ public class CommonUtil {
         return "ballerina".equals(pkg.packageOrg().value())
                 && CommonUtil.PRE_DECLARED_LANG_LIBS.contains(pkg.packageName().value());
     }
-    
+
     public static String getModifiedTypeName(DocumentServiceContext context, TypeSymbol typeSymbol) {
         Pattern pattern = Pattern.compile("([\\w_.]*)/([\\w._]*):([\\w.-]*)");
         String typeSignature = typeSymbol.signature();
@@ -1006,55 +1004,10 @@ public class CommonUtil {
             String replaceText = modulePrefix.isEmpty() ? matchedString + Names.VERSION_SEPARATOR : matchedString;
             typeSignature = typeSignature.replace(replaceText, modulePrefix);
         }
-        
+
         return typeSignature;
     }
 
-    /**
-     * Get the file uri of the symbol within the current project.
-     * This API assumes the symbol is in the same project
-     * 
-     * @param context document service context
-     * @param symbol to be located
-     * @return {@link Optional} URI is empty if the symbol is not located within the current project
-     */
-    public static Optional<String> getSymbolUriInProject(DocumentServiceContext context, Symbol symbol) {
-        Path projectRoot = context.workspace().projectRoot(context.filePath());
-        Optional<Project> project = context.workspace().project(context.filePath());
-        Optional<Location> symbolLocation = symbol.getLocation();
-        ModuleID moduleID = symbol.getModule().get().id();
-        
-        if (project.isEmpty() || symbolLocation.isEmpty() || symbol.getModule().isEmpty()) {
-            return Optional.empty();
-        }
-        
-        String uri;
-        String packageName = project.get().currentPackage().packageName().value();
-        if (project.get().kind() == ProjectKind.SINGLE_FILE_PROJECT && moduleID.moduleName().equals(".")) {
-            // Project is a single file project and also the symbol resides in a single file project
-            uri = projectRoot.toUri().toString();
-        } else if (!project.get().currentPackage().packageOrg().value().equals(moduleID.orgName())) {
-            // in orgname mismatch, we return empty
-            return Optional.empty();
-        } else if (packageName.equals(moduleID.moduleName())) {
-            // Symbol is within the default module
-            uri = projectRoot.resolve(symbolLocation.get().lineRange().filePath()).toUri().toString();
-        } else {
-            /*
-            The hierarchical module names have the following format. Hence replace the package name part
-            packageName.component1.component2 => module name is component1.component2
-             */
-            String moduleName = moduleID.moduleName().replace(packageName + ".", "");
-            String fileName = symbolLocation.get().lineRange().filePath();
-            uri = projectRoot.resolve(ProjectConstants.MODULES_ROOT)
-                    .resolve(moduleName)
-                    .resolve(fileName)
-                    .toUri().toString();
-        }
-        
-        return Optional.ofNullable(uri);
-    }
-    
     public static String getModulePrefix(DocumentServiceContext context, String orgName, String modName) {
         Project project = context.workspace().project(context.filePath()).orElseThrow();
         String currentProjectOrg = project.currentPackage().packageOrg().value();
@@ -1062,7 +1015,7 @@ public class CommonUtil {
         Optional<Module> currentModule = context.currentModule();
         String evalOrgName = isCurrentOrg ? "" : orgName;
         Optional<ImportDeclarationNode> matchedImport = matchingImportedModule(context, evalOrgName, modName);
-        
+
         if (currentModule.isPresent() && modName.equals(getQualifiedModuleName(currentModule.get()))) {
             // If the module name is same as the current module, then return empty
             return "";
