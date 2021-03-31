33da4261963395f8ba4b71a578fb10815e2c70b1
sanjana <anjetnet@gmail.com>
2021-03-31 11:06:15 +0530

    Add create variable code action test for .iterator().next()

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/CreateVariableTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/CreateVariableTest.java
@@ -86,6 +86,7 @@ public class CreateVariableTest extends AbstractCodeActionTest {
                 {"variableAssignmentRequiredCodeAction39.json", "createVariable5.bal"},
 //                {"variableAssignmentRequiredCodeAction40.json", "createVariable5.bal"},   // disabled due to #26996
                 {"variableAssignmentRequiredCodeAction41.json", "createVariable6.bal"},
+                {"variableAssignmentRequiredCodeAction42.json", "createVariable7.bal"},
                 {"ignoreReturnValueCodeAction.json", "createVariable.bal"},
                 {"projectVariableAssignmentRequiredCodeAction1.json", "testproject/main.bal"},
                 {"projectVariableAssignmentRequiredCodeAction2.json", "testproject/main.bal"},
