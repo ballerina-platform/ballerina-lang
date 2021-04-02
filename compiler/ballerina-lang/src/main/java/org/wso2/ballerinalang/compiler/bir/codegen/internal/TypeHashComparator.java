bfd22ce5e7c11ea1eb83c2a99f24c911dd88d24b
Grainier <grainier@wso2.com>
2021-04-02 13:01:10 +0530

    Fix class doc comment

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/internal/TypeHashComparator.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/internal/TypeHashComparator.java
@@ -25,7 +25,7 @@ import java.util.Comparator;
 /**
  * BType hash comparator to sort BIRTypeDefinitions according its type hash code.
  *
- * @since 1.2.0
+ * @since Swan Lake
  */
 public class TypeHashComparator implements Comparator<BIRTypeDefinition> {
 
