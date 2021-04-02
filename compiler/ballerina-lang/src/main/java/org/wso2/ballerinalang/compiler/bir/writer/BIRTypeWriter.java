b18d0dcda2dd4e0b52a4639945b920665c523c14
Pubudu Fernando <pubuduf@wso2.com>
2021-04-02 12:33:52 +0530

    Refactor markdown params doc persisting logic in BIR type writer

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/writer/BIRTypeWriter.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/writer/BIRTypeWriter.java
@@ -479,14 +479,8 @@ public class BIRTypeWriter implements TypeVisitor {
             birbuf.writeInt(markdownDocAttachment.description == null ? -1
                     : addStringCPEntry(markdownDocAttachment.description));
             birbuf.writeInt(markdownDocAttachment.returnValueDescription == null ? -1
-                    : addStringCPEntry(markdownDocAttachment.returnValueDescription));
-            birbuf.writeInt(markdownDocAttachment.parameters.size());
-            for (MarkdownDocAttachment.Parameter parameter : markdownDocAttachment.parameters) {
-                birbuf.writeInt(parameter.name == null ? -1
-                        : addStringCPEntry(parameter.name));
-                birbuf.writeInt(parameter.description == null ? -1
-                        : addStringCPEntry(parameter.description));
-            }
+                                    : addStringCPEntry(markdownDocAttachment.returnValueDescription));
+            writeParamDocs(birbuf, markdownDocAttachment.parameters);
 
             if (markdownDocAttachment.deprecatedDocumentation != null) {
                 birbuf.writeInt(addStringCPEntry(markdownDocAttachment.deprecatedDocumentation));
@@ -494,15 +488,19 @@ public class BIRTypeWriter implements TypeVisitor {
                 birbuf.writeInt(-1);
             }
 
-            birbuf.writeInt(markdownDocAttachment.deprecatedParams.size());
-            for (MarkdownDocAttachment.Parameter param : markdownDocAttachment.deprecatedParams) {
-                birbuf.writeInt(addStringCPEntry(param.name));
-                birbuf.writeInt(addStringCPEntry(param.description));
-            }
+            writeParamDocs(birbuf, markdownDocAttachment.deprecatedParams);
         }
         int length = birbuf.nioBuffer().limit();
         buf.writeInt(length);
         buf.writeBytes(birbuf.nioBuffer().array(), 0, length);
+    }
+
+    private void writeParamDocs(ByteBuf birBuf, List<MarkdownDocAttachment.Parameter> params) {
+        birBuf.writeInt(params.size());
+        for (MarkdownDocAttachment.Parameter param : params) {
+            birBuf.writeInt(addStringCPEntry(param.name));
+            birBuf.writeInt(addStringCPEntry(param.description));
+        }
     }
 
     private void throwUnimplementedError(BType bType) {
