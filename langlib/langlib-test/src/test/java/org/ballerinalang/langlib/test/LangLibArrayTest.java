11557218e88a0aca1d27e64d2ccef858189b4c2b
KRVPerera <rukshan.viduranga@gmail.com>
2021-04-07 18:51:43 +0530

    Fix checkstyleTest

--- langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibArrayTest.java
+++ langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibArrayTest.java
@@ -491,13 +491,13 @@ public class LangLibArrayTest {
         BAssertUtil.validateError(negativeResult, errorIndex++,
                 "invalid sort key function return type: '(string|int)' is not an ordered type", 184, 62);
         BAssertUtil.validateError(negativeResult, errorIndex++,
-                "incompatible types: expected 'anydata[]', found '(Person|error)[]'" ,195, 15);
+                "incompatible types: expected 'anydata[]', found '(Person|error)[]'", 195, 15);
         BAssertUtil.validateError(negativeResult, errorIndex++,
-                "incompatible types: expected 'anydata[]', found '(Person|error)[]'" ,196, 15);
+                "incompatible types: expected 'anydata[]', found '(Person|error)[]'", 196, 15);
         BAssertUtil.validateError(negativeResult, errorIndex++,
                 "incompatible types: expected 'anydata[]', found 'function[]'", 199, 15);
         BAssertUtil.validateError(negativeResult, errorIndex++, "incompatible types: expected 'anydata', found " +
-                        "'function (int) returns (int)'",199, 26);
+                        "'function (int) returns (int)'", 199, 26);
         Assert.assertEquals(negativeResult.getErrorCount(), errorIndex);
     }
 
