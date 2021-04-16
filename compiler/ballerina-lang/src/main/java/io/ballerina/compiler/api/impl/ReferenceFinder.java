201ee8bbf9e4eb98074423932eb28e659e01d7c2
Pubudu Fernando <pubuduf@wso2.com>
2021-04-16 14:14:51 +0530

    Consider module prefix in annot attachments in ref finder

--- compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/impl/ReferenceFinder.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/impl/ReferenceFinder.java
@@ -313,6 +313,13 @@ public class ReferenceFinder extends BaseVisitor {
     @Override
     public void visit(BLangAnnotationAttachment annAttachmentNode) {
         find(annAttachmentNode.expr);
+
+        if (!annAttachmentNode.pkgAlias.value.isEmpty()
+                && annAttachmentNode.annotationSymbol != null
+                && addIfSameSymbol(annAttachmentNode.annotationSymbol.owner, annAttachmentNode.pkgAlias.pos)) {
+            return;
+        }
+
         addIfSameSymbol(annAttachmentNode.annotationSymbol, annAttachmentNode.annotationName.pos);
     }
 
