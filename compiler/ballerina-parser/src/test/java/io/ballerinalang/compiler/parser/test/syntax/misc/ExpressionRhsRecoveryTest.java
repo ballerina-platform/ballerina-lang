78f82cfdd445121754b315371b9c98c1b39a2f39
rdulmina <rdulmina@gmail.com>
2021-04-21 16:45:30 +0530

    Update doc comment

--- compiler/ballerina-parser/src/test/java/io/ballerinalang/compiler/parser/test/syntax/misc/ExpressionRhsRecoveryTest.java
+++ compiler/ballerina-parser/src/test/java/io/ballerinalang/compiler/parser/test/syntax/misc/ExpressionRhsRecoveryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -22,12 +22,12 @@ import org.testng.annotations.Test;
 /**
  * Test recovery of expression rhs.
  * 
- * @since 2.0 Swan lake
+ * @since 2.0
  */
 public class ExpressionRhsRecoveryTest extends AbstractMiscTest {
 
     @Test
-    public void testImportWithPredefinedPrefix() {
+    public void testExpressionRhsRecovery() {
         testFile("expression-rhs-recovery/expression_rhs_recovery_source_01.bal",
                 "expression-rhs-recovery/expression_rhs_recovery_assert_01.json");
     }
