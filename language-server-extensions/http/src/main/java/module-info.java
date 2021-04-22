31ccaa4038dac88875c5bbf21144c0c199a5ee68
Imesha Sudasingha <imesha.sudasingha@gmail.com>
2021-04-22 11:03:30 +0530

    tmp_commit

---
+++ language-server-extensions/http/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.ballerina.language.server.extensions.http {
+    requires io.ballerina.language.server.commons;
+    requires org.eclipse.lsp4j.jsonrpc;
+
+    requires gson;
+    requires io.ballerina.lang;
+    requires org.eclipse.lsp4j;
+}
