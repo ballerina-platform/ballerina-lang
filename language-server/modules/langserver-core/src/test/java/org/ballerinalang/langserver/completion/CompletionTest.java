4452fbfbb6ed8b9a56163944832504184d6b37c3
sanjana <anjetnet@gmail.com>
2021-04-30 10:26:41 +0530

    Revert "Update TemplateExpressionNodeContextTest configs in LS"
    This reverts commit e2abb38d5afe21000566ef026342b55daa15efcc.

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/CompletionTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/CompletionTest.java
@@ -84,13 +84,12 @@ public abstract class CompletionTest {
 //            obj.add("position", configJsonObject.get("position"));
 //            obj.add("source", configJsonObject.get("source"));
 //            obj.add("items", resultList);
-//            String newObjStr = obj.toString().concat(System.lineSeparator());
 //            java.nio.file.Files.write(FileUtils.RES_DIR.resolve(configJsonPath),
-//                    newObjStr.getBytes(java.nio.charset.StandardCharsets.UTF_8));
+//                                      obj.toString().getBytes(java.nio.charset.StandardCharsets.UTF_8));
 //
-//            //This will print nice comparable text in IDE
+//             //This will print nice comparable text in IDE
 //            Assert.assertEquals(responseItemList.toString(), expectedList.toString(),
-//                    "Failed Test for: " + configJsonPath);
+//                        "Failed Test for: " + configJsonPath);
             Assert.fail("Failed Test for: " + configJsonPath);
         }
     }
