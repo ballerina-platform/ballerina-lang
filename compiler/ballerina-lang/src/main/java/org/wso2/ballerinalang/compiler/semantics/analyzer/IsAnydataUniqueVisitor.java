67ab648409dba4dece5cb50f655125d24c950828
KRVPerera <rukshan.viduranga@gmail.com>
2021-05-03 18:37:30 +0530

    Add code review suggestion

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/IsAnydataUniqueVisitor.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/IsAnydataUniqueVisitor.java
@@ -279,8 +279,12 @@ public class IsAnydataUniqueVisitor implements UniqueTypeVisitor<Boolean> {
                 return false;
             }
         }
-        type.isAnyData =
-                type.sealed || (type.restFieldType != null && isPureTypeUniqueVisitor.visit(type.restFieldType));
+
+        if (!type.sealed && (type.restFieldType == null)) {
+            return false;
+        }
+
+        type.isAnyData = type.sealed || isPureTypeUniqueVisitor.visit(type.restFieldType);
         return type.isAnyData;
     }
 
