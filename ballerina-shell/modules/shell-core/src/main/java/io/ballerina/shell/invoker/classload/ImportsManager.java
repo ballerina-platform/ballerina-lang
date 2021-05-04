b47b1055543a16620a5d6b69e32f49d3a7df0835
Pubudu Fernando <pubuduf@wso2.com>
2021-05-04 11:47:18 +0530

    Update pattern for Cloneable in bal shell

--- ballerina-shell/modules/shell-core/src/main/java/io/ballerina/shell/invoker/classload/ImportsManager.java
+++ ballerina-shell/modules/shell-core/src/main/java/io/ballerina/shell/invoker/classload/ImportsManager.java
@@ -50,7 +50,7 @@ public class ImportsManager {
     private static final QuotedIdentifier ANON_SOURCE = new QuotedIdentifier("$");
     // Regex patterns
     private static final Pattern CLONEABLE_SIGNATURE_PATTERN =
-            Pattern.compile("readonly\\|xml<[^>]*>\\|\\(Cloneable\\)\\[]\\|map<Cloneable>\\|table<map<Cloneable>>");
+            Pattern.compile("readonly\\|xml<[^>]*>\\|Cloneable\\[]\\|map<Cloneable>\\|table<map<Cloneable>>");
     private static final Pattern FULLY_QUALIFIED_MODULE_ID_PATTERN =
             Pattern.compile("([\\w]+)/([\\w.]+):([^:]+):([\\w]+)[|]?");
     // Special imports
