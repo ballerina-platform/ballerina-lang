1cd3f4f2bae5add38356f099f75fc15dc3134d6e
pramodya1994 <bpmmendis94@gmail.com>
2021-05-11 15:36:03 +0530

    Resolve PR comments

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/SettingsToml.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/SettingsToml.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *  WSO2 Inc. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -17,6 +17,7 @@
  */
 package io.ballerina.projects;
 
+import io.ballerina.projects.util.ProjectConstants;
 import io.ballerina.toml.semantic.ast.TomlTableNode;
 
 /**
@@ -42,7 +43,7 @@ public class SettingsToml {
     }
 
     public String name() {
-        return "Settings.toml";
+        return ProjectConstants.SETTINGS_TOML;
     }
 
     public TomlTableNode tomlAstNode() {
