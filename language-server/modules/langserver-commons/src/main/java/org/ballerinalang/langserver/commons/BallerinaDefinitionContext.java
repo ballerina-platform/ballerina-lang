8bca95b05d31deaa78c721a616178511ca3dc3a6
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2021-05-19 17:31:39 +0530

    Add definition support to self reference in a class

--- language-server/modules/langserver-commons/src/main/java/org/ballerinalang/langserver/commons/BallerinaDefinitionContext.java
+++ language-server/modules/langserver-commons/src/main/java/org/ballerinalang/langserver/commons/BallerinaDefinitionContext.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *  WSO2 Inc. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -17,18 +17,10 @@
  */
 package org.ballerinalang.langserver.commons;
 
-import org.eclipse.lsp4j.CompletionCapabilities;
-
 /**
- * Represents the Completion operation context.
+ * Represents a Ballerina goto definition context.
  *
  * @since 2.0.0
  */
-public interface CompletionContext extends PositionedOperationContext {
-    /**
-     * Get the client capabilities.
-     *
-     * @return {@link CompletionCapabilities} client's completion capabilities
-     */
-    CompletionCapabilities getCapabilities();
+public interface BallerinaDefinitionContext extends PositionedOperationContext, BallerinaEnclosedPositionContext {
 }
