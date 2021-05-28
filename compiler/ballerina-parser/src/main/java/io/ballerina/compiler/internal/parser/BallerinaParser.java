fde421e9cc5e80fc868193692a47a63dc4d4c8ff
rdulmina <rdulmina@gmail.com>
2021-05-28 16:59:58 +0530

    Add missing return type desc support for function type

--- compiler/ballerina-parser/src/main/java/io/ballerina/compiler/internal/parser/BallerinaParser.java
+++ compiler/ballerina-parser/src/main/java/io/ballerina/compiler/internal/parser/BallerinaParser.java
@@ -1210,7 +1210,12 @@ public class BallerinaParser extends AbstractParser {
                 return parseFuncDefOrFuncTypeDescRhs(metadata, visibilityQualifier, qualifiers, functionKeyword, name,
                                                      isObjectMember, isObjectTypeDesc);
             case OPEN_PAREN_TOKEN:
+                switchContext(ParserRuleContext.VAR_DECL_STMT);
+                startContext(ParserRuleContext.TYPE_DESC_IN_TYPE_BINDING_PATTERN);
+                startContext(ParserRuleContext.FUNC_TYPE_DESC);
                 STNode funcSignature = parseFuncSignature(true);
+                endContext();
+                endContext();
                 return parseFunctionTypeDescRhs(metadata, visibilityQualifier, qualifiers, functionKeyword,
                         funcSignature, isObjectMember, isObjectTypeDesc);
             default:
@@ -1627,7 +1632,7 @@ public class BallerinaParser extends AbstractParser {
         }
 
         // Treat as function definition.
-
+        switchContext(ParserRuleContext.FUNC_DEF);
         // We reach this method only if the func-name is not present.
         STNode name = SyntaxErrors.createMissingTokenWithDiagnostics(SyntaxKind.IDENTIFIER_TOKEN,
                 DiagnosticErrorCode.ERROR_MISSING_FUNCTION_NAME);
@@ -2234,8 +2239,8 @@ public class BallerinaParser extends AbstractParser {
                 return STNodeFactory.createEmptyNode();
             case RETURNS_KEYWORD:
                 break;
-            case IDENTIFIER_TOKEN: // function foo() r<cursor>
-                if (!isFuncTypeDesc) {
+            case IDENTIFIER_TOKEN: // function foo() r<cursor> and function foo() returns function() r<cursor>
+                if (!isFuncTypeDesc || isSafeMissingReturnsParse()) {
                     break;    
                 }
                 // fall through
@@ -2252,6 +2257,22 @@ public class BallerinaParser extends AbstractParser {
         STNode annot = parseOptionalAnnotations();
         STNode type = parseTypeDescriptor(ParserRuleContext.TYPE_DESC_IN_RETURN_TYPE_DESC);
         return STNodeFactory.createReturnTypeDescriptorNode(returnsKeyword, annot, type);
+    }
+
+    private boolean isSafeMissingReturnsParse() {
+        // Contexts that expects an identifier after function type is not safe to parse as a missing return
+        ParserRuleContext[] missingReturnsUnsafeArray = {ParserRuleContext.TYPE_DESC_IN_ANNOTATION_DECL,
+                ParserRuleContext.TYPE_DESC_BEFORE_IDENTIFIER, ParserRuleContext.TYPE_DESC_IN_RECORD_FIELD,
+                ParserRuleContext.TYPE_DESC_IN_PARAM, ParserRuleContext.TYPE_DESC_IN_TYPE_BINDING_PATTERN,
+                ParserRuleContext.VAR_DECL_STARTED_WITH_DENTIFIER, ParserRuleContext.TYPE_DESC_IN_PATH_PARAM,
+                ParserRuleContext.AMBIGUOUS_STMT};
+
+        for (ParserRuleContext ctx : missingReturnsUnsafeArray) {
+            if (this.errorHandler.hasAncestorContext(ctx)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
