43bc2cb162565758fbfe75758386d9510767c3df
Nipuna Marcus <nipunac@wso2.com>
2021-05-31 19:51:17 +0530

    Fix LS test failure

--- misc/diagram-util/src/main/java/org/ballerinalang/diagramutil/SyntaxTreeMapGenerator.java
+++ misc/diagram-util/src/main/java/org/ballerinalang/diagramutil/SyntaxTreeMapGenerator.java
@@ -188,6 +188,7 @@ public class SyntaxTreeMapGenerator extends NodeTransformer<JsonElement> {
 
             nodeJson.add("typeData", symbolJson);
             if (node.kind() == SyntaxKind.FUNCTION_BODY_BLOCK
+                    && (this.visibleEpsForEachBlock.size() > 0 || this.visibleEpsForModule.size() > 0)
                     && nodeJson.get("typeData") != null) {
                 JsonArray eps = new JsonArray();
                 this.visibleEpsForEachBlock.forEach(eps::add);
