664a4199b6f766a6be1946b6f2eaae4de7f03596
Pubudu Fernando <pubuduf@wso2.com>
2021-06-03 22:44:15 +0530

    Remove io module

--- stdlib/io/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module io.ballerina.io {
-    requires io.ballerina.runtime;
-    requires slf4j.api;
-    exports org.ballerinalang.stdlib.io.channels.base;
-    exports org.ballerinalang.stdlib.io.readers;
-    exports org.ballerinalang.stdlib.io.utils;
-    exports org.ballerinalang.stdlib.io.channels;
-}
