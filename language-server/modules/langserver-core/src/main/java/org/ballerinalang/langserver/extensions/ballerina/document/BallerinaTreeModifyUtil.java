ec06dff46af2507452fca6ac2504263820d29d19
kaje94 <a.kajendran@gmail.com>
2021-06-14 19:53:22 +0530

    Reorder imports in ballerinaTreeModifyUtil

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/extensions/ballerina/document/BallerinaTreeModifyUtil.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/extensions/ballerina/document/BallerinaTreeModifyUtil.java
@@ -45,8 +45,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.regex.Matcher;
+import java.util.stream.Collectors;
 
 /**
  * Represents a request for a Ballerina AST Modify.
