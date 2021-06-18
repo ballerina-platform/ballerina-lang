d6b65c93ef771192e6261cd8cd4657b87b9f2c9d
KRVPerera <rukshan.viduranga@gmail.com>
2021-06-18 10:03:22 +0530

    Fix SO issue of BObject and BIntersection

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/typedefs/CyclicTypeDefinitionsTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/typedefs/CyclicTypeDefinitionsTest.java
@@ -61,6 +61,7 @@ public class CyclicTypeDefinitionsTest {
                 {"testCyclicUserDefinedTypes"},
                 {"testCyclicRestType"},
                 {"testCastingToImmutableCyclicTuple"},
+                {"recursiveTupleArrayCloneTest"},
         };
     }
 
