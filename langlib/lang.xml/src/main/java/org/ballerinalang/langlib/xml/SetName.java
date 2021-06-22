9cf43a5974560c692b21f52b8eb8e6c87e8027f5
rpjayasekara <rpjayasekara.16@cse.mrt.ac.lk>
2021-06-22 09:19:23 +0530

    Correct xml:setName substring index

--- langlib/lang.xml/src/main/java/org/ballerinalang/langlib/xml/SetName.java
+++ langlib/lang.xml/src/main/java/org/ballerinalang/langlib/xml/SetName.java
@@ -53,9 +53,9 @@ public class SetName {
             if (xmlVal.getNodeType() == XmlNodeType.ELEMENT) {
                 QName newQName;
                 if (newName.startsWith("{")) {
-                    int endCurly = newName.indexOf('}') + 1;
-                    String nsUri = newName.substring(0, endCurly);
-                    String localPart = newName.substring(endCurly, newName.length());
+                    int endCurly = newName.indexOf('}');
+                    String nsUri = newName.substring(1, endCurly);
+                    String localPart = newName.substring(endCurly + 1, newName.length());
                     newQName = new QName(nsUri, localPart);
                 } else {
                     newQName = new QName(newName);
