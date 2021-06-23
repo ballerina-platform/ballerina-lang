ballerina-platform/ballerina-lang

commit b7d0753d48169c40e74f3f76ed9fdd8853f022d9

Fix unresolved symbols when flushing compiler package cache

compiler/ballerina-lang/src/main/java/io/ballerina/projects/Package.java
@@ -2,6 +2,7 @@ package io.ballerina.projects;
 
 import io.ballerina.projects.internal.ManifestBuilder;
 import io.ballerina.projects.internal.model.CompilerPluginDescriptor;
+import org.ballerinalang.model.elements.PackageID;
 import org.wso2.ballerinalang.compiler.PackageCache;
 import org.wso2.ballerinalang.compiler.util.CompilerContext;
 
@@ -420,13 +421,24 @@ public class Package {
 
             DependencyGraph<ResolvedPackageDependency> newDepGraph = this.project.currentPackage().getResolution()
                     .dependencyGraph();
-            if (!this.dependencyGraph.difference(newDepGraph).isEmpty()) {
-                // A non-empty diff means deletion of nodes from the old graph is
-                // required to get the new graph, hence we flush out the package cache.
+            Set<ResolvedPackageDependency> diff = this.dependencyGraph.difference(newDepGraph);
+            if (!diff.isEmpty()) {
+                // A non-empty diff means deletion of nodes from the old graph is required
+                // to get the new graph, hence we remove these modules from the package caches.
                 CompilerContext compilerContext = project.projectEnvironmentContext()
                         .getService(CompilerContext.class);
                 PackageCache packageCache = PackageCache.getInstance(compilerContext);
-                packageCache.flush();
+                for (ResolvedPackageDependency dependency : diff) {
+                    for (ModuleId moduleId : dependency.packageInstance().moduleIds()) {
+                        Module module = dependency.packageInstance().module(moduleId);
+                        PackageID packageID = module.descriptor().moduleCompilationId();
+                        // remove the module from the compiler packageCache
+                        packageCache.remove(packageID);
+                        // we need to also reset the module in the project environment packageCache
+                        // to make the module recompile and add symbols
+                        module.modify().apply();
+                    }
+                }
             }
             return this.project.currentPackage();
         }

compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/PackageCache.java
@@ -74,9 +74,10 @@ public class PackageCache {
         packageMap.put(getCacheID(packageID), bLangPackage);
     }
 
-    public void flush() {
-        packageMap.clear();
-        packageSymbolMap.clear();
+    public void remove(PackageID packageID) {
+        packageMap.remove(getCacheID(packageID));
+        String[] packageElements = packageID.toString().split(Names.VERSION_SEPARATOR.value);
+        packageSymbolMap.remove(packageElements[0]);
     }
 
     public static String getCacheID(PackageID packageID) {

project-api/project-api-test/src/test/java/io/ballerina/projects/test/DependencyGraphTests.java
@@ -51,7 +51,7 @@ public class DependencyGraphTests extends BaseTest {
         DependencyGraph<ResolvedPackageDependency> dependencyGraphOld =
                 project.currentPackage().getResolution().dependencyGraph();
         // dependency graph should contain self and package_dep
-        Assert.assertEquals(dependencyGraphOld.getNodes().size(), 2);
+        Assert.assertEquals(dependencyGraphOld.getNodes().size(), 3);
 
         // verify that the compiler package cache contains package_dep
         PackageCache packageCache = PackageCache.getInstance(
@@ -76,7 +76,7 @@ public class DependencyGraphTests extends BaseTest {
         DependencyGraph<ResolvedPackageDependency> dependencyGraphNew =
                 project.currentPackage().getResolution().dependencyGraph();
         // dependency graph should contain self and package_c
-        Assert.assertEquals(dependencyGraphNew.getNodes().size(), 2);
+        Assert.assertEquals(dependencyGraphNew.getNodes().size(), 3);
 
         // verify that the package cache is flushed
         Assert.assertNull(packageCache.getSymbol(packageID));

project-api/project-api-test/src/test/java/io/ballerina/projects/test/TestBuildProject.java
@@ -79,7 +79,7 @@ import static org.testng.Assert.assertEquals;
  *
  * @since 2.0.0
  */
-public class TestBuildProject {
+public class TestBuildProject extends BaseTest {
     private static final Path RESOURCE_DIRECTORY = Paths.get("src/test/resources/");
     private final String dummyContent = "function foo() {\n}";
 
@@ -947,6 +947,16 @@ public class TestBuildProject {
                         .packageInstance().packageName().toString().equals("package_dep")).findFirst().get();
         Assert.assertEquals(packageDepNew.packageInstance().packageVersion().toString(), "0.1.1");
         Assert.assertEquals(newCompilation.diagnosticResult().diagnosticCount(), 1);
+
+        // Set the old version again
+        project.currentPackage().dependenciesToml()
+                .get().modify().withContent("" +
+                "[[dependency]]\n" +
+                "org = \"foo\"\n" +
+                "name = \"package_dep\"\n" +
+                "version = \"0.1.0\"\n").apply();
+        PackageCompilation newCompilation2 = project.currentPackage().getCompilation();
+        Assert.assertEquals(newCompilation2.diagnosticResult().diagnosticCount(), 0);
     }
 
     @Test(description = "tests if other documents can be edited ie. Dependencies.toml, Package.md")

project-api/project-api-test/src/test/resources/projects_for_edit_api_tests/package_test_dependencies_toml/main.bal
@@ -1,4 +1,5 @@
 import foo/package_dep;
+import samjs/package_c as _;
 
 public function main() {
     package_dep:main();

commit 7f647a5625c0246a1a0b6c5c9fa63eb97d1fa39e

Fix unresolved symbols bug introduced in b7d0753

compiler/ballerina-lang/src/main/java/io/ballerina/projects/Package.java
@@ -430,13 +430,15 @@ public class Package {
                 PackageCache packageCache = PackageCache.getInstance(compilerContext);
                 for (ResolvedPackageDependency dependency : diff) {
                     for (ModuleId moduleId : dependency.packageInstance().moduleIds()) {
-                        Module module = dependency.packageInstance().module(moduleId);
-                        PackageID packageID = module.descriptor().moduleCompilationId();
-                        // remove the module from the compiler packageCache
-                        packageCache.remove(packageID);
-                        // we need to also reset the module in the project environment packageCache
-                        // to make the module recompile and add symbols
-                        module.modify().apply();
+                        if (!dependency.packageInstance().descriptor().isLangLibPackage()) {
+                            Module module = dependency.packageInstance().module(moduleId);
+                            PackageID packageID = module.descriptor().moduleCompilationId();
+                            // remove the module from the compiler packageCache
+                            packageCache.remove(packageID);
+                            // we need to also reset the module in the project environment packageCache
+                            // to make the module recompile and add symbols
+                            module.moduleContext().setCompilationState(null);
+                        }
                     }
                 }
             }