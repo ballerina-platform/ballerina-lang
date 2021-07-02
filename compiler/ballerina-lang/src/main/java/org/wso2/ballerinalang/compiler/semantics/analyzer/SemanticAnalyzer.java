bebfdbb8a193732e028a0d7ecf12e46bb7b962f4
Dhananjaya Wickramasingha <dhananjayaw@wso2.com>
2021-07-02 12:33:43 +0530

    Infer listener type
    when type-descriptor is not provided
    and the listener's init method may return an error

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SemanticAnalyzer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SemanticAnalyzer.java
@@ -1289,9 +1289,13 @@ public class SemanticAnalyzer extends BLangNodeVisitor {
                     rhsType = symTable.semanticError;
                 }
 
-                if (variable.flagSet.contains(Flag.LISTENER) && !types.checkListenerCompatibility(rhsType)) {
-                    dlog.error(varRefExpr.pos, DiagnosticErrorCode.INCOMPATIBLE_TYPES, LISTENER_NAME, rhsType);
-                    return;
+                if (variable.flagSet.contains(Flag.LISTENER)) {
+                    BType listenerType = getListenerType(rhsType);
+                    if (listenerType == null) {
+                        dlog.error(varRefExpr.pos, DiagnosticErrorCode.INCOMPATIBLE_TYPES, LISTENER_NAME, rhsType);
+                        return;
+                    }
+                    rhsType = listenerType;
                 }
 
                 BLangSimpleVariable simpleVariable = (BLangSimpleVariable) variable;
@@ -1401,6 +1405,32 @@ public class SemanticAnalyzer extends BLangNodeVisitor {
 
                 validateAnnotationAttachmentCount(errorVariable.annAttachments);
                 break;
+        }
+    }
+
+    private BType getListenerType(BType type) {
+        LinkedHashSet<BType> compatibleTypes = new LinkedHashSet<>();
+        if (type.tag == TypeTags.UNION) {
+            for (BType t : ((BUnionType) type).getMemberTypes()) {
+                if (t.tag == TypeTags.ERROR) {
+                    continue;
+                }
+                if (types.checkListenerCompatibility(t)) {
+                    compatibleTypes.add(t);
+                } else {
+                    return null;
+                }
+            }
+        } else if (types.checkListenerCompatibility(type)) {
+            compatibleTypes.add(type);
+        }
+
+        if (compatibleTypes.isEmpty()) {
+            return null;
+        } else if (compatibleTypes.size() == 1) {
+            return compatibleTypes.iterator().next();
+        } else {
+            return BUnionType.create(null, compatibleTypes);
         }
     }
 
