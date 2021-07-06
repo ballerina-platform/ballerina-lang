2969e879420381e719165ff6d5f0fab17a00a596
chiranSachintha <chiransachintha1994@gmail.com>
2021-07-06 10:46:41 +0530

    Add unit tests for function pointers with defaultable parameters

---
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/types/function/BFunctionTypeTest.java
@@ -0,0 +1,41 @@
+/*
+ *   Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ballerinalang.test.types.function;
+
+import org.ballerinalang.test.BCompileUtil;
+import org.ballerinalang.test.BRunUtil;
+import org.ballerinalang.test.CompileResult;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Test cases for Function type.
+ */
+public class BFunctionTypeTest {
+    private CompileResult functionTypeTestResult;
+
+    @BeforeClass
+    public void setup() {
+        functionTypeTestResult = BCompileUtil.compile("test-src/types/function/function_type.bal");
+    }
+
+    @Test
+    public void testFunctionType() {
+        BRunUtil.invoke(functionTypeTestResult, "testFunctionType");
+    }
+}
