a4f597084453bb68e714cb43425ea2273f00be63
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2021-07-08 17:34:16 +0530

    Add resource keyword suggestions within the class body

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/providers/context/ClassDefinitionNodeContext.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/providers/context/ClassDefinitionNodeContext.java
@@ -91,6 +91,7 @@ public class ClassDefinitionNodeContext extends AbstractCompletionProvider<Class
         completionItems.add(new SnippetCompletionItem(context, Snippet.KW_PUBLIC.get()));
         completionItems.add(new SnippetCompletionItem(context, Snippet.KW_FINAL.get()));
         completionItems.add(new SnippetCompletionItem(context, Snippet.KW_REMOTE.get()));
+        completionItems.add(new SnippetCompletionItem(context, Snippet.KW_RESOURCE.get()));
         completionItems.add(new SnippetCompletionItem(context, Snippet.DEF_REMOTE_FUNCTION.get()));
         completionItems.add(new SnippetCompletionItem(context, Snippet.DEF_FUNCTION.get()));
         completionItems.add(new SnippetCompletionItem(context, Snippet.KW_ISOLATED.get()));
