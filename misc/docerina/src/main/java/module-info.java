63413eefcf09f404b703272d662aca9d3f612e55
warunalakshitha <warudanu@gmail.com>
2021-07-15 22:37:14 +0530

    Remove usages of logging and config modules

--- misc/docerina/src/main/java/module-info.java
+++ misc/docerina/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module io.ballerina.docerina {
     exports org.ballerinalang.docgen.docs;
     requires io.ballerina.lang;
-    requires io.ballerina.config;
     requires com.google.gson;
     requires org.apache.commons.io;
     requires slf4j.api;
