4879177042733f71ccb9747a85889c1ca1cd4457
Chiran Fernando <pcnfernando@gmail.com>
2021-07-20 10:44:24 +0530

    Introduce type def symbol and type reference type

---
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/model/symbols/BTypeDefinitionSymbol.java
@@ -0,0 +1,71 @@
+/*
+*  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.wso2.ballerinalang.compiler.semantics.model.symbols;
+
+import io.ballerina.compiler.api.symbols.Qualifier;
+import io.ballerina.tools.diagnostics.Location;
+import org.ballerinalang.model.elements.PackageID;
+import org.ballerinalang.model.symbols.SymbolKind;
+import org.ballerinalang.model.symbols.SymbolOrigin;
+import org.ballerinalang.model.symbols.TypeSymbol;
+import org.wso2.ballerinalang.compiler.semantics.model.types.BType;
+import org.wso2.ballerinalang.compiler.semantics.model.types.BTypeReferenceType;
+import org.wso2.ballerinalang.compiler.util.Name;
+import org.wso2.ballerinalang.compiler.util.Names;
+import org.wso2.ballerinalang.util.Flags;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @since 0.94
+ */
+public class BTypeDefinitionSymbol extends BTypeSymbol {
+
+    public boolean isLabel;
+    public BTypeReferenceType referenceType = null;
+
+    public List<Qualifier> qualifiers;
+    private final boolean readonly;
+
+    public BTypeDefinitionSymbol(int symTag, long flags, Name name, PackageID pkgID, BType type, BSymbol owner,
+                                 Location pos, SymbolOrigin origin) {
+        super(symTag, flags, name, pkgID, type, owner, pos, origin);
+        this.readonly = Symbols.isFlagOn(owner.flags, Flags.READONLY);
+        this.isLabel = false;
+        this.kind = SymbolKind.TYPE_DEF;
+    }
+
+    @Override
+    public String toString() {
+        if (this.pkgID == PackageID.DEFAULT ||
+                this.pkgID.equals(PackageID.ANNOTATIONS) ||
+                this.pkgID.name == Names.DEFAULT_PACKAGE) {
+            return this.name.value;
+        }
+        return this.pkgID.toString() + ":" + this.name;
+    }
+
+    @Override
+    public BTypeDefinitionSymbol createLabelSymbol() {
+        BTypeDefinitionSymbol typeSymbol = Symbols.createTypeDefinitionSymbol(SymTag.TYPE_DEF, flags, Names.EMPTY,
+                pkgID, type, owner, pos, origin);
+        typeSymbol.isLabel = true;
+        return typeSymbol;
+    }
+}
