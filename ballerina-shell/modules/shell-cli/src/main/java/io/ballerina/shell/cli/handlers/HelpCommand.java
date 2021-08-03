aeddfc551f2b94fd98616c990f1917431cb13a00
DamithaSenevirathne <damithasenevirathne@gmail.com>
2021-08-03 11:38:44 +0530

    Update help /topic message

--- ballerina-shell/modules/shell-cli/src/main/java/io/ballerina/shell/cli/handlers/HelpCommand.java
+++ ballerina-shell/modules/shell-cli/src/main/java/io/ballerina/shell/cli/handlers/HelpCommand.java
@@ -58,8 +58,8 @@ public class HelpCommand extends AbstractCommand {
         } else {
             String topic = String.join(" ", Arrays.copyOfRange(args, 0, args.length));
             try {
-                ballerinaShell.outputInfo(DESCRIPTION_PREFIX + NEW_LINE + bbeHelpProvider.getDescription(topic)
-                        .replaceAll(TAGS, EMPTY_STRING));
+                ballerinaShell.outputInfo(DESCRIPTION_PREFIX + NEW_LINE + NEW_LINE +
+                        bbeHelpProvider.getDescription(topic).replaceAll(TAGS, EMPTY_STRING));
                 ballerinaShell.outputInfo(URL_PREFIX + URL + topic.replaceAll(" ", "-"));
 
             } catch (HelpProviderException e) {
