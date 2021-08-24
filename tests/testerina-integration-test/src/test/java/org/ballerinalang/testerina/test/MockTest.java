48b0310733d5af73931d9672cabbf656b64e7e64
Fathima Dilhasha <dilhasha.uom@gmail.com>
2021-08-24 15:54:48 +0530

    Add error object mocking test

--- tests/testerina-integration-test/src/test/java/org/ballerinalang/testerina/test/MockTest.java
+++ tests/testerina-integration-test/src/test/java/org/ballerinalang/testerina/test/MockTest.java
@@ -60,7 +60,7 @@ public class MockTest extends BaseTestCase {
 
     @Test()
     public void testObjectMocking() throws BallerinaTestException {
-        String msg1 = "7 passing";
+        String msg1 = "8 passing";
         String msg2 = "6 failing";
         String[] args = mergeCoverageArgs(new String[]{"object-mocking-tests"});
         String output = balClient.runMainAndReadStdOut("test", args,
