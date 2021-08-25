c4c0a1dd8a51e6a76d68e3634c2bc0eab25048cf
Kanushka Gayan <kanushkanet@gmail.com>
2021-08-25 11:18:08 +0530

    Update lang-server extension's getConnector method with connector metadata generation library

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/extensions/connector/ConnectorTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/extensions/connector/ConnectorTest.java
@@ -19,8 +19,8 @@
 package org.ballerinalang.langserver.extensions.connector;
 
 import org.ballerinalang.langserver.extensions.LSExtensionTestUtil;
-import org.ballerinalang.langserver.extensions.ballerina.connector.BallerinaConnectorResponse;
 import org.ballerinalang.langserver.extensions.ballerina.connector.BallerinaConnectorServiceImpl;
+import org.ballerinalang.langserver.extensions.ballerina.connector.BallerinaConnectorsResponse;
 import org.ballerinalang.langserver.util.FileUtils;
 import org.ballerinalang.langserver.util.TestUtil;
 import org.eclipse.lsp4j.jsonrpc.Endpoint;
@@ -47,30 +47,30 @@ public class ConnectorTest {
         System.setProperty(BallerinaConnectorServiceImpl.DEFAULT_CONNECTOR_FILE_KEY, connectorToml.toString());
         this.serviceEndpoint = TestUtil.initializeLanguageSever();
     }
-//
-//    @Test(description = "Test getting all connectors.")
-//    public void getConnectors() {
-//        BallerinaConnectorsResponse connectorsResponse = LSExtensionTestUtil
-//                .getConnectors(this.serviceEndpoint);
-//        Assert.assertEquals(connectorsResponse.getConnectors().size(), 4);
-//        Assert.assertEquals(connectorsResponse.getConnectors().get(0).getModule(), "nats");
-//        Assert.assertEquals(connectorsResponse.getConnectors().get(0).getName(), "Producer");
-//    }
 
-    @Test(description = "Test getting HTTP connectors.")
-    public void getHTTPConnector() {
-        String org = "ballerina";
-        String module = "http";
-        String version = "1.0.0";
-        String name = "Client";
-        String displayName = "http:Client";
-        BallerinaConnectorResponse connectorsResponse = LSExtensionTestUtil
-                .getConnector(org, module, version, name,
-                        displayName, true, this.serviceEndpoint);
-        Assert.assertNotNull(connectorsResponse);
-        Assert.assertEquals(org, connectorsResponse.getOrg());
-        Assert.assertEquals(module, connectorsResponse.getModule());
+    @Test(description = "Test getting all connectors.")
+    public void getConnectors() {
+        BallerinaConnectorsResponse connectorsResponse = LSExtensionTestUtil
+                .getConnectors(this.serviceEndpoint);
+        Assert.assertEquals(connectorsResponse.getConnectors().size(), 4);
+        Assert.assertEquals(connectorsResponse.getConnectors().get(0).getModule(), "nats");
+        Assert.assertEquals(connectorsResponse.getConnectors().get(0).getName(), "Producer");
     }
+
+//    @Test(description = "Test getting HTTP connectors.")
+//    public void getHTTPConnector() {
+//        String org = "ballerina";
+//        String module = "http";
+//        String version = "1.0.0";
+//        String name = "Client";
+//        String displayName = "http:Client";
+//        BallerinaConnectorResponse connectorsResponse = LSExtensionTestUtil
+//                .getConnector(org, module, version, name,
+//                        displayName, true, this.serviceEndpoint);
+//        Assert.assertNotNull(connectorsResponse);
+//        Assert.assertEquals(org, connectorsResponse.getOrg());
+//        Assert.assertEquals(module, connectorsResponse.getModule());
+//    }
 
 //    @Test(description = "Test getting twitter connectors.")
 //    public void getTwitterConnector() {
