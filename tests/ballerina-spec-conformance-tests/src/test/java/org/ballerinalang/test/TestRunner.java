c68935519870a9772fafa33287e41a51120b1448
chiranSachintha <chiransachintha1994@gmail.com>
2021-08-31 14:56:38 +0530

    Implement test runner for spec conformance tests

---
+++ tests/ballerina-spec-conformance-tests/src/test/java/org/ballerinalang/test/TestRunner.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ballerinalang.test;
+
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Test runner for run spec conformance tests through compiler phases.
+ *
+ * @since 2.0.0
+ */
+public class TestRunner {
+
+    private final Path ballerinaLangDir = Paths.get("").toAbsolutePath().getParent().getParent();
+    private final Path testDir = ballerinaLangDir.resolve("tests").resolve("ballerina-spec-conformance-tests");
+    private final Path testSourcesDirectory = Paths.get("src/test/resources").toAbsolutePath().normalize();
+    CompileResult compileResult;
+
+    @Test(dataProvider = "spec-conformance-test-file-provider")
+    public void test(String type, String path, List<String> outputValues, List<Integer> errLines, String fileName,
+                                                                                                       int absLineNum) {
+        try {
+            compileResult = BCompileUtil.compile(path);
+            Files.delete(testSourcesDirectory.resolve((new File(path)).toPath()));
+        } catch (Exception e) {
+            Assert.fail("failed to run spec conformance test: \"" + fileName + "\"", e);
+        }
+
+        if (!type.equals(TestRunnerUtils.ERROR)) {
+            BRunUtil.ExitDetails exitDetails = BRunUtil.run(compileResult, new String[]{});
+            if (exitDetails.exitCode != 0) {
+                String errOutput = exitDetails.errorOutput;
+                // TODO: handle panic state
+            }
+            // TODO: handle output state
+        } else {
+            TestRunnerUtils.validateError(compileResult, errLines, fileName, absLineNum);
+        }
+    }
+
+    public HashSet<String> skipList() {
+        // to skip balt file
+        HashSet<String> hashSet = new HashSet<>();
+        return hashSet;
+    }
+
+
+    public HashSet<String> runSelectedTests() {
+        final String[] setValues = new String[] {Labels.BOOLEAN_TYPE_DESCRIPTOR};
+        return new HashSet<>(Arrays.asList(setValues));
+    }
+
+    @DataProvider(name = "spec-conformance-test-file-provider")
+    public Iterator<Object[]> dataProvider() {
+        HashSet<String> skippedTests = skipList();
+        Set<String> labels = runSelectedTests();
+        List<Object[]> testCases = new ArrayList<>();
+        try {
+            Files.walk(testDir.resolve("src").resolve("test").resolve("resources").resolve("ballerina-spec-tests")
+                    .resolve("conformance"))
+                    .filter(path -> {
+                        File file = path.toFile();
+                        return file.isFile() && file.getName().endsWith(".balt")
+                                && !skippedTests.contains(file.getName());
+                    })
+                    .map(path -> new Object[]{path.toFile().getName(), path.toString()})
+                    .forEach(object -> {
+                        try {
+                            TestRunnerUtils.readTestFile((String) object[0], (String) object[1], testCases, labels);
+                        } catch (IOException e) {
+                            Assert.fail("Can't resolve spec conformance tests", e);
+                        }
+                    });
+            return testCases.iterator();
+        } catch (IOException e) {
+            Assert.fail("Can't resolve spec conformance tests", e);
+            return testCases.iterator();
+        }
+    }
+}
---
+++ tests/ballerina-spec-conformance-tests/src/test/java/org/ballerinalang/test/TestRunnerUtils.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ballerinalang.test;
+
+import io.ballerina.tools.diagnostics.Diagnostic;
+import org.testng.Assert;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * Test util class for all spec conformance test cases.
+ *
+ * @since 2.0.0
+ */
+public class TestRunnerUtils {
+
+    private static final String START_TEST_CASE = "Test-Case";
+    private static final String DESCRIPTION = "Description";
+    private static final String AUTHOR = "Author";
+    private static final String FAIL_ISSUE = "Fail-Issue";
+    private static final String LABELS = "Labels";
+    public static final String OUTPUT = "output";
+    public static final String PANIC = "panic";
+    public static final String ERROR = "error";
+    public static final String SKIPPED_TEST = "skipped-test";
+    private static final String EMPTY_STRING = "";
+    private static final String COLON = ":";
+    private static final String NEW_LINE_CHARACTER = "\n";
+    private static final String BAL_EXTENSION = ".bal";
+    public static final String RESOURCE_DIR = "src/test/resources/";
+    public static final String TEMP_DIR = "test-src/TempFiles/";
+    private static final String IMPORT_BALLERINAI = "import ballerinai/io";
+    private static int fileCount = 0;
+    private static int absLineNum;
+
+
+    public static void readTestFile(String fileName, String path, List<Object[]> testCases, Set<String> labels)
+                                                                                                    throws IOException {
+        File testFile = new File(path);
+        FileReader fileReader = new FileReader(testFile);
+        BufferedReader buffReader = new BufferedReader(fileReader);
+        String line = buffReader.readLine();
+        absLineNum = 1;
+        String pattern = String.format("^(%s)\\s*:\\s*.*", START_TEST_CASE);
+        while (line != null) {
+            if (Pattern.matches(pattern, line)) {
+                line = createTestFile(fileName, line, buffReader, testCases, labels);
+            } else {
+                line = buffReader.readLine();
+                absLineNum++;
+            }
+        }
+        buffReader.close();
+    }
+
+    private static String createTestFile(String fileName, String typeOfTestFile, BufferedReader buffReader,
+                                         List<Object[]>  testCases, Set<String> labels) throws IOException {
+        String tempFileName = fileName.substring(0, fileName.indexOf(".")) + fileCount++;
+        String tempDir = TEMP_DIR + tempFileName + BAL_EXTENSION;
+        File tempBalFile = new File(RESOURCE_DIR + tempDir);
+        String type = readHeadersOfTestCase(buffReader, typeOfTestFile, labels);
+        if (type.equals(SKIPPED_TEST)) {
+            return EMPTY_STRING;
+        }
+        List<String> outputValues = new ArrayList<>();
+        List<Integer> errLines = new ArrayList<>();
+        Object[] testCase = new Object[6];
+        testCase[0] = type;
+        FileWriter tempFileWriter = new FileWriter(tempBalFile);
+        boolean isOutputTest = type.equals(OUTPUT);
+        if (isOutputTest) {
+            tempFileWriter.write(IMPORT_BALLERINAI + NEW_LINE_CHARACTER);
+        }
+        String line;
+        int relativeLineNum = 0;
+        while ((line = buffReader.readLine()) != null) {
+            if (!isOutputTest) {
+                relativeLineNum = relativeLineNum + 1;
+            }
+            if (line.startsWith(START_TEST_CASE)) {
+                break;
+            }
+            String pattern = String.format(".*//\\s*@\\s*(%s)\\s*.*", type);
+            if (Pattern.matches(pattern, line)) {
+                String output = line.substring(line.indexOf(type) + type.length()).trim();
+                if (!type.equals(OUTPUT)) {
+                    errLines.add(relativeLineNum);
+                }
+                outputValues.add(output);
+            } else if (Pattern.matches(".*//.*", line)) {
+                throw new IllegalStateException("Incorrect output(comment)");
+            }
+            tempFileWriter.write(line + NEW_LINE_CHARACTER);
+        }
+        tempFileWriter.close();
+        testCase[1] = tempDir;
+        testCase[2] = outputValues;
+        testCase[3] = errLines;
+        testCase[4] = fileName;
+        testCase[5] = absLineNum;
+        absLineNum = absLineNum + relativeLineNum;
+        testCases.add(testCase);
+        return line;
+    }
+
+
+
+    private static String checkTypeOfTestCase(String typeOfTestFile) {
+        String pattern = String.format(".*:\\s*(%s|%s|%s)$", OUTPUT, ERROR, PANIC);
+        if (Pattern.matches(pattern, typeOfTestFile.trim())) {
+            return typeOfTestFile.substring(typeOfTestFile.indexOf(COLON) + 1).trim();
+        }
+        throw new AssertionError(String.format("Incorrect test type in line %d expected OUTPUT, ERROR, PANIC",
+                absLineNum));
+    }
+
+    private static String readHeadersOfTestCase(BufferedReader buffReader, String typeOfTestFile,
+                                                Set<String> selectedLabels) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        String line;
+
+        String pattern = String.format("^(%s|%s|%s|%s)\\s*:\\s*.*", DESCRIPTION, AUTHOR, FAIL_ISSUE, LABELS);
+        String type = checkTypeOfTestCase(typeOfTestFile);
+        String key = null;
+        String value = null;
+        while ((line = buffReader.readLine()) != null) {
+            absLineNum++;
+            String header = line.strip();
+            if (header.equals(EMPTY_STRING)) {
+                headers.put(key, value);
+                break;
+            } else if (Pattern.matches(pattern, header)) {
+                if (key != null) {
+                    headers.put(key, value);
+                }
+                int index = line.indexOf(COLON);
+                key = header.substring(0, index).trim();
+                value = header.substring(index + 1).trim();
+                if (key.equals(LABELS)) {
+                    String[] labels = value.split("\\s*,\\s*");
+                    areLabelsDefined(labels);
+                    type = isTestCaseSkipped(selectedLabels, labels, type);
+                }
+            } else {
+                value = value + " " + header.trim();
+            }
+        }
+        return type;
+    }
+
+    private static void areLabelsDefined(String[] labels) {
+        for (String label : labels) {
+            if (!org.ballerinalang.test.Labels.LABELS.contains(label)) {
+                Assert.fail(String.format("%s is not defined", label));
+            }
+        }
+    }
+
+    private static String isTestCaseSkipped(Set<String> selectedLabels, String[] labels, String type) {
+        if (selectedLabels.isEmpty()) {
+            return type;
+        }
+        for (String label : labels) {
+            if (selectedLabels.contains(label)) {
+                return SKIPPED_TEST;
+            }
+        }
+        return type;
+    }
+
+    public static void validateError(CompileResult result, List<Integer> errLines, String filename, int absLineNum) {
+        for (int i = 0; i < result.getDiagnostics().length; i++) {
+            Diagnostic diagnostic = result.getDiagnostics()[i];
+            int expectedErrLine = errLines.get(i);
+            Assert.assertEquals(diagnostic.location().lineRange().startLine().line() + 1 + absLineNum,
+                    expectedErrLine + absLineNum, String.format("In %s, incorrect line number:", filename));
+        }
+    }
+}
