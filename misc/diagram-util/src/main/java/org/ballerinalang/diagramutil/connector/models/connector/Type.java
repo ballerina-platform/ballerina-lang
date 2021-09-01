cf9cf0b0d6e38016708ecf298a32fa5a86995e26
Kanushka Gayan <kanushkanet@gmail.com>
2021-09-01 10:48:36 +0530

    Add copyright header

--- misc/diagram-util/src/main/java/org/ballerinalang/diagramutil/connector/models/connector/Type.java
+++ misc/diagram-util/src/main/java/org/ballerinalang/diagramutil/connector/models/connector/Type.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.ballerinalang.diagramutil.connector.models.connector;
 
 import com.google.gson.annotations.Expose;
@@ -173,9 +190,11 @@ public class Type {
             RecordTypeSymbol recordTypeSymbol = (RecordTypeSymbol) symbol;
             String symbolName = ((BallerinaRecordTypeSymbol) recordTypeSymbol).getBType().toString();
             if (parentSymbols.contains(symbolName)) {
-                return null; // End the recursion if parent record found
+                return null;
+            } else if (!symbolName.contains("record {")) {
+                // TODO: need to figure it out anonymous record types proper way
+                parentSymbols.add(symbolName);
             }
-            parentSymbols.add(symbolName);
 
             List<Type> fields = new ArrayList<>();
             recordTypeSymbol.fieldDescriptors().forEach((name, field) -> {
