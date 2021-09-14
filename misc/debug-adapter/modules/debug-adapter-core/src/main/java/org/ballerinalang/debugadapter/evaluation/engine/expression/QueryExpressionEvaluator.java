4c09ef573d9589889cea03eebb7953de314d7c2f
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2021-09-14 09:33:01 +0530

    Add review suggestions

--- misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/evaluation/engine/expression/QueryExpressionEvaluator.java
+++ misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/evaluation/engine/expression/QueryExpressionEvaluator.java
@@ -36,7 +36,7 @@ public class QueryExpressionEvaluator extends ExpressionAsProgramEvaluator {
     private static ExpressionNode getQueryWithTypeCasts(QueryExpressionNode syntaxNode) {
         if (syntaxNode.parent() != null && syntaxNode.parent().kind() == SyntaxKind.TYPE_CAST_EXPRESSION) {
             // Since Ballerina query expression return type might depend on the contextual type, we need to inject
-            // any existing type castings o top of the query expressions (expression will results in semantic errors
+            // any existing type castings on top of the query expressions (expression will results in semantic errors
             // otherwise.)
             return (TypeCastExpressionNode) syntaxNode.parent();
         } else {
