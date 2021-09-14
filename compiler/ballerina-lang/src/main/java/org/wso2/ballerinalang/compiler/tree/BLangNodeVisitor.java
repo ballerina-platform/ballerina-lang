f84823e493fc86b6372f1ddd1d3c09f761d911ae
Hasitha Aravinda <hasithaa@users.noreply.github.com>
2021-09-14 08:31:54 +0530

    Update doc comments

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/tree/BLangNodeVisitor.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/tree/BLangNodeVisitor.java
@@ -204,6 +204,7 @@ import org.wso2.ballerinalang.compiler.tree.types.BLangValueType;
 
 /**
  * @since 0.94
+ * @deprecated since 2.0.0. Use {@link BLangNodeAnalyzer} or {@link BLangNodeTransformer}.
  */
 public abstract class BLangNodeVisitor {
 
