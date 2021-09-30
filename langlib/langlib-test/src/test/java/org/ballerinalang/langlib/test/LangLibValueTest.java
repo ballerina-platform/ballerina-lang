126aad919bb5c266b0ae9e6db9e3df330b55eee8
Nadeeshan96 <nadeeshand@wso2.com>
2021-09-30 11:56:02 +0530

    Modify json parser tests and error messages

--- langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibValueTest.java
+++ langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibValueTest.java
@@ -135,6 +135,11 @@ public class LangLibValueTest {
     }
 
     @Test
+    public void testFromJsonStringNegative() {
+        BRunUtil.invokeFunction(compileResult, "testFromJsonStringNegative");
+    }
+
+    @Test
     public void testFromJsonFloatString() {
 
         BValue[] returns = BRunUtil.invokeFunction(compileResult, "testFromJsonFloatString");
