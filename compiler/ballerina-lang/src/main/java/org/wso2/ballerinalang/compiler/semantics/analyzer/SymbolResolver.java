c2a1d352a65e76f0c14e78524009f41ee75c1ba8
Chiran Fernando <pcnfernando@gmail.com>
2021-10-06 11:44:44 +0530

    Remove hack used for enum shadowing

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolResolver.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolResolver.java
@@ -320,11 +320,7 @@ public class SymbolResolver extends BLangNodeVisitor {
 
     private boolean hasSameOwner(BSymbol symbol, BSymbol foundSym) {
         // check whether the given symbol owner is same as found symbol's owner
-        if (foundSym.owner == symbol.owner ||
-                //TODO: remove this, once #26284, #30688 are fixed
-                (foundSym.owner.tag & SymTag.PACKAGE) == SymTag.PACKAGE &&
-                (symbol.owner.tag & SymTag.PACKAGE) == SymTag.PACKAGE &&
-                foundSym.pkgID.equals(symbol.pkgID)) {
+        if (foundSym.owner == symbol.owner) {
             return true;
         } else if (Symbols.isFlagOn(symbol.owner.flags, Flags.LAMBDA) &&
                 ((foundSym.owner.tag & SymTag.INVOKABLE) == SymTag.INVOKABLE)) {
