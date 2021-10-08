450ebf7cfacbd307faddfa4ead82bee94f7a1ef0
ushirask <ushirask@gmail.com>
2021-10-08 11:15:18 +0530

    Fix merge conflict

--- semtypes/src/main/java/io/ballerina/types/typeops/DecimalOps.java
+++ semtypes/src/main/java/io/ballerina/types/typeops/DecimalOps.java
@@ -18,10 +18,10 @@
 package io.ballerina.types.typeops;
 
 import io.ballerina.types.Common;
+import io.ballerina.types.Context;
 import io.ballerina.types.EnumerableDecimal;
 import io.ballerina.types.EnumerableSubtype;
 import io.ballerina.types.SubtypeData;
-import io.ballerina.types.TypeCheckContext;
 import io.ballerina.types.UniformTypeOps;
 import io.ballerina.types.subtypedata.DecimalSubtype;
 
@@ -61,7 +61,7 @@ public class DecimalOps extends CommonOps implements UniformTypeOps {
     }
 
     @Override
-    public boolean isEmpty(TypeCheckContext tc, SubtypeData t) {
+    public boolean isEmpty(Context tc, SubtypeData t) {
         return Common.notIsEmpty(tc, t);
     }
 }
