0f859ebe39956d998a28bec5b8a70fea18dcd441
Maryam Ziyad <maryamziyadm@gmail.com>
2021-10-15 12:39:25 +0530

    Fix merge issue

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/javainterop/basic/NegativeValidationTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/javainterop/basic/NegativeValidationTest.java
@@ -458,24 +458,4 @@ public class NegativeValidationTest {
                 + "'No parameter is required to get the value of the static field 'isAvailable' in class " +
                 "'org/ballerinalang/nativeimpl/jvm/tests/JavaFieldAccessMutate''", 7, 1);
     }
-
-    @Test
-    public void testNoClassDefFoundError() {
-        String path = "test-src/javainterop/negative/project_no_class_def_found";
-        CompileResult compileResult = BCompileUtil.compile(path);
-        Assert.assertEquals(compileResult.getDiagnostics().length, 1);
-        BAssertUtil.validateError(compileResult, 0,
-                "{ballerina/jballerina.java}NO_CLASS_DEF_FOUND 'Class definition 'javalibs/app/Bar' " +
-                        "not found'", 19, 1);
-    }
-
-    @Test
-    public void testNoClassDefFoundErrorForConstructorCalls() {
-        String path = "test-src/javainterop/negative/project_no_class_def_found_constructor";
-        CompileResult compileResult = BCompileUtil.compile(path);
-        Assert.assertEquals(compileResult.getDiagnostics().length, 1);
-        BAssertUtil.validateError(compileResult, 0,
-                "{ballerina/jballerina.java}NO_CLASS_DEF_FOUND 'Class definition 'javalibs/app/Bar' " +
-                        "not found'", 19, 1);
-    }
 }
