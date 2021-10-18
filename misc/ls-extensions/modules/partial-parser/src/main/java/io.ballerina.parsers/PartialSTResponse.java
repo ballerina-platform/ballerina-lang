1c3350e2cbffcff5d36b17d774e326c526d0b24d
Madusha <madushajg@gmail.com>
2021-10-18 19:11:49 +0530

    Add initial partial parser service

---
+++ misc/ls-extensions/modules/partial-parser/src/main/java/io.ballerina.parsers/PartialSTResponse.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package io.ballerina.parsers;
+
+import io.ballerina.compiler.syntax.tree.NodeList;
+import io.ballerina.compiler.syntax.tree.StatementNode;
+
+/**
+ * Response format for JsonToBalRecord endpoint.
+ *
+ * @since 2.0.0
+ */
+public class PartialSTResponse {
+
+    private NodeList<StatementNode> syntaxTree;
+
+    public NodeList<StatementNode> getSyntaxTree() {
+        return syntaxTree;
+    }
+
+    public void setSyntaxTree(NodeList<StatementNode> syntaxTree) {
+        this.syntaxTree = syntaxTree;
+    }
+}
