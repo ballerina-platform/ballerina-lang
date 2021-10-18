f779cd89b9770e5bb7497f6de714e3201415402b
Ibaqu <aquib@wso2.com>
2021-10-18 13:46:29 +0530

    Add 'bal pack' command

---
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/cmd/PackCommand.java
@@ -0,0 +1,267 @@
+package io.ballerina.cli.cmd;
+
+import io.ballerina.cli.BLauncherCmd;
+import io.ballerina.cli.TaskExecutor;
+import io.ballerina.cli.task.CleanTargetDirTask;
+import io.ballerina.cli.task.CompileTask;
+import io.ballerina.cli.task.CreateBalaTask;
+import io.ballerina.cli.task.DumpBuildTimeTask;
+import io.ballerina.cli.task.ResolveMavenDependenciesTask;
+import io.ballerina.cli.task.RunTestsTask;
+import io.ballerina.cli.utils.BuildTime;
+import io.ballerina.cli.utils.FileUtils;
+import io.ballerina.projects.BuildOptions;
+import io.ballerina.projects.BuildOptionsBuilder;
+import io.ballerina.projects.Module;
+import io.ballerina.projects.ModuleId;
+import io.ballerina.projects.Project;
+import io.ballerina.projects.ProjectException;
+import io.ballerina.projects.directory.BuildProject;
+import io.ballerina.projects.util.ProjectConstants;
+import io.ballerina.toml.semantic.TomlType;
+import io.ballerina.toml.semantic.ast.TomlTableNode;
+import org.ballerinalang.toml.exceptions.SettingsTomlException;
+import picocli.CommandLine;
+
+import java.io.PrintStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.ballerina.cli.cmd.Constants.PACK_COMMAND;
+import static io.ballerina.cli.utils.CentralUtils.readSettings;
+import static io.ballerina.projects.internal.ManifestBuilder.getStringValueFromTomlTableNode;
+import static io.ballerina.runtime.api.constants.RuntimeConstants.SYSTEM_PROP_BAL_DEBUG;
+
+/**
+ * This class represents the "bal pack" command.
+ *
+ * @since 2.0.0
+ */
+@CommandLine.Command(name = PACK_COMMAND, description = "bal pack - Packages the current package into a .bala file " +
+        "after verifying that it can build with all its dependencies")
+public class PackCommand implements BLauncherCmd {
+
+    private final PrintStream outStream;
+    private final PrintStream errStream;
+    @CommandLine.Parameters(arity = "0..1")
+    private final Path projectPath;
+    private boolean exitWhenFinish;
+
+    @CommandLine.Option(names = {"--offline"}, description = "packs offline without accessing the network")
+    private Boolean offline;
+
+    @CommandLine.Option(names = {"--with-tests"}, description = "perform test compilation and execution")
+    private Boolean withTests;
+
+    @CommandLine.Option(names = "--sticky", description = "stick to exact versions locked (if exists)")
+    private Boolean sticky;
+
+    @CommandLine.Option(names = {"--help", "-h"}, hidden = true)
+    private boolean helpFlag;
+
+    @CommandLine.Option(names = "--debug", description = "run tests in remote debugging mode")
+    private String debugPort;
+
+
+    // TODO : Not sure if we need to include these
+    @CommandLine.Option(names = "--includes", hidden = true,
+            description = "hidden option for code coverage to include all classes")
+    private String includes;
+
+    @CommandLine.Option(names = "--coverage-format", description = "list of supported coverage report formats")
+    private String coverageFormat;
+
+    public PackCommand() {
+        this.projectPath = Paths.get(System.getProperty(ProjectConstants.USER_DIR));
+        this.outStream = System.out;
+        this.errStream = System.err;
+        this.exitWhenFinish = true;
+
+    }
+
+    @Override
+    public void execute() {
+        long start = 0;
+        boolean isSingleFileBuild = false; // Packing cannot be done for single files
+
+        if (this.helpFlag) {
+            String commandUsageInfo = BLauncherCmd.getCommandUsageInfo(PACK_COMMAND);
+            this.errStream.println(commandUsageInfo);
+            return;
+        }
+
+        Project project;
+
+        if (sticky == null) {
+            sticky = false;
+        }
+
+        BuildOptions buildOptions = constructBuildOptions();
+
+        // Throw an error if its a single file
+        if (FileUtils.hasExtension(this.projectPath)) {
+            CommandUtil.printError(this.errStream, "bal pack can only be used with a Ballerina package.", null, false);
+            CommandUtil.exitError(this.exitWhenFinish);
+            return;
+        }
+
+        try {
+            if (buildOptions.dumpBuildTime()) {
+                start = System.currentTimeMillis();
+                BuildTime.getInstance().timestamp = start;
+            }
+
+            project = BuildProject.load(this.projectPath, buildOptions);
+
+            if (buildOptions.dumpBuildTime()) {
+                BuildTime.getInstance().projectLoadDuration = System.currentTimeMillis() - start;
+            }
+
+        } catch (ProjectException e) {
+            CommandUtil.printError(this.errStream, e.getMessage(), null, false);
+            CommandUtil.exitError(this.exitWhenFinish);
+            return;
+        }
+
+
+        // Check if package is empty
+        if (!project.currentPackage().compilerPluginToml().isPresent() && isProjectEmpty(project)) {
+            CommandUtil.printError(this.errStream, "package is empty. please add at least one .bal file.", null,
+                    false);
+            CommandUtil.exitError(this.exitWhenFinish);
+            return;
+        }
+
+        // Check `[package]` section is available when compile
+        if (project.currentPackage().ballerinaToml().get().tomlDocument().toml().getTable("package")
+                .isEmpty()) {
+            CommandUtil.printError(this.errStream,
+                    "'package' information not found in " + ProjectConstants.BALLERINA_TOML,
+                    null,
+                    false);
+            CommandUtil.exitError(this.exitWhenFinish);
+            return;
+        } else {
+            // if not empty, validate the `[package]` section
+
+            TomlTableNode pkgNode = (TomlTableNode) project.currentPackage().ballerinaToml().get().tomlDocument().toml()
+                    .rootNode().entries().get("package");
+            if (pkgNode == null || pkgNode.kind() == TomlType.NONE) {
+                CommandUtil.printError(this.errStream,
+                        "'package' information not found in " + ProjectConstants.BALLERINA_TOML,
+                        null,
+                        false);
+                CommandUtil.exitError(this.exitWhenFinish);
+                return;
+            }
+
+            List<String> pkgErrors = new ArrayList<>();
+            if ("".equals(getStringValueFromTomlTableNode(pkgNode, "org", ""))) {
+                pkgErrors.add("'org'");
+            }
+            if ("".equals(getStringValueFromTomlTableNode(pkgNode, "name", ""))) {
+                pkgErrors.add("'name'");
+            }
+            if ("".equals(getStringValueFromTomlTableNode(pkgNode, "version", ""))) {
+                pkgErrors.add("'version'");
+            }
+
+            if (!pkgErrors.isEmpty()) {
+                String pkgErrorsString;
+                if (pkgErrors.size() == 1) {
+                    CommandUtil.printError(this.errStream,
+                            "to build a package " + pkgErrors.get(0) +
+                                    " field of the package is required in " +
+                                    ProjectConstants.BALLERINA_TOML,
+                            null,
+                            false);
+                    CommandUtil.exitError(this.exitWhenFinish);
+                    return;
+                } else if (pkgErrors.size() == 2) {
+                    pkgErrorsString = pkgErrors.get(0) + " and " + pkgErrors.get(1);
+                } else {
+                    pkgErrorsString = pkgErrors.get(0) + ", " + pkgErrors.get(1) + " and " + pkgErrors.get(2);
+                }
+                CommandUtil.printError(this.errStream,
+                        "to build a package " + pkgErrorsString +
+                                " fields of the package are required in " +
+                                ProjectConstants.BALLERINA_TOML,
+                        null,
+                        false);
+                CommandUtil.exitError(this.exitWhenFinish);
+                return;
+            }
+
+        }
+
+        // Sets the debug port as a system property, which will be used when setting up debug args before running tests.
+        if (!project.buildOptions().skipTests() && this.debugPort != null) {
+            System.setProperty(SYSTEM_PROP_BAL_DEBUG, this.debugPort);
+        }
+
+        // TODO : INCLUDE CODE COVERAGE IF REQUIRED
+
+        // Validate Settings.toml file
+
+        try {
+            readSettings();
+        } catch (SettingsTomlException e) {
+            this.outStream.println("warning: " + e.getMessage());
+        }
+
+        TaskExecutor taskExecutor = new TaskExecutor.TaskBuilder()
+                .addTask(new CleanTargetDirTask(), isSingleFileBuild)
+                .addTask(new ResolveMavenDependenciesTask(outStream))
+                .addTask(new CompileTask(outStream, errStream))
+                .addTask(new RunTestsTask(outStream, errStream, includes, coverageFormat),
+                        project.buildOptions().skipTests() || isSingleFileBuild)
+                .addTask(new CreateBalaTask(outStream))
+                .addTask(new DumpBuildTimeTask(outStream), !project.buildOptions().dumpBuildTime())
+                .build();
+
+        taskExecutor.executeTasks(project);
+        if (this.exitWhenFinish) {
+            Runtime.getRuntime().exit(0);
+        }
+    }
+
+    private BuildOptions constructBuildOptions() {
+        return new BuildOptionsBuilder()
+                .offline(offline)
+                .sticky(sticky)
+                .build();
+    }
+
+    private boolean isProjectEmpty(Project project) {
+        for (ModuleId moduleId : project.currentPackage().moduleIds()) {
+            Module module = project.currentPackage().module(moduleId);
+            if (!module.documentIds().isEmpty() || !module.testDocumentIds().isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return PACK_COMMAND;
+    }
+
+    @Override
+    public void printLongDesc(StringBuilder out) {
+        out.append("packages the current package into a .bala file after verifying that it can build with \n");
+        out.append("all its dependencies. Created .bala file contains the distribution format of the current package");
+
+    }
+
+    @Override
+    public void printUsage(StringBuilder out) {
+        out.append("  bal pack [--offline] [--skip-tests]\\n\" +\n" + "            \"       [<package-path>]");
+    }
+
+    @Override
+    public void setParentCmdParser(CommandLine parentCmdParser) {
+    }
+}
