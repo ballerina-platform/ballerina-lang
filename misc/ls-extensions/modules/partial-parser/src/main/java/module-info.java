ffcc267015632437e56a5d06c3c7d2316bac3b37
Madusha <madushajg@gmail.com>
2021-10-21 22:01:37 +0530

    Minor refactoring

--- misc/ls-extensions/modules/partial-parser/src/main/java/module-info.java
+++ misc/ls-extensions/modules/partial-parser/src/main/java/module-info.java
@@ -1,18 +1,10 @@
 module io.ballerina.LSExtensions.PartialParserService {
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
-    requires io.ballerina.formatter.core;
+    requires io.ballerina.diagram.util;
     requires io.ballerina.lang;
     requires io.ballerina.parser;
-    requires io.ballerina.tools.api;
-    requires io.swagger.v3.oas.models;
-    requires org.apache.commons.lang3;
     requires org.eclipse.lsp4j.jsonrpc;
-    requires swagger.parser.core;
-    requires swagger.parser.v3;
     requires io.ballerina.language.server.commons;
     requires com.google.gson;
-    requires io.ballerina.diagram.util;
 
     exports io.ballerina.parsers;
 }
