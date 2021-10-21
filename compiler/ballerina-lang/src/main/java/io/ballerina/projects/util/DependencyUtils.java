10a3473189f77433c867ff9a3b50a62b00c230d3
pramodya1994 <bpmmendis94@gmail.com>
2021-10-21 19:42:44 +0530

    Remove pull packages
    Pulling packages will automatically happen since we get the
    resolution after disabling offline flag.

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/util/DependencyUtils.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/util/DependencyUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -18,22 +18,7 @@
 package io.ballerina.projects.util;
 
 import io.ballerina.projects.CompilationOptionsBuilder;
-import io.ballerina.projects.PackageResolution;
 import io.ballerina.projects.Project;
-import io.ballerina.projects.ResolvedPackageDependency;
-import io.ballerina.projects.Settings;
-import io.ballerina.projects.environment.PackageCache;
-import io.ballerina.projects.environment.ProjectEnvironment;
-import org.ballerinalang.central.client.CentralAPIClient;
-import org.ballerinalang.central.client.exceptions.CentralClientException;
-import org.ballerinalang.toml.exceptions.SettingsTomlException;
-import org.wso2.ballerinalang.util.RepoUtils;
-
-import java.nio.file.Path;
-
-import static io.ballerina.projects.util.ProjectUtils.getAccessTokenOfCLI;
-import static io.ballerina.projects.util.ProjectUtils.initializeProxy;
-import static org.wso2.ballerinalang.programfile.ProgramFileConstants.SUPPORTED_PLATFORMS;
 
 /**
  * Project dependencies related util methods.
@@ -52,44 +37,8 @@ public class DependencyUtils {
      * @param project project
      */
     public static void pullMissingDependencies(Project project) {
-        // Disable offline flag
         CompilationOptionsBuilder compilationOptionsBuilder = new CompilationOptionsBuilder();
-        compilationOptionsBuilder.offline(false);
-
-        PackageResolution resolution = project.currentPackage().getResolution(compilationOptionsBuilder.build());
-        ProjectEnvironment projectEnvContext = project.projectEnvironmentContext();
-        PackageCache packageCache = projectEnvContext.getService(PackageCache.class);
-
-        Settings settings;
-        try {
-            settings = RepoUtils.readSettings();
-            // Ignore Settings.toml diagnostics in the pull command
-        } catch (SettingsTomlException e) {
-            // Ignore 'Settings.toml' parsing errors and return empty Settings object
-            settings = Settings.from();
-        }
-
-        for (ResolvedPackageDependency dependency : resolution.allDependencies()) {
-            if (packageCache.getPackage(dependency.packageId()).isEmpty()) {
-                Path packagePathInBalaCache = ProjectUtils.createAndGetHomeReposPath()
-                        .resolve(ProjectConstants.REPOSITORIES_DIR)
-                        .resolve(ProjectConstants.CENTRAL_REPOSITORY_CACHE_NAME)
-                        .resolve(ProjectConstants.BALA_DIR_NAME)
-                        .resolve(dependency.packageInstance().packageOrg().value())
-                        .resolve(dependency.packageInstance().packageName().value());
-                CentralAPIClient client = new CentralAPIClient(RepoUtils.getRemoteRepoURL(),
-                        initializeProxy(settings.getProxy()), getAccessTokenOfCLI(settings));
-                for (String supportedPlatform : SUPPORTED_PLATFORMS) {
-                    try {
-                        client.pullPackage(dependency.packageInstance().packageOrg().value(),
-                                dependency.packageInstance().packageName().value(),
-                                dependency.packageInstance().packageVersion().toString(),
-                                packagePathInBalaCache, supportedPlatform, RepoUtils.getBallerinaVersion(), false);
-                    } catch (CentralClientException e) {
-                        // ignore when get package fail
-                    }
-                }
-            }
-        }
+        compilationOptionsBuilder.offline(false).sticky(false);
+        project.currentPackage().getResolution(compilationOptionsBuilder.build());
     }
 }
