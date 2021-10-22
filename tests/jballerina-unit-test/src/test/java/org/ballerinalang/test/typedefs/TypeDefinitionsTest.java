1eda099700e14e20f018184a25a2624f0a52da58
KRVPerera <rukshan.viduranga@gmail.com>
2021-10-22 13:24:17 +0530

    Enable disabled test cases

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/typedefs/TypeDefinitionsTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/typedefs/TypeDefinitionsTest.java
@@ -35,12 +35,12 @@ import org.testng.annotations.Test;
 public class TypeDefinitionsTest {
 
     private CompileResult compileResult;
-//    private CompileResult recordFieldRes;
+    private CompileResult recordFieldRes;
 
     @BeforeClass
     public void setup() {
         compileResult = BCompileUtil.compile("test-src/typedefs/type-definitions.bal");
-//        recordFieldRes = BCompileUtil.compile("test-src/typedefs/record-type-field-resolving.bal");
+        recordFieldRes = BCompileUtil.compile("test-src/typedefs/record-type-field-resolving.bal");
     }
 
     @Test
@@ -156,19 +156,19 @@ public class TypeDefinitionsTest {
         BRunUtil.invoke(compileResult, "testTypeDefReferringToTypeDefDefinedAfter");
     }
 
-//    @Test
-//    public void testRecordTypeResolving() {
-//        BRunUtil.invoke(recordFieldRes, "testRecordTypeResolving");
-//    }
-//
-//    @Test
-//    public void testRecordTypeResolvingWithTypeInclusion() {
-//        BRunUtil.invoke(recordFieldRes, "testRecordTypeResolvingWithTypeInclusion");
-//    }
+    @Test
+    public void testRecordTypeResolving() {
+        BRunUtil.invoke(recordFieldRes, "testRecordTypeResolving");
+    }
+
+    @Test
+    public void testRecordTypeResolvingWithTypeInclusion() {
+        BRunUtil.invoke(recordFieldRes, "testRecordTypeResolvingWithTypeInclusion");
+    }
 
     @AfterClass
     public void tearDown() {
         compileResult = null;
-//        recordFieldRes = null;
+        recordFieldRes = null;
     }
 }
