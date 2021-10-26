a790eef4688ea036d4afcf2f4af808a7eaa5f4dc
Gimantha Bandara <gimantha@wso2.com>
2021-10-26 19:26:34 +0530

    Fix Not giving a compile error for negative index array access

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
@@ -7549,7 +7549,8 @@ public class TypeChecker extends BLangNodeVisitor {
                     actualType = arrayType.eType;
                     break;
                 }
-                actualType = getConstIndex(indexExpr) >= arrayType.size ? symTable.semanticError : arrayType.eType;
+                var indexVal = getConstIndex(indexExpr);
+                actualType = indexVal >= arrayType.size || indexVal < 0 ? symTable.semanticError : arrayType.eType;
                 break;
             case TypeTags.FINITE:
                 BFiniteType finiteIndexExpr = (BFiniteType) indexExprType;
