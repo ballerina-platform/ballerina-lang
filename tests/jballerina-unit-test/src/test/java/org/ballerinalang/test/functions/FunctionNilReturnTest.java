8095c743493b218c797ca6d2bc261deb46259ad3
MaryamZi <maryamziyadm@gmail.com>
2021-10-27 10:18:49 +0530

    Fix tests

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/functions/FunctionNilReturnTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/functions/FunctionNilReturnTest.java
@@ -251,6 +251,13 @@ public class FunctionNilReturnTest {
         validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 257, 59);
         validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 265, 35);
         validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 274, 35);
+        validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 291, 84);
+        validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 294, 84);
+        validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 300, 84);
+        validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 303, 84);
+        validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 306, 84);
+        validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 312, 84);
+        validateWarning(compileResult, i++, WARN_SHOULD_EXPLICITLY_RETURN_A_VALUE, 315, 84);
         Assert.assertEquals(compileResult.getWarnCount(), i);
 
     }
