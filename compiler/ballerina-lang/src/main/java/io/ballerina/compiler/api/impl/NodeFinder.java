fd30db5bb75cfd2c57ed41413fd76d7d6e44a878
sanjana <anjetnet@gmail.com>
2021-10-29 22:08:18 +0530

    Lookup for annotation attachments in simple variable nodes

--- compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/impl/NodeFinder.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/impl/NodeFinder.java
@@ -340,6 +340,7 @@ class NodeFinder extends BaseVisitor {
 
     @Override
     public void visit(BLangSimpleVariable varNode) {
+        lookupNodes(varNode.annAttachments);
         lookupNode(varNode.typeNode);
         lookupNode(varNode.expr);
         setEnclosingNode(varNode, varNode.name.pos);
