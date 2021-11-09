dbb6198ac391a1ee591235751070f1bfd16daa9f
Fathima Dilhasha <dilhasha.uom@gmail.com>
2021-11-10 00:26:38 +0530

    Improve type conversion

--- misc/compiler-plugins/modules/configurable-schema-generator/src/main/java/org/ballerinalang/configschemagenerator/util/ConfigSchemaBuilder.java
+++ misc/compiler-plugins/modules/configurable-schema-generator/src/main/java/org/ballerinalang/configschemagenerator/util/ConfigSchemaBuilder.java
@@ -25,26 +25,11 @@ import com.google.gson.JsonObject;
 import io.ballerina.projects.ProjectKind;
 import io.ballerina.projects.configurations.ConfigModuleDetails;
 import io.ballerina.projects.configurations.ConfigVariable;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BArrayType;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BField;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BFiniteType;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BIntersectionType;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BMapType;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BRecordType;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BType;
-import org.wso2.ballerinalang.compiler.semantics.model.types.BUnionType;
-import org.wso2.ballerinalang.compiler.util.TypeTags;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import static org.wso2.ballerinalang.compiler.util.TypeTags.BOOLEAN;
-import static org.wso2.ballerinalang.compiler.util.TypeTags.BYTE;
-import static org.wso2.ballerinalang.compiler.util.TypeTags.DECIMAL;
-import static org.wso2.ballerinalang.compiler.util.TypeTags.FLOAT;
+import static org.ballerinalang.configschemagenerator.util.TypeConverter.PROPERTIES;
 
 /**
  * Util to build JSON schema for configurable variables.
@@ -52,10 +37,6 @@ import static org.wso2.ballerinalang.compiler.util.TypeTags.FLOAT;
  * @since 2.0.0
  */
 public class ConfigSchemaBuilder {
-
-    static final String PROPERTIES = "properties";
-    static final String ADDITIONAL_PROPERTIES = "additionalProperties";
-    static final String TYPE = "type";
 
     /**
      * Returns string content of config schema JSON.
@@ -79,9 +60,9 @@ public class ConfigSchemaBuilder {
         // Create root node
         JsonObject root = new JsonObject();
         root.addProperty("$schema", "http://json-schema.org/draft-07/schema#");
-        root.addProperty(TYPE, "object");
+        root.addProperty(TypeConverter.TYPE, "object");
         root.add(PROPERTIES, rootNode);
-        root.addProperty(ADDITIONAL_PROPERTIES, false);
+        root.addProperty(TypeConverter.ADDITIONAL_PROPERTIES, false);
         for (Map.Entry<ConfigModuleDetails, List<ConfigVariable>> configModuleDetails : configDetails.entrySet()) {
             List<ConfigVariable> configVariables = configModuleDetails.getValue();
             if (ProjectKind.SINGLE_FILE_PROJECT.equals(configModuleDetails.getKey().projectKind())) {
@@ -93,7 +74,7 @@ public class ConfigSchemaBuilder {
                 JsonObject orgNode;
                 JsonElement orgJsonNode = rootNode.get(configModuleDetails.getKey().orgName());
                 if (orgJsonNode == null) {
-                    orgNode = createObjNode(new JsonObject());
+                    orgNode = TypeConverter.createObjNode(new JsonObject());
                 } else {
                     orgNode = (JsonObject) orgJsonNode;
                 }
@@ -102,7 +83,7 @@ public class ConfigSchemaBuilder {
                 JsonObject orgPropertiesNode = (JsonObject) orgNode.get(PROPERTIES);
                 JsonElement pkgJsonNode = orgPropertiesNode.get(configModuleDetails.getKey().packageName());
                 if (pkgJsonNode == null) {
-                    pkgNode = createObjNode(new JsonObject());
+                    pkgNode = TypeConverter.createObjNode(new JsonObject());
                     orgPropertiesNode.add(configModuleDetails.getKey().packageName(), pkgNode);
                 } else {
                     pkgNode = (JsonObject) pkgJsonNode;
@@ -115,7 +96,8 @@ public class ConfigSchemaBuilder {
                     setRequiredConfigs(configVariables, pkgNode);
                 } else {
                     JsonObject modVarNode = new JsonObject();
-                    JsonObject modObjNode = createObjNode(setConfigVariables(configVariables, modVarNode));
+                    JsonObject modObjNode = TypeConverter.createObjNode(setConfigVariables(configVariables,
+                            modVarNode));
                     setRequiredConfigs(configVariables, modObjNode);
                     packagePropertiesNode.add(configModuleDetails.getKey().moduleName(), modObjNode);
                 }
@@ -165,174 +147,11 @@ public class ConfigSchemaBuilder {
     private static JsonObject setConfigVariables(List<ConfigVariable> configVariables,
                                                  JsonObject node) {
         for (ConfigVariable configVariable : configVariables) {
-            JsonObject typeNode = getType(configVariable.type());
+            JsonObject typeNode = TypeConverter.getType(configVariable.type());
             typeNode.addProperty("description", configVariable.description());
             node.add(configVariable.name(), typeNode);
         }
         return node;
     }
 
-    /**
-     * Get the type as a JSONObject.
-     *
-     * @param type Ballerina type as BType
-     * @return JsonObject with type details
-     */
-    private static JsonObject getType(BType type) {
-        JsonObject typeNode = new JsonObject();
-        if (TypeTags.isSimpleBasicType(type.tag)) {
-            String typeVal = getSimpleType(type);
-            typeNode.addProperty(TYPE, typeVal);
-        } else {
-            if (TypeTags.INTERSECTION == type.tag && type instanceof BIntersectionType) {
-                Set<BType> subTypes = ((BIntersectionType) type).getConstituentTypes();
-                for (BType subType : subTypes) {
-                    if (TypeTags.READONLY == subType.tag) {
-                        continue;
-                    }
-                    if (TypeTags.ARRAY == subType.tag && subType instanceof BArrayType) {
-                        generateArrayType(typeNode, (BArrayType) subType);
-                    }
-                    if (TypeTags.RECORD == subType.tag && subType instanceof BRecordType) {
-                        typeNode = generateRecordType((BRecordType) subType);
-                    }
-                    if (TypeTags.MAP == subType.tag && subType instanceof BMapType) {
-                        generateMapType(typeNode, (BMapType) subType);
-                    }
-                    if (TypeTags.UNION == subType.tag && subType instanceof BUnionType) {
-                        generateUnionType(typeNode, (BUnionType) subType);
-                    }
-
-                }
-            }
-        }
-        return typeNode;
-    }
-
-    /**
-     * Add array type details to the given type object.
-     *
-     * @param typeNode JsonObject representing type
-     * @param subType  BArrayType with array details
-     */
-    private static void generateArrayType(JsonObject typeNode, BArrayType subType) {
-        BType elementType = subType.getElementType();
-        JsonObject subTypeNode = new JsonObject();
-        if (TypeTags.isSimpleBasicType(elementType.tag)) {
-            String typeVal = getSimpleType(elementType);
-            subTypeNode.addProperty(TYPE, typeVal);
-        }
-        typeNode.addProperty(TYPE, "array");
-        typeNode.add("items", subTypeNode);
-    }
-
-    /**
-     * Generate a record type JSON object.
-     *
-     * @param subType BRecordType with record details
-     * @return JsonObject with record details
-     */
-    private static JsonObject generateRecordType(BRecordType subType) {
-        JsonObject typeNode;
-        LinkedHashMap<String, BField> fieldLinkedHashMap = subType.getFields();
-        JsonObject subTypeNode = new JsonObject();
-        for (Map.Entry<String, BField> key : fieldLinkedHashMap.entrySet()) {
-            BField field = key.getValue();
-            JsonObject fieldTypeNode = new JsonObject();
-            if (TypeTags.isSimpleBasicType(field.getType().tag)) {
-                String typeVal = getSimpleType(field.getType());
-                fieldTypeNode.addProperty(TYPE, typeVal);
-            }
-            subTypeNode.add(field.getName().getValue(), fieldTypeNode);
-        }
-        typeNode = createObjNode(subTypeNode);
-        return typeNode;
-    }
-
-    /**
-     * Add map type details to the given type object.
-     *
-     * @param typeNode JsonObject representing type
-     * @param subType  BMapType with map details
-     */
-    private static void generateMapType(JsonObject typeNode, BMapType subType) {
-        BType constraint = subType.getConstraint();
-        typeNode.addProperty(TYPE, "object");
-        JsonObject mapConstraintNode = new JsonObject();
-        if (TypeTags.isSimpleBasicType(constraint.tag)) {
-            String typeVal = getSimpleType(constraint);
-            mapConstraintNode.addProperty(TYPE, typeVal);
-        }
-        typeNode.addProperty(TYPE, "object");
-        typeNode.add("additionalProperties", mapConstraintNode);
-    }
-
-    /**
-     * Add union type details to the given type object.
-     *
-     * @param typeNode JsonObject representing type
-     * @param subType  BUnionType with union details
-     */
-    private static void generateUnionType(JsonObject typeNode, BUnionType subType) {
-        LinkedHashSet<BType> members = subType.getMemberTypes();
-        JsonArray memberArray = new JsonArray();
-        boolean isEnum = false;
-
-        for (BType member : members) {
-            if (TypeTags.isSimpleBasicType(member.tag)) {
-                String typeVal = getSimpleType(member);
-                JsonObject memberObj = new JsonObject();
-                memberObj.addProperty(TYPE, typeVal);
-                memberArray.add(memberObj);
-            } else if (TypeTags.FINITE == member.tag && member instanceof BFiniteType) {
-                BFiniteType memberType = (BFiniteType) member;
-                memberArray.add(memberType.toString());
-                isEnum = true;
-            }
-        }
-        if (isEnum) {
-            typeNode.add("enum", memberArray);
-        } else {
-            typeNode.add("anyOf", memberArray);
-        }
-    }
-
-    /**
-     * Create a JsonObject adding the given variable node as a property.
-     *
-     * @param modVarNode JsonObject representing module variable
-     * @return JsonObject with variable node
-     */
-    private static JsonObject createObjNode(JsonObject modVarNode) {
-        JsonObject node = new JsonObject();
-        node.addProperty(TYPE, "object");
-        node.add(PROPERTIES, modVarNode);
-        node.addProperty(ADDITIONAL_PROPERTIES, false);
-        return node;
-    }
-
-    /**
-     * Get simple type name as String.
-     *
-     * @param type BType representing a simple type
-     * @return simple type name as String
-     */
-    private static String getSimpleType(BType type) {
-        if (TypeTags.isIntegerTypeTag(type.tag)) {
-            return "integer";
-        } else if (TypeTags.isStringTypeTag(type.tag)) {
-            return "string";
-        } else {
-            switch (type.tag) {
-                case BYTE:
-                case FLOAT:
-                case DECIMAL:
-                    return "number";
-                case BOOLEAN:
-                    return "boolean";
-                default:
-                    return "";
-            }
-        }
-    }
 }
