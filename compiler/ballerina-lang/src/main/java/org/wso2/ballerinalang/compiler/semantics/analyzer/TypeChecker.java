424e3392cfddbe3facd8ec6d1181d5ba5b11ad64
SandaruJayawardana <sandaru.jayawardana@gmail.com>
2021-11-09 12:26:28 +0530

    Fix type casting issue
    (cherry picked from commit 026151a0e24bd132382021e03c28dd158fa6d2af)

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
@@ -3945,6 +3945,8 @@ public class TypeChecker extends BLangNodeVisitor {
                 resultType = elseType;
             } else {
                 resultType = BUnionType.create(null, elseType, thenType);
+                types.setImplicitCastExpr(ternaryExpr.thenExpr, thenType, resultType);
+                types.setImplicitCastExpr(ternaryExpr.elseExpr, elseType, resultType);
             }
         } else {
             resultType = expType;
