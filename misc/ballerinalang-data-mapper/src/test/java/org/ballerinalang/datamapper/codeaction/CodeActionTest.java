d847811c2c0b2e75c13c9c15114420b98e7c324a
isururuh <isuru.ruhu@gmail.com>
2021-11-17 08:02:36 +0530

    Fix code action test failures

--- misc/ballerinalang-data-mapper/src/test/java/org/ballerinalang/datamapper/codeaction/CodeActionTest.java
+++ misc/ballerinalang-data-mapper/src/test/java/org/ballerinalang/datamapper/codeaction/CodeActionTest.java
@@ -18,15 +18,9 @@ package org.ballerinalang.datamapper.codeaction;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.ballerinalang.datamapper.util.FileUtils;
 import org.ballerinalang.datamapper.util.TestUtil;
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.ballerinalang.langserver.common.utils.CommonUtil;
 import org.eclipse.lsp4j.jsonrpc.Endpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,7 +32,9 @@ import org.testng.annotations.Test;
 
 
 import java.io.File;
-import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Optional;
 
 import static org.ballerinalang.datamapper.util.DataMapperTestUtils.getCodeActionResponse;
 
@@ -52,26 +48,15 @@ public class CodeActionTest {
 
     private static Endpoint serviceEndpoint;
     private static final Logger log = LoggerFactory.getLogger(CodeActionTest.class);
-    private static Server server;
+    private final Path sourcesPath = new File(getClass().getClassLoader().getResource("codeaction").getFile()).toPath();
 
     @BeforeClass
-    private void init() throws Exception {
+    private void init() {
         serviceEndpoint = TestUtil.initializeLanguageSever();
         String startConfigPath = "codeaction" + File.separator + "config" + File.separator +
                 "startConfig.json";
         JsonObject configs = FileUtils.fileContentAsObject(startConfigPath);
         TestUtil.setWorkspaceConfig(serviceEndpoint, configs);
-
-        server = new Server(8080);
-        Connector connector = new ServerConnector(server);
-        server.addConnector(connector);
-
-        String responseData = "{\"answer\":{\"maths\":\"student.grades.maths\",\"chemistry\":" +
-                "\"student.grades.chemistry\",\"physics\":\"student.grades.physics\"}}";
-
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(responseData));
-        server.start();
     }
 
     @Test(dataProvider = "codeAction-data-mapper-data-provider-1")
@@ -79,133 +64,8 @@ public class CodeActionTest {
         checkAssertion(config, source);
     }
 
-    @Test(dataProvider = "codeAction-data-mapper-data-provider-2")
-    public void testDataMapperCodeAction_2(String config, String source) throws Exception {
-
-        stopServer();
-
-        String responseData = "{\"answer\":{\"city\":\"applicant.applying_student.city\"}}";
-
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(responseData));
-        server.start();
-
-        checkAssertion(config, source);
-    }
-
-    @Test(dataProvider = "codeAction-data-mapper-data-provider-3")
-    public void testDataMapperCodeAction_3(String config, String source) throws Exception {
-
-        stopServer();
-
-        String responseData = "{\"answer\":{\"country\":\"home.birth_country\",\"lane\":" +
-                "\"home.lane\"}}";
-
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(responseData));
-        server.start();
-
-        checkAssertion(config, source);
-    }
-
-    @Test(dataProvider = "codeAction-data-mapper-data-provider-4")
-    public void testDataMapperCodeAction_4(String config, String source) throws Exception {
-
-        stopServer();
-
-        String responseData = "{\"answer\":{\"ID\":\"supplier.supplier_details.id\",\"email\":\"supplier.email\"," +
-                "\"user\":{\"age\":\"supplier.supplier_details.age\",\"id\":\"supplier.id\",\"name\":\"" +
-                "supplier.supplier_details.name\"}}}";
-
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(responseData));
-        server.start();
-
-        checkAssertion(config, source);
-    }
-
-    @Test(dataProvider = "codeAction-data-mapper-data-provider-5")
-    public void testDataMapperCodeAction_5(String config, String source) throws Exception {
-
-        stopServer();
-
-        String responseData = "{\"answer\":{\"history\":\"mark.history\",\"maths\":\"mark.maths\",\"physics\":\"" +
-                "mark.physics\"}}";
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(responseData));
-        server.start();
-
-        checkAssertion(config, source);
-    }
-
-    @Test(dataProvider = "codeAction-data-mapper-data-provider-6")
-    public void testDataMapperCodeAction_6(String config, String source) throws Exception {
-
-        stopServer();
-
-        String responseData = "{\"answer\":{\"address\":\"student.address\",\"age\":\"student.age\",\"name\":\"" +
-                "student.name\"}}";
-
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(responseData));
-        server.start();
-
-        checkAssertion(config, source);
-    }
-
-    @Test(dataProvider = "codeAction-data-mapper-data-provider-7")
-    public void testDataMapperCodeAction_7(String config, String source) throws Exception {
-
-        stopServer();
-
-        String responseData = "{\"answer\":{\"address\":\"student.address\",\"age\":\"student.age\"," +
-                "\"contact_person\":\"student.contact_person\",\"name\":\"student.name\"}}";
-
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(responseData));
-        server.start();
-
-        checkAssertion(config, source);
-    }
-
-    @Test(dataProvider = "codeAction-data-mapper-data-provider-8")
-    public void testDataMapperCodeAction_8(String config, String source) throws Exception {
-
-        stopServer();
-
-        String responseData = "{\"answer\":{\"address\":\"student.contact.address\",\"number\":\"" +
-                "student.contact.number\"}}";
-
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(responseData));
-        server.start();
-
-        checkAssertion(config, source);
-    }
-
-    @Test(dataProvider = "codeAction-data-mapper-data-provider-9")
-    public void testDataMapperCodeAction_9(String config, String source) throws Exception {
-
-        stopServer();
-
-        String responseData = "{\"answer\":{\"age\":\"student.age\",\"name\":\"student.name\"}}";
-
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(responseData));
-        server.start();
-
-        checkAssertion(config, source);
-    }
-
     @Test(dataProvider = "codeAction-data-mapper-data-provider-10")
     public void testDataMapperCodeAction_10(String config, String source) throws Exception {
-
-        stopServer();
-
-        // Set the Handler.
-        server.setHandler(new DataMapperServiceHandler(null));
-        server.start();
-
         // Read expected results
         String configJsonPath = "codeaction" + File.separator + config;
         JsonObject configJsonObject = FileUtils.fileContentAsObject(configJsonPath);
@@ -235,12 +95,19 @@ public class CodeActionTest {
         log.info("Test textDocument/codeAction QuickFixes");
         return new Object[][]{
                 {"dataMapper1.json", "dataMapper1.bal"},
-                {"dataMapper5.json", "dataMapper5.bal"},
+                {"dataMapper2.json", "dataMapper2.bal"},
+                {"dataMapper3.json", "dataMapper3.bal"},
+                {"dataMapper4.json", "dataMapper4.bal"},
                 {"dataMapper6.json", "dataMapper6.bal"},
                 {"dataMapper7.json", "dataMapper7.bal"},
                 {"dataMapper8.json", "dataMapper8.bal"},
                 {"dataMapper9.json", "dataMapper9.bal"},
                 {"dataMapper10.json", "dataMapper10.bal"},
+                {"dataMapper11.json", "dataMapper11.bal"},
+                {"dataMapper12.json", "dataMapper12.bal"},
+                {"dataMapper13.json", "dataMapper13.bal"},
+                {"dataMapper14.json", "dataMapper14.bal"},
+                {"dataMapper15.json", "dataMapper15.bal"},
                 {"dataMapper17.json", "dataMapper17.bal"},
                 {"module-response/defaultDataMapper1.json", "datamapper-module-test/defaultDataMapper1.bal"},
                 {"module-response/defaultDataMapper2.json", "datamapper-module-test/defaultDataMapper2.bal"},
@@ -255,70 +122,6 @@ public class CodeActionTest {
         };
     }
 
-    @DataProvider(name = "codeAction-data-mapper-data-provider-2")
-    public Object[][] codeActionDataMapperDataProvider_2() {
-        log.info("Test textDocument/codeAction QuickFixes");
-        return new Object[][]{
-                {"dataMapper2.json", "dataMapper2.bal"},
-        };
-    }
-
-    @DataProvider(name = "codeAction-data-mapper-data-provider-3")
-    public Object[][] codeActionDataMapperDataProvider_3() {
-        log.info("Test textDocument/codeAction QuickFixes");
-        return new Object[][]{
-                {"dataMapper3.json", "dataMapper3.bal"},
-        };
-    }
-
-    @DataProvider(name = "codeAction-data-mapper-data-provider-4")
-    public Object[][] codeActionDataMapperDataProvider_4() {
-        log.info("Test textDocument/codeAction QuickFixes");
-        return new Object[][]{
-                {"dataMapper4.json", "dataMapper4.bal"},
-        };
-    }
-
-    @DataProvider(name = "codeAction-data-mapper-data-provider-5")
-    public Object[][] codeActionDataMapperDataProvider_5() {
-        log.info("Test textDocument/codeAction QuickFixes");
-        return new Object[][]{
-                {"dataMapper11.json", "dataMapper11.bal"},
-        };
-    }
-
-    @DataProvider(name = "codeAction-data-mapper-data-provider-6")
-    public Object[][] codeActionDataMapperDataProvider_6() {
-        log.info("Test textDocument/codeAction QuickFixes");
-        return new Object[][]{
-                {"dataMapper12.json", "dataMapper12.bal"},
-        };
-    }
-
-    @DataProvider(name = "codeAction-data-mapper-data-provider-7")
-    public Object[][] codeActionDataMapperDataProvider_7() {
-        log.info("Test textDocument/codeAction QuickFixes");
-        return new Object[][]{
-                {"dataMapper13.json", "dataMapper13.bal"},
-        };
-    }
-
-    @DataProvider(name = "codeAction-data-mapper-data-provider-8")
-    public Object[][] codeActionDataMapperDataProvider_8() {
-        log.info("Test textDocument/codeAction QuickFixes");
-        return new Object[][]{
-                {"dataMapper14.json", "dataMapper14.bal"},
-        };
-    }
-
-    @DataProvider(name = "codeAction-data-mapper-data-provider-9")
-    public Object[][] codeActionDataMapperDataProvider_9() {
-        log.info("Test textDocument/codeAction QuickFixes");
-        return new Object[][]{
-                {"dataMapper15.json", "dataMapper15.bal"},
-        };
-    }
-
     @DataProvider(name = "codeAction-data-mapper-data-provider-10")
     public Object[] codeActionDataMapperDataProvider_10() {
         log.info("Test textDocument/codeAction QuickFixes");
@@ -330,20 +133,14 @@ public class CodeActionTest {
     @AfterClass
     private void cleanupLanguageServer() {
         TestUtil.shutdownLanguageServer(serviceEndpoint);
-        try {
-            server.stop();
-            server.destroy();
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
     }
 
     public void checkAssertion(String config, String source) throws Exception {
-
-        JsonArray editResponse = null;
         // Read expected results
         String configJsonPath = "codeaction" + File.separator + config;
         JsonObject configJsonObject = FileUtils.fileContentAsObject(configJsonPath);
+
+        Path sourcePath = sourcesPath.resolve("source").resolve(source);
 
         // Get code action from language server
         JsonObject responseJson = getCodeActionResponse(source, configJsonObject, serviceEndpoint);
@@ -351,60 +148,69 @@ public class CodeActionTest {
         JsonObject expectedResponse = configJsonObject.get("expected").getAsJsonObject();
         String title = expectedResponse.get("title").getAsString();
 
-        int numberOfDataMappingCodeAction = 0;
         boolean codeActionFound = false;
-        boolean codeActionFoundOnlyOnce = false;
         for (JsonElement jsonElement : responseJson.getAsJsonArray("result")) {
-            JsonElement right = jsonElement.getAsJsonObject().get("right");
-            if (right.getAsJsonObject().get("title").getAsString().equals("Generate mapping function")) {
-                JsonElement editText = right.getAsJsonObject().get("edit");
-                if (editText == null) {
+            String expTitle = "Generate mapping function";
+            JsonObject right = jsonElement.getAsJsonObject().get("right").getAsJsonObject();
+            if (right == null) {
+                continue;
+            }
+
+            // Match title
+            String actualTitle = right.get("title").getAsString();
+            if (!expTitle.equals(actualTitle)) {
+                continue;
+            }
+            // Match edits
+            if (expectedResponse.get("edits") != null) {
+                JsonArray actualEdit = right.get("edit").getAsJsonObject().get("documentChanges")
+                        .getAsJsonArray().get(0).getAsJsonObject().get("edits").getAsJsonArray();
+                JsonArray expEdit = expectedResponse.get("edits").getAsJsonArray();
+                if (!expEdit.equals(actualEdit)) {
                     continue;
                 }
-                JsonArray edit = editText.getAsJsonObject().get("documentChanges")
-                        .getAsJsonArray().get(0).getAsJsonObject().get("edits").getAsJsonArray();
-                editResponse = edit;
-                boolean editsMatched = expectedResponse.get("edits").getAsJsonArray().equals(edit);
-                if (right.getAsJsonObject().get("title").getAsString().equals(title) && editsMatched) {
-                    codeActionFound = true;
-                    numberOfDataMappingCodeAction = numberOfDataMappingCodeAction + 1;
+            }
+            // Match args
+            if (expectedResponse.get("command") != null) {
+                JsonObject expectedCommand = expectedResponse.get("command").getAsJsonObject();
+                JsonObject actualCommand = right.get("command").getAsJsonObject();
+
+                if (!Objects.equals(actualCommand.get("command"), expectedCommand.get("command"))) {
+                    continue;
+                }
+
+                if (!Objects.equals(actualCommand.get("title"), expectedCommand.get("title"))) {
+                    continue;
+                }
+
+                JsonArray actualArgs = actualCommand.getAsJsonArray("arguments");
+                JsonArray expArgs = expectedCommand.getAsJsonArray("arguments");
+                if (!org.ballerinalang.langserver.util.TestUtil.isArgumentsSubArray(actualArgs, expArgs)) {
+                    continue;
+                }
+
+                boolean docUriFound = false;
+                for (JsonElement actualArg : actualArgs) {
+                    JsonObject arg = actualArg.getAsJsonObject();
+                    if ("doc.uri".equals(arg.get("key").getAsString())) {
+                        Optional<Path> docPath = CommonUtil.getPathFromURI(arg.get("value").getAsString());
+                        if (docPath.isPresent()) {
+                            // We just check file names, since one refers to file in build/ while
+                            // the other refers to the file in test resources
+                            docUriFound = docPath.get().getFileName().equals(sourcePath.getFileName());
+                        }
+                    }
+                }
+
+                if (!docUriFound) {
+                    continue;
                 }
             }
+            // Code-action matched
+            codeActionFound = true;
+            break;
         }
-        if (codeActionFound && numberOfDataMappingCodeAction == 1) {
-            codeActionFoundOnlyOnce = true;
-        }
-        Assert.assertTrue(
-                codeActionFoundOnlyOnce, "Cannot find expected Code Action for: " + title +
-                        "\n expected response : " + expectedResponse.get("edits").getAsJsonArray() +
-                        "\n actual response : " + editResponse);
-    }
-
-    public void stopServer() {
-        try {
-            server.stop();
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
-    }
-}
-
-class DataMapperServiceHandler extends AbstractHandler {
-    String responseData;
-    int responseCode = 200;
-
-    public DataMapperServiceHandler(String responseData) {
-        this.responseData = responseData;
-    }
-
-    @Override
-    public void handle(String target, Request jettyRequest, HttpServletRequest request,
-                       HttpServletResponse response) throws IOException {
-        // Mark the request as handled by this Handler.
-        jettyRequest.setHandled(true);
-
-        response.setStatus(responseCode);
-        response.setContentType("text/html; charset=UTF-8");
-        response.getWriter().print(responseData);
+        Assert.assertTrue(codeActionFound,
+                "Cannot find expected Code Action for: " + title + " in " + sourcePath);
     }
 }
