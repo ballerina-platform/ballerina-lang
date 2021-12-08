cd7562747081b1451c7c07e6a1fe968a3f60e736
DamithaSenevirathne <damithasenevirathne@gmail.com>
2021-12-08 20:08:23 +0530

    Add ShellCompilation

---
+++ ballerina-shell/modules/shell-core/src/main/java/io/ballerina/shell/ShellCompilation.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.shell;
+
+import io.ballerina.projects.PackageCompilation;
+
+import java.util.Optional;
+
+/**
+ * Main shell entry point.
+ * Creates an virtual shell which will accept input from
+ * a terminal and evaluate each expression.
+ *
+ * @since 2.0.0
+ */
+public class ShellCompilation {
+
+    private final Optional<PackageCompilation> packageCompilation;
+    private final Enum<ExceptionStatus> exceptionStatus;
+
+    ShellCompilation(Optional<PackageCompilation> packageCompilation, Enum<ExceptionStatus> exceptionStatus) {
+        this.packageCompilation = packageCompilation;
+        this.exceptionStatus = exceptionStatus;
+    }
+
+    public Optional<PackageCompilation> getPackageCompilation() {
+        return packageCompilation;
+    }
+
+    public Enum<ExceptionStatus> getExceptionStatus() {
+        return exceptionStatus;
+    }
+}
