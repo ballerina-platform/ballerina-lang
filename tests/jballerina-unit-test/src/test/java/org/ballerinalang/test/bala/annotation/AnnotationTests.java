7b155ef12ea1d478bb94afa6e948945c02ac1099
MaryamZi <maryamziyadm@gmail.com>
2021-12-14 23:23:03 +0530

    Add tests

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/bala/annotation/AnnotationTests.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/bala/annotation/AnnotationTests.java
@@ -17,6 +17,8 @@
 package org.ballerinalang.test.bala.annotation;
 
 import org.ballerinalang.core.model.values.BValue;
+import org.ballerinalang.model.elements.PackageID;
+import org.ballerinalang.model.symbols.AnnotationSymbol;
 import org.ballerinalang.test.BAssertUtil;
 import org.ballerinalang.test.BCompileUtil;
 import org.ballerinalang.test.BRunUtil;
@@ -25,6 +27,17 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.wso2.ballerinalang.compiler.semantics.model.Scope;
+import org.wso2.ballerinalang.compiler.semantics.model.symbols.BAnnotationSymbol;
+import org.wso2.ballerinalang.compiler.semantics.model.symbols.BClassSymbol;
+import org.wso2.ballerinalang.compiler.semantics.model.symbols.BInvokableSymbol;
+import org.wso2.ballerinalang.compiler.semantics.model.symbols.BVarSymbol;
+import org.wso2.ballerinalang.compiler.tree.BLangPackage;
+import org.wso2.ballerinalang.compiler.util.Name;
+import org.wso2.ballerinalang.compiler.util.Names;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Test cases for reading annotations.
@@ -61,6 +74,53 @@ public class AnnotationTests {
         Assert.assertNotNull(returns[0]);
         Assert.assertEquals(returns[0].stringValue(), "{numVal:10, textVal:\"text\", conditionVal:false, " +
                 "recordVal:{nestNumVal:20, nextTextVal:\"nestText\"}}");
+    }
+
+    @Test
+    public void testParamAnnotAttachmentsViaBir() {
+        BCompileUtil.compileAndCacheBala("test-src/bala/test_projects/test_annotation_project");
+        BCompileUtil.compileAndCacheBala("test-src/bala/test_projects/test_annotation_usage_project");
+        CompileResult result = BCompileUtil.compile(
+                "test-src/bala/test_bala/annotations/param_annot_attachments_bala_test.bal");
+
+        BLangPackage bLangPackage = (BLangPackage) result.getAST();
+        Map<Name, Scope.ScopeEntry> importedModuleEntries = bLangPackage.getImports().get(0).symbol.scope.entries;
+
+        List<? extends AnnotationSymbol> annotationAttachmentSymbols =
+                ((BInvokableSymbol) importedModuleEntries.get(
+                        Names.fromString("func")).symbol).params.get(0).getAnnotations();
+        Assert.assertEquals(annotationAttachmentSymbols.size(), 1);
+        BAnnotationSymbol annotationSymbol = (BAnnotationSymbol) annotationAttachmentSymbols.get(0);
+        PackageID pkgID = annotationSymbol.pkgID;
+        Assert.assertEquals(pkgID.orgName.value, "annots");
+        Assert.assertEquals(pkgID.pkgName.value, "usage");
+        Assert.assertEquals(pkgID.version.value, "0.2.0");
+        Assert.assertEquals(annotationSymbol.name.value, "Allow");
+
+        List<BVarSymbol> params = ((BClassSymbol) importedModuleEntries.get(Names.fromString("TestListener")).symbol)
+                .initializerFunc.symbol.params;
+        annotationAttachmentSymbols = params.get(0).getAnnotations();
+        Assert.assertEquals(annotationAttachmentSymbols.size(), 0);
+        annotationAttachmentSymbols = params.get(1).getAnnotations();
+        Assert.assertEquals(annotationAttachmentSymbols.size(), 2);
+
+        for (AnnotationSymbol annotationAttachmentSymbol : annotationAttachmentSymbols) {
+            annotationSymbol = (BAnnotationSymbol) annotationAttachmentSymbol;
+            pkgID = annotationSymbol.pkgID;
+            Assert.assertEquals(pkgID.orgName.value, "annots");
+
+            String value = pkgID.pkgName.value;
+
+            if ("defn".equals(value)) {
+                Assert.assertEquals(pkgID.version.value, "0.0.1");
+                Assert.assertEquals(annotationSymbol.name.value, "Expose");
+                continue;
+            }
+
+            Assert.assertEquals(value, "usage");
+            Assert.assertEquals(pkgID.version.value, "0.2.0");
+            Assert.assertEquals(annotationSymbol.name.value, "Allow");
+        }
     }
 
     @AfterClass
