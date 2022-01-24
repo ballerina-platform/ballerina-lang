49729380d12f6f76eb6caf3d0e76ae827175660e
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2022-01-24 11:38:01 +0530

    Add initial package

---
+++ misc/compiler-plugins/modules/semver-checker/src/test/java/org/ballerinalang/semver/checker/SemverCheckerTests.java
@@ -0,0 +1,12 @@
+package org.ballerinalang.semver.checker;
+
+import org.ballerinalang.semver.checker.util.FileUtils;
+import org.testng.annotations.Test;
+
+public class SemverCheckerTests {
+
+    @Test
+    public void test1() {
+        FileUtils.createFile();
+    }
+}
