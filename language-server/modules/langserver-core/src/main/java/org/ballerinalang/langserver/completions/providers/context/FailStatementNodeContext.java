3659474deb6e0e868f4316f493dac341f1921947
gayalkuruppu <gayalkuruppu.gk@gmail.com>
2022-02-07 19:10:25 +0530

    give priority to error type symbols in fail-stmt

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/providers/context/FailStatementNodeContext.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/completions/providers/context/FailStatementNodeContext.java
@@ -15,20 +15,25 @@
  */
 package org.ballerinalang.langserver.completions.providers.context;
 
-import io.ballerina.compiler.api.symbols.Symbol;
+import io.ballerina.compiler.api.symbols.*;
 import io.ballerina.compiler.syntax.tree.FailStatementNode;
 import io.ballerina.compiler.syntax.tree.NonTerminalNode;
 import io.ballerina.compiler.syntax.tree.QualifiedNameReferenceNode;
 import io.ballerina.compiler.syntax.tree.SyntaxKind;
 import org.ballerinalang.annotation.JavaSPIService;
+import org.ballerinalang.langserver.common.utils.CommonUtil;
+import org.ballerinalang.langserver.common.utils.SymbolUtil;
 import org.ballerinalang.langserver.common.utils.completion.QNameReferenceUtil;
 import org.ballerinalang.langserver.commons.BallerinaCompletionContext;
 import org.ballerinalang.langserver.commons.completion.LSCompletionException;
 import org.ballerinalang.langserver.commons.completion.LSCompletionItem;
+import org.ballerinalang.langserver.completions.SymbolCompletionItem;
 import org.ballerinalang.langserver.completions.providers.AbstractCompletionProvider;
+import org.ballerinalang.langserver.completions.util.SortingUtil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Completion provider for {@link FailStatementNode} context.
@@ -56,5 +61,64 @@ public class FailStatementNodeContext extends AbstractCompletionProvider<FailSta
         this.sort(context, node, completionItems);
         
         return completionItems;
+    }
+
+    @Override
+    public void sort(BallerinaCompletionContext context, FailStatementNode node,
+                     List<LSCompletionItem> completionItems) {
+        for (LSCompletionItem lsCItem : completionItems) {
+            if (lsCItem.getType() == LSCompletionItem.CompletionItemType.SYMBOL) {
+                SymbolCompletionItem symbolCompletionItem = (SymbolCompletionItem) lsCItem;
+                if (isCompletionItemSubTypeError(symbolCompletionItem)) {
+                    lsCItem.getCompletionItem().setSortText(SortingUtil.genSortText(1));
+                } else {
+                    int rank = SortingUtil.toRank(context, lsCItem);
+                    lsCItem.getCompletionItem().setSortText(SortingUtil.genSortText(rank));
+                }
+            } else if (SortingUtil.isModuleCompletionItem(lsCItem)) {
+                lsCItem.getCompletionItem().setSortText(SortingUtil.genSortText(2));
+            } else {
+                int rank = SortingUtil.toRank(context, lsCItem);
+                lsCItem.getCompletionItem().setSortText(SortingUtil.genSortText(rank));
+            }
+
+        }
+    }
+
+    private boolean isUnionSymbolSubtypeError(UnionTypeSymbol unionTypeSymbol) {
+        List<TypeSymbol> typeSymbols = unionTypeSymbol.memberTypeDescriptors();
+        for (TypeSymbol typeSymbol :typeSymbols) {
+            if (typeSymbol.typeKind() != TypeDescKind.TYPE_REFERENCE) {
+                return false;
+            }
+            TypeReferenceTypeSymbol typeReferenceTypeSymbol = (TypeReferenceTypeSymbol) typeSymbol;
+            if (typeReferenceTypeSymbol.typeDescriptor().typeKind() != TypeDescKind.ERROR) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isCompletionItemSubTypeError(SymbolCompletionItem symbolCompletionItem) {
+        if (symbolCompletionItem.getSymbol().isPresent()) {
+            if (symbolCompletionItem.getSymbol().get().kind() == SymbolKind.TYPE_DEFINITION) {
+                return false;
+            }
+            Optional<TypeSymbol> tSymbol = SymbolUtil.getTypeDescriptor(symbolCompletionItem.getSymbol().get());
+            if (tSymbol.isPresent()) {
+                TypeSymbol typeSymbol = CommonUtil.getRawType(tSymbol.get());
+                if (typeSymbol.typeKind() == TypeDescKind.ERROR) {
+                    return true;
+                } else if (typeSymbol.typeKind() == TypeDescKind.UNION) {
+                    UnionTypeSymbol unionTypeSymbol = (UnionTypeSymbol) typeSymbol;
+                    return isUnionSymbolSubtypeError(unionTypeSymbol);
+                } else if (typeSymbol.typeKind() == TypeDescKind.FUNCTION) {
+                    FunctionTypeSymbol functionTypeSymbol = (FunctionTypeSymbol) typeSymbol;
+                    Optional<TypeSymbol> retTypeDec = functionTypeSymbol.returnTypeDescriptor();
+                    return retTypeDec.isPresent() && retTypeDec.get().typeKind() == TypeDescKind.ERROR;
+                }
+            }
+        }
+        return false;
     }
 }
