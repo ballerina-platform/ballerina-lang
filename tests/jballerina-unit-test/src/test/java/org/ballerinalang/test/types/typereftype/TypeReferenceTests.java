b0e8d963bbd4688a44915521f9db8b007c0dfbfd
SandaruJayawardana <sandaru.jayawardana@gmail.com>
2022-02-10 11:30:30 +0530

    Fix checkstyle error

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/types/typereftype/TypeReferenceTests.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/types/typereftype/TypeReferenceTests.java
@@ -20,7 +20,6 @@ import org.ballerinalang.test.BCompileUtil;
 import org.ballerinalang.test.BRunUtil;
 import org.ballerinalang.test.CompileResult;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static org.ballerinalang.test.BAssertUtil.validateError;
