905d643924a3b053917fadfde7af5d4f35520b25
praveennadarajah <n.praveen.rc@gmail.com>
2022-02-17 19:25:01 +0530

    Avoid adding dynamic breakpoints from line 0 in the caller function

--- misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/JDIEventProcessor.java
+++ misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/JDIEventProcessor.java
@@ -291,7 +291,9 @@ public class JDIEventProcessor {
             Location currentLocation = balStackFrame.getJStackFrame().location();
             ReferenceType referenceType = currentLocation.declaringType();
             List<Location> allLocations = currentLocation.method().allLineLocations();
-            Optional<Location> firstLocation = allLocations.stream().min(Comparator.comparingInt(Location::lineNumber));
+            Optional<Location> firstLocation = allLocations.stream()
+                    .filter(location -> location.lineNumber() > 0)
+                    .min(Comparator.comparingInt(Location::lineNumber));
             Optional<Location> lastLocation = allLocations.stream().max(Comparator.comparingInt(Location::lineNumber));
             if (firstLocation.isEmpty()) {
                 return;
