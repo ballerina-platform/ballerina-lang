eba1a0e63207720d15d887dd9bd595cfb1c2f79e
suleka96 <sulekahelmini96comp@gmail.com>
2022-02-21 10:50:52 +0530

    Update tests

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeBuilder.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeBuilder.java
@@ -1351,8 +1351,8 @@ public class BLangNodeBuilder extends NodeTransformer<BLangNode> {
     @Override
     public BLangNode transform(SingletonTypeDescriptorNode singletonTypeDescriptorNode) {
         BLangFiniteTypeNode bLangFiniteTypeNode = new BLangFiniteTypeNode();
-        BLangExpression literalOrExpression = createLiteralOrExpression(singletonTypeDescriptorNode.simpleContExprNode(),
-                true);
+        BLangExpression literalOrExpression = 
+                createLiteralOrExpression(singletonTypeDescriptorNode.simpleContExprNode(), true);
         bLangFiniteTypeNode.pos = literalOrExpression.pos;
         bLangFiniteTypeNode.valueSpace.add(literalOrExpression);
         return bLangFiniteTypeNode;
