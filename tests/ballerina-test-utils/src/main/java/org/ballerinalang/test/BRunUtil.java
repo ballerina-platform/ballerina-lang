590fb156c47e3f10d3e7d76df7a01f16c733a63e
HindujaB <bhinduja08@gmail.com>
2022-02-28 15:10:40 +0530

    Address review suggestions

--- tests/ballerina-test-utils/src/main/java/org/ballerinalang/test/BRunUtil.java
+++ tests/ballerina-test-utils/src/main/java/org/ballerinalang/test/BRunUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -415,9 +415,6 @@ public class BRunUtil {
             scheduler.start();
             final Throwable t = out.panic;
             if (t != null) {
-                if (t instanceof BLangRuntimeException) {
-                    throw new BLangRuntimeException(t.getMessage());
-                }
                 if (t instanceof ErrorValue) {
                     throw new BLangRuntimeException("error: " + ((ErrorValue) t).getPrintableStackTrace());
                 }
