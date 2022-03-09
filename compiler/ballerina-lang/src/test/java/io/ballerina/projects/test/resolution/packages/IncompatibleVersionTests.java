4d647f10e6e662bb63b1b612a986fd21a9560d76
azinneera <azinneera@gmail.com>
2022-03-09 23:52:39 +0530

    Add package resolution graph tests

--- compiler/ballerina-lang/src/test/java/io/ballerina/projects/test/resolution/packages/IncompatibleVersionTests.java
+++ compiler/ballerina-lang/src/test/java/io/ballerina/projects/test/resolution/packages/IncompatibleVersionTests.java
@@ -17,10 +17,25 @@ public class IncompatibleVersionTests extends AbstractPackageResolutionTest {
     @DataProvider(name = "resolutionTestCaseProvider")
     public static Object[][] testCaseProvider() {
         return new Object[][]{
-                // 1. User specifies an incompatible version in Ballerina.toml
+                // 1. User specifies an incompatible version for a direct dependency in Ballerina.toml
                 {"suite-incompatible_versions", "case-0001", true},
                 {"suite-incompatible_versions", "case-0001", false},
-
+                // 2. User specifies an incompatible version for a dependency in Ballerina.toml - advanced
+                {"suite-incompatible_versions", "case-0002", true},
+                {"suite-incompatible_versions", "case-0002", false},
+                // 3. User adds a new dependency which has a dependency
+                // that conflicts with an existing direct dependency
+                {"suite-incompatible_versions", "case-0003", true},
+                {"suite-incompatible_versions", "case-0003", false},
+                // 4. User specifies an incompatible version for an indirect dependency in Ballerina.toml
+                {"suite-incompatible_versions", "case-0004", true},
+                {"suite-incompatible_versions", "case-0004", false},
+                // 5. User specifies an incompatible version for an indirect dependency in Ballerina.toml - advanced
+                {"suite-incompatible_versions", "case-0005", true},
+                {"suite-incompatible_versions", "case-0005", false},
+                // 6. Two incompatible versions found as transitive dependencies
+                {"suite-incompatible_versions", "case-0006", true},
+                {"suite-incompatible_versions", "case-0006", false},
         };
     }
 }
