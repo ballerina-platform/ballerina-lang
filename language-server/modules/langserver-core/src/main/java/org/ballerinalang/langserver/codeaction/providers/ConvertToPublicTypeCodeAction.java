d774c0205efcde44814e6aac98c2ee152ece4986
mindula <rowelmindula@gmail.com>
2022-03-09 11:04:28 +0530

    Address Review Suggestions

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/codeaction/providers/ConvertToPublicTypeCodeAction.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/codeaction/providers/ConvertToPublicTypeCodeAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ballerinalang.langserver.codeaction.providers;
 
 import io.ballerina.compiler.api.ModuleID;
@@ -25,10 +40,10 @@ import java.util.Optional;
 /**
  * Code Action to make type public.
  *
- * @since 2.0.0
+ * @since 2.0.2
  */
 @JavaSPIService("org.ballerinalang.langserver.commons.codeaction.spi.LSCodeActionProvider")
-public class MakePublicCodeAction extends AbstractCodeActionProvider {
+public class ConvertToPublicTypeCodeAction extends AbstractCodeActionProvider {
     @Override
     public List<CodeAction> getDiagBasedCodeActions(Diagnostic diagnostic,
                                                     DiagBasedPositionDetails positionDetails,
@@ -38,15 +53,27 @@ public class MakePublicCodeAction extends AbstractCodeActionProvider {
         }
         Range range = new Range(CommonUtil.toPosition(diagnostic.location().lineRange().startLine()),
                 CommonUtil.toPosition(diagnostic.location().lineRange().endLine()));
+        if (context.currentSyntaxTree().isEmpty() || context.currentSemanticModel().isEmpty()) {
+            return Collections.emptyList();
+        }
         NonTerminalNode nonTerminalNode = CommonUtil.findNode(range, context.currentSyntaxTree().get());
         Optional<Symbol> symbol = context.currentSemanticModel().get().symbol(nonTerminalNode);
+        if (symbol.isEmpty() || symbol.get().getModule().isEmpty()) {
+            return Collections.emptyList();
+        }
 
         ModuleID moduleID = symbol.get().getModule().get().id();
         String orgName = moduleID.orgName();
         String moduleName = moduleID.moduleName();
         Optional<Project> project = context.workspace().project(context.filePath());
+        if (project.isEmpty()) {
+            return Collections.emptyList();
+        }
         Optional<Path> filePath = DefinitionUtil.getFilePathForDependency(orgName, moduleName, project.get(),
                 symbol.get(), context);
+        if (filePath.isEmpty() || context.workspace().syntaxTree(filePath.get()).isEmpty()) {
+            return Collections.emptyList();
+        }
         URI uri = filePath.get().toUri();
 
         Optional<NonTerminalNode> node = CommonUtil.findNode(symbol.get(),
@@ -60,9 +87,8 @@ public class MakePublicCodeAction extends AbstractCodeActionProvider {
         String editText = "public ";
         TextEdit textEdit = new TextEdit(recordRange, editText);
         List<TextEdit> editList = List.of(textEdit);
-        String recordToPublic = CommandConstants.MAKE_TYPE_PUBLIC;
-        List<CodeAction> codeActions = List.of(createQuickFixCodeAction(recordToPublic, editList, uri.getPath()));
-        return codeActions;
+        String recordToPublic = String.format(CommandConstants.MAKE_TYPE_PUBLIC, symbol.get().getName().orElse(""));
+        return List.of(createQuickFixCodeAction(recordToPublic, editList, uri.toString()));
     }
 
     @Override
--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/common/constants/CommandConstants.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/common/constants/CommandConstants.java
@@ -150,5 +150,5 @@ public class CommandConstants {
 
     public static final String IMPLEMENT_ALL = "Implement all";
 
-    public static final String MAKE_TYPE_PUBLIC = "Make type public";
+    public static final String MAKE_TYPE_PUBLIC = "Convert '%s' to public";
 }
