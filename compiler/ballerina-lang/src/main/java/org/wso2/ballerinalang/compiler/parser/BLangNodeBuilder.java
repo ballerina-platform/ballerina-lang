be81e9123acf2a083c160db7ab2a3ed349ae302c
lochana-chathura <39232462+lochana-chathura@users.noreply.github.com>
2022-03-22 14:18:26 +0530

    Implement AST representation for SpreadMemberNode

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeBuilder.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeBuilder.java
@@ -174,6 +174,7 @@ import io.ballerina.compiler.syntax.tree.SimpleNameReferenceNode;
 import io.ballerina.compiler.syntax.tree.SingletonTypeDescriptorNode;
 import io.ballerina.compiler.syntax.tree.SpecificFieldNode;
 import io.ballerina.compiler.syntax.tree.SpreadFieldNode;
+import io.ballerina.compiler.syntax.tree.SpreadMemberNode;
 import io.ballerina.compiler.syntax.tree.StartActionNode;
 import io.ballerina.compiler.syntax.tree.StatementNode;
 import io.ballerina.compiler.syntax.tree.StreamTypeDescriptorNode;
@@ -328,6 +329,7 @@ import org.wso2.ballerinalang.compiler.tree.expressions.BLangInvocation.BLangAct
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangLambdaFunction;
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangLetExpression;
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangListConstructorExpr;
+import org.wso2.ballerinalang.compiler.tree.expressions.BLangListConstructorExpr.BLangListConstructorSpreadOpExpr;
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangLiteral;
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangMarkDownDeprecatedParametersDocumentation;
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangMarkDownDeprecationDocumentation;
@@ -1981,9 +1983,23 @@ public class BLangNodeBuilder extends NodeTransformer<BLangNode> {
         List<BLangExpression> argExprList = new ArrayList<>();
         BLangListConstructorExpr listConstructorExpr = (BLangListConstructorExpr)
                 TreeBuilder.createListConstructorExpressionNode();
-        for (Node expr : listConstructorExprNode.listMembers()) {
-            argExprList.add(createExpression(expr));
+
+        for (Node listMember : listConstructorExprNode.listMembers()) {
+            BLangExpression memberExpr;
+            if (listMember.kind() == SyntaxKind.SPREAD_MEMBER) {
+                Node spreadMemberExpr = ((SpreadMemberNode) listMember).expression();
+                BLangExpression bLangExpr = createExpression(spreadMemberExpr);
+
+                BLangListConstructorSpreadOpExpr spreadOpExpr = new BLangListConstructorSpreadOpExpr();
+                spreadOpExpr.setExpression(bLangExpr);
+                spreadOpExpr.pos = getPosition(spreadMemberExpr);
+                memberExpr = spreadOpExpr;
+            } else {
+                memberExpr = createExpression(listMember);
+            }
+            argExprList.add(memberExpr);
         }
+
         listConstructorExpr.exprs = argExprList;
         listConstructorExpr.pos = getPosition(listConstructorExprNode);
         return listConstructorExpr;
