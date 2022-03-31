274d2c40ba24db076736c1e53e99e337eea94556
sanjana <anjetnet@gmail.com>
2022-03-31 06:34:21 +0530

    Implement the Map type builder

--- compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/types/BallerinaMapBuilder.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/types/BallerinaMapBuilder.java
@@ -1,26 +1,45 @@
 package io.ballerina.compiler.api.types;
 
+import io.ballerina.compiler.api.impl.symbols.AbstractTypeSymbol;
+import io.ballerina.compiler.api.impl.symbols.TypesFactory;
 import io.ballerina.compiler.api.symbols.MapTypeSymbol;
 import io.ballerina.compiler.api.symbols.TypeSymbol;
+import org.wso2.ballerinalang.compiler.semantics.model.SymbolTable;
+import org.wso2.ballerinalang.compiler.semantics.model.types.BMapType;
+import org.wso2.ballerinalang.compiler.semantics.model.types.BType;
 import org.wso2.ballerinalang.compiler.util.CompilerContext;
+import org.wso2.ballerinalang.compiler.util.TypeTags;
 
 public class BallerinaMapBuilder implements TypeBuilder.MapTypeBuilder {
 
-    CompilerContext context;
+    private final TypesFactory typesFactory;
+    private final SymbolTable symTable;
+    private TypeSymbol typeParam;
 
     public BallerinaMapBuilder(CompilerContext context) {
-        this.context = context;
+        typesFactory = TypesFactory.getInstance(context);
+        symTable = SymbolTable.getInstance(context);
     }
 
     @Override
-    public TypeSymbol withTypeParam(TypeSymbol typeParam) {
-
-        return null;
+    public BallerinaMapBuilder withTypeParam(TypeSymbol typeParam) {
+        this.typeParam = typeParam;
+        return this;
     }
 
     @Override
     public MapTypeSymbol build() {
+        BMapType mapType = new BMapType(TypeTags.MAP, getBType(this.typeParam), symTable.mapType.tsymbol);
+        return (MapTypeSymbol) typesFactory.getTypeDescriptor(mapType);
+    }
 
-        return null;
+    private BType getBType(TypeSymbol typeSymbol) {
+        if (typeSymbol != null) {
+            if (typeSymbol instanceof AbstractTypeSymbol) {
+                return ((AbstractTypeSymbol) typeSymbol).getBType();
+            }
+        }
+
+        return symTable.noType;
     }
 }
