0ca616e57082a0963b20caeeda548e2ef71ff3ef
Kavindu Gimhan Zoysa <kavindu175@gmail.com>
2022-04-07 14:39:21 +0530

    Fix NPE occures for object-constructor in DataFlowAnalyser

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/object/ObjectConstructorTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/object/ObjectConstructorTest.java
@@ -57,6 +57,7 @@ public class ObjectConstructorTest {
                 {"testObjectConstructorWithDistinctTypeReferenceVar"},
                 {"testObjectConstructorWithDefiniteTypeAndWithoutReference"},
                 {"testObjectConstructorExprWithReadOnlyCET"},
+                {"testMultipleVarAssignments"}
         };
     }
 
