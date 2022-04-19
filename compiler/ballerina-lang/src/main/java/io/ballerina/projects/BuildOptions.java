1a18f88cc61e09d873e6019eb96328ed62faab93
pramodya1994 <bpmmendis94@gmail.com>
2022-04-20 00:00:40 +0530

    Change deprected description in BuildOptions

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/BuildOptions.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/BuildOptions.java
@@ -67,7 +67,7 @@ public class BuildOptions {
      * Checks whether experimental compilation option is set.
      *
      * @return Is experimental compilation option is set
-     * @deprecated Since language no longer supports experimental features
+     * @deprecated Since language no longer has experimental features
      */
     @Deprecated(forRemoval = true)
     public boolean experimental() {
@@ -245,7 +245,7 @@ public class BuildOptions {
          * Set experimental compilation option.
          *
          * @return Build options builder
-         * @deprecated Since language no longer supports experimental features
+         * @deprecated Since language no longer has experimental features
          */
         @Deprecated(forRemoval = true)
         public BuildOptionsBuilder setExperimental(Boolean value) {
