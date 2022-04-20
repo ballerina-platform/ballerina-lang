938e5de372d6af7694961789792e2671ae815e46
HirumalPriyashan <hirupriyashanrc@gmail.com>
2022-04-20 09:06:30 +0530

    Add tests for bal shell service

---
+++ misc/ls-extensions/modules/bal-shell-service/src/test/java/io/ballerina/shell/service/BalShellServiceTests.java
@@ -0,0 +1,121 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package io.ballerina.shell.service;
+
+import org.ballerinalang.langserver.util.TestUtil;
+import org.eclipse.lsp4j.jsonrpc.Endpoint;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class BalShellServiceTests {
+    private static final String GET_RESULT = "balShell/getResult";
+    private static final String NOTEBOOK_RESTART = "balShell/restartNotebook";
+    private static final String GET_VARIABLES = "balShell/getVariableValues";
+
+    private static final String GET_SHELL_FILE_SOURCE = "balShell/getShellFileSource";
+    private static final Path RES_DIR = Paths.get("src/test/resources/").toAbsolutePath();
+
+    private Endpoint serviceEndpoint;
+
+    @BeforeClass
+    public void startLanguageServer() {
+        this.serviceEndpoint = TestUtil.initializeLanguageSever();
+    }
+
+    @Test(description = "Test with simple arithmetic")
+    public void testSimpleArithmetic() throws ExecutionException, IOException, InterruptedException {
+        runGetResultTest("simple_arithmetic.json");
+    }
+
+    @Test(description = "Test with variable definitions")
+    public void testBasicVariables() throws ExecutionException, IOException, InterruptedException {
+        runGetResultTest("basic_variables.json");
+    }
+
+    @Test(description = "Test with function definitions")
+    public void testFunctions() throws ExecutionException, IOException, InterruptedException {
+        runGetResultTest("functions.json");
+    }
+
+    @Test(description = "Test with mime types")
+    public void testMimeTypes() throws ExecutionException, IOException, InterruptedException {
+        runGetResultTest("mime_types.json");
+    }
+
+    @Test(description = "Test with errors")
+    public void testErrors() throws ExecutionException, IOException, InterruptedException {
+        runGetResultTest("errors.json");
+    }
+
+    @Test(description = "Test for get bal shell file source")
+    public void getShellFileSource() throws ExecutionException, InterruptedException {
+        CompletableFuture<?> result = serviceEndpoint.request(GET_SHELL_FILE_SOURCE, null);
+        ShellFileSourceResponse generatedResult  = (ShellFileSourceResponse) result.get();
+        Assert.assertNotNull(generatedResult.getFilePath());
+    }
+
+    @Test(description = "Test for notebookRestart")
+    public void testGetVariables() throws IOException {
+        Path file = RES_DIR.resolve("testcases").resolve("get_variables.json");
+        GetVariableTestCase[] testCases = TestUtils.loadVariableTestCases(file);
+        serviceEndpoint.request(NOTEBOOK_RESTART, null); // restart the notebook
+        for (GetVariableTestCase testCase: testCases) {
+            BalShellRequest request = new BalShellRequest(testCase.getSource());
+            CompletableFuture<?> snippetExecute = serviceEndpoint.request(GET_RESULT, request); // execute the snippets
+            snippetExecute.thenRun(() -> {
+                CompletableFuture<?> result = serviceEndpoint.request(GET_VARIABLES, null);
+                List<Map<String, String>> generatedResult;
+                try {
+                    generatedResult = (List<Map<String, String>>) result.get();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+                TestUtils.assertJsonValues(generatedResult, testCase.getResult());
+            });
+        }
+    }
+
+    @Test(description = "Test for notebookRestart")
+    public void testRestart() throws ExecutionException, InterruptedException {
+        CompletableFuture<?> result = serviceEndpoint.request(NOTEBOOK_RESTART, null);
+        boolean generatedResult  = (boolean) result.get();
+        Assert.assertTrue(generatedResult);
+    }
+
+    private void runGetResultTest(String filename) throws ExecutionException, IOException, InterruptedException {
+        Path file = RES_DIR.resolve("testcases").resolve(filename);
+        GetResultTestCase[] testCases = TestUtils.loadResultTestCases(file);
+        for (GetResultTestCase testCase: testCases) {
+            BalShellRequest request = new BalShellRequest(testCase.getSource());
+            CompletableFuture<?> result = serviceEndpoint.request(GET_RESULT, request);
+            BalShellResponse generatedResult  = (BalShellResponse) result.get();
+            TestUtils.assertJsonValues(generatedResult, testCase.getResult());
+        }
+    }
+}
---
+++ misc/ls-extensions/modules/bal-shell-service/src/test/java/io/ballerina/shell/service/TestUtils.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package io.ballerina.shell.service;
+
+import com.google.gson.Gson;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TestUtils {
+    public static GetResultTestCase[] loadResultTestCases(Path filePath) throws IOException {
+        Gson gson = new Gson();
+        return gson.fromJson(readFile(filePath), GetResultTestCase[].class);
+    }
+
+    public static GetVariableTestCase[] loadVariableTestCases(Path filePath) throws IOException {
+        Gson gson = new Gson();
+        return gson.fromJson(readFile(filePath), GetVariableTestCase[].class);
+    }
+
+    public static String readFile(Path filePath) throws IOException {
+        return Files.readString(filePath);
+    }
+
+    public static void assertJsonValues(Object generated, Object expected){
+        Gson gson = new Gson();
+        String jsonGenerated = gson.toJson(generated);
+        String jsonExpected = gson.toJson(expected);
+        Assert.assertEquals(jsonGenerated, jsonExpected);
+    }
+}
