2df397a4940b75a810ec791057e15ddf7e4131f0
HirumalPriyashan <hirupriyashanrc@gmail.com>
2022-04-20 09:06:30 +0530

    Add tests for bal shell service

---
+++ misc/ls-extensions/modules/bal-shell-service/src/test/java/io/ballerina/shell/service/GetResultTestCase.java
@@ -0,0 +1,14 @@
+package io.ballerina.shell.service;
+
+public class GetResultTestCase {
+    private String source;
+    private BalShellResponse result;
+
+    public String getSource() {
+        return source;
+    }
+
+    public BalShellResponse getResult() {
+        return result;
+    }
+}
---
+++ misc/ls-extensions/modules/bal-shell-service/src/test/java/io/ballerina/shell/service/GetVariableTestCase.java
@@ -0,0 +1,16 @@
+package io.ballerina.shell.service;
+
+import java.util.List;
+
+public class GetVariableTestCase {
+    private String source;
+    private List<Object> result;
+
+    public String getSource() {
+        return source;
+    }
+
+    public List<Object> getResult() {
+        return result;
+    }
+}
