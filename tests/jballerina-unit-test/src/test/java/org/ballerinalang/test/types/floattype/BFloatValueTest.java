f7bd66f3dcb3429f0b6444a039a28b6e05789886
suleka96 <sulekahelmini96comp@gmail.com>
2022-04-27 16:30:26 +0530

    Refactor code

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/types/floattype/BFloatValueTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/types/floattype/BFloatValueTest.java
@@ -18,7 +18,6 @@
 package org.ballerinalang.test.types.floattype;
 
 import io.ballerina.runtime.api.values.BArray;
-import io.ballerina.runtime.api.values.BArray;
 import org.ballerinalang.test.BAssertUtil;
 import org.ballerinalang.test.BCompileUtil;
 import org.ballerinalang.test.BRunUtil;
