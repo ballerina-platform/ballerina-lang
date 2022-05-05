d5a6af46e6757e1673f47712325eb2c27386a7bf
Dulaj <balasuriyadulaj@gmail.com>
2022-05-05 11:31:25 +0530

    Add special case handling for string type in `BallerinaSingletonTypeSymbol`
    This reverts commit dc464df76ce3fbeac4e62958dd640776e40db266.

--- compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/impl/symbols/BallerinaSingletonTypeSymbol.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/impl/symbols/BallerinaSingletonTypeSymbol.java
@@ -46,8 +46,9 @@ public class BallerinaSingletonTypeSymbol extends AbstractTypeSymbol implements 
         if (shape.value == null
                 && shape.getBType().tag == TypeTags.NIL) {
             this.typeName = "()";
-        } else if (shape.getOriginalValue() != null) {
-            this.typeName = shape.getOriginalValue();
+        // Special case handling for string type.
+        } else if (shape.getBType().tag == TypeTags.STRING) {
+            this.typeName = "\"" + shape + "\"";
         } else {
             this.typeName = shape.toString();
         }
