15c279bd71ac2783a66e78560bc21f510a877a62
gabilang <ganeshwargabil@gmail.com>
2022-05-18 12:40:50 +0530

    Fix checkstyle issue

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/arrays/ArrayLValueFillTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/arrays/ArrayLValueFillTest.java
@@ -20,7 +20,6 @@ package org.ballerinalang.test.statements.arrays;
 
 import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BArray;
-import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BObject;
 import io.ballerina.runtime.internal.util.exceptions.BLangRuntimeException;
 import org.ballerinalang.test.BCompileUtil;
