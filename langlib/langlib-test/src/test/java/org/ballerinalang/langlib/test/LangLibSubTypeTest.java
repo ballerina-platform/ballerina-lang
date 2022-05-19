bf947b3b7258f4ba1bf9dfc2b07c65e144b241b9
suleka96 <sulekahelmini96comp@gmail.com>
2022-05-19 15:07:47 +0530

    Update tests

--- langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibSubTypeTest.java
+++ langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibSubTypeTest.java
@@ -573,8 +573,8 @@ public class LangLibSubTypeTest {
                 "'(int:Signed8|int:Signed32|int:Unsigned32)', found 'int'", 560, 49);
         BAssertUtil.validateError(result, err++, "incompatible types: expected " +
                 "'(int:Unsigned32|int:Unsigned8|int:Signed16)', found 'int'", 561, 51);
-        BAssertUtil.validateError(result, err++, "'9223372036854775808' is out of range for 'int'", 565, 21);
-        BAssertUtil.validateError(result, err++, "'9223372036854775809' is out of range for 'int'", 566, 23);
+        BAssertUtil.validateError(result, err++, "'9223372036854775808' is out of range for 'int:Signed8'", 565, 21);
+        BAssertUtil.validateError(result, err++, "'9223372036854775809' is out of range for 'int:Signed16'", 566, 23);
 
         Assert.assertEquals(result.getErrorCount(), err);
     }
