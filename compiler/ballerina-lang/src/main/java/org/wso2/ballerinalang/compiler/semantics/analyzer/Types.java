7f2e1ff73aeb1de5a98c5e428f1144c195586fde
ushirask <ushirask@gmail.com>
2022-05-31 14:32:33 +0530

    Check if union contains any|error

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/Types.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/Types.java
@@ -3688,6 +3688,14 @@ public class Types {
                         continue;
                     }
                 }
+                //  readonly can match to a union similar to any|error
+                if (sMember.tag == TypeTags.READONLY && isAssignable(symTable.anyOrErrorType, targetUnion)) {
+                    unresolvedTypes.add(new TypePair(sMember, targetUnion));
+                    if (isAssignable(sMember, targetUnion, unresolvedTypes)) {
+                        sourceIterator.remove();
+                        continue;
+                    }
+                }
                 continue;
             }
 
