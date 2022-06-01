98c2fc51a0a4f14ed031498954e0cfc152708acb
lochana-chathura <39232462+lochana-chathura@users.noreply.github.com>
2022-06-01 17:58:31 +0530

    Fix auto merge issue

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/types/xml/XMLLiteralTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/types/xml/XMLLiteralTest.java
@@ -190,17 +190,16 @@ public class XMLLiteralTest {
         BAssertUtil.validateError(negativeResult, index++, "incompatible types: expected " +
                 "'xml:Text', found 'xml<xml>'", 124, 19);
         BAssertUtil.validateError(negativeResult, index++, "missing xml CDATA end token", 128, 49);
+        BAssertUtil.validateError(negativeResult, index++, "xml namespaces cannot be interpolated", 133, 45);
         BAssertUtil.validateError(negativeResult, index++,
-                "incompatible types: expected 'xml', found 'string'", 132, 60);
+                "incompatible types: expected 'xml', found 'string'", 140, 60);
         BAssertUtil.validateError(negativeResult, index++,
-                "incompatible types: expected 'xml', found 'string'", 133, 60);
+                "incompatible types: expected 'xml', found 'string'", 141, 60);
         BAssertUtil.validateError(negativeResult, index++,
-                "incompatible types: expected 'xml', found 'int'", 134, 69);
+                "incompatible types: expected 'xml', found 'int'", 142, 69);
         BAssertUtil.validateError(negativeResult, index++,
                 "invalid literal for type 'xml': raw templates can only be assigned to " +
-                        "abstract subtypes of 'ballerina/lang.object:0.0.0:RawTemplate'", 135, 52);
-
-        BAssertUtil.validateError(negativeResult, index++, "xml namespaces cannot be interpolated", 133, 45);
+                        "abstract subtypes of 'ballerina/lang.object:0.0.0:RawTemplate'", 143, 52);
 
         Assert.assertEquals(index, negativeResult.getErrorCount());
     }
