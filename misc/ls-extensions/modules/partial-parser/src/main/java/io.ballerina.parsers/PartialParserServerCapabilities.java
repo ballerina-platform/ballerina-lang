9bf9ee11894fa6e30e5c97e65a867c2f941b3710
Madusha Gunasekara <madushajg@gmail.com>
2022-06-08 09:56:38 +0530

    Update the bal versions in java docs

--- misc/ls-extensions/modules/partial-parser/src/main/java/io.ballerina.parsers/PartialParserServerCapabilities.java
+++ misc/ls-extensions/modules/partial-parser/src/main/java/io.ballerina.parsers/PartialParserServerCapabilities.java
@@ -20,7 +20,7 @@ import org.ballerinalang.langserver.commons.registration.BallerinaServerCapabili
 /**
  * Server capabilities for the partialParser service.
  *
- * @since 2.0.0
+ * @since 2201.1.1
  */
 public class PartialParserServerCapabilities extends BallerinaServerCapability {
 
