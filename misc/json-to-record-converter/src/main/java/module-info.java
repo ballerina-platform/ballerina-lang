47db22b6e74fe68586840ab1ce83bc48f2ca42c4
Azeem Muzammil <azeemmuzammil@yahoo.com>
2022-06-17 14:35:31 +0530

    Add apache common lang3 dependency to use StringUtils
    (cherry picked from commit a33ae56e49a53c3a101942db5c10492aecf2bc9b)
    (cherry picked from commit 3153a39ef42a6dc49ebd552741c5df6ae30047a4)

--- misc/json-to-record-converter/src/main/java/module-info.java
+++ misc/json-to-record-converter/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module io.ballerina.Misc.jsonToRecordConverter {
     requires com.google.gson;
     requires io.ballerina.parser;
     requires io.ballerina.formatter.core;
+    requires org.apache.commons.lang3;
 
     exports io.ballerina.converters;
 }
