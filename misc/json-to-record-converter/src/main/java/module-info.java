89f2078c5eb7a80969255c35fd804a3b64966200
Azeem Muzammil <azeemmuzammil@yahoo.com>
2022-06-20 23:30:23 +0530

    Add JavaTuples library

--- misc/json-to-record-converter/src/main/java/module-info.java
+++ misc/json-to-record-converter/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module io.ballerina.Misc.jsonToRecordConverter {
     requires io.ballerina.parser;
     requires io.ballerina.formatter.core;
     requires org.apache.commons.lang3;
+    requires javatuples;
 
     exports io.ballerina.converters;
 }
