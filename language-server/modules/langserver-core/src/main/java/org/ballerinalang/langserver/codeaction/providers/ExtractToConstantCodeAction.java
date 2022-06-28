fc42ff808c0ed4d73f4db4a63cf156bc13600663
Tharushi Jayasekara <tharushi68@gmail.com>
2022-06-28 12:01:27 +0530

    Add extract to constant code action

---
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/codeaction/providers/ExtractToConstantCodeAction.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ballerinalang.langserver.codeaction.providers;
+
+import io.ballerina.compiler.api.symbols.Symbol;
+import io.ballerina.compiler.syntax.tree.BasicLiteralNode;
+import io.ballerina.compiler.syntax.tree.Node;
+import io.ballerina.compiler.syntax.tree.SyntaxKind;
+import org.ballerinalang.annotation.JavaSPIService;
+import org.ballerinalang.langserver.common.constants.CommandConstants;
+import org.ballerinalang.langserver.common.utils.NameUtil;
+import org.ballerinalang.langserver.common.utils.PositionUtil;
+import org.ballerinalang.langserver.commons.CodeActionContext;
+import org.ballerinalang.langserver.commons.codeaction.CodeActionNodeType;
+import org.ballerinalang.langserver.commons.codeaction.spi.NodeBasedPositionDetails;
+import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.CodeActionKind;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4j.TextEdit;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Code Action for extracting to a constant.
+ *
+ * @since 2201.1.2
+ */
+@JavaSPIService("org.ballerinalang.langserver.commons.codeaction.spi.LSCodeActionProvider")
+public class ExtractToConstantCodeAction extends AbstractCodeActionProvider {
+
+    public static final String NAME = "Extract To Constant";
+    private static final String CONSTANT_NAME_PREFIX = "Constant";
+
+    public ExtractToConstantCodeAction() {
+        super(Collections.singletonList(CodeActionNodeType.LITERAL));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<CodeAction> getNodeBasedCodeActions(CodeActionContext context,
+                                                    NodeBasedPositionDetails posDetails) {
+        
+        Node node = posDetails.matchedCodeActionNode();
+        if (node.parent().kind() == SyntaxKind.CONST_DECLARATION || 
+                node.parent().kind() == SyntaxKind.INVALID_EXPRESSION_STATEMENT ||
+                (node.kind() != SyntaxKind.NUMERIC_LITERAL && node.kind() != SyntaxKind.STRING_LITERAL && 
+                        node.kind() != SyntaxKind.BOOLEAN_LITERAL)) {
+            return Collections.emptyList();
+        }
+
+        BasicLiteralNode basicLiteralNode = (BasicLiteralNode) node;
+        String value = basicLiteralNode.literalToken().toString().strip();
+        
+        Set<String> visibleSymbolNames = context.visibleSymbols(context.cursorPosition()).stream()
+                .map(Symbol::getName)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toSet());
+        String name = NameUtil.generateTypeName(CONSTANT_NAME_PREFIX, visibleSymbolNames);
+        
+        Node modPartNode = node;
+        while (modPartNode.parent().kind() != SyntaxKind.MODULE_PART) {
+            modPartNode = modPartNode.parent();
+        }
+        
+        Range declRange = new Range(PositionUtil.toPosition(modPartNode.lineRange().startLine()),
+                PositionUtil.toPosition(modPartNode.lineRange().startLine()));
+        String constDecl = String.format("const %s = %s;\n\n", name, value);
+        TextEdit constDeclEdit = new TextEdit(declRange, constDecl);
+        
+        Range usageRange = new Range(PositionUtil.toPosition(basicLiteralNode.lineRange().startLine()),
+                PositionUtil.toPosition(basicLiteralNode.lineRange().endLine()));
+        TextEdit replaceEdit = new TextEdit(usageRange,  name);
+
+        return Collections.singletonList(createCodeAction(CommandConstants.EXTRACT_TO_CONSTANT, List.of(constDeclEdit, replaceEdit), context.fileUri(),
+                CodeActionKind.RefactorExtract));
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+}
