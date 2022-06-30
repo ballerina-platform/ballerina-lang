786b9407df40940fe54ea1015ac9effc9e14eace
SumayyaZiyad <github.sumayyaziyad@gmail.com>
2022-06-30 11:27:40 +0530

    Allow terminal kind string inconsistencies

--- misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/config/ClientConfigHolder.java
+++ misc/debug-adapter/modules/debug-adapter-core/src/main/java/org/ballerinalang/debugadapter/config/ClientConfigHolder.java
@@ -93,7 +93,7 @@ public class ClientConfigHolder {
 
     public RunInTerminalRequestArgumentsKind getRunInTerminalKind() {
         if (clientRequestArgs.get(ARG_TERMINAL_KIND) != null) {
-            String terminalConfig = clientRequestArgs.get(ARG_TERMINAL_KIND).toString();
+            String terminalConfig = clientRequestArgs.get(ARG_TERMINAL_KIND).toString().toUpperCase();
             if (terminalConfig.equals(INTEGRATED_TERMINAL_KIND)) {
                 return RunInTerminalRequestArgumentsKind.INTEGRATED;
             } else if (terminalConfig.equals(EXTERNAL_TERMINAL_KIND)) {
