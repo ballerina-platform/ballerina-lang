1047b53c98682c9af213d1d79cff12421f77d80e
Nadeeshan96 <nadeeshand@wso2.com>
2022-07-04 20:07:27 +0530

    Add expected output to the error message

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/troubleshoot/StrandDumpTest.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/troubleshoot/StrandDumpTest.java
@@ -133,15 +133,15 @@ public class StrandDumpTest extends BaseTest {
             expectedOutputRegEx = Files.lines(expectedOutputFilePath)
                     .filter(s -> !s.isBlank()).collect(Collectors.toList());
         } catch (IOException e) {
-            throw new BallerinaTestException("Failure to read from expected strand dump output file");
+            throw new BallerinaTestException("Failure to read from the expected strand dump output file");
         }
 
         for (String expectedRegEx : expectedOutputRegEx) {
             Pattern pattern = Pattern.compile(expectedRegEx);
             Matcher matcher = pattern.matcher(obtainedStrandDump);
             if (!matcher.find()) {
-                throw new BallerinaTestException("Failure to obtain the expected strand dump. Obtained strand dump:\n"
-                + obtainedStrandDump);
+                throw new BallerinaTestException("Failure to obtain the expected strand dump.\nExpected regular " +
+                        "expression: " + expectedRegEx + "\nObtained strand dump:\n" + obtainedStrandDump);
             }
         }
     }
