082e74e54a60e6563abf12fcbcdc558230da3ea9
malinthar <malintha1996@gmail.com>
2022-07-11 15:58:42 +0530

    Enable LS action node tests

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/ActionNodeContextTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/ActionNodeContextTest.java
@@ -20,6 +20,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -45,7 +46,6 @@ public class ActionNodeContextTest extends CompletionTest {
 
     @Override
     public List<String> skipList() {
-        // Issue: #36891
-        return List.of("remote_action_config3.json", "client_resource_access_action_config8.json");
+        return Collections.emptyList();
     }
 }
