5e381b093675aed25b2b94448e970c276e0050de
Tharushi Jayasekara <tharushi68@gmail.com>
2022-07-18 11:25:36 +0530

    Update license header

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/ExtractToConstantTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/ExtractToConstantTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ * Copyright (c) 2022, WSO2 Inc. (http://wso2.com) All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -23,6 +23,8 @@ import java.io.IOException;
 
 /**
  * Test cases for Extract to Constant code action.
+ * 
+ * @since 2201.2.0
  */
 public class ExtractToConstantTest extends AbstractCodeActionTest {
 
