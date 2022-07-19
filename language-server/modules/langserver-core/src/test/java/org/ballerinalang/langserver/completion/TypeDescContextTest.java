4f0cf27cbd5ff3ee2889ac0ecd522f269409ce71
SasinduDilshara <dilsharasasindu@gmail.com>
2022-07-19 11:19:15 +0530

    Fix error messages in langlib tests

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/TypeDescContextTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/completion/TypeDescContextTest.java
@@ -22,7 +22,6 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
