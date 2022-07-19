53d3a26a5186e5496a7d20506fc01c860256ed94
Prakanth Thilakaraj <tprakanth1997@gmail.com>
2022-07-19 09:11:09 +0530

    Create class to manage language properties files

---
+++ compiler/ballerina-parser/src/main/java/io/ballerina/compiler/internal/parser/Configurations.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package io.ballerina.compiler.internal.parser;
+
+import java.util.Base64;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public final class Configurations {
+
+    private ResourceBundle properties = null;
+    private static volatile Configurations instance = null;
+
+    private Configurations (String language){
+        Locale currentLanguage = new Locale(language);
+        this.properties = ResourceBundle.getBundle("token", currentLanguage);
+    }
+
+    public static Configurations getInstance(String language) {
+        if (instance == null) {
+            synchronized (Configurations.class) {
+                if (instance == null) {
+                    instance = new Configurations(language);
+                }
+            }
+        }
+        return instance;
+    }
+
+    public String getProperty(String key){
+        String result = null;
+        if (key !=null){
+            result = this.properties.getString(key);
+            result = decodeTokenText(result);
+        }
+        return result;
+    }
+
+    public String decodeTokenText(String encodedTokenText) {
+        return new String(Base64.getDecoder().decode(encodedTokenText));
+    }
+}
