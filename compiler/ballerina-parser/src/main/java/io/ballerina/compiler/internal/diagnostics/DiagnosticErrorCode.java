fa054b9b9bb102247969fe151dcdc38c0c0003a4
lasinicl <lasini.1021@gmail.com>
2022-07-21 12:23:49 +0530

    Add error codes

--- compiler/ballerina-parser/src/main/java/io/ballerina/compiler/internal/diagnostics/DiagnosticErrorCode.java
+++ compiler/ballerina-parser/src/main/java/io/ballerina/compiler/internal/diagnostics/DiagnosticErrorCode.java
@@ -192,6 +192,7 @@ public enum DiagnosticErrorCode implements DiagnosticCode {
     ERROR_MISSING_STREAM_KEYWORD("BCE0292", "error.missing.stream.keyword"),
     ERROR_MISSING_READONLY_KEYWORD("BCE0293", "error.missing.readonly.keyword"),
     ERROR_MISSING_DISTINCT_KEYWORD("BCE0294", "error.missing.distinct.keyword"),
+    ERROR_MISSING_RE_KEYWORD("BCE0295", "error.missing.re.keyword"),
 
     // Missing other tokens
     ERROR_MISSING_IDENTIFIER("BCE0400", "error.missing.identifier"),
@@ -345,7 +346,13 @@ public enum DiagnosticErrorCode implements DiagnosticCode {
     ERROR_INVALID_BINDING_PATTERN("BCE0677", "error.invalid.binding.pattern"),
     ERROR_RESOURCE_PATH_CANNOT_BEGIN_WITH_SLASH("BCE0678", "error.resource.path.cannot.begin.with.slash"),
     REST_PARAMETER_CANNOT_BE_INCLUDED_RECORD_PARAMETER("BCE0679",
-            "error.rest.parameter.cannot.be.included.record.parameter")
+            "error.rest.parameter.cannot.be.included.record.parameter"),
+    ERROR_INVALID_TOKEN_IN_REG_EXP("BCE0680", "error.invalid.token.in.reg.exp"),
+    ERROR_INVALID_UNICODE_PROP_ESCAPE_IN_REG_EXP("BCE0681", "error.invalid.unicode.property.escape.in.reg.exp"),
+    ERROR_INVALID_QUANTIFIER_IN_REG_EXP("BCE0682", "error.invalid.quantifier.in.reg.exp"),
+    ERROR_INVALID_CHARACTER_SET_IN_REG_EXP("BCE0683", "error.invalid.character.set.in.reg.exp"),
+    ERROR_INVALID_FLAG_IN_REG_EXP("BCE0684", "error.invalid.flag.in.reg.exp"),
+
     ;
 
     String diagnosticId;
