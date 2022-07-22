ecc961fea0cd546a75d2d05c15002d0cac84d1ae
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2022-07-22 17:52:54 +0530

    Include function type checking for DDT

--- misc/testerina/modules/testerina-runtime/src/main/java/org/ballerinalang/test/runtime/BTestRunner.java
+++ misc/testerina/modules/testerina-runtime/src/main/java/org/ballerinalang/test/runtime/BTestRunner.java
@@ -26,6 +26,7 @@ import io.ballerina.runtime.api.types.ByteType;
 import io.ballerina.runtime.api.types.DecimalType;
 import io.ballerina.runtime.api.types.ErrorType;
 import io.ballerina.runtime.api.types.FloatType;
+import io.ballerina.runtime.api.types.FunctionType;
 import io.ballerina.runtime.api.types.IntegerType;
 import io.ballerina.runtime.api.types.MapType;
 import io.ballerina.runtime.api.types.ObjectType;
@@ -47,6 +48,7 @@ import io.ballerina.runtime.internal.types.BTupleType;
 import io.ballerina.runtime.internal.values.ArrayValue;
 import io.ballerina.runtime.internal.values.DecimalValue;
 import io.ballerina.runtime.internal.values.ErrorValue;
+import io.ballerina.runtime.internal.values.FPValue;
 import io.ballerina.runtime.internal.values.MapValue;
 import io.ballerina.runtime.internal.values.ObjectValue;
 import io.ballerina.runtime.internal.values.XmlValue;
@@ -957,6 +959,8 @@ public class BTestRunner {
             type = ObjectValue.class;
         } else if (elementType instanceof ErrorType) {
             type = ErrorValue.class;
+        } else if (elementType instanceof FunctionType) {
+            type =  FPValue.class;
         } else {
             // default case
             type = Object.class;
