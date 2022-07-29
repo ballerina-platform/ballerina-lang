9d2153e3ffc4add8b05fbc4e87aa1a9c7bc3fb05
Nipuna Fernando <nipuna.18@cse.mrt.ac.lk>
2022-07-29 13:10:58 +0530

    Add compiler plugin to Testerina

---
+++ misc/testerina/modules/testerina-compiler-plugin/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module io.ballerina.testerina.compiler {
+    exports org.ballerinalang.testerina.compiler;
+    requires io.ballerina.lang;
+    requires io.ballerina.parser;
+    requires io.ballerina.tools.api;
+}
