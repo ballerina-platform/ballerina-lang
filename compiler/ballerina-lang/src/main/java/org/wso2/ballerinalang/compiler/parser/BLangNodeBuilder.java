f3cd86bc452f6f6fc9cbbd3b6b71e91fefa12165
lasinicl <lasini.1021@gmail.com>
2022-08-03 13:29:16 +0530

    Fix issues with syntax

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeBuilder.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/parser/BLangNodeBuilder.java
@@ -2342,6 +2342,8 @@ public class BLangNodeBuilder extends NodeTransformer<BLangNode> {
                 BLangNode xmlTemplateLiteral = createXmlTemplateLiteral(expressionNode);
                 xmlTemplateLiteral.pos = getPosition(expressionNode);
                 return xmlTemplateLiteral;
+            // This will be handled with the rest of the implementation.
+            case REGEX_TEMPLATE_EXPRESSION:
             case STRING_TEMPLATE_EXPRESSION:
                 return createStringTemplateLiteral(expressionNode.content(), getPosition(expressionNode));
             case RAW_TEMPLATE_EXPRESSION:
