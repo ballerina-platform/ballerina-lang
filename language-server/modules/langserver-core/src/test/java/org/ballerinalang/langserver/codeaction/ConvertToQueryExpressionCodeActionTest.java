6be4a09c64e80a1cfc0c2a337e734be3ae259c96
Imesha Sudasingha <imesha.sudasingha@gmail.com>
2022-08-19 20:11:27 +0530

    Add convert to query expr code action initial version

---
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/ConvertToQueryExpressionCodeActionTest.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2022, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.ballerinalang.langserver.codeaction;
+
+import org.ballerinalang.langserver.commons.workspace.WorkspaceDocumentException;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class ConvertToQueryExpressionCodeActionTest extends AbstractCodeActionTest {
+
+    @Test(dataProvider = "codeaction-data-provider")
+    @Override
+    public void test(String config) throws IOException, WorkspaceDocumentException {
+        super.test(config);
+    }
+
+    @DataProvider(name = "codeaction-data-provider")
+    @Override
+    public Object[][] dataProvider() {
+        return new Object[][]{
+                {"convert__to_query_expr1.json"}
+        };
+    }
+
+    @Override
+    public String getResourceDir() {
+        return "convert-to-query-expr";
+    }
+}
