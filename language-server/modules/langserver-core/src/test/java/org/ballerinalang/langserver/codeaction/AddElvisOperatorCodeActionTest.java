9ffc1a2a7175d030af9d77735bf23a6e87607725
malinthar <malintha1996@gmail.com>
2022-08-21 19:30:18 +0530

    Add more test cases

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/AddElvisOperatorCodeActionTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/AddElvisOperatorCodeActionTest.java
@@ -7,11 +7,11 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 /**
- * Test Cases for {@link org.ballerinalang.langserver.codeaction.providers.AddElvisOperatorCodeAction}.
+ * Test cases for {@link org.ballerinalang.langserver.codeaction.providers.AddElvisOperatorCodeAction}.
  *
  * @since 2201.2.1
  */
-public class AddElvisOperatorCodeActionTest extends AbstractCodeActionTest{
+public class AddElvisOperatorCodeActionTest extends AbstractCodeActionTest {
 
     @Test(dataProvider = "codeaction-data-provider")
     @Override
@@ -28,21 +28,23 @@ public class AddElvisOperatorCodeActionTest extends AbstractCodeActionTest{
     @DataProvider(name = "codeaction-data-provider")
     @Override
     public Object[][] dataProvider() {
-        return new Object[][] {
+        return new Object[][]{
                 {"add_elvis_operator_in_mapping_constructor.json"},
                 {"add_elvis_operator_in_variable_declaration1.json"},
                 {"add_elvis_operator_in_variable_declaration2.json"},
                 {"add_elvis_operator_in_positional_argument.json"},
+                {"add_elvis_operator_in_return_statement.json"},
+                {"add_elvis_operator_in_let_var1.json"},
+                {"add_elvis_operator_in_let_var2.json"}
         };
     }
 
     @DataProvider(name = "negative-codeaction-data-provider")
     public Object[][] negativeDataProvider() {
-        return new Object[][] {
-                {"add_elvis_operator_in_mapping_constructor_negative1.json"}
+        return new Object[][]{
+                {"negative_add_elvis_operator_in_mapping_constructor.json"}
         };
     }
-    
 
     @Override
     public String getResourceDir() {
