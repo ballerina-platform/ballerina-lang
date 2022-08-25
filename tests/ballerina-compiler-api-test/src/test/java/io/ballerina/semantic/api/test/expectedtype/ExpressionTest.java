7b582e21e44967b024162c3255a813d5b17d3a6a
DamithaSenevirathne <damithasenevirathne@gmail.com>
2022-08-25 10:49:24 +0530

    Add testcases

---
+++ tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/expectedtype/ExpressionTest.java
@@ -0,0 +1,44 @@
+package io.ballerina.semantic.api.test.expectedtype;
+
+import io.ballerina.compiler.api.SemanticModel;
+import io.ballerina.compiler.api.symbols.TypeDescKind;
+import io.ballerina.compiler.api.symbols.TypeSymbol;
+import io.ballerina.projects.Document;
+import io.ballerina.projects.Project;
+import io.ballerina.tools.text.LinePosition;
+import org.ballerinalang.test.BCompileUtil;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+import static io.ballerina.semantic.api.test.util.SemanticAPITestUtils.getDefaultModulesSemanticModel;
+import static io.ballerina.semantic.api.test.util.SemanticAPITestUtils.getDocumentForSingleSource;
+import static org.testng.Assert.assertEquals;
+
+public class ExpressionTest {
+    private SemanticModel model;
+    private Document srcFile;
+
+    @BeforeClass
+    public void setup() {
+        Project project = BCompileUtil.loadProject("test-src/expected-type/expression_test.bal");
+        model = getDefaultModulesSemanticModel(project);
+        srcFile = getDocumentForSingleSource(project);
+    }
+
+    @Test(dataProvider = "LinePosProvider")
+    public void testExpectedType(int line, int col, TypeDescKind typeDescKind) {
+        Optional<TypeSymbol> typeSymbol = model.
+                expectedType(srcFile, LinePosition.from(line, col));
+        assertEquals(typeSymbol.get().typeKind(), typeDescKind);
+    }
+
+    @DataProvider(name = "LinePosProvider")
+    public Object[][] getLinePos() {
+        return new Object[][]{
+                {23, 19, TypeDescKind.INT},
+        };
+    }
+}
