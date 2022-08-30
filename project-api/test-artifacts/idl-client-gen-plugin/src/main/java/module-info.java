86c98ec6adab70f5c5ec70bfd723cb03f5eb7ae9
azinneera <azinneera@gmail.com>
2022-08-30 10:12:47 +0530

    Fix failing compiler plugin tests

--- project-api/test-artifacts/idl-client-gen-plugin/src/main/java/module-info.java
+++ project-api/test-artifacts/idl-client-gen-plugin/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module compiler.plugin.test.init.func.codemodify {
     requires io.ballerina.lang;
     requires io.ballerina.parser;
     requires io.ballerina.tools.api;
-    requires compiler.plugin.test.diagnostic.utils.lib;
 
     exports io.asmaj.plugins.idlclient;
 }
