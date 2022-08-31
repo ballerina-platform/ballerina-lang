296a8d19a50b618a021c6d0a4d51e5f39448220a
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2022-08-31 11:16:47 +0530

    Add test cases for change sub type of raw tmplt CA

---
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/ChangeSubTypeOfRawTemplateCodeActionTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2022, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ballerinalang.langserver.codeaction;
+
+import org.ballerinalang.langserver.commons.workspace.WorkspaceDocumentException;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * Test cases for ChangeSubTypeOfRawTemplateCA.
+ *
+ * @since 2201.2.0
+ */
+public class ChangeSubTypeOfRawTemplateCodeActionTest extends AbstractCodeActionTest {
+
+    @Override
+    @Test(dataProvider = "codeaction-data-provider")
+    public void test(String config) throws IOException, WorkspaceDocumentException {
+        super.test(config);
+    }
+
+    @DataProvider(name = "codeaction-data-provider")
+    @Override
+    public Object[][] dataProvider() {
+        return new Object[][]{
+                {"config1.json"},
+                {"config2.json"},
+                {"config3.json"},
+                {"config4.json"},
+                {"config5.json"},
+                {"config6.json"},
+                {"config7.json"},
+        };
+    }
+
+    @Override
+    public String getResourceDir() {
+        return "raw-template";
+    }
+}
