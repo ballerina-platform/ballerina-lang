a8dade2c8bee9a3ea60f69ccd1cdc0885b18f468
MaryamZi <maryamziyadm@gmail.com>
2022-08-31 14:12:28 +0530

    Remove unnecessary dependency

--- project-api/test-artifacts/idl-import-simple-compiler-plugin/src/main/java/module-info.java
+++ project-api/test-artifacts/idl-import-simple-compiler-plugin/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module simple.idl.client.plugin {
     requires io.ballerina.lang;
     requires io.ballerina.parser;
     requires io.ballerina.tools.api;
-    requires compiler.plugin.test.diagnostic.utils.lib;
 
     exports io.idl.plugins.simpleclient;
 }
