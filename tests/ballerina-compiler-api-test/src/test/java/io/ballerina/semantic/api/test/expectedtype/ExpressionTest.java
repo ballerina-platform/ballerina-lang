f87f53dbba11c5d322a417d5b3167d43838908af
DamithaSenevirathne <damithasenevirathne@gmail.com>
2022-09-15 13:12:36 +0530

    Refactor code

--- tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/expectedtype/ExpressionTest.java
+++ tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/expectedtype/ExpressionTest.java
@@ -17,6 +17,11 @@ import static io.ballerina.semantic.api.test.util.SemanticAPITestUtils.getDefaul
 import static io.ballerina.semantic.api.test.util.SemanticAPITestUtils.getDocumentForSingleSource;
 import static org.testng.Assert.assertEquals;
 
+/**
+ * Test cases for the find expected types in expressions.
+ *
+ * @since 2.3.0
+ */
 public class ExpressionTest {
     private SemanticModel model;
     private Document srcFile;
@@ -44,6 +49,20 @@ public class ExpressionTest {
                 {51, 20, TypeDescKind.TYPE_REFERENCE},
                 {52, 23, TypeDescKind.UNION},
                 {57, 14, TypeDescKind.INT},
+                {62, 8, TypeDescKind.BOOLEAN},
+//                {68, 10, TypeDescKind.BOOLEAN},
+                {89, 32, TypeDescKind.STRING},
+                {90, 40, TypeDescKind.INT},
+                {91, 44, TypeDescKind.INT},
+                {96, 19, TypeDescKind.ANY},
+                {111, 31, TypeDescKind.STRING},
+                {111, 31, TypeDescKind.STRING},
+                {112, 33, TypeDescKind.TYPE_REFERENCE},
+////                {113, 35, TypeDescKind.TYPE_REFERENCE} this is a failing case
+                {117, 28, TypeDescKind.INT},
+                {118, 19, TypeDescKind.INT},
+                {119, 18, TypeDescKind.ARRAY},
+
         };
     }
 }
