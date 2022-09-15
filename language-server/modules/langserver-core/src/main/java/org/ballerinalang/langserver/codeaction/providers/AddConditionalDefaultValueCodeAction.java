773edb688d4dfa3ff5e03d5479b24351d5869d34
malinthar <malintha1996@gmail.com>
2022-09-15 16:44:32 +0530

    Fix checkstyle issue

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/codeaction/providers/AddConditionalDefaultValueCodeAction.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/codeaction/providers/AddConditionalDefaultValueCodeAction.java
@@ -39,7 +39,6 @@ import org.eclipse.lsp4j.TextEdit;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
