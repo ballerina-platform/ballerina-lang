4dbae994eb4e1f4bed72891860a130ed5610259f
prakanth <50439067+prakanth97@users.noreply.github.com>
2022-09-20 17:47:05 +0530

    Add table constructor reference type test

---
+++ tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/ReferenceTypeTest.java
@@ -0,0 +1,51 @@
+package io.ballerina.semantic.api.test;
+
+import io.ballerina.compiler.api.SemanticModel;
+import io.ballerina.compiler.api.impl.symbols.BallerinaTypeReferenceTypeSymbol;
+import io.ballerina.compiler.api.symbols.TypeSymbol;
+import io.ballerina.compiler.syntax.tree.Node;
+import io.ballerina.compiler.syntax.tree.NodeVisitor;
+import io.ballerina.compiler.syntax.tree.SyntaxTree;
+import io.ballerina.compiler.syntax.tree.TableConstructorExpressionNode;
+import io.ballerina.projects.DocumentId;
+import io.ballerina.projects.ModuleId;
+import io.ballerina.projects.Package;
+import io.ballerina.projects.Project;
+import org.ballerinalang.test.BCompileUtil;
+import org.testng.annotations.Test;
+import java.util.Optional;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+/**
+ * This class contains constructor's type reference type related test cases.
+ */
+public class ReferenceTypeTest {
+
+    @Test
+    public void testLookup() {
+        Project project = BCompileUtil.loadProject("test-src/reference_type_test.bal");
+        Package currentPackage = project.currentPackage();
+        ModuleId defaultModuleId = currentPackage.getDefaultModule().moduleId();
+        DocumentId docId = currentPackage.getDefaultModule().documentIds().iterator().next();
+        SyntaxTree syntaxTree = currentPackage.getDefaultModule().document(docId).syntaxTree();
+        SemanticModel model = currentPackage.getCompilation().getSemanticModel(defaultModuleId);
+        syntaxTree.rootNode().accept(getNodeVisitor(model));
+    }
+
+    NodeVisitor getNodeVisitor(SemanticModel model) {
+        return new NodeVisitor() {
+
+            @Override
+            public void visit(TableConstructorExpressionNode tableConstructorExpressionNode) {
+                assertType(tableConstructorExpressionNode, model, "BarTable");
+            }
+        };
+    }
+
+    private void assertType(Node node, SemanticModel model, String ReferenceTypeName) {
+        Optional<TypeSymbol> type = model.typeOf(node);
+        assertTrue(type.isPresent());
+        assertEquals((((BallerinaTypeReferenceTypeSymbol) type.get()).getBType().toString()), ReferenceTypeName);
+    }
+}
