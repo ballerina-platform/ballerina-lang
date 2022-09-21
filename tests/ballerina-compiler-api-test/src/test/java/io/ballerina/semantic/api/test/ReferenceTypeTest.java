52e62071f3bb537d9aa1052386aac2e784b9e268
prakanth <50439067+prakanth97@users.noreply.github.com>
2022-09-21 08:14:19 +0530

    Add license header

--- tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/ReferenceTypeTest.java
+++ tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/ReferenceTypeTest.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package io.ballerina.semantic.api.test;
 
 import io.ballerina.compiler.api.SemanticModel;
@@ -20,6 +37,8 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 /**
  * This class contains constructor's type reference type related test cases.
+ *
+ * @since 2201.3.0
  */
 public class ReferenceTypeTest {
 
