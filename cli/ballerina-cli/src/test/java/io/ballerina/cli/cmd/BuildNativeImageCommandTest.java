648186d156a8ffeaf3e7f7666856e9bb9bddb303
Ibaqu <aquib@wso2.com>
2022-10-01 00:14:57 +0530

    Fix license and comments

--- cli/ballerina-cli/src/test/java/io/ballerina/cli/cmd/BuildNativeImageCommandTest.java
+++ cli/ballerina-cli/src/test/java/io/ballerina/cli/cmd/BuildNativeImageCommandTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.ballerina.cli.cmd;
 
 import io.ballerina.projects.ProjectEnvironmentBuilder;
@@ -22,7 +40,7 @@ import static io.ballerina.projects.util.ProjectConstants.DIST_CACHE_DIRECTORY;
 /**
  * Build native command tests.
  *
- * @since 2.0.0
+ * @since 2201.3.0
  */
 public class BuildNativeImageCommandTest extends BaseCommandTest {
 
