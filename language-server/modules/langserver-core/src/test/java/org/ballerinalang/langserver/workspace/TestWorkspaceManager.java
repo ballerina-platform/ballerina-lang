1495757a73988258181e8a9ed1f94348abe1f5ed
gayalkuruppu <gayalkuruppu.gk@gmail.com>
2022-10-06 09:39:34 +0530

    Add tests for loadProject

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/workspace/TestWorkspaceManager.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/workspace/TestWorkspaceManager.java
@@ -20,6 +20,7 @@ package org.ballerinalang.langserver.workspace;
 import io.ballerina.projects.Document;
 import io.ballerina.projects.Module;
 import io.ballerina.projects.Project;
+import io.ballerina.projects.ProjectException;
 import io.ballerina.projects.ProjectKind;
 import io.ballerina.projects.util.ProjectConstants;
 import org.ballerinalang.langserver.common.utils.CommonUtil;
@@ -399,6 +400,24 @@ public class TestWorkspaceManager {
         } finally {
             Files.move(modulesPathNew, modulesPath);
         }
+    }
+
+    @Test
+    public void testWSLoadProject() throws WorkspaceDocumentException, ProjectException {
+        Path filePath1 = RESOURCE_DIRECTORY.resolve("myproject").resolve("main.bal").toAbsolutePath();
+        Path filePath2 = RESOURCE_DIRECTORY.resolve("myproject2").resolve("main.bal").toAbsolutePath();
+
+        // Load projects
+        Project project1 = workspaceManager.loadProject(filePath1);
+        Project project2 = workspaceManager.loadProject(filePath2);
+
+        // Both projects should be loaded
+        Assert.assertNotNull(project1);
+        Assert.assertNotNull(project2);
+
+        // Loaded project should not be empty
+        Optional<Project> project = workspaceManager.project(filePath1);
+        Assert.assertTrue(project.isPresent());
     }
 
     private void openFile(Path singleFile) throws WorkspaceDocumentException {
