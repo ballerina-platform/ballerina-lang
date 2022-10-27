b8422da87e21ffc4ed26c2c6832366f25968b740
Chiran Fernando <pcnfernando@gmail.com>
2022-10-27 13:58:56 +0530

    Handle non-matching capturing groups

--- langlib/lang.regexp/src/main/java/org/ballerinalang/langlib/regexp/RegexUtil.java
+++ langlib/lang.regexp/src/main/java/org/ballerinalang/langlib/regexp/RegexUtil.java
@@ -18,7 +18,9 @@
 package org.ballerinalang.langlib.regexp;
 
 import io.ballerina.runtime.api.PredefinedTypes;
+import io.ballerina.runtime.api.creators.ValueCreator;
 import io.ballerina.runtime.api.utils.StringUtils;
+import io.ballerina.runtime.api.values.BArray;
 import io.ballerina.runtime.api.values.BRegexpValue;
 import io.ballerina.runtime.api.values.BString;
 import io.ballerina.runtime.internal.regexp.RegExpFactory;
@@ -52,5 +54,29 @@ public class RegexUtil {
         String patternStr = StringUtils.getStringValue(translatedRegExpVal, null);
         Pattern pattern = Pattern.compile(patternStr);
         return pattern.matcher(inputStr);
+    }
+
+    static BArray getGroupZeroAsSpan(Matcher matcher) {
+        BArray resultTuple = ValueCreator.createTupleValue(SPAN_AS_TUPLE_TYPE);
+        resultTuple.add(0, matcher.start());
+        resultTuple.add(1, matcher.end());
+        resultTuple.add(2, StringUtils.fromString(matcher.group()));
+        return resultTuple;
+    }
+
+    static BArray getMatcherGroupsAsSpanArr(Matcher matcher) {
+        BArray group = ValueCreator.createArrayValue(GROUPS_AS_SPAN_ARRAY_TYPE);
+        for (int i = 1; i <= matcher.groupCount(); i++) {
+            int matcherStart = matcher.start(i);
+            if (matcherStart == -1) {
+                continue;
+            }
+            BArray resultTuple = ValueCreator.createTupleValue(SPAN_AS_TUPLE_TYPE);
+            resultTuple.add(0, matcherStart);
+            resultTuple.add(1, matcher.end(i));
+            resultTuple.add(2, StringUtils.fromString(matcher.group(i)));
+            group.append(resultTuple);
+        }
+        return group;
     }
 }
