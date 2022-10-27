ad1b656251425eea111657cbfe39a39ae1cd5b19
sanjana <anjetnet@gmail.com>
2022-10-27 12:32:48 +0530

    Add javadoc comment

--- compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/impl/SymbolFilter.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/compiler/api/impl/SymbolFilter.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2022, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package io.ballerina.compiler.api.impl;
 
 import io.ballerina.tools.diagnostics.Location;
@@ -24,6 +42,12 @@ import org.wso2.ballerinalang.compiler.tree.types.BLangRecordTypeNode;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+/**
+ * Finds the list of AST node symbols to be filtered in the visible symbol finding logic.
+ *
+ * @since 2.0.0
+ */
 
 public class SymbolFilter extends BaseVisitor {
 
