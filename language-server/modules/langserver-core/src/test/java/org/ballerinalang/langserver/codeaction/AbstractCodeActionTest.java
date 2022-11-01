0a1a24315b9c1476399d1eeac6ef0c3b97ab7b63
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2022-11-01 22:57:27 +0530

    Update code action cmd path testing logic

--- language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/AbstractCodeActionTest.java
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/codeaction/AbstractCodeActionTest.java
@@ -189,9 +189,9 @@ public abstract class AbstractCodeActionTest extends AbstractLSTest {
 
                         JsonArray actualArgs = actualCommand.getAsJsonArray("arguments");
                         JsonArray expArgs = expectedCommand.getAsJsonArray("arguments");
-                        if (expArgs == null 
+                        if (expArgs == null
                                 || !validateAndModifyArguments(
-                                        actualCommand, actualArgs, expArgs, sourceRoot, sourcePath)) {
+                                actualCommand, actualArgs, expArgs, sourceRoot, sourcePath)) {
                             misMatched = true;
                         }
                         actual.command = actualCommand;
@@ -401,7 +401,9 @@ public abstract class AbstractCodeActionTest extends AbstractLSTest {
             if (actualArgs.size() == 2) {
                 Optional<String> actualFilePath =
                         PathUtil.getPathFromURI(actualArgs.get(0).getAsString())
-                                .map(path -> path.toString().replace(sourceRoot.toString(), ""));
+                                .map(path -> path.toUri().toString()
+                                        .replace("\\", "/")
+                                        .replace(sourceRoot.toUri().toString(), ""));
                 int actualRenamePosition = actualArgs.get(1).getAsInt();
                 String expectedFilePath = expArgs.get(0).getAsString();
                 int expectedRenamePosition = expArgs.get(1).getAsInt();
