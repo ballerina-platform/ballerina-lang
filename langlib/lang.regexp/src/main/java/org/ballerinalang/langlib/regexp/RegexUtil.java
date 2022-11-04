b5c19cff6cd27050f6fb4cc442b360d1d533433f
Chiran Fernando <pcnfernando@gmail.com>
2022-11-04 22:35:58 +0530

    Add license header

--- langlib/lang.regexp/src/main/java/org/ballerinalang/langlib/regexp/RegexUtil.java
+++ langlib/lang.regexp/src/main/java/org/ballerinalang/langlib/regexp/RegexUtil.java
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.ballerinalang.langlib.regexp;
 
 import io.ballerina.runtime.api.PredefinedTypes;
