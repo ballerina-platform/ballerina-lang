e06f74b937531f35be97730c397e7785e7462294
Nadeeshan96 <nadeeshand@wso2.com>
2022-11-23 14:35:36 +0530

    Add imported global var to global var list

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/JvmInstructionGen.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/JvmInstructionGen.java
@@ -419,11 +419,21 @@ public class JvmInstructionGen {
         BType bType = JvmCodeGenUtil.getReferredType(varDcl.type);
 
         switch (varDcl.kind) {
+            case GLOBAL: {
+                BIRNode.BIRGlobalVariableDcl globalVar = (BIRNode.BIRGlobalVariableDcl) varDcl;
+                String moduleName = JvmCodeGenUtil.getPackageName(globalVar.pkgId);
+
+                String varName = varDcl.name.value;
+                String className = jvmPackageGen.lookupGlobalVarClassName(moduleName, varName);
+
+                String typeSig = getTypeDesc(bType);
+                mv.visitFieldInsn(GETSTATIC, className, varName, typeSig);
+                return;
+            }
             case SELF:
                 mv.visitVarInsn(ALOAD, 0);
                 return;
-            case CONSTANT:
-            case GLOBAL:
+            case CONSTANT: {
                 String varName = varDcl.name.value;
                 PackageID moduleId = ((BIRNode.BIRGlobalVariableDcl) varDcl).pkgId;
                 String pkgName = JvmCodeGenUtil.getPackageName(moduleId);
@@ -431,8 +441,7 @@ public class JvmInstructionGen {
                 String typeSig = getTypeDesc(bType);
                 mv.visitFieldInsn(GETSTATIC, className, varName, typeSig);
                 return;
-            default:
-                break;
+            }
         }
 
         generateVarLoadForType(mv, bType, valueIndex);
