c2c2875574eae203bd1347c97cc34c0cf4f0aeaf
Azeem Muzammil <azeemmuzammil@yahoo.com>
2022-11-25 15:12:26 +0530

    Add compiler plugin to generate component model

---
+++ misc/model-generator/modules/model-generator-compiler-plugin/src/main/java/io/ballerina/modelgenerator/plugin/CompilationAnalysisTask.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2022, WSO2 LLC. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package io.ballerina.modelgenerator.plugin;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.ballerina.modelgenerator.core.ComponentModel;
+import io.ballerina.modelgenerator.core.ComponentModelBuilder;
+import io.ballerina.projects.BuildOptions;
+import io.ballerina.projects.Project;
+import io.ballerina.projects.plugins.AnalysisTask;
+import io.ballerina.projects.plugins.CompilationAnalysisContext;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static io.ballerina.modelgenerator.plugin.PluginConstants.COMPONENT_MODEL;
+import static io.ballerina.modelgenerator.plugin.PluginConstants.PATH_SEPARATOR;
+
+/**
+ * Compilation analyzer to generate component model.
+ *
+ * @since 2201.4.0
+ */
+public class CompilationAnalysisTask implements AnalysisTask<CompilationAnalysisContext> {
+    @Override
+    public void perform(CompilationAnalysisContext compilationAnalysisContext) {
+        Project project = compilationAnalysisContext.currentPackage().project();
+        //Used build option exportComponentModel() to enable plugin at the build time.
+        BuildOptions buildOptions = project.buildOptions();
+        if (buildOptions.exportComponentModel()) {
+            Path outPath = project.targetDir();
+            ComponentModelBuilder componentModelBuilder = new ComponentModelBuilder();
+            ComponentModel projectModel = componentModelBuilder
+                    .constructComponentModel(compilationAnalysisContext.currentPackage(),
+                            compilationAnalysisContext.compilation());
+            Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+            String componentModelJson = gson.toJson(projectModel) + "\n";
+            writeComponentModelJson(outPath, componentModelJson);
+        }
+    }
+
+    private void writeComponentModelJson(Path outPath, String componentModelJson) {
+        try {
+            // Create ComponentModel directory if not exists in the path. If exists do not throw an error
+            Files.createDirectories(Paths.get(outPath + PATH_SEPARATOR + COMPONENT_MODEL));
+            Path writePath = outPath.resolve(COMPONENT_MODEL + PATH_SEPARATOR + "model.json");
+            writeFile(writePath, componentModelJson);
+        } catch (IOException e) {
+//            DiagnosticMessages error = DiagnosticMessages.OAS_CONVERTOR_108;
+//            ExceptionDiagnostic diagnostic = new ExceptionDiagnostic(error.getCode(),
+//                    error.getDescription(), null, e.toString());
+//            diagnostics.add(BuildExtensionUtil.getDiagnostics(diagnostic));
+        }
+    }
+
+    private void writeFile(Path filePath, String content) throws IOException {
+        try (FileWriter writer = new FileWriter(filePath.toString(), StandardCharsets.UTF_8)) {
+            writer.write(content);
+        }
+    }
+}
