d3189f4602049e79ee520a4b582c3c07e349f574
lasinicl <lasini.1021@gmail.com>
2022-12-01 01:56:04 +0530

    Add tests

---
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/query/QueryWithClosuresTest.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.ballerinalang.test.query;
+
+import org.ballerinalang.test.BCompileUtil;
+import org.ballerinalang.test.BRunUtil;
+import org.ballerinalang.test.CompileResult;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * This contains methods to test query expressions and query actions with closures.
+ *
+ * @since 2201.3.1
+ */
+public class QueryWithClosuresTest {
+    private CompileResult result;
+
+    @BeforeClass
+    public void setup() {
+        result = BCompileUtil.compile("test-src/query/query_with_closures.bal");
+    }
+
+    @Test(dataProvider = "dataToTestQueryWithClosures")
+    public void testQueryWithClosures(String functionName) {
+        BRunUtil.invoke(result, functionName);
+    }
+
+    @DataProvider
+    public Object[] dataToTestQueryWithClosures() {
+        return new Object[]{
+                "testClosuresInQueryInSelect",
+                "testClosuresInQueryInterpolatedInXmlInSelect",
+                "testClosureInQueryActionInDo",
+                "testClosuresInQueryInLet",
+                "testClosuresInNestedQueryInSelect",
+                "testClosuresInNestedQueryInSelect2",
+                "testClosureInQueryActionInDo2"
+        };
+    }
+}
