896ddb6bc49004b04c5d819c0864940fad3cc2c5
Chiran Fernando <pcnfernando@gmail.com>
2023-01-05 22:41:51 +0530

    Remove unused field
    (cherry picked from commit e4b310c2d14af43f27e8013ac7fc09fd32e597f7)

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/workspace/BallerinaWorkspaceManager.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/workspace/BallerinaWorkspaceManager.java
@@ -1163,8 +1163,6 @@ public class BallerinaWorkspaceManager implements WorkspaceManager {
 
         private boolean crashed;
 
-        public PackageCompilation crashedCompilation;
-
         private ProjectPair(Project project, Lock lock) {
             this.project = project;
             this.lock = lock;
