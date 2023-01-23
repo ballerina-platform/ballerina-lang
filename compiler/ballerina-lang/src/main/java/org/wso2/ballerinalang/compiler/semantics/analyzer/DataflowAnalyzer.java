bf4915bf6e775f66670afe01dbab656c5779c2f7
chiranSachintha <chiransachintha1994@gmail.com>
2023-01-23 22:18:07 +0530

    fix `getting unused variable warning` with tuple annotation

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/DataflowAnalyzer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/DataflowAnalyzer.java
@@ -2038,7 +2038,12 @@ public class DataflowAnalyzer extends BLangNodeVisitor {
 
     @Override
     public void visit(BLangTupleTypeNode tupleTypeNode) {
-        tupleTypeNode.members.forEach(member -> analyzeNode(member, env));
+        for (BLangSimpleVariable member : tupleTypeNode.members) {
+            analyzeNode(member, env);
+            for (BLangAnnotationAttachment annotationAttachment : member.annAttachments) {
+                analyzeNode(annotationAttachment.expr, env);
+            }
+        }
     }
 
     @Override
