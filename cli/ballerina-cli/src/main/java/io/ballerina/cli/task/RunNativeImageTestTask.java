bc3bbc369d31403b92d6b5effe6f40e3ba03a0db
Thevakumar-Luheerathan <Luhee27@gmail.com>
2023-02-02 16:19:10 +0530

    Address review

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/task/RunNativeImageTestTask.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/task/RunNativeImageTestTask.java
@@ -283,7 +283,7 @@ public class RunNativeImageTestTask implements Task {
         coverage = project.buildOptions().codeCoverage();
 
         if (coverage) {
-            this.out.println("WARNING: Code coverage generation is not supported by Ballerina native test");
+            this.out.println("WARNING: Code coverage generation is not supported with Ballerina native test");
         }
 
         if (report) {
