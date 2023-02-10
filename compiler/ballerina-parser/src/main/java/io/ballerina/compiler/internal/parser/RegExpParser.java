a2715ef3eeb7a4a9e7dc79a47007710b71bf55b0
mohan <mohan.vive@gmail.com>
2023-02-10 16:08:20 +0530

    Revert invalid change

--- compiler/ballerina-parser/src/main/java/io/ballerina/compiler/internal/parser/RegExpParser.java
+++ compiler/ballerina-parser/src/main/java/io/ballerina/compiler/internal/parser/RegExpParser.java
@@ -311,7 +311,6 @@ public class RegExpParser extends AbstractParser {
             case "S":
             case "w":
             case "W":
-            case "-":
                 return true;
             default:
                 return false;
