47f66a69804cf6c45cc56f01cb572dcaf221138b
suleka96 <sulekahelmini96comp@gmail.com>
2023-02-24 00:46:25 +0530

    Refactor code

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolEnter.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolEnter.java
@@ -3341,7 +3341,8 @@ public class SymbolEnter extends BLangNodeVisitor {
             if (errorCause != null) {
                 if (errorCause.getKind() == NodeKind.VARIABLE &&
                         names.fromIdNode(((BLangSimpleVariable) errorCause).name) == Names.IGNORE) {
-                    dlog.error(errorCause.pos, DiagnosticErrorCode.CANNOT_USE_WILD_CARD_BINDING_PATTERN_FOR_ERROR_CAUSE);
+                    dlog.error(errorCause.pos,
+                            DiagnosticErrorCode.CANNOT_USE_WILD_CARD_BINDING_PATTERN_FOR_ERROR_CAUSE);
                     return false;
                 }
                 defineMemberNode(errorVariable.cause, env, symTable.errorOrNilType);
