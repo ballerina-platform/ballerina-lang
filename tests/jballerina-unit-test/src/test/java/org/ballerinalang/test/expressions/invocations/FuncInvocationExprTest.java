f0b2ff8c454924e61e281c5dbed3d0220681a999
suleka96 <sulekahelmini96comp@gmail.com>
2023-03-10 04:31:50 +0530

    Refactor code

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/invocations/FuncInvocationExprTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/invocations/FuncInvocationExprTest.java
@@ -17,7 +17,6 @@
  */
 package org.ballerinalang.test.expressions.invocations;
 
-import io.ballerina.runtime.internal.util.exceptions.BLangRuntimeException;
 import org.ballerinalang.test.BCompileUtil;
 import org.ballerinalang.test.BRunUtil;
 import org.ballerinalang.test.CompileResult;
