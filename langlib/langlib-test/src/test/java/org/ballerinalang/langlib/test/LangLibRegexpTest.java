4328032ea977861f0468c745c6616f4c6a477a3a
sanjana <anjetnet@gmail.com>
2023-03-13 08:07:43 +0530

    Update langlib regexp test cases

--- langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibRegexpTest.java
+++ langlib/langlib-test/src/test/java/org/ballerinalang/langlib/test/LangLibRegexpTest.java
@@ -18,6 +18,7 @@
 
 package org.ballerinalang.langlib.test;
 
+import org.ballerinalang.test.BAssertUtil;
 import org.ballerinalang.test.BCompileUtil;
 import org.ballerinalang.test.BRunUtil;
 import org.ballerinalang.test.CompileResult;
@@ -26,6 +27,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.internal.collections.Pair;
+
+import java.util.List;
 
 /**
  * Test cases for the lang.regexp library.
@@ -151,5 +155,33 @@ public class LangLibRegexpTest {
                 {"testInvalidRegexpPatternSyntax5", "Unclosed character class near index 23" + NEW_LINE_CHAR +
                         "(?xsmi:[]\\P{sc=Braille})" + NEW_LINE_CHAR + "                       ^"},
         };
+    }
+
+    @Test
+    public void testEmptyRegexpCompilationError() {
+        CompileResult errResult = BCompileUtil.compile("test-src/regexp_empty_test_negative.bal");
+        Assert.assertEquals(errResult.getErrorCount(), 13);
+        for (int i = 0; i < getErrorIndexes().size(); i++) {
+            BAssertUtil.validateError(errResult, i, "regular expression with empty string is not allowed",
+                    getErrorIndexes().get(i).first(), getErrorIndexes().get(i).second());
+        }
+    }
+
+    private List<Pair<Integer, Integer>> getErrorIndexes() {
+        return List.of(
+                Pair.of(20, 12),
+                Pair.of(21, 29),
+                Pair.of(22, 29),
+                Pair.of(23, 27),
+                Pair.of(24, 27),
+                Pair.of(25, 30),
+                Pair.of(26, 30),
+                Pair.of(27, 33),
+                Pair.of(28, 33),
+                Pair.of(29, 40),
+                Pair.of(30, 40),
+                Pair.of(31, 32),
+                Pair.of(32, 32)
+        );
     }
 }
