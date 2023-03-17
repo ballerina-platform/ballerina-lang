83b5bfc67c04c13aaa7beefd4bb4beb4a50ff901
Chiran Fernando <pcnfernando@gmail.com>
2023-03-17 19:37:28 +0530

    Fix test failure

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/onfail/OnFailClauseTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/statements/onfail/OnFailClauseTest.java
@@ -131,7 +131,7 @@ public class OnFailClauseTest {
         BAssertUtil.validateError(negativeResult, i++, "variable 'j' may not have been initialized", 1056, 9);
         BAssertUtil.validateError(negativeResult, i++, "variable 'i' may not have been initialized", 1071, 9);
         BAssertUtil.validateError(negativeResult, i++, "variable 'j' may not have been initialized", 1072, 9);
-        BAssertUtil.validateError(negativeResult, i++, "variable 'j' may not have been initialized", 1102, 9);
+        BAssertUtil.validateError(negativeResult, i++, "variable 'i' may not have been initialized", 1102, 9);
         Assert.assertEquals(negativeResult.getErrorCount(), i);
     }
 
