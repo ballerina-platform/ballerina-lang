3672dfbc982cd7c0f2ed9190cd6b0cfa9989bd13
gabilang <ganeshwargabil@gmail.com>
2023-03-28 18:19:33 +0530

    Remove redundent null check

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/JvmPackageGen.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/JvmPackageGen.java
@@ -829,7 +829,7 @@ public class JvmPackageGen {
         BIRFunction userMainFunc = null;
         if (module.packageID.skipTests) {
             for (BIRFunction func : module.functions) {
-                if (func != null && func.name.value.equals(MAIN_METHOD)) {
+                if (func.name.value.equals(MAIN_METHOD)) {
                     userMainFunc = func;
                     break;
                 }
@@ -842,7 +842,7 @@ public class JvmPackageGen {
         BIRFunction testExecuteFunction = null;
         if (!module.packageID.skipTests) {
             for (BIRFunction func : module.functions) {
-                if (func != null && func.name.value.equals(TEST_EXECUTE_METHOD)) {
+                if (func.name.value.equals(TEST_EXECUTE_METHOD)) {
                     testExecuteFunction = func;
                     break;
                 }
