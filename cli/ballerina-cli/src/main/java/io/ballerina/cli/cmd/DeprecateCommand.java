658668a3ee263c2eee23f9248cc975d9c9f7f064
Fathima Dilhasha <dilhasha.uom@gmail.com>
2023-04-11 15:31:46 +0530

    Add package deprecation support

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/cmd/DeprecateCommand.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/cmd/DeprecateCommand.java
@@ -103,14 +103,6 @@ public class DeprecateCommand implements BLauncherCmd {
             return;
         }
 
-        String packageValue = argList.get(0);
-        if (packageValue.split(":").length != 2) {
-            CommandUtil.printError(errStream, "invalid package. Provide the package with the version.",
-                    USAGE_TEXT, false);
-            CommandUtil.exitError(this.exitWhenFinish);
-            return;
-        }
-
         // validate deprecation message
         if (!validateDeprecationMsg(deprecationMsg)) {
             CommandUtil.printError(errStream, "invalid deprecation message. The message cannot contain " +
@@ -124,7 +116,7 @@ public class DeprecateCommand implements BLauncherCmd {
         if (deprecationMsg != null && undoFlag) {
             this.outStream.println("warning: ignoring --message flag since this is an undo request");
         }
-        deprecateInCentral(packageValue);
+        deprecateInCentral(argList.get(0));
 
         if (this.exitWhenFinish) {
             Runtime.getRuntime().exit(0);
@@ -172,10 +164,14 @@ public class DeprecateCommand implements BLauncherCmd {
                 // Ignore 'Settings.toml' parsing errors and return empty Settings object
                 settings = Settings.from();
             }
+            String packageValue = packageInfo;
+            if (packageInfo.split(":").length != 2) {
+                packageValue = packageInfo + ":*";
+            }
             CentralAPIClient client = new CentralAPIClient(RepoUtils.getRemoteRepoURL(),
                     initializeProxy(settings.getProxy()),
                     getAccessTokenOfCLI(settings));
-            client.deprecatePackage(packageInfo, deprecationMsg,
+            client.deprecatePackage(packageValue, deprecationMsg,
                     JvmTarget.JAVA_11.code(),
                     RepoUtils.getBallerinaVersion(), this.undoFlag);
         } catch (CentralClientException e) {
