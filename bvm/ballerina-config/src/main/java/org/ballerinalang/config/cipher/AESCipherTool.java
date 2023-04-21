cdd749e36193a43d6b8ad8d2d5dedb78d46d2d70
bhashinee <bhashineen@gmail.com>
2023-04-21 16:26:11 +0530

    Change the IV size to 12

--- bvm/ballerina-config/src/main/java/org/ballerinalang/config/cipher/AESCipherTool.java
+++ bvm/ballerina-config/src/main/java/org/ballerinalang/config/cipher/AESCipherTool.java
@@ -54,7 +54,7 @@ public class AESCipherTool {
     private static final String ALGORITHM_AES = "AES";
     private static final String ALGORITHM_SHA_256 = "SHA-256";
     //private static final int IV_SIZE = 96;
-    private static final int IV_SIZE = 16;
+    private static final int IV_SIZE = 12;
     private static final int SECRET_KEY_LENGTH = 16; // TODO: Make this 32 again after switching to Java 9
 
     private final SecretKey secretKey;
