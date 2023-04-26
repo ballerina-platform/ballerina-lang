612a39d7523b348ff0b52c7a426089f83c0d1a44
gabilang <ganeshwargabil@gmail.com>
2023-04-26 20:37:11 +0530

    Fix NPE with start span due to observability not included

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/observability/tracer/TracersStore.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/observability/tracer/TracersStore.java
@@ -17,6 +17,8 @@
  */
 package io.ballerina.runtime.observability.tracer;
 
+import io.ballerina.runtime.api.creators.ErrorCreator;
+import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.observability.tracer.spi.TracerProvider;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.propagation.ContextPropagators;
@@ -58,6 +60,10 @@ public class TracersStore {
      * @return trace implementations i.e: zipkin, jaeger
      */
     public Tracer getTracer(String serviceName) {
+        if (!isInitialized()) {
+            throw ErrorCreator.createError(StringUtils.fromString("error: tracer store is not initialized " +
+                    "due to observability has not been enabled"));
+        }
         Tracer tracer;
         if (store.containsKey(serviceName)) {
             tracer = store.get(serviceName);
