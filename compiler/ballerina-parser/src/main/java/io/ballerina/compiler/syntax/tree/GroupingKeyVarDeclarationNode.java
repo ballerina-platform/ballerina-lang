dd5f6cc473a8643405f4812a35f8bed5a3321bb9
kavindu <kavindu175@gmail.com>
2023-05-17 19:01:00 +0530

    Change the versions

--- compiler/ballerina-parser/src/main/java/io/ballerina/compiler/syntax/tree/GroupingKeyVarDeclarationNode.java
+++ compiler/ballerina-parser/src/main/java/io/ballerina/compiler/syntax/tree/GroupingKeyVarDeclarationNode.java
@@ -1,19 +1,19 @@
 /*
- *  Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package io.ballerina.compiler.syntax.tree;
 
@@ -24,7 +24,7 @@ import java.util.Objects;
 /**
  * This is a generated syntax tree node.
  *
- * @since 2201.4.0
+ * @since 2201.6.0
  */
 public class GroupingKeyVarDeclarationNode extends NonTerminalNode {
 
@@ -94,7 +94,7 @@ public class GroupingKeyVarDeclarationNode extends NonTerminalNode {
     /**
      * This is a generated tree node modifier utility.
      *
-     * @since 2.0.0
+     * @since 2201.6.0
      */
     public static class GroupingKeyVarDeclarationNodeModifier {
         private final GroupingKeyVarDeclarationNode oldNode;
