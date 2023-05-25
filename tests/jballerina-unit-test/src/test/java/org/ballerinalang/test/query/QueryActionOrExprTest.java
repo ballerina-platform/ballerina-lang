3dac75b8c65275ce781cd5ff2008707d2ea224a0
sanjana <anjetnet@gmail.com>
2023-05-25 10:51:05 +0530

    Add test cases for any or err resutl type in query actions

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/query/QueryActionOrExprTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/query/QueryActionOrExprTest.java
@@ -74,6 +74,7 @@ public class QueryActionOrExprTest {
                 "testQueryActionOrExprWithGroupedClientResourceAccessAction",
                 "testNestedQueryActionOrExprWithClientResourceAccessAction",
                 "testQueryActionOrExpressionWithUnionRecordResultType",
+                "testQueryActionOrExprWithAnyOrErrResultType",
         };
     }
 
