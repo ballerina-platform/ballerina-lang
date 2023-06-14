a7a2e589a49aca5cdb48b04f4f047253ab81ecd7
ushirask <ushirask@gmail.com>
2023-06-14 14:37:30 +0530

    Check for union type

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
@@ -7368,7 +7368,7 @@ public class TypeChecker extends SimpleBLangNodeAnalyzer<TypeChecker.AnalyzerDat
             types.checkType(arg.pos, inferredType, expectedType, DiagnosticErrorCode.INCOMPATIBLE_TYPES);
             return;
         }
-        if (arg.getKind() == NodeKind.REST_ARGS_EXPR) {
+        if (arg.getKind() == NodeKind.REST_ARGS_EXPR && expectedType.tag == TypeTags.UNION) {
             // Need to infer the type with noType for rest args.
             checkExpr(arg, symTable.noType, data);
         } else {
