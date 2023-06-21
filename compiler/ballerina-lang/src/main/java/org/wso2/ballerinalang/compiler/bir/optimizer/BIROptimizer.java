5752b2fbb798321535745d95bebe99c52fc0e7a2
Nadeeshan96 <nadeeshand@wso2.com>
2023-06-21 20:27:25 +0530

    Add test for array init with global vars

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/optimizer/BIROptimizer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/optimizer/BIROptimizer.java
@@ -471,7 +471,7 @@ public class BIROptimizer {
 
         @Override
         public void visit(BIRNonTerminator.Move birMove) {
-            if (birMove.lhsOp.variableDcl.kind != VarKind.TEMP || birMove.rhsOp.variableDcl.kind == VarKind.GLOBAL) {
+            if (birMove.lhsOp.variableDcl.kind != VarKind.TEMP) {
                 this.env.newInstructions.add(birMove);
                 return;
             }
