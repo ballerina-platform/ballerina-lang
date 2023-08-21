819859971e41181b5f3e70d7bd11480e621dce3d
mindula <rowelmindula@gmail.com>
2023-08-21 09:30:22 +0530

    Add memory usage monitor test class

---
+++ language-server/modules/langserver-core/src/test/java/org/ballerinalang/langserver/memoryusagemonitor/MemoryUsageMonitorTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ballerinalang.langserver.memoryusagemonitor;
+
+import org.ballerinalang.langserver.AbstractLSTest;
+import org.ballerinalang.langserver.BallerinaLanguageServer;
+import org.ballerinalang.langserver.MemoryUsageMonitor;
+import org.ballerinalang.langserver.commons.client.ExtendedLanguageClient;
+import org.ballerinalang.langserver.commons.workspace.WorkspaceDocumentException;
+import org.ballerinalang.langserver.util.TestUtil;
+import org.eclipse.lsp4j.MessageParams;
+import org.eclipse.lsp4j.MessageType;
+import org.mockito.Mockito;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+
+/**
+ * Tests {@link MemoryUsageMonitor}.
+ *
+ * @since 2201.9.0
+ */
+public class MemoryUsageMonitorTest extends AbstractLSTest {
+
+    @Test
+    public void test() throws WorkspaceDocumentException, IOException, InterruptedException {
+
+        MemoryUsageMonitor memoryMonitor = new MemoryUsageMonitor(createMockMemoryMXBean());
+        BallerinaLanguageServer languageServer = new BallerinaLanguageServer();
+        languageServer.getServerContext().put(MemoryUsageMonitor.MEMORY_USAGE_MONITOR_KEY, memoryMonitor);
+
+        ExtendedLanguageClient mockClient = Mockito.mock(ExtendedLanguageClient.class);
+        TestUtil.newLanguageServer().withLanguageServer(languageServer).withClient(mockClient).build();
+        Thread.sleep(2000);
+
+        Mockito.verify(mockClient).showMessage(new MessageParams(MessageType.Error,
+                "Memory usage is high. Please reload the window.")
+        );
+    }
+
+    private MemoryMXBean createMockMemoryMXBean() {
+        MemoryMXBean mockMemoryMXBean = Mockito.mock(MemoryMXBean.class);
+        MemoryUsage mockHeapMemoryUsage = new MemoryUsage(1_000_000_000, 900_000_000, 900_000_000, 1_000_000_000);
+        Mockito.when(mockMemoryMXBean.getHeapMemoryUsage()).thenReturn(mockHeapMemoryUsage);
+        return mockMemoryMXBean;
+    }
+}
