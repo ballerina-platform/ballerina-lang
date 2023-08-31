2d79739c94bcdc4757ac1d1b98b7842195201081
warunalakshitha <warudanu@gmail.com>
2023-08-31 07:08:04 +0530

    Disable testPushWithEmptyPackageMd test

--- cli/ballerina-cli/src/test/java/io/ballerina/cli/cmd/PushCommandTest.java
+++ cli/ballerina-cli/src/test/java/io/ballerina/cli/cmd/PushCommandTest.java
@@ -326,7 +326,7 @@ public class PushCommandTest extends BaseCommandTest {
         Assert.assertTrue(actual.contains(expected));
     }
 
-    @Test ()
+    @Test (enabled = false)
     public void testPushWithEmptyPackageMd() throws IOException {
         Path projectPath = this.testResources.resolve(VALID_PROJECT);
         System.setProperty("user.dir", projectPath.toString());
