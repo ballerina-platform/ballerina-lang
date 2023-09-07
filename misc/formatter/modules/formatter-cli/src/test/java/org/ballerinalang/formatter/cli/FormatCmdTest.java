0a73f9e1d4b08cb791ebc0d0f1ed7a8a498c24a4
poorna2152 <poorna2152@gmail.com>
2023-09-07 14:27:27 +0530

    Add tests

--- misc/formatter/modules/formatter-cli/src/test/java/org/ballerinalang/formatter/cli/FormatCmdTest.java
+++ misc/formatter/modules/formatter-cli/src/test/java/org/ballerinalang/formatter/cli/FormatCmdTest.java
@@ -29,6 +29,8 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 /**
  * Format CLI tool test suit for testing tool's exceptions.
@@ -51,6 +53,9 @@ public class FormatCmdTest {
             RES_DIR.resolve("BallerinaProject/source/ProjectTemp");
     private static final Path BALLERINA_PROJECT_ASSERT =
             RES_DIR.resolve("BallerinaProject/assert/Project");
+    private static final Path[] BALLERINA_CONFIGURATION_DIRS =
+            {RES_DIR.resolve("configurations/general/source"), RES_DIR.resolve("configurations/functions/source"),
+                    RES_DIR.resolve("configurations/blocks/source"), RES_DIR.resolve("configurations/imports/source")};
 
     @Test(description = "Test single file project formatting")
     public void formatCLIOnASingleFileProject() {
@@ -79,6 +84,30 @@ public class FormatCmdTest {
             FileUtils.copyDirectory(BALLERINA_PROJECT_TEMP_SOURCE.toFile(), BALLERINA_PROJECT_SOURCE.toFile());
         } catch (IOException e) {
             Assert.fail("Failed to update the source file.");
+        }
+    }
+
+    @Test(description = "Test ballerina project formatting")
+    public void formatCLIOnBallerinaProjectWithConfigurations() {
+        List<String> argList = new ArrayList<>();
+        for (Path dir : BALLERINA_CONFIGURATION_DIRS) {
+            try {
+                List<Path> subDirs = Files.list(dir)
+                        .filter(Files::isDirectory)
+                        .filter(d -> !d.getFileName().toString().endsWith("Temp"))
+                        .collect(Collectors.toList());
+                for (Path subDir : subDirs) {
+                    Path tempDir = subDir.resolveSibling(subDir.getFileName() + "Temp");
+                    Path assertDir = Paths.get(subDir.toString().replace("/source/", "/assert/"));
+
+                    FormatUtil.execute(argList, false, null, null, false, subDir);
+                    Assert.assertEquals(Files.readString(subDir.resolve("main.bal")),
+                            Files.readString(assertDir.resolve("main.bal")));
+                    FileUtils.copyDirectory(tempDir.toFile(), subDir.toFile());
+                }
+            } catch (IOException e) {
+                Assert.fail("Failed to update the source file.");
+            }
         }
     }
 
