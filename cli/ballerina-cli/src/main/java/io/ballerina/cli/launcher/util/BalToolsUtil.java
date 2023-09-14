8b46aba0b416690a3e70cc3e5d162e481832446e
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2023-09-14 17:36:18 +0530

    Fix messages and use home dir env variable

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/launcher/util/BalToolsUtil.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/launcher/util/BalToolsUtil.java
@@ -89,7 +89,6 @@ import static io.ballerina.projects.util.ProjectConstants.BALA_DIR_NAME;
 import static io.ballerina.projects.util.ProjectConstants.BAL_TOOLS_TOML;
 import static io.ballerina.projects.util.ProjectConstants.CENTRAL_REPOSITORY_CACHE_NAME;
 import static io.ballerina.projects.util.ProjectConstants.CONFIG_DIR;
-import static io.ballerina.projects.util.ProjectConstants.HOME_REPO_DEFAULT_DIRNAME;
 import static io.ballerina.projects.util.ProjectConstants.REPOSITORIES_DIR;
 
 /**
@@ -117,9 +116,9 @@ public class BalToolsUtil {
     // if a command is a built-in tool command, add it to this list
     private static final List<String> builtInToolCommands = Arrays.asList();
 
-    private static final Path balToolsTomlPath = Path.of(
-            System.getProperty(CommandUtil.USER_HOME), HOME_REPO_DEFAULT_DIRNAME, CONFIG_DIR, BAL_TOOLS_TOML);
-    private static Path balaCacheDirPath = ProjectUtils.createAndGetHomeReposPath()
+    private static final Path balToolsTomlPath = RepoUtils.createAndGetHomeReposPath().resolve(
+            Path.of(CONFIG_DIR, BAL_TOOLS_TOML));
+    private static final Path balaCacheDirPath = ProjectUtils.createAndGetHomeReposPath()
             .resolve(REPOSITORIES_DIR).resolve(CENTRAL_REPOSITORY_CACHE_NAME)
             .resolve(ProjectConstants.BALA_DIR_NAME);
 
@@ -165,7 +164,7 @@ public class BalToolsUtil {
     }
 
     private static List<File> getToolCommandJarAndDependencyJars(String commandName) {
-        Path userHomeDirPath = Path.of(System.getProperty(CommandUtil.USER_HOME), HOME_REPO_DEFAULT_DIRNAME);
+        Path userHomeDirPath = RepoUtils.createAndGetHomeReposPath();
         Path balToolsTomlPath = userHomeDirPath.resolve(Path.of(CONFIG_DIR, BAL_TOOLS_TOML));
         Path centralBalaDirPath = userHomeDirPath.resolve(
                 Path.of(REPOSITORIES_DIR, CENTRAL_REPOSITORY_CACHE_NAME, BALA_DIR_NAME));
@@ -178,8 +177,8 @@ public class BalToolsUtil {
                     .flatMap(map -> map.values().stream())
                     .filter(BalToolsManifest.Tool::active)
                     .map(tool1 -> findJarFiles(CommandUtil.getPlatformSpecificBalaPath(
-                            tool1.org(), tool1.name(), tool1.version(), centralBalaDirPath).resolve(TOOL).resolve(LIBS)
-                            .toFile()))
+                        tool1.org(), tool1.name(), tool1.version(), centralBalaDirPath).resolve(TOOL).resolve(LIBS)
+                        .toFile()))
                     .flatMap(List::stream)
                     .collect(Collectors.toList());
         }
