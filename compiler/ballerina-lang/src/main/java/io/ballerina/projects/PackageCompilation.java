102f599e06cb513f0942b306f62de6423a87980d
gimantha <gimantha@wso2.com>
2023-09-22 12:10:02 +0530

    Fix test failures in project API

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/PackageCompilation.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/PackageCompilation.java
@@ -210,8 +210,6 @@ public class PackageCompilation {
                     diagnostics.add(new PackageDiagnostic(diagnostic, moduleContext.descriptor(),
                             moduleContext.project()));
                 }
-                //TODO: remove this once ballerina-lang#41407 is fixed
-                ModuleContext.shrinkDocuments(moduleContext);
             }
         }
         // add plugin diagnostics
