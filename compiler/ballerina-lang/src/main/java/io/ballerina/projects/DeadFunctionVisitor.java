7ef703b0f07204aec00825f2e5129ddbda08592b
Thushara-Piyasekara <thusharap@wso2.com>
2023-09-27 15:33:21 +0530

    Fix certain child functions not being added to parent symbol

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/DeadFunctionVisitor.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/DeadFunctionVisitor.java
@@ -25,23 +25,25 @@ import java.util.HashSet;
 
 /**
  * This visitor replaces unused functions with null
- *
  */
 public class DeadFunctionVisitor extends ClassVisitor {
+
     private final HashSet<String> deadFunctionNames;
 
-    public DeadFunctionVisitor(int api, ClassWriter cw,HashSet<String> deadFunctionNames) {
+    public DeadFunctionVisitor(int api, ClassWriter cw, HashSet<String> deadFunctionNames) {
+
         super(api, cw);
         this.deadFunctionNames = deadFunctionNames;
     }
 
     @Override
     public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+
         MethodVisitor methodVisitor;
-        if (!name.contains("init") && !deadFunctionNames.contains(name)) {
-            methodVisitor = super.visitMethod(access, name, desc, signature, exceptions);
-        } else {
+        if (!name.contains("init") && deadFunctionNames.contains(name)) {
             methodVisitor = null;
+        } else {
+            methodVisitor = super.visitMethod(access, name, desc, signature, exceptions);
         }
         return methodVisitor;
     }
