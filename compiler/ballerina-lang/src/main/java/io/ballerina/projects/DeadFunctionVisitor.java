85a16bf1ae3f4c25ca9a18f0b59e1eabf33fb652
Thushara-Piyasekara <thusharap@wso2.com>
2023-10-05 11:30:01 +0530

    Refactor licence and variable declarations

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/DeadFunctionVisitor.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/DeadFunctionVisitor.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
  *
  *  WSO2 LLC. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -11,7 +11,7 @@
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
+ *  KIND, either express or implied. See the License for the
  *  specific language governing permissions and limitations
  *  under the License.
  */
@@ -27,9 +27,7 @@ import java.util.HashSet;
  * This visitor replaces unused functions with null
  */
 public class DeadFunctionVisitor extends ClassVisitor {
-
     private static final String INIT_FUNCTION_IDENTIFIER = "init";
-
     private final HashSet<String> deadFunctionNames;
 
     public DeadFunctionVisitor(int api, ClassWriter cw, HashSet<String> deadFunctionNames) {
