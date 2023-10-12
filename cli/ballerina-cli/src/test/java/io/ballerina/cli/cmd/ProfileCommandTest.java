f27874e253ffe5b9e66fa2332bb618288f504aaa
gabilang <ganeshwargabil@gmail.com>
2023-10-12 14:39:26 +0530

    Add profiler resources for cli-tests

--- cli/ballerina-cli/src/test/java/io/ballerina/cli/cmd/ProfileCommandTest.java
+++ cli/ballerina-cli/src/test/java/io/ballerina/cli/cmd/ProfileCommandTest.java
@@ -81,7 +81,7 @@ public class ProfileCommandTest extends BaseCommandTest {
         profileCommand.execute();
         String buildLog = readOutput(true).replaceAll("\r", "").strip();
         Assert.assertEquals(buildLog, getOutput("run-project-with-profile.txt"));
-        Path htmlPath = projectPath.resolve("target").resolve("bin").resolve("ProfilerOutput.html");
+        Path htmlPath = projectPath.resolve("target").resolve("profiler").resolve("ProfilerOutput.html");
         Assert.assertTrue(htmlPath.toFile().exists());
         try {
             String htmlContent = Files.readString(htmlPath);
