bf3c55eadbea5acb161ea9dd5fc402ae0e21b3c4
Nipuna Fernando <nipunafe@gmail.com>
2023-10-16 15:14:57 +0530

    Remove duplicate keys in TableValueImpl

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/values/TupleValueImpl.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/values/TupleValueImpl.java
@@ -43,7 +43,6 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.StringJoiner;
 import java.util.stream.IntStream;
 
@@ -91,13 +90,6 @@ public class TupleValueImpl extends AbstractArrayValue {
                 hasRestElement == that.hasRestElement &&
                 type.equals(that.type) &&
                 Arrays.equals(refValues, that.refValues);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(type, tupleType);
-        result = 31 * result + Arrays.hashCode(refValues);
-        return result;
     }
 
     public TupleValueImpl(Object[] values, TupleType type) {
