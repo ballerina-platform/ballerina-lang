5b19a71b3585b5700a0caa96b821fa8bfb317429
warunalakshitha <warudanu@gmail.com>
2023-10-20 15:34:44 +0530

    Revert profiler arg change

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/task/RunProfilerTask.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/task/RunProfilerTask.java
@@ -77,10 +77,10 @@ public class RunProfilerTask implements Task {
             commands.add(getPackageJarName(project, projectKind));
             commands.add("--target");
             commands.add(targetPath.toString());
-            commands.add("--source-root");
+            commands.add("--sourceroot");
             commands.add(getProjectPath(project).toString());
             if (isInProfileDebugMode()) {
-                commands.add("--profiler-debug");
+                commands.add("--profilerDebug");
                 commands.add(getProfileDebugArg(err));
             }
             ProcessBuilder pb = new ProcessBuilder(commands).inheritIO();
