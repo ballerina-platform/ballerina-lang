0233cfc7479831c96c2daa925ebdda06e90c9575
Thushara-Piyasekara <thusharap@wso2.com>
2023-10-31 13:35:30 +0530

    Add documentation for TypeDefAnalyzer

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/TypeDefAnalyzer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/TypeDefAnalyzer.java
@@ -1,6 +1,22 @@
+/*
+ *  Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.wso2.ballerinalang.compiler.bir;
 
-import org.ballerinalang.model.symbols.SymbolOrigin;
 import org.wso2.ballerinalang.compiler.bir.model.BIRNode;
 import org.wso2.ballerinalang.compiler.semantics.model.TypeVisitor;
 import org.wso2.ballerinalang.compiler.semantics.model.types.BAnnotationType;
@@ -35,6 +51,10 @@ import org.wso2.ballerinalang.compiler.semantics.model.types.BXMLType;
 
 import java.util.HashMap;
 
+/**
+ * Analyzes the BIRTypeDefinition nodes and generate the dependency graph for type definitions
+ *
+ */
 public class TypeDefAnalyzer implements TypeVisitor {
 
     private static TypeDefAnalyzer typeDefAnalyzerInstance;
