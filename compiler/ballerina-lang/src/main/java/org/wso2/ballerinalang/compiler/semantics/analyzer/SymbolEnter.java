db0d5292e8cd4f261be090b48de1021acce86642
Thushara-Piyasekara <thusharap@wso2.com>
2023-12-20 10:51:44 +0530

    Add compilation support for langlibs

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolEnter.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/SymbolEnter.java
@@ -183,6 +183,7 @@ import java.util.stream.Stream;
 
 import javax.xml.XMLConstants;
 
+import static org.ballerinalang.model.elements.PackageID.ANNOTATIONS;
 import static org.ballerinalang.model.elements.PackageID.ARRAY;
 import static org.ballerinalang.model.elements.PackageID.BOOLEAN;
 import static org.ballerinalang.model.elements.PackageID.DECIMAL;
@@ -191,14 +192,18 @@ import static org.ballerinalang.model.elements.PackageID.FLOAT;
 import static org.ballerinalang.model.elements.PackageID.FUNCTION;
 import static org.ballerinalang.model.elements.PackageID.FUTURE;
 import static org.ballerinalang.model.elements.PackageID.INT;
+import static org.ballerinalang.model.elements.PackageID.INTERNAL;
+import static org.ballerinalang.model.elements.PackageID.JAVA;
 import static org.ballerinalang.model.elements.PackageID.MAP;
 import static org.ballerinalang.model.elements.PackageID.OBJECT;
 import static org.ballerinalang.model.elements.PackageID.QUERY;
 import static org.ballerinalang.model.elements.PackageID.REGEXP;
+import static org.ballerinalang.model.elements.PackageID.RUNTIME;
 import static org.ballerinalang.model.elements.PackageID.STREAM;
 import static org.ballerinalang.model.elements.PackageID.STRING;
 import static org.ballerinalang.model.elements.PackageID.TABLE;
 import static org.ballerinalang.model.elements.PackageID.TRANSACTION;
+import static org.ballerinalang.model.elements.PackageID.TRANSACTION_INTERNAL;
 import static org.ballerinalang.model.elements.PackageID.TYPEDESC;
 import static org.ballerinalang.model.elements.PackageID.VALUE;
 import static org.ballerinalang.model.elements.PackageID.XML;
@@ -3688,10 +3693,31 @@ public class SymbolEnter extends BLangNodeVisitor {
             return;
         }
         if (langLib.equals(REGEXP)) {
+            symResolver.loadRawTemplateType();
+            symResolver.bootstrapIterableType();
+            symResolver.bootstrapIntRangeType();
+
             symTable.langRegexpModuleSymbol = packageSymbol;
             symTable.updateRegExpTypeOwners();
             return;
         }
+        if (langLib.equals(ANNOTATIONS)) {
+            symTable.langAnnotationModuleSymbol = packageSymbol;
+        }
+        if (langLib.equals(JAVA)) {
+            symTable.langJavaModuleSymbol = packageSymbol;
+        }
+        if (langLib.equals(INTERNAL)) {
+            symTable.langInternalModuleSymbol = packageSymbol;
+        }
+        if (langLib.equals(RUNTIME)) {
+            symTable.langRuntimeModuleSymbol = packageSymbol;
+        }
+        if (langLib.equals(TRANSACTION_INTERNAL)) {
+            symTable.internalTransactionModuleSymbol = packageSymbol;
+        }
+
+
     }
 
     public boolean isValidAnnotationType(BType type) {
