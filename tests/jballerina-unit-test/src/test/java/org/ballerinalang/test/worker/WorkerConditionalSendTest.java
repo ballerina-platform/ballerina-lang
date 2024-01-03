15da91b17b46fffc47c4b6c175aff375f23c2c95
lochana-chathura <39232462+lochana-chathura@users.noreply.github.com>
2024-01-03 16:08:03 +0530

    Fix jballerina-unit-test:checkstyleTest failure

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/worker/WorkerConditionalSendTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/worker/WorkerConditionalSendTest.java
@@ -21,7 +21,6 @@ package org.ballerinalang.test.worker;
 import org.ballerinalang.test.BCompileUtil;
 import org.ballerinalang.test.BRunUtil;
 import org.ballerinalang.test.CompileResult;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
