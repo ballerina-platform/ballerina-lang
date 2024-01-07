ballerina-platform/ballerina-lang

commit c786533f6396a4ccd796ecc58a231080359901be

Fix result type for multiple receive action

compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
@@ -2870,7 +2870,6 @@ public class TypeChecker extends SimpleBLangNodeAnalyzer<TypeChecker.AnalyzerDat
         }
 
         multipleWorkerReceive.setBType(compatibleType);
-        data.resultType = compatibleType;
 
         if (TypeTags.RECORD == compatibleType.tag) {
             BRecordType recordType = (BRecordType) compatibleType;
@@ -2899,6 +2898,7 @@ public class TypeChecker extends SimpleBLangNodeAnalyzer<TypeChecker.AnalyzerDat
         for (BLangMultipleWorkerReceive.BLangReceiveField receiveFiled : multipleWorkerReceive.getReceiveFields()) {
             checkExpr(receiveFiled.getWorkerReceive(), receiveFieldExpType, data);
         }
+        data.resultType = compatibleType;
     }
 
     private BType validateAndGetMultipleReceiveCompatibleType(BType bType,

commit 5b0a8d7dd2feb491693b841d974d33aaadd76f1f

Fix regression issue introduced with commit c786533

compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/TypeChecker.java
@@ -2884,6 +2884,7 @@ public class TypeChecker extends SimpleBLangNodeAnalyzer<TypeChecker.AnalyzerDat
 
                 checkExpr(receiveFiled.getWorkerReceive(), receiveFieldExpType, data);
             }
+            data.resultType = compatibleType;
             return;
         }
 