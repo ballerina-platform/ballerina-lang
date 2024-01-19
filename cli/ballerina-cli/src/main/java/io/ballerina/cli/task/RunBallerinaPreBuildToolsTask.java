5b0704617596d5ccab2f1dcb2ba531ed382efe83
ShammiL <shammi0107@gmail.com>
2024-01-19 07:17:01 +0530

    Improve diagnostic handling for build tools

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/task/RunBallerinaPreBuildToolsTask.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/task/RunBallerinaPreBuildToolsTask.java
@@ -20,18 +20,24 @@ package io.ballerina.cli.task;
 
 import io.ballerina.cli.tool.CodeGeneratorTool;
 import io.ballerina.cli.utils.FileUtils;
+import io.ballerina.projects.Diagnostics;
 import io.ballerina.projects.Project;
-import io.ballerina.projects.ProjectException;
 import io.ballerina.projects.ToolContext;
+import io.ballerina.projects.internal.PackageDiagnostic;
+import io.ballerina.projects.internal.ProjectDiagnosticErrorCode;
 import io.ballerina.toml.api.Toml;
 import io.ballerina.tools.diagnostics.Diagnostic;
+import io.ballerina.tools.diagnostics.DiagnosticInfo;
 import io.ballerina.tools.diagnostics.DiagnosticSeverity;
 
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ServiceLoader;
+import java.util.stream.Collectors;
 
 import static io.ballerina.cli.launcher.LauncherUtils.createLauncherException;
 import static io.ballerina.projects.PackageManifest.Tool;
@@ -42,7 +48,6 @@ import static io.ballerina.projects.PackageManifest.Tool;
  * @since 2201.9.0
  */
 public class RunBallerinaPreBuildToolsTask implements Task {
-
     private final PrintStream outStream;
 
     public RunBallerinaPreBuildToolsTask(PrintStream out) {
@@ -51,41 +56,61 @@ public class RunBallerinaPreBuildToolsTask implements Task {
 
     @Override
     public void execute(Project project) {
-        Collection<Diagnostic> toolDiagnostics = project.currentPackage().manifest().diagnostics().diagnostics();
-        boolean hasTomlErrors = project.currentPackage().manifest().diagnostics().hasErrors();
-        if (hasTomlErrors) {
-            toolDiagnostics.forEach(outStream::println);
-            throw createLauncherException("compilation contains errors");
-        }
+        this.outStream.println("\nExecuting pre build tools\n");
+        // Print all build tool manifest diagnostics
+        Collection<Diagnostic> toolManifestDiagnostics = project.currentPackage().manifest().diagnostics()
+                .diagnostics().stream().filter(diagnostic -> diagnostic.diagnosticInfo().code().startsWith("BCE54"))
+                .collect(Collectors.toList());
+        toolManifestDiagnostics.forEach(outStream::println);
+
+        //Build tool execution
+        Map<String, ToolContext> toolContextMap = new HashMap<>();
         List<Tool> tools = project.currentPackage().manifest().tools();
         ServiceLoader<CodeGeneratorTool> buildRunners = ServiceLoader.load(CodeGeneratorTool.class);
         for (Tool tool : tools) {
+            String commandName = tool.getType();
+            boolean hasOptionErrors = false;
             try {
-                String commandName = tool.getType();
-                CodeGeneratorTool targetTool = getTargetTool(commandName, buildRunners);
-                if (targetTool == null) {
-                    // TODO: Install tool if not found
-                    outStream.println("Command not found: " + commandName);
-                    return;
-                }
-                try {
-                    validateOptionsToml(tool.getOptionsToml(), commandName);
-                } catch (IOException e) {
-                    outStream.println("WARNING: Skipping the validation of tool options due to: " +
+                hasOptionErrors = validateOptionsToml(tool.getOptionsToml(), commandName);
+            } catch (IOException e) {
+                outStream.println("\nWARNING: Skipping the validation of tool options due to: " +
                         e.getMessage());
-                }
-                ToolContext toolContext = ToolContext.from(tool, project.currentPackage());
-                targetTool.execute(toolContext);
-                toolContext.diagnostics().forEach(outStream::println);
-                for (Diagnostic d : toolContext.diagnostics()) {
-                    if (d.diagnosticInfo().severity().equals(DiagnosticSeverity.ERROR)) {
-                        throw new ProjectException("compilation contains errors");
+            }
+            if (!tool.hasErrorDiagnostic() && !hasOptionErrors) {
+                try {
+                    CodeGeneratorTool targetTool = getTargetTool(commandName, buildRunners);
+                    ToolContext toolContext = ToolContext.from(tool, project.currentPackage());
+                    if (targetTool == null) {
+                        // TODO: Installing tool if not found to be implemented at a later phase
+                        DiagnosticInfo diagnosticInfo = new DiagnosticInfo(
+                                ProjectDiagnosticErrorCode.BUILD_TOOL_NOT_FOUND.diagnosticId(),
+                                "Build tool '" + tool.getType() + "' not found",
+                                DiagnosticSeverity.ERROR);
+                        PackageDiagnostic diagnostic = new PackageDiagnostic(diagnosticInfo,
+                                tool.getType());
+                        this.outStream.println(diagnostic);
+                        toolContext.reportDiagnostic(diagnostic);
+                        toolContextMap.put(tool.getId(), toolContext);
+                        continue;
                     }
+                    this.outStream.println("\nExecuting build tool '" + tool.getType() +
+                            "' for tool configurations '" + tool.getId() + "'.");
+                    targetTool.execute(toolContext);
+                    toolContext.diagnostics().forEach(outStream::println);
+                    if (!Diagnostics.filterErrors(toolContext.diagnostics()).isEmpty()) {
+                        outStream.println("WARNING: Execution of Build tool '" + tool.getType() +
+                            "' for tool configurations '" + tool.getId() + "' contains errors\n");
+                    }
+                    toolContextMap.put(tool.getId(), toolContext);
+                } catch (Exception e) {
+                    throw createLauncherException(e.getMessage());
                 }
-            } catch (ProjectException e) {
-                throw createLauncherException(e.getMessage());
+            } else {
+                outStream.println("WARNING: Skipping execution of build tool '" + tool.getType() +
+                        "' as tool configurations in Ballerina.toml contains errors\n");
             }
         }
+        project.currentPackage().setPackageContextTools(toolContextMap);
     }
 
     private CodeGeneratorTool getTargetTool(String commandName, ServiceLoader<CodeGeneratorTool> buildRunners) {
@@ -97,16 +122,12 @@ public class RunBallerinaPreBuildToolsTask implements Task {
         return null;
     }
 
-    private void validateOptionsToml(Toml optionsToml, String toolName) throws IOException {
+    private boolean validateOptionsToml(Toml optionsToml, String toolName) throws IOException {
         if (optionsToml == null) {
             throw new IOException("No tool options found");
         }
         FileUtils.validateToml(optionsToml, toolName);
         optionsToml.diagnostics().forEach(outStream::println);
-        for (Diagnostic d : optionsToml.diagnostics()) {
-            if (d.diagnosticInfo().severity().equals(DiagnosticSeverity.ERROR)) {
-                throw new ProjectException("compilation contains errors");
-            }
-        }
+        return !Diagnostics.filterErrors(optionsToml.diagnostics()).isEmpty();
     }
 }
