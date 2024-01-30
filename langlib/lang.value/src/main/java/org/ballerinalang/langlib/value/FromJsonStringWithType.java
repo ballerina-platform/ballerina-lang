a9b6d05feb890959d15bc8c4f8896f46cce7a223
Nadeeshan96 <nadeeshand@wso2.com>
2024-01-30 11:21:23 +0530

    Use StreamParser in FromJsonStringWithType internals

--- langlib/lang.value/src/main/java/org/ballerinalang/langlib/value/FromJsonStringWithType.java
+++ langlib/lang.value/src/main/java/org/ballerinalang/langlib/value/FromJsonStringWithType.java
@@ -22,7 +22,7 @@ import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BString;
 import io.ballerina.runtime.api.values.BTypedesc;
-import io.ballerina.runtime.internal.JsonParser;
+import io.ballerina.runtime.internal.util.StreamParser;
 
 import static io.ballerina.runtime.internal.errors.ErrorReasons.VALUE_LANG_LIB_CONVERSION_ERROR;
 
@@ -34,16 +34,11 @@ import static io.ballerina.runtime.internal.errors.ErrorReasons.VALUE_LANG_LIB_C
  */
 public class FromJsonStringWithType {
 
-    public static Object fromJsonStringWithType(BString value, BTypedesc t) {
+    private FromJsonStringWithType() {}
 
-        String str = value.getValue();
+    public static Object fromJsonStringWithType(BString value, BTypedesc t) {
         try {
-            if (str.equals("null")) {
-                return FromJsonWithType.fromJsonWithType(null, t);
-            } else {
-                Object jsonFromString = JsonParser.parse(str);
-                return FromJsonWithType.fromJsonWithType(jsonFromString, t);
-            }
+            return StreamParser.parse(value.getValue(), t.getDescribingType());
         } catch (BError e) {
             return ErrorCreator.createError(VALUE_LANG_LIB_CONVERSION_ERROR,
                                             StringUtils.fromString(e.getMessage()));
