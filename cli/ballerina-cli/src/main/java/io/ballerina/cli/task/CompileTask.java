35c2d0b6c958cc89f61b26e0410c128ca862ce37
Radith Samarakoon <jjeli1234@gmail.com>
2024-02-02 14:35:18 +0530

    Filter duplicate diagnostics

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/task/CompileTask.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/task/CompileTask.java
@@ -46,6 +46,7 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.HashSet;
 
 import static io.ballerina.cli.launcher.LauncherUtils.createLauncherException;
 import static io.ballerina.projects.util.ProjectConstants.DOT;
@@ -226,13 +227,17 @@ public class CompileTask implements Task {
                 }
             }
             boolean hasErrors = false;
+            // HashSet to keep track of the diagnostics to avoid duplicate diagnostics
+            HashSet<String> diagnosticSet = new HashSet<>();
             for (Diagnostic d : diagnostics) {
                 if (d.diagnosticInfo().severity().equals(DiagnosticSeverity.ERROR)) {
                     hasErrors = true;
                 }
                 if (d.diagnosticInfo().code() == null || !d.diagnosticInfo().code().equals(
                         ProjectDiagnosticErrorCode.BUILT_WITH_OLDER_SL_UPDATE_DISTRIBUTION.diagnosticId())) {
-                    err.println(AnnotateDiagnostics.renderDiagnostic(d));
+                    if (diagnosticSet.add(d.toString())) {
+                        err.println(AnnotateDiagnostics.renderDiagnostic(d));
+                    }
                 }
             }
             if (hasErrors) {
