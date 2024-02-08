3648fec3f6359d80d44ea066e514f81686605caa
Thushara-Piyasekara <thusharap@wso2.com>
2024-02-08 07:55:10 +0530

    Remove unused code

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/CompilationOptions.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/CompilationOptions.java
@@ -134,7 +134,7 @@ public class CompilationOptions {
         return toBooleanDefaultIfNull(this.remoteManagement);
     }
     public boolean optimizeCodegen() {
-        return toBooleanDefaultIfNull(this.optimizeCodegen);
+        return toBooleanDefaultIfNull(optimizeCodegen);
     }
 
     /**
