662b8df4e53e8c220cbd8ffc70aa47fb063b387d
Radith Samarakoon <jjeli1234@gmail.com>
2024-02-15 11:56:10 +0530

    Use existing syntaxtree, handle bal build projects

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/task/CompileTask.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/task/CompileTask.java
@@ -21,6 +21,7 @@ package io.ballerina.cli.task;
 //import io.ballerina.cli.tool.AnnotateDiagnostics2;
 
 
+import io.ballerina.projects.*;
 import io.ballerina.shell.cli.AnnotateDiagnostics;
 import io.ballerina.cli.utils.BuildTime;
 import io.ballerina.projects.CodeGeneratorResult;
@@ -237,7 +238,17 @@ public class CompileTask implements Task {
             boolean hasErrors = false;
             // HashSet to keep track of the diagnostics to avoid duplicate diagnostics
             HashSet<String> diagnosticSet = new HashSet<>();
+            // HashMap for documents based on filename
+            HashMap<String, Document> documentMap = new HashMap<>();
             int terminalWidth = AnnotateDiagnostics.getTerminalWidth();
+
+            project.currentPackage().moduleIds().forEach(moduleId -> {
+                project.currentPackage().module(moduleId).documentIds().forEach(documentId -> {
+                    Document document = project.currentPackage().module(moduleId).document(documentId);
+                    documentMap.put(getDocumentPath(document.module().moduleName(), document.name()), document);
+                });
+            });
+
             for (Diagnostic d : diagnostics) {
                 if (d.diagnosticInfo().severity().equals(DiagnosticSeverity.ERROR)) {
                     hasErrors = true;
@@ -245,7 +256,12 @@ public class CompileTask implements Task {
                 if (d.diagnosticInfo().code() == null || !d.diagnosticInfo().code().equals(
                         ProjectDiagnosticErrorCode.BUILT_WITH_OLDER_SL_UPDATE_DISTRIBUTION.diagnosticId())) {
                     if (diagnosticSet.add(d.toString())) {
-                        err.println(AnnotateDiagnostics.renderDiagnostic(d, terminalWidth));
+                        Document document = documentMap.get(d.location().lineRange().fileName());
+                        if (document != null) {
+                            err.println(AnnotateDiagnostics.renderDiagnostic(d, document, terminalWidth));
+                        } else {
+                            err.println(d.toString());
+                        }
                     }
                 }
             }
@@ -256,6 +272,13 @@ public class CompileTask implements Task {
         } catch (ProjectException e) {
             throw createLauncherException("compilation failed: " + e.getMessage());
         }
+    }
+
+    private String getDocumentPath(ModuleName moduleName, String documentName) {
+        if (moduleName.isDefaultModuleName()) {
+            return documentName;
+        }
+        return "modules" + "/" + moduleName.moduleNamePart() + "/" + documentName;
     }
 
     private boolean isPackCmdForATemplatePkg(Project project) {
