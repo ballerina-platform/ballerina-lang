6cb7b44ab9938f14cdd026c53da83bb57e173afe
Thushara-Piyasekara <thusharap@wso2.com>
2024-02-20 11:31:42 +0530

    Fix review suggestions

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/task/CreateExecutableTask.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/task/CreateExecutableTask.java
@@ -140,7 +140,6 @@ public class CreateExecutableTask implements Task {
             Path relativePathToExecutable = currentDir.relativize(executablePath);
             String relativePathToExecutableString =
                     relativePathToExecutable.toString().replace(".jar", "_OPTIMIZED.jar");
-
             String executablePathString =
                     executablePath.toString().replace(".jar", "_OPTIMIZED.jar");
 
