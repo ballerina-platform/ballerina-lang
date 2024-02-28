65209f1d55c3772a1903deef4f22006605e0e91a
Radith Samarakoon <jjeli1234@gmail.com>
2024-02-28 11:23:36 +0530

    Add license header and class comment

--- compiler/ballerina-parser/src/main/java/io/ballerina/compiler/internal/diagnostics/StringDiagnosticProperty.java
+++ compiler/ballerina-parser/src/main/java/io/ballerina/compiler/internal/diagnostics/StringDiagnosticProperty.java
@@ -1,3 +1,19 @@
+// Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package io.ballerina.compiler.internal.diagnostics;
 
 import io.ballerina.tools.diagnostics.DiagnosticProperty;
@@ -5,6 +21,11 @@ import io.ballerina.tools.diagnostics.DiagnosticPropertyKind;
 
 import java.util.Arrays;
 
+/**
+ * Represents a string diagnostic property.
+ *
+ * @since 2201.9.0
+ */
 public class StringDiagnosticProperty implements DiagnosticProperty<String> {
     private final DiagnosticPropertyKind kind;
     private final String value;
