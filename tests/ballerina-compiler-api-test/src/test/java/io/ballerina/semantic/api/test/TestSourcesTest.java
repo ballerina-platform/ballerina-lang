8fc3f94d6859be7734ca88cbde9c9e9837a17d00
dulaj <dulaj@wso2.com>
2024-02-28 10:44:11 +0530

    Add tests

--- tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/TestSourcesTest.java
+++ tests/ballerina-compiler-api-test/src/test/java/io/ballerina/semantic/api/test/TestSourcesTest.java
@@ -21,10 +21,15 @@ import io.ballerina.compiler.api.SemanticModel;
 import io.ballerina.compiler.api.symbols.Symbol;
 import io.ballerina.compiler.api.symbols.TypeDescKind;
 import io.ballerina.compiler.api.symbols.TypeSymbol;
+import io.ballerina.compiler.syntax.tree.BasicLiteralNode;
+import io.ballerina.compiler.syntax.tree.ConstantDeclarationNode;
+import io.ballerina.compiler.syntax.tree.NodeVisitor;
+import io.ballerina.compiler.syntax.tree.SyntaxTree;
 import io.ballerina.projects.Document;
 import io.ballerina.projects.DocumentId;
 import io.ballerina.projects.Module;
 import io.ballerina.projects.Project;
+import io.ballerina.tools.diagnostics.Location;
 import io.ballerina.tools.text.LineRange;
 import org.ballerinalang.test.BCompileUtil;
 import org.testng.annotations.BeforeClass;
@@ -43,6 +48,7 @@ import static io.ballerina.semantic.api.test.util.SemanticAPITestUtils.getSymbol
 import static io.ballerina.tools.text.LinePosition.from;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
 
 /**
  * Test cases for the positions of symbols.
@@ -53,10 +59,11 @@ public class TestSourcesTest {
 
     private SemanticModel model;
     private Document srcFile;
+    private Project project;
 
     @BeforeClass
     public void setup() {
-        Project project = BCompileUtil.loadProject("test-src/test-project");
+        this.project = BCompileUtil.loadProject("test-src/test-project");
         Module baz = getModule(project, "baz");
         model = project.currentPackage().getCompilation().getSemanticModel(baz.moduleId());
         DocumentId id = baz.testDocumentIds().iterator().next();
@@ -103,7 +110,7 @@ public class TestSourcesTest {
     public Object[][] getVisibleSymbolPos() {
         List<String> moduleSymbols = List.of("PI", "ZERO", "add", "concat", "newPerson", "Person", "Employee",
                                              "BasicType", "PersonObj", "Digit", "FileNotFoundError", "EofError",
-                                             "Error", "Address");
+                                             "Error", "Address", "IV");
         return new Object[][]{
                 {17, 0, getSymbolNames(moduleSymbols, "testConstUse", "testAdd", "test")},
                 {26, 31, getSymbolNames(moduleSymbols, "testConstUse", "testAdd", "test", "sum")},
@@ -124,6 +131,74 @@ public class TestSourcesTest {
                 {20, 22, 20, 24, FLOAT},
                 {20, 26, 20, 30, FLOAT},
                 {26, 22, 26, 25, INT},
+        };
+    }
+
+    @Test
+    public void testSemanticModelForWrongModule() {
+        Module foo = getModule(this.project, "foo");
+        SemanticModel fooSemanticModel = foo.getCompilation().getSemanticModel();
+
+        Module baz = getModule(this.project, "baz");
+        SemanticModel bazSemanticModel = baz.getCompilation().getSemanticModel();
+
+        DocumentId bazConstantsDocId = baz.documentIds().stream()
+                .filter(docId -> docId.toString().contains("baz.bal"))
+                .findFirst().get();
+
+        SyntaxTree bazSyntaxTree = baz.document(bazConstantsDocId).syntaxTree();
+
+        // `symbol(Node node)` and `typeOf(Node node)`
+        // use `fooSemanticModel` for `baz` module
+        bazSyntaxTree.rootNode().accept(getNodeVisitor(fooSemanticModel));
+
+        Document bazConstantsDoc = baz.document(bazConstantsDocId);
+
+        // `symbol(Document sourceDocument, LinePosition position)`
+        Optional<Symbol> personRecSymbol = fooSemanticModel.symbol(bazConstantsDoc, from(16, 13));
+        assertTrue(personRecSymbol.isEmpty());
+
+        // `visibleSymbols(Document sourceFile, LinePosition position, DiagnosticState... states)`
+        List<Symbol> visibleSymbols = fooSemanticModel.visibleSymbols(bazConstantsDoc, from(16, 23));
+        assertTrue(visibleSymbols.isEmpty());
+
+        // `references(Document sourceDocument, LinePosition position)`
+        List<Location> references1 = fooSemanticModel.references(bazConstantsDoc, from(0, 0));
+        assertTrue(references1.isEmpty());
+
+        // `references(Document sourceDocument, LinePosition position, boolean withDefinition)`
+        List<Location> references2 = fooSemanticModel.references(bazConstantsDoc, from(0, 0), true);
+        assertTrue(references2.isEmpty());
+
+        // `references(Symbol symbol, Document targetDocument, boolean withDefinition)`
+        Optional<Symbol> constSym = bazSemanticModel.symbol(bazConstantsDoc, from(16, 13));
+        assertTrue(constSym.isPresent());
+        List<Location> references3 = fooSemanticModel.references(constSym.get(), bazConstantsDoc, true);
+        assertTrue(references3.isEmpty());
+
+        // `references(Document sourceDocument, Document targetDocument, LinePosition position, boolean withDefinition)`
+        List<Location> references4 = fooSemanticModel.references(bazConstantsDoc, bazConstantsDoc, from(16, 13), true);
+        assertTrue(references4.isEmpty());
+
+        // `expectedType(Document sourceDocument, LinePosition linePosition)`
+        Optional<TypeSymbol> expType = fooSemanticModel.expectedType(bazConstantsDoc, from(16, 18));
+        assertTrue(expType.isEmpty());
+    }
+
+    private NodeVisitor getNodeVisitor(SemanticModel semanticModel) {
+        return new NodeVisitor() {
+            @Override
+            public void visit(ConstantDeclarationNode constantDeclarationNode) {
+                Optional<Symbol> foundSymbol = semanticModel.symbol(constantDeclarationNode);
+                assertTrue(foundSymbol.isEmpty());
+                constantDeclarationNode.initializer().accept(this);
+            }
+
+            @Override
+            public void visit(BasicLiteralNode basicLiteralNode) {
+                Optional<TypeSymbol> foundTypeSymbol = semanticModel.typeOf(basicLiteralNode);
+                assertTrue(foundTypeSymbol.isEmpty());
+            }
         };
     }
 }
