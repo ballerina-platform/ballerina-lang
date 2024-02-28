dcc8dfa17101f94e0d569939b4064b1b82b64119
Radith Samarakoon <jjeli1234@gmail.com>
2024-02-28 11:23:36 +0530

    Add license header and class comment

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/utils/AnnotateDiagnostics.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/utils/AnnotateDiagnostics.java
@@ -1,3 +1,19 @@
+// Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package io.ballerina.cli.utils;
 
 import io.ballerina.compiler.internal.diagnostics.StringDiagnosticProperty;
@@ -12,6 +28,11 @@ import java.util.ArrayList;
 
 import static io.ballerina.cli.utils.DiagnosticAnnotation.SEVERITY_COLORS;
 
+/**
+ * This class is used to generate diagnostic annotations from diagnostics.
+ *
+ * @since 2201.9.0
+ */
 public class AnnotateDiagnostics {
 
     public static String renderDiagnostic(Diagnostic diagnostic, Document document, int terminalWidth) {
