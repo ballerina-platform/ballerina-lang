49f7dcd974ea8df23b057ec9287231f947be36f9
gabilang <ganeshwargabil@gmail.com>
2024-03-06 15:37:13 +0530

    Use log leechers for assertion

--- tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/runtime/api/RuntimeAPITest.java
+++ tests/jballerina-integration-test/src/test/java/org/ballerinalang/test/runtime/api/RuntimeAPITest.java
@@ -19,14 +19,13 @@ package org.ballerinalang.test.runtime.api;
 import org.ballerinalang.test.BaseTest;
 import org.ballerinalang.test.context.BMainInstance;
 import org.ballerinalang.test.context.BallerinaTestException;
-import org.ballerinalang.test.util.AssertionUtils;
+import org.ballerinalang.test.context.LogLeecher;
+import org.ballerinalang.test.context.ServerLogReader;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -65,6 +64,47 @@ public class RuntimeAPITest extends BaseTest {
     public void testRuntimeAPIsForBalFunctionInvocation() throws BallerinaTestException {
         Path jarPath = Paths.get(testFileLocation, "function_invocation", "target", "bin",
                 "function_invocation.jar").toAbsolutePath();
+        compileJavaSource(jarPath);
+        unzipJarFile(jarPath);
+        createExecutableJar();
+
+        // Run the executable jar and assert the output
+        Path execJarPath = Paths.get(javaSrcLocation.toString(), "targetDir", "test-exec.jar").toAbsolutePath();
+        List<String> runCmdSet = new ArrayList<>();
+        runCmdSet.add("java");
+        runCmdSet.add("-jar");
+        runCmdSet.add(execJarPath.toString());
+        ProcessBuilder runProcessBuilder = new ProcessBuilder(runCmdSet);
+        String jacocoArgLine = "-javaagent:" + Paths.get(System.getProperty(BALLERINA_HOME), "bre", "lib",
+                "jacocoagent.jar") + "=destfile=" + Paths.get(System.getProperty(USER_DIR))
+                .resolve("build").resolve("jacoco").resolve("test.exec") + " ";
+        runProcessBuilder.environment().put("JAVA_OPTS", jacocoArgLine);
+        try {
+            Process runProcess = runProcessBuilder.start();
+            ServerLogReader serverInfoLogReader = new ServerLogReader("inputStream", runProcess.getInputStream());
+            List<LogLeecher> leechers = new ArrayList<>();
+            leechers.add(new LogLeecher("12"));
+            leechers.add(new LogLeecher("Hello World"));
+            leechers.add(new LogLeecher("{\"id\":1001,\"name\":\"John\"," +
+                    "\"sportsActivity\":{\"event\":\"100m\",\"year\":2020}}"));
+            addToServerInfoLogReader(serverInfoLogReader, leechers);
+            serverInfoLogReader.start();
+            bMainInstance.waitForLeechers(leechers, 5000);
+            runProcess.waitFor();
+            serverInfoLogReader.stop();
+            serverInfoLogReader.removeAllLeechers();
+        } catch (IOException | InterruptedException e) {
+            throw new BallerinaTestException("Error occurred while running the java file");
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        bMainInstance = null;
+        deleteDirectory(Paths.get(javaSrcLocation.toString(), "targetDir").toFile());
+    }
+
+    private static void compileJavaSource(Path jarPath) throws BallerinaTestException {
         List<String> compileCmdSet = new ArrayList<>();
         compileCmdSet.add("javac");
         compileCmdSet.add("-cp");
@@ -79,7 +119,9 @@ public class RuntimeAPITest extends BaseTest {
         } catch (InterruptedException | IOException e) {
             throw new BallerinaTestException("Error occurred while compiling the java file");
         }
+    }
 
+    private static void unzipJarFile(Path jarPath) throws BallerinaTestException {
         List<String> unzipProcessCmdSet = new ArrayList<>();
         unzipProcessCmdSet.add("unzip");
         unzipProcessCmdSet.add("-qq");
@@ -94,7 +136,9 @@ public class RuntimeAPITest extends BaseTest {
         } catch (IOException | InterruptedException e) {
             throw new BallerinaTestException("Error occurred while unzipping the jar file");
         }
+    }
 
+    private static void createExecutableJar() throws BallerinaTestException {
         List<String> jarCmdSet = new ArrayList<>();
         jarCmdSet.add("jar");
         jarCmdSet.add("-cfe");
@@ -109,33 +153,11 @@ public class RuntimeAPITest extends BaseTest {
         } catch (IOException | InterruptedException e) {
             throw new BallerinaTestException("Error occurred while packing the jar file");
         }
-
-        Path execJarPath = Paths.get(javaSrcLocation.toString(), "targetDir", "test-exec.jar").toAbsolutePath();
-        List<String> runCmdSet = new ArrayList<>();
-        runCmdSet.add("java");
-        runCmdSet.add("-jar");
-        runCmdSet.add(execJarPath.toString());
-        ProcessBuilder runProcessBuilder = new ProcessBuilder(runCmdSet);
-        String jacocoArgLine = "-javaagent:" + Paths.get(System.getProperty(BALLERINA_HOME), "bre", "lib",
-                "jacocoagent.jar") + "=destfile=" + Paths.get(System.getProperty(USER_DIR))
-                .resolve("build").resolve("jacoco").resolve("test.exec") + " ";
-        runProcessBuilder.environment().put("JAVA_OPTS", jacocoArgLine);
-        runProcessBuilder.redirectErrorStream(true);
-        Path outputFileDir = Paths.get(javaSrcLocation.toString(), "targetDir", "output.txt");
-        runProcessBuilder.redirectOutput(new File(outputFileDir.toAbsolutePath().toString()));
-        try {
-            Process runProcess = runProcessBuilder.start();
-            runProcess.waitFor();
-            String programOutput = Files.readString(outputFileDir);
-            AssertionUtils.assertOutput("RuntimeAPITest-invokeFunction.txt", programOutput);
-        } catch (IOException | InterruptedException e) {
-            throw new BallerinaTestException("Error occurred while running the java file");
-        }
     }
 
-    @AfterClass
-    public void tearDown() {
-        bMainInstance = null;
-        deleteDirectory(Paths.get(javaSrcLocation.toString(), "targetDir").toFile());
+    private static void addToServerInfoLogReader(ServerLogReader serverInfoLogReader, List<LogLeecher> leechers) {
+        for (LogLeecher leecher : leechers) {
+            serverInfoLogReader.addLeecher(leecher);
+        }
     }
 }
