5fca925af59d39e27ac6fe7905fd4b0e71bb2259
hindujaB <bhinduja08@gmail.com>
2024-03-29 19:43:23 +0530

    Fix `never` type frame fields loaded as XML

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/methodgen/MethodGen.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/codegen/methodgen/MethodGen.java
@@ -754,6 +754,9 @@ public class MethodGen {
                 mv.visitFieldInsn(GETFIELD, frameName, localVar.jvmVarName,
                         GET_BSTRING);
                 mv.visitVarInsn(ASTORE, index);
+            } else if (bType.tag == TypeTags.NEVER) {
+                mv.visitFieldInsn(GETFIELD, frameName, localVar.jvmVarName, GET_OBJECT);
+                mv.visitVarInsn(ASTORE, index);
             } else if (types.isAssignable(bType, symbolTable.xmlType)) {
                 mv.visitFieldInsn(GETFIELD, frameName, localVar.jvmVarName,
                         GET_XML);
