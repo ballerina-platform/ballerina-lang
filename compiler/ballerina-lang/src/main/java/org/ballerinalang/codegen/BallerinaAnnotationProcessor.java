9134010813a98aff3c8b0305b75cf12abcedd4d7
Felix Schnabel <f.schnabel@tum.de>
2024-05-21 17:31:03 +0200

    Fix some warnings

--- compiler/ballerina-lang/src/main/java/org/ballerinalang/codegen/BallerinaAnnotationProcessor.java
+++ compiler/ballerina-lang/src/main/java/org/ballerinalang/codegen/BallerinaAnnotationProcessor.java
@@ -53,7 +53,7 @@ import javax.tools.StandardLocation;
  */
 @SupportedAnnotationTypes({ "org.ballerinalang.annotation.JavaSPIService",
         "org.ballerinalang.natives.annotations.BallerinaFunction" })
-@SupportedSourceVersion(SourceVersion.RELEASE_11)
+@SupportedSourceVersion(SourceVersion.RELEASE_17)
 @SupportedOptions({ "nativeEntityProviderPackage", "nativeEntityProviderClass" })
 public class BallerinaAnnotationProcessor extends AbstractProcessor {
     
