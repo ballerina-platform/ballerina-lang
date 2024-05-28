68b2298c943bca23a57ffa31ad40b502c4040a35
Thushara-Piyasekara <thusharap@wso2.com>
2024-05-28 12:36:59 +0530

    Fix getJarFilePathsRequiredForExecution() overload

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/workspace/BallerinaWorkspaceManager.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/workspace/BallerinaWorkspaceManager.java
@@ -733,7 +733,7 @@ public class BallerinaWorkspaceManager implements WorkspaceManager {
 
     private String getAllClassPaths(JarResolver jarResolver) {
         StringJoiner cp = new StringJoiner(File.pathSeparator);
-        for (JarLibrary lib : jarResolver.getJarFilePathsRequiredForExecution(false)) {
+        for (JarLibrary lib : jarResolver.getJarFilePathsRequiredForExecution()) {
             cp.add(lib.path().toString());
         }
         return cp.toString();
