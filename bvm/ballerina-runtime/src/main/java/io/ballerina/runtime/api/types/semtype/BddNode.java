00e71601c56a3b8506dfb17fd2bb23c349622d78
Heshan Padmasiri <heshanp@wso2.com>
2024-05-29 15:53:25 +0530

    Make it possible to create cell type

---
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/api/types/semtype/BddNode.java
@@ -0,0 +1,82 @@
+/*
+ *  Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package io.ballerina.runtime.api.types.semtype;
+
+import io.ballerina.runtime.internal.types.semtype.SubTypeData;
+
+public final class BddNode extends SubType implements Bdd {
+
+    private final Atom atom;
+    private final Bdd left;
+    private final Bdd middle;
+    private final Bdd right;
+
+    public BddNode(Atom atom, Bdd left, Bdd middle, Bdd right) {
+        super(false, false);
+        this.atom = atom;
+        this.left = left;
+        this.middle = middle;
+        this.right = right;
+    }
+
+    static BddNode bddAtom(TypeAtom atom) {
+        return new BddNode(atom, BddAllOrNothing.ALL, BddAllOrNothing.NOTHING, BddAllOrNothing.NOTHING);
+    }
+
+    public Atom atom() {
+        return atom;
+    }
+
+    public Bdd left() {
+        return left;
+    }
+
+    public Bdd middle() {
+        return middle;
+    }
+
+    public Bdd right() {
+        return right;
+    }
+
+    @Override
+    public SubType union(SubType other) {
+        throw new IllegalStateException("Unimplemented");
+    }
+
+    @Override
+    public SubType intersect(SubType other) {
+        throw new IllegalStateException("Unimplemented");
+    }
+
+    @Override
+    public SubType complement() {
+        throw new IllegalStateException("Unimplemented");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new IllegalStateException("Unimplemented");
+    }
+
+    @Override
+    public SubTypeData data() {
+        throw new IllegalStateException("Unimplemented");
+    }
+}
