fd0bfa724cd0db1e08d286e52ddc1693df448209
Thushara-Piyasekara <thusharap@wso2.com>
2024-06-03 13:56:49 +0530

    Add missing headers and javadoc comments

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/UsedTypeDefAnalyzer.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/UsedTypeDefAnalyzer.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package io.ballerina.projects;
 
 import org.wso2.ballerinalang.compiler.PackageCache;
@@ -10,6 +27,11 @@ import org.wso2.ballerinalang.compiler.util.TypeTags;
 import java.util.HashMap;
 import java.util.HashSet;
 
+/**
+ * Analyses a given type definition and marks its dependencies.
+ *
+ * @since 2201.10.0
+ */
 public class UsedTypeDefAnalyzer extends SimpleBTypeAnalyzer<UsedTypeDefAnalyzer.AnalyzerData> {
 
     private static final CompilerContext.Key<UsedTypeDefAnalyzer> BIR_TYPE_DEF_ANALYZER_KEY =
