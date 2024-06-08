b9f6d48b0a8191b13486eefc118708de4dbd5cf5
Felix Schnabel <f.schnabel@tum.de>
2024-06-08 19:21:19 +0200

    TODO: Why is entrySet not used? Typechecking error if adding generics to return type:
    Required type: Set <Entry<K, V>>
    Provided type: LinkedHashSet<List<Entry<K, V>>>

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/values/TableValueImpl.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/values/TableValueImpl.java
@@ -254,7 +254,7 @@ public class TableValueImpl<K, V> implements TableValue<K, V> {
         for (List<Map.Entry<K, V>> entry: entries.values()) {
             entrySet.addAll(entry);
         }
-        return new LinkedHashSet(entries.values());
+        return entrySet;
     }
 
     @Override
