2085beb7e9321d0e6cf4f70cab1a88d7eb1c873e
Heshan Padmasiri <heshanp@wso2.com>
2024-06-10 08:43:55 +0530

    Cache type check results

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/api/types/semtype/SemType.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/api/types/semtype/SemType.java
@@ -22,7 +22,13 @@ import io.ballerina.runtime.internal.types.BSemTypeWrapper;
 import io.ballerina.runtime.internal.types.semtype.PureSemType;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static io.ballerina.runtime.api.types.semtype.BasicTypeCode.CODE_UNDEF;
 
 /**
  * Runtime representation of SemType.
@@ -36,17 +42,28 @@ public abstract sealed class SemType implements BasicTypeBitSet permits BSemType
     private final SubType[] subTypeData;
     private static final SubType[] EMPTY_SUBTYPE_DATA = new SubType[0];
     private Integer hashCode;
+    private static volatile AtomicInteger nextId = new AtomicInteger(0);
+    private final Integer typeID = nextId.getAndIncrement();
+    private final Map<Integer, Boolean> cachedSubTypeRelations;
+    private static final int CACHEABLE_TYPE_MASK = (~BasicTypeCode.BASIC_TYPE_MASK) & ((1 << (CODE_UNDEF + 1)) - 1);
+    private static final int MAX_CACHE_LIMIT = 1000;
+    private final boolean useCache;
 
     protected SemType(int all, int some, SubType[] subTypeData) {
         this.all = all;
         this.some = some;
         this.subTypeData = subTypeData;
+        if ((some & CACHEABLE_TYPE_MASK) != 0) {
+            useCache = true;
+            this.cachedSubTypeRelations = new ConcurrentHashMap<>();
+        } else {
+            useCache = false;
+            this.cachedSubTypeRelations = null;
+        }
     }
 
     protected SemType(int all) {
-        this.all = all;
-        this.some = 0;
-        this.subTypeData = EMPTY_SUBTYPE_DATA;
+        this(all, 0, EMPTY_SUBTYPE_DATA);
     }
 
     protected SemType(SemType semType) {
@@ -105,5 +122,24 @@ public abstract sealed class SemType implements BasicTypeBitSet permits BSemType
 
     private int computeHashCode() {
         return Objects.hash(all, some, Arrays.hashCode(subTypeData));
+    }
+
+    Optional<Boolean> cachedSubTypeRelation(SemType other) {
+        if (!useCache) {
+            return Optional.empty();
+        }
+        if (other.typeID.equals(this.typeID)) {
+            return Optional.of(true);
+        }
+        return Optional.ofNullable(cachedSubTypeRelations.get(other.typeID));
+    }
+
+    void cacheSubTypeRelation(SemType other, boolean result) {
+        if (useCache) {
+            if (cachedSubTypeRelations.size() > MAX_CACHE_LIMIT) {
+                cachedSubTypeRelations.clear();
+            }
+            cachedSubTypeRelations.put(other.typeID, result);
+        }
     }
 }
