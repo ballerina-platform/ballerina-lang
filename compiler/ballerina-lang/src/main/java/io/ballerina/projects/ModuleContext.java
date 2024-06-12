4ba60e0dc6b3b1805888670396020629c783da66
Felix Schnabel <f.schnabel@tum.de>
2024-06-12 22:30:56 +0200

    Fix checkstyle

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/ModuleContext.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/ModuleContext.java
@@ -460,7 +460,7 @@ class ModuleContext {
             try {
                 compilerPhaseRunner.performBirGenPhases(moduleContext.bLangPackage);
             } catch (Throwable t) {
-                assert false : "Compilation failed due to " + ((Supplier<String>)() -> {
+                assert false : "Compilation failed due to " + ((Supplier<String>) () -> {
                     StringWriter errors = new StringWriter();
                     t.printStackTrace(new PrintWriter(errors));
                     return errors.toString();
