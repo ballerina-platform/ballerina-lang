0606f6aec446f26774eba146e9c043103f6e96c8
Thushara-Piyasekara <thusharap@wso2.com>
2024-06-14 21:43:54 +0530

    Add optimize flag validation for jar packing

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/JBallerinaBackend.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/JBallerinaBackend.java
@@ -1042,6 +1042,10 @@ public class JBallerinaBackend extends CompilerBackend {
     }
 
     private ByteArrayOutputStream getOptimizedStream(String pathName) {
+        if (this.optimizedJarStreams == null) {
+            return null;
+        }
+
         for (Map.Entry<String, ByteArrayOutputStream> entry : this.optimizedJarStreams.entrySet()) {
             if (pathName.contains(entry.getKey())) {
                 return entry.getValue();
