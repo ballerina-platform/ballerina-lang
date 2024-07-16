fee530e6f2bfc96d0fe689fc0ea0726ab591fe6b
hindujaB <bhinduja08@gmail.com>
2024-07-16 14:48:07 +0530

    Fix replacing imported global vars

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/BIRGen.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/bir/BIRGen.java
@@ -1698,7 +1698,7 @@ public class BIRGen extends BLangNodeVisitor {
     }
 
     private boolean isInSamePackage(BSymbol objectTypeSymbol, PackageID packageID) {
-        return objectTypeSymbol.pkgID.equals(packageID);
+        return objectTypeSymbol.pkgID.equals(packageID) && !packageID.isTestPkg;
     }
 
     @Override
