13f84d84e0b8fe2d12c356a1ed67f7c8c6e163d2
mindula <rowelmindula@gmail.com>
2024-07-22 11:29:18 +0530

    Remove unnecessary spaces

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/CodeAnalyzer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/CodeAnalyzer.java
@@ -3087,7 +3087,6 @@ public class CodeAnalyzer extends SimpleBLangNodeAnalyzer<CodeAnalyzer.AnalyzerD
                                                                                   bLangLambdaFunction.function);
             }
         }
-
         // If this is a worker we are already in a worker action system,
         // if not we need to initiate a worker action system
         if (isWorker) {
