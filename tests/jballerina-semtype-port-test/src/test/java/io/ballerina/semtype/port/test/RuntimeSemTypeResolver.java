b1a42ffb139af0ca418115a68fac14e940097ddc
Heshan Padmasiri <heshanp@wso2.com>
2024-08-13 07:34:44 +0530

    Remove fallback type checker from type checking

--- tests/jballerina-semtype-port-test/src/test/java/io/ballerina/semtype/port/test/RuntimeSemTypeResolver.java
+++ tests/jballerina-semtype-port-test/src/test/java/io/ballerina/semtype/port/test/RuntimeSemTypeResolver.java
@@ -22,9 +22,9 @@ import io.ballerina.runtime.api.types.semtype.Builder;
 import io.ballerina.runtime.api.types.semtype.CellAtomicType;
 import io.ballerina.runtime.api.types.semtype.Context;
 import io.ballerina.runtime.api.types.semtype.Core;
+import io.ballerina.runtime.api.types.semtype.Definition;
 import io.ballerina.runtime.api.types.semtype.Env;
 import io.ballerina.runtime.api.types.semtype.SemType;
-import io.ballerina.runtime.api.types.semtype.Definition;
 import io.ballerina.runtime.internal.types.semtype.ErrorUtils;
 import io.ballerina.runtime.internal.types.semtype.FunctionDefinition;
 import io.ballerina.runtime.internal.types.semtype.FunctionQualifiers;
@@ -618,7 +618,7 @@ class RuntimeSemTypeResolver extends SemTypeResolver<SemType> {
             case NEVER -> Builder.neverType();
             case XML -> Builder.xmlType();
             case FUTURE -> Builder.futureType();
-            // FIXME: implement json type
+            // TODO: implement json type
 
             default -> throw new UnsupportedOperationException("Built-in ref type not implemented: " + td.typeKind);
         };
