61017f97fd396b4c779338e02d398b9ac4fea031
Heshan Padmasiri <heshanp@wso2.com>
2024-08-19 15:03:10 +0530

    Fix result caching for futures

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/utils/MapUtils.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/utils/MapUtils.java
@@ -118,6 +118,7 @@ public final class MapUtils {
     }
 
     private static boolean containsNilType(Type type) {
+        // FIXME:
         type = TypeUtils.getImpliedType(type);
         int tag = type.getTag();
         if (tag == TypeTags.UNION_TAG) {
