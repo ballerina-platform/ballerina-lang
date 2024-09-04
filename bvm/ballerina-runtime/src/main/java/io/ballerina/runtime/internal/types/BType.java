214af52954137b08b8f83669317acacc36dae6f4
Heshan Padmasiri <heshanp@wso2.com>
2024-09-04 10:17:53 +0530

    Fix string shape calculation

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/types/BType.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/types/BType.java
@@ -28,7 +28,6 @@ import io.ballerina.runtime.api.types.semtype.SemType;
 import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.internal.TypeChecker;
 
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -51,7 +50,6 @@ public abstract non-sealed class BType extends SemType implements Type, MutableS
     private Type cachedImpliedType = null;
     private volatile SemType cachedSemType = null;
     private TypeCreator.TypeMemoKey lookupKey = null;
-    private Map<SemType, CachedResult> cachedResults;
 
     protected BType(String typeName, Module pkg, Class<? extends Object> valueClass) {
         this.typeName = typeName;
@@ -275,7 +273,6 @@ public abstract non-sealed class BType extends SemType implements Type, MutableS
     public BType clone() {
         try {
             BType clone = (BType) super.clone();
-            clone.cachedResults = null;
             clone.cachedSemType = null;
             clone.setCachedImpliedType(null);
             clone.setCachedReferredType(null);
