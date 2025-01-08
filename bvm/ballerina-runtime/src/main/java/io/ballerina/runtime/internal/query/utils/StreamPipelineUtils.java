baf412cc86b6437dbf36d0c4fa142ccaffd9c905
chamikam <chamikamadhushan35@gmail.com>
2025-01-08 14:58:58 +0530

    Update select clause

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/utils/StreamPipelineUtils.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/utils/StreamPipelineUtils.java
@@ -1,16 +1,15 @@
 package io.ballerina.runtime.internal.query.utils;
 
-import io.ballerina.runtime.api.values.BArray;
-import io.ballerina.runtime.api.creators.ValueCreator;
-import io.ballerina.runtime.api.values.BString;
+
 import io.ballerina.runtime.internal.query.clauses.FromClause;
+import io.ballerina.runtime.internal.query.clauses.LimitClause;
 import io.ballerina.runtime.internal.query.clauses.SelectClause;
 import io.ballerina.runtime.internal.query.clauses.WhereClause;
 import io.ballerina.runtime.internal.query.pipeline.FrameContext;
 import io.ballerina.runtime.internal.query.pipeline.StreamPipeline;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 public class StreamPipelineUtils {
     public static <T> StreamPipeline<T> initializePipeline(Class<T> completionType, Object collection) {
@@ -23,14 +22,19 @@ public class StreamPipelineUtils {
         pipeline.addStage(fromClause);
     }
 
-    public static <T> void addSelectClause(StreamPipeline<T> pipeline, Map<String, String> selectMappings) {
-        SelectClause<T> selectClause = new SelectClause<>(selectMappings);
+    public static <T> void addSelectClause(StreamPipeline<T> pipeline, Function<FrameContext<T>, Object> selectMapper) {
+        SelectClause<T> selectClause = new SelectClause<>(selectMapper);
         pipeline.addStage(selectClause);
     }
 
     public static <T> void addWhereClause(StreamPipeline<T> pipeline, Predicate<FrameContext<T>> condition) {
         WhereClause<T> whereClause = new WhereClause<>(condition);
         pipeline.addStage(whereClause);
+    }
+
+    public static <T> void addLimitClause(StreamPipeline<T> pipeline, long limit){
+        LimitClause<T> limitClause = new LimitClause<>(limit);
+        pipeline.addStage(limitClause);
     }
 
 //    public static <T> BArray toBArray(StreamPipeline<T> pipeline) {
