92a888bc51c4b4e7b2d8289e4aac4e8f243a2446
chamikam <chamikamadhushan35@gmail.com>
2025-01-09 19:47:53 +0530

    Fix datatypes

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/pipeline/StreamPipeline.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/pipeline/StreamPipeline.java
@@ -1,41 +1,101 @@
 package io.ballerina.runtime.internal.query.pipeline;
 
-import io.ballerina.runtime.internal.query.clauses.PipelineStage;
+import io.ballerina.runtime.api.values.BArray;
+import io.ballerina.runtime.api.values.BCollection;
+import io.ballerina.runtime.api.values.BTypedesc;
+import io.ballerina.runtime.api.values.BValue;
+import io.ballerina.runtime.internal.query.utils.BallerinaIteratorUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.function.Function;
 
-public class StreamPipeline<T> {
-    private Stream<FrameContext<T>> stream;
-    private final Class<T> completionType;
-    private final List<PipelineStage<T>> stages = new ArrayList<>();
-    private final Object collection;
+/**
+ * A class that represents a stream pipeline for processing data.
+ */
+public class StreamPipeline<T, R> {
 
-    public StreamPipeline(Class<T> completionType, Object collection) {
+    private final Stream<T> stream;
+    private final List<Function<T, R>> pipelineStages;
+    private final BTypedesc constraintType;
+    private final BTypedesc completionType;
+
+    /**
+     * Constructor for creating a StreamPipeline.
+     *
+     * @param collection      The collection to process (Ballerina `BCollection`).
+     * @param constraintType  The type descriptor for the constraint type.
+     * @param completionType  The type descriptor for the completion type.
+     * @param isLazyLoading   Flag to indicate if lazy loading is enabled.
+     */
+    public StreamPipeline(Object collection, BTypedesc constraintType, BTypedesc completionType, boolean isLazyLoading) {
+        int a = 8;
+        this.stream = initializeStream((BCollection) collection);
+        this.pipelineStages = new ArrayList<>();
+        this.constraintType = constraintType;
         this.completionType = completionType;
-        this.collection = collection;
     }
 
-    public void addStage(PipelineStage<T> stage) {
-        stages.add(stage);
+    /**
+     * Adds a stage (function) to the pipeline.
+     *
+     * @param stage A function to process each element.
+     */
+    public void addStage(Function<T, R> stage) {
+        this.pipelineStages.add(stage);
     }
 
-    public void execute(Object collection) {
-        for (PipelineStage<T> stage : stages) {
-            stream = stage.apply(stream);
-        }
+    /**
+     * Executes the pipeline and returns the result stream.
+     *
+     * @return A stream of results after applying all pipeline stages.
+     */
+    public Stream<R> executePipeline() {
+        Stream<R> resultStream = this.stream.map(element -> {
+            R result = null;
+            for (Function<T, R> stage : this.pipelineStages) {
+                result = stage.apply(element);
+            }
+            return result;
+        });
+        return resultStream;
     }
 
-    public Object getCollection() {
-        return collection;
+    /**
+     * Converts the pipeline's result stream to a list of frames.
+     *
+     * @return A list of Ballerina `BMap` (frames).
+     */
+    public List<R> toFrames() {
+        Stream<R> resultStream = executePipeline();
+        List<R> frames = new ArrayList<>();
+        resultStream.forEach(frames::add);
+        return frames;
     }
 
-    public Stream<FrameContext<T>> getStream() {
-        return stream;
+    /**
+     * Initializes a Java stream from the provided Ballerina collection using BallerinaIteratorUtils.
+     *
+     * @param collection The Ballerina collection.
+     * @return A Java stream of elements.
+     */
+    private Stream<T> initializeStream(BCollection collection) {
+        return BallerinaIteratorUtils.toStream(collection);
     }
 
-    public Class<T> getCompletionType() {
+    /**
+     * Resets the pipeline for reuse.
+     */
+    public void reset() {
+        // Logic to reset the pipeline, if applicable
+    }
+
+    public BTypedesc getConstraintType() {
+        return constraintType;
+    }
+
+    public BTypedesc getCompletionType() {
         return completionType;
     }
 }
--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/utils/BallerinaIteratorUtils.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/utils/BallerinaIteratorUtils.java
@@ -9,12 +9,25 @@ import java.util.stream.StreamSupport;
 
 public class BallerinaIteratorUtils {
 
+    /**
+     * Converts a Ballerina collection to a Java stream.
+     *
+     * @param collection The Ballerina collection.
+     * @param <T>        The type of elements in the collection.
+     * @return A Java Stream of elements.
+     */
     public static <T> Stream<T> toStream(BCollection collection) {
         Iterator<T> javaIterator = getIterator(collection);
-
         return StreamSupport.stream(((Iterable<T>) () -> javaIterator).spliterator(), false);
     }
 
+    /**
+     * Returns a Java iterator for a given Ballerina collection.
+     *
+     * @param collection The Ballerina collection.
+     * @param <T>        The type of elements in the collection.
+     * @return A Java Iterator for the collection.
+     */
     public static <T> Iterator<T> getIterator(Object collection) {
         if (collection instanceof BCollection) {
             BIterator<?> iterator = ((BCollection) collection).getIterator();
@@ -26,6 +39,7 @@ public class BallerinaIteratorUtils {
                 }
 
                 @Override
+                @SuppressWarnings("unchecked")
                 public T next() {
                     return (T) iterator.next();
                 }
