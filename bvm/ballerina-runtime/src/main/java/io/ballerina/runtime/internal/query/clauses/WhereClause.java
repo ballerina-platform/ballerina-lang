41cb4cb76eaaece1f6bd735a374917381dd5c651
chamikam <chamikamadhushan35@gmail.com>
2025-01-20 16:14:19 +0530

    Fix from clause
    fixed the select to a extent

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/clauses/WhereClause.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/clauses/WhereClause.java
@@ -1,29 +1,52 @@
-//package io.ballerina.runtime.internal.query.clauses;
-//
-//import io.ballerina.runtime.internal.query.pipeline.FrameContext;
-//
-//import java.util.function.Predicate;
-//import java.util.stream.Stream;
-//
-///**
-// * The WhereClause filters the data in the stream based on the provided condition.
-// *
-// * @param <T> the type of data stored in the FrameContext
-// */
-//public class WhereClause<T> implements PipelineStage<T> {
-//    private final Predicate<FrameContext<T>> condition;
-//
-//    /**
-//     * Constructs a WhereClause with the specified condition.
-//     *
-//     * @param condition the predicate condition used to filter the stream
-//     */
-//    public WhereClause(Predicate<FrameContext<T>> condition) {
-//        this.condition = condition;
-//    }
-//
-//    @Override
-//    public Stream<FrameContext<T>> apply(Stream<FrameContext<T>> input) {
-//        return input.filter(condition); // Filters based on the predicate condition
-//    }
-//}
+package io.ballerina.runtime.internal.query.clauses;
+
+import io.ballerina.runtime.api.Environment;
+import io.ballerina.runtime.api.values.BFunctionPointer;
+import io.ballerina.runtime.internal.query.pipeline.Frame;
+
+import java.util.stream.Stream;
+
+/**
+ * Represents a `where` clause in the query pipeline that filters a stream of frames.
+ */
+public class WhereClause implements PipelineStage {
+    private final BFunctionPointer filterFunc;
+    private final Environment env;
+
+    /**
+     * Constructor for the WhereClause.
+     *
+     * @param filterFunc The function to filter frames.
+     */
+    public WhereClause(Environment env, BFunctionPointer filterFunc) {
+        this.filterFunc = filterFunc;
+        this.env = env;
+    }
+
+    public static WhereClause initWhereClause(Environment env, BFunctionPointer filterFunc) {
+        return new WhereClause(env, filterFunc);
+    }
+
+    /**
+     * Filters a stream of frames by applying the filter function to each frame.
+     *
+     * @param inputStream The input stream of frames.
+     * @return A filtered stream of frames.
+     * @throws Exception If an error occurs during filtering.
+     */
+    @Override
+    public Stream<Frame> process(Stream<Frame> inputStream) throws Exception {
+        return inputStream.filter(frame -> {
+            try {
+                // Apply the filter function to the current frame and evaluate the result.
+                Object result = filterFunc.call(env.getRuntime(), frame.getRecord().get("value"));
+                if (result instanceof Boolean) {
+                    return (Boolean) result; // Include frame if predicate is true.
+                }
+                throw new IllegalStateException("Filter function must return a boolean.");
+            } catch (Exception e) {
+                throw new RuntimeException("Error applying filter function.", e);
+            }
+        });
+    }
+}
