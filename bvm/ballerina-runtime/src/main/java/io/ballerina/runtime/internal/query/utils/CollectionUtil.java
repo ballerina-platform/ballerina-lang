4210fc0da3b4fb9c6a34d6178d3546c0d1c75272
chamikam <chamikamadhushan35@gmail.com>
2025-02-10 17:26:08 +0530

    Implement do clause
    do clause fixed

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/utils/CollectionUtil.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/utils/CollectionUtil.java
@@ -5,6 +5,7 @@ import io.ballerina.runtime.api.creators.ValueCreator;
 import io.ballerina.runtime.api.types.PredefinedTypes;
 import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BArray;
+import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BString;
 import io.ballerina.runtime.api.values.BValue;
 import io.ballerina.runtime.internal.query.pipeline.Frame;
@@ -14,6 +15,13 @@ import java.util.stream.Stream;
 
 public class CollectionUtil {
     private static final BString VALUE_FIELD = StringUtils.fromString("$value$");
+
+    public static void consumeStream(Object frameStream) {
+        Stream<Frame> strm = (Stream<Frame>) frameStream;
+        strm.forEach(frame -> {
+            BMap<BString, Object> result = frame.getRecord();
+        });
+    }
 
     public static BArray createArray(Stream<Frame> strm, BArray arr) {
         Object[] tmpArr = strm
