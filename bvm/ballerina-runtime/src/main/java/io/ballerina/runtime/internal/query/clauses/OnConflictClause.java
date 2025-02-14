93ad296ee5e48a05a996303ea8e68c97161e8369
chamikam <chamikamadhushan35@gmail.com>
2025-02-14 15:05:52 +0530

    on conflict clause added

---
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/clauses/OnConflictClause.java
@@ -0,0 +1,41 @@
+package io.ballerina.runtime.internal.query.clauses;
+
+import io.ballerina.runtime.api.Environment;
+import io.ballerina.runtime.api.values.BFunctionPointer;
+import io.ballerina.runtime.api.values.BMap;
+import io.ballerina.runtime.api.values.BString;
+import io.ballerina.runtime.internal.query.pipeline.Frame;
+
+import java.util.stream.Stream;
+
+public class OnConflictClause implements PipelineStage {
+    private final BFunctionPointer onConflictFunction;
+    private final Environment env;
+
+    public OnConflictClause(Environment env, BFunctionPointer onConflictFunction) {
+        this.onConflictFunction = onConflictFunction;
+        this.env = env;
+    }
+
+    public static OnConflictClause initOnConflictClause(Environment env, BFunctionPointer onConflictFunction) {
+        return new OnConflictClause(env, onConflictFunction);
+    }
+
+
+    @Override
+    public Stream<Frame> process(Stream<Frame> inputStream) {
+        return inputStream.map(frame -> {
+            try {
+                Object result = onConflictFunction.call(env.getRuntime(), frame.getRecord());
+                if (result instanceof BMap) {
+                    frame.updateRecord((BMap<BString, Object>) result);
+                    return frame;
+                } else {
+                    throw new RuntimeException("Invalid on conflict result: " + result);
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Error during on conflict", e);
+            }
+        });
+    }
+}
