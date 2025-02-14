ea1f6348392d1b371acb339921057e598c13fe58
Heshan Padamsiri <hpheshan@gmail.com>
2025-02-14 06:51:04 +0530

    Fix checkstyle

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/types/semtype/CacheFactory.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/types/semtype/CacheFactory.java
@@ -8,8 +8,8 @@ public class CacheFactory {
     private CacheFactory() {
     }
 
-    private final static int LIGHT_CACHE_SIZE = 100;
-    private final static int DEFAULT_CACHE_SIZE = 100_000;
+    private static final int LIGHT_CACHE_SIZE = 100;
+    private static final int DEFAULT_CACHE_SIZE = 100_000;
 
     public static <K, V> Cache<K, V> createPerInstanceCache() {
         return Caffeine.newBuilder().maximumSize(LIGHT_CACHE_SIZE).build();
