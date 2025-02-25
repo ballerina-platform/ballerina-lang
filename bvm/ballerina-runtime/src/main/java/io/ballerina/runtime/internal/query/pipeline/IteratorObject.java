d4c4d39199f7074a932ac406eab341a623fbc2f9
chamikam <chamikamadhushan35@gmail.com>
2025-02-25 10:11:14 +0530

    returning stream issue fixed( should consider more)

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/pipeline/IteratorObject.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/pipeline/IteratorObject.java
@@ -1,6 +1,7 @@
 package io.ballerina.runtime.internal.query.pipeline;
 
 import io.ballerina.runtime.api.types.Type;
+import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BString;
 import io.ballerina.runtime.api.creators.ValueCreator;
@@ -16,15 +17,20 @@ public class IteratorObject {
     private static final BString VALUE_FIELD = StringUtils.fromString("$value$");
 
     public static Object next(Object pipeline) {
-        Iterator<Frame> iterator = ((StreamPipeline) pipeline).getStream().iterator();
-        Type describingType = ((StreamPipeline) pipeline).getConstraintType().getDescribingType();
+        Type constraintType = ((StreamPipeline) pipeline).getConstraintType().getDescribingType();
+        Type completionType = ((StreamPipeline) pipeline).getCompletionType().getDescribingType();
+        try {
+            Iterator<Frame> iterator = ((StreamPipeline) pipeline).getStream().iterator();
 
-        if (iterator.hasNext()) {
-            Frame frame = iterator.next();
-            BMap<BString, Object> value = (BMap<BString, Object>) frame.getRecord().get(VALUE_FIELD);
-            BMap<BString, Object> record = ValueCreator.createRecordValue(BALLERINA_QUERY_PKG_ID, "nextRecord");
-            record.put(StringUtils.fromString("value"), value);
-            return record;
+            if (iterator.hasNext()) {
+                Frame frame = iterator.next();
+                BMap<BString, Object> value = (BMap<BString, Object>) frame.getRecord().get(VALUE_FIELD);
+                BMap<BString, Object> record = ValueCreator.createRecordValue(BALLERINA_QUERY_PKG_ID, "nextRecord");
+                record.put(StringUtils.fromString("value"), value);
+                return record;
+            }
+        } catch (Exception e) {
+            return null;
         }
 
         return null;
