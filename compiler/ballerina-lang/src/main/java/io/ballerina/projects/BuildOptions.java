76703b9e375dd0c9f9ec1b14337c7d5f8f9d679d
rdulmina <rdulmina@gmail.com>
2025-03-27 14:56:34 +0530

    Revert "Remove experimental from CompilationOptions"
    This reverts commit 4dc75b8e45e9295b1f58fa60fc559c1a05952acc.

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/BuildOptions.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/BuildOptions.java
@@ -91,7 +91,7 @@ public class BuildOptions {
      */
     @Deprecated(forRemoval = true)
     public boolean experimental() {
-        return false;
+        return this.compilationOptions.experimental();
     }
 
     public boolean observabilityIncluded() {
@@ -199,6 +199,7 @@ public class BuildOptions {
 
         CompilationOptions compilationOptions = this.compilationOptions.acceptTheirs(theirOptions.compilationOptions());
         buildOptionsBuilder.setOffline(compilationOptions.offlineBuild);
+        buildOptionsBuilder.setExperimental(compilationOptions.experimental);
         buildOptionsBuilder.setObservabilityIncluded(compilationOptions.observabilityIncluded);
         buildOptionsBuilder.setDumpBir(compilationOptions.dumpBir);
         buildOptionsBuilder.setDumpBirFile(compilationOptions.dumpBirFile);
@@ -342,6 +343,7 @@ public class BuildOptions {
          */
         @Deprecated(forRemoval = true)
         public BuildOptionsBuilder setExperimental(Boolean value) {
+            compilationOptionsBuilder.setExperimental(value);
             return this;
         }
 
