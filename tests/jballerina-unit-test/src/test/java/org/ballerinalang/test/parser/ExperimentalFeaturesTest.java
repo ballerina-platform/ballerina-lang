8991d7c28a89802bd8da81edd304e601c11ea440
rdulmina <rdulmina@gmail.com>
2025-04-03 14:06:18 +0530

    Address review suggestions

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/parser/ExperimentalFeaturesTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/parser/ExperimentalFeaturesTest.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.org).
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -28,6 +28,8 @@ import java.util.Arrays;
 
 /**
  * Test for using experimental features without enabling them.
+ *
+ * @since 2201.13.0
  */
 public class ExperimentalFeaturesTest {
 
