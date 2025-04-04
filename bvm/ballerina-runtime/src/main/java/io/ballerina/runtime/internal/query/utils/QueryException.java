b151e9ec4c11383a0f1f8f1c87081bc29e6b4de9
chamikam <chamikamadhushan35@gmail.com>
2025-04-04 10:27:32 +0530

    Fix distinct error handling

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/utils/QueryException.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/query/utils/QueryException.java
@@ -1,14 +1,10 @@
 package io.ballerina.runtime.internal.query.utils;
 
 import io.ballerina.runtime.api.Module;
-import io.ballerina.runtime.api.constants.TypeConstants;
 import io.ballerina.runtime.api.creators.ErrorCreator;
-import io.ballerina.runtime.api.types.PredefinedTypes;
 import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BString;
-import io.ballerina.runtime.internal.TypeChecker;
-import io.ballerina.runtime.internal.types.BErrorType;
 import io.ballerina.runtime.internal.values.ErrorValue;
 import io.ballerina.runtime.internal.values.MapValueImpl;
 
@@ -41,8 +37,8 @@ public class QueryException extends RuntimeException {
     }
 
     public BError createDistinctError(String typeIdName, Module typeIdPkg, BError error) {
-        MapValueImpl<BString, Object> details = new MapValueImpl<>(PredefinedTypes.TYPE_ERROR_DETAIL);
-        return new ErrorValue(new BErrorType(TypeConstants.ERROR, PredefinedTypes.TYPE_ERROR.getPackage(), TypeChecker
-                .getType(details)), error.getErrorMessage(), null, details, typeIdName, typeIdPkg);
+        MapValueImpl<BString, Object> details = (MapValueImpl<BString, Object>) error.getDetails();
+        return new ErrorValue(error.getType(), error.getErrorMessage(),
+                error.getCause(), details, typeIdName, typeIdPkg);
     }
 }
