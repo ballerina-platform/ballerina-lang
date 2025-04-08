485a0ea126ab5de459adb93778b03a0e2ac0bb5f
chamikam <chamikamadhushan35@gmail.com>
2025-04-08 14:50:30 +0530

    Add copyright to test files

--- tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/query/QueryInputTypeTest.java
+++ tests/jballerina-unit-test/src/test/java/org/ballerinalang/test/query/QueryInputTypeTest.java
@@ -15,6 +15,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
+
 package org.ballerinalang.test.query;
 
 import io.ballerina.runtime.api.utils.StringUtils;
