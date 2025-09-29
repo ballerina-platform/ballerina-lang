9415c26f35175a865508fafb959795b675dbdc40
NipunaMadhushan <nipuna.madhushan96@gmail.com>
2025-09-29 10:08:48 +0530

    Update error message when tracer is not initialized

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/observability/tracer/TracersStore.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/observability/tracer/TracersStore.java
@@ -35,6 +35,7 @@ public class TracersStore {
     private TracerProvider tracerProvider;
     private Map<String, Tracer> store;
     private static final PrintStream consoleError = System.err;
+    private static final PrintStream consoleOut = System.out;
     private static final TracersStore instance = new TracersStore();
     private ContextPropagators propagators;
 
@@ -73,7 +74,7 @@ public class TracersStore {
                     tracer = tracerProvider.getTracer(serviceName);
                 } catch (Throwable e) {
                     tracer = io.opentelemetry.api.trace.TracerProvider.noop().get("");
-                    consoleError.println("error: tracing disabled as getting tracer for " + serviceName + " service. "
+                    consoleOut.println("warning: tracing disabled as getting tracer for " + serviceName + " service. "
                             + e.getMessage());
                 }
                 store.put(serviceName, tracer);
