fbbf73194550a01fd684268a3d1f70f0b72d3d84
azinneera <azinneera@gmail.com>
2025-10-31 22:38:39 +0530

    Address review suggestions

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/BuildToolResolution.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/BuildToolResolution.java
@@ -112,11 +112,11 @@ public class BuildToolResolution {
 
         PackageLockingMode packageLockingMode = getPackageLockingMode(currentProject);
         updateLockedToolDependencyVersions(buildTools, currentProject);
-        List<BuildTool> resolvedTools = resolveToolVersions(packageLockingMode, offline, buildTools);
+        List<BuildTool> resolvedTools = resolveToolVersions(packageLockingMode, buildTools);
         this.resolvedTools.addAll(resolvedTools);
     }
 
-    private List<BuildTool> resolveToolVersions(PackageLockingMode packageLockingMode, boolean offline,
+    private List<BuildTool> resolveToolVersions(PackageLockingMode packageLockingMode,
                                                 List<BuildTool> unresolvedTools) {
         List<BuildTool> toolResolutionResponseOffline =
                 getToolResolutionResponseOffline(unresolvedTools, packageLockingMode);
