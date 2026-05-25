dfc4d065c2a58ae66d321e0f491ba9da6d5418dd
Thevakumar-Luheerathan <Luhee27@gmail.com>
2026-05-25 12:26:09 +0530

    Allow field with default value as non-required

--- misc/compiler-plugins/modules/configurable-schema-generator/src/test/java/org/ballerinalang/config/schema/generator/ConfigSchemaGenTaskTest.java
+++ misc/compiler-plugins/modules/configurable-schema-generator/src/test/java/org/ballerinalang/config/schema/generator/ConfigSchemaGenTaskTest.java
@@ -93,7 +93,8 @@ public class ConfigSchemaGenTaskTest {
     public Object[][] dpMethod2() {
         return new Object[][]{{"DefaultModuleProjects", "ComplexTypeConfigs2", false},
                 {"DefaultModuleProjects", "ComplexTypeConfigs3", false},
-                {"MultiModuleProjects", "IndirectImports", false}};
+                {"MultiModuleProjects", "IndirectImports", false},
+                {"DefaultModuleProjects", "RecordTypeConfigs", false}};
     }
 
     static Project loadBuildProject(Path projectPath, boolean isSingleFileProject) {
